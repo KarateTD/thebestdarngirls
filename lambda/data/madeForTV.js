a95870c87e7783e1ba534acb9521a1c2785d5791
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2018-12-24 23:35:43 -0500

    fix grammer

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -2,7 +2,7 @@ var madeForTV = [
 {
 	"option":"1",
 	"mtitle":"Jingle Around The Clock",
-	"review":"Elle throws a Christmas bash for her and her friends every year.  But with the added pressure at work, she is having a hard time juggling everything on her own.  She must learn to ask for help or she will lose her job and her traditions.  During this time a year, it's good to be reminded that it is ok to slow down and ask for help.  So are you guilty of pulling an Elle?<br/><br/>3.5 out of 5 stars.",
+	"review":"Elle throws a Christmas bash for herself and her friends every year.  But with the added pressure at work, she is having a hard time juggling everything on her own.  She must learn to ask for help or she will lose her job and her traditions.  During this time a year, it's good to be reminded that it is ok to slow down and ask for help.  So are you guilty of pulling an Elle?<br/><br/>3.5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/jinglearoundtheclock.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/jinglearoundtheclock.jpg"
@@ -11,7 +11,7 @@ var madeForTV = [
 {
 	"option":"2",
 	"mtitle":"A Golden Christmas",
-	"review":"As a child Jessie found a dog that introduced her to her crush and best friend in the woods.  Because the gave fake names, Jessie was never able to find him again.  Years later, she plans on buying her parents house to be closer to the same woods.  But without warning her parents sold it.  They sold it to her long lost and unknown crush Michael.  She wants the house but doesn't know from whom she is take it.  This is a sweet movie of love lost and love found but it's forgettable.<br/><br/>3 out of 5 stars.",
+	"review":"As a child Jessie found a dog that introduced her to her crush and best friend in the woods.  Because the gave fake names, Jessie was never able to find him again.  Years later, she plans on buying her parents house to be closer to the same woods.  But without warning her parents sold it.  They sold it to her long lost and unknown crush Michael.  She wants the house but doesn't know from whom she is taken it.  This is a sweet movie of love lost and love found but it's forgettable.<br/><br/>3 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/agoldenchristmas.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/agoldenchristmas.jpg"
@@ -20,7 +20,7 @@ var madeForTV = [
 {
 	"option":"3",
 	"mtitle":"Christmas at Grand Valley",
-	"review":"While reconsidering her career, Kelly agrees to run the Christmas Camp at her cousin's lodge.  Two of the kids belong to Leo, the man in charge of determing if the lodge should be shutdown.  Kelly hopes to save the lodge and her brothers job.  Once again the kids save the day. Wiser than their years, they make the movie funny and charming.<br/><br/>3.5 out of 5 stars.",
+	"review":"While reconsidering her career, Kelly agrees to run the Christmas Camp at her cousin's lodge.  Two of the kids belong to Leo, the man in charge of determing if the lodge should be shutdown.  Kelly hopes to save the lodge and her cousin's job.  Once again the kids save the day. Wiser than their years, they make the movie funny and charming.<br/><br/>3.5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/christmasatgrandvalley.jpeg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/christmasatgrandvalley.jpeg"
@@ -38,7 +38,7 @@ var madeForTV = [
 {
 	"option":"5",
 	"mtitle":"A Christmas Kiss",
-	"review":"When Wendy realizes the stranger she kissed is her boss's boyfriend, she has to choose between work and love.  It will take a broken nose and flutters to untangle this mess.  Be prepared to laugh, talk to the screen, and say OMG more than once.<br/><br/>5 out of 5 stars.",
+	"review":"When Wendy realizes the stranger she kissed is her boss's boyfriend, she has to choose between work and love.  It will take a broken nose and flutters to untangle this mess.  Be prepared to laugh, yell to the screen, and say OMG more than once.<br/><br/>5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/achristmaskiss.jpeg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/achristmaskiss.jpeg"
