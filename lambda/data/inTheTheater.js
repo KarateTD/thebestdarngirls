1672e9a5627d38a102b5f182f2c4389b016f45a4
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-01-09 23:59:12 -0500

    add upside

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
 	"option":"1",
+	"mtitle":"The Upside",
+	"review":"Dell, trying to get signatures to avoid jail time, ends up landing a job as a live-in caregiver.  Phillip inspires Dell to do better and Dell inspires Phillip to look beyond what he can't do.  This serious yet funny movie proves that everyone has problems but we are all blessed.  See it in theaters today.<br/><br/>3.5 out of 5 stars.",
+	"image":{
+		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/theupside.jpeg",
+		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/theupside.jpeg"
+	}
+}
+{
+	"option":"2",
 	"mtitle":"Vice",
 	"review":"Lynne Cheney told her husband to straighten up or she would walk out.  That statement leads him to become the most powerful ghost in U.S. history.  Republicans watch this for the start of Fox News.  Democrats watch this for the Unitary Executive Theory.  Both sides should watch this for the focus groups and ask yourself: Am I leading or am I being led?<br/><br/>3 out of 5 stars.",
 	"image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"2",
+	"option":"3",
 	"mtitle":"Mary Queen of Scots",
 	"review":"Queen Elizabeth has had Protestant rule over Catholic Scotland.  But when Queen Mary comes back after the death of her husband, she is a threat to Elizabeth.  This living chess match between these two queens will be wrapped up in gender, religion, duty, and birth right.  It is one to see on the big screen but not to buy.<br/><br/>3.5 out of 5 stars.",
 	"image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"3",
+	"option":"4",
 	"mtitle":"Disney Mary Poppins Returns",
 	"review":"With Michael Banks on the brink of losing his home, Mary Poppins comes back to help him through his children, Georgie, and older twins, Annabel and John.  These children can see the wolf and sheep's clothing but can they convince their dad and save their home.  The movie is memorable but the songs are not.  However, see it on the big screen for all its visual glory.<br/><br/>3 out of 5 stars.",
 	"image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"4",
+	"option":"5",
 	"mtitle":"Second Act",
 	"review":"Maya wishes her street smarts were enough and her god-son Gil helps.  He gives her a social media make over and she gets an interview.  She sits and gives a honest review of their products and gets hired as a consultant.  But this job, will bring her lies and her past all crashing down.  This movie will emphasize how much validation we put into college degrees.  Also, every seen with Leah is pure gold.  You will wish you had Joan for a friend.  This is a surprisingly good comedy that deserves to be seen.<br/><br/>3.5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/secondact.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/secondact.jpg"
-	}
-},
-{
-	"option":"5",
-	"mtitle":"Welcome to Marwen",
-	"review":"After a brutal attack, Mark can no longer draw.  So he created a town named Marwen.  He has dolls that represent all the important women in his life and Nazis for his attackers.  However, with the sentencing hearing days away, Mark is shutting down and the Nazis are pulling him apart.  This movie is just lacking something.  While the graphics are amazing, the story is incomplete and jumps plotlines from triumph to love to triumph.  It just falls flat.<br/><br/>2 out of 5 stars.",
-	"image":{
-		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/welcometomarwen.jpg",
-		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/welcometomarwen.jpg"
 	}
 }
 ];
