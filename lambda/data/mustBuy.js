27d9b88e9be3c8877a8fa26d3d69a1f75a8fcf5d
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-01-22 20:51:16 -0500

    update for the hate u give

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,15 @@
 var mustBuy = [
 {
-	"option":"1",
+    "option":"1",
+    "mtitle":"The Hate U Give",
+    "review":"After witnessing her friend, Khalil being shot by a police officer, Starr must determine if she should talk or remain quiet.  Her family, her friends, and her safety all hang in the balance.  This movie covers so many topics in 2 hours.  It covers police brutality, racism, no snitching, gang violence, the talk, and inherited hatred.  You should not pass judgement on this movie before you see it.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thehateugive.jpeg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thehateugive.jpeg"
+    }
+},
+{
+	"option":"2",
 	"mtitle":"Bad Times at the El Royale",
 	"review":"A priest, a singer, a woman, and vacuum salesman check into the bi-state El Royale hotel and casino.  Everyone has secrets and everyone is being watched.  This movie is a 100% thrill ride with so many twists and turns.  Every minute is enjoyable and will keep you guessing.  There will be blood.<br/><br/>5 out of 5 stars.",
 	"image":{
@@ -9,7 +18,7 @@ var mustBuy = [
 	}
 },
 {
-	"option":"2",
+	"option":"3",
 	"mtitle":"White Boy Rick",
 	"review":"Rick was given a choice, help the FBI or your dad will go to jail for 10 years.  This decision will leave a sad mark in the record book.  When watching this movie, ask yourself who is to blame for Rick's rise in the drug world.  Rick, his dad, or the government.  This movie is a different kind of cautionary tale.<br/><br/>4 out of 5 stars.",
 	"image":{
@@ -18,30 +27,21 @@ var mustBuy = [
 	}
 },
 {
-	"option":"3",
+	"option":"4",
 	"mtitle":"A Simple Favor",
-	"review":"Stephanie unexpectantly becomes friends with Emily.  But when Emily goes missing, Stephanie will learn how little she truely knew her.  This movie isn't just a roller coaster ride, it's the entire theme park.  It has love, sex, lies, and murder.  See it now.<br/><br/>5 out of 5 stars.",
+	"review":"Stephanie unexpectedly becomes friends with Emily.  But when Emily goes missing, Stephanie will learn how little she truly knew her.  This movie isn't just a roller coaster ride, it's the entire theme park.  It has love, sex, lies, and murder.  See it now.<br/><br/>5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/asimplefavor.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/asimplefavor.jpg"
 	}
 },
 {
-	"option":"4",
+	"option":"5",
 	"mtitle":"SMALLFOOT",
 	"review":"Migo dreams of being the next gong-banger.  Later, when a practice run fails but the sun still comes up, he goes down the mountain to question the stone keeper.  That's when he finds the so-called Smallfoot.  This is a great movie about the power and the constraint of belief.  Watch it with your kids today.<br/><br/>4 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/smallfoot.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/smallfoot.jpg"
-	}
-},
-{
-	"option":"5",
-	"mtitle":"Mission Impossible: Fallout",
-	"review":"Ethan has to fix is mistakes but with Erica's operative Walker watching his every move.  How will Ethan handle it when he is the one under suspicion.  This mission is impossible to miss and a 100% thrill ride.  See it on the big screen today.<br/><br/>5 out of 5 stars.",
-	"image":{
-		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/missionimpossible6.jpg",
-		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/missionimpossible6.jpg"
 	}
 }
 ];
@@ -99,7 +99,7 @@ module.exports = mustBuy;
 {
 	"option":"3",
 	"mtitle":"Spider-Man: Into the Spider-Verse",
-	"review":"In 24 hours, Miles Morales is bitten by a radio active spider, witnesses the death of Spider-Man, is hunted, and discover 5 other Spider-Men.  Each Spider-Man has their own back story, but they have one thing in common: Being a reluctant superhero.  Miles will sadly join this club.  Seeing the seemless mesh of different animation styles with a strong story and great life lessons for kids is superb.  Stay pass the end credits and look out for Stan Lee.<br/><br/>5 out of 5 stars.",
+	"review":"In 24 hours, Miles Morales is bitten by a radio active spider, witnesses the death of Spider-Man, is hunted, and discover 5 other Spider-Men.  Each Spider-Man has their own back story, but they have one thing in common: Being a reluctant superhero.  Miles will sadly join this club.  Seeing the seamless mesh of different animation styles with a strong story and great life lessons for kids is superb.  Stay pass the end credits and look out for Stan Lee.<br/><br/>5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/spidermanintothespiderverse.jpg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/spidermanintothespiderverse.jpg"
