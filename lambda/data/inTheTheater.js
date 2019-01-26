e051b860a59e64e4883882ba542df1ab9044305f
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-01-25 20:47:04 -0500

    update numbers

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -9,7 +9,7 @@ var inTheTheaters = [
     }
 },
 {
-	"option":"1",
+	"option":"2",
 	"mtitle":"If Beale Street Could Talk",
 	"review":"Tish and Fonny was just about to start their lives together when he is accused of rape.  With a baby on the way, Tish and her family will do anything to get him out.  While you may not like the ending, it is honest.  This movie is beautifully shot but some things are lost in the audio choices.<br/><br/>3.5 out of 5 stars.",
 	"image":{
@@ -18,7 +18,7 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"2",
+	"option":"3",
 	"mtitle":"On the Basis of Sex",
 	"review":"Ruth Bader Ginsburg has fought sex discrimination in a world that doesn't believe it exist.  But when she is presented a case where a man is discriminated against because of his gender, she will use that as a stepping stone to take down an unjust system.  This movie will inspire all to see that discrimination can effect everyone and bring out the activist in you.<br/><br/>3.5 out of 5 stars.",
 	"image":{
@@ -27,7 +27,7 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"3",
+	"option":"4",
 	"mtitle":"Replicas",
 	"review":"After his family dies in a car crash, Ben, a bio medicine engineer, will use cloning and brain mapping to bring them back.  This movie will reach a cult following in the constant questions of 'what if' and morality.  If you like weird Sci-Fi, see it matinee.  Otherwise, wait for Netflix.<br/><br/>2.5 out of 5 stars.",
 	"image":{
@@ -36,21 +36,12 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"4",
+	"option":"5",
 	"mtitle":"The Upside",
 	"review":"Dell, trying to get signatures to avoid jail time, ends up landing a job as a live-in caregiver.  Phillip inspires Dell to do better and Dell inspires Phillip to look beyond what he can't do.  This serious yet funny movie proves that everyone has problems but we are all blessed.  See it in theaters today.<br/><br/>3.5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/theupside.jpeg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/theupside.jpeg"
-	}
-},
-{
-	"option":"5",
-	"mtitle":"Vice",
-	"review":"Lynne Cheney told her husband to straighten up or she would walk out.  That statement leads him to become the most powerful ghost in U.S. history.  Republicans watch this for the start of Fox News.  Democrats watch this for the Unitary Executive Theory.  Both sides should watch this for the focus groups and ask yourself: Am I leading or am I being led?<br/><br/>3 out of 5 stars.",
-	"image":{
-		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/vice.jpg",
-		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/vice.jpg"
 	}
 }
 ];
