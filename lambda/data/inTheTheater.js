85833ca7171455e0a4ca6045766303f70044d62e
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-01-27 23:22:16 -0500

    add 5 tv and 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"The LEGO Movie 2: The Second Part",
+    "review":"In 5 years, the Systar aliens have changed Bricksburg to Apocalypseburg.  Now General Sweet Mayhem has kidnapped Emmet's friends to convince Batman to marry Queen Watevra Wa-Nabi.  Emmet must save his friends before his dark premonition comes true.  This is how sequels should be done.  It is funnier, more sincere, and better built than the first.  While there is no need to stay after the end credits, you should stay for the credit song.  And no matter how hard you try, the Catchy Song will stay with you..<br/><br/>5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thelegomovie2thesecondpart.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thelegomovie2thesecondpart.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Serenity",
+    "review":"Baker is faced with a moral dilemma when his ex-wife Karen ask him to kill her new husband Frank.  She doesn't want Baker to kill him for the abuse, she wants Baker to do it for their son Patrick.  Patrick and Baker share a unique connection.  When Baker gets a visit from Reid, Baker's world is turned upside down.  This is the most confusing movie since Inception.  You will leave the theater with more questions than answers.<br/><br>2 out of 5 stars",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/serenity.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/serenity.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle":"The Kid Who Would Be King",
     "review":"After finding Excalibur, Alex has 4 days to gather his knights, find a magical portal, and defeat King Arthur's evil half-sister Morgana.  If he loses, the world will be her slaves.  Don't miss the first 2 minutes for a brief King Arthur recap.  This movie wonderfully stitches together computer animation and the real world with lots of laughs.  See it with the family.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-	"option":"2",
+	"option":"4",
 	"mtitle":"If Beale Street Could Talk",
 	"review":"Tish and Fonny was just about to start their lives together when he is accused of rape.  With a baby on the way, Tish and her family will do anything to get him out.  While you may not like the ending, it is honest.  This movie is beautifully shot but some things are lost in the audio choices.<br/><br/>3.5 out of 5 stars.",
 	"image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
 	}
 },
 {
-	"option":"3",
+	"option":"5",
 	"mtitle":"On the Basis of Sex",
 	"review":"Ruth Bader Ginsburg has fought sex discrimination in a world that doesn't believe it exist.  But when she is presented a case where a man is discriminated against because of his gender, she will use that as a stepping stone to take down an unjust system.  This movie will inspire all to see that discrimination can effect everyone and bring out the activist in you.<br/><br/>3.5 out of 5 stars.",
 	"image":{
 		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/onthebasisofsex.jpeg",
 		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/onthebasisofsex.jpeg"
-	}
-},
-{
-	"option":"4",
-	"mtitle":"Replicas",
-	"review":"After his family dies in a car crash, Ben, a bio medicine engineer, will use cloning and brain mapping to bring them back.  This movie will reach a cult following in the constant questions of 'what if' and morality.  If you like weird Sci-Fi, see it matinee.  Otherwise, wait for Netflix.<br/><br/>2.5 out of 5 stars.",
-	"image":{
-		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/replicas.jpg",
-		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/replicas.jpg"
-	}
-},
-{
-	"option":"5",
-	"mtitle":"The Upside",
-	"review":"Dell, trying to get signatures to avoid jail time, ends up landing a job as a live-in caregiver.  Phillip inspires Dell to do better and Dell inspires Phillip to look beyond what he can't do.  This serious yet funny movie proves that everyone has problems but we are all blessed.  See it in theaters today.<br/><br/>3.5 out of 5 stars.",
-	"image":{
-		"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/theupside.jpeg",
-		"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/theupside.jpeg"
 	}
 }
 ];
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
 {
     "option":"1",
-    "mtitle":"Escaping the Madhouse: The Nellie Bly Story",
-    "review":"Nellie Brown wakes up in the Bellvue Hospital with no memory of her former life.  She tries to speak out against the abuse against the patients.  But with no rights or memory, who will help her? Nellie Bly changed journalism and mental illness institutions forever.  And it's astounding how many days she was there, what it did to her, and all the women around her.  Watch it with the lights on.<br/><br/>5 out of 5 stars.",
+    "mtitle":"Love You to Death",
+    "review":"Police are called to a bloody crime scene.  The find hundreds of pills, a dead mother, and a missing daughter.  As the search for Esme, they learn the horrible truth about her mother, her illness, and her boyfriend.  Inspired by the story of Dee Dee and Gypsy Rose, this movie will make you question if justice was served.  Pay attention.  The first 40 minutes are told from Camile's point of view and the rest from Esme's.<br/><br/>4 out of 5 stars",
     "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/escapingthemadhouse.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/escapingthemadhouse.jpg"
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/loveyoutodeath.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/loveyoutodeath.jpg"
     }
 },
 {
     "option":"2",
-    "mtitle":"Winter Love Story",
-    "review":"Cassie must go on a book tour to drum up book sales.  To attract more readers, her publicist pairs her with a successful author named Elliot.  The thought of standing in front of a crowd makes Cassie weak in the knees.  The thought of letting his readers down is causing Elliot writer's block.  The two will lean on each other to gain the strength and confidence to grow.  This is a sweet story that follows the Hallmark format.<br/><br/>2.5 out of 5 stars.",
+    "mtitle":"SnowComing",
+    "review":"Samantha comes back to her hometown hoping to avoid her ex-boyfriend and superstar athlete Jake.  Only to learn Jake is staying at her dad's house.  As they help prepare for SnowComing, the spark between Jack and Samantha grow.  But can one nosey reporter bring it all down?  The individual stories of the characters' growth is more interesting than the love story.  It follows the Hallmark format too closely and it can be skipped.<br/><br/>3.5 out of 5 stars",
     "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/winterlovestory.jpeg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/winterlovestory.jpeg"
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/snowcoming.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/snowcoming.jpg"
     }
 },
 {
     "option":"3",
-    "mtitle":"Winter Princess",
-    "review":"Carlie wants to help a small town by throwing a ball to encourage year-round work.  When Jessie finds out that she is a Princess, he vows to keep her secret.  The moment Carlie's brother, father, and ex love interest comes to her job, she knows its a matter of time before the world knows her real identity.  This movie breaks from the mold because the female is already a Princess. However, the rest follows the Hallmark format.<br/><br/>2.5 out of 5 stars.",
+    "mtitle":"Seduced by a Killer",
+    "review":"Tessa is overjoyed to hear she is removed from the wait list and able to attend VIT.  She won't have to worry about a long distance relationship because her boyfriend, Will, is attending the same school.  When he is murdered, Tessa goes on a path of self-destruction that started before she was born.  This movie just doesn't grab you attention even with the plot twist.  It can be skipped.<br/><br/>2 out of 5 stars",
     "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/winterprincess.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/winterprincess.jpg"
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/seducedbyakiller.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/seducedbyakiller.jpg"
     }
 },
 {
     "option":"4",
-    "mtitle":"When Vows Break",
-    "review":"When her sister Ella goes missing on her honeymoon, Lydia suspects her abusive husband Tolin killed her.  She digs up dirt and finds investigators to help with the search for answers.  But when her digging pulls up the truth, can she keep telling a lie? This movie doesn't save the twist for the last 5 minutes.  It basis the plot around the twist.  It gives the viewer a great conundrum to solve.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Bixler High Private Eye",
+    "review":"Xander heads to Bixler to find his missing father.  When the trail goes cold Xander will need his classmate, Margaret, his grandpa, and a dog to find his father.  This movie is a funny, educational, and well-written mystery.  It keeps the viewer guessing while learning.  The movie could become a TV show or a film series.<br/><br/>4.5 out of 5 stars",
     "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/whenvowsbreak.png",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/whenvowsbreak.png"
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/bixlerhighprivateeye.png",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/bixlerhighprivateeye.png"
     }
 },
 {
     "option":"5",
-    "mtitle":"Best Friend's Betrayal",
-    "review":"Jess finds romance with Nick, a crime author.  But when his writings come to life, Katie, Jess's best friend, is on the case.  This movie provides enough excitement and suspects to keep the viewer watching.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"Deadly Match",
+    "review":"Trina creates an app to earn money for school.  However, when she discovers its being used for prostitution she wants to shut it down.  Then she learns her friend Jade was a call-girl and her killer maybe a member of the app.  She decides to keep the app open and find the killer.  Unlike most mysteries, no hint of the killer is given to tho the last 10 minutes.  You will have fun speculating with each new person in Trina's life.<br/><br/>4 out of 5 stars",
     "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/bestfriendsbetrayal.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/bestfriendsbetrayal.jpg"
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/deadlymatch.jpeg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/deadlymatch.jpeg"
     }
 }
 ];
