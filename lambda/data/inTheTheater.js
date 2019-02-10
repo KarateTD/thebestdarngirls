0764ab5ec5283756422ec5f702a148fc6a45fcde
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-02-10 15:07:42 -0500

    add one movie

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Cold Pursuit",
+    "review":"After receiving Kehoe's Citizen of the Year award, Nels Coxman learns is son died of a drug overdose.  Nels goes to his work's garage to commit suicide when he finds his son's friend, Dante bleeding and crying in the corner.  Dante tells Nelson, his son was killed over drugs he stole.  Nelson demands the name of his son's killer.  A name that will set off a killing spree.  This movie is nothing new.  The double crossing, killing, and setups are predictable and pretty boring.  It's the women of Kehoe that will make you laugh.  See it matinee or wait for Redbox.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/coldpursuite.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/coldpursuite.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"What Men Want",
     "review":"After a wild bachelorette party, Ali can hear men's thoughts.  At first, she considers getting rid of this ability.  Then, she decides to use it to enter the boys club at her sports management company.  What could go wrong?  While this movie does make fun of men, it doesn't demonize them.  It allows the viewer to see that Ali's biggest obstacle is herself.  This is a great date movie but not a family movie.  Also, you will leave wanting to know: Who voted against Ali?<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Miss Bala",
     "review":"In order to save her friend, Suzu, Gloria is put in between a rock and a hard place.  Gloria is threaten to work with drug lord Lino to get information about Suzu.  She unintentionally blows up a DEA safe house.  In an attempt to get help, she gets threaten by the DEA.  So, she is risking life or jail to find her friend.  This movie is 15 minutes of set up and the rest all action.  Once it gets going, it never stops.  And it has more twist and turns than a soap opera.  See it today and hope for a part 2.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"How to Train Your Dragon: The Hidden World",
     "review":"Grimmel thought he killed all but one of the Night Furies.   When he hears of Toothless's existence, he decides to use his Light Fury to bait and kill him.  Seeing that Berk is overpopulated with dragons and knowing of Grimmel's plan, Hiccup decides to find the hidden world his father, Stoick, talked about when he was a young boy.  There, the residents of Berk and their dragons can live in peace and safety with room for all.  The final installment of the How to Train Your Dragon franchise, answers all questions about their characters' future.  It is beautifully drawn and concluded, even though some jokes fall flat.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,38 +36,15 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"The LEGO Movie 2: The Second Part",
     "review":"In 5 years, the Systar aliens have changed Bricksburg to Apocalypseburg.  Now General Sweet Mayhem has kidnapped Emmet's friends to convince Batman to marry Queen Watevra Wa-Nabi.  Emmet must save his friends before his dark premonition comes true.  This is how sequels should be done.  It is funnier, more sincere, and better built than the first.  While there is no need to stay after the end credits, you should stay for the credit song.  And no matter how hard you try, the Catchy Song will stay with you.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thelegomovie2thesecondpart.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thelegomovie2thesecondpart.jpg"
     }
-},
-{
-    "option":"5",
-    "mtitle":"Serenity",
-    "review":"Baker is faced with a moral dilemma when his ex-wife, Karen, ask him to kill her new husband Frank.  She doesn't want Baker to kill him for the abuse, she wants Baker to do it for their son Patrick.  Patrick and Baker share a unique connection.  When Baker gets a visit from Reid, Baker's world is turned upside down.  This is the most confusing movie since Inception.  You will leave the theater with more questions than answers.<br/><br/>2 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/serenity.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/serenity.jpg"
-    }
 }
 ];
-
-/*
-
-{
-    "option":"1",
-    "mtitle":"Cold Pursuit",
-    "review":".<br/><br/>* out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/coldpursuite.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/coldpursuite.jpg"
-    }
-}
-
-*/
 
 module.exports = inTheTheaters;
 
