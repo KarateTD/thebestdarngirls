16c7bba148ec8dcd16eacb496a425e1b1e0d53c2
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-02-24 04:49:34 -0500

    update made for tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -2,7 +2,7 @@ var madeForTV = [
 {
     "option": "1",
     "mtitle": "Who's Stalking Me?",
-    "review": ".<br/><br/>* out of 5 stars.",
+    "review": "Detective Dawson offers his help after Amanda is attacked in her home.  While his intentions are noble on the surface, he is hiding a dark secret.  All Lifetime fans should watch this movie and stick it out to the end.  It will give a shock you will never see coming with a decent body count.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl": "https://s3.amazonaws.com/thebestdarngirls/small-image/whosstalkingme.jpg",
         "largeImageUrl": "https://s3.amazonaws.com/thebestdarngirls/large-image/whosstalkingme.jpg"
@@ -11,7 +11,7 @@ var madeForTV = [
 {
     "option": "2",
     "mtitle": "Love on the Menu",
-    "review": ".<br/><br/>* out of 5 stars.",
+    "review": "Hank mistakes Maggie for being a food critic and believes her bad review cost him to lose a star.  But she is a VP for a frozen food company and wants to hire him to create gourmet recipes for a frozen food line.  They will have to work together to create the recipes and save his restaurant.  This movie is a smart, easy laugh with true family bonding.  Watch it with the whole family today.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/loveonthemenu.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/loveonthemenu.jpg"
@@ -20,16 +20,16 @@ var madeForTV = [
 {
     "option": "3",
     "mtitle": "Instakiller",
-    "review": "Harper's life was saved with a heart transplant less than a year ago.  She uses her social media account to promote her fashion design.  She is ecstatic to see she has 1,000 new followers overnight.  But one of the followers is a stalker set on harming anyone that comes in between them.  This movie has great acting but an average script.  Lifetime fans will figure out the stalker in the first hour.<br/><br/>2.5 out of 5 stars.",
+    "review": "Harper's life was saved with a heart transplant less than a year ago.  She uses her social media account to promote her fashion designs.  She is ecstatic to see she has 1,000 new followers overnight.  But one of the followers is a stalker set on harming anyone that comes in between them.  This movie has great acting but an average script.  Lifetime fans will figure out the stalker in the first hour.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/instakiller.jpeg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/instakiller.jpeg"
     }
 },
 {
-    "option": "2",
+    "option": "4",
     "mtitle": "Chronicle Mysteries: Recovered",
-    "review": "For season 2 of her podcast, Alex decided to head back to her hometown to solve the mystery of Gina DeSalvo.  Gina DeSalvo was last seen leaving her job after getting her tips.  Her car crashed into a boulder and was recovered with blood on the windshield and no body.  Alex was determined to find her.  This mystery gives you a rare look into a sleuth's mind while she is figuring out the crime.  With the podcast, you feel more like a friend than a fly on the wall.  Hopefully Hallmark will extend this movie series.<br/><br/>5 out of 5 stars.",
+    "review": "For season 2 of her podcast, Alex decides to head back to her hometown to solve the mystery of Gina DeSalvo.  Gina DeSalvo was last seen leaving her job after getting her tips.  Her car crashed into a boulder and was recovered with blood on the windshield and no body.  Alex was determined to find her.  This mystery gives you a rare look into a sleuth's mind while she is figuring out the crime.  With the podcast, you feel more like a friend than a fly on the wall.  Hopefully Hallmark will extend this movie series.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/chroniclemysteriesrecovered.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/chroniclemysteriesrecovered.jpg"
