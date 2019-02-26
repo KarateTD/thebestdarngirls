0a1d19edb46a4f425c1948338bb25f6cef4b4c16
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-02-25 20:46:31 -0500

    add 1 movie

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Fighting with My Family",
+    "review":"Saraya trained for years to become a WWE superstar, so when she and her brother, Zak, got a try-out, they were floored.  Changing her name to Paige, they give the tryout their all.  But when she is picked and not Zak, she wonders if she can do it alone.  And Zak starts to spiral.  This family needs to stick together to save Zak and encourage Saraya.  This isn't just another wrestling movie, it's about family and dreams versus reality.  Saraya has to overcome her lack of strength, her misconceptions, and self-doubts but she can't do it without her honest and loving family.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/fightingwithmyfamily.jpeg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/fightingwithmyfamily.jpeg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Isn't It Romantic",
     "review":"Natalie believes that romantic comedies are bad for women because they are unrealistic and cliche.  When an accident leaves her stuck in a romantic comedy, she must follow the cliches to get a man to fall in love with her and get out.  This movie makes fun of each cliche as Natalie performs them.  It's funny because Natalie is saying exactly what you are thinking.  Using Pretty Woman, The Wedding Singer, and other romantic classics, this movie is a date night movie for those who love and hate romantic comedies.  It's not a classic but it's fun to watch.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Happy Death Day 2 U",
     "review":"Ryan is killed and wakes up in the same day.  When he tells Tree and Carter, they know exactly what's happening to him.  Like Tree, he will have to keep dying to figure out who killed him.  But after a science experiment goes awry, Tree is back to her original loop but in a different dimension.  Now, she has to solve her murder again to stop the loop but she may want to stay in this new dimension.  This sci-fi twist to a suspense movie is long and boring.  Instead of the loop making Tree a better person, it was a fluke.  If you are a fan of the first or not, wait for Redbox.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Alita: Battle Angel",
     "review":"After Dr. Ido finds her functional brain in a scrap yard, Alita must battle to recover her memory and her drive for the future.  This movie surpasses Avatar visually while having the drama of a teen film and the action of NASCAR.  It ends on a cliff-hanger and should be seen in Dolby.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Cold Pursuit",
     "review":"After receiving Kehoe's Citizen of the Year award, Nels Coxman learns is son died of a drug overdose.  Nels goes to his work's garage to commit suicide when he finds his son's friend, Dante bleeding and crying in the corner.  Dante tells Nelson, his son was killed over drugs he stole.  Nelson demands the name of his son's killer.  A name that will set off a killing spree.  This movie is nothing new.  The double crossing, killing, and setups are predictable and pretty boring.  It's the women of Kehoe that will make you laugh.  See it matinee or wait for Redbox.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/coldpursuite.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/coldpursuite.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"What Men Want",
-    "review":"After a wild bachelorette party, Ali can hear men's thoughts.  At first, she considers getting rid of this ability.  Then, she decides to use it to enter the boys club at her sports management company.  What could go wrong?  While this movie does make fun of men, it doesn't demonize them.  It allows the viewer to see that Ali's biggest obstacle is herself.  This is a great date movie but not a family movie.  Also, you will leave wanting to know: Who voted against Ali?<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/whatmenwant.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/whatmenwant.jpg"
     }
 }
 ];
