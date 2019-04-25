1a8ba48c3013a2da60b000742af14209ac06e544
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-04-25 01:30:43 -0400

    a few fixes

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -2,7 +2,7 @@ var inTheTheaters = [
 {
     "option":"1",
     "mtitle":"Booksmart",
-    "review":"Molly and Amy are inspired to spend their last night as high school students having the 4 years of fun they missed at Nick's party.  They just need one thing: the address.<br/><br/>This is not a \"coming-of-age\" movie.  These woman are strong, smart, and liked.  They just forgot to have fun.  Their friendship and world views will be tested.  And you will laugh at every milestone.<br/><br/>5 out of 5 stars.",
+    "review":"Molly and Amy are inspired to spend their last night as high school students having the 4 years of fun they missed by attending Nick's party.  They just need one thing: the address.<br/><br/>This is not a \"coming-of-age\" movie.  These women are strong, smart, and liked.  They just forgot to have fun.  Their friendship and world views will be tested.  And you will laugh at every milestone.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/booksmart.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/booksmart.jpg"
@@ -11,7 +11,7 @@ var inTheTheaters = [
 {
     "option":"2",
     "mtitle":"Breakthrough",
-    "review":"John walked on a frozen lake and fell through the ice.  After 20 minutes under water and 25 minutes of rescue efforts, the nurses stop working to allow John's mother to say her goodbyes.  As she prays for God to save her son, his heart starts beating again.  She will need her faith, her family, and her pastor to get her through as she prays for her son.<br/><br/>This movie will have you in tears.  While it shows John's journey, it doesn't sugar coat his mom's struggles.  She appeared to be a rock but she was pushing others away.  Also, this movie give insight to John's experience after his survival.  See it with the whole family..<br/><br/>4.5 out of 5 stars.",
+    "review":"John walked on a frozen lake and fell through the ice.  After 20 minutes under water and 25 minutes of rescue efforts, the nurses stop working to allow John's mother to say her goodbyes.  As she prays for God to save her son, his heart starts beating again.  She will need her faith, her family, and her pastor to get her through as she prays for her son.<br/><br/>This movie will have you in tears.  While it shows John's journey, it doesn't sugar coat his mom's struggles.  She appeared to be a rock but she was pushing others away.  Also, this movie gives insight to John's experience after his survival.  See it with the whole family.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/breakthrough.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/breakthrough.jpg"
@@ -20,7 +20,7 @@ var inTheTheaters = [
 {
     "option":"3",
     "mtitle":"After",
-    "review":"Straight laced Tessa is not impressed with bad boy Hardin.  So when she gets an opportunity to kiss him, she turns him down.  But when she looks past his facade, can she resist is charms?  What will it costs Tessa to have Hardin in her life?<br/><br/>This movie is twilight without vampires.  Nothing really sets it apart from other teen angst movies.  Since it is book one in a 4 book series, the movie ends on a cliff hanger.  If you are a fan of the genre, see it matinee.  Otherwise, wait for Redbox..<br/><br/>2.5 out of 5 stars.",
+    "review":"Straight laced Tessa is not impressed with bad boy Hardin.  So when she gets an opportunity to kiss him, she turns him down.  But when she looks past his facade, can she resist his charms?  What will it costs Tessa to have Hardin in her life?<br/><br/>This movie is twilight without vampires.  Nothing really sets it apart from other teen angst movies.  Since it is the first book in a 4 book series, the movie ends on a cliff hanger.  If you are a fan of the genre, see it matinee.  Otherwise, wait for Redbox..<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/after.png",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/after.png"
@@ -29,7 +29,7 @@ var inTheTheaters = [
 {
     "option":"4",
     "mtitle":"Missing Link",
-    "review":"Sir Lionel Frost is determine to join the society of great men. He has proof a Sasquatch could be real.  So he makes a deal, if he can find Sasquatch, they will allow him to join the club.  Otherwise, he will leave the group alone.  Lionel is surprised the letter writer about the Sasquatch, is the Sasquatch.  He offers to give Lionel proof of his existence, if Lionel takes him to meet is cousins:  The Yeti of the Himalayas.<br/><br/>This movie is only fun for the first and last 10 minutes.  The rest of the movie is boring.  Even the amazing graphics can't save this film.  Wait for cable.<br/><br/>1 out of 5 stars.",
+    "review":"Sir Lionel Frost is determine to join the society of great men. He has proof a Sasquatch could be real.  So he makes a deal.  If he can find Sasquatch, they will allow him to join the club.  Otherwise, he will leave the group alone.  Lionel is surprised to discover the letter writer about the Sasquatch, is the Sasquatch.  He offers to give Lionel proof of his existence, if Lionel takes him to meet is cousins:  The Yeti of the Himalayas.<br/><br/>This movie is only fun for the first and last 10 minutes.  The rest of the movie is boring.  Even the amazing graphics can't save this film.  Wait for cable.<br/><br/>1 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/missinglink.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/missinglink.jpg"
