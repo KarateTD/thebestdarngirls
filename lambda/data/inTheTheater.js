4af9f04adad4a54dadf6ab0915d5d9000f639492
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-05-03 10:26:53 -0400

    add pikachu and long shot

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"POKEMON Detective Pikachu",
+    "review":"Tim goes to his father's apartment after Harry dies and discovers a purple vial and a Pikachu.  The vial sprays him in the face and releases the gas out the window.  The gas makes Pokemon temporarily evil and give Tim the ability to talk to Pikachu.  Tim discovers that Pikachu was his dad's partner and he believes Harry is still alive.  Together, they will discover what really happened to Harry.<br/><br/>Because this movie is based on a video game, it's easy to count it out.  However, the plot offers twist, turns, and lots of revelations.  You will be left guessing until the end.  While the characters were CGI, they were not overdone.  The Pokemon didn't look too realistic and it allows the viewer to stay in a world of wonder.  Another good aspect is Pikachu's humor.  It is equivalent to a PG Deadpool.  See it with the family and have fun.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/pokemondetectivepikachu.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/pokemondetectivepikachu.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Long Shot",
+    "review":"Fred meets his former baby-sister, now Secretary of State, Charlotte at a party.  He lets it slip that he was fired from his job and she looks into his work.  Her staffers tell her she needs more humor in her speaches, so she hires Fred to write for her.  A relationship blooms.  But can it withstand power dynamics, public opinion, and blackmail.<br/><br/>This movie hits a lot of topics on relationships, politics, and the media while being very funny and light-hearted.  It's a romantic comedy with a little action thrown in.  While this is great for the theater, you won't have an urge to buy it.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/longshot.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/longshot.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle":"Avengers: Endgame",
     "review":"After learning the stones are gone, the team tries to cope with their new reality.  But when Ant-Man comes back from the quantum realm, he develops an idea to get the stones back, recreate the gauntlet and bring everyone back.<br/><br/>This movie is more than you could have imagined.  The 3 hours do not feel long enough.  Stunning sendoffs are given to each original Avenger and Stan Lee.  You will need to see this movie multiple times because it will continue to blow your mind and bring tears to your eyes.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"4",
     "mtitle":"Booksmart",
     "review":"Molly and Amy are inspired to spend their last night as high school students having the 4 years of fun they missed by attending Nick's party.  They just need one thing: the address.<br/><br/>This is not a \"coming-of-age\" movie.  These women are strong, smart, and liked.  They just forgot to have fun.  Their friendship and world views will be tested.  And you will laugh at every milestone.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"5",
     "mtitle":"Breakthrough",
     "review":"John walked on a frozen lake and fell through the ice.  After 20 minutes under water and 25 minutes of rescue efforts, the nurses stop working to allow John's mother to say her goodbyes.  As she prays for God to save her son, his heart starts beating again.  She will need her faith, her family, and her pastor to get her through as she prays for her son.<br/><br/>This movie will have you in tears.  While it shows John's journey, it doesn't sugar coat his mom's struggles.  She appeared to be a rock but she was pushing others away.  Also, this movie gives insight to John's experience after his survival.  See it with the whole family.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/breakthrough.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/breakthrough.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"After",
-    "review":"Straight laced Tessa is not impressed with bad boy Hardin.  So when she gets an opportunity to kiss him, she turns him down.  But when she looks past his facade, can she resist his charms?  What will it costs Tessa to have Hardin in her life?<br/><br/>This movie is twilight without vampires.  Nothing really sets it apart from other teen angst movies.  Since it is the first book in a 4 book series, the movie ends on a cliff hanger.  If you are a fan of the genre, see it matinee.  Otherwise, wait for Redbox..<br/><br/>2.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/after.png",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/after.png"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Missing Link",
-    "review":"Sir Lionel Frost is determine to join the society of great men. He has proof a Sasquatch could be real.  So he makes a deal.  If he can find Sasquatch, they will allow him to join the club.  Otherwise, he will leave the group alone.  Lionel is surprised to discover the letter writer about the Sasquatch, is the Sasquatch.  He offers to give Lionel proof of his existence, if Lionel takes him to meet is cousins:  The Yeti of the Himalayas.<br/><br/>This movie is only fun for the first and last 10 minutes.  The rest of the movie is boring.  Even the amazing graphics can't save this film.  Wait for cable.<br/><br/>1 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/missinglink.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/missinglink.jpg"
     }
 }
 ];
