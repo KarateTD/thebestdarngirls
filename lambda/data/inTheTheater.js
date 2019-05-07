6c0dd53553ecbcdd70f8f5f77bf91d2b38811d53
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-05-06 21:32:36 -0400

    add ugly dolls and the intruder

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"The Intruder",
+    "review":"After landing the biggest deal in his company's history, Scott wants to give his wife, Annie, her dream home.  They get the owner Charlie to lower the price and move in the next week.  Charlie says he is moving to Florida but he keeps popping up at their house.  In Charlie's mind, the house and Annie are his.  He will do anything to have them.<br/><br/>Be ready to hate Annie's extremely trusting nature.  No matter how many times anyone tells her Charlie is dangerous, she keeps inviting him in the house.  You have little to no sympathy for her character.  This movie should have been on Lifetime, not the theaters.<br/><br/>* out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/theintruder.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/theintruder.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"UglyDolls",
+    "review":"Moxy dream of a world outside of UgleVille.  A world where a child will love her.  With mis-directed guidance, she convinces 4 friends to follow the pipe that dropped them into UglyVille.  So, Moxy and her Ugly Gang follow the pipe to the Institute of Perfection.  There perfect dolls are trained and tested in the gauntlet.  If they pass, they can be matched with a human.  The leader of the Institute, Lou, allows Moxy and the other Ugly Dolls to stay but only if they pass the Gauntlet.  And he will try every underhanded trick in the book to make sure they fail.<br/><br/>This movie is all about self esteem and self worth.  The dolls in UglyVille don't know that Ugly is bad until they go to the Institute of perfect.  It can be the same for kids who enter school the first time.  While its a sweet story, it's not memorable.  The songs are average.  The plot is overdone and brings nothing to the table.  See this matinee if you have kids.  Otherwise, wait for Redbox.<br/><br/>2 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/uglydolls.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/uglydolls.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle":"POKEMON Detective Pikachu",
     "review":"Tim goes to his father, Harry's apartment after Harry dies and discovers a purple vial and a Pikachu.  The vial sprays Tim in the face and releases the gas out the window.  The gas makes Pokemon temporarily evil but gives Tim the ability to talk to Pikachu.  Tim discovers that Pikachu was his dad's police partner and Pikachu believes Harry is still alive.  Together, they will discover what really happened to Harry.<br/><br/>Because this movie is based on a video game, it's easy to count it out.  However, the plot offers twists, turns, and lots of revelations.  You will be left guessing until the end.  While the characters were CGI, they were not overdone.  The Pokemon don't look too realistic and it allows the viewer to stay in a world of wonder.  Another good aspect is Pikachu's humor.  It is equivalent to a PG Deadpool.  See it with the family and have fun.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"4",
     "mtitle":"Long Shot",
     "review":"Fred meets his former babysitter, now Secretary of State, Charlotte at a party.  He lets it slip that he was fired from his job and she looks into his work.  Her staffers tell her she needs more humor in her speeches, so she hires Fred to write for her.  A relationship blooms.  But can it withstand power dynamics, public opinion, and blackmail.<br/><br/>This movie hits a lot of topics on relationships, politics, and the media while being very funny and light-hearted.  It's a romantic comedy with a little action thrown in.  While this is great for the theater, you won't have an urge to buy it on DVD.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"5",
     "mtitle":"Avengers: Endgame",
     "review":"After learning the stones are gone, the team tries to cope with their new reality.  But when Ant-Man comes back from the quantum realm, he develops an idea to get the stones back, recreate the gauntlet and bring everyone back.<br/><br/>This movie is more than you could have imagined.  The 3 hours do not feel long enough.  Stunning sendoffs are given to each original Avenger and Stan Lee.  You will need to see this movie multiple times because it will continue to blow your mind and bring tears to your eyes.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/avengersendgame.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/avengersendgame.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Booksmart",
-    "review":"Molly and Amy are inspired to spend their last night as high school students having the 4 years of fun they missed by attending Nick's party.  They just need one thing: the address.<br/><br/>This is not a \"coming-of-age\" movie.  These women are strong, smart, and liked.  They just forgot to have fun.  Their friendship and world views will be tested.  And you will laugh at every milestone.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/booksmart.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/booksmart.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Breakthrough",
-    "review":"John walked on a frozen lake and fell through the ice.  After 20 minutes under water and 25 minutes of rescue efforts, the nurses stop working to allow John's mother to say her goodbyes.  As she prays for God to save her son, his heart starts beating again.  She will need her faith, her family, and her pastor to get her through as she prays for her son.<br/><br/>This movie will have you in tears.  While it shows John's journey, it doesn't sugar coat his mom's struggles.  She appeared to be a rock but she was pushing others away.  Also, this movie gives insight to John's experience after his survival.  See it with the whole family.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/breakthrough.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/breakthrough.jpg"
     }
 }
 ];
