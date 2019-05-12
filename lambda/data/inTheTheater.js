ffb25c65b15db4eefe5d03bf1cb44e980c90b0c3
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-05-12 17:33:11 -0400

    add poms and the hustle

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Poms",
+    "review":"Martha's only regret is not being able to preform as a cheerleader when her mother got sick.  Now she is in a retirement community and has to find a group to stay.  She decides to creates a cheerleading club.  After an epic fail goes viral, the regroup with an enemy to compete in the biggest cheerleading competition on the south coast.  But Martha is keeping her cancer diagnosis a secret.<br/><br/>This movie is funny, inspiring, and a lesson for everyone on how to treat our elders.  While Martha is the heart af the group, Sheryl is the backbone.  Her wit, charm, and determination is the guiding force behind Martha.  Seeing it full price is worth it but you won't have the urge to buy.<br/><br/>3.5 out of 5 stars",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/poms.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/poms.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"The Hustle",
+    "review":"Josephine is impressed with Penny skills as a small-time con-artist but she wants her out of the way.  Josephine is a big-time grifter with her eyes on a larger marks.  And having another con-artist in the area could ruin her plans.  So after trying to get rid of her by training her, they place a wager.  They have to con the same man.  Who ever cons him first wins $500,000 and gets to stay.<br/><br/>This movie is only watchable due to Rebel's physical comedy.  She carries the movie while Ann's accents all sound the same.  If you are a fan of Rebel, see it matinee.  Otherwise, wait for FandangoNow.<br/><br/>2 out of 5 stars",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thehustle.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thehustle.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle":"The Intruder",
     "review":"After landing the biggest deal in his company's history, Scott wants to give his wife, Annie, her dream home.  They get the owner Charlie to lower the price and move in the next week.  Charlie says he is moving to Florida but he keeps popping up at their house.  In Charlie's mind, the house and Annie are his.  He will do anything to have them.<br/><br/>Be ready to hate Annie's extremely trusting nature.  No matter how many times anyone tells her Charlie is dangerous, she keeps inviting him in the house.  You have little to no sympathy for her character.  This movie should have been on Lifetime, not the theaters.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"4",
     "mtitle":"UglyDolls",
     "review":"Moxy dreams of a world outside of UglyVille.  A world where a child will love her.  With mis-directed guidance, she convinces 4 friends to follow the pipe that dropped them into UglyVille.  So, Moxy and her Ugly Gang follow the pipe to the Institute of Perfection.  There, perfect dolls are trained and tested in the gauntlet.  If they pass, they can be matched with a human.  The leader of the Institute, Lou, allows Moxy and the other Ugly Dolls to stay but only if they pass the Gauntlet.  And he will try every underhanded trick in the book to make sure they fail.<br/><br/>This movie is all about self esteem and self worth.  The dolls in UglyVille don't know that Ugly is bad until they go to the Institute of Perfection.  It can be the same for kids who enter school for the first time.  While its a sweet story, it's not memorable.  The songs are average.  The plot is overdone and brings nothing to the table.  See this matinee if you have kids.  Otherwise, wait for Redbox.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"5",
     "mtitle":"POKEMON Detective Pikachu",
     "review":"Tim goes to his father, Harry's apartment after Harry dies and discovers a purple vial and a Pikachu.  The vial sprays Tim in the face and releases the gas out the window.  The gas makes Pokemon temporarily evil but gives Tim the ability to talk to Pikachu.  Tim discovers that Pikachu was his dad's police partner and Pikachu believes Harry is still alive.  Together, they will discover what really happened to Harry.<br/><br/>Because this movie is based on a video game, it's easy to count it out.  However, the plot offers twists, turns, and lots of revelations.  You will be left guessing until the end.  While the characters were CGI, they were not overdone.  The Pokemon don't look too realistic and it allows the viewer to stay in a world of wonder.  Another good aspect is Pikachu's humor.  It is equivalent to a PG Deadpool.  See it with the family and have fun.<br/><br/>3 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/pokemondetectivepikachu.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/pokemondetectivepikachu.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Long Shot",
-    "review":"Fred meets his former babysitter, now Secretary of State, Charlotte at a party.  He lets it slip that he was fired from his job and she looks into his work.  Her staffers tell her she needs more humor in her speeches, so she hires Fred to write for her.  A relationship blooms.  But can it withstand power dynamics, public opinion, and blackmail.<br/><br/>This movie hits a lot of topics on relationships, politics, and the media while being very funny and light-hearted.  It's a romantic comedy with a little action thrown in.  While this is great for the theater, you won't have an urge to buy it on DVD.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/longshot.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/longshot.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Avengers: Endgame",
-    "review":"After learning the stones are gone, the team tries to cope with their new reality.  But when Ant-Man comes back from the quantum realm, he develops an idea to get the stones back, recreate the gauntlet and bring everyone back.<br/><br/>This movie is more than you could have imagined.  The 3 hours do not feel long enough.  Stunning sendoffs are given to each original Avenger and Stan Lee.  You will need to see this movie multiple times because it will continue to blow your mind and bring tears to your eyes.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/avengersendgame.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/avengersendgame.jpg"
     }
 }
 ];
