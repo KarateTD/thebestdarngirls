3577ccfc8bec80629723bb05e2d98bac32eb08fc
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-05-14 01:09:07 -0400

    add marriage killer

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 {
-	"option":"1",
+    "option":"1",
+    "mtitle":"Marriage Killer",
+    "review":"After an injury, Morgan learns about a new adult club form Isabelle.  She politely declines.  At a dinner party later, Isabelle mentions the club around Morgan's husband Robert.  He wats to see wht the fuss is about and convinces Morgan to go.  The two decide the club isn't for them but make love on the car ride home.  Isabelle weasels her way into their lives until the 3 make love.  The morning after, Isabelle is in love with the couple but they don't reciprocate.  Heartbroken, she will do anything to be a part of their family.<br/><br/>This movie maybe to spicy and to real for Lifetime.  The plot is wild but it is grounded in truth based on the public backlash.  In today's world, you realy go beyond the video tape.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/marriagekiller.png",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/marriagekiller.png"
+    }
+},
+{
+	"option":"2",
 	"mtitle":"Pretty Rosebud",
 	"review":"Cecilia feels constricted with her structured and religious life, so she uses affairs as a release.  This is a tale of how ignoring your happiness can lead to a road of self-destruction and how loving yourself can save you.<br/><br/>3.5 out of 5 stars",
 	"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 	}
 },
 {
-	"option":"2",
+	"option":"3",
 	"mtitle":"Betrayed",
 	"review":"When the Russian mob kidnaps the wrong type of victims for sex slavery, Mayor Alderman decides to stop looking the other way and cut them off.  His daughter will pay the price.  This bloody tale has enough twist and turns to keep you entertained.<br/><br/>4 out of 5 stars",
 	"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 	}
 },
 {
-	"option":"3",
+	"option":"4",
 	"mtitle":"Killer Island",
 	"review":"Ashley and Mike are trying to gain back the trust in their relationship.  But when Ashley finds a bracelet, this beautiful island will be exposed as a den for killers.  Great movie to rent and kill a few hours.  This will be a future Lifetime movie.<br/><br/>3 out of 5 stars.",
 	"image":{
@@ -27,7 +36,7 @@ var videoOnDemand = [
 	}
 },
 {
-	"option":"4",
+	"option":"5",
 	"mtitle":"Service To Man",
 	"review":"Eli and Michael are two freshman students at an all-black medical school.  However, both face harassment.  Michael for being rich and Eli for being white.  To succeed, they must work together.  This movie has a strong message with two points of view on the same topic.<br/><br/>5 out of 5 stars.",
 	"image":{
