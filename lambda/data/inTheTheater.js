1e451821ff5229425332efa5b6c8086cf52d3c55
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-05-19 23:57:37 -0400

    add john wick 3, a dogs journey, and the sun is also a star

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"John Wick: Chapter 3 - Parabellum",
+    "review":"Winston gives John 1 hour to get his affairs in order.  Then he will be excommunicated and a bounty of $14 million will be placed on his head.  Every assassin in the world will be after him and no one will offer help.  John uses the hour to find a safe place for his dog, get a ticket, get a marker, and get some gold coins.  Since some assassins couldn't wait for an hour, John needed to seek medical treatment.  In the middle of getting stitches, John's time runs out and the doctor stops the work.  John has to complete is stitches and heads to the Director.  He asks the Director to give him passage to Casablanca.  He knows an assassin there who can lead him to the High Table.  He hopes to plead his case so the High Table will let him live.<br/><br/>This movie proves better weapons, excellent choreographed fights, and mind blowing cinematography that all action lovers need.  With so much going on, this movie provides 30 to 45 minutes of dialogue.  It gives you more of John's training history with the Director but not enough information of the blood oath between him and Sophia.  Seeing John Wick 2 is necessary for this movie.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/johnwickchapter3parabellum.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/johnwickchapter3parabellum.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"The Sun is Also a Star",
+    "review":"Natasha has to leave the US after ICE discovers her family are undocumented immigrants.  She has an 8 a.m. appointment to speak to U.S. Immigration.  Daniel is a 1st generation American who follows his parents dream.  He has an interview for a college recommendation this morning.  With 2 appointments, 1 jacket, and the most benevolent star, these 2 will meet.  After saving her life, Natasha gives Daniel 1 hour to prove love exists.  But she doesn't tell him this will be her last day in New York.  This will be an hour of destiny.<br/><br/>While this movie deals with real world issue, it doesn't get to heavy or become a political satire.  It remains a teen love story that provides the most realistic view for this relationship.  It is fun to watch at full price, you won't have an immediate urge to buy it.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thesunisalsoastar.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thesunisalsoastar.jpg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"A Dog's Journey",
+    "review":"Ethan and Hanna adore their granddaughter CJ, their dog Bailey, and their daughter in law Gloria.  But Gloria doesn't feel the same way.  She jealously fears they want to take her child, so she movies away.  When Ethan's dog dies, he tells Bailey to find and protect CJ.  He knows it will work because Bailey was reincarnated several times before.  With a new mission, Bailey will come back as different dogs to protect, love, and comfort CJ.<br/><br/>For this sequel, pull out our tissues and be ready to cry.  While watch this movie, you discover how strong dogs are and how vulnerable they are in shelters.  You will get the urge to adopt a dog today but only see this movie once.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/adogsjourney.jpg",
+        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/adogsjourney.jpg"
+    }
+},
+{
+    "option":"4",
     "mtitle":"Poms",
     "review":"Martha's only regret is not being able to perform as a cheerleader when her mother got sick.  Now she is in a retirement community and has to find a group to stay.  She decides to create a cheerleading club.  After an epic fail goes viral, they regroup with an enemy to compete in the biggest cheerleading competition on the south coast.  But Martha is keeping her cancer diagnosis a secret.<br/><br/>This movie is funny, inspiring, and a lesson for everyone on how to treat our elders.  While Martha is the heart of the group, Sheryl is the backbone.  Her wit, charm, and determination is the guiding force behind Martha.  Seeing it full price is worth it but you won't have the urge to buy.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"5",
     "mtitle":"The Hustle",
     "review":"Josephine is impressed with Penny skills as a small-time con-artist but she wants her out of the way.  Josephine is a big-time grifter with her eyes on larger marks.  And having another con-artist in the area could ruin her plans.  So after trying to get rid of Penny by training her, they place a wager.  They have to con the same man.  Who ever cons him first wins $500,000 and gets to stay.<br/><br/>This movie is only watchable due to Rebel's physical comedy.  She carries the movie while Ann's accents all sound the same.  If you are a fan of Rebel, see it matinee.  Otherwise, wait for FandangoNow.<br/><br/>2 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thehustle.jpg",
         "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thehustle.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"The Intruder",
-    "review":"After landing the biggest deal in his company's history, Scott wants to give his wife, Annie, her dream home.  They get the owner Charlie to lower the price and move in the next week.  Charlie says he is moving to Florida but he keeps popping up at their house.  In Charlie's mind, the house and Annie are his.  He will do anything to have them.<br/><br/>Be ready to hate Annie's extremely trusting nature.  No matter how many times anyone tells her Charlie is dangerous, she keeps inviting him in the house.  You have little to no sympathy for her character.  This movie should have been on Lifetime, not the theaters.<br/><br/>2 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/theintruder.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/theintruder.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"UglyDolls",
-    "review":"Moxy dreams of a world outside of UglyVille.  A world where a child will love her.  With mis-directed guidance, she convinces 4 friends to follow the pipe that dropped them into UglyVille.  So, Moxy and her Ugly Gang follow the pipe to the Institute of Perfection.  There, perfect dolls are trained and tested in the gauntlet.  If they pass, they can be matched with a human.  The leader of the Institute, Lou, allows Moxy and the other Ugly Dolls to stay but only if they pass the Gauntlet.  And he will try every underhanded trick in the book to make sure they fail.<br/><br/>This movie is all about self esteem and self worth.  The dolls in UglyVille don't know that Ugly is bad until they go to the Institute of Perfection.  It can be the same for kids who enter school for the first time.  While its a sweet story, it's not memorable.  The songs are average.  The plot is overdone and brings nothing to the table.  See this matinee if you have kids.  Otherwise, wait for Redbox.<br/><br/>2 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/uglydolls.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/uglydolls.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"POKEMON Detective Pikachu",
-    "review":"Tim goes to his father, Harry's apartment after Harry dies and discovers a purple vial and a Pikachu.  The vial sprays Tim in the face and releases the gas out the window.  The gas makes Pokemon temporarily evil but gives Tim the ability to talk to Pikachu.  Tim discovers that Pikachu was his dad's police partner and Pikachu believes Harry is still alive.  Together, they will discover what really happened to Harry.<br/><br/>Because this movie is based on a video game, it's easy to count it out.  However, the plot offers twists, turns, and lots of revelations.  You will be left guessing until the end.  While the characters were CGI, they were not overdone.  The Pokemon don't look too realistic and it allows the viewer to stay in a world of wonder.  Another good aspect is Pikachu's humor.  It is equivalent to a PG Deadpool.  See it with the family and have fun.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/pokemondetectivepikachu.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/pokemondetectivepikachu.jpg"
     }
 }
 ];
