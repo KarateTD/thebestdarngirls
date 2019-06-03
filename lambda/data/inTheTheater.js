5dabb1d2a32bbed32b9ba006c388b390e4f4bd86
Tiffany.Gray <Tiffany.Gray@ablevets.com>
2019-06-02 20:52:27 -0400

    a few changes

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -2,7 +2,7 @@ var inTheTheaters = [
 {
     "option": "1",
     "mtitle": "Rocketman",
-    "review": "Elton John allowed his talents flourish while being raised by a neglectful mom and cold hearted father.  Looking for the love he missed had home will send him into a downward spiral of drug abuse and an abusive relationship.  But the words, Bernie's words, that catapulted him into the business will save him from himself.<br/><br/>Rocketman is a musical that is too big for any stage.  As Elton reveals the layers of his life, he slowly takes off his costume as a symbolic gesture.  At the end, Elton will take control of his life and make amends with himself.<br/><br/>3.5 out of 5 stars.",
+    "review": "Elton John allowed his talents to flourish while being raised by a neglectful mom and cold hearted father.  Looking for the love he missed had home will send him into a downward spiral of drug abuse and an abusive relationship.  But the words, Bernie's words, that catapulted him into the business will save him from himself.<br/><br/>Rocketman is a musical that is too big for any stage.  As Elton reveals the layers of his life, he slowly takes off his costume as a symbolic gesture.  At the end, Elton will take control of his life and make amends with himself.<br/><br/>3.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rocketman.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rocketman.jpg"
@@ -11,7 +11,7 @@ var inTheTheaters = [
 {
     "option": "2",
     "mtitle": "Ma",
-    "review": "Sue Ann ignores Maggie's request to buy alcohol until she sees Andy and his truck.  She changes her mind and buys the alcohol.  Later, she offers them a place to party in her home and even makes snacks.  She becomes the party hub for all the kids in town and the nickname her Ma.  After Maggie and Haley break one of the rules and Ma freaks out.  The girls decide they don't want to go to Ma's and tells everyone else to do the same.  Ma must get the girls back to exact her ultimate plan of revenge.<br/><br/>This movie provides a great backing track of 70's music with some cringe worthy moments.  However, you are never afraid of Ma and you have no sympathy for these kids.  See this movie if you like to yell at the screen.<br/><br/>2.5 out of 5 stars.",
+    "review": "Sue Ann ignores Maggie's request to buy alcohol until she sees Andy and his truck.  She changes her mind and buys the alcohol.  Later, she offers them a place to party in her home and even makes snacks.  She becomes the party hub for all the kids in town and they nickname her Ma.  After Maggie and Haley break one of the rules and Ma freaks out, the girls decide they don't want to go to Ma's and tells everyone else to do the same.  Ma must get the girls back to exact her ultimate plan of revenge.<br/><br/>This movie provides a great backing track of 70's music with some cringe worthy moments.  However, you are never afraid of Ma and you have no sympathy for these kids.  See this movie if you like to yell at the screen.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ma.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ma.jpg"
@@ -20,7 +20,7 @@ var inTheTheaters = [
 {
     "option": "3",
     "mtitle": "Godzilla: King of the Monsters",
-    "review": "Emma creates ORCA.  A machine that can replicate the alpha call of any animal.  When she, ORCA, and her daughter Madison are kidnapped by a band of eco-terrorists lead by Alan, her ex-husband Mark is called in to help.  And for his help, he will be betrayed.  To save the world Mark will kill, revive, and team up with his sworn enemy: Godzilla.<br/><br/>Godzilla had great CGI and the classic monsters you love.  But the overall plot has too much foreshadowing.  Also, the writer keeps changing the genre of the movie.  Is it an alien movie, monster movie, action movie, or environmental PSA?  No one knows.  However, if you are a fan of the original, you will enjoy this version.  Everyone else, wait for Fandango.<br/><br/>2.5 out of 5 stars.",
+    "review": "Emma creates ORCA.  A machine that can replicate the alpha call of any animal.  When she, ORCA, and her daughter Madison are kidnapped by a band of eco-terrorists, lead by Alan, her ex-husband Mark is called in to help.  And for his help, he will be betrayed.  To save the world Mark will kill, revive, and team up with his sworn enemy: Godzilla.<br/><br/>Godzilla has great CGI and the classic monsters you love.  But the overall plot has too much foreshadowing.  Also, the writer keeps changing the genre of the movie.  Is it an alien movie, monster movie, action movie, or environmental PSA?  No one knows.  However, if you are a fan of the original, you will enjoy this version.  Everyone else, wait for Fandango.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/godzillakingofthemonsters.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/godzillakingofthemonsters.jpg"
