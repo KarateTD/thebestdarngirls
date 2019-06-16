92aa00909a1aea8b3d8ebecf3406b8abbc7a39b4
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-06-15 20:20:30 -0400

    add shaft and mib

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"MIB: International",
+    "review":"A little girl witness her parent's being neuralyzered and helped an alien escaped.  Now, she reads the hot sheets and searches for alien life.  And one day, she finds an alien crash site protected by a hologram.  She follows agents back to the MIB New York headquarters and walks in.  In seconds, she is detected.  She convinces Agent O to hire her as an MIB agent.  After some debating, Agent O renames her Agent M and hires her on a probationary period.  Agent M is assigned to the London office and finagles her way into becoming Agent H's partner.  Agent H is a legend in MIB because he defeated The Hive with Agent T.  But when MIB suspects a mole, Agent M doesn't know who to trust in MIB.  At least, she has Pawny.<br/><br/>This movie falls flat of its predecessors.  While the actors have proven their comedic abilities, the script just doesn't support it.  In addition, the buddy copy movie is changes for a budding romance movie.  The CGI is top flight and old characters are brought back for the original fans.  However, its just not enough.  Also, there are not post credit scenes.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mibinternational.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mibinternational.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Shaft",
+    "review":"20 years ago, Maya took John Jr. away to keep him safe from Shaft's dangerous occupation.  Now, John Jr. is an FBI data analyst who is often disrespected.  After Kiram, his best friend, dies over an overdose John Jr. can't believe it.  Sasha sees the autopsy and confirms Karim was murdered.  After failing to investigate on his own, John decides to go to the only man who can help.  His father, a bad mother - shut your mouth - I'm only talking about Shaft.<br/><br/>This is easily the funniest movie of the year.  Shaft brings the gender gap of masculinity to the surface without getting mushy.  Both men learn something and both stand up for their way of life.  See this movie today.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shaft.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/shaft.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle": "Dark Phoenix",
     "review": "In 1975, Professor X came to Jean Grey's aid after she accidentally killed her parents by causing a car accident.  The lone survivor, Jean decides to stay with Professor X to hone her gift.  Seventeen years later, Jean is one of the most powerful X-Men.  In a mission to save astronauts, Jean is hit with a solar flare that amplifies her power.  Her new powers are so strong, they are releasing the painful secrets in her mind that Professor X closed.  Now, Jean wants answers and leaves the school.  The X-Men go to save her and lose.  Heartbroken by her greatest mistake, Jean seeks out Magneto.  But Vuk is searching for Jean.  That solar flare was really a power surge that destroyed Vuk's planet.  She wants Jean's powers to wipe out humankind and rebuild her planet on earth.  She will do it with Jean or without her.<br/><br/>Be warned, there is no Quicksilver scene.  However, the cinematography is a great representation of Jean's internal struggles.  The CGI is the best of all X-Men movies.  But the overall story is flat.  It should expand on Vuk's life on D'Bari and Jean's relationship with Scott.  See in on the big screen for the effects.  But the next time you will want to see it, is on TV.  In addition, there are no post-credit scenes.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option": "2",
+    "option": "4",
     "mtitle": "Rocketman",
     "review": "Elton John allowed his talents to flourish while being raised by a neglectful mom and cold hearted father.  Looking for the love he missed had home will send him into a downward spiral of drug abuse and an abusive relationship.  But the words, Bernie's words, that catapulted him into the business will save him from himself.<br/><br/>Rocketman is a musical that is too big for any stage.  As Elton reveals the layers of his life, he slowly takes off his costume as a symbolic gesture.  At the end, Elton will take control of his life and make amends with himself.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option": "3",
+    "option": "5",
     "mtitle": "Ma",
     "review": "Sue Ann ignores Maggie's request to buy alcohol until she sees Andy and his truck.  She changes her mind and buys the alcohol.  Later, she offers them a place to party in her home and even makes snacks.  She becomes the party hub for all the kids in town and they nickname her Ma.  After Maggie and Haley break one of the rules and Ma freaks out, the girls decide they don't want to go to Ma's and tells everyone else to do the same.  Ma must get the girls back to complete her ultimate plan of revenge.<br/><br/>This movie provides a great backing track of 70's music with some cringe worthy moments.  However, you are never afraid of Ma and you have no sympathy for these kids.  See this movie if you like to yell at the screen.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ma.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ma.jpg"
-    }
-},
-{
-    "option": "4",
-    "mtitle": "Godzilla: King of the Monsters",
-    "review": "Emma creates ORCA.  A machine that can replicate the alpha call of any animal.  When she, ORCA, and her daughter Madison are kidnapped by a band of eco-terrorists, led by Alan, her ex-husband Mark is called in to help.  And for his help, he will be betrayed.  To save the world Mark will kill, revive, and team up with his sworn enemy: Godzilla.<br/><br/>Godzilla has great CGI and the classic monsters you love.  But the overall plot has too much foreshadowing.  Also, the writer keeps changing the genre of the movie.  Is it an alien movie, monster movie, action movie, or environmental PSA?  No one knows.  However, if you are a fan of the original, you will enjoy this version.  Everyone else, wait for FandangoNow.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/godzillakingofthemonsters.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/godzillakingofthemonsters.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Late Night",
-    "review":"After starring in her late night show for decades, Katherine is being replaced due to continuous falling ratings.  She decides to hire a female writer for a change of pace and to prove she doesn't hate women.  Molly entered a contest to meet a corporate executive.  She chooses the owner of Katherine's network to get a job interview on Katherine's show.  Having no background in comedy writing, she is hired on the spot because she's a woman.  While Molly can work and compete with the guys, it's Katherine that's her biggest obstacle.  With great advice from Walter and an office mate, Molly will grow.  But is it enough to save the show and get Katherine through a scandal.<br/><br/>If you can't take a political joke, this movie isn't for you.  This movie throws PC out the window for a good laugh.  However, at times, you will be too overcome with shock and awe to laugh.  Emma's Katherine is tougher than Meryl's Miranda and John Lithgow plays a more serious role.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/latenight.png",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/latenight.png"
     }
 }
 ];
