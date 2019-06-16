4589689f7f86c8dcba3fc7fa874d682898d20ed2
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-06-15 23:53:14 -0400

    add 3 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"Tempting Fate",
+    "review":"Gabby has a stereotypical suburban life.  Husband, kids, and the white picket fence.  On the inside, she is angry at her husband Elliott.  He got a vasectomy when he knows she wanted children.  While Elliott is away with their children, Gabby meets Matt.  The two spark of a friendship that becomes a business relationship.  Their texts become more intimate each time they talk.  On a surprise visit, Gabby's emotional affair becomes physical.  Regretful, Gabby draws the line in the sand and tells Matt they are just friends.  That would be true if she wasn't pregnant.  Now she will see the ripple effect of her decision.<br/><br/>You will feel every possible emotion with this new book-to-screen movie.  Gabby is never painted as a victim or a villain.  She is a misguided woman who made a mistake and is trying her best to fix it.  Just keep that in mind before you judge Matt, Elliott, or their children.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/temptingfate.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/temptingfate.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"My Stepfather's Secret",
+    "review":"Bailey doesn't have the money to attend a geology project at the Grand Canyon.  Instead, she will be going home for college summer vacation.  At home, her mom, Tina, has made some changes.  She is working out and eating vegan.  Bailey accepts her mom's changes until she sees the reason why.  Hugo, a healthy life consultant, asked Tina to elope and get married.  But Tina could never get married without her daughter.  However, Tina wants to get married this Saturday.  Bailey tries to be excited until a webcam, a wallet, and too friendly hands, forces her to change her mind.  What will it take to get her mom to see Hugo for who he really is?<br/><br/>It's difficult to review a movie you can't hear.  The environment noise, in outdoor scenes, were too loud.  And the backing track drowned out the actors constantly.  The evil stepfather plot was updated with blockchain but you could barely hear it.  The director needs to work with a new sound engineer and editor.<br/><br/>2 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mystepfatherssecret.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mystepfatherssecret.jpg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"My Boyfriend's Back: Wedding March 5",
+    "review":"Annalise is starting an event design company.  Her first event is Jen and Lucas's wedding.  She wants to add their wedding to her portfolio to attract investors.  Only one small problem.  Her ex-boyfriend, whom she hasn't spoken to in 5 years, is the best man. She tries to keep it professional for the wedding but she can't help her feelings for him.  And to make matters worst, there are 11 more people coming to the wedding ... including an investor.<br/><br/>This movie makes great timing out of uncomfortable situations.  You will thoroughly enjoy watching Annalise and Brad tiptoe around each other.  Also, you will see Olivia and Mick guide the couple's love while starting a new adventure for themselves.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myboyfriendsbackweddingmarch5.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myboyfriendsbackweddingmarch5.jpg"
+    }
+},
+{
+    "option":"4",
     "mtitle":"Adriana Trigiani's Very Valentine",
     "review":"Valentine has an opportunity to save her family's wedding shoe business by designing a shoe for the biggest name in wedding fashion.  But when the dress sketch is less than inspiring, Teodora invites Valentine to go to Italy with her for inspiration.  It's there that Valentine finds her grandmother, inspiration, and love.<br/><br/>Based on the novel by Adriana Trigiani, this movie is more complex than others on Lifetime and it offers a healthy dose of reality.  You will notice that Valentine slowly gains confidence, strength, and love for herself when her fears are mirrored by Roman.  And that is the true lesson behind this movie.  In the end, you will want to be her grandmother, Teodora.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
 },
 {
-    "option":"2",
+    "option":"5",
     "mtitle":"Wedding at Graceland",
     "review":"Clay and Laurel are engaged and they choose to get married at the one and only Graceland Chapel.  Graceland is the place they met and fell in love.  The event planner tells them she has a date, but it's over a year away.  The closest available date is 3 weeks away.  The two decide to take a leap of faith and get married in 3 weeks.  While that's not easy, their families are arriving early to uniquely help.  With meddling, jaded, judging, and extremely different in-laws, will Clay and Laurel make it down the aisle?<br/><br/> The second installment of this series is too funny.  While you feel the happy couple's pain, you can't help but laugh at their parents butting heads.  This movies' realistic view of a wedding is wonderful and it offers practical and fair solutions to their problems.  Watch with the family and your in-laws today.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/weddingatgraceland.jpeg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/weddingatgraceland.jpeg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Recipe for Danger",
-    "review":"Social media butterfly and restaurateur, Vanessa is on the upswing until her sous chef and daughter's godfather, Ricardo, is murdered.  After some time has passed, she needs a new sous chef to help.  She flips through applicants and finds only disappointment.  Heidi tells her to post about the job on social media.  By the end of the day, she has 197 applicants.  She is ecstatic.  Vanessa doesn't know a social stalker has been watching her every move.  The stalker wants what Vanessa took from her.  And killing is just the cost of doing business.<br/><br/>Vanessa overlooks too many warnings.  Four people close to her tell her the newcomer in her life is dangerous, but she keeps defending that person.  You will find yourself getting frustrated with Vanessa's need to trust.  This movie gives the viewers a great lesson: Post With Caution.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/recipefordanger.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/recipefordanger.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Pride and Prejudice: Atlanta",
-    "review":"Mrs. Bennet wants her 5 daughters to find a rich, handsome, strong husband.  Her daughter, Lizzie, is an activist who wants to stop the building of a strip mall.  She meets Antwone, a future congressmen, they argue and he asks her out on a date.  She laughs and run away.  Then, William Darcy meets Lizzie in a wedding and refuses to talk to him.  Finally, Stevie, a future church leader wants Lizzie after Jane said no.<br/><br/>Mrs. Bennet's dialogue will have you laughing and quoting for days.  Their are a few lines from the book, so listen hard.  It was funny for teens and up.  But, this movie feels more like a future TV series than a movie.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/prideandprejudiceatlanta.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/prideandprejudiceatlanta.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Secrets of the Sisterhood",
-    "review":"Ashley has been having a rough time with her job and divorce.  When her sister Jasmine comes to her door step, Ashley fears her drug addiction will be the last pillar in the trifecta.  But Jasmine has been clean for 9 months thanks to the Sisterhood.  This secret society works to help people and they want to help Ashley.  As Ashley's life changes for the better, she joins.  This decision will leave her life, career, and family in danger.<br/><br/>The writer has done major research on cult membership, recruitment, and social constructs.  Within moments, Ashley is love bombed by the women.  She feels empowered.  But the moment she wants to leave, they leave her in fear, confusion, and broke.  This movie could be a teaching guide.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretsofthesisterhood.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretsofthesisterhood.jpg"
     }
 }
 ];
