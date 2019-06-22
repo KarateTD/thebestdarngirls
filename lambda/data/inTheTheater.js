5265d238f7f9804b2f1052ee0ef18227bd52061f
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-06-21 21:29:34 -0400

    add toy story

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Toy Story 4",
+    "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike with Andy, Woody is not her favorite toy and not the leader of the toys.  Woody, knowing his job is to protect Bonnie, hitch a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>This movie is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background charaters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see his is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl”:”https://thebestdarngirls.s3.amazonaws.com/small-image/toystory4.jpg",
+        "largeImageUrl”:”https://thebestdarngirls.s3.amazonaws.com/large-image/toystory4.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"M.I.B.: International",
     "review":"A little girl witnessed her parent's memory being erased while she helped an alien escape.  Now, she reads the hot sheets and searches for alien life.  One day, she finds an alien crash site protected by a hologram.  She follows agents back to the M.I.B. New York headquarters and walks in.  In seconds, she is detected.  She convinces Agent O to hire her as an M.I.B. agent.  After some debating, Agent O renames her Agent M and hires her on a probationary period.  Agent M is assigned to the London office and finagles her way into becoming Agent H's partner.  Agent H is a legend in M.I.B. because he defeated The Hive with Agent T.  But when M.I.B. suspects a mole, Agent M doesn't know who to trust.  At least, she has Pawny.<br/><br/>This movie falls flat of its predecessors.  While the actors have proven their comedic abilities, the script just doesn't support it.  In addition, the buddy cop movie changes for a budding romance movie.  The CGI is top flight and old characters are brought back for the original fans.  However, its just not enough.  Also, there are no post credit scenes.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Shaft",
     "review":"20 years ago, Maya took John Jr. away to keep him safe from Shaft's dangerous occupation.  Now, John Jr. is an FBI data analyst who is often disrespected.  After Kiram, his best friend, dies of an overdose, John Jr. can't believe it.  Sasha sees the autopsy and confirms Karim was murdered.  After failing to investigate on his own, John decides to go to the only man who can help.  His father, a bad mother - shut your mouth - I'm only talking about Shaft.<br/><br/>This is easily the funniest movie of the year.  Shaft brings the gender gap of masculinity to the surface without getting mushy.  Both men learn something and stand up for their way of life.  See this movie today.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle": "Dark Phoenix",
     "review": "In 1975, Professor X came to Jean Grey's aid after she accidentally killed her parents by causing a car accident.  The lone survivor, Jean decides to stay with Professor X to hone her gift.  Seventeen years later, Jean is one of the most powerful X-Men.  In a mission to save astronauts, Jean is hit with a solar flare that amplifies her power.  Her new powers are so strong, they are releasing the painful secrets in her mind that Professor X closed.  Now, Jean wants answers and leaves the school.  The X-Men go to save her and lose.  Heartbroken by her greatest mistake, Jean seeks out Magneto.  But Vuk is searching for Jean.  That solar flare was really a power surge that destroyed Vuk's planet.  She wants Jean's powers to wipe out humankind and rebuild her planet on earth.  She will do it with Jean or without her.<br/><br/>Be warned, there is no Quicksilver scene.  However, the cinematography is a great representation of Jean's internal struggles.  The CGI is the best of all X-Men movies.  But the overall story is flat.  It should expand on Vuk's life on D'Bari and Jean's relationship with Scott.  See in on the big screen for the effects.  But the next time you will want to see it, is on TV.  In addition, there are no post-credit scenes.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option": "4",
+    "option": "5",
     "mtitle": "Rocketman",
     "review": "Elton John allowed his talents to flourish while being raised by a neglectful mom and cold hearted father.  Looking for the love he missed had home will send him into a downward spiral of drug abuse and an abusive relationship.  But the words, Bernie's words, that catapulted him into the business will save him from himself.<br/><br/>Rocketman is a musical that is too big for any stage.  As Elton reveals the layers of his life, he slowly takes off his costume as a symbolic gesture.  At the end, Elton will take control of his life and make amends with himself.<br/><br/>3.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rocketman.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rocketman.jpg"
-    }
-},
-{
-    "option": "5",
-    "mtitle": "Ma",
-    "review": "Sue Ann ignores Maggie's request to buy alcohol until she sees Andy and his truck.  She changes her mind and buys the alcohol.  Later, she offers them a place to party in her home and even makes snacks.  She becomes the party hub for all the kids in town and they nickname her Ma.  After Maggie and Haley break one of the rules and Ma freaks out, the girls decide they don't want to go to Ma's and tells everyone else to do the same.  Ma must get the girls back to complete her ultimate plan of revenge.<br/><br/>This movie provides a great backing track of 70's music with some cringe worthy moments.  However, you are never afraid of Ma and you have no sympathy for these kids.  See this movie if you like to yell at the screen.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ma.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ma.jpg"
     }
 }
 ];
