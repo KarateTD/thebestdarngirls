3eb5ba24cfa7c4ed8d58c01b09840fa1b41fd84e
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-06-26 23:27:38 -0400

    add stuber

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Stuber",
+    "review":"Stu drives an Uber to help invest in his friends spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on the path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will changes these men's lives forever.  But whether or not it's for the better, is up to you.<br/><br/>Be ready to cry until it hurts.  Don't make this mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  In addition, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Toy Story 4",
     "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike with Andy, Woody is not her favorite toy and not the toy leaders.  Woody, knowing his job is to protect Bonnie, hitches a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>Toy Story 4 is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background characters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see he is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,16 +18,16 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"M.I.B.: International",
     "review":"A little girl witnessed her parent's memory being erased while she helped an alien escape.  Now, she reads the hot sheets and searches for alien life.  One day, she finds an alien crash site protected by a hologram.  She follows agents back to the M.I.B. New York headquarters and walks in.  In seconds, she is detected.  She convinces Agent O to hire her as an M.I.B. agent.  After some debating, Agent O renames her Agent M and hires her on a probationary period.  Agent M is assigned to the London office and finagles her way into becoming Agent H's partner.  Agent H is a legend in M.I.B. because he defeated The Hive with Agent T.  But when M.I.B. suspects a mole, Agent M doesn't know who to trust.  At least, she has Pawny.<br/><br/>This movie falls flat of its predecessors.  While the actors have proven their comedic abilities, the script just doesn't support it.  In addition, the buddy cop movie changes for a budding romance movie.  The CGI is top flight and old characters are brought back for the original fans.  However, its just not enough.  Also, there are no post credit scenes.<br/><br/>2.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mibinternational.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mibinternational.jpg"
     }
-},
+}
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Shaft",
     "review":"20 years ago, Maya took John Jr. away to keep him safe from Shaft's dangerous occupation.  Now, John Jr. is an FBI data analyst who is often disrespected.  After Kiram, his best friend, dies of an overdose, John Jr. can't believe it.  Sasha sees the autopsy and confirms Karim was murdered.  After failing to investigate on his own, John decides to go to the only man who can help.  His father, a bad mother - shut your mouth - I'm only talking about Shaft.<br/><br/>This is easily the funniest movie of the year.  Shaft brings the gender gap of masculinity to the surface without getting mushy.  Both men learn something and stand up for their way of life.  See this movie today.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle": "Dark Phoenix",
     "review": "In 1975, Professor X came to Jean Grey's aid after she accidentally killed her parents by causing a car accident.  The lone survivor, Jean decides to stay with Professor X to hone her gift.  Seventeen years later, Jean is one of the most powerful X-Men.  In a mission to save astronauts, Jean is hit with a solar flare that amplifies her power.  Her new powers are so strong, they are releasing the painful secrets in her mind that Professor X closed.  Now, Jean wants answers and leaves the school.  The X-Men go to save her and lose.  Heartbroken by her greatest mistake, Jean seeks out Magneto.  But Vuk is searching for Jean.  That solar flare was really a power surge that destroyed Vuk's planet.  She wants Jean's powers to wipe out humankind and rebuild her planet on earth.  She will do it with Jean or without her.<br/><br/>Be warned, there is no Quicksilver scene.  However, the cinematography is a great representation of Jean's internal struggles.  The CGI is the best of all X-Men movies.  But the overall story is flat.  It should expand on Vuk's life on D'Bari and Jean's relationship with Scott.  See in on the big screen for the effects.  But the next time you will want to see it, is on TV.  In addition, there are no post-credit scenes.<br/><br/>3 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/darkphoenix.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/darkphoenix.jpg"
-    }
-},
-{
-    "option": "5",
-    "mtitle": "Rocketman",
-    "review": "Elton John allowed his talents to flourish while being raised by a neglectful mom and cold hearted father.  Looking for the love he missed had home will send him into a downward spiral of drug abuse and an abusive relationship.  But the words, Bernie's words, that catapulted him into the business will save him from himself.<br/><br/>Rocketman is a musical that is too big for any stage.  As Elton reveals the layers of his life, he slowly takes off his costume as a symbolic gesture.  At the end, Elton will take control of his life and make amends with himself.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rocketman.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rocketman.jpg"
     }
 }
 ];
