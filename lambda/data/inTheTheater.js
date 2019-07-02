18bde59a45fb8d7fa685edd825abeb1a7f87215d
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-07-02 04:48:49 -0400

    add spiderman

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Spider-Man: Far From Home",
+    "review":"After everyone was snapped back, people want the next Iron Man.  Spider-Man doesn't feel like he can step in.  But Nick Fury needs him now.  The Elementals have invaded Earth.  The wind and earth elements were stopped my Mysterio.  With the assistance of Spider-Man, Mysterio was able to stop the water element in Venice.  Now, Nick wants Peter to go to Prague with Mysterio to stop the most powerful element, Fire.  Peter wants to enjoy his vacation with his friends, so he turns down Nick's offer.  Before Peter and his friends board the bus, the teacher informs them the trip go an upgrade and they are going to Prague.  Peter knows fury has hijacked their vacation.  With Tony Stark's Glasses and EDITH by his side, Peter will take on Fire.  But can Peter trust what he sees?<br/><br/>While Homecoming introduced us to a younger Spider-Man, Far From Home is his coming of age story without Iron Man.  He has to figure out who he wants to be as a man and a superhero.  While the CGI has gotten better, you can still pinpoint moments where you are witnessing 100% CGI versus live capture scenes.  There are 2 post credit scenes and a cameo that original Spider-Man fans will love.  See this movie in IMAX.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/spidermanfarfromhome.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/spidermanfarfromhome.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Yesterday",
     "review":"After a worldwide blackout causes Jack to be hit by a bus, he is the only person who remembers The Beatles.  He decides to jump start his failed singing career by passing off their songs as his own.  Within months, he is the most promising singer/songwriter in history.  But can he keep up the lie if it means losing Ellie?<br/><br/>This movie would be another boring romantic comedy if it wasn't for the music, Debra, and a special cameo.  If you are a fan of The Beatles, see the matinee.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Anna",
     "review":"Anna is a model with a deadly secret.  She is a KGB operative.  After a hit, she is brought into CIA headquarters for questioning.  While Special Agent Leonard lets her go, he keeps an eye on her.  Her purse will lead her to a dangerous cat and mouse game.<br/><br/>This movie tries to be John Wick and the Atomic Blonde with the Tarintino Effect.  It falls short with 2 out of the 3.  The plot is unoriginal, boring, and extremely predictable.  This movie should have gone straight to TV.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Stuber",
     "review":"Stu drives an Uber to help invest in his friends spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on a path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will change these men's lives forever.  But whether or not it's for the better, is up to you.<br/><br/>Be ready to laugh until it hurts.  Don't make the mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  In addition, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Toy Story 4",
     "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike with Andy, Woody is not her favorite toy and not the toy leaders.  Woody, knowing his job is to protect Bonnie, hitches a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>Toy Story 4 is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background characters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see he is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/toystory4.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/toystory4.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"M.I.B.: International",
-    "review":"A little girl witnessed her parent's memory being erased while she helped an alien escape.  Now, she reads the hot sheets and searches for alien life.  One day, she finds an alien crash site protected by a hologram.  She follows agents back to the M.I.B. New York headquarters and walks in.  In seconds, she is detected.  She convinces Agent O to hire her as an M.I.B. agent.  After some debating, Agent O renames her Agent M and hires her on a probationary period.  Agent M is assigned to the London office and finagles her way into becoming Agent H's partner.  Agent H is a legend in M.I.B. because he defeated The Hive with Agent T.  But when M.I.B. suspects a mole, Agent M doesn't know who to trust.  At least, she has Pawny.<br/><br/>This movie falls flat of its predecessors.  While the actors have proven their comedic abilities, the script just doesn't support it.  In addition, the buddy cop movie changes for a budding romance movie.  The CGI is top flight and old characters are brought back for the original fans.  However, its just not enough.  Also, there are no post credit scenes.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mibinternational.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mibinternational.jpg"
     }
 }
 ];
