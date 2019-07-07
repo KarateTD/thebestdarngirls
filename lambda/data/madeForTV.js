cf9a16e20e2ee3d48603cb336b3ed6cd3d2e4a75
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-07-06 23:28:24 -0400

    add 3 movies and 1 commented must buy

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"Trapped Model",
+    "review":"An aspiring model, Grace, answers an ad for a model match up site.  She leaves her mother and boyfriend, Matt, behind to go to Miami and meet Hunter Kelly.  She is picked up by his assistant, Nicole, and given vague details about the models' apartment.  After a few test photos and missed opportunities to leave, Grace is trapped in Hunter's guest house.  She must perform for his live-webcam or he will kill her mother.  It will take a kiss and 3 little words to save Grace from Hunter.<br/><br/>So, the set up is annoying.  Grace can tell this is too good to be true but she still goes to Miami.  The plot is strong.  Grace can sense Hunter and Nicole's weaknesses and uses them to attempt several escapes.  Your heart will beat faster each time she tries.  The conclusion is predictable and average. You can see how the movie will end a mile away.  But you are too committed to change the channel.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/smalll-image/trappedmodel.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/trappedmodel.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"The Wrong Stepmother",
+    "review":"Lily doesn't trust her father's new girlfriend, Maddie.  She does try to be nice for the sake of her father, Michael.  But when Maddie steps over the line one too many times, Lily calls for reinforcements. And when she finds the startling truth about Maddie, it will turn Lily's world upside down.<br/><br/>This movie is too predictable and overdone.  The best moment is when Ms. Price calls Maddie out on her B.S.  This movie can be saved for background noise.  Don't bother putting it on your DVR.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewrongstepmother.jpeg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewrongstepmother.jpeg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"Sister of the Bride",
+    "review":"Stephanie and Tracy are sisters with the same wedding dream: to be married at the family's vacation home on the 4th of July.  While Stephanie is willing to wait a year, Tracy wants to get married in 4 weeks.  If that isn't crazy enough, their parents haven't told them they want to sell the vacation home soon.  With some hard-hitting questions, work commitments, one sick relative, and the search for photographer gets creepy, will either wedding happen?<br/><br/>Oh. The. Shade.  Roger's shade towards Ben is unprecedented and a breath of fresh air for Hallmark movie.  Also, this movie doesn't wrap itself nicely into a bow.  This causes the viewer to be genuinely concerned about Stephanie and Tracy's relationships.  This movie will instantly become a Hallmark classic.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sisterofthebride.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sisterofthebride.jpg"
+    }
+},
+{
+    "option":"4",
     "mtitle":"The Bobby DeBarge Story",
     "review":"Bobby DeBarge wishes he could do it all over again as he thinks about his career and childhood.  He was the eldest son in a family of 10 by interracial parents.  His father, Robert, was physically, mentally, and sexually abusive to his children.  Now, Bobby's star is falling as his siblings' careers are taking off.  Jealousy is hidden behind more drugs.  Bobby's pain will lead to a family, prison, and a life full of regret.<br/><br/>First, you must get past the bad wigs to enjoy this movie. Next, DeBarge fans won't learn anything new about Bobby or the family from this movie.  Movie lovers won't get a true idea of this family's talent because only a few songs are displayed.  But, this is still a heartbreaking movie.  Seeing the children abused by their father and the pattern of abuse continue with the family will make you sick to your stomach.  It's amazing more of the family didn't turn to drugs.  This movie feels more like a Part 2 of the DeBarge movie than a Bobby DeBarge movie.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
 },
 {
-    "option":"2",
+    "option":"5",
     "mtitle":"Family Pictures",
     "review":"After spotting her dad's picture on another family's wall, Eve runs to her mom Sylvie with the horrible news.  However, Sylvie starts the conversation first and tells Eve she knows that her bulimia is back.  Eve decides to keep quiet about Mark.  But Grace confronts her mother, Maggie, head-on with the accusations.  But Maggie is more upset with Grace's timing.  Soon, Maggie and Sylvie can't ignore the elephant in the room and start to communicate.  They develop a friendship which allows them to move on and grow.  But when Sylvie betrays Maggie, their friendship will be pulled apart.  Can they get back on track?<br/><br/>This movie will take you on a roller coaster ride.  Since this movie is based on a book, it doesn't give you the Lifetime movie conclusion of a dead husband.  Instead, this movie gives you adult conversations, reasonable solutions. and a satisfying ending.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/familypictures.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/familypictures.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Anniversary Nightmare",
-    "review":"Liz and Andrew celebrate their 10th anniversary in Hawaii.  However, she wakes up alone with blood on her.  When the cops investigate, they find a burning body and arrest Liz.  Liz is able to get back to the mainland because her mother pulls some strings.  Liz tries to maintain a sense of normalcy, but she is arrested for manslaughter and tampering with evidence.  In prison, she makes friends that convince her to investigate Andrew's disappearance on her own.  The first step in her plan: break out of jail.<br/><br/>This movie uses the Lifetime platform but changes your suspected ending.  The ending will throw seasoned Lifetime fans for a loop while still being unsatisfying.  This movie feels like a statement for prison and sentencing reform than a mystery plot.  Also, the backing track is often too loud and there is an outrageous number of close-ups.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/anniversarynightmare.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/anniversarynightmare.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Mystery 101: Playing Dead",
-    "review":"Professor Amy is a script consultant for a local play.  While there is scripted drama on stage, the real show is happening behind the scenes.  Three men love the same woman, Bella.  Her husband Duncan.  Her ex Sebastian.  And her co-star Elliot.  But when a stage light almost kills Bella, Amy is worried but Bella wants to continue.  During the play, Lacey comes on stage and fires her gun.  To everyone's surprise, it fired a real bullet almost killing Bella.  Detective Travis stops the play and makes it an active crime scene.  The play is over but Bella still has the after party.  Duncan and Elliot fight and the party is over.  The next morning someone will be dead.<br/><br/>Record this movie and watch it 3 times.  There is so much going on because you aren't unraveling the case with Amy, you are a part of it.  The murder doesn't occur until after the 1st hour.  You may believe you have it figured out.  But you don't.  This is a strong mystery that needs your undivided attention.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/msytery101playingdead.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/msytery101playingdead.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"The Last Bridesmaid",
-    "review":"Becca is the only single bridesmaid.  And after 9 weddings, she still hasn't found Mr. Right.  She met Kyle, the photographer at the wedding but he is not a romantic.  After her cousin, Lucy gets engaged, Becca is the maid of honor and Kyle is the videographer.  Lucy points out that Kyle is cute but Becca wants a hopeless romantic like her.  So, Lucy sets her up with Aiden, the best man.  While busy at the wedding, Kyle sees the wedding shop owner take a shine to Becca's jewelry.  She offers to buy a few pieces but Becca says no.  Kyle agrees to create and show his dream film project if she lets go of her fears and sells her jewelry.  Letting go of their fears helps them find love together.<br/><br/>This movie is so funny as Becca and Kyle trade verbal jabs.  They always sound like an old married couple.  It's easy to see the love between them as they lose their fears.  This movie will bring a smile to your face.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelastbridesmaid.jpeg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelastbridesmaid.jpeg"
     }
 }
 ];
