9fc56727816ff8a9d053011f679a86941b79d178
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-07-12 02:47:51 -0400

    add blinded by the light

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Blinded by the Light",
+    "review":"Javed is harassed on the street and controlled at home.  While his factory-working father, Malik, wants Javed to be an accountant or lawyer, Javed dreams of being a writer.  After bumping Foop's cassettes on the ground, Foops lets Javed borrow them.  Javed can't believe a white American will have any idea what he goes through.  At the lowest point in his life, Javed turns on the cassette and presses play.  That pivotal decision will alter the course of his life forever.<br/><br/>Based on a memoir, this movie chooses the best Bruce Springsteen lyrics for critical moments in Javed's life.  He finds the courage to write, talk to a girl, and stand up to the haters.  Springsteen fans will fall in love while Javed goes through self-discovery with Bruce leading the way.  Like 'Yesterday', the music is more memorable than the movie.  Also, don't give up on Malik, he is a strong man with a lot of pain inside.  You will like this movie but not enough to buy it.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blindedbythelight.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blindedbythelight.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Spider-Man: Far From Home",
     "review":"After everyone was snapped back, people want the next Iron Man.  Spider-Man doesn't feel like he can step into Tony's shoes.  But Nick Fury needs him now.  The Elementals have invaded Earth.  The Wind and Earth elements were stopped by Mysterio.  With the assistance of Spider-Man, Mysterio was able to stop the water element in Venice.  Now, Nick wants Peter to go to Prague with Mysterio to stop the most powerful element, Fire.  Peter wants to enjoy his vacation with his friends, so he turns down Nick's offer.  Before Peter and his friends board the bus, the teacher informs them the trip got an upgrade and they are going to Prague.  Peter knows fury has hijacked their vacation.  With Tony Stark's Glasses and EDITH by his side, Peter will take on Fire.  But can Peter trust what he sees?<br/><br/>While Homecoming introduced us to a younger Spider-Man, Far From Home is his coming of age story without Iron Man by his side.  He has to figure out who he wants to be as a man and a superhero.  While the CGI has gotten better, you can still pinpoint moments where you are witnessing 100% CGI versus live-capture scenes.  There are 2 post credit scenes and a cameo that original Spider-Man fans will love.  See this movie in IMAX.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Yesterday",
     "review":"After a worldwide blackout causes Jack to be hit by a bus, he is the only person who remembers The Beatles.  He decides to jump start his failed singing career by passing off their songs as his own.  Within months, he is the most promising singer/songwriter in history.  But can he keep up the lie if it means losing Ellie?<br/><br/>This movie would be another boring romantic comedy if it wasn't for the music, Debra, and a special cameo.  If you are a fan of The Beatles, see the matinee.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Anna",
     "review":"Anna is a model with a deadly secret.  She is a KGB operative.  After a hit, she is brought into CIA headquarters for questioning.  While Special Agent Leonard lets her go, he keeps an eye on her.  Her purse will lead her to a dangerous cat and mouse game.<br/><br/>This movie tries to be John Wick and the Atomic Blonde with the Tarintino Effect.  It falls short with 2 out of the 3.  The plot is unoriginal, boring, and extremely predictable.  This movie should have gone straight to TV.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Stuber",
     "review":"Stu drives an Uber to help invest in his friends spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on a path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will change these men's lives forever.  But whether or not it's for the better, is up to you.<br/><br/>Be ready to laugh until it hurts.  Don't make the mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  In addition, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Toy Story 4",
-    "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike with Andy, Woody is not her favorite toy and not the toy leaders.  Woody, knowing his job is to protect Bonnie, hitches a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>Toy Story 4 is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background characters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see he is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/toystory4.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/toystory4.jpg"
     }
 }
 ];
--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -97,16 +97,6 @@ module.exports = mustBuy;
 }
 
 {
-    "option":"1",
-    "mtitle":"The Secret Life of Pets 2",
-    "review":"Max wants a life with just him, Duke and Katie.  No kids allowed.  Until Katie gets married to Chuck and gives birth to Liam.  Now, he will do anything to protect Liam.  Max is so stressed with Liam's safety, he is scratching uncontrollably.  Coned by a behavioral specialist, Max his happy to go on a trip in a car.  But this city dog will have to adapt to farm life while caring for Liam.<br/><br/>This is only one of the three plots to this amazing movie.  It is an instant classic with jokes you and your children will be laughing at simultaneously.  While there is nothing after the end credits, you will see pictures of kids and pets and enjoy a Snowball rap.  This movie is award worthy.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/thesecretlifeofpets2.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/thesecretlifeofpets2.jpg"
-    }
-}
-
-{
     "option":"4",
     "mtitle":"John Wick: Chapter 3 - Parabellum",
     "review":"Winston gives John 1 hour to get his affairs in order.  Then he will be excommunicated and a bounty of $14 million will be placed on his head.  Every assassin in the world will be after him and no one will offer help.  John uses the hour to find a safe place for his dog, get a ticket, get a marker, and get some gold coins.  Since some assassins couldn't wait for an hour, John needed to seek medical treatment.  In the middle of getting stitches, John's time runs out and the doctor stops the work.  John has to complete is stitches and heads to the Director.  He asks the Director to give him passage to Casablanca.  He knows an assassin there who can lead him to the High Table.  He hopes to plead his case so the High Table will let him live.<br/><br/>This movie proves better weapons, excellent choreographed fights, and mind blowing cinematography is all action lovers need.  With so much going on, this movie provides 30 to 45 minutes of dialogue.  It gives you more of John's training history with the Director but not enough information of the blood oath between him and Sophia.  Seeing John Wick 2 is necessary for this movie.<br/><br/>4.5 out of 5 stars.",
