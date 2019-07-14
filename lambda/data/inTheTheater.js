e4508eab9b1853760ca1a77881cee7f35675c190
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-07-14 16:13:41 -0400

    adding crawl

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Crawl",
+    "review":"After swim practice, Beth tells Hailey hurricane Wendy has changed course and headed straight for Florida.  Beth has been trying to call their father, Dave, all day but can't reach them.  She can't find him at his condo, so she goes to the family's old lake house.  There she finds her dad wounded in the crawlspace of the home with 2 alligators.  To make matters worse, the hurricane floodwaters have swept alligators around their home and the crawlspace if flooding.  Hailey and Dave have to find a way out without being eaten.<br/><br/>Talk about the worst day ever!  This movie is a mix of 47 Meters Down, Deep Blue Sea, and The Shallows.  Each smart step the pair try to take is met with another obstacle.  Even staying the safe part of the crawl space means drowning.  The CGI alligators are well done and the kill scenes well executed.  This movie is a decent jump scare or a good laugh, depending on your level of the thriller movie experience.<br/><br/>3 out of 5 stars",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/crawl.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/crawl.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Blinded by the Light",
     "review":"Javed is harassed on the street and controlled at home.  While his factory-working father, Malik, wants Javed to be an accountant or lawyer, Javed dreams of being a writer.  After bumping Foop's cassettes on the ground, Foops lets Javed borrow them.  Javed can't believe a white American will have any idea what he goes through.  At the lowest point in his life, Javed turns on the cassette and presses play.  That pivotal decision will alter the course of his life forever.<br/><br/>Based on a memoir, this movie chooses the best Bruce Springsteen lyrics for critical moments in Javed's life.  He finds the courage to write, talk to a girl, and stand up to the haters.  Springsteen fans will fall in love while Javed goes through self-discovery with Bruce leading the way.  Like 'Yesterday', the music is more memorable than the movie.  Also, don't give up on Malik.  He is a strong man with a lot of pain inside.  You will like this movie but not enough to buy it.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Spider-Man: Far From Home",
     "review":"After everyone was snapped back, people want the next Iron Man.  Spider-Man doesn't feel like he can step into Tony's shoes.  But Nick Fury needs him now.  The Elementals have invaded Earth.  The Wind and Earth elements were stopped by Mysterio.  With the assistance of Spider-Man, Mysterio was able to stop the water element in Venice.  Now, Nick wants Peter to go to Prague with Mysterio to stop the most powerful element, Fire.  Peter wants to enjoy his vacation with his friends, so he turns down Nick's offer.  Before Peter and his friends board the bus, the teacher informs them the trip got an upgrade and they are going to Prague.  Peter knows fury has hijacked their vacation.  With Tony Stark's Glasses and EDITH by his side, Peter will take on Fire.  But can Peter trust what he sees?<br/><br/>While Homecoming introduced us to a younger Spider-Man, Far From Home is his coming of age story without Iron Man by his side.  He has to figure out who he wants to be as a man and a superhero.  While the CGI has gotten better, you can still pinpoint moments where you are witnessing 100% CGI versus live-capture scenes.  There are 2 post credit scenes and a cameo that original Spider-Man fans will love.  See this movie in IMAX.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,21 +27,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Yesterday",
     "review":"After a worldwide blackout causes Jack to be hit by a bus, he is the only person who remembers The Beatles.  He decides to jump start his failed singing career by passing off their songs as his own.  Within months, he is the most promising singer/songwriter in history.  But can he keep up the lie if it means losing Ellie?<br/><br/>This movie would be another boring romantic comedy if it wasn't for the music, Debra, and a special cameo.  If you are a fan of The Beatles, see the matinee.<br/><br/>3 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/yesterday.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/yesterday.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Anna",
-    "review":"Anna is a model with a deadly secret.  She is a KGB operative.  After a hit, she is brought into CIA headquarters for questioning.  While Special Agent Leonard lets her go, he keeps an eye on her.  Her purse will lead her to a dangerous cat and mouse game.<br/><br/>This movie tries to be John Wick and the Atomic Blonde with the Tarintino Effect.  It falls short with 2 out of the 3.  The plot is unoriginal, boring, and extremely predictable.  This movie should have gone straight to TV.<br/><br/>2 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/anna.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/anna.jpg"
     }
 },
 {
