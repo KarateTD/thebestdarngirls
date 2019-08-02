5c08ffb91abacaaa9d6f92776ef80ef17152902b
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-08-02 02:49:07 -0400

    add hobbs and shaw

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Fast and Furious Presents: Hobbs and Shaw",
+    "review":"Hattie injects herself with a deadly virus to prevent Brixton and Eteon from obtaining it.  After Brixton makes Hattie look like an M.I-6 traitor, Hattie is enemy number one.  In L.A., Hobbs gets a call to find Hattie and bring her and the virus back to the CIA.  In London, Shaw is called to find Hattie and the virus.  The two men answer the call but refuse to work together.  Hobbs finds Hattie with logistics, but Shaw uses his heart.  Hattie is his sister.  When Hobbs catches Hattie and brings her back to the CIA, both men believe they have a handle on the situation, but Hattie reveals the truth.  The virus is inside of her in plastic capsules.  If they don't find a way to remove it, the virus will melt her from the inside out and spread in epidemic proportions.  Also, Shaw reveals Hattie is his sister.  The two must work together to save the world and find Brixton before the virus kills Hattie.<br/><br/>This spin-off to the Fast and Furious franchise provides a more in-depth view of Hobbs and Shaw's histories but is not necessary to the overall franchise storyline.  While it helps to see the last 4 movies, you don't need to see this one to enjoy the franchise.  The action sequences are nothing new.  Also, the final battle has new school hacking mixed with old school Samoan fighting style.  See it in IMAX with the matinee price and stay past the end credits.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hobbsandshaw.jpeg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hobbsandshaw.jpeg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Once Upon a Time in Hollywood",
     "review":"Rick Dalton lives next door to Sharon Tate and Roman Polanski.  As Sharon's star rises in Hollywood, Rick's star is falling.  He played in a popular western years ago but he has been reduced to guest starring as the 'heavy' on newer, more popular shows.  Producer Marvin Schwarzs warns Rick this will be the end of his career.  Rick isn't the only one effected by his failing career.  His stunt double, Cliff, is feeling the effects too.  Now, Cliff works as Rick's driver and gofer to make ends meet.  With 2 nerve-racking encounters with the Manson Family, one August night will end in bloodshed.<br/><br/>This period piece paid close attention to the details of the time.  The clothes, slang, tech, and commercials were all done perfectly.  It's hard to establish what's true or false when watching this movie.  You will feel the urge to do research the moment you get home.  However, you won't feel the urge to buy.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,21 +18,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Disney's The Lion King",
     "review":"Simba is next in line for the throne.  But his Uncle Scar believes it's his rightful place.  Scar convinces Simba to go to the gorge to practice his roar.  Soon, Simba is in the middle of a stampede.  Scar runs to his brother, King Mufasa to help Simba but its a trap.  When Simba isn't looking, Scar kills Mufasa and blames the death on Simba.  Scar tells Simba to run away and never come back.  Scar promotes the lawless hyenas as his army while they lay waste to the pride.  It will take one old friend, 2 new friends, and fur to bring Simba back to the pride.<br/><br/>The live-adaption of this Disney classic stays true to the original with astounding attention to detail within mind-blowing graphics.  However, making these animals look like animals, takes away the emotions the animated characters possessed.  This forced the movie to rely on the voice actors to convey every pure emotion.  Some succeeded while other's fell flat.  Also, the song \"Can You Feel the Love Tonight\" is performed midday.  It feels out of place.  Overall, the movie may not be better than the first but it is still a strong movie worth seeing.  It's better in IMAX and/or 3D.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelionking.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelionking.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Crawl",
-    "review":"After swim practice, Beth tells Hailey hurricane Wendy has changed course and headed straight for Florida.  Beth has been trying to call their father, Dave, all day but can't reach them.  Hailey searches for him but can't find him at his condo.  So, she goes to the family's old lake house.  There, she finds her dad wounded in the crawlspace of the home with 2 alligators.  To make matters worse, the hurricane floodwaters have swept alligators around their home and the crawlspace is flooding.  Hailey and Dave have to find a way out without being eaten.<br/><br/>Talk about the worst day ever!  This movie is a mix of 47 Meters Down, Deep Blue Sea, and The Shallows.  Each smart step the pair try to take is met with another obstacle.  Even staying in the safe part of the crawl space means drowning.  The CGI alligators are well done and the kill scenes well executed.  This movie is a decent jump scare or a good laugh, depending on your level of the thriller movie experience.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/crawl.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/crawl.jpg"
     }
 },
 {
@@ -52,19 +52,4 @@ module.exports = inTheTheaters;
 *  do not user '&' use the word 'and'
 *  small image 720w x 480h (in pixels)
 *  large image 1200w x 800h (in pixels)
-*/
-
-/*
-{
-    "option":"1",
-    "mtitle":"Fast and Furious Presents: Hobbs and Shaw",
-    "review":".<br/><br/>* out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hobbsandshaw.jpeg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hobbsandshaw.jpeg"
-    }
-}
-
-
-
 */
