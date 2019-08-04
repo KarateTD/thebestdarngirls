370b680d5bab16bc40cae97c3f0cfe4927af22cc
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-08-04 01:15:37 -0400

    update with 5 tv movie and 1 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"The Angry Birds Movie 2",
+    "review":"Red loves being the Bird Island hero.  But when Leonard offers a truce, Red secretly doesn't want it.  He worries he won't be the hero and the town won't like him.  But they have a new enemy in a third island they never knew existed.  And this rivalry goes beyond the Pigs and Bird's wars.  It goes all the way back to the 90s.<br/><br/>Sequel to The Angry Birds movie is no better or worse that the first movie.  It will cause parents and kids to have a light chuckle like but won't leave a lasting impression.  See the matinee with the kids.  Everyone else, wait for Netflix.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theangrybirdsmovie2.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theangrybirdsmovie2.jpg"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Fast and Furious Presents: Hobbs and Shaw",
     "review":"Hattie injects herself with a deadly virus to prevent Brixton and Eteon from obtaining it.  After Brixton makes Hattie look like an M.I. 6 traitor, Hattie is enemy number one.  In L.A., Hobbs gets a call to find Hattie and bring her and the virus back to the CIA.  In London, Shaw is called to find Hattie and the virus.  The two men answer the call but refuse to work together.  Hobbs finds Hattie with logistics, but Shaw uses his heart.  Hattie is his sister.  When Hobbs catches Hattie and brings her back to the CIA, both men believe they have a handle on the situation, but Hattie reveals the truth.  The virus is inside of her in plastic capsules.  If they don't find a way to remove it, the virus will melt her from the inside out and spread in epidemic proportions.  Also, Shaw reveals Hattie is his sister.  The two must work together to save the world and find Brixton before the virus kills Hattie.<br/><br/>This spin-off to the Fast and Furious franchise provides a more in-depth view of Hobbs and Shaw's histories but is not necessary to the overall franchise storyline.  While it helps to see the last 4 movies, you don't need to see this spin-off to enjoy the franchise.  The action sequences are nothing new.  Also, the final battle has new school hacking mixed with old school Samoan fighting style.  See it in IMAX with the matinee price and stay past the end credits.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Once Upon a Time in Hollywood",
     "review":"Rick Dalton lives next door to Sharon Tate and Roman Polanski.  As Sharon's star rises in Hollywood, Rick's star is falling.  He played in a popular western years ago but he has been reduced to guest starring as the 'heavy' on newer, more popular shows.  Producer Marvin Schwarzs warns Rick this will be the end of his career.  Rick isn't the only one effected by his failing career.  His stunt double, Cliff, is feeling the effects too.  Now, Cliff works as Rick's driver and gofer to make ends meet.  With 2 nerve-racking encounters with the Manson Family, one August night will end in bloodshed.<br/><br/>This period piece paid close attention to the details of the time.  The clothes, slang, tech, and commercials were all done perfectly.  It's hard to establish what's true or false when watching this movie.  You will feel the urge to do research the moment you get home.  However, you won't feel the urge to buy.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Disney's The Lion King",
     "review":"Simba is next in line for the throne.  But his Uncle Scar believes it's his rightful place.  Scar convinces Simba to go to the gorge to practice his roar.  Soon, Simba is in the middle of a stampede.  Scar runs to his brother, King Mufasa to help Simba but its a trap.  When Simba isn't looking, Scar kills Mufasa and blames the death on Simba.  Scar tells Simba to run away and never come back.  Scar promotes the lawless hyenas as his army while they lay waste to the pride.  It will take one old friend, 2 new friends, and fur to bring Simba back to the pride.<br/><br/>The live-adaption of this Disney classic stays true to the original with astounding attention to detail within mind-blowing graphics.  However, making these animals look like animals, takes away the emotions the animated characters possessed.  This forced the movie to rely on the voice actors to convey every pure emotion.  Some succeeded while other's fell flat.  Also, the song \"Can You Feel the Love Tonight\" is performed midday.  It feels out of place.  Overall, the movie may not be better than the first but it is still a strong movie worth seeing.  It's better in IMAX and/or 3D.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Blinded by the Light",
     "review":"Javed is harassed on the street and controlled at home.  While his factory-working father, Malik, wants Javed to be an accountant or lawyer, Javed dreams of being a writer.  After bumping Foop's cassettes on the ground, Foops lets Javed borrow them.  Javed can't believe a white American will have any idea what he goes through.  At the lowest point in his life, Javed turns on the cassette and presses play.  That pivotal decision will alter the course of his life forever.<br/><br/>Based on a memoir, this movie chooses the best Bruce Springsteen lyrics for critical moments in Javed's life.  He finds the courage to write, talk to a girl, and stand up to the haters.  Springsteen fans will fall in love while Javed goes through self-discovery with Bruce leading the way.  Like 'Yesterday', the music is more memorable than the movie.  Also, don't give up on Malik.  He is a strong man with a lot of pain inside.  You will like this movie but not enough to buy it.<br/><br/>3 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blindedbythelight.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blindedbythelight.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Stuber",
-    "review":"Stu drives an Uber to help invest in his friends spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on a path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will change these men's lives forever.  But whether or not it's for the better, is up to you.<br/><br/>Be ready to laugh until it hurts.  Don't make the mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  In addition, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg"
     }
 }
 ];
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
 {
     "option":"1",
-    "mtitle":"V.C. Andrews's Heaven",
-    "review":"Before she died, Heaven's grandmother told her the family secret.  Sarah Casteel is not her mother.  Her mother Leigh Vanvoreen, died giving birth to her.  She is astounded by the omission, but there is more to come.  Her father sold all his children except Heaven.  But after one night together, he sells her too.  Heaven sees Kitty and Cal's home and wealth and she believes this is the fresh start she needed.   Soon this fresh start will leave her on the wrong side of family gossip.<br/><br/>If you are a fan of the book, don't watch the movie.  Important character traits, backstories, and characters are missing.  It's as if the screenplay writer or the director only read the first book.  For everyone else, you will love the movie for providing new and exciting twist and turns to the Lifetime format.  Be ready to spit out your tea.<br/><br/>4 out of 5 stars.",
+    "mtitle":"V.C. Andrews' Dark Angel",
+    "review":"Heaven left her small town life behind for her mother's family.  She is taken back by their wealth and instantly out of place.  Her grandmother, Jillian only wanted her around for a few days, her step-grandfather, Tony, offered to make the stay permanent.  She just has to give up her father's side of the family and give into his needs.  Heaven agress and becomes a lady of wealth.  But when she gets engaged, lies told by everyone are exposed leave a path of pain, destruction, and death.<br/><br>Book lovers beware, this movie hasn't fixed the first movies mistakes.  Movie lovers, get ready for some the biggest shocks in Lifetime's history.  One remarkable aspect is the costume and set design work.  As Heaven transforms her clothes do too but they stay within the margins of the time period.  This attention to detail is paid to the sets as well.<br/><br/>4 out of 5 stars.",
     "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsheaven.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsheaven.jpg"
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewdarkangel.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewdarkangel.jpg"
     }
 },
 {
     "option":"2",
-    "mtitle":"The Wrong Tutor",
-    "review":"After a D.U.I., Eric is under the watchful eye of his mother, coach, and girlfriend Jess.  He is getting stronger on the field but still failing in school.  He worries he will be kicked off the team until the new girl, Emily, offers to tutor him.  She doesn't want money, she wants introductions to the popular kids.  Eric agrees and he starts during better at school.  But when an argument with Jess leads to a kiss with Emily, Eric quickly tells Emily it was a mistake.  The next day, Eric explains he will never kiss her again and Emily seems ok.  But on the inside, she is coming unhinged and ready to kill.  She will do anything to anyone to have Eric for herself.<br/><br/>While this movie builds with all the Lifetime hints, it leads to nowhere.  You are not sure if the movie ended or could there be another 30 minutes.  This movie feels more like a P.S.A. on filing false sexual harassment claims than a crazy stalker movie.<br/><br/>2 out of 5 stars.",
+    "mtitle":"Disney's Descendants 3",
+    "review":"After Hades attempts to escape on V.K. Day, Mal makes the decision to close the barrier around Auradon for good.  This change in ideals hurts King Ben so she decides to keep it secret.  When a thief takes the crown and Malificent's septor, Mal is hit with a dose of magic that can only be reversed by Hades's amulet.  The V.K.'s go back to the Isle to retrieve the septor and learn the thief has cast a spell on Auradon.  Now, Mal has to depend on the villians she knows to stop a villian she has never encountered.  What what will happen to Mal's friendships when her decision to close the barrier is exposed?<br/><br/>A bittersweet movie for Disney's Descendants franchise will bring joy, and a little sorrow, to every fans' heart.  With the untimely passing of Cameron Boyce, this movie should be deemed as a celebration of his life, talent, and passion.  With memorable songs, costumes, and choregraphy, this movie still maintained a strong plot.  It differed from the first two films because it didn't rely on the bad guys you know.  It create new characters and delivered a powerful message to kids and adults alike.  See this movie with the whole family.  Also, donate to The Cameron Boyce Foundation.<br/><br/>5 out of 5 stars.",
     "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewrongtutor.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewrongtutor.jpg"
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/descendants3.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/descendants3.jpg"
     }
 },
 {
     "option":"3",
-    "mtitle":"Loved to Death",
-    "review":"Monica wants to help Jackson achieve his dreams but he can't seem to hold down a job.  While her mother and sister, Tiffany, tolerate him because he is Mr. Fix-it, her sister, Courtney and her daughter Meghan make their dislike known.  After a warning letter, bruises, and a god-fearing man, Monica feels the strength to leave Jackson behind.  But what will it cost her?<br/><br/>TV One's third Love, Lies, and Murder movie gives another end to this type of toxic relationship.  While Jackson never hits her, he leaves bruises and intimidates her.  Also, he displays a narcissistic attitude.  This movie hints at a falling out between Meghan and Jackson but it never really explains what happened.  Also, it never goes into depth about Monica's last marriage.  This history would give us more insight into Monica's thinking.<br/><br/>4 out of 5 stars.",
+    "mtitle":"A Lover Scorned",
+    "review":"When Brooke meets her new insurance agent, Jake, she is reminded that her marriage is empty and loveless.  Jake and Brooke start to have an affair.  Out of guilt, Brook tells her husband Steve.  Steve takes the blame and wants to repair the relationship by going to the mountains.  Brooke tells Jake she wants to work it out, Jake comes up with a plan: To kill Steve.  With 15 million reasons, can Jake convince Brooke to go with his plan.<br/><br/>So, who is really running the game and who are the players?  Record this movie because you will need to watch it twice to keep up.  With so many people having a play, it's hard to see who is in control?  There are a few unanswered questions with this movie but the original plot makes up for the holes.<br/><br/>4.5 out of 5 stars.",
     "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lovedtodeath.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lovedtodeath.jpg"
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aloverscorned.jpeg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aloverscorned.jpeg"
     }
 },
 {
     "option":"4",
-    "mtitle":"In Broad Daylight",
-    "review":"After handing her son to a childcare professional, Jordan is kidnapped in broad daylight.  Her family worries Jordan was another victim of the man who kidnapped Ophelia Baker.  But Jordan's cousin, Malik, is keeping a secret.  He believes it was Jordan's ex and father of her child, Steve.  While Jordan's family knows she had a restraining order against Steve, they believe the two were on good terms.  However, Malik knows Steve was abusive and stalking Jordan.  Jordan's attempts to keep family out of her business has clouded their judgment.  Now, to save Jordan's life, Malik has to do the one thing he promised not to do: break her confidence.  Can they save Jordan in time?<br/><br/>Inspired by true events, this movie not only covers the physical side of abuse, but it also covers the psychological as well.  Under the guise of caring for her, Steve moves her away from family, keeps her isolated, and convinces her to leave her job.  On the other hand, before they met, Jordan took the blame for her past relationship failures.  While she is a strong woman, consistently taking the blame makes her prey.  For a 2-hour movie, these characters are well written and have incredible depth.  They are expertly acted to convey every emotion in an abusive relationship.  In seconds, Steve is loving, scary, and loving all over again.  As the relationship hits a downward spiral, you can see the light fade from Jordan's eyes as she flinches at Steve's touches.  While Steve is abusive to Jordan, the movie is careful not to portray him as completely evil.  He continues to be a loving and caring father to his son and his reason for control is given in great detail.  This is one to watch over again and discuss.  By the way, do you know any Barbeque Babies?<br/><br/>5 out of 5 stars.",
+    "mtitle":"Deadly Dispatch",
+    "review":"After her friend Shawn is killed, Tiffany is devastated.  She goes to the funeral home to help plan his homegoing and is shocked, yet again, to hear his wife, Amber, wants to cremate him.  Tiffany knows Shawn wouldn't want that and Amber's lack of respect for Shawn leads Tiffany to believes that Amber his behind his murder.  She has 3 days to find evidence or Shawn will be cremated and Amber will go free.<br/><br/>The last movie in the Love, Lies, and Murder promo fall flat of the others.  This movie feels more like a Lifetime movie that TV One.  Since the ending is predictable, the journey must be worthwhile.  This one missed the mark.  The best moment is the revealed secret at the end.<br/><br/>3 out of 5 stars.",
     "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/inbroaddaylight.png",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/inbroaddaylight.png"
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlydispatch.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlydispatch.jpg"
     }
 },
 {
     "option":"5",
-    "mtitle":"Sins of a Father",
-    "review":"Detective Felicia sits across a sweet young man who killed his stepmother.  She doesn't believe he did it alone but he won't talk to her.  His mother, Mrs. Banks, explains that Robert is under the thumb of a manipulative con-artist: His father, Pastor Clarence.  Her parents helped Clarence legally take Robert from her because they disapproved of her sexuality.  Once Robert moved in with Clarence and Karen, she never heard or saw her son until now.  She believes Robert pulled the trigger but Clarence put the murder plot in his hands, mind, and heart.  Now, Detective Felicia has to make Robert do the unthinkable: Turn against his father.<br/><br/>This movie is a study in nature vs. nurture.  Reminiscent of the Cinnamon Brown case, Clarence will manipulate anyone to win.  He commits all the seven deadly sins and sees the 10 commandments as instructions.  The script falls flat but the superb acting saves this film.<br/><br/>3 out of 5 stars.",
+    "mtitle":"Rome in Love",
+    "review":"Katherine convinces her sister Amelia to tryout for the lead in an Italian movie.  She is flabbergasted when she gets the part.  However, she doesn't know how to handle all the new demands.  To get ahead of the media, her director Dominic hires a local paper to write about her.  The writer happens to be Philip.  Philip was her bellhop and pretended to not speak English when they met.  As he interviews her, they get close.  But when he breaks a promise, will this budding romance survive his writing?<br/><br/>Inspired by a book, this movie just doesn't capture your full attention.  The side characters are more interesting with the lengths they go through to keep them together.  This is a movie to watch if you want to kill 2 hours..<br/><br/>2 out of 5 stars.",
     "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sinsofafather.jpeg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sinsofafather.jpeg"
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/romeinlove.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/romeinlove.jpg"
     }
 }
 ];
