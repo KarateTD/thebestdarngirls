8bd981c7f7217f6707a849a2b69e7325400ef4bd
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-08-11 00:23:42 -0400

    a few updates

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -2,7 +2,7 @@ var madeForTV = [
 {
     "option":"1",
     "mtitle":"V.C. Andrews' Fallen Hearts",
-    "review":"Heaven is back home and living with her grandfather.  She is making a good life as a teacher.  But when tragedy strikes her family again, Heaven places the blame on herself.  And uses a wise man's last words to give forgiveness.  Heaven marries Logan and hopes for a bright future.  It's not long that the ghost of her past start paving a dark road to her future.  What secrets will be revealed in this installment of the Casteel series?<br/><br/>Book lovers rejoice!  This movie fixes a few of the wrongs in the first movie.  Directed by Jason Priestly, this movie captures the actors but the scenery is well.  This is, no doubt, a testament to the great job the set and costume designers are doing.  It makes the story stronger as the Casteel family airs their dirty laundry for the whole town to see.<br/><br/>4.5 out of 5 stars.",
+    "review":"Heaven is back home and living with her grandfather.  She is making a good life as a teacher.  But when tragedy strikes her family again, Heaven places the blame on herself.  And uses a wise man's last words to give forgiveness.  Heaven marries Logan and hopes for a brighter future.  It's not long that the ghost of her past start paving a dark road to her future.  What secrets will be revealed in this installment of the Casteel series?<br/><br/>Book lovers rejoice!  This movie fixes a few of the wrongs in the first movie.  Directed by Jason Priestly, this movie captures the actors but the scenery is well.  This is, no doubt, a testament to the great job the set and costume designers are doing.  It makes the story stronger as the Casteel family airs their dirty laundry for the whole town to see.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsfallenhearts.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsfallenhearts.jpg"
@@ -11,7 +11,7 @@ var madeForTV = [
 {
     "option":"2",
     "mtitle":"Deadly Influencer",
-    "review":" Monica helped Skyler grow her social media following.  With a management agency calling her, Skyler decides to leave Monica.  Monica pushes her off a balcony.  Now, Monica is focused on Jessica.  She believes Jessica will have a bigger following than Skylar.  Monica wants to manage Jessica's social career and create a management brand.  She just has to get Jessica to sign a 10-year contract.  And if anyone gets in her way, off with their heads.<br/><br/>Giving a new meaning to a moment of silence, this movie delves into so deep lessons for teens while remaining entertaining.  It talks about confusing likes with being liked.  Also, it analysis the downside of being Insta-famous.  Watch this movie with your teen.<br/><br/>4.5 out of 5 stars.",
+    "review":" Monica helped Skyler grow her social media following.  With a management agency calling her, Skyler decides to leave Monica.  Monica pushes her off a balcony.  Now, Monica is focused on Jessica.  She believes Jessica will have a bigger following than Skyler.  Monica wants to manage Jessica's social career and create a management brand.  She just has to get Jessica to sign a 10-year contract.  And if anyone gets in her way, off with their heads.<br/><br/>Giving a new meaning to a moment of silence, this movie delves into to deep lessons for teens while remaining entertaining.  It talks about confusing likes with being liked.  Also, it analysis the downside of being Insta-famous.  Watch this movie with your teen.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlyinfluencer.jpeg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlyinfluencer.jpeg"
@@ -20,7 +20,7 @@ var madeForTV = [
 {
     "option":"3",
     "mtitle":"Aurora Teagarden: A Game of Cat and Mouse",
-    "review":"Aurora display for her Real Mystery club is destroyed by a vandal.  But the left a clue behind reference Shakespeare.  Aurora concludes the vandal leaves clues for their next target.  When one clue references a librarian, Aida is worried for Aurora's safety.  Aurora is determined to save herself and find the vandal.<br/><br/>Applause is needed for the next installment of the Aurora Teagarden series.  With so many sketchy characters, it compels you to solve the crime with Aurora.  Also, the vandal's clues will add a few more great mysteries to your collection.<br/><br/>5 out of 5 stars.",
+    "review":"Aurora's display for her Real Mystery club is destroyed by a vandal.  But a clue left behind reference Shakespeare.  Aurora concludes the vandal leaves clues for their next target.  When one clue references a librarian, Aida is worried for Aurora's safety.  Aurora is determined to save herself and find the vandal.<br/><br/>Applause is needed for the next installment of the Aurora Teagarden series.  With so many sketchy characters, it compels you to solve the crime with Aurora.  Also, the vandal's clues will add a few more great mysteries to your collection.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aurorateagardenagameofcatandmouse.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aurorateagardenagameofcatandmouse.jpg"
@@ -29,10 +29,10 @@ var madeForTV = [
 {
     "option":"4",
     "mtitle":"Love and Sunshine",
-    "review":"While Jake was fighting for his country, Ally has fostered his dog. Jake returns home safely and picks up his dog, Sunshine.  It's hard for Ally to let Sunshine go because the dog gave her companionship when her engagement ended.  Now, Jake needs her to get over his Dear John letter he received during his tour.  Their constant run-ins over Sunshine ignites a spark of love between the two.  Hopefully, Ally and Jake will see it.<br/><br/>This is a strong Hallmark love story.  It's amazing to see Ally gain her confidence with so much poise and strength.  Watch and be inspired.<br/><br/>4 out of 5 stars.",
+    "review":"While Jake was fighting for his country, Ally has fostered his dog. Jake returns home safely and picks up his dog, Sunshine.  It's hard for Ally to let Sunshine go because the dog gave her companionship when her engagement ended.  Now, Jake needs his dog to get over his Dear John letter he received during his tour.  Their constant run-ins over Sunshine ignites a spark of love between the two.  Hopefully, Ally and Jake will see it.<br/><br/>This is a strong Hallmark love story.  It's amazing to see Ally gain her confidence with so much poise and strength.  Watch and be inspired.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveandsunshine.jpg",
-        "largeImageurl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveandsunshine.jpg"
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveandsunshine.jpg"
     }
 },
 {
