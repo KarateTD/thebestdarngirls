19fa334f5c7a3ec784940ab61f374854a043e13a
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-08-15 00:37:58 -0400

    add peanut butter falcon

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"The Peanut Butter Falcon",
+    "review":"Zack, a 22-year-old with Down Syndrome, escapes the retirement home in the dead of night.  The next morning, he hides in a boat.  Soon the boat starts to move.  Tyler, a fisherman, has jumped in the boat and sailed off to avoid Duncan and Ratboy.  Tyler stole their crab haul and burned $12,000 o materials.  Tyler docks his boat in deep marshes and turns off the boat.  Then, he hears Zack throwing up.  Tyler silences Zack and escapes Duncan and Ratboy.  Tyler wants to leave Zack behind until he learns Zack is an outlaw like him.  Now, on his way to Florida, Tyler promises to help Zack achieve his dream: To go to Salt Water Redneck Wrestling school and become a professional wrestler.  It will be tough because Tyler's nurse, Eleanor will move heaven and earth to find him.  Ratboy and Duncan will go through fire and brimstone to find Tyler.<br/><br/>This is the type of movie that will have you smiling when you leave the theater.   Each character finds a flaw in how they looked at the world and grow together.  Eleanor learns to stop underestimating Zack.  Zack learns his differences doesn't make him a bad guy.  Tyler learns to stop hurting himself with bad behavior.  This movie is safe for tweens and teens.  It will teach then kindness, friendship, and respect.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thepeanutbutterfalcon.png",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thepeanutbutterfalcon.png"
+    }
+},
+{
+    "option":"2",
     "mtitle":"Brian Banks",
     "review":"Brian wants to continue his deferred dream of being in the NFL.  But the new law to fit paroled sex-offenders with an ankle monitor has cut that dream short again.  Seven years ago, with bad legal counsel, he pleaded no contest to rape charges.  He was told he would get probation.  Instead, he got 6 years in jail, 3 years on parole, and lifetime on the sex offenders list.  Now on parole, Brian wants the California Innocence Project to clear his name.  Only then can he live his dream.  A ping on his computer will change his life forever.<br/><br/>Be ready to get angry and cheer.  The hoops Brian has to go through to clear his name is astonishing.  Unbelievably, he never gives up.  But with a ticking clock on his back and Jerome's words propelling him forward, Brian is determined to make it.  This story will inspire you to join in the fight with the California Innocence Project.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Kitchen",
     "review":"Ruby, Kathy, and Claire watch their husbands get sentenced to 3 years in prison for several crimes.  Their mob family promises the women they will be taken care of while their husbands are in prison.  Their first payment doesn't cover rent so the women go to talk to Little Jackie.  Jackie refuses to give them more.  So the women take their lives and the family business in their own hands.  They start collecting, making deals, and killing.  But will they be ready for the biggest play of all?  Their husbands are coming home.<br/><br/>Based on the graphic novel, this movie feels more like a part one of a multi-part movie.  Each woman desires the mobster life for different reasons.  Kathy wants to take care of her family.  Ruby wants to have power.  Claire wants control of her life.  Seeing these women get bloody business done is entertaining but something feels like its missing.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Dora and the City of Gold",
     "review":"After falling into a hole, Dora's parents won't let her go on their next exploration.  They believe they have discovered the City of Gold, Parapata.  Since Dora can't be trusted with her safety, her parents send her to live in the city with her cousin Diego.  Dora worries she won't make friends because she doesn't know how to make friends.  Dora becomes the weird girl in school and needs her parents.  However, she hasn't been able to get in touch with them.  While on a field trip, Dora, Diego, and 2 classmates are kidnapped.  They want Dora to find her parents so her parents will lead them to Parapata.  With Alejandro's help, Dora escapes and plans to find her parents before the treasure hunters do.<br/><br/>Original fans relax.  This movie gives you the classic Dora theme, 4th wall breaks, and the cartoon.  Everyone else, enjoy.  This movie is a mix of Dora, Indiana Jones, and Spy Kids.  You will get a genuine laugh while kids learn about Incas, aqueducts, and frogs.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"The Art of Racing in the Rain",
     "review":"Enzo is purchased by Denny and starts the life he always thought he should have.  While he loves Denny's house, Enzo considers the race track to be his true home.  In his heart, Enzo, a dog, wants to be a racer.  Luckily, his owner has a gift for racing in the rain.  He dreams of becoming a great racer while teaching others.  Through love, a baby, death, lawsuits, and joy, Enzo is there for Denny every step of the way.  And Enzo has a plan to be with Denny forever.<br/><br/>Be ready to feel every emotion possible.  This movie is told mostly from Enzo's perspective.  He has trial and tribulations like a human.  When Enzo is sad, conniving, and hurt, you will cry, laugh, and feel his pain.  Don't be surprised if you hug your family, including your dog, after this movie.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theartofracingintherain.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theartofracingintherain.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"The Farewell",
-    "review":"Billi got devastating news.  Her grandmother has stage 4 lung cancer and has 3 months to live.  Billi wants to fly to China to see her grandmother but her parents don't want her to come.  The family is following tradition and decides not to tell her grandmother she's sick.  Her parents worry Billi won't be able to keep the secret.  Tradition states that people who get cancer die.  Not because of cancer but because of fear.  The family chooses to bear the burden of that fear so her grandmother won't have to do it.  Her parents fly to China.  A few days later - shocking her family - Billi flies there too.  Billi has to walk the line between Chinese tradition and American culture.  Will she reveal the good lie?<br/><br/>Based on an actual lie, this movie points out the difference between the cultures without condemning one or the other.  Both are explained to the audience with compassion and understanding.  The movie is 90% in Mandarin.  The story is so compelling, it's like reading a great book.  Sidenote: Bring food.  The eating scenes will make you hungry.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thefarewell.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thefarewell.jpg"
     }
 }
 ];
