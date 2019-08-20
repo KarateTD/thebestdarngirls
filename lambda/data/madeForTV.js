e11108a8242ab1c7004d2553f60639dca4f1c716
tiffany.gray <Tiffany.Gray@ablevets.com>
2019-08-20 00:52:31 -0400

    add 3 theater, 4 tv, 1 must buy

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"V.C. Andrews' Gates of Paradise",
+    "review":"Heaven, Logan, and Fannie's children are 18 and ready to tackle the world.  Annie dreams about going to Farthinggale manor but Heaven shuts her down without explanation.  After a night of drinking, Logan crashes the car killing heaven and himself.  But Annie is still alive.  When Annie wakes up, she can't feel her legs, her parents are dead, and her step great-grandfather, Tony, is standing over her.  Tony offers to take care of her while she recovers and she accepts. Annie has no idea the dark secrets the manor holds.<br/><br/>Knowing the truth of Luke and Annie's kinship makes the longing glances and near kisses palatable.  However, the overall plot feels like a lack-luster Misery.  It just doesn't command your attention like the predecessors.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsgatesofparadise.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsgatesofparadise.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Home Abduction",
+    "review":"Maggie and her daughter Ava move into a new home to start a new life.  Maggie worries the home may be haunted after doors and windows open and close themselves.  Then Sylvia, Maggie's neighbor, tells Maggie the homes bad past.  The home is connected to a child disappearance and a murder.  But Maggie has no idea how close and how deep the past goes.  Maggie has to learn who is turning her life upside down to keep her and Ava save.<br/><br/>This movie could have been based on a real story.  With a mix of new technology and non-technical methods, Sylvia can get under Maggie's skin and create real fear for here.  There are 2 big reveals in the movie.  The first is predictable and the other is not.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/homeabduction.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/homeabduction.jpg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"Aurora Teagarden Mysteries: An Inheritance to Die For",
+    "review":"After a fun mystery night, Aurora attends a wedding for Sally's cousin.  Gladys is stuck in between her son and her assistant.  They fight like cats and dogs over her health.  And that fighting continues after Gladys drops dead during the reception.  The will reading has several members of Gladys' family fuming when she leaves a large amount to her chef and her company to Aida.  But everything is put on hold when police discover Gladys was poisoned.<br/><br/>Score another one for Aurora Teagarden.  With such an unlikable victim, viewers are left with a vast number of suspects.  And to make your sleuthing harder, murder isn't the only crime being committed.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aurorateagardenmysteriesaninheritancetodiefor.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aurorateagardenmysteriesaninheritancetodiefor.jpg"
+    }
+},
+{
+    "option":"4",
+    "mtitle":"A Taste of Summer",
+    "review":"After quitting her job and relationship, Gabby, sous chef, heads to New England to visit her sister Christina.  Christina wants Gabby to branch off on her own and she has the perfect space.  Christina shows Gabby a restaurant with a great deal.  Gabby is on the fence until she meets the local competition, Caleb.  The two passive-aggressively fight to be the hit in their town and the Taste of Summer competition.<br/><br/>A Taste of Summer will make you hungry for food and love.  So, have at least one nearby.  The amount of personal arc growth is amazing and well crafted.  While the movie predictable, it's still fun to watch.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/atasteofsummer.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/atasteofsummer.jpg"
+    }
+},
+{
+    "option":"5",
     "mtitle":"V.C. Andrews' Fallen Hearts",
     "review":"Heaven is back home and living with her grandfather.  She is making a good life as a teacher.  But when tragedy strikes her family again, Heaven places the blame on herself.  And uses a wise man's last words to give forgiveness.  Heaven marries Logan and hopes for a brighter future.  It's not long that the ghost of her past start paving a dark road to her future.  What secrets will be revealed in this installment of the Casteel series?<br/><br/>Book lovers rejoice!  This movie fixes a few of the wrongs in the first movie.  Directed by Jason Priestly, this movie captures the actors but the scenery is well.  This is, no doubt, a testament to the great job the set and costume designers are doing.  It makes the story stronger as the Casteel family airs their dirty laundry for the whole town to see.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsfallenhearts.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsfallenhearts.jpg"
-    }
-},
-{
-    "option":"2",
-    "mtitle":"Deadly Influencer",
-    "review":" Monica helped Skyler grow her social media following.  With a management agency calling her, Skyler decides to leave Monica.  Monica pushes her off a balcony.  Now, Monica is focused on Jessica.  She believes Jessica will have a bigger following than Skyler.  Monica wants to manage Jessica's social career and create a management brand.  She just has to get Jessica to sign a 10-year contract.  And if anyone gets in her way, off with their heads.<br/><br/>Giving a new meaning to a moment of silence, this movie delves into deep lessons for teens while remaining entertaining.  It talks about confusing likes with being liked.  Also, it analyzes the downside of being Insta-famous.  Watch this movie with your teen.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlyinfluencer.jpeg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlyinfluencer.jpeg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Aurora Teagarden: A Game of Cat and Mouse",
-    "review":"Aurora's display for her Real Mystery club is destroyed by a vandal.  But a clue left behind references Shakespeare.  Aurora concludes that the vandal leaves clues for their next target.  When one clue references a librarian, Aida is worried for Aurora's safety.  Aurora is determined to save herself by finding the vandal.<br/><br/>Applause is needed for the next installment of the Aurora Teagarden series.  With so many sketchy characters, it compels you to solve the crime with Aurora.  Also, the vandal's clues will add a few more great mysteries to your collection.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aurorateagardenagameofcatandmouse.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aurorateagardenagameofcatandmouse.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Love and Sunshine",
-    "review":"While Jake was fighting for his country, Ally has fostered his dog. Jake returns home safely and picks up his dog, Sunshine.  It's hard for Ally to let Sunshine go because the dog gave her companionship when her engagement ended.  Now, Jake needs his dog to get over his Dear John letter he received during his tour.  Their constant run-ins over Sunshine ignites a spark of love between the two.  Hopefully, Ally and Jake will see it.<br/><br/>This is a strong Hallmark love story.  It's amazing to see Ally gain her confidence with so much poise and strength.  Watch and be inspired.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveandsunshine.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveandsunshine.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"V.C. Andrews' Dark Angel",
-    "review":"Heaven left her small-town life behind for her mother's family.  She is taken back by their wealth and feels instantly out of place.  Her grandmother, Jillian only wanted her around for a few days, her step-grandfather, Tony, offers to make the stay permanent.  She just has to give up her father's side of the family and give in to his needs.  Heaven agrees and becomes a lady of wealth.  But when she gets engaged, lies told by everyone are exposed and leave a path of pain, destruction, and death.<br/><br/>Book lovers beware, this movie hasn't fixed the mistakes of the first movie.  Movie lovers, get ready for some of the biggest shocks in Lifetime's history.  One remarkable aspect is the costume and set design work.  As Heaven transforms, her clothes do too but they stay within the margins of the period.  This attention to detail is paid to the sets as well.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewdarkangel.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewdarkangel.jpg"
     }
 }
 ];
