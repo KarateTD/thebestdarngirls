cde892ada839871f1c53991ccfdf7c5b9264d791
Tiffany Gray <KarateTD@yahoo.com>
2019-09-01 02:16:24 -0400

    adding 4 tv and 1 movie

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"Who Stole My Daughter",
+    "review":"On the 5th anniversary of their daughter's disappearance, Tim and Janet are floored when she appears at his subject.  Katie doesn't speak but is hiding a dark secret.  Tim and Janet must uncover Katie's secret or they all will be dead.<br/><br/>This movie does have a slow start but it has a dependable and strong ending.  You must pay attention while watching this movie.  Every line, gesture, and moment have meaning to the plot.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/whostolemydaughter.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/whostolemydaughter.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Chronicle Mysteries: The Deep End",
+    "review":"So here's what we know, Stephanie is on trial for her husband's murder and all evidence points to her.  But her lawyer, who is Alex's best friend, Katie, believe she didn't do it.  Before Elliot die, his children overheard Elliot talking to Claire.  Katie asks Alex to use her podcast to find Claire.  Alex agrees.  As more evidence comes to light, Alex and her friends start investigating the case for the real killer.<br/><br/>With well established relationships, this episode in the movie series focuses on the case.  As Alex investigate, she will reveal an underground poker game, planes, and secret storage lockers.  There is a personal aspect missing, this movie still captures your attention.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chroniclemysteriesthedeepend.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chroniclemysteriesthedeepend.jpg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"Hitsville: The Making of Motown",
+    "review":"Berry Gordy used his experience as a newspaper salesman, record store owner, and Ford Motors factoring worker to create an artist assembly line.  He learned self-reliance from his family who created a family bank.  He asked his sister to borrow $1000 from the family bank to start a record label.  She approved $800.  Berry bought a 2 story home, hired the best of the best, and created a sound that would become ingrained in American culture to this day.<br/><br/>As best friends Berry and Smokey walk around Hitsville, now the Motown Museum they discuss the rise, competition, change, and legacy of Motown.  As they talk they create and settle a $100 bet.  Dare you not to sing while watching this movie.<br/><br/>5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hitsvillethemakingofmotown.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hitsvillethemakingofmotown.jpg"
+    }
+},
+{
+    "option":"4",
+    "mtitle":"All Summer Long",
+    "review":"After working 2 years straight on a legal case, Tia wants to take some time off.  She decides to take over as Captain for her uncle's boat, which doubles as a restaurant.  It's not long before learns her uncle Roland hired her ex-boyfriend, Jake to be the head chef.  Can Tia put her broken heart and anger to the side to help Roland.<br/><br/>This movie follows the classic plot of former lovers must work together and fall in love. It does offer a unique perspective on business but not romance.<br/><br/>2 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/allsummerlong.png",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/allsummerlong.png"
+    }
+},
+{
+    "option":"5",
     "mtitle":"V.C. Andrews' Web of Dreams",
     "review":"Annie returns to Farthinggale Manor to bury her father Troy.  Although she recently discovered her real father, she feels obligated to return to the home where she was kept as a prisoner.  When she walks around the home, she gets flashes of her abusive past at the hands of her grandfather Tony.  Before she leaves, the butler hands her a diary.  The diary belonged to her grandmother Leigh.  Annie sits and reads the painful past endured by Leigh and the courage her grandmother had at such a young age.<br/><br/>Web of Dreams is a prequel for the Castel series.  This prequel may feel unnecessary or out of order to the viewer, it is told at the right time for Annie.  It gives Annie an understanding that she isn't alone in her abuse and she can overcome her trauma.  Also, this movie answers a lot of questions about what happened to Leigh and her volatile relationships.  It's best to watch the prequel last as the author intended.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewswebofdreams.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewswebofdreams.jpg"
-    }
-},
-{
-    "option":"2",
-    "mtitle":"A Deadly Dance",
-    "review":"Kate believes the death of her friend Theresa is somehow linked to the death of her identical twin sister.  With this belief, she starts to work with police to help the investigation.  In hours, she is stalked, harassed, and assaulted.  With her life on the line, she has no idea who wants to hurt her and why.  Unfortunately, everyone around her personal and professional life has a motive.<br/><br/>A Deadly Dance has taken the Hallmark plot and added a twist of sex and sin.  The mystery provides enough characters with suspicious behavior to question everyone as a potential suspect.  From Kate's husband to her security officer to her boss, anyone could have killed Theresa.  While the mystery is strong, the acting does not live up to the script.   And the crime of Kate's identical twin is never solved.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/adeadlydance.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/adeadlydance.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Aurora Teagarden Mysteries: A Very Foul Play",
-    "review":"Aurora, Philip, Nick, and Aida head to a True Crime Mystery Conference.  At the hotel, they run into Lynn, Arthur, and Lillian.  Once Lynn discovers Aurora's friends and family are at the same hotel, she asks them to join her True Crime play.  Every year they have conference attendees act out a mystery and allow the audience to solve it.  During the performance, the lights go out too soon.  When the lights are turned on, Philip is holding a knife and the star of the show is dead.  Aurora must find the real killer before her cousin Philip is sent to prison for murder.<br/><br/>This whodunit has espionage, love, entitlement, and narcissism at its core.  These aspects will keep fans guessing.  Now, seasoned fans will be able to figure out the who but not the why.  So, keep watching until the end.  You think you know but you don't.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aurorateagardenmysteriesaveryfoulplay.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aurorateagardenmysteriesaveryfoulplay.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"A Summer Romance",
-    "review":"Richard needs to secure the purchase of Walker Ranch to finalize the deal for a merger.  Without Walker Ranch, the merger will fall through.  After landing in Big Horn, Montana, the airport is shut down and Richard is stuck without hotel reservations.  All the hotels are booked because of a big festival in a few days.  So a beautiful woman offers to let him stay on her property.  Richard is thankful, then he discovers the woman is Sam Walker, the owner of Walker Ranch.  She plants her feet and tells him under no circumstances will she sell her family's ranch.  She will make him see why when she teaches him The Cowboy Code's 4 Pillars.  Can she change his heart while he tries to change her mind?<br/><br/>Following the Hallmark format, this movie is still funny and charming at is core.  Also, Richard isn't projected as a bad guy for wanting to buy Walker Ranch.  Viewers are shown a charming and benevolent boss who knows all of his employees by name.  This aspect of Richard's character makes the love story more believable.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/asummerromance.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/asummerromance.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"V.C. Andrews' Gates of Paradise",
-    "review":"Heaven, Logan, and Fannie's children are 18 and ready to tackle the world.  Annie dreams about going to Farthinggale Manor but Heaven shuts her down without explanation.  After a night of drinking, Logan crashes the car killing Heaven and himself.  But Annie is still alive.  When Annie wakes up, she can't feel her legs, her parents are dead, and her step great-grandfather, Tony, is standing over her.  Tony offers to take care of her while she recovers and she accepts. Annie has no idea the dark secrets the manor holds.<br/><br/>Knowing the truth of Luke and Annie's kinship makes the longing glances and near kisses palatable.  However, the overall plot feels like a lack-luster Misery.  It just doesn't command your attention like the predecessors.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsgatesofparadise.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsgatesofparadise.jpg"
     }
 }
 ];
