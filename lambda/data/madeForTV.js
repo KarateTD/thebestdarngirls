113f6dd982486834a175554c7369b526d87d6668
Tiffany Gray <KarateTD@yahoo.com>
2019-09-01 02:29:27 -0400

    fixed all

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -2,7 +2,7 @@ var madeForTV = [
 {
     "option":"1",
     "mtitle":"Who Stole My Daughter",
-    "review":"On the 5th anniversary of their daughter's disappearance, Tim and Janet are floored when she appears at his subject.  Katie doesn't speak but is hiding a dark secret.  Tim and Janet must uncover Katie's secret or they all will be dead.<br/><br/>This movie does have a slow start but it has a dependable and strong ending.  You must pay attention while watching this movie.  Every line, gesture, and moment have meaning to the plot.<br/><br/>4 out of 5 stars.",
+    "review":"On the 5th anniversary of their daughter's disappearance, Tim and Janet are floored when she appears at their doorstep.  Katie doesn't speak but is hiding a dark secret.  Tim and Janet must uncover Katie's secret because their family is in danger.<br/><br/>This movie does have a slow start but it has a dependable and strong ending.  You must pay attention while watching this movie.  Every line, gesture, and moment have meaning to the plot.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/whostolemydaughter.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/whostolemydaughter.jpg"
@@ -11,7 +11,7 @@ var madeForTV = [
 {
     "option":"2",
     "mtitle":"Chronicle Mysteries: The Deep End",
-    "review":"So here's what we know, Stephanie is on trial for her husband's murder and all evidence points to her.  But her lawyer, who is Alex's best friend, Katie, believe she didn't do it.  Before Elliot die, his children overheard Elliot talking to Claire.  Katie asks Alex to use her podcast to find Claire.  Alex agrees.  As more evidence comes to light, Alex and her friends start investigating the case for the real killer.<br/><br/>With well established relationships, this episode in the movie series focuses on the case.  As Alex investigate, she will reveal an underground poker game, planes, and secret storage lockers.  There is a personal aspect missing, this movie still captures your attention.<br/><br/>4.5 out of 5 stars.",
+    "review":"So here's what we know, Stephanie is on trial for her husband's murder and all evidence points to her.  But her lawyer, who is Alex's best friend, Katie, believe she didn't do it.  Before Elliot died, his children overheard Elliot talking to Claire.  Katie asks Alex to use her podcast to find Claire.  Alex agrees.  As more evidence comes to light, Alex and her friends start investigating the case for the real killer.<br/><br/>With well established relationships, this episode in the movie series focuses on the case.  As Alex investigate, she will reveal an underground poker game, planes, and secret storage lockers.  There is a personal aspect missing but this movie still captures your attention.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chroniclemysteriesthedeepend.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chroniclemysteriesthedeepend.jpg"
@@ -20,7 +20,7 @@ var madeForTV = [
 {
     "option":"3",
     "mtitle":"Hitsville: The Making of Motown",
-    "review":"Berry Gordy used his experience as a newspaper salesman, record store owner, and Ford Motors factoring worker to create an artist assembly line.  He learned self-reliance from his family who created a family bank.  He asked his sister to borrow $1000 from the family bank to start a record label.  She approved $800.  Berry bought a 2 story home, hired the best of the best, and created a sound that would become ingrained in American culture to this day.<br/><br/>As best friends Berry and Smokey walk around Hitsville, now the Motown Museum they discuss the rise, competition, change, and legacy of Motown.  As they talk they create and settle a $100 bet.  Dare you not to sing while watching this movie.<br/><br/>5 out of 5 stars.",
+    "review":"Berry Gordy used his experience as a newspaper salesman, record store owner, and Ford Motors factory worker to create an artist assembly line.  He learned self-reliance from his family who created a family bank.  He asked his sister to borrow $1000 from the family bank to start a record label.  She approved $800.  Berry bought a 2 story home, hired the best of the best, and created a sound that would become ingrained in American culture to this day.<br/><br/>As best friends Berry and Smokey Robinson walk around Hitsville, now the Motown Museum they discuss the rise, competition, change, and legacy of Motown.  As they talk they create and settle a $100 bet.  Dare you not to sing while watching this movie.<br/><br/>5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hitsvillethemakingofmotown.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hitsvillethemakingofmotown.jpg"
@@ -29,7 +29,7 @@ var madeForTV = [
 {
     "option":"4",
     "mtitle":"All Summer Long",
-    "review":"After working 2 years straight on a legal case, Tia wants to take some time off.  She decides to take over as Captain for her uncle's boat, which doubles as a restaurant.  It's not long before learns her uncle Roland hired her ex-boyfriend, Jake to be the head chef.  Can Tia put her broken heart and anger to the side to help Roland.<br/><br/>This movie follows the classic plot of former lovers must work together and fall in love. It does offer a unique perspective on business but not romance.<br/><br/>2 out of 5 stars.",
+    "review":"After working 2 years straight on a legal case, Tia wants to take some time off.  She decides to take over as Captain for her uncle's boat, which doubles as a restaurant.  It's not long before she learns her uncle Roland hired her ex-boyfriend, Jake to be the head chef.  Can Tia put her broken heart and anger to the side to help Roland.<br/><br/>This movie follows the classic plot of former lovers forced to work together and fall in love. It does offer a unique perspective on business but not romance.<br/><br/>2 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/allsummerlong.png",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/allsummerlong.png"
