9e201776a9dbfd2820cc2ac9029d42253bc5d80b
Tiffany Gray <KarateTD@yahoo.com>
2019-09-10 23:48:03 -0400

    update data to look like master

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"After the Wedding",
+    "review":"Isabel must meet with her orphanage's benefactor in New York before she will distribute funds.  Isabel meets with Theresa but Theresa is overwhelmed with her daughter Grace getting married, selling her company, and creating the orphanage fund.  She asks Isabel to give her some time to look over the numbers and invites Isabel to Grace's wedding.  At the wedding, Isabel lays eyes on Theresa's husband, Oscar, and becomes emotional.  When Grace gives her speech, Isabel quietly radiates with fury.  After the wedding, one question will unveil an avalanche of secrets.<br/><br/>Your mind will be racing with potential secrets for the first 30 minutes.  But rest assured, the mystery isn't over.  There is a puppet master behind the reveal with a bigger plan in motion.  The natural lighting choice can be distracting for the first few scenes but you will be brought back into focus with strong and sincere performances.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/afterthewedding.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/afterthewedding.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Overcomer",
+    "review":"Orphaned, sticky-fingered Hannah is the only runner who showed up for the cross country team.  With a reluctant coach, Hannah will unearth the truth about her past, find a path to faith, and discover the fire of an athlete inside of her with a redeemed coach in her ear.  All she needs is in Ephesians, chapter 1 and 2.<br/><br/>This is a Christian film but it also a coming-of-age, sports drama that serves lots of tea.  When you leave you will ask yourself one question, \"Who am I\"?<br/><br/>4.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/overcomer.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/overcomer.jpg"
+    }
+},
+{
+    "option":"3",
     "mtitle":"Don't Let Go",
     "review":"Jack often picks up the slack for his brother Garrett, especially for Garrett's daughter Ashley.  When Garrett forgets to pick Ashley up again, Jack is there to help.  The two bond at a diner.  Later, Jack gives Garrett an earful.  The next time Ashley calls, Jack is too busy with a case to give Ashley his full attention.  During her final call, Jack hears screaming and the call is disconnected.  When Jack gets to Garrett's home, Garrett, his wife, their dog, and Ashley are all dead.  At the funeral, he prays to fix it.  Weeks later he gets a call and Ashley's picture appears on the screen.  Jack thinks he is going crazy until he can't find Ashley's phone. He finds it at the crime scene.  While he is holding Ashley's phone, Ashley calls Jack again.  Jack realizes Ashley is calling from 2 weeks in the past.  He may have not been able to save Ashley in his time, but he can save Ashley in her own.<br/><br/>This time-bending movie is a cross between the Butterfly Effect and Cradle 2 the Grave.  It wraps a thriller with a mystery.  Be warned, some scenes will make you dizzy.  While the loving bond and time disruption is fun to watch, the overall mystery is predictable.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"4",
     "mtitle":"Angel Has Fallen",
     "review":"Mike is facing a tough choice.  His heart is in the secret service action.  But his body can't take the pounding.  President Trumbull and Mike's wife Leah encourages him to take the jobs as Director of the Secret Service.  Before Mike can answer, the president is hit with a drone strike.  When Mike wakes up, his entire team is dead, the president is in a coma, and he is the number one suspect.  Mike has to find out who attacked the president or end up in jail.  Good thing he has dear old dad to help.<br/><br/>While this movie is better than the second, it still does not live up to the first.  However, it is more grounded in reality with discussion on pill dependencies, post-traumatic stress syndrome, and father-son relationships.  If you are a fan of the Fallen series, see the matinee.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"5",
     "mtitle":"Luce",
     "review":"Adopted from war-torn Eritrea 10 years ago, Luce is an exemplary student, star athlete, and famed speaker within his school.  His mother Amy is confused when she gets a call from Ms. Wilson to discuss Luce.  Ms. Wilson gave the class an assignment to write a paper in the voice of a famous person.  Luce chose a war criminal who uses violence to settle political disputes.  Frightened by the paper, Ms. Wilson searches Luce's locker and finds illegal fireworks.  Now everyone is questioning Luce's past versus his present.  But, are the questions justified?<br/><br/>This is the type of movie that leaves with you and keeps you up at night.  Luce is a complex character with many sides.  While some would see his actions as warranted, others would see it as psychopathic.  While you never know the whole truth, you are left wondering: who is the real Luce.<br/><br/>3.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/luce.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/luce.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Ready Or Not",
-    "review":"Former foster child, Grace is happy to start a permanent family with her husband Alex.  On their wedding day, several of his family members make it known that they don't like her.  But his mother and brother Daniel are the exceptions.  On their wedding night, the couple is asked to join the family.  Whenever someone new enters the family, they play a game.  Some got checkers or Old Maid.  Grace picks the Hide and Seek card.  She has to hide in the house and not be found until sunrise.   Unbeknownst to Grace, while she is finding a hiding spot, Alex's family are gathering weapons.  Years ago, their ancestor made a deal.  If a person picks the Hide and Seek card, they must be killed by sunrise or the family will die.  Alex has to find and save Grace before his family kills her.<br/><br/>Don't be fooled by the premises.  Grace is not a damsel in distress.  Once she learns the deadly nature of the game, she changes her shoes and rips her wedding dress.  She refuses to go quietly and gives his family a bloody and funny run for their money.  Be advised, this isn't like The Purge.  It is far funnier and a true portrayal of a dysfunctional family.  See it and have a good laugh.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/readyornot.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/readyornot.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Good Boys",
-    "review":"Max is invited to a kissing party with the popular kids, including his crush.  Max requests for the invitation to be extended to his friends Thor and Lucas.   The cool kids decide to let them all come.  Now, Max has one big problem: he has to learn how to kiss.  After several failed attempts to learn kissing, they use Max's dad's drone to watch Thor's neighbor kiss her boyfriend.  She spots the drone and takes it out.  Max has to get the drone back or he will be grounded and can't attend the party.<br/><br/>As a warning, if you can't handle seeing kids curse, drink, and play with adult toys, this isn't the movie for you.  This coming-of-age story is just as raunchy as Superbad.  And with everything being so over-the-top, the resolution was the most honest and heartfelt of anything in this movie.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/goodboys.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/goodboys.jpg"
     }
 }
 ];
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"Identity Theft of a Cheerleader",
+    "review":"Vicky is tired of her mother's disappointment.  She dropped out of high school, got a loser boyfriend, and a dead-end job.  Now, at the age of 30, Vicky wants a change.  She steals her co-worker's identity and enrolls in high school.  Vicky, now Caitlyn, will become captain of the cheer squad, date an athlete, become popular, and graduate.  What could go wrong?<br/><br/>This movie is what happens when someone takes the crazy train to self-help.  Blood-thirsty Lifetime fans will get a body count but a predictable story.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/identitytheftofacheerleader.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/identitytheftofacheerleader.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"The Secret Lives of Cheerleaders",
+    "review":"Ava and her mother move to a new town to give Ava a fresh start after a bumpy year.  Since the school doesn't have a dance team, her mother tells Ava to try out for the cheerleading team.  At first, Ava doesn't want to try out but does when she can't make friends.  Ava makes the team but when she is in the path of the queen bee, Katrina, Ava is in for a world of hurt.<br/><br/>This is a movie to watch with your teen.  So much of this story could have been avoided if Ava had talked to her mother.  And Katrina's reign of hazing and bullying could have been stopped if adults did their job.  It doesn't matter if kids stand up to bullies when adults consistently let them down.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesecretlivesofcheerleaders.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesecretlivesofcheerleaders.jpg"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"My One and Only",
+    "review":"Stephanie reluctantly got chosen for a reality T.V. show called The One.  Matchmakers find you the perfect person, you go on 2 weeks of dates, and then decide of that person is the one.  Self-described realist, Stephanie is matched with Oliver.  But she can't keep her eyes off of Alex.  Alex is the ranch owner's son and he left Stephanie with a not-so-good first impression.  So, who will be the one?<br/><br/>The moment Stephanie lays eyes on Alex, you know where the story is going.  It's the character development in the subplot that will keep you watching.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myoneandonly.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myoneandonly.jpg"
+    }
+},
+{
+    "option":"4",
     "mtitle":"Who Stole My Daughter",
     "review":"On the 5th anniversary of their daughter's disappearance, Tim and Janet are floored when she appears at their doorstep.  Katie doesn't speak but is hiding a dark secret.  Tim and Janet must uncover Katie's secret because their family is in danger.<br/><br/>This movie does have a slow start but it has a dependable and strong ending.  You must pay attention while watching this movie.  Every line, gesture, and moment have meaning to the plot.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
 },
 {
-    "option":"2",
+    "option":"5",
     "mtitle":"Chronicle Mysteries: The Deep End",
     "review":"So here's what we know, Stephanie is on trial for her husband's murder and all evidence points to her.  But her lawyer, who is Alex's best friend, Katie, believe she didn't do it.  Before Elliot died, his children overheard Elliot talking to Claire.  Katie asks Alex to use her podcast to find Claire.  Alex agrees.  As more evidence comes to light, Alex and her friends start investigating the case for the real killer.<br/><br/>With well established relationships, this episode in the movie series focuses on the case.  As Alex investigates, she will reveal an underground poker game, planes, and secret storage lockers.  There is a personal aspect missing but this movie still captures your attention.<br/><br/>4.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chroniclemysteriesthedeepend.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chroniclemysteriesthedeepend.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"Hitsville: The Making of Motown",
-    "review":"Berry Gordy used his experience as a newspaper salesman, record store owner, and Ford Motors factory worker to create an artist assembly line.  He learned self-reliance from his family who created a family bank.  He asked his sister to borrow $1000 from the family bank to start a record label.  She approved $800.  Berry bought a 2 story home, hired the best of the best, and created a sound that would become ingrained in American culture to this day.<br/><br/>As best friends Berry and Smokey Robinson walk around Hitsville, now the Motown Museum, they discuss the rise, competition, change, and legacy of Motown.  As they talk, they create and settle a 100 dollar bet.  Dare you not to sing while watching this movie.<br/><br/>5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hitsvillethemakingofmotown.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hitsvillethemakingofmotown.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"All Summer Long",
-    "review":"After working 2 years straight on a legal case, Tia wants to take some time off.  She decides to take over as Captain for her uncle's boat, which doubles as a restaurant.  It's not long before she learns her uncle Roland hired her ex-boyfriend, Jake, to be the head chef.  Can Tia put her broken heart and anger to the side to help Roland?<br/><br/>This movie follows the classic plot of former lovers forced to work together and fall in love. It does offer a unique perspective on business but not romance.<br/><br/>2 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/allsummerlong.png",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/allsummerlong.png"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"V.C. Andrews' Web of Dreams",
-    "review":"Annie returns to Farthinggale Manor to bury her father Troy.  Although she recently discovered her real father, she feels obligated to return to the home where she was kept as a prisoner.  When she walks around the home, she gets flashes of her abusive past at the hands of her grandfather Tony.  Before she leaves, the butler hands her a diary.  The diary belonged to her grandmother Leigh.  Annie sits and reads the painful past endured by Leigh and the courage her grandmother had at such a young age.<br/><br/>Web of Dreams is a prequel for the Castel series.  This prequel may feel unnecessary or out of order to the viewer, it is told at the right time for Annie.  It gives Annie an understanding that she isn't alone in her abuse and she can overcome her trauma.  Also, this movie answers a lot of questions about what happened to Leigh and her volatile relationships.  It's best to watch the prequel last as the author intended.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewswebofdreams.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewswebofdreams.jpg"
     }
 }
 ];
