a8ed1411fcb1383a9bcf266eeeea432d62bf9e4f
Tiffany Gray <KarateTD@yahoo.com>
2019-09-12 23:25:37 -0400

    add hustlers

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
 {
     "option":"1",
+    "mtitle":"Hustlers",
+    "review":"In 2007, Destiny is tired of making next to nothing stripping all night.  She asks Ramona, a headliner, to coach her. Ramona teaches Destiny how to pole dance, private dance, and read the clients.  Everything is going great until the market crashed and Destiny gets pregnant. Two and a half years later, Destiny's unique skills don't translate to a 9 to 5, so she heads back to the club.  But times have changed.  Destiny doesn't know what to do until Ramona walks into the club.  Ramona doesn't pole dance anymore.  She fishes for the club.  She and Mercedes find a guy at the bar, get him drunk, take him to the club, charge his card, and get him to sign the check.  The women get a percentage of whatever the club makes.  Destiny is in.  But when Ramona decides to cut the club out of the deal, things go off the rails.<br/><br/>So, is Ramona a sociopath, psychopath, or neither?  Based on a true story, this movie is an episode of American Greed told in detail.  Like all cons, it goes great until the players get too greedy and too sloppy.<br/><br/>3.5 out ouf 5 stars.",
+    "image":{
+        "smallImageUrl":"",
+        "largeImageUrl":""
+    }
+},
+{
+    "option":"2",
     "mtitle":"After the Wedding",
     "review":"Isabel must meet with her orphanage's benefactor in New York before she will distribute funds.  Isabel meets with Theresa but Theresa is overwhelmed with her daughter Grace getting married, selling her company, and creating the orphanage fund.  She asks Isabel to give her some time to look over the numbers and invites Isabel to Grace's wedding.  At the wedding, Isabel lays eyes on Theresa's husband, Oscar, and becomes emotional.  When Grace gives her speech, Isabel quietly radiates with fury.  After the wedding, one question will unveil an avalanche of secrets.<br/><br/>Your mind will be racing with potential secrets for the first 30 minutes.  But rest assured, the mystery isn't over.  There is a puppet master behind the reveal with a bigger plan in motion.  The natural lighting choice can be distracting for the first few scenes but you will be brought back into focus with strong and sincere performances.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"2",
+    "option":"3",
     "mtitle":"Overcomer",
     "review":"Orphaned, sticky-fingered Hannah is the only runner who showed up for the cross country team.  With a reluctant coach, Hannah will unearth the truth about her past, find a path to faith, and discover the fire of an athlete inside of her with a redeemed coach in her ear.  All she needs is in Ephesians, chapter 1 and 2.<br/><br/>This is a Christian film but it also a coming-of-age, sports drama that serves lots of tea.  When you leave you will ask yourself one question, \"Who am I\"?<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"3",
+    "option":"4",
     "mtitle":"Don't Let Go",
     "review":"Jack often picks up the slack for his brother Garrett, especially for Garrett's daughter Ashley.  When Garrett forgets to pick Ashley up again, Jack is there to help.  The two bond at a diner.  Later, Jack gives Garrett an earful.  The next time Ashley calls, Jack is too busy with a case to give Ashley his full attention.  During her final call, Jack hears screaming and the call is disconnected.  When Jack gets to Garrett's home, Garrett, his wife, their dog, and Ashley are all dead.  At the funeral, he prays to fix it.  Weeks later he gets a call and Ashley's picture appears on the screen.  Jack thinks he is going crazy until he can't find Ashley's phone. He finds it at the crime scene.  While he is holding Ashley's phone, Ashley calls Jack again.  Jack realizes Ashley is calling from 2 weeks in the past.  He may have not been able to save Ashley in his time, but he can save Ashley in her own.<br/><br/>This time-bending movie is a cross between the Butterfly Effect and Cradle 2 the Grave.  It wraps a thriller with a mystery.  Be warned, some scenes will make you dizzy.  While the loving bond and time disruption is fun to watch, the overall mystery is predictable.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
 },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Angel Has Fallen",
     "review":"Mike is facing a tough choice.  His heart is in the secret service action.  But his body can't take the pounding.  President Trumbull and Mike's wife Leah encourages him to take the jobs as Director of the Secret Service.  Before Mike can answer, the president is hit with a drone strike.  When Mike wakes up, his entire team is dead, the president is in a coma, and he is the number one suspect.  Mike has to find out who attacked the president or end up in jail.  Good thing he has dear old dad to help.<br/><br/>While this movie is better than the second, it still does not live up to the first.  However, it is more grounded in reality with discussion on pill dependencies, post-traumatic stress syndrome, and father-son relationships.  If you are a fan of the Fallen series, see the matinee.<br/><br/>3 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/angelhasfallen.png",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/angelhasfallen.png"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Luce",
-    "review":"Adopted from war-torn Eritrea 10 years ago, Luce is an exemplary student, star athlete, and famed speaker within his school.  His mother Amy is confused when she gets a call from Ms. Wilson to discuss Luce.  Ms. Wilson gave the class an assignment to write a paper in the voice of a famous person.  Luce chose a war criminal who uses violence to settle political disputes.  Frightened by the paper, Ms. Wilson searches Luce's locker and finds illegal fireworks.  Now everyone is questioning Luce's past versus his present.  But, are the questions justified?<br/><br/>This is the type of movie that leaves with you and keeps you up at night.  Luce is a complex character with many sides.  While some would see his actions as warranted, others would see it as psychopathic.  While you never know the whole truth, you are left wondering: who is the real Luce.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/luce.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/luce.jpg"
     }
 }
 ];
