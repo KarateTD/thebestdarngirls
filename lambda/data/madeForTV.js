7a3e1fe950de35e14bc567e9e3985b09ede2477f
Tiffany Gray <KarateTD@yahoo.com>
2019-09-14 23:34:37 -0400

    add 4 movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
 {
     "option":"1",
+    "mtitle":"The Cheerleader Escort",
+    "review":"Ghosted by her father, Cassie has to figure out a way to pay for her 2nd semester in college.  To avoid her mother taking a 2nd job or a 2nd mortgage, Cassie decides to take her teammate's offer.  Gabby tells her that wealthy alumni men make donations to the cheerleading team.  But they will make individual donations for a girl the like.  Cassie believes Terry wants a relationship.  She has no idea she is part of an escort ring and her pimp is closer than she realizes.  When danger becomes deadly, can she save herself?<br/><br/>Small-town girl unknowingly falling into an escort ring is overdone and a bore.  This isn't the best of Lifetime Cheerleader series but its too soon to determine if its the worst.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecheerleaderescort.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecheerleaderescort.jpg"
+    }
+},
+{
+    "option":"2",
+    "mtitle":"Dating a Sociopath",
+    "review":"After surviving a car accident, Jane's father can't face her and her mother is dating someone else.  Under the guise of physical therapy, Jane's mother Samantha introduces her to her new boyfriend Brian.  Jane refuses to like him.  Jane tries to help her parents reconcile but it ends with her father, Paul, arrested and served with a restraining order.  When Brian tries to kill her via overdose, Jane is determined to expose him.  She doesn't know he is a sociopath who has killed before.<br/><br/>The crazy plotline is only saved by the understandable sleuthing techniques used by Jane and her friend.  While it is well-acted, the script is average to any Lifetime plot.<br/><br/>3 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/datingasociopath.png",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/datingasociopath.png"
+    }
+},
+{
+    "option":"3",
+    "mtitle":"Witness to Murder: A Darrow Mystery",
+    "review":"Casey was the nail in Joanna's coffin for the end of her New York legal career.  So, Joanna and Claire are shocked when she comes to their office asking for help.  Casey was about to blow the whistle on her company's bad practices but they arrested her for insider trading.  If she is arrested, Casey will lose her child custody case.  When Claire and Joanna got to Casey's boss's home to question him about Casey, they witness the murder of his paralegal assistant.  They decided to help Casey.  Not just for her insider trading case but for capital murder too.<br/><br/>You better have our DVR ready.  This is a mystery that needs to be watched 3 times to understand every twist and turn.  And Darrow fans rejoice.  The moment we have all been waiting for finally happens.<br/><br/>5 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/witnesstomurderadarrowmystery.png",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/witnesstomurderadarrowmystery.png"
+    }
+},
+{
+    "option":"4",
+    "mtitle":"The Wrong Cheerleader",
+    "review":"Jen convinces her best friend Becky to try out for the cheerleading team.  Becky makes it and attracts the attention of Rob.  Not knowing that Rob and Becky met recently, Jen sets them up on a blind date.  They become inseparable.  But when Jen and her boyfriend Chris sees Rob's dark side, can they save Becky?<br/><br/>Lifetime has done plenty of teen abuse movies but this is the first time they have delved into why someone has control issues.  Rob's history of abandonment, abuse, and alcoholism isn't an excuse but it does give an understanding of why he needs control.  Also, Becky overlooks Rob's issues until he hits her.  Unlike most Lifetime movie, one hit is enough.<br/><br/>4 out of 5 stars.",
+    "image":{
+        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewrongcheerleader.jpg",
+        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewrongcheerleader.jpg"
+    }
+},
+{
+    "option":"5",
     "mtitle":"Identity Theft of a Cheerleader",
     "review":"Vicky is tired of her mother's disappointment.  She dropped out of high school, got a loser boyfriend, and a dead-end job.  Now, at the age of 30, Vicky wants a change.  She steals her co-worker's identity and enrolls in high school.  Vicky, now Caitlyn, will become captain of the cheer squad, date an athlete, become popular, and graduate.  What could go wrong?<br/><br/>This movie is what happens when someone takes the crazy train to self-help.  Blood-thirsty Lifetime fans will get a body count but a predictable story.<br/><br/>3.5 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/identitytheftofacheerleader.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/identitytheftofacheerleader.jpg"
-    }
-},
-{
-    "option":"2",
-    "mtitle":"The Secret Lives of Cheerleaders",
-    "review":"Ava and her mother move to a new town to give Ava a fresh start after a bumpy year.  Since the school doesn't have a dance team, her mother tells Ava to try out for the cheerleading team.  At first, Ava doesn't want to try out but does when she can't make friends.  Ava makes the team but when she is in the path of the queen bee, Katrina, Ava is in for a world of hurt.<br/><br/>This is a movie to watch with your teen.  So much of this story could have been avoided if Ava had talked to her mother.  And Katrina's reign of hazing and bullying could have been stopped if adults did their job.  It doesn't matter if kids stand up to bullies when adults consistently let them down.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesecretlivesofcheerleaders.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesecretlivesofcheerleaders.jpg"
-    }
-},
-{
-    "option":"3",
-    "mtitle":"My One and Only",
-    "review":"Stephanie reluctantly got chosen for a reality T.V. show called The One.  Matchmakers find you the perfect person, you go on 2 weeks of dates, and then decide of that person is the one.  Self-described realist, Stephanie is matched with Oliver.  But she can't keep her eyes off of Alex.  Alex is the ranch owner's son and he left Stephanie with a not-so-good first impression.  So, who will be the one?<br/><br/>The moment Stephanie lays eyes on Alex, you know where the story is going.  It's the character development in the subplot that will keep you watching.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myoneandonly.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myoneandonly.jpg"
-    }
-},
-{
-    "option":"4",
-    "mtitle":"Who Stole My Daughter",
-    "review":"On the 5th anniversary of their daughter's disappearance, Tim and Janet are floored when she appears at their doorstep.  Katie doesn't speak but is hiding a dark secret.  Tim and Janet must uncover Katie's secret because their family is in danger.<br/><br/>This movie does have a slow start but it has a dependable and strong ending.  You must pay attention while watching this movie.  Every line, gesture, and moment have meaning to the plot.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/whostolemydaughter.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/whostolemydaughter.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"Chronicle Mysteries: The Deep End",
-    "review":"So here's what we know, Stephanie is on trial for her husband's murder and all evidence points to her.  But her lawyer, who is Alex's best friend, Katie, believe she didn't do it.  Before Elliot died, his children overheard Elliot talking to Claire.  Katie asks Alex to use her podcast to find Claire.  Alex agrees.  As more evidence comes to light, Alex and her friends start investigating the case for the real killer.<br/><br/>With well established relationships, this episode in the movie series focuses on the case.  As Alex investigates, she will reveal an underground poker game, planes, and secret storage lockers.  There is a personal aspect missing but this movie still captures your attention.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chroniclemysteriesthedeepend.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chroniclemysteriesthedeepend.jpg"
     }
 }
 ];
