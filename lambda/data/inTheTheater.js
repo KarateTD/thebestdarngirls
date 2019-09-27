61f07982a100ebf92ed83ef3ee905a90ad2db90b
Tiffany Gray <KarateTD@yahoo.com>
2019-09-27 00:55:03 -0400

    add black and blue

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
     {
         "option":"1",
+        "mtitle":"Black and Blue",
+        "review":"Afghan vet, Alicia West is a 3-week rookie at the New Orleans Police Department.  After hearing gunshots, Alicia walks into an abandoned plant to investigate.  She sees 2 men dead on the ground another begging for his life.  She witnesses NARC Malone shoot the man in cold blood.  Another NARC spots her and shoots her multiple times.  She escapes but they want her body cam.  To cover his tracks, Malone tells the chief Alicia went rogue and shot all 3 men.  When the neighborhood kingpin Darius arrives on the scene, he is heartbroken to see his nephew dead on the ground.  Malone tells him Alicia killed his nephew.  Now, Alicia has dirty armed cops and heavily armed gang members hunting her down.  She must get the camera back to the station to upload the footage.  She hopes to clear her name with the police, call off Darius's hit, and hold Malone accountable for his actions.  Good thing she has a Mouse on her side.<br/><br/>While this movie address profiling and police brutality, it touches on 2 issues that are easy to miss.  First, the lack of technology.  If the footage was wirelessly uploaded to the cloud, this movie would not exist.  Second, good cops holding bad cops accountable.  When Alicia decides to tell the truth, she knew it would be labeled a rat.  She doesn't care.  This movie is a wild ride that doesn't get too preachy by adding some quick wit.<br/><br/>4 out of 5 stars.",
+        "iamge":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackandblue.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackandblue.jpg"
+        }
+    },
+    {
+        "option":"2",
         "mtitle":"Rambo: Last Blood",
         "review":"John Rambo is dealing with his PTSD by building tunnels under his farm while taking medication.  He cares for his niece Gabby with the help of his farm manager Maria.  Gabby discovered the whereabouts of her father Manuel and wants to see him.  Maria and John are against it.  Under the guise of spending time with friends, Gabby goes to Mexico to see her father.  She doesn't return.  John must go back to the darkest parts of his mind and become Rambo to get Gabby back.<br/><br/>With the drive of Bryan Mills, the ingenuity of Kevin McCallister, and the striking blow of Scarlet Witch, Rambo delivers the bloody, revenge story we all want to see from this franchise.  New fans don't have to see the first 4 to watch.  Original fans will see the links between First Blood and Last Blood.  If you have a strong stomach, see it today.<br/><br/>4 out of 5 stars.",
         "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"2",
+        "option":"3",
         "mtitle":"Ad Astra",
         "review":"Soon, humans can go to the moon the way we travel to Orlando or LA.  We can do this because the sacrifice Clifford McBride made.  He was the first man on Mars and the first man to attempt to travel to Neptune.  Tragically, his project, the Lima Project, did not survive the main voyage.  Major Roy McBride, Clifford's son, continues his father's work today.  While working on an antenna outside the Earth's atmosphere, an energy blast hits.  The surge of power knocked out power grids and caused thousands of deaths.  Roy is called into work to be debriefed.  Roy learns the horrific truth.  Clifford is still alive.  During his mission to Neptune, he killed his team and turned off all communications.  The government believes Clifford created the energy blast.  They want Roy to fly to Mars and send a message to Clifford to stop the attacks.  No one knows Roy is depressed.  Staring into the void with Clifford sounds better than being on Earth.<br/><br/>The graphics, weightless rigging, and other special effects are superb.  But that is almost taken away with Roy's incessant monologuing.  The Twilight-like dread makes the movie extremely dull.<br/><br/>2.5 out of 5 stars.",
         "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"3",
+        "option":"4",
         "mtitle":"Official Secrets",
         "review":"Katharine is an interpreter for the British Government.  It's her job to listen to British enemies' phone conversations and report any terrorist plot.  While working, she gets a Top Secret email.  The US wants to gain damning information about U.N. allies against the war.  They will use the information to blackmail the allies to vote for the war.  Katharine won't stand for this government intrusion of our allies.  So, she prints the email and gives it to a friend.  That decision will put her motives, marriage, and freedom in jeopardy.<br/><br/>While watching this movie, you have to determine if Katharine is a whistleblower, traitor, or both.  While her voice did not stop the war, it did make people second guess the war and their government.  Although a lot of information is covered, something still feels missing.  You will need to read the book to fill in the holes.<br/><br/>3 out of 5 stars.",
         "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
         }
     },
 {
-    "option":"4",
+    "option":"5",
     "mtitle":"Brittany Runs a Marathon",
     "review":"Brittany tries to scam Adderall from a doctor but he prescribes her a life of healthy living.  He wants her to lose 45 to 55 pounds, get more sleep, and eat better.  Her first attempt leaves her crying on the floor.  Her neighbor, Catherine, asks Brittany to join her running club.  At first reluctant, Brittany attends and meets Seth, a married dad of 2.  After the 3 run together for a while, Catherine admits that in the past she was selected for the New York Marathon.  But she got pregnant and had to drop out.  This inspires Brittany to sign up.  When her dream is deferred due to injury, will the old Brittany come back?<br/><br/>Inspired by a true story, this movie makes you laugh one minute and tear up the next.  This story of triumph includes a love story.  Also, this movie challenges the social norm of acceptance.  See this movie and be inspired to join a 5K of your own.<br/><br/>4 out of 5 stars.",
     "image":{
         "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/brittanyrunsamarathon.jpg",
         "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/brittanyrunsamarathon.jpg"
-    }
-},
-{
-    "option":"5",
-    "mtitle":"The Goldfinch",
-    "review":"While looking at paintings with his mother, Theodore admires the Goldfinch.  His mom walks away to look at another painting and an explosive device detonates.  Theodore wakes up and Mr. Blackwell is clinging to life and his mother is dead.  He gives Theo a ring with his company logo on it and tells him to take the Goldfinch painting.  As Theo's life became ever increasingly unstable, he secretly clung to the painting like a life raft.  Now, in a hotel room, a life of depression, loss, abuse, abandonment, and drugs has led Theo to consider suicide.  He only regrets not returning the painting.<br/><br/>Masterfully shot and acted, The Goldfinch is a tragic story of loss and love.  While this mystery is entertaining, it has several moments of lulls.  However the moment Boris and Theo are on the screen, all lulls are forgotten.<br/><br/>3 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegoldfinch.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegoldfinch.jpg"
     }
 }
 ];
