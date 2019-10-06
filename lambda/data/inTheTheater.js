e6e968cbc9206ccb29cce3d6daad211572c20ad1
Tiffany Gray <KarateTD@yahoo.com>
2019-10-05 23:51:18 -0400

    one in theater and 4 tv

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
     {
-    "option":"1",
+        "option":"1",
+        "mtitle":"Joker",
+        "review":"Arthur, an aspiring stand-up comedian, works as a clown.  After being attacked by kids, losing his job, and getting a gun, Arthur shoots and kills 3 businessmen on the train during an assault.  This is the only time Arthur has felt alive and he doesn't feel bad.  The next morning, the downtrodden of Gotham see the masked clown as their voice.  He has stood up to the elite that looks down on him.  Arthur finally feels heard and seen.  But when his mother's letters lead him to a dark truth, he will cross paths with a young Bruce Wayne and bring madness to Gotham as the world has never seen.<br/><br/>Now here is the proverbial question: Did the darkness of Gotham create the Joker or did Joker bring out the darkness in Gotham.  This is not for children and it is not the M.C.U.  It's a D.C. comics movie, the way it's supposed to be.  Dark, complicated, gritty, raw, and unfiltered.  You will enjoy this moment from beginning to end.  Don't stay past the end credits and there is no need for IMAX or 3D.  Dolby is good enough.<br/><br/>5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/joker.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/joker.jpg"
+        }
+    },
+    {
+    "option":"2",
     "mtitle":"Abominable",
     "review":"After working several odd jobs to earn money to take a tour of China, Yi finds a creature on the roof of her apartment building.   She gets the creature to trust her and names him Everest.  Later, her friends Peng and Jin discovers her secret and try to call the police.  Dr. Zara and Burnish hear the call over their police scanner.  Everest escaped Dr. Zara's lab and Burnish, Dr. Zara's funder, wants Everest back.  Burnish likes to collect rare and exotic creatures.  Yi decides she must do everything to get Everest back home.  And Peng and Jin are unwittingly along for the ride.<br/><br/>You must see this movie in 3D to truly enjoy the magic of Everest.  With all cartoons, Yi has a sad backstory that drives her compassion.  There are a plethora of jokes for parents and kids alike.  See the matinee in 3D.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"2",
+        "option":"3",
         "mtitle":"Black and Blue",
         "review":"Afghan vet, Alicia West is a 3-week rookie at the New Orleans Police Department.  After hearing gunshots, Alicia walks into an abandoned plant to investigate.  She sees 2 men dead on the ground an another begging for his life.  She witnesses NARC Malone shoot the man in cold blood.  Another NARC spots her and shoots her multiple times.  She escapes but they want her body cam.  To cover his tracks, Malone tells the chief Alicia went rogue and shot all 3 men.  When the neighborhood kingpin Darius arrives on the scene, he is heartbroken to see his nephew dead on the ground.  Malone tells him Alicia killed his nephew.  Now, Alicia has dirty, armed cops and heavily armed gang members hunting her down.  She must get the camera back to the station to upload the footage.  She hopes to clear her name with the police, call off Darius' hit, and hold Malone accountable for his actions.  Good thing she has a Mouse on her side.<br/><br/>While this movie addresses profiling and police brutality, it touches on 2 issues that are easy to miss.  First, the lack of technology.  If the footage was wirelessly uploaded to the cloud, this movie would not exist.  Second, good cops holding bad cops accountable.  When Alicia decides to tell the truth, she knew she would be labeled a rat.  She doesn't care.  This movie is a wild ride that doesn't get too preachy by adding some quick wit.<br/><br/>4 out of 5 stars.",
         "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"3",
+        "option":"4",
         "mtitle":"Rambo: Last Blood",
         "review":"John Rambo is dealing with his PTSD by building tunnels under his farm while taking medication.  He cares for his niece Gabby with the help of his farm manager Maria.  Gabby discovered the whereabouts of her father Manuel and wants to see him.  Maria and John are against it.  Under the guise of spending time with friends, Gabby goes to Mexico to see her father.  She doesn't return.  John must go back to the darkest parts of his mind and become Rambo to get Gabby back.<br/><br/>With the drive of Bryan Mills, the ingenuity of Kevin McCallister, and the striking blow of Scarlet Witch, Rambo delivers the bloody, revenge story we all want to see from this franchise.  New fans don't have to see the first 4 to watch.  Original fans will see the links between First Blood and Last Blood.  If you have a strong stomach, see it today.<br/><br/>4 out of 5 stars.",
         "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"4",
+        "option":"5",
         "mtitle":"Ad Astra",
         "review":"Soon, humans can go to the moon the way we travel to Orlando or LA.  We can do this because the sacrifice Clifford McBride made.  He was the first man on Mars and the first man to attempt to travel to Neptune.  Tragically, his project, the Lima Project, did not survive the main voyage.  Major Roy McBride, Clifford's son, continues his father's work today.  While working on an antenna outside the Earth's atmosphere, an energy blast hits.  The surge of power knocked out power grids and caused thousands of deaths.  Roy is called into work to be debriefed.  Roy learns the horrific truth.  Clifford is still alive.  During his mission to Neptune, he killed his team and turned off all communications.  The government believes Clifford created the energy blast.  They want Roy to fly to Mars and send a message to Clifford to stop the attacks.  No one knows Roy is depressed.  Staring into the void with Clifford sounds better than being on Earth.<br/><br/>The graphics, weightless rigging, and other special effects are superb.  But that is almost taken away with Roy's incessant monologuing.  The Twilight-like dread makes the movie extremely dull.<br/><br/>2.5 out of 5 stars.",
         "image":{
             "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/adastra.jpg",
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/adastra.jpg"
-        }
-    },
-    {
-        "option":"5",
-        "mtitle":"Official Secrets",
-        "review":"Katharine is an interpreter for the British Government.  It's her job to listen to British enemies' phone conversations and report any terrorist plot.  While working, she gets a Top Secret email.  The US wants to gain damning information about U.N. allies against the war.  They will use the information to blackmail the allies to vote for the war.  Katharine won't stand for this government intrusion of our allies.  So, she prints the email and gives it to a friend.  That decision will put her motives, marriage, and freedom in jeopardy.<br/><br/>While watching this movie, you have to determine if Katharine is a whistleblower, traitor, or both.  While her voice did not stop the war, it did make people second guess the war and their government.  Although a lot of information is covered, something still feels missing.  You will need to read the book to fill in the holes.<br/><br/>3 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/officialsecrets.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/officialsecrets.jpg"
         }
     }
 ];
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
     {
         "option":"1",
+        "mtitle":"Abducted: The Mary Stauffer Story",
+        "review":"The Stauffer family is moving to the Philippines for missionary work.  Before they leave, Mary and Beth go to the salon for a mother-daughter day of beauty.  As they leave the parking lot a man comes up behind them and forces them into the car at gunpoint.  After driving down the road, he makes Mary and Beth get into the trunk.  Jason, a young boy, sees the man on the side of the road and tries to help.  The man grabs him and puts him in the trunk with Mary and Beth.  Later he takes Jason out the trunk, Mary and Beth never see him again.  The man brings Mary and Beth to a house and locks than in the closet.  That afternoon he takes Mary out and interviews her.  She doesn't remember him.  So he reminds her.  They met 15 years ago when she taught 9th grade.  His name is Ming and he blames her for everything wrong in his life.  He has been obsessed with her since.  Mary must survive to save her life and her daughters.<br/><br/>Mary and Beth's ordeal will leave chills up your spine.  Ming romanticized a relationship with her that includes rape and kidnapping.  At multiple times they could have been saved but their cries for help were ignored.  Mary and Beth's survival is a testament to love, strength and faith.<br/><br/>5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/abductedthemarystaufferstory.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/abductedthemarystaufferstory.jpg"
+        }
+    },
+    {
+        "option":"2",
+        "mtitle":"Smuggling in Suburbia",
+        "review":"Joanie is invited to a party by Annabelle and S.  She meets Annabelle's boyfriend Danny.  Danny calibrates expensive camera lenses and Annabelle and S hand delivers them because they are too fragile.  He offers to hire Joanie to make a delivery.  She is happy to make her first $100 but her mother is pissed and starts asking questions.  However, the questions stop when she gives her mom the money.  During the next delivery, Joanie starts getting suspicious and looks into the lenses.  She finds several uncut diamonds.  Joanie wants out.  But when her brother needs surgery she goes back.  Soon, the feds are at her doorstep.<br/><br/> Joanie's story is the typical naive girl lead astray.  You feel some relief when her mother says everything you are thinking but when she caves, you want to scream.  This movie does successfully show the link between diamond smuggling to blood diamonds and the 9/11 attacks.  While the lead up to Joanie's arrest is not typical, the ending is 100% Lifetime.<br/><br/>3.5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/smugglinginsuburbia.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/smugglinginsuburbia.jpg"
+        }
+    },
+    {
+        "option":"3",
+        "mtitle":"A Working Mom's Nightmare",
+        "review":"After maternity leave, Kathy is back but on shaky ground.  Her boss Phillip decided to keep her replacement Hanna as the C.S.O and her husband, Mark, is lying about where he is at night.  When her home and work life starts to fall apart, Kathy blames Hanna.  But no one believes her.  They think she is paranoid or is threaten because Hanna is so good.  Kathy will prove them wrong.<br/><br/>Based on a novel, this movie is a slow build to a very little climax.  This movie follows all the Lifetime rules and becomes background noise.  You are better off reading the book.<br/><br/>2 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aworkingmomsnightmare.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aworkingmomsnightmare.jpg"
+        }
+    },
+    {
+        "option":"4",
+        "mtitle":"Ruby Herring Mysteries: Her Last Breath",
+        "review":"Ruby comes to Natalie's yoga studio to interview her about her successful vitamin supplements.  But Natalie is nowhere to be found.  Using the Find My Phone app, Natalie is found dead at the bottom of an embankment.  Ruby must work with Detective Jake to find out who killed her friend and why.<br/><br/> The second episode in the series opens up the life of Detective Jake.  We find out why he left New York for Seattle as well as his other talents.  This mystery does provide plenty of suspects with their motives but it not the strongest series this fall.<br/><br/>4 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rubyherringmysteriesherlastbreath.jpeg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rubyherringmysteriesherlastbreath.jpeg"
+        }
+    },
+    {
+        "option":"5",
         "mtitle":"Trapped: The Alex Cooper Story",
         "review":"Alex has turned her life around.  She stopped partying and attends school and church regularly.  One day she meets Frankie.  They hit it off and Alex starts spending more time with her.  But when she comes home late with a hickey, her parents want answers.  Alex admits she is attracted to girls and her parents kick her out.  Later, in hopes of reconciliation, Alex agrees to stay with her grandparents in Utah.  But her parents pull up to a home she doesn't know and ask her to get out of the car.  Once inside, her parents give the homeowners, Johnny and Tiana, parental custody.  Alex's parents hope they can make her straight.  And they will use physical and emotional abuse to do it.<br/><br/>Regardless of how you feel about homosexuality, it's gut-wrenching to watch Alex's abuse.  You will be horrified when the abuse leads to a suicide attempt.  It's hard to believe people can do this to each other and her parents allowed it.  So watch and discuss.<br/><br/>4 out of 5 stars.",
         "image":{
             "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/trappedthealexcooperstory.jpeg",
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/trappedthealexcooperstory.jpeg"
-        }
-    },
-    {
-        "option":"2",
-        "mtitle":"Mystery 101: Dead Talk",
-        "review":"Amy is invited to speak at a Teachable Talk by her ex-boyfriend Tim.  Out of jealous curiosity, Detective Travis gives Graham a ride to the talk.  During a rehearsal, Amy is interrupted by Mitchell, the CEO of McHale Tech.  Amy saw Mitchell's wife, Alexis, and Mitchell's best friend, Billy, very close at dinner last night.  When Mitchell falls off the roof and dies, Amy believes Billy or Alexis is the killer.  On the other hand, Travis thinks its Tim.  Travis tries to stay in the background because they are in another jurisdiction but can't stop looking for clues when he knows the investigation is going in the wrong direction.  He gets a glimpse of what Amy goes through every day.  The two will work together to go against the system to find Mitchell's killer.<br/><br/>Another episode in the signature series stays on track.  You have an unlikeable victim, lots of suspects, and different motives.  A bonus is the side conversations that play a bigger role in the mystery.  So, pay attention.<br/><br/>5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mystery101deadtalk.jpeg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mystery101deadtalk.jpeg"
-        }
-    },
-    {
-        "option":"3",
-        "mtitle":"Always a Bridesmaid",
-        "review":"Corina has been a bridesmaid 13 times and number 14 is around the corner.  With love in the air, her friends want her to get back into dating.  They set her up on blind double dates and encourage her to put herself out there.   When her friend doesn't see progress, she takes Corina's phone and adds her onto all the dating apps.  In a tug-a-war, Corina accidentally swipes right on Mark.  She blames it on catfishing but the two go out.  Things go great for months until Mark asks one question that brings down the relationship.  Can the two get back on track before Mark moves away?<br/><br/>Here is an examination on how women can be toxic to their relationship.  Corina has avoided love because of broken promises.  Not from lovers but her father.  Mark is quick to call her out on it.  She wants love but at a distance.  It will cause you to look at your relationship patterns.<br/><br/>3.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/alwaysabridesmaid.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/alwaysabridesmaid.jpg"
-        }
-    },
-    {
-        "option":"4",
-        "mtitle":"Escaping the NXIVM Cult: A Mother's Fight to Save Her Daughter",
-        "review":"Catherine's script is rejected again.  India, Catherine's daughter, has a failing business.  India is lost and doesn't know what to do next.  Catherine was told about an Executive Success Program by a friend.  She believes it will help her and India grow in business and get out of their slump.  Plus, they can do it together.  After the first seminar, Catherine and India sign up for more in Albany.  Catherine witnesses some disturbing trends in the house and wants to leave, But India wants to stay.  Catherine doesn't know she is leaving India with a sex cult called NXIVM.  It will take public pleas and a news article to free India from their charismatic leader Keith.<br/><br/>From the outside looking in, you can see the 4 pillars of indoctrination.  Picking a target, love bombing, isolation, and control is how all cults find and keep their members.  The branding scene may be disturbing for younger viewers but this is a great teaching tool.<br/><br/>4.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/escapingthenxivmcult.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/escapingthenxivmcult.jpg"
-        }
-    },
-    {
-        "option":"5",
-        "mtitle":"Mystery 101: Words Can Kill",
-        "review":"Amy attends her father's book festival to get her mind off her recent office break-in.  Later that night, she is doing great until Travis spots a man sitting outside Graham's house.  Travis checks the man out and discovers the man is dead.  Shaken but not deterred, Graham continues with the festival.  But Graham will end the night in handcuffs when he is suspected of killing Betsy.  At one time, Betsy had the number one author because she stole Graham's first manuscript and gave it to another author.  The author is no longer successful but Graham still holds resentment.  If Amy and Travis don't help him, he could be serving a life sentence.<br/><br/>While there are plenty of good suspects and a strong plotline, the killer is an easy choice for long-time Hallmark fans.  So watch this movie for how the killer committed the crime, not the killer.<br/><br/>4 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mystery101wordscankill.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mystery101wordscankill.jpg"
         }
     }
 ];
