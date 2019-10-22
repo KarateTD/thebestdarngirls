603a48012a884d6ce52e15d81896046412aadf3a
Tiffany Gray <KarateTD@yahoo.com>
2019-10-22 17:07:14 -0400

    add lion king

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,15 @@
 var mustBuy = [
     {
         "option":"1",
+        "mtitle":"Disney's The Lion King",
+        "review":"Simba is next in line for the throne.  But his Uncle Scar believes it's his rightful place.  Scar convinces Simba to go to the gorge to practice his roar.  Soon, Simba is in the middle of a stampede.  Scar runs to his brother, King Mufasa to help Simba but its a trap.  When Simba isn't looking, Scar kills Mufasa and blames the death on Simba.  Scar tells Simba to run away and never come back.  Scar promotes the lawless hyenas as his army while they lay waste to the pride.  It will take one old friend, 2 new friends, and fur to bring Simba back to the pride.<br/><br/>The live-adaption of this Disney classic stays true to the original with astounding attention to detail within mind-blowing graphics.  However, making these animals look like animals, takes away the emotions the animated characters possessed.  This forced the movie to rely on the voice actors to convey every pure emotion.  Some succeeded while others fell flat.  Also, the song \"Can You Feel the Love Tonight\" is performed midday.  It feels out of place.  Overall, the movie may not be better than the first but it is still a strong movie worth seeing.  It's better in IMAX and/or 3D.<br/><br/>4 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelionking.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelionking.jpg"
+        }
+    },
+    {
+        "option":"2",
         "mtitle":"Stuber",
         "review":"Stu drives an Uber to help invest in his friend's spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on a path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will change these men's lives forever.  But whether or not it's for the better is up to you.<br/><br/>Be ready to laugh until it hurts.  Don't make the mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  Also, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
         "image":{
@@ -9,7 +18,7 @@ var mustBuy = [
         }
     },
     {
-        "option":"2",
+        "option":"3",
         "mtitle":"Toy Story 4",
         "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike his time with Andy, Woody is not her favorite toy and not the leader of the toys.  Woody, knowing his job is to protect Bonnie, hitches a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>Toy Story 4 is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background characters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see he is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
         "image":{
@@ -18,7 +27,7 @@ var mustBuy = [
         }
     },
     {
-        "option":"3",
+        "option":"4",
         "mtitle":"Spider-Man: Far From Home",
         "review":"After everyone was snapped back, people want the next Iron Man.  Spider-Man doesn't feel like he can step into Tony's shoes.  But Nick Fury needs him now.  The Elementals have invaded Earth.  The Wind and Earth elements were stopped by Mysterio.  With the assistance of Spider-Man, Mysterio was able to stop the water element in Venice.  Now, Nick wants Peter to go to Prague with Mysterio to stop the most powerful element, Fire.  Peter wants to enjoy his vacation with his friends, so he turns down Nick's offer.  Before Peter and his friends board the bus, the teacher informs them the trip got an upgrade and they are going to Prague.  Peter knows Fury has hijacked their vacation.  With Tony Stark's Glasses and EDITH by his side, Peter will take on Fire.  But can Peter trust what he sees?<br/><br/>While Homecoming introduced us to a younger Spider-Man, Far From Home is his coming of age story without Iron Man by his side.  He has to figure out who he wants to be as a man and a superhero.  While the CGI has gotten better, you can still pinpoint moments where you are witnessing 100% CGI versus green screen scenes.  There are 2 post-credit scenes and a cameo that original Spider-Man fans will love.  See this movie in IMAX.<br/><br/>4.5 out of 5 stars.",
         "image":{
@@ -27,23 +36,14 @@ var mustBuy = [
         }
     },
     {
-        "option":"4",
+        "option":"5",
         "mtitle":"Shaft",
         "review":"20 years ago, Maya took John Jr. away to keep him safe from Shaft's dangerous occupation.  Now, John Jr. is an FBI data analyst who is often disrespected.  After Kiram, his best friend, dies of an overdose, John Jr. can't believe it.  Sasha sees the autopsy and confirms Karim was murdered.  After failing to investigate on his own, John decides to go to the only man who can help.  His father, a bad mother - shut your mouth - I'm only talking about Shaft.<br/><br/>This is easily the funniest movie of the year.  Shaft brings the gender gap of masculinity to the surface without getting mushy.  Both men learn something and stand up for their way of life.  See this movie today.<br/><br/>4 out of 5 stars.",
         "image":{
             "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shaft.jpg",
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/shaft.jpg"
         }
-    },
-{
-    "option":"5",
-    "mtitle":"John Wick: Chapter 3 - Parabellum",
-    "review":"Winston gives John 1 hour to get his affairs in order.  Then he will be excommunicated and a bounty of $14 million will be placed on his head.  Every assassin in the world will be after him and no one will offer help.  John uses the hour to find a safe place for his dog, get a ticket, get a marker, and get some gold coins.  Since some assassins couldn't wait for an hour, John needed to seek medical treatment.  In the middle of getting stitches, John's time runs out and the doctor stops the work.  John has to complete is stitches and heads to the Director.  He asks the Director to give him passage to Casablanca.  He knows an assassin there who can lead him to the High Table.  He hopes to plead his case so the High Table will let him live.<br/><br/>This movie proves better weapons, excellently choreographed fights, and mind-blowing cinematography is all action lovers need.  With so much going on, this movie provides 30 to 45 minutes of dialogue.  It gives you more of John's training history with the Director but not enough information about the blood oath between him and Sophia.  Seeing John Wick 2 is necessary for this movie.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/johnwickchapter3parabellum.jpg",
-        "largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/johnwickchapter3parabellum.jpg"
     }
-}
 ];
 
 module.exports = mustBuy;
@@ -55,16 +55,6 @@ module.exports = mustBuy;
 */
 
 /*  For Must Buy later
-
-{
-    "option":"4",
-    "mtitle":"Disney's The Lion King",
-    "review":"Simba is next in line for the throne.  But his Uncle Scar believes it's his rightful place.  Scar convinces Simba to go to the gorge to practice his roar.  Soon, Simba is in the middle of a stampede.  Scar runs to his brother, King Mufasa to help Simba but its a trap.  When Simba isn't looking, Scar kills Mufasa and blames the death on Simba.  Scar tells Simba to run away and never come back.  Scar promotes the lawless hyenas as his army while they lay waste to the pride.  It will take one old friend, 2 new friends, and fur to bring Simba back to the pride.<br/><br/>The live-adaption of this Disney classic stays true to the original with astounding attention to detail within mind-blowing graphics.  However, making these animals look like animals, takes away the emotions the animated characters possessed.  This forced the movie to rely on the voice actors to convey every pure emotion.  Some succeeded while other's fell flat.  Also, the song \"Can You Feel the Love Tonight\" is performed midday.  It feels out of place.  Overall, the movie may not be better than the first but it is still a strong movie worth seeing.  It's better in IMAX and/or 3D.<br/><br/>4 out of 5 stars.",
-    "image":{
-        "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelionking.jpg",
-        "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelionking.jpg"
-    }
-}
 
 {
     "option":"1",
@@ -155,4 +145,34 @@ module.exports = mustBuy;
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/joker.jpg"
         }
     }
+
+        {
+        "option":"1",
+        "mtitle":"Maleficent: Mistress of Evil",
+        "review":"After asking Aurora to marry him, Prince Phillip extends an invitation for Maleficent to meet his parents, King John and Queen Ingrith.  At the dinner, both women take verbal jabs at each other.  But Maleficent can't take it any longer.  She uses her magic to push everyone away and the king collapses and falls into a deep sleep.  The queen tells her guard, Gerda, to shoot Maleficent with a lead bullet for cursing the king.  Gerda takes the shot and Maleficent falls from the sky.  But she is saved by a dark winged creature.  This creature will show Maleficent a world she never knew existed.  Maleficent will need to decide: bring peace for the love of one human or bring war for the love of all fairies like her.<br/><br/>See this movie in IMAX.  Any other form will be cheating yourself.  Serving as a sequel, this movie gives a new story for Maleficent while respecting the original.  The CGI, make-up, and costume design are second to none.  Each fairy has a unique look but with little details to let you know they are related.  Be prepared to laugh, clutch your pearls, and gasp with amazement.  This movie is only missing a visual aspect of Ingrith's story.  It's hard to understand if she is lying or telling the truth about her past.<br/><br/>4 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/maleficentmistressofevil.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/maleficentmistressofevil.jpg"
+        }
+    }
+
+    {
+        "option":"2",
+        "mtitle":"Always in Season",
+        "review":"When Claudia receives the news her son Lennon was dead, she was heartbroken.  However, her grief turned to rage when the police declared it a suicide.  She knows her son would never kill himself.  He was happy and making plans for the future.  So Lennon's brother Pierre started investigating.  His results will lead to this case being presented to the F.B.I..  Pierre and Claudia hope Lennon's case will be fully and properly investigated.<br/><br/>This documentary will anger you to your core while bringing a heavy sadness on your chest.  As Lennon's family and friends give you a glimpse of Lennon's life, you are endeared to him.  During the documentary, the lynchings of Moore's Ford and Claude Neal are discussed in great detail.  Hearing first-hand accounts are eye-opening and allow the viewer to see these crimes as a thing of the present.  The film does point the finger at someone but doesn't allow that person to present themselves to the audience.  It's crucial to the validity of the accusations.<br/><br/>4.5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/alwaysinseason.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/alwaysinseason.jpg"
+        }
+    }
+
+    {
+        "option":"3",
+        "mtitle":"Jexi",
+        "review":"After crashing into Cate, Phil has to replace his phone.  His phone has kept him entertained and isolated from the world.  His phone comes with a digital assistant named Jexi.  Phil accepts the user agreement without reading it.  This is a mistake.  He has given Jexi permission to override his commands if Jexi has an option that will make Phil's life better.  Phil tries to replace his phone but Jexi downloads its software from the cloud.  So, Phil gives in.  At first, Jexi is helping Phil get a job, friends, and Cate.  When Phil ignores Jexi for his better life, Jexi goes crazy like a stalker ex.<br/><br/>This extremely funny comedy will have you examining how much you live by your phone.  You will question if you Instagram life is better than your real life.  When Denise compares cell phone addicts to crackheads, she believes crackheads are better.  Crackheads have friends, exercise, and get out of the house.  Phil is lacking at all 3.  While this movie has a strong message, it doesn't get preachy.  A bonus: Jexi trash talks Alexa, Siri, and Cortana.<br/><br/>4 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/jexi.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/jexi.jpg"
+        }
+    },
 */
