e3d260ed7a0d46d4011fcdd30376f58107b36aa6
Tiffany Gray <KarateTD@yahoo.com>
2019-10-26 23:20:15 -0400

    add 5 made for tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,49 +1,49 @@
 var madeForTV = [
-    {
-        "option":"1",
-        "mtitle":"Patsy and Loretta",
-        "review":"After waking up in the hospital with injuries from a car accident, Patsy hears Loretta singing one of her songs on the radio.  Patsy asks her husband to bring Loretta to her hospital room.  Loretta comes into the room loaded with an apology but Patsy was flattered by her dedication.  The two talk about their music, their husbands, and their children.  Patsy takes Loretta under her wing and helps guide her career.  The two form a friendship that lasts until Patsy's untimely demise.<br/><br/>In a time of hashtag girl power, this movie is the embodiment of female empowerment.  Patsy never saw Loretta as a threat, she saw Loretta as a friend and admirer.  It's difficult to imagine today's artists being able to do the same thing without their ego getting in the way.  We should all be students like Loretta to become teachers like Patsy.<br/><br/>5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/patsyandloretta.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/patsyandloretta.jpg"
-        }
-    },
-    {
-        "option":"2",
-        "mtitle":"Sleeping with My Student",
-        "review":"On her girls' trip, Kathy has a sexy tryst with a young maintenance worker named Ian.  She goes home to her daughter and her new job like nothing happened.  That changes on her first day of school as principal.  Ian is there and he is a student.  While he can legally consent, she worries he will talk.  When her daughter Bree starts hanging out with Ian, Kathy tries to put a stop to it.  Kathy believes she has it under control until Ian shows her a video of their night together.  He wants money or he will release the tape.  Ian is blackmailing Kathy for love, but not his love of her.  The love of someone he is bonded within tragedy.<br/><br/>You must watch this movie from the beginning to weave in all the moving parts.  Ian is not after Kathy for her body or love, he wants justice.  The story is bland, the twist unimaginative, and the conclusion is average.  This is a movie to watch while you tweet.<br/><br/>3 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sleepingwithmystudent.jpeg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sleepingwithmystudent.jpeg"
-        }
-    },
-    {
-        "option":"3",
-        "mtitle":"Psycho BFF",
-        "review":"New girl Deandra encounters bullies on her first day.  In poetry class, Deandra is paired with Olivia.  They bond over Olivia's poem about her deceased best friend Liberty.  Olivia even steps in to defend Deandra from her bullies.  Soon, Deandra and Olivia are inseparable.  Renee, Deandra's mother, worries Olivia is a bad influence.  With a bond so strong, Olivia will do anything to anyone to keep it.  Only a gun could come between them.<br/><br/>New girl makes a best friend that's crazy and takes a long time to figure it out.  Sounds familiar? It should.  It's like most storylines in the Psycho Genre.  This movie doesn't stand out in any way and can be used as background noise.<br/><br/>2.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psychobff.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psychobff.jpg"
-        }
-    },
-    {
-        "option":"4",
-        "mtitle":"Swag, Inc;",
-        "review":"After helping Reggie get the woman of his dreams, David receives $500 as payment.  This gives David an idea: Create an app and charge to give men dating advice.  The fee will include personal one-on-one training.  David presents his idea to his best friend AJ.  AJ uses his newly acquired certifications to create the app and they call it Swag, Inc..  The first day, they get 20,000 subscribers and quit their jobs.  Both are elated to have the app up and successful but their is trouble brewing around the corner.  AJ paid Swag, Inc. hosting account with his old company's credit card.  AJ's boss wants him to turn over Swag, Inc. or they will sue and have him put in jail.  AJ and David have to find a way to keep Swag, Inc..  The solution is in the question.<br/><br/>While AJ and David can get any woman they want, they can't maintain the relationship.  Unfortunately, they approached creating an app the same way they did dating.  It's easy for the viewer to understand, they must strive for longevity to be successful.  It's the only way the app can grow.  This movie is funny but it can get corny at times.<br/><br/><br/>3.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/swaginc.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/swaginc.jpg"
-        }
-    },
-    {
-        "option":"5",
-        "mtitle":"Crossword Mysteries: Proposing Murder",
-        "review":"Five days ago, Lyle asked his friend Tess to hide his marriage proposal to Abby in her crossword puzzle.  Now, Detective Logan stands over Lyle's body determining what happened to the tenured professor.  His house was ransacked but nothing was stolen.  Tess, against the advice of Detective Logan, starts to investigate.  It will take a cipher, beef stew, and a children's book to solve this mystery.  Will Tess live to see the perpetrator captured.<br/><br/>It is hard to believe but Hallmark has made a detective out of a crossword editor.  The second installment in this series is 10 times more captivating than its predecessor.  There are multiple suspects with a plethora of motives.  The viewer is allowed to solve the case along with Tess while getting to know Tess and Logan better.<br/><br/>5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/crosswordmysteriesproposingmurder.jpeg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/crosswordmysteriesproposingmurder.jpeg"
-        }
+  {
+    "option":"1",
+    "mtitle":"The Road Home for Christmas",
+    "review":"Wes and Lindsey play dueling pianos at a bar in New Orleans.  When she loses a last-minute gig, Lindsey doesn't have enough money to catch a flight home for the holiday.  After a discussion with Wes, Lindsey discovers they are one town apart.  Desperate not to miss the birth of her niece or nephew, Lindsey agrees to take a 15-hour road trip.  Wes and Lindsey will find nuns, a nativity play, a childhood idol, and each other.<br/><br/>Famous last words before a road trip: What could go wrong.  The moment Wes utters these words, you know everything will go wrong.  As much as this movie makes you laugh, it will pull at your heartstrings.  Nothing will bring a tear to your eye more than learning why Wes needs to get home too.  This isn't revealed until an hour into the movie.  This movie will become a favorite of the holiday.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theroadhomeforchristmas.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theroadhomeforchristmas.jpg"
     }
+  },
+  {
+    "option":"2",
+    "mtitle":"A Merry Christmas Match",
+    "review":"Corey works in her father's antique shop when Ryder walks into looking around the store.  Ryder is heir to a real estate tycoon.  He and Corey get into verbal jabs but he comes out on top with a new clock and chair.  Smitten by her, he makes more excuses to see her.  However, when he asks her to come to L.A. to visit him and attend his family's holiday benefit party, she says no.  But one gift will change her mind.  Now, she has to find him before she loses him to another woman.<br/><br/>Now this is a movie with lots of twists and turns.  Normally, when a woman in a Christmas movie is asked to move for love, she says yes, the kiss, and it snows.  The moment Corey says no, everything changes.  She has to pursue him.  This is a movie of self-discovery wrapped in a love story.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/amerrychristmasmatch.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/amerrychristmasmatch.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Sweet Mountain Christmas",
+    "review":"Laney's manager books her a spot on Santapalooza to debut her new look and sound.  This country artist is going pop.  Laney is ready until she learns it is being held on Christmas Eve in New York.  Laney wants to spend the holidays with her mom.  Her manager books her the last possible flight so she can spend some time with her mom and some in New York.  Once home, Laney is hit with star-struck fans but also some cold shoulders.  Her former sing partner, Laurie felt left behind along with her brother Robbie.  Robbie and Laney were in love before she went to Hollywood.  Now, he is a jack of all trades.  He plows the snow, delivers Christmas trees, and is the town mayor.  When the town needs Laney's star power to save the Baxter Theater show, she forgets all about New York.  She will have to make a decision.  The life she dreamt of or the life she left behind.<br/><br/>With entertaining music, this is a Hallmark movie on Lifetime.  A small-town girl heads home to find an old boyfriend and falls in love.  The plot is nothing new just a different season.  While it is charming, the music is its saving grace.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sweetmountainchristmas.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sweetmountainchristmas.jpg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Designed to Kill",
+    "review":"David is bullied every day for his appearance.  His photography teacher, Jennifer, decides to give him a confidence boost after seeing him strike out with a girl.  Jennifer buys him new clothes, a facial, and a new hair cut.  She takes some pictures of him and sends them to a magazine.  David goes from zero to hero overnight at school.  The most popular girl, Brittany, asks him out.  Excited at his turn of events, David runs and tells Jennifer.  This was a huge mistake.  David doesn't know Jennifer is in love with him.  She will lie, photoshop, and kill to keep him.<br/><br/>Seeing this teacher's obsession with David is dark and disturbing.  The viewer is alerted to her sick nature at the beginning of the movie but you don't know why until the middle.  It's an intriguing watch but you need to see it from the beginning.  However, the answer to the final question is left up to your imagination.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/designedtokill.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/designedtokill.png"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Kindred Spirits",
+    "review":"Much to the surprise of Chloe, her sister Sadie comes to town.  After a year, Chloe is happy to see the sister she raised after losing their parents.  Chloe's daughter Nicole is ecstatic.  Nicole and Sadie grew up like sisters rather than aunt and niece.  So, Nicole isn't worried when Sadie does her hair, dresses, and talks like her.  She thinks Sadie is being sweet.  Little does Nicole know, under that sweet exterior is a woman trying to claim her life.  Sadie doesn't want to be the loving sister and aunt, she wants to be the only daughter.  Someone will have to die to make this happen.<br/><br/>Single White Female comes to Lifetime Movie Network.  This film turns up the creepy when Sadie tries to pass herself off as a virgin high school student.  And it has the most gruesome scenes you have ever seen on TV.  But there are a lot of unanswered questions.  How did the police learn it was Sadie and was Nicole ever real?  This is a good one to watch during Halloween.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/kindredspirits.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/kindredspirits.jpg"
+    }
+  }
 ];
 
 module.exports = madeForTV;
