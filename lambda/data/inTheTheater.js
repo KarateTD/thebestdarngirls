fe22879b5f20a587da36f6fa5b4c78cbbb868502
Tiffany Gray <KarateTD@yahoo.com>
2019-10-27 03:47:23 -0400

    add 2 tv movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
+  {
+    "option":"1",
+    "mtitle":"The Current War",
+    "review":".<br/><br/>* out of 5 stars",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecurrentwar.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecurrentwar.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Countdown",
+    "review":".<br/><br/>* out of 5 stars",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/countdown.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/countdown.jpeg"
+    }
+  },
     {
-        "option":"1",
+        "option":"3",
         "mtitle":"Maleficent: Mistress of Evil",
         "review":"After asking Aurora to marry him, Prince Phillip extends an invitation for Maleficent to meet his parents, King John and Queen Ingrith.  At the dinner, both women take verbal jabs at each other.  But Maleficent can't take it any longer.  She uses her magic to push everyone away and the king collapses and falls into a deep sleep.  The queen tells her guard, Gerda, to shoot Maleficent with a lead bullet for cursing the king.  Gerda takes the shot and Maleficent falls from the sky.  But she is saved by a dark winged creature.  This creature will show Maleficent a world she never knew existed.  Maleficent will need to decide: bring peace for the love of one human or bring war for the love of all fairies like her.<br/><br/>See this movie in IMAX.  Any other form will be cheating yourself.  Serving as a sequel, this movie gives a new story for Maleficent while respecting the original.  The CGI, make-up, and costume design are second to none.  Each fairy has a unique look but with little details to let you know they are related.  Be prepared to laugh, clutch your pearls, and gasp with amazement.  This movie is only missing a visual aspect of Ingrith's story.  It's hard to understand if she is lying or telling the truth about her past.<br/><br/>4 out of 5 stars.",
         "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"2",
+        "option":"4",
         "mtitle":"Always in Season",
         "review":"When Claudia receives the news her son Lennon was dead, she was heartbroken.  However, her grief turned to rage when the police declared it a suicide.  She knows her son would never kill himself.  He was happy and making plans for the future.  So Lennon's brother Pierre started investigating.  His results will lead to this case being presented to the F.B.I..  Pierre and Claudia hope Lennon's case will be fully and properly investigated.<br/><br/>This documentary will anger you to your core while bringing a heavy sadness on your chest.  As Lennon's family and friends give you a glimpse of Lennon's life, you are endeared to him.  During the documentary, the lynchings of Moore's Ford and Claude Neal are discussed in great detail.  Hearing first-hand accounts are eye-opening and allow the viewer to see these crimes as a thing of the present.  The film does point the finger at someone but doesn't allow that person to present themselves to the audience.  It's crucial to the validity of the accusations.<br/><br/>4.5 out of 5 stars.",
         "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
         }
     },
     {
-        "option":"3",
+        "option":"5",
         "mtitle":"Jexi",
         "review":"After crashing into Cate, Phil has to replace his phone.  His phone has kept him entertained and isolated from the world.  His phone comes with a digital assistant named Jexi.  Phil accepts the user agreement without reading it.  This is a mistake.  He has given Jexi permission to override his commands if Jexi has an option that will make Phil's life better.  Phil tries to replace his phone but Jexi downloads its software from the cloud.  So, Phil gives in.  At first, Jexi is helping Phil get a job, friends, and Cate.  When Phil ignores Jexi for his better life, Jexi goes crazy like a stalker ex.<br/><br/>This extremely funny comedy will have you examining how much you live by your phone.  You will question if you Instagram life is better than your real life.  When Denise compares cell phone addicts to crackheads, she believes crackheads are better.  Crackheads have friends, exercise, and get out of the house.  Phil is lacking at all 3.  While this movie has a strong message, it doesn't get preachy.  A bonus: Jexi trash talks Alexa, Siri, and Cortana.<br/><br/>4 out of 5 stars.",
         "image":{
             "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/jexi.jpg",
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/jexi.jpg"
-        }
-    },
-    {
-        "option": "4",
-        "mtitle": "Gemini Man",
-        "review": "Henry, an assassin, is ready for retirement.  His conscious is starting to bother him and he is making unsure shots.  His last hit is a bioterrorist.  However, his former colleague Jack has some startling news.  The D.I.A. lied about the last hit.  The man was a molecular biologist with no ties to terrorism.  Henry wants answers.  That night a hit squad tries to kill Henry but he escapes with Danny.  He calls Baron to pick them up and take them to South America.  Before Henry can get some answers another hitman comes after him.  Through Henry's scope, he sees something familiar.  He can't pinpoint it because he only saw the hitman for a few seconds.  Henry doesn't know that a special program took note of his abilities and took his blood.  They created a clone of him and raised it.  Now, the leader of the project has sent the clone to kill Henry.  Will Henry have to kill himself to save himself?<br/><br/>Be ready for some of the best graphics ever witnessed in a movie.  With live-action capture, all the emotional nuances in Will Smith's face is properly displayed and aged down.  The subtle changes in his voice make the younger hitman believable.  See this movie in High Frame Rate if you can.  It is 100% worth it.<br/><br/>3.5 out of 5 stars.",
-        "image": {
-            "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/geminiman.jpg",
-            "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/geminiman.jpg"
-        }
-    },
-    {
-        "option": "5",
-        "mtitle": "The Addams Family",
-        "review": "Wednesday and Pugsley Addams never left the Addams Family estate.  Morticia and Gomez want the children to live free without the judgment of townspeople and pitchforks.  When the marsh is drained by Margaux, a reality D.I.Y. star, the fog is lifted and the Addams are exposed.  Below, Margaux has created a town called Assimilation.  She wants everyone to be the same.  If the Addams can't conform, she will push them out.  Good thing the Addams are more than willing to push back.<br/><br/>Parents, no need to worry, this is the Addams family you know and love.  And your children will have plenty of laughs too.  They will laugh at the Three Stooges violence, Addams dancing, and wild hijinks.  Parents will laugh at the subliminal jokes about death, sex, and love.  Bring the family and be ready to snap at the end.<br/><br/>3.5 out of 5 stars.",
-        "image": {
-            "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/theaddamsfamily.jpeg",
-            "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/theaddamsfamily.jpeg"
         }
     }
 ];
