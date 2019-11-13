9dd368f7e4bd7778e922e672a4859be3e234f721
Tiffany Gray <KarateTD@yahoo.com>
2019-11-13 02:22:01 -0500

    add 5 made for tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
-    "mtitle":"Christmas Reservations",
-    "review":"Holly is the event coordinator for the Treeline Lodge.  She has a week full of holiday events for her guests.  One guest throws her for a loop.  It's her ex-boyfriend Kevin and his two kids.  Kevin is a widower who loves to find ways to use renewable energy.  He is dumbfounded to find her at the Lodge too.  When the guests see the sparks between them, they all have a hand in getting Holly and Kevin together.  With Kevin's new job opportunity, can they get together in time?<br/><br/>Subplots are the key ingredient in this movie.  Not only are the guests focused on getting the main characters together, but they are also involved in their character growth.  You will watch this movie like a soap opera, anticipating what will happen next for each character.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"Radio Christmas",
+    "review":"Kara has to temporarily change her the location of her radio show to Bethlehem, Pennsylvania. This change derails her planned interviews. However, she wants to gain more listeners during her busiest time. Once in Bethlehem, she learns of a secret Santa who saved the town from bankruptcy. She plans on unveiling the new Santa on her radio show. With the exposed truth could potentially cost her the man of her dreams, can she fix it?<br/><br/>Starting with a strong mystery, this movie falls flat after the secret Santa is revealed. It becomes just another Christmas movie with a cute kid.<br/><br/>3 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/christmasreservations.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/christmasreservations.jpg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/radiochristmas.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/radiochristmas.jpg"
     }
   },
   {
     "option":"2",
-    "mtitle":"Two Turtle Doves",
-    "review":"Sharon goes back to her home town.  It's a bittersweet reunion.  This is the first time she has been back since her grandmother, Vera, died.  With Christmas being Vera's favorite holiday, Sharon has no idea how to celebrate the holiday without her.  Vera already put a plan in motion.  During the will reading, Vera's lawyer Sam give Sharon a tape recorder and several notebooks.  Vera recorded one final message.  Vera wants Sharon to complete 12 family traditions by Christmas.  Afterward, she can decide if she wants to keep Vera's house and stay in town.  Or sell the house and leave.  Sharon pairs an ornament with each task.  Sadly, she discovers one of the ornaments, the Two Turtle Doves, are missing.  She is on a mission to complete her task and find the ornament.  In her quest, she finds strength, community, and love.<br/><br/>Get your tissues ready.  This movie is the P.S. I Love You of Christmas.  The more time Sharon spends around Sam's daughter Michela, they bond over losing their mothers at a young age.  Your heart will break when Michela talks about her fear of forgetting her mom.  While this movie has a love story, the power of healing through tradition will stay with you.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"Holiday for Heroes",
+    "review":"Devin shares a bag of coffee his sister, Audrey sends him in a care package with Sargent Matt. So impressed by her coffee, Matt writes her a thank you note. The two become pen pals. When Matt finds out he will be spending the rest of his military days in Connecticut, he writes Audrey to tell her he will be there to see her. But the letter gets dropped. He finds her in the knick of time. She needs help putting together the Holiday for Heros dinner and Toy Drive. Since the local heroes can't come home for the holiday, the mayor decided to cancel. Now, Audrey and Matt must work together to create a holiday celebration. Matt's time with Audrey makes his next decision for his life harder than ever. He has to choose between Connecticut with her, a teaching job in Virginia, or re-enlisting for 3 more years. What will he choose?<br/><br/>Yes, you know how this one will end but troops coming home will always bring a tear to your eye. And seeing the town ban together will warm your spirit.<br/><br/>4.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/twoturtledoves.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/twoturtledoves.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/holidayforheroes.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/holidayforheroes.jpeg"
     }
   },
   {
     "option":"3",
-    "mtitle":"Your Family or Your Life",
-    "review":"Dr. Kathy refuses to accept her husband, David committed suicide.  He was happy with his life and his family.  He showed no signs of depression.  However, the toxicology report found diazepam and a high level of alcohol in his system.  With David's 16 years of sobriety, she knows that can't be right.  She reads his suicide note and it mentions the Velveteen Rabbit.  She pulls the book off the shelf and a key falls out.  This confirms her suspicions.  David's suicide note is a clue to who killed him and why.  Following the clues will put Kathy, her daughter April, and her best friend Michelle in danger.<br/><br/>When watching this movie, you know who did the crime and why.  You don't know how far each woman will go to protect themselves.  This game of chess includes verbal and physical sparring.  The body count is typical for this movie but no shovels are involved.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Was I Really Kidnapped",
+    "review":"Confused, Elle asks for help at a local gas station. Her sister reported her missing days ago, but she has no memory of where she was or who took her. When interviewing her sister, boyfriend, and therapist. Police are taking a hard look at Elle's boyfriend. The morning before she went missing, Elle turned down his marriage proposal. After talking to her therapist, police uncover Elle has a disorder that allows her to lie will simultaneously believing it. Now, police are wondering if she was ever truly kidnapped.<br/><br/>Be ready to have your mouth on the floor. With every revelation, you will fall further and further down the rabbit hole of confuses. Experienced mystery watcher will pick up on the subtle hints and guess what's going on after the 5th commercial break. However, this movie is still worth your time.<br/><br/>4.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/yourfamilyoryourlife.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/yourfamilyoryourlife.jpg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/wasireallykidnapped.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wasireallykidnapped.jpg"
     }
   },
   {
     "option":"4",
-    "mtitle":"Nostalgic Christmas",
-    "review":"Anne returns home to help her father close his toy shop.  Keith knocks on the door with his daughter Jesse.  She forgot to buy a toy for the gift exchange.  Jesse finds a wooden Santa Anne made years ago by hand.  Before a proper introduction could be made, Keith puts his foot in his mouth.  Later, Anne and Keith see each other at the town hall.  The town is trying to find someone to run the town Christmas festival.  Keith raises his hand to give his daughter a high-five and accidentally volunteers to chair the festival.  Anne raises her hand to correct the issue and she accidentally volunteers to be co-chair.  The two believe this will be a walk in the park since most of the necessities were saved from last year.  When the 40-foot tree goes missing and the music director has to leave, everything gets complicated.  Good thing they have each other.<br/><br/>Sweet at its core, this movie follows the Hallmark holiday format.  This allows the townspeople to be more interesting than the main characters.  So watch until the end to see how Keith helps the town.<br/><br/>3 out of 5 stars.",
+    "mtitle":"A Blue Ridge Mountain Christmas",
+    "review":"Willow takes a break from her job to help her sister, Juni's wedding. Juni is having a crisis. Her wedding venue is experiencing roof damage because of the snow and its unsafe to have the wedding there. Willow suggests using the family's old hotel, The Eagle Ridge. The current owner, David, says no. He no longer holds events at that hotel. When Willow finds out David is selling the hotel, she convinces David to host the wedding to impress potential buyers. He agrees as long as he doesn't have to do any work. Of course, that's a lie. While working together, they discover its hard to let go of the hotel and love.<br/><br/>Another win for cute kids. Seeing Willow connect with David’s daughter Natalie, you want them to become a couple so badly. This movie can be a favorite of the season but it won't be a classic.<br/><br/>3.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nostalgicchristmas.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nostalgicchristmas.jpg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ablueridgemountainchristmas.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ablueridgemountainchristmas.jpeg"
     }
   },
   {
     "option":"5",
-    "mtitle":"A Christmas Movie Christmas",
-    "review":"Eve Bell works for a boss that never appreciates her talent.  Lacy Bell dates a string of losers.  While Lacy would rather watch real crime T.V. shows, Eve watches holiday movies and dreams of having a perfect Christmas.  To escape their awful apartment, Lacy and Eve take a walk.  A donation Santa tells them to make a wish.  Eve wishes for a perfect Christmas like the ones on T.V. while Lacy keeps her wish to herself.  They go home and fall asleep.  In the morning, they wake up in different clothes, a different bed, and in a different house with a full face of makeup and hair done.  They head downstairs with laughable weapons in hand and find their Gram Gram making waffles.  The two girls realize they are in a Christmas movie.  Lacy is determined to go back home to her normal life.  Eve wants the movie to play out.  She wants to save the town, meet a guy, fall in love, and have a kiss that causes snow.  Eve is the star and Lacy is the sister sidekick in this plot.  Will the women be able to complete the movie and make it home?<br/><br/>One of the most original Christmas movies will have you laughing out loud in real life.  This Christmas version of Isn't It Romantic says everything you have been saying about holiday movies.  It points out the obvious plot twist and montage scenes.  Watch it with the family today.<br/><br/>5 out of 5 stars.",
+    "mtitle":"Disney The Little Mermaid Live",
+    "review":"Ariel dreams of living on the surface with humans, especially Prince Eric. After saving him, Ariel waits for Eric to regain consciousness. He sees her face and hears her voice, but when he regains his wherewithal, she is long gone. When her father, King Triton hears of her travels to the surface, he is furious. Trying to regain control, she admits she is in love with Eric. King Triton bands her from ever going to the surface. In tears, Ariel comes across Ursala. Ursula can give Ariel legs in exchange for her voice. If Ariel can get true love's kiss from Eric, she can keep her legs forever. If she fails, she will belong to Ursala forever.<br/><br/>Dinsey's next venture into live-animation only presence the music in live performances, not the entire show. The rest of the show is the 1989 classic movie. This is a far cry from what movie watchers were expecting. Shaggy should have done a voice over for a puppet if he didn't want to wear a crab suit.<br/><br/>3 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/achristmasmoviechristmas.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/achristmasmoviechristmas.jpg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelittlemermaidlive.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelittlemermaidlive.jpeg"
     }
   }
 ];
