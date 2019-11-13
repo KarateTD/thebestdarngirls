82d6db98dac377efb2d9318205d60b0293660f41
Tiffany Gray <KarateTD@yahoo.com>
2019-11-12 23:27:35 -0500

    add good boys, the farewell, and brian banks"

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,33 @@
 var mustBuy = [
     {
         "option":"1",
+        "mtitle":"Brian Banks",
+        "review":"Brian wants to continue his deferred dream of being in the NFL.  But the new law to fit paroled sex-offenders with an ankle monitor has cut that dream short again.  Seven years ago, with bad legal counsel, he pleaded no contest to rape charges.  He was told he would give probation.  Instead, he got 6 years in jail, 3 years on parole, and a lifetime on the sex offenders list.  Now on parole, Brian wants the California Innocence Project to clear his name.  Only then can he live his dream.  A ping on his computer will change his life forever.<br/><br/>Be ready to get angry and cheer.  The hoops Brian has to go through to clear his name is astonishing.  Unbelievably, he never gave up.  But with a ticking clock on his back and Jerome's words propelling him forward, Brian is determined to make it.  This story will inspire you to join in the fight with the California Innocence Project.<br/><br/>4.5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/brianbanks.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/brianbanks.jpg"
+        }
+    },
+    {
+        "option":"2",
+        "mtitle":"The Farewell",
+        "review":"Billi got devastating news.  Her grandmother, Nai Nai, has stage 4 lung cancer and has 3 months to live.  Billi wants to fly to China to see Nai Nai but her parents don't want her to come.  The family is following tradition and decides not to tell Nai Nai she is sick.  Her parents worry Billi won't be able to keep the secret.  Tradition states that people who get cancer die.  Not because of cancer but because of fear.  The family chooses to bear the burden of that fear so Nai Nai won't have to do it.  Her parents fly to China.  A few days later - shocking her family - Billi flies there too.  Billi has to walk the line between Chinese tradition and American culture.  Will she reveal the good lie?<br/><br/>Based on an actual lie, this movie points out the difference between the cultures without condemning one or the other.  Both are explained to the audience with compassion and understanding.  The movie is 90% in Mandarin.  The story is so compelling, it's like reading a great book.  Sidenote: Bring food.  The eating scenes will make you hungry.<br/><br/>4 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thefarewell.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thefarewell.jpg"
+        }
+    },
+    {
+        "option":"3",
+        "mtitle":"Good Boys",
+        "review":"Max is invited to a kissing party with the popular kids, including his crush.  Max requests for the invitation to be extended to his friends Thor and Lucas.   The cool kids decide to let them all come.  Now, Max has one big problem: he has to learn how to kiss.  After several failed attempts to learn to kiss, they use Max's dad's drone to watch Thor's neighbor kiss her boyfriend.  She spots the drone and takes it out.  Max has to get the drone back or he will be grounded and can't attend the party.<br/><br/>As a warning, if you can't handle seeing kids curse, drink, and play with adult toys, this isn't the movie for you.  This coming-of-age story is just as raunchy as Superbad.  And with everything being so over-the-top, the resolution was the most honest and heartfelt of anything in this movie.<br/><br/>4.5 out of 5 stars.",
+        "image":{
+            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/goodboys.jpg",
+            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/goodboys.jpg"
+        }
+    },
+    {
+        "option":"4",
         "mtitle":"The Art of Racing in the Rain",
         "review":"Enzo is purchased by Denny and starts the life he always thought he should have.  While he loves Denny's house, Enzo considers the race track to be his true home.  In his heart, Enzo, a dog, wants to be a racer.  Luckily, his owner has a gift for racing in the rain.  He dreams of becoming a great racer while teaching others.  Through love, babies, death, lawsuits, and joy, Enzo is there for Denny every step of the way.  And Enzo has a plan to be with Denny forever.<br/><br/>Be ready to feel every emotion possible.  This movie is told mostly from Enzo's perspective.  He has trials and tribulations like a human.  When Enzo is sad, conniving, and hurt, you will cry, laugh, and feel his pain.  Don't be surprised if you hug your family, including your dog, after this movie.<br/><br/>4 out of 5 stars.",
         "image":{
@@ -9,39 +36,12 @@ var mustBuy = [
         }
     },
     {
-        "option":"2",
+        "option":"5",
         "mtitle":"Disney's The Lion King",
         "review":"Simba is next in line for the throne.  But his Uncle Scar believes it's his rightful place.  Scar convinces Simba to go to the gorge to practice his roar.  Soon, Simba is in the middle of a stampede.  Scar runs to his brother, King Mufasa to help Simba but its a trap.  When Simba isn't looking, Scar kills Mufasa and blames the death on Simba.  Scar tells Simba to run away and never come back.  Scar promotes the lawless hyenas as his army while they lay waste to the pride.  It will take one old friend, 2 new friends, and fur to bring Simba back to the pride.<br/><br/>The live-adaption of this Disney classic stays true to the original with astounding attention to detail within mind-blowing graphics.  However, making these animals look like animals, takes away the emotions the animated characters possessed.  This forced the movie to rely on the voice actors to convey every pure emotion.  Some succeeded while others fell flat.  Also, the song \"Can You Feel the Love Tonight\" is performed midday.  It feels out of place.  Overall, the movie may not be better than the first but it is still a strong movie worth seeing.  It's better in IMAX and/or 3D.<br/><br/>4 out of 5 stars.",
         "image":{
             "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelionking.jpg",
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelionking.jpg"
-        }
-    },
-    {
-        "option":"3",
-        "mtitle":"Stuber",
-        "review":"Stu drives an Uber to help invest in his friend's spin cycle business.  Hopelessly stuck in the friend zone, Stu wishes for more.  Vic is on a path to revenge after Tedjo murders his partner.  After having the case for 6 months, Captain McHenry tells Vic the Feds are ready to take over.  Angry, Vic gets Lasik and has lunch with his daughter.  Since he can't see and she is busy with her gallery opening, she downloads Uber on his phone so he can attend.  While Vic's eyes heal, Vic gets a call from his C.I. Leon.  Leon tells Vic Tedjo has a huge shipment coming today and the call drops.  Vic tries to drive to Leon but crashes his car.  He orders an Uber to take him to Leon's shop and gets Stu.  This ride will change these men's lives forever.  But whether or not it's for the better is up to you.<br/><br/>Be ready to laugh until it hurts.  Don't make the mistake of calling this a buddy-cop movie.  Stu is not a cop and the two men are not friends.  This is a comedy with stunning action sequences and personal character growth.  This movie may not be appropriate for kids under 14.  Also, there are no post-credit scenes.  If you are looking for the best laugh you will get in the theater, Stuber is for you.<br/><br/>5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stuber.jpg"
-        }
-    },
-    {
-        "option":"4",
-        "mtitle":"Toy Story 4",
-        "review":"Nine years after saying goodbye to Bo Peep, Woody is collecting dust bunnies in Bonnie's closet.  Unlike his time with Andy, Woody is not her favorite toy and not the leader of the toys.  Woody, knowing his job is to protect Bonnie, hitches a ride in her backpack to help her with kindergarten orientation.  With Woody's helping hand, Bonnie makes Forky.  While Forky is her favorite toy, he dreams of going into the trash.  Woody spends every moment trying to save Forky from himself for Bonnie.  But when Woody and Forky are separated from Bonnie on a family trip, Woody has to bring Forky back to her.  He will need Bo Peep, Buzz, Duke Caboom, and Officer Giggle McDimples to come to his rescue.<br/><br/>Toy Story 4 is everything you thought it would be and more.  With hilarious running gags, stupendous CGI, and astounding voice acting, you are in a trance of wonderment.  Adults and children will love this story alike.  While Jessie, Rex, and Ham are background characters, the story of Woody shines through.  He is so focused on helping Forky with his existential crisis, Woody doesn't see he is having his own.  Not until Bo Peep calls him a lost toy.  While 3D isn't necessary, stay past the end credits for an oddly satisfying moment.<br/><br/>4.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/toystory4.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/toystory4.jpg"
-        }
-    },
-    {
-        "option":"5",
-        "mtitle":"Spider-Man: Far From Home",
-        "review":"After everyone was snapped back, people want the next Iron Man.  Spider-Man doesn't feel like he can step into Tony's shoes.  But Nick Fury needs him now.  The Elementals have invaded Earth.  The Wind and Earth elements were stopped by Mysterio.  With the assistance of Spider-Man, Mysterio was able to stop the water element in Venice.  Now, Nick wants Peter to go to Prague with Mysterio to stop the most powerful element, Fire.  Peter wants to enjoy his vacation with his friends, so he turns down Nick's offer.  Before Peter and his friends board the bus, the teacher informs them the trip got an upgrade and they are going to Prague.  Peter knows Fury has hijacked their vacation.  With Tony Stark's Glasses and EDITH by his side, Peter will take on Fire.  But can Peter trust what he sees?<br/><br/>While Homecoming introduced us to a younger Spider-Man, Far From Home is his coming of age story without Iron Man by his side.  He has to figure out who he wants to be as a man and a superhero.  While the CGI has gotten better, you can still pinpoint moments where you are witnessing 100% CGI versus green screen scenes.  There are 2 post-credit scenes and a cameo that original Spider-Man fans will love.  See this movie in IMAX.<br/><br/>4.5 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/spidermanfarfromhome.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/spidermanfarfromhome.jpg"
         }
     }
 ];
