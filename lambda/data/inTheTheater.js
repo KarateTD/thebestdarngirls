f9d3dac2030ed95cd9e2ccc50f938a3ac4383630
Tiffany Gray <KarateTD@yahoo.com>
2019-11-13 00:46:04 -0500

    adding midway and last christmas

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Midway",
+    "review":"Four years before the attack on Pearl Harbor Isoroku Yamamoto warned Edwin Langston if the U.S. took away oil from the Japanese, they would have no option but to retaliate. These words rang true on December 7th, 1941. After the attack, Pearl Harbor was considered the biggest intelligence failure in U.S. history. With Admiral Chester Nimitz hired to head the U.S. Pacific fleet, Nimitz will do the one thing the government refuses to do, listen to Edwin Langston. He will use the guidance of Edwin Langston and his codebreakers to change the tide of the war, U.S. intelligence, and the world.<br/><br/>The costume design and era props were expertly selected. And it was poignant to have the faces of the real men and their live's conclusion on the screen. However, the spotty CGI ruined the overall effect of the movie. However, it should still be seen on the big screen to feel the full impact.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/midway.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/midway.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Last Christmas",
+    "review":"After a life-threatening illness, Katrina is stumbling through life by couch surfing and living out of her suitcase. She works at the Yuletide store - a store that celebrates Christmas all year round. While cleaning figurines, she sees Tom looking up at something. She goes outside to discover he is looking at a rare bird. The bird poops on her face. He laughs while she is disgusted. He offers to take her on a walk but she says no. After a bad audition, she bumps into him and takes an unexpected walk with him. He only has one rule: Look Up. This walk will redirect her life.<br/><br/>This is a sweet Christmas story with a predictable ending. Once you learn her illness, it is easy to see where the story is going. While the story is compelling, it just doesn't tug at your heartstrings like it is aiming to do. This movie could have been on Lifetime. If you want to see a holiday movie, see this matinee.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lastchristmas.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/last-image/lastchristmas.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Harriet",
     "review":"After Gideon hears Minty praying for his father's death, he tells her she will be nothing more than property, just one of his pigs.  So, God will never listen to her prayers.  The next morning, Gideon's father dies.  Minty knows Gideon blames her and will make plans to sell her down south in the morning.  Slaves sold to the south never return.  Minty makes a plan to escape with her husband John.  Unlike Minty, John is free but he doesn't want to have kids with Minty because they will be born slaves.  Minty escapes and tries to meet up with John but Gideon is already confronting him.  So she keeps running.  One hundred miles later, Minty has left enslavement in Maryland to the freedom of Pennsylvania.  She meets with William Still.  He documents her story in his book of runaway slaves and tells her to pick a new name.  She chooses Harriet after her mother and Tubman like her husband John.  Now, she is Harriet Tubman.  A year later, Harriet wants to go back to get her husband and later get her family.  William is against it but her friend and businesswoman, Marie, gives her guidance on social graces, dress, and gives her free papers.  The decision to go back with turn the woman into the myth of Moses and the legend of Harriet.<br/><br/>If you are looking for a movie that focuses on the brutality and inhumane treatment of enslaved people, this is not the movie for you.  This movie focuses on Harriet's hardships to become the American Historical figure we know today.  At that time her spells were described as messages from God, now we would consider it brain damage.  This should not be mistaken as the writers giving her a superhuman quality.  This movie should be used as a jumping point for you to research the strength of this one woman.  However, the movie's end felt rushed.  It only mentions her work as a spy in the Civil War and her envolvement in the woman's suffrage movement.  Like many history books, these great strides for women are only a footnote.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Terminator: Dark Fate",
     "review":"In 1997, Sarah watches her son, John, order food.  In her peripheral, she sees a Terminator model T-800.  He pushes her out the way and kills John. Twenty-two years later, she hunts terminators.  From an encrypted number. she gets coordinates, a date, and a time a terminator will appear.  She follows the information and kills the terminator.  One day, those coordinates lead her to a bridge.  She finds Grace protecting a young woman from a Terminator and shoots.  She goes down to finish the job when Grace steals her SUV.  Sarah tracks them down and finds Grace protecting Dani.  Dani is the new Sarah and the Rev-9 is out to kill her.  Grace is an augmented human sent from the future to protect Dani's life at all costs.  The women must find a way to destroy the Rev-9 and save the future.<br/><br/>You should see the first two Terminator movies to understand what is going on in this movie.  The other movies are irrelevant.  The action sequences are well-choreographed and expertly shot with well-blended CGI.  And the new Terminator is a great blend of the T-800 and the T-1000.  However with great action, the dialog and jokes fall flat causing the movie to become boring.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,32 +36,14 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Current War",
     "review":"Thomas Edison created the light bulb and the Direct Current to bring power to people's homes.  He has asked J.P. Morgan to finance is endeavors to bring the current to homes all over the United States.  George Westinghouse sees flaws in Edison's design and creates Alternating Currents.  Westinghouse's designs are cheaper and will work in rural areas.  When Edison hears about Westinghouse's improvements, he tells the newspapers the Alternating Current can kill.  The current war begins and spans over a decade.  While Edison and Westinghouse fight over the present, Nikola Tesla is fighting for the future.  He wants to turn water into energy and power machines.  The former Edison employee has ideas and concepts but no inventions to show for it.  The winner of the current war will be decided by the Chicago World's Fair.<br/><br/>When these men couldn't fight in the courtroom, they fought in the papers.  While the current war spearheaded the United States into the 20th century, it spawned new inventions, new companies, and a deadly consequence.  Seeing these men try every underhanded approach to win, with their wives cheering them on, will delight you.  You will walk away from this movie learning more about these great inventors and their lasting impact.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecurrentwar.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecurrentwar.jpg"
     }
-  },
-  {
-    "option":"4",
-    "mtitle":"Countdown",
-    "review":"Quinn downloads a new app called Countdown out of curiosity.  The app tells its users the exact second they will die.  Quinn's clock gives her 2 days to live.  At first, she blows it off as a bad app until her patient dies the exact second the app said he would.  Quinn is starting to see and hear things.  She tries to get a new phone, new number, new carrier, and a new SIM card but the app downloads itself again.  After freaking out at a cell phone store, Matt tries to help.  He has fewer hours to live than Quinn.  The two decide to work together to beat the app and live.<br/><br/>With a mix of Final Destination and Unfriended, this movie falls short.  It sends its users a 2-minute warning before they die.  This reduces your jump scares.  One take away from this movie: Always read the user agreement.  Wait to see this movie on Vudu.  And if you do see it, wait until after the end credits.<br/><br/>2 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/countdown.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/countdown.jpeg"
-    }
-  },
-    {
-        "option":"5",
-        "mtitle":"Maleficent: Mistress of Evil",
-        "review":"After asking Aurora to marry him, Prince Phillip extends an invitation for Maleficent to meet his parents, King John and Queen Ingrith.  At the dinner, both women take verbal jabs at each other.  But Maleficent can't take it any longer.  She uses her magic to push everyone away and the king collapses and falls into a deep sleep.  The queen tells her guard, Gerda, to shoot Maleficent with a lead bullet for cursing the king.  Gerda takes the shot and Maleficent falls from the sky.  But she is saved by a dark winged creature.  This creature will show Maleficent a world she never knew existed.  Maleficent will need to decide: bring peace for the love of one human or bring war for the love of all fairies like her.<br/><br/>See this movie in IMAX.  Any other form will be cheating yourself.  Serving as a sequel, this movie gives a new story for Maleficent while respecting the original.  The CGI, make-up, and costume design are second to none.  Each fairy has a unique look but with little details to let you know they are related.  Be prepared to laugh, clutch your pearls, and gasp with amazement.  This movie is only missing a visual aspect of Ingrith's story.  It's hard to understand if she is lying or telling the truth about her past.<br/><br/>4 out of 5 stars.",
-        "image":{
-            "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/maleficentmistressofevil.jpg",
-            "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/maleficentmistressofevil.jpg"
-        }
-    }
+  }
 ];
 
 module.exports = inTheTheaters;
