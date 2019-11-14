8bd2ecc4f549b841e9a8324dbb56f5e7f92e97f1
Tiffany Gray <KarateTD@yahoo.com>
2019-11-13 23:54:06 -0500

    add mr rogers neighborhood

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -1,6 +1,15 @@
 var earlyScreening = [
   {
     "option":"1",
+    "mtitle":"A Beautiful Day in the Neighborhood",
+    "review":"Lloyd Vogel, an investigative journalist for Esquire Magazin is given his next assignment. He has to profile television star, Mr. Rogers. Vogel doesn't want to do a puff piece about a children's show host. For the heroes' issues, Mr. Rogers was the only hero willing to be interviewed by Lloyd. Lloyd is ready to do an expose on the man behind Mr. Rogers. Instead, he learns about the man who became Mr. Rogers. And a one-minute assignment will put Lloyd's life, his perspective on Mr. Rogers, and Lloyd's family into focus.<br/><br/>It's remarkable to watch his movie because it was shot as one large Mr. Roger's episode. All aerial shots are done with Mr. Roger's town models. You will see a picture-in-picture about how magazines are made. Also, you will sit will the audience for one-minute, completing a Mr. Rogers assignment, and be able to hear a pin drop. This was an amazing tribute to a beautiful man.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/abeautifuldayintheneighborhood.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/abeautifuldayintheneighborhood.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Playing with Fire",
     "review":"Superintendent Jack Carson, a.k.a. Supe, had his Alpha Team leave him for another band of smokejumpers.  Supe doesn't let this get him down.  He still has his Beta team: Rodrigo, Axe, and Mark.  When they get a fire alert, Supe uses Dr. Amy's cameras to find a log cabin on fire with movement inside.  Supe jumps into the burning cabin and rescues 3 kids: Brynn, Will, and Zoey.  With the increasing winds, Supe can't fly the children to the hospital.  Safe Haven Law says they can stay with Supe until they are reunited with their parents.  When the local news catches wind of Supe's bravery, Division Commander Richards calls him.  Richards is retiring and is considering Supe for the job.  Supe needs to apply and be prepared for a surprise inspection.  How hard can that be with 3 kids around?  Especially since one likes to steal, one likes to destroy, and one makes lethal boom-booms.<br/><br/>This movie relies on awkward moments, poop jokes, and running gags to keep you laughing.  Everything you would expect from Nickelodeon.  While this movie does provide laughs, it gives the viewers a lot of heart.  Supe and Brynn must learn to let their guards down and trust.  They will do it together.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var earlyScreening = [
     }
   },
     {
-      "option":"2",
+      "option":"3",
       "mtitle":"Knives Out",
       "review":"Benoit Blanc recieved an envelope under his door one day.  Inside was a newsclipping of Harlan Thrombey and a large sum of money.  A week ago, Harlan committed suicide after his 85th birthday party.  After sitting down with each of his family members, Benoit believes Harlan was murdered.  Now, he must figure out who, how, and why?  It comes down to lies, mud, and Watson.<br/><br/>Movies this good are hard to come by.  It is as funny as it is mysterious.  With his family members, his nurse, and his housekeeper all having a reason to kill him, this movie allows the twist and turns to be endless.  You will be guessing, laughing, and intrigued until the end.<br/><br/>5 out of 5 stars.",
       "image":{
