7dbd525afac242e1475304bd34d1c124c42de87a
Tiffany Gray <KarateTD@yahoo.com>
2019-11-18 01:24:19 -0500

    add good liar, charlies angels, ford v ferrari

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Ford v. Ferrari",
+    "review":"Henry Ford the 2nd tried to buy Ferrari with the hopes their superior racing team would boost Ford's car sells. Instead, Enzo Ferrari used Ford's interest in Ferrari to sell to Fiat at a higher price. Now Ford wants to destroy Ferrari by beating them at Le Mans, a 24-hour race. Lee Iacocca knows the engineer to build the car, Carroll Shelby. Shelby, a Le Mans winner, can't race because of a heart condition. But he knows the best racer for the job, Ken Miles. Ken hates the idea. He knows Ford won't let them do what the need to do to win. Shelby promises otherwise. It only takes one race for Shelby to realizes Ken was right and it will cost them everything.<br/><br/>Get ready to have all the feels: happiness, anger, sadness, and pure adrenaline. As Ken races, you can feel the pressure on your chest and will hold on to your armrest. This movie reminds you that accountants and corporate ruined American cars.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fordvferrari.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fordvferrari.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Charlie's Angels",
+    "review":"Elena tried to warn her boss the clean energy source she created can be hacked and used as an EMP for humans. The warning falls on death ears and her boss presents it to the CEO for release. She finds a business card for the Townsend Agency and blows the whistle on her company. In moments, she is under attack and saved by a waitress. After a bullet-ridden car chase leaves her submerged in a car, Elena is saved by Jane and Sabina. They are met by Bosley and taken to a safe house. Bosley pulls Jane and Sabina aside. She feels the assassin and too much firepower for just Elena, he must be after the Angels. The angels must protect Elena, stop Calisto from falling into the wrong hands, and save themselves. All in a day's work, Angels.<br/><br/>Based on the movie franchise and TV series, this movie is not a remake. It is a continuation of the Charlie's Angels' story. They show images and have cameos from past angels. This movie is better than expected with more focus on skills than looks. While it may not be on your gift list, it is work seeing in theaters.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/charliesangels.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/charliesangels.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"The Good Liar",
+    "review":"Brian and Estelle meet in person after creating profiles online. Brian hates lying and confesses the truth. His real name is Roy. Estelle opens up too. Her real name is Betty. They have a great first day and Betty offers to let her grandson, Steven, drive Roy home. But Roy lives close by. However, he doesn't go home. He goes to a strip club and meets with other con artists to discuss their next marks. They plan on stealing $80,000 from Russian businessmen. However, Roy plans on conning Betty too. After seeing her home and car, he believes she is work $200,000. When Roy has his partner, Vincent, come over and pretend to be a financial advisor, Roy learns she is worth 2 million. And he wants it all. The only kink in his plan is Steven, her grandson. It will take a good liar to protect Betty.<br/><br/>Based on a novel, this movie is a true cat-and-mouse game. You have a sneaking suspicion that more is going on than meets the eye. You just don’t know who and why. But when all is revealed, you will be satisfied.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegoodliar.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegoodliar.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Midway",
     "review":"Four years before the attack on Pearl Harbor, Isoroku Yamamoto warned Edwin Langston if the U.S. took away oil from the Japanese, they would have no option but to retaliate. These words rang true on December 7th, 1941. After the attack, Pearl Harbor was considered the biggest intelligence failure in U.S. history. With Admiral Chester Nimitz hired to head the U.S. Pacific fleet, Nimitz will do the one thing the government refuses to do, listen to Edwin Langston. He will use the guidance of Edwin Langston and his codebreakers to change the tide of the war, U.S. intelligence, and the world.<br/><br/>The costume design and era props were expertly selected. And it was poignant to have the faces of the real men and their live's conclusion on the screen. However, the spotty CGI ruined the overall effect of the movie. It should still be seen on the big screen to feel the full impact.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Last Christmas",
     "review":"After a life-threatening illness, Katrina is stumbling through life by couch surfing and living out of her suitcase. She works at the Yuletide store - a store that celebrates Christmas all year round. While cleaning figurines, she sees Tom looking up at something. She goes outside to discover he is looking at a rare bird. The bird poops on her face. He laughs while she is disgusted. He offers to take her on a walk but she says no. After a bad audition, she bumps into him and takes an unexpected walk. He only has one rule: Look Up. This walk will redirect her life.<br/><br/>This is a sweet Christmas story with a predictable ending. Once you learn her illness, it is easy to see where the story is going. While the story is compelling, it just doesn't tug at your heartstrings like it is aiming to do. This movie could have been on Lifetime. If you want to see a holiday movie, see this matinee.<br/><br/>2.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lastchristmas.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lastchristmas.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Harriet",
-    "review":"After Gideon hears Minty praying for his father's death, he tells her she will be nothing more than property, just one of his pigs.  So, God will never listen to her prayers.  The next morning, Gideon's father dies.  Minty knows Gideon blames her and will make plans to sell her down south in the morning.  Slaves sold to the south never return.  Minty makes a plan to escape with her husband John.  Unlike Minty, John is free but he doesn't want to have kids with Minty because they will be born slaves.  Minty escapes and tries to meet up with John but Gideon is already confronting him.  So she keeps running.  One hundred miles later, Minty has left enslavement in Maryland to the freedom of Pennsylvania.  She meets with William Still.  He documents her story in his book of runaway slaves and tells her to pick a new name.  She chooses Harriet after her mother and Tubman like her husband John.  Now, she is Harriet Tubman.  A year later, Harriet wants to go back to get her husband and later get her family.  William is against it but her friend and businesswoman, Marie, gives her guidance on social graces, dress, and gives her free papers.  The decision to go back with turn the woman into the myth of Moses and the legend of Harriet.<br/><br/>If you are looking for a movie that focuses on the brutality and inhumane treatment of enslaved people, this is not the movie for you.  This movie focuses on Harriet's hardships to become the American Historical figure we know today.  At that time her spells were described as messages from God, now we would consider it brain damage.  This should not be mistaken as the writers giving her a superhuman quality.  This movie should be used as a jumping point for you to research the strength of this one woman.  However, the movie's end felt rushed.  It only mentions her work as a spy in the Civil War and her envolvement in the woman's suffrage movement.  Like many history books, these great strides for women are only a footnote.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/harriet.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/harriet.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Terminator: Dark Fate",
-    "review":"In 1997, Sarah watches her son, John, order food.  In her peripheral, she sees a Terminator model T-800.  He pushes her out the way and kills John. Twenty-two years later, she hunts terminators.  From an encrypted number. she gets coordinates, a date, and a time a terminator will appear.  She follows the information and kills the terminator.  One day, those coordinates lead her to a bridge.  She finds Grace protecting a young woman from a Terminator and shoots.  She goes down to finish the job when Grace steals her SUV.  Sarah tracks them down and finds Grace protecting Dani.  Dani is the new Sarah and the Rev-9 is out to kill her.  Grace is an augmented human sent from the future to protect Dani's life at all costs.  The women must find a way to destroy the Rev-9 and save the future.<br/><br/>You should see the first two Terminator movies to understand what is going on in this movie.  The other movies are irrelevant.  The action sequences are well-choreographed and expertly shot with well-blended CGI.  And the new Terminator is a great blend of the T-800 and the T-1000.  However with great action, the dialog and jokes fall flat causing the movie to become boring.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/terminatordarkfate.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/terminatordarkfate.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Current War",
-    "review":"Thomas Edison created the light bulb and the Direct Current to bring power to people's homes.  He has asked J.P. Morgan to finance is endeavors to bring the current to homes all over the United States.  George Westinghouse sees flaws in Edison's design and creates Alternating Currents.  Westinghouse's designs are cheaper and will work in rural areas.  When Edison hears about Westinghouse's improvements, he tells the newspapers the Alternating Current can kill.  The current war begins and spans over a decade.  While Edison and Westinghouse fight over the present, Nikola Tesla is fighting for the future.  He wants to turn water into energy and power machines.  The former Edison employee has ideas and concepts but no inventions to show for it.  The winner of the current war will be decided by the Chicago World's Fair.<br/><br/>When these men couldn't fight in the courtroom, they fought in the papers.  While the current war spearheaded the United States into the 20th century, it spawned new inventions, new companies, and a deadly consequence.  Seeing these men try every underhanded approach to win, with their wives cheering them on, will delight you.  You will walk away from this movie learning more about these great inventors and their lasting impact.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecurrentwar.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecurrentwar.jpg"
     }
   }
 ];
@@ -52,36 +52,4 @@ module.exports = inTheTheaters;
 *  do not user '&' use the word 'and'
 *  small image 720w x 480h (in pixels)
 *  large image 1200w x 800h (in pixels)
-*/
-
-/*
-
-{
-  "option":"1",
-  "mtitle":"Ford v. Ferrari",
-  "review":".<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fordvferrari.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fordvferrari.jpg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Charlie's Angels",
-  "review":".<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/charliesangels.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/charliesangels.jpg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"The Good Liar",
-  "review":".<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegoodliar.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegoodliar.jpg"
-  }
-},
-
 */
