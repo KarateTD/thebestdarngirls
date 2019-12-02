afdf6dfc4d4ce7af778bf8d68ffb58cde566ece2
Tiffany Gray <KarateTD@yahoo.com>
2019-12-02 09:04:10 -0800

    adding 1 early and 1 in theater

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -1,6 +1,15 @@
 var earlyScreening = [
   {
     "option":"1",
+    "mtitle":"Dark Waters",
+    "review":"Wilbur Tennant walks into Robbie Bilott with a box full of evidence that DuPont is poisoning his farmland. Robbie agrees to check out the farm at the request of his grandmother. At the farm, he sees 190 dead with enlarged intestines, rotting teeth, and tumors. Robbie can't ignore the problem. So, this chemical company attorney will fight for the people. With knowledge of every underhanded trick in the book, Robbie will fight DuPont with is marriage, safety, and life on the line.<br/><br/>Based on the New York Times article, you will leave the theater wanting to be tested for P.F.O.A., also known as C-8. Hearing the number of products it’s used in and how long DuPont knew it was making its customer sick will disgust you. And your heart will break when you hear the townspeople defend their biggest employer and benefactor. You know them. What happened to them, and America, all for the sake of one company’s profits is shameful.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/darkwaters.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/darkwaters.jpg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"A Beautiful Day in the Neighborhood",
     "review":"Lloyd Vogel, an investigative journalist for Esquire Magazine is given his next assignment. He has to profile television star, Mr. Rogers. Vogel doesn't want to do a puff piece about a children's show host. For the heroes' issue, Mr. Rogers was the only hero willing to be interviewed by Lloyd. Lloyd is ready to do a piece exposing the man behind Mr. Rogers. Instead, he learns about the man who became Mr. Rogers. A one-minute assignment will put Lloyd's life, his perspective on Mr. Rogers, and Lloyd's family into focus.<br/><br/>It's remarkable to watch his movie because it was shot as one large Mr. Roger's episode. All aerial shots are done with Mr. Roger's town models. You will see a picture-in-picture about how magazines are made. Also, you will sit will the audience for one-minute, completing a Mr. Rogers assignment, and be able to hear a pin drop. This was an amazing tribute to a beautiful man.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var earlyScreening = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Playing with Fire",
     "review":"Superintendent Jack Carson, a.k.a. Supe, had his Alpha Team leave him for another band of smokejumpers.  Supe doesn't let this get him down.  He still has his Beta team: Rodrigo, Axe, and Mark.  When they get a fire alert, Supe uses Dr. Amy's cameras to find a log cabin on fire with movement inside.  Supe jumps into the burning cabin and rescues 3 kids: Brynn, Will, and Zoey.  With the increasing winds, Supe can't fly the children to the hospital.  Safe Haven Law says they can stay with Supe until they are reunited with their parents.  When the local news catches wind of Supe's bravery, Division Commander Richards calls him.  Richards is retiring and is considering Supe for the job.  Supe needs to apply and be prepared for a surprise inspection.  How hard can that be with 3 kids around?  Especially since one likes to steal, one likes to destroy, and one makes lethal boom-booms.<br/><br/>This movie relies on awkward moments, poop jokes, and running gags to keep you laughing.  Everything you would expect from Nickelodeon.  While this movie does provide laughs, it gives the viewers a lot of heart.  Supe and Brynn must learn to let their guards down and trust.  They will do it together.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var earlyScreening = [
     }
   },
     {
-      "option":"3",
+      "option":"4",
       "mtitle":"Knives Out",
       "review":"Benoit Blanc recieved an envelope under his door one day.  Inside was a newsclipping of Harlan Thrombey and a large sum of money.  A week ago, Harlan committed suicide after his 85th birthday party.  After sitting down with each of his family members, Benoit believes Harlan was murdered.  Now, he must figure out who, how, and why?  It comes down to lies, mud, and Watson.<br/><br/>Movies this good are hard to come by.  It is as funny as it is mysterious.  With his family members, his nurse, and his housekeeper all having a reason to kill him, this movie allows the twist and turns to be endless.  You will be guessing, laughing, and intrigued until the end.<br/><br/>5 out of 5 stars.",
       "image":{
--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Queen and Slim",
+    "review":"After losing a client's case, a woman finds a man on tinder. The man contacted her three weeks ago and was happy to hear from her. After the date, the two struggled over a cell phone causing the man to swerve in the road. An officer pulled them over. After an altercation, the officer shoots the woman in the leg. The man and the officer fight. The man grabs the officer's gun and shoots him. The woman tells the man they have to run. No one will understand what happened. The decision to run will affect the lives of everyone they encounter or who hears their story. They will be immortalized as Queen and Slim.<br/><br/>This whole night didn’t have to happen this way. You will become more and more exasperated as the officer and Queen keep taking the situation to a new level. While a lot is focused on their relationship’s development, you don’t learn too much about Slim. In the end, you realize you never knew the real character’s names until you hear them announced in a news report. But they will forever be known as Queen and Slim.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/queenandslim.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/queenandslim.jpg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Frozen 2",
     "review":"Fire and water disappear from Arendelle and the wind wreaks havoc on the town. Queen Elsa gets the townspeople to safety. Grand Pabbie tells Elsa the Elemental Spirits in the Enchanted Forest want the truth to come out. Queen Elsa, Princess Anna, Kristoff, Sven, and Olaf walk to the Enchanted Forest. The mist hides the truth about Elsa's powers, Anna's strength, Kristoff's true north, and Olaf's knowledge. But can they sacrifice the world they know for the forgotten past?<br/><br/>First, you must see Frozen before you see this movie. Even though Olaf does an amazing recap, he misses some of the nuances. Second, you should see this movie in IMAX, 3D, or both if you can. This Disney movie is different because their isn't a real villain. This movie focuses more on correcting Arendelle's past with the people of Northuldra. Parents will laugh at some slick jokes while having a sense of nostalgia at Kristoff's 90s power ballad styled music video. Kids will be engrossed in the action scenes. You won't regret seeing this one in the theaters.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"21 Bridges",
     "review":"Andre is called to a scene with 7 dead officers and another on life support. The store owner is dead with a gun in his hand. A volt is filled with hundreds of kilos of cocaine. Andre deduces their must have been 2 shooters but they were not prepared for the amount of cocaine in the volt or that the police officers were nearby. Andre believes something went wrong for the two shooters, now they are on the run. With so much cocaine on them, Andre knows they have to stay local to convert it into cash. He decides to trap the shooters in Manhanttan by closing the tunnels, trains, ferries, and 21 bridges. To get off the island, they have to go through him.<br/><br/>Now, the first 15 minutes is used to help you understand the type of cop Andre is and the type he isn’t. The rest of the movie is a heart-pounding race to the two shooters. But keep your eyes and ears open. Everything is a clue to a bigger plan in play.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Ford versus Ferrari",
     "review":"Henry Ford the 2nd tried to buy Ferrari with the hopes their superior racing team would boost Ford's car sells. Instead, Enzo Ferrari used Ford's interest in Ferrari to sell to Fiat at a higher price. Now Ford wants to destroy Ferrari by beating them at Le Mans, a 24-hour race. Lee Iacocca knows the engineer to build the car, Carroll Shelby. Shelby, a Le Mans winner, can't race because of a heart condition. But he knows the best racer for the job, Ken Miles. Ken hates the idea. He knows Ford won't let them do what they need to do to win. Shelby promises otherwise. It only takes one race for Shelby to realize Ken was right and it will cost them everything.<br/><br/>Get ready to have all the feels: happiness, anger, sadness, and pure adrenaline. As Ken races, you can feel the pressure on your chest and will hold on to your armrest. This movie reminds you that accountants and corporate honchos ruined American cars.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Charlie's Angels",
     "review":"Elena tried to warn her boss the clean energy source, Calisto, she created can be hacked and used as an EMP for humans. The warning falls on death ears and her boss presents it to the CEO for release. She finds a business card for the Townsend Agency and blows the whistle on her company. In moments, she is under attack and saved by a waitress. After a bullet-ridden car chase leaves her submerged in a car, Elena is saved by Jane and Sabina. They are met by Bosley and taken to a safe house. Bosley pulls Jane and Sabina aside. She feels the assassin had too much firepower for just Elena, he must be after the Angels. The angels must protect Elena, stop Calisto from falling into the wrong hands, and save themselves. All in a day's work.<br/><br/>Based on the movie franchise and TV series, this movie is not a remake. It is a continuation of the Charlie's Angels' story. They show images and have cameos from past angels. This movie is better than expected with more focus on skills than looks. While it may not be on your gift list, it is work seeing in theaters.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/charliesangels.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/charliesangels.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Good Liar",
-    "review":"Brian and Estelle meet in person after creating profiles online. Brian hates lying and confesses the truth. His real name is Roy. Estelle opens up too. Her real name is Betty. They have a great first date and Betty offers to let her grandson, Steven, drive Roy home. But Roy's home is close by. However, he doesn't go home. He goes to a strip club and meets with other con artists to discuss their next marks. They plan on stealing $80,000 from Russian businessmen. However, Roy plans on conning Betty too. After seeing her home and car, he believes she is work $200,000. When Roy has his partner, Vincent, come over and pretend to be a financial advisor, Roy learns she is worth 2 million. And he wants it all. The only kink in his plan is Steven. It will take a good liar to protect Betty.<br/><br/>Based on a novel, this movie is a true cat-and-mouse game. You have a sneaking suspicion that more is going on than meets the eye. You just don’t know who and why. But when all is revealed, you will be satisfied.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegoodliar.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegoodliar.jpg"
     }
   }
 ];
