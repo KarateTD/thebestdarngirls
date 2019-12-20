a15e2aca36fc597aeddc864d19c9703c077b2383
Tiffany Gray <KarateTD@yahoo.com>
2019-12-20 00:17:29 -0500

    add starwars

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Star Wars: The Rise of Skywalker",
+    "review":"Kylo Ren has been promised the throne to the Final Order, he only has to kill Rey. But he is warned, she is not who she appears to be. Fin, Po, and Chewbacca got a message from a First Order mole that Kylo Ren is coming for Ren and the Sith are on the rise again. They fly to General Leia and Rey to update them with the intel. Rey is still complete her Jedi training. When she gets the intel from Po and Fin, she wants to face Kylo Ren alone. Recently, Rey has been having visions of her sitting on the Sith Throne next to Kylo Ren. She fears what she may become. But her friends refuse to let her do this on her own. They have to get a Wayfinder and discover the Sith planet and put an end to the First and Final Order.<br/><br/>Extreme Star Wars fans will not like this movie. Strong Star Wars fans will pick apart its inconsistencies but applaud the effort. Casual fans will love the cinematography and plot connections with the 4th, 5th, and 6th Episodes. Action fans will appreciate the choreographed fight scenes but want more blood. Overall, this story provided a final bow for the franchise. Each character was given a solid ending to their story and you got lost in the world of Star Wars. This movie is in 3D but it's not necessary. On the other side, you should see it in IMAX. And don't forget to bring your droid hacker.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/starwarstheriseofskywalker.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/starwarstheriseofskywalker.png"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Richard Jewell",
     "review":"In 1986, Watson noticed Richard Jewell's attention to detail and nicknamed him Radar. When Richard gets a job in law enforcement, Watson gives him $100 to not become a jerk. Ten years later, Watson hears Richard is being interviewed for finding a bomb in Centennial Park. Watson is proud because Richard, labeled a hero, gives praise to everyone involved in the rescue efforts. When Richard is given book offers, he calls the only lawyer he knows, Watson. But when the FBI focuses their investigation on Richard, he will need Watson for a lot more.<br/><br/>It’s so hard to see a system Richard loves and respects so much turn on him. Richard tries to explain the FBI's actions to his family and friends. He believes they are the good guys like him. The FBI’s mistakes are sadly laughable as it is disgusting. In a day when sensationalism and clicks determine innocence and guilt, how long until another Richard Jewell is tried by the media?<br/><br/>5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Jumanji: The Next Level",
     "review":"Spencer feels like his life is in a rut so he goes back into Jumanji to feel brave.  Realizing what he has done, Beth, Anthony, and Marsha go back in to save him.  However, Bethany is left behind and Spencer's grandfather, Eddie, and Eddie's former friend, Milo are pulled into Jumanji.  With new avatars, missions, and goals, this is the next level of Jumanji.<br/><br/>Jumanji does just what the title says it will do, it goes to the next level.  Every character you know is back and more were added for fun.  It has all of the laughs and craziness you want from this franchise.  As the trailers say, not everyone will leave the game.  Jack Black is the breakout again playing 3 different characters.  Kevin Hart shows his acting chops as he plays Milo Walker.  See this movie as soon as you can.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Queen and Slim",
     "review":"After losing a client's death row case, a woman finds a man on Tinder. The man contacted her three weeks ago and was happy to hear from her. After the date, the two struggled over a cell phone causing the man to swerve in the road. An officer pulled them over. After an altercation, the officer shoots the woman in the leg. The man and the officer fight. The man grabs the officer's gun and shoots him. The woman tells the man they have to run because no one will understand what happened. The decision to run will affect the lives of everyone they encounter or who hears their story. They will be immortalized as Queen and Slim.<br/><br/>This whole night didn’t have to happen this way. You will become more and more exasperated as the officer and Queen keep taking the situation to a new level. While a lot is focused on their relationship’s development, you don’t learn too much about Slim. In the end, you realize you never knew the real character’s names until you hear them announced in a news report. But they will forever be known as Queen and Slim.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Frozen 2",
     "review":"Fire and water disappear from Arendelle and the wind wreaks havoc on the town. Queen Elsa gets the townspeople to safety. Grand Pabbie tells Elsa the Elemental Spirits in the Enchanted Forest want the truth to come out. Queen Elsa, Princess Anna, Kristoff, Sven, and Olaf walk to the Enchanted Forest. The mist hides the truth about Elsa's powers, Anna's strength, Kristoff's true north, and Olaf's knowledge. But can they sacrifice the world they know for the forgotten past?<br/><br/>First, you must see Frozen before you see this movie. Even though Olaf does an amazing recap, he misses some of the nuances. Second, you should see this movie in IMAX, 3D, or both if you can. This Disney movie is different because their isn't a real villain. This movie focuses more on correcting Arendelle's past with the people of Northuldra. Parents will laugh at some slick jokes while having a sense of nostalgia at Kristoff's 90s power ballad styled music video. Kids will be engrossed in the action scenes. You won't regret seeing this one in the theaters.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/frozen2.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/frozen2.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"21 Bridges",
-    "review":"Andre is called to a scene with 7 dead officers and another on life support. The store owner is dead with a gun in his hand. A volt is filled with hundreds of kilos of cocaine. Andre deduces their must have been 2 shooters but they were not prepared for the amount of cocaine in the volt or that the police officers were nearby. Andre believes something went wrong for the two shooters, now they are on the run. With so much cocaine on them, Andre knows they have to stay local to convert it into cash. He decides to trap the shooters in Manhanttan by closing the tunnels, trains, ferries, and 21 bridges. To get off the island, they have to go through him.<br/><br/>Now, the first 15 minutes is used to help you understand the type of cop Andre is and the type he isn’t. The rest of the movie is a heart-pounding race to the two shooters. But keep your eyes and ears open. Everything is a clue to a bigger plan in play.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/21bridges.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/21bridges.jpg"
     }
   }
 ];
