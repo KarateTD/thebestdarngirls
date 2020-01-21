b739cc7a67ba3b1e045f5633b51b33f65d771538
Tiffany Gray <KarateTD@yahoo.com>
2020-01-20 23:59:35 -0500

    add dolittle

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
- {
+  {
     "option":"1",
+    "mtitle":"Dolittle",
+    "review":"John Dolittle gained a following as the man who could talk to animals. His following grew so large, the Queen of England asked him to help her. She rewarded him with a manor to care for all animals. And the manor would be his for life. While working on the animals, he meets Lily. They fell in love and worked at the manor helping animals far and wide. One day, Lily went on a mission to find an exotic tree with a berry that could heal any ailment. On her mission, her boat was destroyed and she died. John retreated behind the walls of Dolittle Manor with a handful of animals and refused to come out again. Years later, Tommy Stubbins is forced to go hunting with his uncle and cousin. In an attempt to miss a duck, Thomas shoots a squirrel. He brings the squirrel to Dolittle Mannor for John to treat. Lady Rose and Thomas are greeted by Chee-Chee the gorilla. Overcoming his distrust of people, John’s instincts take over to save the squirrel. In amazement, Lady Rose explains her presence at Dolittle Manor. The Queen of England has fallen ill, she wants John to come and help her. If she dies, he will lose the manor. John laughs and reminds Lady Rose that he owns the manor for life. Lady Rose corrects his misunderstanding. He owns the manor for Queen Victoria’s life, not his. John’s dog, Jip, examines the Queen and smells something strange on her breath. With John’s help, Jip identifies the smell as Nightlock, a poisonous flower that kills days after consumption. John has to recover the berry his wife died trying to find to save the Queen’s life and his manor.<br/><br/>This movie is more related to the book than the other Dr. Dolittle movies. In a great shot, you go through the looking glass and see John speaking to the animals in gorilla, macaw, octopus, and dog speak as Lady Rose and Thomas look on during the squirrel’s surgery. The viewer understands Dolittle isn’t speaking English and the animals magically understand him. He is speaking their language and it’s being translated on screen. The jokes are cute but they aren’t memorable. For the parents, the movie is safe for kids and not too scary. Also, there are no songs. Stay pass the first end crawl but not to the very end. See this one in the standard format.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dolittle.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dolittle.jpg"
+    }
+  },
+ {
+    "option":"2",
     "mtitle":"Just Mercy",
     "review":"In 1987, Walter McMillian, Johnny D to his family, cleared trees and looked up to the evening sky. He had no idea it would be the last day he would look at the sky as a free man. He was arrested for the brutal murder of Ronda Morrison. After the jury sentenced Johnny D to life, the judge overrode their sentence and gave Johnny D the death penalty. In Jackson, Georgia, Bryan Stevenson is an intern meeting with death row inmates to update them about their case. After he graduates Harvard 2 years later, Bryan moves to Alabama, he gets federal funding to start the Equal Justice Initiative or E.J.I., and he hires Eva as his Director of Operations. After meeting 5 other clients, Bryan meets Johnny D. He offers to help Johnny D but Johnny D doesn’t want to hear it. Bryan refuses to quit and meets with Johnny D’s family. They all admit attending a church fish fry with Johnny D the day of the murder. After hearing Bryan met with his family, Johnny D agrees to let Bryan be his lawyer.<br/><br/>Based on Just Mercy: A Story of Justice and Redemption by Bryan Stevenson, this story is moving, painful, and honest. It speaks on the unfair scales of justice based on race, class, economics, and political standing. You will be moved to tears when Bryan’s efforts fall short for some. At first glance, you will become angry at Ralph Myers. But as the film progress, you will learn the true nature of why he has done what he has done. You will sympathize with his agonizing decision. When you watch, remind yourself: This didn’t happen during slavery or Jim Crow laws. This happened in the late 1980s, less than 40 years ago. If you are inspired, donate to the E.J.I..<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
  },
  {
-    "option":"2",
+    "option":"3",
     "mtitle":"Underwater",
     "review":"TIAN industries are drilling 7 miles to the bottom of the Mariana Trench for resources.  Nora, a mechanical engineer, brushes her teeth as the Kepler - their underwater station - around her starts to break apart.  The drilling has caused a huge earthquake causing the rig to rip apart.  She runs for safety when she catches up to Rodrigo.  They close up their hatch to prevent further damage, leaving many to perish.  As they crawl through the Kepler to find the escape pods, they find Paul in the rubble alive.  They find Captain Lucien alone and all the escape pods are gone.  Lucien, Nora, Paul, and Rodrigo make it to the control base to find Emily and Liam, biologist and engineer respectively.  After they can’t make contact with the surface, Lucien comes up with a plan.  They will put on pressurized suits, descend to the ocean floor, walk one mile to the Roebuck station, and board the escape pods.  Nora points out that the suit wasn’t designed to handle that amount of pressure.  They could die of insufficient oxygen or damaged suits.  Lucien points out that staying will ensure their death.  Leaving will give them a chance to survive.  They walk out not knowing the horror in front of them.  When TIAN drilled, they let something out.<br/><br/>With limited and predictable jump scares, this movie could have been made with real sea predators and had the same impact.  If you choose to see this movie in the theaters, you need to see it from the beginning.  The story of TIAN industries and what they are doing is covered in newspaper clippings.  During the film and in the end, you see hints for a possible prequel and sequel.  See this movie when it comes to Vudu.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
  },
  {
-    "option":"3",
+    "option":"4",
     "mtitle":"1917",
     "review":"General Erinmore wants Blake and Schofield to send a message to the 2nd battalion of the Devonshire Regiment.  The Regiment's leader,  Colonel Mackenzie, believes he has the upper hand because the Germans have retreated.  However, this was a strategic maneuver.   The Germans moved their line 9 miles back to ambush them with an attack like no other seen in the war.  Blake and Schofield must find Colonel Mackenzie and tell him to stop the attack.  If Blake and Schofield don't deliver the message in time, 1600 men - including Blake's older brother - will die.  Blake wants to leave now but Schofield tells him to wait until nightfall.  Schofield has been through this before and received a medal for his service.  Hearing his brother is in danger has lit a fire inside Blake, he is determined to go with or without Schofield now.  The two men leave the trenches to carry out their mission.<br/><br/>With well-choreographed, continuous shots, this story - derived from the director's grandfather's accounts - will leave you with awe, heartbreak, and pride.  These men conquer rats, barbed wire, tripwire, being buried alive, and the enemy with a stiff upper lip and bravery.  With each step, they are reminded of the danger that lies ahead.  They run across dead horses and dead men being eaten by scavengers and rotting away on the battlefield.  You should view this movie on the largest screen possible to enjoy its majesty.  Be warned: if you or a loved one suffer from battle-related P.T.S.D., this movie may be too much for you.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Uncut Gems",
     "review":"Howie allows Kevin Garnett to borrow a precious, uncut black opal with his championship ring as collateral. He tells Kevin he needs the opal back in time for the auction. Howie takes Garnett's ring and pawns it for gambling money. He hopes the bet will cover his losses, payback bookies, and get Kevin's ring back, so he can get the opal. Once at auction, Howie believes the Opal will fetch $1,000,000. However, someone stopped his bet. Now, he must stay one step ahead while everyone is on his tail.<br/><br/>This movie will have you on the edge of your seat from beginning to end. You have no idea which way is up the entire time of the movie. There are no good guys or bad guys in the movie, only hustlers. Each time Howie is one step ahead, he gets knocked backward. Get ready for one huge roller coaster ride.<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/uncutgems.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/uncutgems.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Spies in Disguise",
-    "review":"Lance is upset his bomb released glitter and a video of a cat. This mishap allowed Killian to scan is face and almost get away with a killer drone. Lance confronts Walter and tells him to make things that go boom. Walter believes there is a non-violent way to get things done. He offers to show Lance an invention that will allow him to disappear. Instead of inquiring about the invention, Lance fires Walter. Lance takes the drone to his boss but she discovers the drone is missing. Marcy has a video of Lance stealing it. Lance knows Killian set him up. Lance escapes and wishes he could disappear to find Killian. Lance knocks on Walter's door. Without question, Lance drinks Walter's invention and is turned into a pigeon. When Lance asks to be changed back, Walter admits he doesn't have an antidote. If Walter makes the antidote, Lance will get him re-hired. Together, they will make one strong non-killing team.<br/><br/>This movie will have you laughing while working through a tough subject. Lance believes that criminals should be taken down even with deadly force. Even has a child, Walter dreamed of making weapons to save the world but not kill. He still believes it’s possible. You only need to see this movie in 2D format, anything else is unnecessary. Parents, just a warning, there is a 15-second moment of cartoon butt and some subtitles.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/spiesindisguise.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/spiesindisguise.jpg"
     }
   }
 ];
