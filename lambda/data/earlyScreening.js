179f50bdcf556a758b6222959e3f29f40b7b19c9
Tiffany Gray <KarateTD@yahoo.com>
2020-01-30 00:09:40 -0500

    add my spy

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -1,6 +1,15 @@
 var earlyScreening = [
   {
     "option":"1",
+    "mtitle":"My Spy",
+    "review":"After killing everyone but Azar, before the C.I.A could obtain pertinent information about the plans for using stolen plutonium, J.J. is in the hot seat.  His boss, David Kim, doesn't believe J.J. is cut out for the C.I.A.  J.J. has one more chance to prove him wrong.  Victor's brother created a miniature nuclear bomb and Victor killed him for the plans.  However, Victor's brother hid the plans before Victor could find him. Victor's sister-in-law and his niece left Paris and moved to Chicago, J.J. and Bobbi - the tech - have to watch the widow and her child, in case Victor comes for them.  In Chicago, Sophie is a 9-year-old girl who is to smart for her own good. One day, her dog gives her a weird ball.  Sophie opens the ball and finds a computer chip.  She uses the internet to discover the ball is a wireless camera.  She traces the wireless connection to an apartment to find Bobbi and J.J. arguing.  She starts to record them and sess their monitors spying on their department. Sophie's father was Victor's brother.  Sophie offers to keep their secret if J.J. helps her gain popularity in school.  If J.J. doesn't do it, she will upload the video of them and their surveillance to the internet.  This will end J.J. and Bobbi's career in the C.I.A.. Over time, he will become her tutor, mentor, confidant, and protector.  And she will show J.J. there is more to life than working alone.<br/><br/>J.J. gives the strong tough guy images but he loves his fish Blueberry, sings Britney Spears, and wants soothing music to go to sleep.  Sophie is 9-years-old but she is a 2020's 9-year-old girl.  She can track WIFI connections, pass lie detectors, and use her street smarts to get in anywhere.  These two create a new buddy-kid-cop movie for this generation. Sophie isn't a helpless child. Her intelligence and acceptance is a refreshing twist on this genre.    Because of the violence - albeit mild - this movie is better suited for ages 8 and up.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myspy.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myspy.jpg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Bad Boys for Life",
     "review":"Mike is weaving through the streets of Miami with Marcus screaming in the passenger seat.  They are in a race against the clock.  But they are not chasing bad guys.  They are headed to the hospital.  Marcus is a grandfather.  Megan and Reggie just had a baby boy, they named Marcus.  Marcus takes Mike aside and tells him its time for him to retire.  He wants to see Little Marcus grow up.  In order to prove Marcus wrong, the two decide to have a foot race.  If Mike wins, Marcus stays on the force.  If Marcus wins, he can leave.  Mike gains the lead and celebrates the win.  But the celebration is short-lived.  Mike is the target of a hit.  He is rushed to the emergency room with 4 bullet wounds to the torso.  Six months later, Mike is feeling better but stunned.  While he was recuperating, Marcus officially retired.  In order for Mike to discover who is trying to kill him and why; the Bad Boys of the Bible will go head to head against a Witch, the Aretas cartel, and a doting son.<br/><br/>The third installment of the Bad Boys franchise has all the bullets, explosions, and crazy car chases you’d expect.  While Marcus has matured into his role as grandfather and husband, Mike still wants to chase bad guys.  Marcus truly makes an attempt to retire.  Even when a C.I. tries to pull Marcus back in, he calls Mike to take over.  It takes one bullet to make Marcus ride one last time with Mike.  This movie laughs at itself by describing it as a real-life telenovela.   The action sequences makes IMAX a great choice for this film.  Stay after the first set of end credits but not until the end.  Don’t worry loyal fans, they give you the slow double stand, blue pill joke, a Reggie flashback, and a set up for the next class of Bad Boys.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var earlyScreening = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Like a Boss",
     "review":"Mel and Mia achieved their childhood dream of owning a cosmetics line.  Mia is the creative side and Mel the finance side.  When Claire Luna asks to see them for a partnership, Mia says no.  But Mel tells Mia they are $437,000 in debt and can only keep their store open for 6 more months.  Mia agrees to see Claire.  Claire offers to pay their debt for 51% of the company.  If their launch goes well, Claire will give them 1.7 million dollars.  Mia says no.  She wants to be the boss.  Mel and Mia argue about the deal but Claire gives them another offer.  She will pay the debt for 49% of the company.  Giving Mia and Mel controlling interest.  If either of them quit, she will give 51%.  They agree.  While they are working for Claire, Claire is working them against each other.  Mel and Mia will have to get honest and use friendship to rise above the corporate world to be a boss.<br/><br/>This movie is a mix of The Boss and Girls Trip.  But with less business sense and less humor.  It does equally flush out the flaws in both characters and lays out their strengths.  You just don't get to see Mel have her greater moments.  Only her moments of weakness and being manipulated.  It would have been great to see her cultivate the ultimate plan and explain it to each member.  If you like a quick, easy laugh with a business 101 lesson added, see this movie matinee.  Otherwise, wait for Vudu.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var earlyScreening = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Dark Waters",
     "review":"Wilbur Tennant walks into Robbie Bilott's office with a box full of evidence that DuPont is poisoning his farmland. Robbie agrees to check out the farm at the request of his grandmother. At the farm, he sees 190 dead cows with enlarged intestines, rotting teeth, and tumors. Robbie can't ignore the problem. So, this chemical company attorney will fight for the people. With knowledge of every underhanded trick in the book, Robbie will fight DuPont with the people of Parksburg health, Wilbur's safety, and his life on the line.<br/><br/>Based on the New York Times article, you will leave the theater wanting to be tested for P.F.O.A., also known as C-8. Hearing the number of products it’s used in and how long DuPont knew it was making its customer sick will disgust you. And your heart will break when you hear the townspeople defend their biggest employer and benefactor. You know these people.  They are your family, friends, and co-workers. What happened to them, and America, all for the sake of one company’s profits is shameful.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var earlyScreening = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"A Beautiful Day in the Neighborhood",
     "review":"Lloyd Vogel, an investigative journalist for Esquire Magazine is given his next assignment. He has to profile television star, Mr. Rogers. Vogel doesn't want to do a puff piece about a children's show host. For the heroes' issue, Mr. Rogers was the only hero willing to be interviewed by Lloyd. Lloyd is ready to do a piece exposing the man behind Mr. Rogers. Instead, he learns about the man who became Mr. Rogers. A one-minute assignment will put Lloyd's life, his perspective on Mr. Rogers, and Lloyd's family into focus.<br/><br/>It's remarkable to watch his movie because it was shot as one large Mr. Roger's episode. All aerial shots are done with Mr. Roger's town models. You will see a picture-in-picture about how magazines are made. Also, you will sit will the audience for one-minute, completing a Mr. Rogers assignment, and be able to hear a pin drop. This was an amazing tribute to a beautiful man.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/abeautifuldayintheneighborhood.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/abeautifuldayintheneighborhood.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Playing with Fire",
-    "review":"Superintendent Jack Carson, a.k.a. Supe, had his Alpha Team leave him for another band of smokejumpers.  Supe doesn't let this get him down.  He still has his Beta team: Rodrigo, Axe, and Mark.  When they get a fire alert, Supe uses Dr. Amy's cameras to find a log cabin on fire with movement inside.  Supe jumps into the burning cabin and rescues 3 kids: Brynn, Will, and Zoey.  With the increasing winds, Supe can't fly the children to the hospital.  Safe Haven Law says they can stay with Supe until they are reunited with their parents.  When the local news catches wind of Supe's bravery, Division Commander Richards calls him.  Richards is retiring and is considering Supe for the job.  Supe needs to apply and be prepared for a surprise inspection.  How hard can that be with 3 kids around?  Especially since one likes to steal, one likes to destroy, and one makes lethal boom-booms.<br/><br/>This movie relies on awkward moments, poop jokes, and running gags to keep you laughing.  Everything you would expect from Nickelodeon.  While this movie does provide laughs, it gives the viewers a lot of heart.  Supe and Brynn must learn to let their guards down and trust.  They will do it together.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/playingwithfire.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/playingwithfire.jpg"
     }
   }
 ];
