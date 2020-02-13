d82d7f9ef3d353522a7c67da2ab7264e2d25b7b6
Tiffany Gray <KarateTD@yahoo.com>
2020-02-13 11:40:26 -0500

    fix image url

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -3,7 +3,7 @@ var videoOnDemand = [
 		"option":"1",
 		"mtitle":"Bombshells and Dollies",
 		"review":"Every year, thousands flock to Las Vegas to enjoy Viva Las Vegas Rockabilly Weekend to enjoy the fashion, cars, and music of the 1950s.  For 12 women this celebration of culture means much more.  They are here for the 18th Annual Miss Viva Las Vegas Pinup Contest.  Women for every corner of the globe style their hair, clothes, and makeup to resemble that of a 1950's pinup girl with the hopes of winning the crown. Each woman has a journey that led them to the pinup lifestyle.  Like the pinup girls in the 1950s, these women aren't oversexualized, objectified, or demeaned.  They are revered for the femineity and strength.  They are business owners, contractors, moms, philanthropist, bloggers, and fashion designers. They were proud to wear work boots and heels without judgment or loss of their womanhood.   Anyone, anywhere, any body type can be a pinup girl.<br/><br/>During this documentary, everyone is careful not to say pageant but contest.  'Pageant' has developed a negative connotation because it only covers one body type.  This is a contest, not a pageant.  For these women, the road to pinup has been riddled with personal struggles and growth.  Some have suffered or are suffering from eating disorders, fibromyalgia, and teen suicide.  Pinup was a building block to gain perseverance and awareness of their cause. As these women tell their stories, it's hard not to cry and identify with a struggle. This documentary proves the World of Pinup is for those who have struggled, been outcasted, and been underrepresented.  So have your tissues nearby so your cat doesn't smudge, you will need it.<br/><br/>4.5 out of 5 stars.",
-		"image:":{
+		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bombshellsanddollies.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bombshellsanddollies.jpeg"
 		}
@@ -13,8 +13,8 @@ var videoOnDemand = [
 	    "mtitle":"Marriage Killer",
     	"review":"After an injury, Morgan learns about a new adult club from Isabelle.  She politely declines.  At a dinner party later, Isabelle mentions the club around Morgan's husband, Robert.  He wants to see what the fuss is about and convinces Morgan to go.  The two decide the club isn't for them but make love in the car on the ride home.  Isabelle weasels her way into their lives until the 3 make love.  The morning after, Isabelle is in love with the couple but they don't reciprocate.  Heartbroken, she will do anything to be a part of their family.<br/><br/>This movie maybe too spicy and too real for Lifetime.  The plot is wild but it is grounded in truth based on the public backlash.  In today's world, you rarely go beyond the video tape.<br/><br/>4 out of 5 stars.",
 	    "image":{
-    	    "smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/marriagekiller.png",
-        	"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/marriagekiller.png"
+    	    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/marriagekiller.png",
+        	"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/marriagekiller.png"
   	  }
 	},
 	{
@@ -22,8 +22,8 @@ var videoOnDemand = [
 		"mtitle":"Pretty Rosebud",
 		"review":"Cecilia feels constricted with her structured and religious life, so she uses affairs as a release.  This is a tale of how ignoring your happiness can lead to a road of self-destruction and how loving yourself can save you.<br/><br/>3.5 out of 5 stars",
 		"image":{
-			"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/prettyrosebud.jpg",
-			"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/prettyrosebud.jpg"
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/prettyrosebud.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/prettyrosebud.jpg"
 		}
 	},
 	{
@@ -31,8 +31,8 @@ var videoOnDemand = [
 		"mtitle":"Betrayed",
 		"review":"When the Russian mob kidnaps the wrong type of victims for sex slavery, Mayor Alderman decides to stop looking the other way and cut them off.  His daughter will pay the price.  This bloody tale has enough twist and turns to keep you entertained.<br/><br/>4 out of 5 stars",
 		"image":{
-			"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/betrayed.jpg",
-			"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/betrayed.jpg"
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/betrayed.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/betrayed.jpg"
 		}
 	},
 	{
@@ -40,8 +40,8 @@ var videoOnDemand = [
 		"mtitle":"Killer Island",
 		"review":"Ashley and Mike are trying to gain back the trust in their relationship.  But when Ashley finds a bracelet, this beautiful island will be exposed as a den for killers.  Great movie to rent and kill a few hours.  This will be a future Lifetime movie.<br/><br/>3 out of 5 stars.",
 		"image":{
-			"smallImageUrl":"https://s3.amazonaws.com/thebestdarngirls/small-image/killerisland.jpg",
-			"largeImageUrl":"https://s3.amazonaws.com/thebestdarngirls/large-image/killerisland.jpg"
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/killerisland.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/killerisland.jpg"
 		}
 	}
 ];
