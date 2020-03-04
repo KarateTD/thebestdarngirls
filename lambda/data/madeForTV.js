84534abecebf26eaa255a7d300ed7e2299463215
Tiffany Gray <KarateTD@yahoo.com>
2020-03-03 22:31:32 -0500

    2 in theater, 4 tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Dying to Be You",
+    "review":"Thirteen years ago, Lilly was assigned a penpal for school.  The students were told to write to each other for the entire school year.  After the year was over, they could decide if they wanted to keep writing or not.  Lilly was assigned Molly in Nebraska.  The girls wrote, emailed, and later, friended each other online.  One day, Molly went radio silent.  When Lilly was ready to cut off the friendship, Molly finally wrote back and wants to be friends again.  Lilly is happy to reconnect.  A few days later, Lilly arrives home to a surprise birthday party thrown by her boyfriend Matthew.  Matthew’s biggest surprise, Molly.  Molly flew from Nebraska to Seattle to have a real face-to-face friendship.  Molly is overstaying her welcome and Lilly wants her out.  Molly is dressing like her, coming on to Matthew, and Lilly’s things are going missing.  Lilly has no idea how unhinged Molly can get.<br/><br/>Unlike most Lifetime movies, Lilly isn’t clueless.  The first moment Molly steps out of line, Lilly calls her on it.  She has both eyes open and questions everything Molly does.  When things don’t line up, Lilly starts to do her homework.  It’s not long before she is telling Molly to leave her home and putting up other boundaries.  Don’t miss the first 5 minutes.   It is the complete setup for the movie and getting to know the characters.  Also, see if you can spot another stereotype this movie breaks.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dyingtobeyou.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dyingtobeyou.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"A Killer in My Home",
+    "review":"Ryan and his wife, Allison celebrate their daughter, Hollie’s college acceptance with a big party.  After a beautiful speech, Ryan has a heart attack.  At the hospital, the nurse tells Allison and Holly there is nothing they can do for Ryan.  As they sit by Ryan’s bedside, a woman and a young man walk into the room.  Ryan tells Allison and Hollie the young man is his son Joshua and his mother Jenna.  He makes a deathbed confession to having an affair with Jenna.  And he asks Allison to take care of Joshua when he is gone.  Stunned, Allison agrees.   Allison got Jenna and Joshua jobs at her country club but Jenna needs a place to stay for a while.  Allison agrees to let her and Joshua live in the guest house for Ryan’s sake.  Hollie hates the idea.  Jenna is grateful for the help.  When Allison is robbed and Hollie is accused of plagiarism, Hollie suspects their house guest.  But Allison believes it someone else.  Who is behind it all?<br/><br/>With a kidnapping to open the movie, you not sure where the movie is going next.  This movie has plenty of misdirections to make you think twice but your first hunch is always right.  While the why is obvious, it’s who and how many are involved that will get your attention.  While you will feel for Allison and Hollie, your heart will break for Joshua.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/akillerinmyhome.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/akillerinmyhome.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Riddled with Deceit: Martha's Vineyard Mysteries",
+    "review":"Chief Madieras asks Jeff to give his expert security knowledge on a pricey situation.  The Emerald broach is worth $5 million and has a rich back history of disownment, displacement, and a curse.  The night before, someone tried to steal the broach.  One thief got away but the other is in ICU.  Britt, the rightful owner of the broach, will display it by wearing it for a week.  Then, she will sign it over to the Smithsonian.  She locks the broach up with a key padlock and fingerprint scanner until the next day.  Later, Britt calls Zee.  She fears someone is in her house.  Zee tells her to call the cops and leave.  When Zee and Jeff arrive at Britt’s home, she is unconscious and bleeding.  With Britt in the hospital, Jeff checks on the broach.  He discovers it’s missing.  They must find the broach before anyone else is hurt.<br/><br/>Based on Vineyard Deceit: Martha’s Vineyard Mystery by Philip Craig, this movie is a mystery within a mystery.  As Zee and Jeff look for the brooch, people are dying left and right.  They worry it could be Britt’s cousin Lloyd, the hotel waiter Rodney, Willard, or the previous brooch owners Freddie and Regina.  While searching for a killer, they have to solve the mystery of the brooch's location.  It is hidden within a riddle that will take them from the land to the sea and back.  You will be on the edge of your seat every step of the way and happily surprised with the location of the emerald.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/riddledwithdeceitamarthasvineyardmystery.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/riddledwithdeceitamarthasvineyardmystery.jpg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Vision of Love",
+    "review":"Kat is a divorce attorney on the fast track to becoming a partner at her firm.  While she is juggling clients, she is the maid of honor for her best friend, Marcy’s wedding.  With love in the air, Marcy wants Kat to have love in her life too.  So, Marcy takes them to get their nails done.  Marcy’s nail technician, Dora, is a psychic.  Dora predicts the love of her life will be named Michael.  Later that night, Rick and Marcy introduce Kat to the best man Tom.  Tom is a novelist who writes love stories.  This first book was based on the diary his wife left after she died.  As Tom and Kat work together to plan the perfect rehearsal party, souvenirs, and make-up dates, Kat is still looking for her Michael.  As she finds them, is it the right Michael for her.<br/><br/>Seeing Kat on dates, with a variety of dud Michaels, will have you cringing.  From a ventriloquist to a jerk, Kat dates them all.  When she runs screaming in the street for a Michael, she runs into her high school sweetheart.  Aptly named Michael.  However, you don’t want this to work because the chemistry between Kat and Tom is so strong.  And rest assured, you will be happy with the ‘Michael’ Kat gets in the end.  And it’s not who you think.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/visionoflove.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/visionoflove.jpg"
+    }
+  },
+  {
+    "option":"5",
     "mtitle":"Love in Store",
     "review":"Terrie and David host segments for Shopping at Home Network with distinct styles.  Terrie displays a perfect persona with facts.  David is carefree with inspiration.  They often compete for segments and products.  The network’s top seller, Sharon, has great news.  She is retiring.  Her prime time slot, monthly pick, and spot as head of the network are up for grabs.  Sharon lets them know they are already in the running.  Before stepping on set, Terrie gets some bad news.  Her co-host, Savannah was put on on bedrest until she has her baby.  Terrie offers to do her segments alone, but Sharon believes everyone needs a co-host.  She picks David.  Now they have to work together to compete for the same job.  Too bad love is standing in the way.<br/><br/>With nods to Balsam Hills, an umbrella, and Charlie Brown, this movie will have you laughing.  Terrie and David have a low blow battle of the sexes while keeping it professional.  When Terrie feels in control, her boyfriend, Noah, causes her to have dead air for 10 seconds.  Noah breaks a cardinal relationship rule.  As Terrie and David’s romance is building, a new job, Noah, and the network stand in love’s way.  While the storyline is predictable; however, Terrie and David’s banter makes the movie watchable.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveinstore.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveinstore.jpg"
-    }
-  },
-  {
-    "option":"2",
-    "mtitle":"Picture Perfect Mysteries: Dead Over Diamonds",
-    "review":"Camille is having a gallery showing for James Risand’s collection.  Among his items are rare photographs, a Nazareth dagger, and the Manchester Diamond Necklace.  During the showing, James is upset the dagger is locked inside a glass case and requests Camille to open it so attendees can see it.  Against her better judgment, she opens it but tells James’s brother he must stay with it along with a security guard.  Allie, commissioned by Camille to take photos, takes pictures of everyone in attendance.  Camille asks James to speak about his collection to the attendees.  As James speaks the lights flicker, then Mr. White starts having heart problems.  Soon the entire gallery hears Camille’s screams.  The Manchester Diamond Necklace is missing.  Every person is searched and released.  Detective Sam arrives on the case and at ease with catching the thief.  The necklace was surrounded by 8 cameras.  Feeling he has the case solved, he asks Allie out to coffee.  However, the camera footage for 2 minutes is missing.  Allie plans on staying out until she talks to Camille’s son and her assistant, Noah.  Noah tells Allie that James is planning on ruining Camille’s reputation if the necklace is not recovered.  In the small art community, if a reputation is ruined, a gallery will fail.  Allie has to help Camille before the gallery closes.  And Sam has to find the thief before Allie is hurt.<br/><br/>For all the Perfectionist and Markies, this mystery will pull you in and won’t let go.  Every clue is a step closer to the thief but still a misdirection.  You will need to keep a notepad, or a collage, nearby to keep up with all the suspects.  Now dedicated fans of H.M.M. will believe they have the case all wrapped up but you don’t know how wrong you are, yet right at the same time.  It’s this contradiction that will keep you intrigued and keeping score. So get your DVRs ready for the big reveal.  Also, you will get a glimpse of why Sam left New York and its a tough, heartfelt watch.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/pictureperfectmysteriesdeadoverdiamonds.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/pictureperfectmysteriesdeadoverdiamonds.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Twist of Fate",
-    "review":"Kelly is an interior designer with bad luck.  After her fiance cheated on her, she put all her focus into her work.  Kelly gathers her sketches for a proposal and accidentally puts her car in gear.  She crashes into a ditch and gets stuck under the passenger side dashboard.  The airbag deployment triggers Autocall.  She is connected with Jeff.  Jeff left advertising to find a more rewarding job.  He hoped to help people in their times of crisis.  When he talks to Kelly, he forgoes his training and keeps her calm by sharing personal details about himself.  They bond and laugh about movies and books.  Then, Kelly realizes she can free herself.  They hang up the line but not their heart.  With every friendship, job, walk, and blind date, a twist of fate is trying to pull them together.  But will lying by omission, tear them apart.<br/><br/>You root for this couple because they have amazing chemistry and heartbreaking backstories.  Each time they come close, you will be so excited.  Each missed opportunity will dash your hopes of them ever meeting.  When they come face to face and realize who the other is, Jeff’s past comes to light.   After all the ups and downs, you don’t want them to fall apart because of a misunderstanding.  Hang in there, they have one more unforeseen opportunity to work it all out.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/twistoffate.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/twistoffate.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"The Secret Ingredient",
-    "review":"Kelly runs the Cake Factory in Bailey’s Fork, North Carolina.  While preparing for the day, she gets an order for a 4-Layer Honey Almond Cake.  She knows it can only be for one person.  Her ex-fiance Andrew.  Kelly agrees to make the cake but hopes her best friend and co-worker, Sarah can deliver it.  But Sarah has plans, so Kelly has to make the delivery.  Soon Kelly is face to face with her past.  They make polite conversation and then she leaves.  The next day, Mineet, host of Four Square Cooking Show, comes into the Cake Factory with a camera crew.  A mystery person has chosen Kelly to compete on the special Valentine’s edition of the show.   She will be competing against 3 other chefs but will never see them.  While touring New York with Brenda – her show’s confidant and handler, she runs into Andrew.  In the competition, the sparks between her and Andrew reignite.  However, Kelly doesn’t know that Andrew is in the competition and vice versa.  It will take a very special person and cake to bring the secret to the light.<br/><br/>This yummy romantic tale will make you salivate.  While you know the ending, you anxiously wait for the shoe to drop.  You see them trying to mend their relationship while the competition can cut it like scissors.  Watch this when you have 2 hours to kill and some cupcakes nearby.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesecretingredient.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesecretingredient.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"You Can't Take My Daughter",
-    "review":"Amy celebrates passing the bar with friends and classmates, Owen and Letty.  When Owen’s trainer, Demetri, buys them a round of shots, Amy doesn’t drink it. They share a cab to head home.  Demetri lets Amy out of the cab but asks to come in for a nightcap.  She tells him no and goes home.  Later that night, she hears a knock at her door and assumes it’s her neighbor Chickie.  She opens the door and sees Demetri.  He beats and rapes her.  Since she knows her attacker, the police decide a rape kit isn’t necessary.  Amy is dumbfounded by their inability to help her.  Then, Demetri threatens her in a public parking lot in broad daylight.  After her restraining order, Amy discovers she’s pregnant.  She believes in a woman’s right to choose and she chooses to have her child.  When the cops finally question Demetri, they let it slip that Amy is pregnant.  Demetri ignores the restraining order and comes to Amy’s home with a gun.  He threatens to kill her and the unborn baby if she doesn’t drop the rape charge.  Amy decides she can’t wait for the police to protect her and her baby and moves to Georgia to start a new life.   Years later, Amy is the proud and over-protective mother of Maddy.  She is served with custody papers.  Demetri wants custody and visitation of Maddy.  Since the rape charge happened in another state, it can’t be brought up in the custody case.  Amy’s fight will launch a change for all rape survivors and their children.<br/><br/>Be ready to scream and throw things at the screen after hearing every horrific statistic and every hurdle Amy has to jump over to keep her child safe.  Inspired by the true story of Analyn Megison, the Rape Survivor Child Custody Act was signed in 1995.  While this act was a huge step for survivors, Analyn is highly aware that most of the work has to be done at the state level.  So, check your state laws and be involved in making a change.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/youcanttakemydaughter.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/youcanttakemydaughter.jpg"
     }
   }
 ];
