38eff5f17340e39b1387a43e2b033fb9d347b6bb
Tiffany Gray <KarateTD@yahoo.com>
2020-03-08 21:31:49 -0400

    add emma and the way back

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"The Way Back",
+    "review":"Jack Cunningham drinks to wake up, in the shower, on the job, and a case to go to sleep at night.  Jack checks his messages and gets one from his old high school principal.  The principal wants him to coach the new boys' basketball team for Bishop Hayes High School.  The team hasn't had a good year since Jack played in 1995. The principal tells Jack to go home, think about it, and give him an answer in the morning.  Jack practices turning down the principal while drinking a case of beer.  With all his practice, Jack shows up the next morning. Jack sits on the sidelines and watches the boys play.  Jack can't fathom having so few players on the team.  They have to borrow players from the J.V. team just to practice.  Jack sees their best shooter can't make a 3-pointer and the best guy on the team doesn't talk. Their way back to the playoffs is his way back to life.  When his past comes back to haunt him, will the old Jack resurface?<br/><br/>This feels like an episode of Intervention without the confessionals.  You don't have a true understanding of how bad Jack's drinking is until he drinks a case of beer in a few hours.  There is a moment of parallel in the storyline. Marcus is begging to stay on the team after Jack kicks him off for being late and disobedient.  By the end of the film, Jack's drinking leads him to a car crash, breaking and entering, and missed practices.  He has to beg to coach the team like Marcus.  Seeing him beg for a second chance to fix his life will rock you to the core.<br/><br/>3.5 out of 5 stars.",
+    "iamge":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewayback.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewayback.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Emma.",
+    "review":"Emma Woodhouse is a 21-year-old woman with no plans to marry.  She is described as handsome, clever, and rich.  She believes women only marry for money, employment, or consequence.  In her 21 years, she has never been distressed or vexed.  However, Emma loves playing matchmaker. When her friend, Harriet comes to visit, she comes with good news.  She met a suitor named Mr. Martin.  Mr. Martin wants to marry Harriet but Emma tells her he isn't good enough.  She must find a man that is above her station.  So Harriet refuses his proposal.  Emma believes Mr. Elton is better suited for Harriet and makes arrangements for them to meet. While Emma thinks she is doing Harriet a service, Mr. Knightley counters her.  He thinks Emma is spoiled, vain, and selfish. Also, he hates seeing Emma wait for Mr. Churchill to return her affection. Mr. Knightley believes Mr. Churchill is stringing Emma along.  Emma continues to wait but she will be challenged when Harriet has eyes for another man ... Mr. Knightley.  If that isn't enough, Mr, Churchill pops in for a surprise visit.  Then her unknowing rival Jane Fairfax is coming to visit.  Leaving Emma in the middle of two men with Harriet and Jane Fairfax standing in her way.<br/><br/>Based on Emma by Jane Austen, this latest adaptation is funny and accurate in the clothing, set design, and manners.  While you won't be doubled over in laughter, you will find a quick chuckle with side-eye glances, polite looks of disgust, and subtle verbal jabs with extreme etiquette. This is a good matinee movie if you like period pieces or Jane Austen. Everyone else should wait for Vudu.<br/><br/>3 out of 5 stars.",
+    "iamge":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/emma.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/emma.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"The Invisible Man",
     "review":"Cecilia has put her plan into place.  As Adrian sleeps, she moves the camera, turns off the alarm, and tracks him in bed on her phone.  She climbs over the cement barrier, runs through the woods and waits for Emily.  As she gets in Emily’s car, she drops the Diazepam she used to drug Adrian and gets in the car.  Emily drives off leaving Adrian behind.  Adrian finds the bottle on the ground and heads home.  Two weeks later, Cecilia is hiding out in James’s home with his daughter Sydney.  Walking to the mailbox causes Cecilia enormous anxiety.  She knows Adrian is waiting for her to let down her guard.  Emily tells her to relax because Adrian committed suicide.  Cecilia breathes a sigh of relief.  The next day, Cecilia gets a letter from Adrian’s brother Tom.  Tom is the executor of Adrian’s estate.  Adrian left Cecilia $5 million to be paid monthly in $100,000 installments without income or estate taxes.  One night, Cecilia gets a feeling that someone is around but sees no one.  She brushes it off and goes to sleep.  Cecilia is awakened to notice her covers on the floor at the foot of the bed.  She pulls the covers but they are stuck on something.  She stares to see footprints appear out of nowhere.  She snaps them and hears something fall to the floor and run. James tells her it’s all in her mind.  Then she remembers his threat.  If you leave me, I will find you.  You won’t see me but I will leave something behind to let you know I was there.  While no one else believes her, she knows Adrian has faked his death and found a way to make himself invisible.<br/><br/>That feeling of someone being in the room, someone watching you, or someone listening to you is a feeling we all know too well.  As the camera pans to an empty spot in the room, you will swear you can see Adrian standing there.  Being invisible allows each attack to be shocking.  This modern twist on The Invisible Man novel is a mix of The Invisible Man movie and Enough.  With strong acting, amazing special effects, and killer designs, the plot feels believable.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Seberg",
     "review":"In May 1968, Jean Seberg sits in first class with her manager, Walt, discussing her next role.  As they talk, Jean can hear a man yelling at the stewardess.  Hakim Jamal is angry Betty Shabazz has to sit in a tiny coach seat.  She is the widow of Malcolm X and Hakim believes she should be treated like royalty.  Jean is intrigued by his stance and wants to know more about him.  As they leave the plane, she sees him put a fist in the air with others from the Nation of Islam.  As the press snaps photos, Seberg stands with them in solidarity.  Agents Jack Solomon and Carl Kowalski follow Hakim to his home and listen to his evening prayers through planted listening devices.  As they watch and listen, they see Jean’s car drive up to Hakim’s home.  Hakim excuses his parishioners and talks to Jean about the movement and her potential role.  Carl and Jack listen to them start an affair and record every moment.  They see she has given money to the NAACP and Black Panthers; they label her a sympathizer and start a dossier on her.  They plant bugs in her home, follow her across the globe, and leak her life to the tabloids.  Because of her relationship with Hakim, Jean is now the focus of a COINTELPRO investigation.<br/><br/>It’s disturbing to watch Jean slowly unravel under the scrutiny and lies of the FBI.  She wanted to be known for more than the characters she played on the screen.  She wanted to use her fame to bring light to the issues she believed were hurting the country.  This movie makes you ask, how far is too far.  At what point does an investigation become predatory.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Call of the Wild",
     "review":"Buck is a dog who is too big for the house and too tame for the wild. After Buck destroys Judge Miller's important business meal, he makes Buck sleep outside.  During the night, a man calls Buck's name and offers him food.  When Buck runs to the food and eats it, the man traps him inside the box.  After days in the box, Buck wakes up in Yukon, Alaska. After escaping his abusive captures, Buck is bought by Perrault and Françoise.  They run a mail route through the Yukon river with sled dogs.  Perrault believes Buck will fit in nicely but Françoise doesn't.  Before Buck starts his journey he bumps into John. He travels for peace of mind.  His son died and John has not been able to cope.  Buck realizes John dropped his harmonica and runs to John to return it.  John heads off on a ferry up north and Buck starts his journey as a sled dog.  This chance encounter is the start of a call to the wild for them both.<br/><br/>Based on The Call of the Wild by Jack London, this adaptation is a feast for the eyes.  The CGI animators were able to give Buck full expressions without taking away his canine qualities.  The actors work well within the blank spaces and have great cued interaction with Buck. The movie is tame and full of heart.  Just like Buck.  While you won't add this movie to your collection.  It is worth seeing in the theaters to enjoy the beauty of Alaska and Buck.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecallofthewild.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecallofthewild.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Downhill",
-    "review":"Peter, Billie, and their two sons are ready to enjoy their ski vacation in Austria. For Peter, this getaway means a lot since his father passed away 8 months ago. After a day on the slopes, the family goes to a mountainside restaurant for lunch.  As they mull over what to have, they see snow coming down the hill fast.  At first, they watch in awe.  But as it comes too close for comfort, Peter grabs his phone and runs.  Billie stays with the two boys and hunkers down.  After the snow clears, Peter comes back, sits down, and orders soup.  Billie is in disbelief.  She believes Peter abandoned his family to die.  Peter believes he saw they were ok and ran to get help.  Now, their marriage and vision of fatherhood are in limbo.<br/><br/>The Force Majeure by Ruben Östlund remake ramps up to important talks about marriage, family, and duty.  But it just falls flat at the end with even more questions.  While the leads give a strong performance, the script doesn't deliver the storytelling necessary to keep you engaged.  This movie should have gone straight to streaming or DVD, not the theaters.<br/><br/>2 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/downhill.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/downhill.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Photograph",
-    "review":"Michael travels to New Orleans to talk to people still affected by the hurricane.  During this interview with Isaac, he comes across several photos.  One photo is Christine, his former flame.  At some point, Isaac and Christine had a hot and heavy romance.  But Christine left for New York to pursue her dream of photography, leaving Isaac behind.  Isaac’s only regret is not going after her.  Michael can feel a story brewing in this tale of lost love and goes home to New York.  He finds Christine’s daughter Mae.  Mae tells Michael her mother died of cancer a few months ago.  But there is more to the story.  Christine left Mae a safe deposit box with 2 letters and photographs.  One envelope has Mae’s name, the other is blank.  Michael can’t get Mae off his mind and attends a museum event he knows she will be attending.  He tries to be smooth and ask her out.  They go out for dinner; then, talk about music and romance.  After the date, they see each other regularly.  While Mae feels she is doomed to repeat her mother’s mistakes, Michael believes their love can make it.<br/><br/>With nods to Kanye, Drake, and Kendrick Lamar, this movie tells the tale of two love stories.  One between Isaac and Christine and the other between Mae and Michael.  Changes in music, technology, and clothes help you bounce from one time period to another.  With each love story, you get the same message.  Mae and Christine aren’t bad at love; they are bad at communication.  This is a good date matinee movie.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thephotograph.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thephotograph.jpg"
     }
   }
 ];
