414e992b6dfbbf5fe0d5f0f21a92f822dc8260ba
Tiffany Gray <KarateTD@yahoo.com>
2020-03-16 20:00:49 -0400

    add 3 tv and 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Bloodshot",
+    "review":"Ray wakes up with no memory of his past and Dr. Emil by his bedside.  His body was donated by the military to Rising Spirit Technologies or R.S.T. because his family didn’t claim his body.  Ray is R.S.T’s first fully revived candidate.  Ray feels normal but Dr. Emil cuts his hand and puts it under a microscope.  Ray can see nanites repairing his cut before it has a chance to bleed.  Now, Ray is superhuman.  KT sees Ray is having a hard time adjusting so she offers to help Ray remember which alcohol he likes.  As they take shots, Psycho Killer is played over the speaker.  Ray doesn’t know the killer was planted in his head.  Dr. Emil is using Ray to eliminate former RST employees that are a threat to his business.  When Ray discovers the truth, he will need to gain control of his new augmented body and bring down his creator.<br/><br/>Based on the Bloodshot comics, this movie doesn’t hold a candle to the comic.  The plot is a mix of Deadpool and Frankenstein but it still doesn’t excite you.  The graphics are cartoonish, the action mundane, and the plot overdone.  Thank goodness for the comic relief Wilfred.  This lovable computer genius is the wingman or ‘guy in the chair’ every superhero needs.<br/><br/>2 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bloodshot.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bloodshot.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"I Still Believe",
+    "review":"Jeremy Camp leaves home with a brand new guitar for Calvary Chapel Bible College.  After some time on campus, he sneaks backstage to hear and get tips from musician Jean-Luc.  Not only did Jean-Luc give Jeremy tips, but he also let Jeremy tune his guitar.  As Jeremy switches out the guitar, he sees a beautiful girl in the crowd.  After the concert, he introduces himself to Melissa.  The next day, she sees him play guitar under a tree.  She invites him to a bonfire and tells him to bring his guitar.  At the bonfire, Jeremy is surprised to see Jean-Luc there with Melissa.  She explains her relationship with Jean-Luc is complicated.  Jean-Luc wants more from their friendship but she doesn't.  Unknown to Jean-Luc, Jeremy and Melissa start to date.  The couple breaks up after a few months because Jeremy is tired of being a secret.  During winter break, Jeremy gets a phone call from Jean-Luc.  Melissa was taken to the hospital and it’s bad.  Melissa tells Jeremy she has stage C3 cancer.  They removed a cancerous growth, the size of an orange, from her stomach and she will start chemotherapy soon.  To keep the love of his life, Jeremy asks Melissa to marry him.   He is inspired to write songs for and ask fans to pray for his wife.  With those decisions, Jeremy will find the miracle before the supernova.<br/><br/>Based on I Still Believe by Jeremy Camp, this movie is a moving tribute to Melissa and the legacy she wanted to leave behind.  She wanted Jeremy to use her story to make people believe in faith and hope, not lose it during hard times.  It may not have the same outcome as Breakthrough, it does give you a powerful love story within a deeper love story.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/istillbelieve.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/istillbelieve.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"The Way Back",
     "review":"Jack Cunningham drinks to wake up, in the shower, on the job, and a case to go to sleep at night.  Jack checks his messages and gets one from his old high school principal.  The principal wants him to coach the new boys' basketball team for Bishop Hayes High School.  The team hasn't had a good year since Jack played in 1995. The principal tells Jack to go home, think about it, and give him an answer in the morning.  Jack practices turning down the principal while drinking a case of beer.  With all his practice, Jack shows up the next morning. Jack sits on the sidelines and watches the boys play.  Jack can't fathom having so few players on the team.  They have to borrow players from the J.V. team just to practice.  Jack sees their best shooter can't make a 3-pointer and the best guy on the team doesn't talk. Their way back to the playoffs is his way back to life.  When his past comes back to haunt him, will the old Jack resurface?<br/><br/>This feels like an episode of Intervention without the confessionals.  You don't have a true understanding of how bad Jack's drinking is until he drinks a case of beer in a few hours.  There is a moment of parallel in the storyline. Marcus is begging to stay on the team after Jack kicks him off for being late and disobedient.  By the end of the film, Jack's drinking leads him to a car crash, breaking and entering, and missed practices.  He has to beg to coach the team like Marcus.  Seeing him beg for a second chance to fix his life will rock you to the core.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Emma.",
     "review":"Emma Woodhouse is a 21-year-old woman with no plans to marry.  She is described as handsome, clever, and rich.  She believes women only marry for money, employment, or consequence.  In her 21 years, she has never been distressed or vexed.  However, Emma loves playing matchmaker. When her friend, Harriet comes to visit, she comes with good news.  She met a suitor named Mr. Martin.  Mr. Martin wants to marry Harriet but Emma tells her he isn't good enough.  She must find a man that is above her station.  So Harriet refuses his proposal.  Emma believes Mr. Elton is better suited for Harriet and makes arrangements for them to meet. While Emma thinks she is doing Harriet a service, Mr. Knightley counters her.  He thinks Emma is spoiled, vain, and selfish. Also, he hates seeing Emma wait for Mr. Churchill to return her affection. Mr. Knightley believes Mr. Churchill is stringing Emma along.  Emma continues to wait but she will be challenged when Harriet has eyes for another man ... Mr. Knightley.  If that isn't enough, Mr. Churchill pops in for a surprise visit.  Then her unknowing rival Jane Fairfax is coming to visit.  Leaving Emma in the middle of two men with Harriet and Jane Fairfax standing in her way.<br/><br/>Based on Emma by Jane Austen, this latest adaptation is funny and accurate in the clothing, set design, and manners.  While you won't be doubled over in laughter, you will find a quick chuckle with side-eye glances, polite looks of disgust, and subtle verbal jabs with extreme etiquette. This is a good matinee movie if you like period pieces or Jane Austen. Everyone else should wait for Vudu.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Invisible Man",
     "review":"Cecilia has put her plan into place.  As Adrian sleeps, she moves the camera, turns off the alarm, and tracks him in bed on her phone.  She climbs over the cement barrier, runs through the woods and waits for Emily.  As she gets in Emily’s car, she drops the Diazepam she used to drug Adrian and gets in the car.  Emily drives off leaving Adrian behind.  Adrian finds the bottle on the ground and heads home.  Two weeks later, Cecilia is hiding out in James’s home with his daughter Sydney.  Walking to the mailbox causes Cecilia enormous anxiety.  She knows Adrian is waiting for her to let down her guard.  Emily tells her to relax because Adrian committed suicide.  Cecilia breathes a sigh of relief.  The next day, Cecilia gets a letter from Adrian’s brother Tom.  Tom is the executor of Adrian’s estate.  Adrian left Cecilia $5 million to be paid monthly in $100,000 installments without income or estate taxes.  One night, Cecilia gets a feeling that someone is around but sees no one.  She brushes it off and goes to sleep.  Cecilia is awakened to notice her covers on the floor at the foot of the bed.  She pulls the covers but they are stuck on something.  She stares to see footprints appear out of nowhere.  She snaps them and hears something fall to the floor and run. James tells her it’s all in her mind.  Then she remembers his threat.  If you leave me, I will find you.  You won’t see me but I will leave something behind to let you know I was there.  While no one else believes her, she knows Adrian has faked his death and found a way to make himself invisible.<br/><br/>That feeling of someone being in the room, someone watching you, or someone listening to you is a feeling we all know too well.  As the camera pans to an empty spot in the room, you will swear you can see Adrian standing there.  Being invisible allows each attack to be shocking.  This modern twist on The Invisible Man novel is a mix of The Invisible Man movie and Enough.  With strong acting, amazing special effects, and killer designs, the plot feels believable.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvisibleman.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvisibleman.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Seberg",
-    "review":"In May 1968, Jean Seberg sits in first class with her manager, Walt, discussing her next role.  As they talk, Jean can hear a man yelling at the stewardess.  Hakim Jamal is angry Betty Shabazz has to sit in a tiny coach seat.  She is the widow of Malcolm X and Hakim believes she should be treated like royalty.  Jean is intrigued by his stance and wants to know more about him.  As they leave the plane, she sees him put a fist in the air with others from the Nation of Islam.  As the press snaps photos, Seberg stands with them in solidarity.  Agents Jack Solomon and Carl Kowalski follow Hakim to his home and listen to his evening prayers through planted listening devices.  As they watch and listen, they see Jean’s car drive up to Hakim’s home.  Hakim excuses his parishioners and talks to Jean about the movement and her potential role.  Carl and Jack listen to them start an affair and record every moment.  They see she has given money to the NAACP and Black Panthers; they label her a sympathizer and start a dossier on her.  They plant bugs in her home, follow her across the globe, and leak her life to the tabloids.  Because of her relationship with Hakim, Jean is now the focus of a COINTELPRO investigation.<br/><br/>It’s disturbing to watch Jean slowly unravel under the scrutiny and lies of the FBI.  She wanted to be known for more than the characters she played on the screen.  She wanted to use her fame to bring light to the issues she believed were hurting the country.  This movie makes you ask, how far is too far.  At what point does an investigation become predatory.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/seberg.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/seberg.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Call of the Wild",
-    "review":"Buck is a dog who is too big for the house and too tame for the wild. After Buck destroys Judge Miller's important business meal, he makes Buck sleep outside.  During the night, a man calls Buck's name and offers him food.  When Buck runs to the food and eats it, the man traps him inside the box.  After days in the box, Buck wakes up in Yukon, Alaska. After escaping his abusive captures, Buck is bought by Perrault and Françoise.  They run a mail route through the Yukon river with sled dogs.  Perrault believes Buck will fit in nicely but Françoise doesn't.  Before Buck starts his journey he bumps into John. He travels for peace of mind.  His son died and John has not been able to cope.  Buck realizes John dropped his harmonica and runs to John to return it.  John heads off on a ferry up north and Buck starts his journey as a sled dog.  This chance encounter is the start of a call to the wild for them both.<br/><br/>Based on The Call of the Wild by Jack London, this adaptation is a feast for the eyes.  The CGI animators were able to give Buck full expressions without taking away his canine qualities.  The actors work well within the blank spaces and have great cued interaction with Buck. The movie is tame and full of heart.  Just like Buck.  While you won't add this movie to your collection.  It is worth seeing in the theaters to enjoy the beauty of Alaska and Buck.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecallofthewild.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecallofthewild.jpg"
     }
   }
 ];
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Black Widow Killer",
+    "review":"udy, deputy mayor, was served with divorce papers from her husband Steven.  She tries to protect her daughter, Abby, but the high school senior knows everything.  Judy tells Abby not to get too attached to her high school sweetheart, Daniel.  Later, Judy goes out with her friend, Kendra, to celebrate joining the divorced wives club.  The next morning, Steven calls Judy.  Kendra was found dead in her home.  The autopsy lists the cause of death as heart failure.  Judy doesn’t believe it but Steven tells her to stay out of it.  Judy can’t let it go.  She swears the night they went out Jason Hall was following them.  She believes Jason Hall had something to do with Kendra’s death.  When the toxicology report comes back positive for poison, Judy is relentless in her pursuit of Jason.  But does she have the right man?<br><br/>When this movie is done, be prepared to have no sympathy for any of the characters.  As they are picked off one by one, you see the reason for the killer’s revenge and rage.  Seeing the part all 4 played, you believe they deserve to be punished.  Experienced Lifetimers will see the killer coming from a mile away.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackwidowkiller.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackwidowkiller.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"No Good Deed Goes Unpunished",
+    "review":"Karen went to the drug store to pick up some last minutes items. Sophie, Karen’s sister-in-law, watches Karen’s son, Max, while she shops.  It was a mundane trip until a man comes in demanding fentanyl patches.  He points the gun at Jeremy and Karen jumps in front of him.  In seconds, the gun flashes, Karen and Jeremy drop to the ground, and police are apprehending the robber.  Karen gives her statement and heads home to Max.  Jeremy tries to talk to her but can’t.  And the police won’t tell him anything about her.  Out of nowhere, Jeremy approaches Karen on the sidewalk.  He thanks her for saving his life and offers to buy her dinner.  When she declines, he offers lunch instead.  She doesn’t know how ill he is and what he will do to repay the favor.   It will take a computer virus, sneakers, coffee, a box of extortion, and a freezer for Karen to see she is in trouble.<br/><br/>This movie is all about bullies, no matter if they are children or adults.  It’s unnerving to see Karen stand up to a man with a gun but not a tenant who won’t pay or rude co-worker.  Max and Jeremy have the same issue.  They are consistently bullied by the people around them.  When helping Karen doesn’t work, he decides to become a savior.  While the actors did an amazing job, the ending falls flat.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nogooddeedgoesunpunished.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nogooddeedgoesunpunished.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Tomboy",
+    "review":"Tori is one of the best brand and marketing strategists at her firm.  As a girl, Tori played baseball and hung out with her best friend Adam.  Now, adult Tori and Adam are still friends and coach a baseball team with Adam’s niece, Rocky, as a player.  Tori  has never been a girly-girl.  She hates makeup, dresses, and heels.  One day, the firm is chosen to create a new campaign for Shelley Sweat cosmetics.  Tori’s boss, Randy, gives the campaign pitch to her because she is a woman.  Reluctantly, Tori takes the campaign.  During the research, she feels focus groups aren’t enough.  With the help of her friend and office manager, Krista, Tori gets a head to toe makeover.  The hot guy in the office and Tori’s crush, Brian takes notice.  As she keeps up the facade, Adam challenges her: Change back and see if Brian still likes you.  This tomboy turned girly-girl will get a response she never expected, leading her to find her true self and true love.<br/><br/>Don’t be mistaken, Tori isn’t the only one who changes in this movie.  Krista, a self-professed girly-girl, changes for a guy too.  Accept she pretends to like sports and sidewalk hotdogs.  Seeing who these women are, on the inside, will cause their men to have two different reactions.  One woman is accepted and the other is not.  The movie is funny and has merit, however, it’s a plot we know well.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/tomboy.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/tomboy.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"My Nightmare Landlord",
     "review":"Lydia looks for a new apartment to leave her obsessive, alcoholic boyfriend Tim for good.  Before a potential landlord can sign the lease, Tim shows up and ruins the deal.  Lydia turns Tim down again and keeps looking for a new place.  She walks into a building and sees Drew fixing a light.  He offers an apartment with smart controls.  Drew leaves and Lydia gets settled in her new place.  Lydia juggles a job while going to school.  So when she gets a leak in her apartment, Drew is already there to fix it.  After fixing the leak, she offers him a drink. She has no idea how Drew could be so perfect for her.  But he has an ace up his sleeve.  He set up cameras in her apartment and watches her all the time.   As long as she is nice, he will be a dream.  But Lydia pumps the breaks when he goes too far, causing him to become a nightmare.<br/><br/>This movie does shine a light on Scotophobia and its causes but the plotline is a bit overdone.  Now, the best aspect of this film is that Lydia has an idea of what's wrong with Drew.  She can see he is coming unhinged and puts an end to the relationship.  She doesn't make excuses or blames herself, she tells him to leave and immediately puts up boundaries.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Fame at a Deadly Cost",
     "review":"Blake is a smart girl whose intelligence is only appreciated by her boyfriend Mason.  Everyone else at school calls her a nerd.  She records herself singing and playing her guitar.  She sends the video to Mason.  As she sits with her mom, her phone's notifications come in quicker than a jackrabbit in heat.  She clicks on the link and sees the video of her singing.  And it has gone viral.  At school, she confronts Mason.  He admits to posting the video.  He wanted the school to see her talent.  Mason isn't the only person who takes notice.  Rachel, a talent agent, saw the video and wants to sign Blake. Blake begs Michelle to give her a chance and she agrees.  Blake starts going to parties, getting a fake ID, and meeting producers.  One, in particular, is D.C. Conway.  D.C. has written hit songs for several artists and thinks Blake will be the next big thing.  While she is recording, Blake starts to cut off her friends and family.  Next, D.C. asks her to get a passport. Before Michelle returns home, someone breaks into their home.  They don't notice Blake's passport was the only thing stolen.  D.C. convinces Blake to leave home and record a record in Mexico.  After she leaves, Michelle learns about the dark side of D.C. and his Mexican villa.  With a text message, a reporter, a kind stranger, and a near-death experience, Michelle will do anything to get her daughter back.<br/><br/>Thank goodness for the music.  Blake's singing and playing are not dubbed nor lip-synced.  The actress puts her heart and soul into each song. You will be dancing in your seat or feeling deep compassion for human trafficking victims.  While the plot feels like something from Surviving R. Kelly, it's the music that will stick with you.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fameatadeadlycost.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fameatadeadlycost.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"His Fatal Fixation",
-    "review":"Lilly and her boyfriend fought off her stalker.  While her boyfriend died, she was able to push her stalker, Spencer out of a window.  But not before he cut her face.  Six months later, Lilly has a noticeable scar and goes by the name Stella.  At her plastic surgery appointment, she wows Dr. Michael Marlon and is P.A. Joyce with her knowledge of surgical procedures.  After her surgery to fix her Hypertrophic scar, Stella is asked to fill in for Joyce. Stella is happy to take the job.  At first, everything is going great.  However, Stella is having an affair, is labeled a pill popper, and causes strife in the office.  While the office is suspicious of her, she's worried Spencer is back and coming after everyone she cares about at work.<br/><br/>With such a short timeframe between Lilly becoming Stella, its obvious who Spencer is and is not.  This fact offers little to no suspense in this film.  Now, the way Stella a.k.a. Lilly gets into Spenser's mind to save the day is thought-provoking.  The last 15 minutes will grab your attention.<br/><br/>2 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hisfatalfixation.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hisfatalfixation.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Secret Millionaire",
-    "review":"Sean Peyton's father, William, has given him an ultimatum.  Sean must get and keep a job for one week or he will be forced to work for his father's company.  The thought of working in real estate is mind-numbing to Sean.  But if he doesn't do it, his father will cut him off.  So he heads to his car and unlocks it.  He sees a young woman jump from behind his car.  She assumes he is a valet and tells him to get down.  Allison is a law student, an activist, and works part-time in a food truck called C’est si Bon.  She was papering cars with flyers to save the Fountain Green Park.  The Peyton Real Estate company wants to bulldoze the park for a strip mall.  Sean keeps up the ruse and helps Allison escape the country club. She worries he will get fired because of her stunt.  So she offers him a job as a sous chef on the food truck.  As he spends time with her, Sean starts to fall for her and she for him.  But when the truth of his lineage comes out, will she stay with him?<br/><br/>While this ending is predictable, the story arc of the character's growth is a good watch.  Seeing that Sean has a connection to the park drives his need to save it while proving himself to his father.  Allison also learns to forgive.  In a world of activism, its easy to focus on black and white, Sean helps her see the gray.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretmillionaire.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretmillionaire.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Bad Date Chronicles",
-    "review":"Leigh writes a blog for the Richard Records called the Bad Date Chronicles.   People, mostly women, tell her about their bad dates and she writes about them with colorful commentary.  Their boss and editor, Allison believes they are ready to expand but nothing too flowery.  Leigh leaves work early to head on a date with Conner. After a date that almost left the roof on fire, Leigh writes about it for her blog.  Conner reads the article and is dumbfounded by Leigh's side of the story.  So he writes a rebuttal telling his side.  Allison sees the readership skyrocket.  When she discovers Conner works for her long time rival and Peterson Post Editor, Milo, she sets up a meeting.  Allison sits with Milo and they come up with an idea.  They will have Conner and Leigh go out on 3 dates.  Whichever writer attracts the most readers, the editor will win $2. However, as Allison and Milo ramp up the stakes, Conner and Leigh's dates get crazier.  It will take a rumor and a post to ruin 3 relationships.<br/><br/>This movie is as funny as it is eye-opening.  Just because someone has a different perspective, it doesn't make them wrong.  On the first date, Leigh thought he skipped out on her and left her with the bill.  However, Conner got locked in the bathroom and couldn't call for help.  On the second date, Conner thought Leigh was uninterested because she was paying attention to her phone.  However, she was being set up by her boss. This is a movie you will fall in love with and laugh with over and over again.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/baddatechronicles.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/baddatechronicles.jpg"
     }
   }
 ];
