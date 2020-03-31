300aeb20fc710fcd3610e36ba148e25d591ad6db
Tiffany Gray <KarateTD@yahoo.com>
2020-03-31 18:40:44 -0400

    update in stores

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,24 @@
 var mustBuy = [
   {
     "option":"1",
+    "mtitle":"Star Wars: The Rise of Skywalker",
+    "review":"Kylo Ren has been promised the throne to the Final Order, he only has to kill Rey. But he is warned, she is not who she appears to be. Fin, Po, and Chewbacca got a message from a First Order mole that Kylo Ren is coming for Rey and the Sith are on the rise again. They fly to General Leia and Rey to update them with the intel. Rey is still completing her Jedi training. When she gets the intel from Po and Fin, she wants to face Kylo Ren alone. Recently, Rey has been having visions of her sitting on the Sith Throne next to Kylo Ren. She fears what she may become. But her friends refuse to let her do this on her own. They have to get a Wayfinder, discover the Sith planet, and put an end to the First and Final Order.<br/><br/>Extreme Star Wars fans will not like this movie. Strong Star Wars fans will pick apart its inconsistencies but applaud the effort. Casual fans will love the cinematography and plot connections with the 4th, 5th, and 6th Episodes. Action fans will appreciate the choreographed fight scenes but want more blood. Overall, this story provided a final send off for the franchise. Each character was given a solid ending to their story and you got lost in the world of Star Wars. This movie is in 3D but it's not necessary. But, you should see it in IMAX. And don't forget to bring your droid hacker.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/starwarstheriseofskywalker.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/starwarstheriseofskywalker.png"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Current War",
+    "review":"Thomas Edison created the light bulb and the Direct Current to bring power to people's homes.  He has asked J.P. Morgan to finance is endeavors to bring the current to homes all over the United States.  George Westinghouse sees flaws in Edison's design and creates Alternating Currents.  Westinghouse's designs are cheaper and will work in rural areas.  When Edison hears about Westinghouse's improvements, he tells the newspapers the Alternating Current can kill.  The current war begins and spans over a decade.  While Edison and Westinghouse fight over the present, Nikola Tesla is fighting for the future.  He wants to turn water into energy and power machines.  The former Edison employee has ideas and concepts but no inventions to show for it.  The winner of the current war will be decided by the Chicago World's Fair.<br/><br/>When these men couldn't fight in the courtroom, they fought in the papers.  While the current war spearheaded the United States into the 20th century, it spawned new inventions, new companies, and a deadly consequence.  Seeing these men try every underhanded approach to win, with their wives cheering them on, will delight you.  You will walk away from this movie learning more about these great inventors and their lasting impact.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecurrentwar.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecurrentwar.jpg"
+    }
+  },  
+  {
+    "option":"3",
     "mtitle":"1917",
     "review":"General Erinmore wants Blake and Schofield to send a message to the 2nd battalion of the Devonshire Regiment.  The Regiment's leader,  Colonel Mackenzie, believes he has the upper hand because the Germans have retreated.  However, this was a strategic maneuver.   The Germans moved their line 9 miles back to ambush them with an attack like no other seen in the war.  Blake and Schofield must find Colonel Mackenzie and tell him to stop the attack.  If Blake and Schofield don't deliver the message in time, 1600 men - including Blake's older brother - will die.  Blake wants to leave now but Schofield tells him to wait until nightfall.  Schofield has been through this before and received a medal for his service.  Hearing his brother is in danger has lit a fire inside Blake, he is determined to go with or without Schofield now.  The two men leave the trenches to carry out their mission.<br/><br/>With well-choreographed, continuous shots, this story - derived from the director's grandfather's accounts - will leave you with awe, heartbreak, and pride.  These men conquer rats, barbed wire, tripwire, being buried alive, and the enemy with a stiff upper lip and bravery.  With each step, they are reminded of the danger that lies ahead.  They run across dead horses and dead men being eaten by scavengers and rotting away on the battlefield.  You should view this movie on the largest screen possible to enjoy its majesty.  Be warned: if you or a loved one suffer from battle-related P.T.S.D., this movie may be too much for you.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Jumanji: The Next Level",
     "review":"Spencer feels like his life is in a rut so he goes back into Jumanji to feel brave.  Realizing what he has done, Beth, Anthony, and Marsha go back in to save him.  However, Bethany is left behind and Spencer's grandfather, Eddie, and Eddie's former friend, Milo are pulled into Jumanji.  With new avatars, missions, and goals, this is the next level of Jumanji.<br/><br/>Jumanji does just what the title says it will do, it goes to the next level.  Every character you know is back and more were added for fun.  It has all of the laughs and craziness you want from this franchise.  As the trailers say, not everyone will leave the game.  Jack Black is the breakout again playing 3 different characters.  Kevin Hart shows his acting chops as he plays Milo Walker.  See this movie as soon as you can.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Richard Jewell",
     "review":"In 1986, Watson noticed Richard Jewell's attention to detail and nicknamed him Radar. When Richard gets a job in law enforcement, Watson gives him $100 to not become a jerk. Ten years later, Watson hears Richard is being interviewed for finding a bomb in Centennial Park. Watson is proud because Richard, labeled a hero, gives praise to everyone involved in the rescue efforts. When Richard is given book offers, he calls the only lawyer he knows, Watson. But when the FBI focuses their investigation on Richard, he will need Watson for a lot more.<br/><br/>It’s so hard to see a system Richard loves and respects so much turn on him. Richard tries to explain the FBI's actions to his family and friends. He believes they are the good guys like him. The FBI’s mistakes are sadly laughable as it is disgusting. In a day when sensationalism and clicks determine innocence and guilt, how long until another Richard Jewell is tried by the media?<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/richardjewell.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/richardjewell.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Spies in Disguise",
-    "review":"Lance is upset his bomb released glitter and a video of a cat. This mishap allowed Killian to scan is face and almost get away with a killer drone. Lance confronts Walter and tells him to make things that go boom. Walter believes there is a non-violent way to get things done. He offers to show Lance an invention that will allow him to disappear. Instead of inquiring about the invention, Lance fires Walter. Lance takes the drone to his boss but she discovers the drone is missing. Marcy has a video of Lance stealing it. Lance knows Killian set him up. Lance escapes and wishes he could disappear to find Killian. Lance knocks on Walter's door. Without question, Lance drinks Walter's invention and is turned into a pigeon. When Lance asks to be changed back, Walter admits he doesn't have an antidote. If Walter makes the antidote, Lance will get him re-hired. Together, they will make one strong non-killing team.<br/><br/>This movie will have you laughing while working through a tough subject. Lance believes that criminals should be taken down even with deadly force. Even has a child, Walter dreamed of making weapons to save the world but not kill. He still believes it’s possible. You only need to see this movie in 2D format, anything else is unnecessary. Parents, just a warning, there is a 15-second moment of cartoon butt and some subtitles.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/spiesindisguise.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/spiesindisguise.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Uncut Gems",
-    "review":"Howie allows Kevin Garnett to borrow a precious, uncut black opal with his championship ring as collateral. He tells Kevin he needs the opal back in time for the auction. Howie takes Garnett's ring and pawns it for gambling money. He hopes the bet will cover his losses, payback bookies, and get Kevin's ring back, so he can get the opal. Once at auction, Howie believes the Opal will fetch $1,000,000. However, someone stopped his bet. Now, he must stay one step ahead while everyone is on his tail.<br/><br/>This movie will have you on the edge of your seat from beginning to end. You have no idea which way is up the entire time of the movie. There are no good guys or bad guys in the movie, only hustlers. Each time Howie is one step ahead, he gets knocked backward. Get ready for one huge roller coaster ride.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/uncutgems.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/uncutgems.jpg"
     }
   }
 ];
@@ -66,16 +66,7 @@ module.exports = mustBuy;
         }
     }
 
-    {
-    "option":"5",
-    "mtitle":"The Current War",
-    "review":"Thomas Edison created the light bulb and the Direct Current to bring power to people's homes.  He has asked J.P. Morgan to finance is endeavors to bring the current to homes all over the United States.  George Westinghouse sees flaws in Edison's design and creates Alternating Currents.  Westinghouse's designs are cheaper and will work in rural areas.  When Edison hears about Westinghouse's improvements, he tells the newspapers the Alternating Current can kill.  The current war begins and spans over a decade.  While Edison and Westinghouse fight over the present, Nikola Tesla is fighting for the future.  He wants to turn water into energy and power machines.  The former Edison employee has ideas and concepts but no inventions to show for it.  The winner of the current war will be decided by the Chicago World's Fair.<br/><br/>When these men couldn't fight in the courtroom, they fought in the papers.  While the current war spearheaded the United States into the 20th century, it spawned new inventions, new companies, and a deadly consequence.  Seeing these men try every underhanded approach to win, with their wives cheering them on, will delight you.  You will walk away from this movie learning more about these great inventors and their lasting impact.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecurrentwar.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecurrentwar.jpg"
-    }
-  }  
-
+    
     {
     "option":"1",
     "mtitle":"Frozen 2",
@@ -83,16 +74,6 @@ module.exports = mustBuy;
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/frozen2.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/frozen2.jpeg"
-    }
-  }
-
-    {
-    "option":"3",
-    "mtitle":"Star Wars: The Rise of Skywalker",
-    "review":"Kylo Ren has been promised the throne to the Final Order, he only has to kill Rey. But he is warned, she is not who she appears to be. Fin, Po, and Chewbacca got a message from a First Order mole that Kylo Ren is coming for Rey and the Sith are on the rise again. They fly to General Leia and Rey to update them with the intel. Rey is still completing her Jedi training. When she gets the intel from Po and Fin, she wants to face Kylo Ren alone. Recently, Rey has been having visions of her sitting on the Sith Throne next to Kylo Ren. She fears what she may become. But her friends refuse to let her do this on her own. They have to get a Wayfinder, discover the Sith planet, and put an end to the First and Final Order.<br/><br/>Extreme Star Wars fans will not like this movie. Strong Star Wars fans will pick apart its inconsistencies but applaud the effort. Casual fans will love the cinematography and plot connections with the 4th, 5th, and 6th Episodes. Action fans will appreciate the choreographed fight scenes but want more blood. Overall, this story provided a final send off for the franchise. Each character was given a solid ending to their story and you got lost in the world of Star Wars. This movie is in 3D but it's not necessary. But, you should see it in IMAX. And don't forget to bring your droid hacker.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/starwarstheriseofskywalker.png",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/starwarstheriseofskywalker.png"
     }
   }
 
