e8d98ba1f71d4178547dc357dc049031fae491cd
Tiffany Gray <KarateTD@yahoo.com>
2020-04-11 13:44:24 -0400

    add trolls world tour

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Trolls World Tour",
+		"review":"Poppy is enjoying her reign as a queen with dignity and song.  Branch still lives in his bunker.  He has fallen in love with Poppy and doesn’t know how to tell her.  As the trolls celebrate the birth of Tiny Diamond, Poppy gets a letter from a rock star.  Queen Barb of the Rock Trolls plans to come to Pop Village and create one nation under Rock.  King Peppy reveals the Troll’s history to Poppy.  Many years ago, there were 6 six strings that played different types of music: Pop, Country, Classical, Rock, Techno, and Funk.  Over time, they became intolerant of each other’s music and started to fight.  Each troll musical faction took their string and left.  oppy refuses to run and wants to protect her kingdom.  She fires up a hot air balloon to find Queen Barb and the other factions.  Then, have a party to unite all the trolls with her pop sound.  Branch sees it differently.  He believes that Queen Barb is ready for a fight and prepares to fight back. So, he reluctantly joins her.Meanwhile, Queen Barb has gotten Poppy’s reply of friendship and she is disgusted.  She wants to unite the strings to make all the trolls Rock Zombies by playing the Ultimate Power Chord.  When every troll is the same, no one will fight.  As both Queens start their mission, they will learn the beauty of individuality, respect, and how history is written.<br/><br/>Based on Good Luck Trolls and the sequel to Trolls, this rainbow blast includes music for all ages and genres like Dee-Lite, Cyndi Lauper, Beethoven, Spice Girls, George Clinton, Baha Men, PSY, Chic, Scorpions, Daft Punk, and more.  Unlike other movies that teach children togetherness, it praises individuality through a groove, a riff, or a yodel.  With this storyline, you get 2 subplots.  The first subplot, Branch is finding a way to express his love for Poppy.  The second subplot, Cooper is finding his real family and music faction.  As a strong spectacle of entertainment, it surpasses the first movie and leaves you wanting more movies in the future.<br/><br/>5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/trollsworldtour.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/trollsworldtour.jpg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"Never Rarely Sometimes Always",
 		"review":"Autumn sings He’s Got The Power at the school talent show when someone screams vulgar comments at her.  Autumn has more than a song on her mind.  She skips school to get a pregnancy test at the women’s clinic.  While the doctor warns her of false-negative results, Autumn’s test is positive.  Her next appointment reveals she is 10-weeks pregnant.  Her reaction to the news concerns the receptionist.  The receptionist asks her about adoption and Autumn doesn’t see that as an option.  Then, the receptionist asks Autumn if she is abortion-minded.  When Autumn’s silence says yes, the receptionist plays her a pro-life video.  She goes home and starts looking up alternatives online.  Autumn doesn’t want her parents to know about the pregnancy so she tries to cause a miscarriage.  Nothing works.  She goes to work and her cousin Skylar sees she is sick.  She finally admits she is pregnant.  Skylar steals money from work and the cousins go to New York for Autumn's abortion.<br/><br/>At first, it’s hard to believe these to girls can go across state lines without their parents noticing.  When you discover they are being sexually harassed, verbally tormented, and sexually active, it’s easy to see the abject neglect they have lived through.  Also, you and Autumn have no idea of the pain she has suffered until she answers those hard-hitting questions for the first time.  As Autumn goes through the physical and emotional pain of this journey, it’s Skylar that develops psychological scars when they run out of money.  This movie does not glorify abortion, it gives an honest account of what someone goes through when they have to get one.<br/><br/>3 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Troop Zero",
 		"review":"Christmas Flint is raised by her widowed father, Ramsey \“Bossman\” Flint, and his reluctant assistant, Rayleen.  Christmas’s love of reading and space never allowed her to fit in with other girls.  While she sat in the tree with Joseph, labeled a girl-boy by his classmates, she overheard Principal Massey talking to her Birdie Scout troop.  Persad, a representative for NASA, wants to add scouts to the golden record.  The golden record will be played in space.  While Massey’s Troop 5 is not excited, Christmas is hit with a new determination.  First, she asks the current troop if she can join them.  They cut her hair and stuff her into a locker.  Then, she buys a Birdie Scout rule book and learns the rules for making a troop.  First, she has to find four other members.  Christmas has her 4 members: Joseph, Anne-Claire, Hell-No, and Smash.  Second, they have to find a Troop Mother.  Christmas turns to Rayleen.  Third, they have to get a troop number.  So, they go to Principal Massey and asks for a number.  With hesitation, the only number in the state of Georgia is Zero.  Finally, to join the Jamboree each girl will have to earn a badge.  While it sounds easy, Rayleen has to instill confidence in each girl to find their special talent.  However, Rayleen has to find confidence in herself to stand up to the establishment so Christmas can achieve her golden dream.<br/><br/>With a song and a special mark of solidarity, this troop stands firm against anyone who teases or berates them.  They have to overcome the expectations of others based on their socio-economic standings in life.  While she is teased for bed-wetting, others in the troop are teased for having less money and less grace.   Principal Massey and Rayleen constantly tell them ‘they want too much from life’.  When these kids form a troop, they stand against society’s expectations and prove that having less can make you too much to handle.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Bombshells and Dollies",
 		"review":"Every year, thousands flock to Las Vegas for Viva Las Vegas Rockabilly Weekend to enjoy the fashion, cars, and music of the 1950s.  For 12 women this celebration of culture means much more.  They are here for the 18th Annual Miss Viva Las Vegas Pinup Contest.  Women from every corner of the globe style their hair, clothes, and makeup to resemble that of a 1950's pinup girl with the hopes of winning the crown. Each woman has a journey that led them to the pinup lifestyle.  Like the pinup girls in the 1950s, these women aren't oversexualized, objectified, or demeaned.  They are revered for their femininity and strength.  They are business owners, contractors, moms, philanthropist, bloggers, and fashion designers. They were proud to wear work boots and heels without judgment or loss of their womanhood.   Anyone, anywhere, any body type can be a pinup girl.<br/><br/>During this documentary, everyone is careful not to say pageant but contest.  'Pageant' has developed a negative connotation because it only covers one body type.  This is a contest, not a pageant.  For these women, the road to pinup has been riddled with personal struggles and growth.  Some have suffered or are suffering from eating disorders, fibromyalgia, and teen suicide.  Pinup was a building block to gain perseverance and gain awareness for their causes. As these women tell their stories, it's hard not to cry and identify with a struggle. This documentary proves the World of Pinup is for those who have struggled, been outcast, and been underrepresented.  So have your tissues nearby so your cat eye doesn't smudge, you will need it.<br/><br/>4.5 out of 5 stars.",
 		"image":{
@@ -27,22 +36,13 @@ var videoOnDemand = [
 		}
 	},
 	{
-    	"option":"4",
+    	"option":"5",
 	    "mtitle":"Marriage Killer",
     	"review":"After an injury, Morgan learns about a new adult club from Isabelle.  She politely declines.  At a dinner party later, Isabelle mentions the club around Morgan's husband, Robert.  He wants to see what the fuss is about and convinces Morgan to go.  The two decide the club isn't for them but make love in the car on the ride home.  Isabelle weasels her way into their lives until the 3 make love.  The morning after, Isabelle is in love with the couple but they don't reciprocate.  Heartbroken, she will do anything to be a part of their family.<br/><br/>This movie maybe too spicy and too real for Lifetime.  The plot is wild but it is grounded in truth based on the public backlash.  In today's world, you rarely go beyond the video tape.<br/><br/>4 out of 5 stars.",
 	    "image":{
     	    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/marriagekiller.png",
         	"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/marriagekiller.png"
   	  }
-	},
-	{
-		"option":"5",
-		"mtitle":"Pretty Rosebud",
-		"review":"Cecilia feels constricted with her structured and religious life, so she uses affairs as a release.  This is a tale of how ignoring your happiness can lead to a road of self-destruction and how loving yourself can save you.<br/><br/>3.5 out of 5 stars",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/prettyrosebud.jpg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/prettyrosebud.jpg"
-		}
 	}
 ];
 
