5cff90fe9f43a87ff4e307e38b208c281d1f6667
Tiffany Gray <KarateTD@yahoo.com>
2020-05-10 00:46:22 -0400

    3 made for tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Maternal Secrets",
+    "review":"Two years ago, Jackson won a state Senate seat.  Before he starts campaigning again, he and girlfriend Aubrey are going on a vacation to Bermuda.  For Aubrey, this is a babymoon. At dinner, Aubrey brings up the subject of meeting his mom but Jackson isn't ready. The next morning, they sit poolside and Aubrey starts to feel ill.  She tells Jackson to stay while she lays down in the room.  Hours later, she wakes up to find Jackson is still gone.  She tries to text him, call him, and look for him.  But no luck. Finally, she calls the police and files a missing person's report. Out of the blue, Aubrey gets an email for Jackson.  He has left Bermuda and gone home.  He felt like they are moving too fast and he wants some space.  Aubrey is devastated and the police call off the search.  But something doesn't sit right with Aubrey. Aubrey goes to the lobby to get more information and a woman introduces herself.  Her name is Rose and she is Jackson's mother. Rose believes Jackson is with the woman he truly loves.  Only one name comes to Aubrey's mind: Samantha. She will make deals with the devil to find Jackson and get out of hell.<br/><br/>You have to give your full attention to this movie.  It has flashbacks within flashbacks. And keep your eye on the bottom left side of your screen to get the date of the flashbacks.  This will make the twist much more revealing.  With Jackson's letter, a bloody bathroom floor, and an ex-girlfriend, you get a fun mystery of what happened to Jackson.  Soon, you will start questioning Aubrey. And in case you are wondering, The Bermuda Indenture and The Expectant Father are real books (FTC Affiliate Disclaimer).  In films like this, a change of characters' appearances will help with the flashback.  So stick with this movie for a good climax and a strong payoff at the end.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/maternalsecrets.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/maternalsecrets.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Psycho Escort",
+    "review":"Diane is a hard-working lawyer and wants the promotion to partner.  But Russell tells her hard work isn't enough.  She needs to become a more visible presence at the firm's events.  When her boss, Garrett, invites her to wine tasting, she says yes. Her husband died 2 years ago and she cringes at the thought of sympathetic looks at the party when she comes alone.  Her sister, Laurie, tells her about the Companionship Collective.  You can hire someone to accompany you to events without it being a date. She is captivated by Miles and picks him. Diana meets Miles at a coffee shop and they have a great conversation.  She tells him about the wine tasting and he agrees to go.  Before they leave she tries to take a picture of him and he quickly shuts her down.  He says it's against the rules to have the employee's pictures on social media.  Diane apologizes and later meets him at the wine tasting.  Miles impresses all of her co-workers except for Kyle.  Kyle is eyeing the same promotion and will do anything to undermine Diane. When she hires Miles again, he starts to stalk her.  He vows to make Diane his, at any cost.<br/><br/>Not much to see here as Diane makes every possible Lifetime mistake.  She forgets to keep her distance, look into his background, and listen to her conscious about Miles.  Even as the truth stares at her from a computer screen, she ignores it and continues to see Miles off the books.  With each mistake, you feel less compassion. This movie can be skipped now and you can watch it during a \"Psycho\" marathon.<br/><br/>2 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psychoescort.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psychoescort.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"A Moving Romance",
+    "review":"After an underwhelming and dismissive presentation, Olivia is fired from her interior design job. Defeated, Olivia goes home and goes through her mail.  She finds an invitation for the 25th-anniversary party for Wilson Family Movers.  The moving company is owned by her father Neil. Olivia heads to LA and meets her father's crew.  Scott is the crew leader.  He got a great score on the LSAT and hopes to become a lawyer.  But he has not applied to law school. Pete wants more responsibility but doesn't know how to ask for it.  However, he is keeping quiet in another aspect of his life, his love life. Craig is the oldtimer on the job.  He knows the truth about Neil's company.  It's falling on hard times.  They are losing their clients to Acme Movers. However, Neil refuses the offer but keeps it quiet from Olivia.  In turn, Olivia keeps her job loss quiet from her father.  When Neil and Olivia meet for lunch, he talks about missing a fishing trip due to work.  Olivia offers to take over for him so he can relax and have fun.  With that decision, Olivia will find her interior design spark, Scott will find his passion, Pete will find his voice, and Neil will find the next step in his life.<br/><br/>A sweet story with a heartfelt lesson in interior design.  If you are looking for a love story, you will be captivated by the love story between Pete and Karen.  They act as each other's wingmen on a double blind-date and it ends with them standing on the corner together.  You will be so drawn into their love story, you will forget about Scott and Olivia's budding romance. This is an endearing story about love, loss, and foundations.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/amovingromance.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/amovingromance.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Deadly Mile High Club",
     "review":"Tanya wakes up in a hospital bed with one question on her mind, \"Where's Jake\".  Her nurse informs her that Jake, the co-pilot, died in the plane crash.  Miraculously, she, the pilot survived.  Years later, Tanya is a flight instructor. One student introduces himself as Jake Sherman.  Tanya believes her Jake has come back to her and she starts to fixate on him.  She uses this time to get to know him better.  Jake tells Tanya his wife, Annie, and his mother-in-law, Margaret, are against him learning to fly. Tanya starts to flirt and they kiss.  Jake stops and says he can't, he is a married man. She decides Jake is hers.  She will flirt, manipulate, and kill to have Jake all to herself.<br/><br/>Wow! Tanya takes nuts to a whole new level.  She is a mix of Seven with Throw Momma From the Train and a bit of Basic Instinct.  Your heart will skip a beat when she takes Annie and Jake for a flight.  However, you will experience mixed emotions for Margaret's inflight instructions. And you must watch it for yourself to enjoy the skilled writing and forethought that went into this script.  This movie has a great resolution.  Although, there could be a part two with some creative writing.  This is a fun and adult watch.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Killer in the Guest House",
     "review":"Freelance photographer Gina needs to find another source of income or she will lose her mother's home.  She decides to rent out a room online. After searching through applicants' profiles, she lands on Mark.  Mark is a pilot, a veteran, and has a green thumb. Gina accepts his application.  The next day, he moves in and makes an Irish breakfast.  During breakfast, she asks him to model for her portfolio.  While modeling, Gina tells Mark she has written emails to different agencies but has yet to send them. With just a few seconds of encouragement, Gina presses send.  After the modeling and life coach session, Mark hears Gina arguing with her ex-boyfriend, Levon. When he refused to pay her all the money she was owed, she took the photos.  Now, he is ready to play and pay.  Hearing Gina's distress over Levon angers Mark.  Mark has decided to protect Gina and her dreams at all costs.<br/><br/>Lifetimers, we have a shovel!!! And it comes early in the movie too.  Mark is sweet in the beginning, but Gina never checked his references.  So you know something bad will happen. It's just strange to see it happen so fast.  After 1 day with Gina, he is obsessed.  And it's nothing she does for him or to him, it's everything he does for her. You would expect the obsession to go the other way around.  This movie gives the viewers a good dish of crazy but no real cause for the obsession.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/killerintheguesthouse.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/killerintheguesthouse.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Woman of the House",
-    "review":"Five years ago, Sarah was on cloud nine.  She was the president of Kappa Beta and graduating from college.  Now, the party has ended and no one at work shares her enthusiasm for mixers and dressing up. Sarah wishes she could go back to her college days, Taylor tells her to visit the old Kappa Beta house. The current president, Jess, isn't attracting new pledges, doesn't throw mixers, and has no social events scheduled.  Current members are leaving daily.  If Jess doesn't turn things around, they could lose their chapter.  Sarah goes to Dean Kent and gets more bad news.  If the Kappas don't get 33 new members, they will lose the house.  Sarah decides to become house-mom and put Kappa back on the map.  With an ex-boyfriend, a competing sorority, and broken friendships, can Sarah bring Kappa Beta back to its glory days?<br/><br/>This movie has the bite of Mean Girls with the sorority love of Legally Blonde and the competitive spirit of Bring It On.  While the overall love story is in the background, it falls in step with Sarah's internal struggle.  Will she hold on to her collegiate past or face the real world?  Her ex-boyfriend Brad, is the frat dad for Theta Rho. Kent is the responsible guy who is a stickler for the rules.  But determining what man she wants, she ultimately determines her future.  With great comedic timing and well-written characters, you discover adulting is hard but someone has to do it.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/womanofthehouse.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/womanofthehouse.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"MatchMaker Mysteries: A Fatal Romance",
-    "review":"Angie's book, Written in the Stars, is a number one bestseller.  With her newfound notoriety, she was asked to speak at a writer's panel along with authors Beatrice and Helen.  Before the panel, Angie saw Beatrice arguing with someone on the phone and rudely cutting off her assistant Margaret.  During the panel, Beatrice and Helen made the time to take quick verbal jabs at each other.  Then, an attendee started to heckle Beatrice about her affinity towards her fans.  Before Beatrice could defend herself, she got sick and was taken to the hospital.  By the next day, Detective Kyle reports to Angie that Beatrice died of poisoning.  Margaret tells Angie she wasn't just Beatrice's assistant, they were sisters.  Margaret believes Ethan Plume, the famous publisher, killed Beatrice. Angie's heart drops when she hears this.  Ethan is Angie's ex-fiance. Angie believes it could be Beatrice's estranged husband Ted.  When Angie interviews Helen, Helen points to Margaret as a suspect. So, Angie gets access to Margaret's hotel room and searches it.  She finds a cufflink with the letters EP inscribed on it.  Detective Kyle uses this evidence to arrest Ethan.  Angie will risk her life to clear Ethan's name.<br/><br/>A great installment for the series.  It provides a strong mystery for the viewers and expands on Angie's character.  She has grown professionally as a writer and you get to see her romantic history too.  Leaving a challenger for Detective Kyle in the future.  The mystery is strong and demands your full attention.  Small clues are given throughout the story, but you don't know the true motive until the end.  This allows you to keep guessing and solving.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/matchmakermysteriesafatalromance.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/matchmakermysteriesafatalromance.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Dying for Motherhood",
-    "review":"Tracy runs to her boss, Ed, and asks for an advance.  She needs to run from her abusive boyfriend Bobby.  If Bobby discovers she is 21-weeks pregnant, he will shoot her.  Ed gives her all the cash he has and Tracy leaves work with a trash bag full of clothes.  But she finds Bobby waiting for her.  Once Bobby is out of sight, she runs.  Tracy gets a hotel room and finds an adoption agency.  The adoption liaison introduces her to Patrick and Genevieve Parker.  After meeting the Parkers, Tracy still isn’t sure but the liaison talks her into adoption.  Back at the hotel, the manager starts to harass Tracy for payment.  When he gets too touchy-feely, Patrick comes to Tracy’s rescue.   Patrick offers to let Tracy stay at their house for the night.  After one night, Tracy picks the Parkers to adopt her baby girl.  They are elated and asks her to sign a contract.  Without reading one word, Tracy signs it and moves in until the baby is born.  Tracy doesn’t know that the Parkers are unhinged.  Patrick is not taking his meds and Genevieve hasn’t been open about her past.  But leaving isn’t the easiest option, Bobby is hot on Tracy’s tail.<br/><br/>It’s hard to know where to start.  This movie brings the crazy level to ten from the very beginning.  Having people coming at Tracy from all angles gets you invested and interested.  There are two critical moments in the movie that will shock you.  Tracy’s confrontation with Bobby at the mall and Genevieve’s confrontation with Bobby at their house.  You have no idea who you want to win.  And the wrap-up is worth the wait.  This is a fun watch to save on your DVR.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dyingformotherhood.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dyingformotherhood.jpg"
     }
   }
 ];
