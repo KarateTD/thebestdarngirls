1284f2404c91e6b2c87c8bf66a687cc342eafef1
Tiffany Gray <KarateTD@yahoo.com>
2020-05-26 18:30:58 -0400

    add 2 in stores movies

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,24 @@
 var mustBuy = [
   {
-    "option":"1",
+		"option":"1",
+		"mtitle":"Endings, Beginnings",
+		"review":"After breaking up with Adrian and quitting her job, Daphne is moving into her sister, Billy's, pool house.  Daphne's friends tell her she needs to take a sabbatical from dating and figure out what she wants in life. During a New Year's Eve party, Daphne walks away to be alone and she is found by Frank.  He bums a cigarette and enters her suffer zone. Moments later, Daphne sees Jack.  Jack is a writer and a friend of Billy's and Frank's.  Jack and Daphne ring in the new year with a conversation.  The next morning Frank sends her a Spotify playlist called \"Music to Suffer To\". At her job, Jack walks in and asks her out on a date. They laugh and connect over art, cigarettes, expectations, and the future.  During their conversation, Frank texts.  He wants to see her. She decides to meet Frank at a local bar.  He accuses her of trying to make him jealous.  They sit and talk about the danger of their passion. But their words come crashing down when they spend the night together.  The next morning, she leaves Frank on the floor and heads home. Daphne text them both goodbye.  Frank says goodbye but Jack believes they have something special and wants to see her again.  She meets Jack at his place and they eat.  He warns her that Frank is unreliable, volatile, and messy. Now, Daphne is stuck between two different but broken men.  When her past collides with her present, it will leave a roller coaster of emotions for her future.<br/><br/>As she lives her life over the year, you see why things slowly unhinged in her relationship with Adrian and her job.  She goes back and forth between the two men with the realization that she doesn't feel right alone. She describes Frank and Jack as two people who give different sides of what she needs. Both men allow her to take the focus off herself. and her plight. The end will leave you with one burning question and fantasies to come.  While part of Daphne's life ends, it brings a beginning.<br/><br/>4 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/endingsbeginnings.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/endingsbeginnings.jpg"
+		}
+	},
+  {
+    "option":"2",
+    "mtitle":"The Invisible Man",
+    "review":"Cecilia has put her plan into place.  As Adrian sleeps, she moves the camera, turns off the alarm, and tracks him in bed on her phone.  She climbs over the cement barrier, runs through the woods and waits for Emily.  As she gets in Emily’s car, she drops the Diazepam she used to drug Adrian and gets in the car.  Emily drives off leaving Adrian behind.  Adrian finds the bottle on the ground and heads home.  Two weeks later, Cecilia is hiding out in James’s home with his daughter Sydney.  Walking to the mailbox causes Cecilia enormous anxiety.  She knows Adrian is waiting for her to let down her guard.  Emily tells her to relax because Adrian committed suicide.  Cecilia breathes a sigh of relief.  The next day, Cecilia gets a letter from Adrian’s brother Tom.  Tom is the executor of Adrian’s estate.  Adrian left Cecilia $5 million to be paid monthly in $100,000 installments without income or estate taxes.  One night, Cecilia gets a feeling that someone is around but sees no one.  She brushes it off and goes to sleep.  Cecilia is awakened to notice her covers on the floor at the foot of the bed.  She pulls the covers but they are stuck on something.  She stares to see footprints appear out of nowhere.  She snaps them and hears something fall to the floor and run. James tells her it’s all in her mind.  Then she remembers his threat.  If you leave me, I will find you.  You won’t see me but I will leave something behind to let you know I was there.  While no one else believes her, she knows Adrian has faked his death and found a way to make himself invisible.<br/><br/>That feeling of someone being in the room, someone watching you, or someone listening to you is a feeling we all know too well.  As the camera pans to an empty spot in the room, you will swear you can see Adrian standing there.  Being invisible allows each attack to be shocking.  This modern twist on The Invisible Man novel is a mix of The Invisible Man movie and Enough.  With strong acting, amazing special effects, and killer designs, the plot feels believable.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvisibleman.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvisibleman.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Onward",
     "review":"Onward was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access'.",
     "image":{
@@ -9,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Bad Boys for Life",
     "review":"Bad Boys for Life was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access'.",
     "image":{
@@ -18,30 +36,12 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Last Full Measure",
     "review":"On September 9, 1999, Scott is stuck in DC traffic. He gets a call that his boss is retiring, leaving his job as a Pentagon staffer up in the air. He leaves his wife, Tara, in traffic and jumps on the metro. Scott sits and reads is daily packet and meets with is 11 o'clock appointment. It's Vietnam Air Force Vet Thomas Tully. Decades ago, Tully applied for William Hope Pitsenbarger to receive the Medal of Honor. After an investigation, it was downgraded to an Air Force Cross. Tully believes this is wrong. Staff Sargent Pitsenbarger was an Air Force Pararescuemen who was on a mission to airlift injured army men out of the battlefield in Vietnam in mission Abilene. When the first man is pulled up, Pitsenbarger realizes it's the Army medic. Pitsenbarger knows the men on the ground will need help to get the others to safety. When the last helicopter was about to fly off, they told Pitsenbarger to get back on the plane. He sent up one more man, signaled them to leave him, got a gun, and prepared for battle. He lost his life for men he never met. To get the Medal of Honor, Scott knows you need 3 things. Pitsenbarger is only missing the approval from the chain of command. Scott starts his investigation and sees the toll the war has taken on these men but he sees their courage in trying to honor their fallen comrade. When Scott's job is at risk because of political football, can he overcome his fear to get Pitsenbarger his medal before his father, Frank Pitsenbarger, dies of cancer?<br/><br/>n a time veterans are fighting for decent healthcare, this movie will make you want to stand up for them. The physical and emotional scars are presented to the viewer for shock and compassion. Each man has worked tirelessly for decades to obtain the Medal of Honor while coping with loss, regret, pain, and PTSD. Their constant disappointment in this country is reflected the moment they see Scott. They believe he is another puppet sent to sweep Pitsenbarger's valor under the rug. When he shows his true dedication to the project, it allows him to open his heart and fight it through to the end.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelastfullmeasure.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelastfullmeasure.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"The Gentlemen",
-    "review":"Raymond comes home and gets a bottle of liquor when he hears ice rattling behind him.  It's private investigator Fletcher.  Fletcher was hired to find dirt on Raymond's boss, Mickey, for 150,000 pounds.  Fletcher has found a score of information and turned it into a script.  If Raymond pays 20 million pounds, he can have the script.  Raymond is annoyed by Fletcher but offers to hear him out.  Fletcher starts with his boss Big Dave.  Big Dave hired Fletcher to get dirt on Mickey because Mickey snubbed him at a party in front of some very influential people. Mickey is ready to give up the marijuana business and retire from the mobster lifestyle.  He has decided to sell his operation to Matthew for 400 million. When word gets out that Mickey is going to retire, Dry Eye comes to Mickey to strike a deal.  Mickey thinks Dry Eyes' offer is disrespectful and unapproved by his boss Lord George.  Then, Mickey gets bad news.  One of his stash houses has been robbed and the video is streaming online as a rap video. Mickey knows the underworld sees his retirement as a weakness and everyone is gunning for him, his wife, and his business.  And that's just half of Fletcher's story.<br/><br/>In true Guy Ritchie storytelling, it's not over until it's over.  You have to be at this movie on time or you will miss crucial parts of the setup.  Of course, you can't trust what you see because Fletcher is telling a story for the 1st half of the movie.  The second half is told in real-time and has everyone one-upping each other.  This movie will become an instant cult classic while leaving an ending for part two.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegentlemen.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegentlemen.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Just Mercy",
-    "review":"In 1987, Walter McMillian, Johnny D to his family, cleared trees and looked up to the evening sky. He had no idea it would be the last day he would look at the sky as a free man. He was arrested for the brutal murder of Ronda Morrison. After the jury sentenced Johnny D to life, the judge overrode their sentence and gave Johnny D the death penalty. In Jackson, Georgia, Bryan Stevenson is an intern meeting with death row inmates to update them about their case. After he graduates Harvard 2 years later, Bryan moves to Alabama, he gets federal funding to start the Equal Justice Initiative or E.J.I., and he hires Eva as his Director of Operations. After meeting 5 other clients, Bryan meets Johnny D. He offers to help Johnny D but Johnny D doesn’t want to hear it. Bryan refuses to quit and meets with Johnny D’s family. They all admit attending a church fish fry with Johnny D the day of the murder. After hearing Bryan met with his family, Johnny D agrees to let Bryan be his lawyer.<br/><br/>Based on Just Mercy: A Story of Justice and Redemption by Bryan Stevenson, this story is moving, painful, and honest. It speaks on the unfair scales of justice based on race, class, economics, and political standing. You will be moved to tears when Bryan’s efforts fall short for some. At first glance, you will become angry at Ralph Myers. But as the film progress, you will learn the true nature of why he has done what he has done. You will sympathize with his agonizing decision. When you watch, remind yourself: This didn’t happen during slavery or Jim Crow laws. This happened in the late 1980s, less than 40 years ago. If you are inspired, donate to the E.J.I..<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/justmercy.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/justmercy.jpeg"
     }
   }
 
@@ -58,16 +58,6 @@ module.exports = mustBuy;
 /*  For Must Buy later    
 
     {
-		"option":"5",
-		"mtitle":"Endings, Beginnings",
-		"review":"After breaking up with Adrian and quitting her job, Daphne is moving into her sister, Billy's, pool house.  Daphne's friends tell her she needs to take a sabbatical from dating and figure out what she wants in life. During a New Year's Eve party, Daphne walks away to be alone and she is found by Frank.  He bums a cigarette and enters her suffer zone. Moments later, Daphne sees Jack.  Jack is a writer and a friend of Billy's and Frank's.  Jack and Daphne ring in the new year with a conversation.  The next morning Frank sends her a Spotify playlist called \"Music to Suffer To\". At her job, Jack walks in and asks her out on a date. They laugh and connect over art, cigarettes, expectations, and the future.  During their conversation, Frank texts.  He wants to see her. She decides to meet Frank at a local bar.  He accuses her of trying to make him jealous.  They sit and talk about the danger of their passion. But their words come crashing down when they spend the night together.  The next morning, she leaves Frank on the floor and heads home. Daphne text them both goodbye.  Frank says goodbye but Jack believes they have something special and wants to see her again.  She meets Jack at his place and they eat.  He warns her that Frank is unreliable, volatile, and messy. Now, Daphne is stuck between two different but broken men.  When her past collides with her present, it will leave a roller coaster of emotions for her future.<br/><br/>As she lives her life over the year, you see why things slowly unhinged in her relationship with Adrian and her job.  She goes back and forth between the two men with the realization that she doesn't feel right alone. She describes Frank and Jack as two people who give different sides of what she needs. Both men allow her to take the focus off herself. and her plight. The end will leave you with one burning question and fantasies to come.  While part of Daphne's life ends, it brings a beginning.<br/><br/>4 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/endingsbeginnings.jpg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/endingsbeginnings.jpg"
-		}
-	}
-
-    {
         "option":"2",
         "mtitle":"Always in Season",
         "review":"When Claudia receives the news her son Lennon was dead, she was heartbroken.  However, her grief turned to rage when the police declared it a suicide.  She knows her son would never kill himself.  He was happy and making plans for the future.  So Lennon's brother Pierre started investigating.  His results will lead to this case being presented to the F.B.I..  Pierre and Claudia hope Lennon's case will be fully and properly investigated.<br/><br/>This documentary will anger you to your core while bringing a heavy sadness on your chest.  As Lennon's family and friends give you a glimpse of Lennon's life, you are endeared to him.  During the documentary, the lynchings of Moore's Ford and Claude Neal are discussed in great detail.  Hearing first-hand accounts are eye-opening and allow the viewer to see these crimes as a thing of the present.  The film does point the finger at someone but doesn't allow that person to present themselves to the audience.  It's crucial to the validity of the accusations.<br/><br/>4.5 out of 5 stars.",
@@ -76,15 +66,5 @@ module.exports = mustBuy;
             "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/alwaysinseason.jpg"
         }
     }
-
-    {
-    "option":"5",
-    "mtitle":"The Invisible Man",
-    "review":"Cecilia has put her plan into place.  As Adrian sleeps, she moves the camera, turns off the alarm, and tracks him in bed on her phone.  She climbs over the cement barrier, runs through the woods and waits for Emily.  As she gets in Emily’s car, she drops the Diazepam she used to drug Adrian and gets in the car.  Emily drives off leaving Adrian behind.  Adrian finds the bottle on the ground and heads home.  Two weeks later, Cecilia is hiding out in James’s home with his daughter Sydney.  Walking to the mailbox causes Cecilia enormous anxiety.  She knows Adrian is waiting for her to let down her guard.  Emily tells her to relax because Adrian committed suicide.  Cecilia breathes a sigh of relief.  The next day, Cecilia gets a letter from Adrian’s brother Tom.  Tom is the executor of Adrian’s estate.  Adrian left Cecilia $5 million to be paid monthly in $100,000 installments without income or estate taxes.  One night, Cecilia gets a feeling that someone is around but sees no one.  She brushes it off and goes to sleep.  Cecilia is awakened to notice her covers on the floor at the foot of the bed.  She pulls the covers but they are stuck on something.  She stares to see footprints appear out of nowhere.  She snaps them and hears something fall to the floor and run. James tells her it’s all in her mind.  Then she remembers his threat.  If you leave me, I will find you.  You won’t see me but I will leave something behind to let you know I was there.  While no one else believes her, she knows Adrian has faked his death and found a way to make himself invisible.<br/><br/>That feeling of someone being in the room, someone watching you, or someone listening to you is a feeling we all know too well.  As the camera pans to an empty spot in the room, you will swear you can see Adrian standing there.  Being invisible allows each attack to be shocking.  This modern twist on The Invisible Man novel is a mix of The Invisible Man movie and Enough.  With strong acting, amazing special effects, and killer designs, the plot feels believable.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvisibleman.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvisibleman.jpg"
-    }
-  }
 
 */
