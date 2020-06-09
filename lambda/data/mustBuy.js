8170d5d09791cfd670b96f3896c122a470110231
Tiffany Gray <KarateTD@yahoo.com>
2020-06-09 18:35:16 -0400

    add the hunt

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,15 @@
 var mustBuy = [
   {
-		"option":"1",
+    "option":"1",
+    "mtitle":"The Hunt",
+    "review":"A dozen people wake up in a field with bits in their mouths and locks on the bits’ buckle.  They have no idea where they are or how they got there.  They find a crate in the middle of the field with a crowbar by its side.  Only one man grabs the crowbar and opens it.  The man opens the crate and a pig wearing a shirt comes out.  The man pulls a rack filled with weapons.   As people come to grab a weapon, a young girl finds a key.  The key unlocks the bits.  They gather weapons and start to talk when shots ring through the air.  Only half make it out the field and into the woods.  The six don’t know they are part of a hunt.  Liberal elites have chosen them to travel to the manor and be hunted.  They made one mistake: Underestimating Crystal.<br/><br/>Based on The Most Dangerous Game by Richard Connell, this movie takes a swipe at everyone.  The Hunt is The Hunger Games and Kill Bill all combined into one political satire and mystery.  This movie shows the ugliness and prejudice in everyone.  Whether we like it or not.  If you have a weak stomach, there will be too much gore for you.  Who is the real snowball?<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thehunt.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thehunt.jpg"
+    }
+  },
+  {
+		"option":"2",
 		"mtitle":"Endings, Beginnings",
 		"review":"After breaking up with Adrian and quitting her job, Daphne is moving into the pool house owned by her sister Billy.  Daphne's friends tell her she needs to take a sabbatical from dating and figure out what she wants in life. During a New Year's Eve party, Daphne walks away to be alone and she is found by Frank.  He bums a cigarette and enters her suffer zone. Moments later, Daphne sees Jack.  Jack is a writer and a friend of Billy's and Frank's.  Jack and Daphne ring in the new year with a conversation.  The next morning Frank sends her a Spotify playlist called \"Music to Suffer To\". At her job, Jack walks in and asks her out on a date. They laugh and connect over art, cigarettes, expectations, and the future.  During their conversation, Frank texts.  He wants to see her. She decides to meet Frank at a local bar.  He accuses her of trying to make him jealous.  They sit and talk about the danger of their passion. But their words come crashing down when they spend the night together.  The next morning, she leaves Frank on the floor and heads home. Daphne text them both goodbye.  Frank says goodbye but Jack believes they have something special and wants to see her again.  She meets Jack at his place and they eat.  He warns her that Frank is unreliable, volatile, and messy. Now, Daphne is stuck between two different but broken men.  When her past collides with her present, it will leave a roller coaster of emotions for her future.<br/><br/>As she lives her life over the year, you see why things slowly unhinged in her relationship with Adrian and her job.  She goes back and forth between the two men with the realization that she doesn't feel right alone. She describes Frank and Jack as two people who give different sides of what she needs. Both men allow her to take the focus off herself and her plight. The end will leave you with one burning question and fantasies to come.  While part of Daphne's life ends, it brings a beginning.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var mustBuy = [
 		}
 	},
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Invisible Man",
     "review":"Cecilia has put her plan into place.  As Adrian sleeps, she moves the security camera, turns off the alarm, and tracks him in bed on her phone.  She climbs over the cement barrier, runs through the woods and waits for Emily.  As she gets in Emily’s car, she drops the Diazepam she used to drug Adrian.  Emily drives off with Cecilia, leaving Adrian behind.  Adrian finds the bottle on the ground and heads home.  Two weeks later, Cecilia is hiding out in James’s home with his daughter Sydney.  Walking to the mailbox causes Cecilia enormous anxiety.  She knows Adrian is waiting for her to let down her guard.  Emily tells her to relax because Adrian committed suicide.  Cecilia breathes a sigh of relief.  The next day, Cecilia gets a letter from Adrian’s brother Tom.  Tom is the executor of Adrian’s estate.  Adrian left Cecilia $5 million to be paid monthly in $100,000 installments without income taxes or estate taxes.  One night, Cecilia gets a feeling that someone is around but sees no one.  She brushes it off and goes to sleep.  Cecilia is awakened to notice her covers on the floor at the foot of the bed.  She pulls the covers but they are stuck on something.  She stares to see footprints appear out of nowhere.  She snaps them and hears something fall to the floor and runs. James tells her it’s all in her mind.  Then she remembers Adrian's threat.  If you leave me, I will find you.  You won’t see me but I will leave something behind to let you know I was there.  While no one else believes her, she knows Adrian has faked his death and found a way to make himself invisible.<br/><br/>That thought of someone being in the room, someone watching you, or someone listening to you is a feeling we all know too well.  As the camera pans to an empty spot in the room, you will swear you can see Adrian standing there.  Being invisible allows each attack to be shocking.  This modern twist on The Invisible Man novel is a mix of The Invisible Man movie and Enough.  With strong acting, amazing special effects, and killer designs, the plot feels believable.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Onward",
     "review":"Onward was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access'.",
     "image":{
@@ -27,24 +36,14 @@ var mustBuy = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Bad Boys for Life",
     "review":"Bad Boys for Life was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access'.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/badboysforlife.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/badboysforlife.jpg"
     }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Last Full Measure",
-    "review":"On September 9, 1999, Scott is stuck in DC traffic. He gets a call that his boss is retiring, leaving his job as a Pentagon staffer up in the air. He leaves his wife, Tara, in traffic and jumps on the metro. Scott sits and reads is daily packet and meets with is 11 o'clock appointment. It's Vietnam Air Force Vet Thomas Tully. Decades ago, Tully applied for William Hope Pitsenbarger to receive the Medal of Honor. After an investigation, it was downgraded to an Air Force Cross. Tully believes this is wrong. Staff Sargent Pitsenbarger was an Air Force Pararescuemen who was on a mission to airlift injured army men out of the battlefield in Vietnam in mission Abilene. When the first man is pulled up, Pitsenbarger realizes it's the Army medic. Pitsenbarger knows the men on the ground will need help to get the others to safety. When the last helicopter was about to fly off, they told Pitsenbarger to get back on the plane. He sent up one more man, signaled them to leave him, got a gun, and prepared for battle. He lost his life for men he never met. To get the Medal of Honor, Scott knows you need 3 things. Pitsenbarger is only missing the approval from the chain of command. Scott starts his investigation and sees the toll the war has taken on these men but he sees their courage in trying to honor their fallen comrade. When Scott's job is at risk because of political football, can he overcome his fear to get Pitsenbarger his medal before his father, Frank Pitsenbarger, dies of cancer?<br/><br/>n a time veterans are fighting for decent healthcare, this movie will make you want to stand up for them. The physical and emotional scars are presented to the viewer for shock and compassion. Each man has worked tirelessly for decades to obtain the Medal of Honor while coping with loss, regret, pain, and PTSD. Their constant disappointment in this country is reflected the moment they see Scott. They believe he is another puppet sent to sweep Pitsenbarger's valor under the rug. When he shows his true dedication to the project, it allows him to open his heart and fight it through to the end.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelastfullmeasure.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelastfullmeasure.jpg"
-    }
   }
-
 ];
 
 module.exports = mustBuy;
