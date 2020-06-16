1ceae18498870b83e59302795153c4a1aa680165
Tiffany Gray <KarateTD@yahoo.com>
2020-06-16 19:13:54 -0400

    add 2 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Their Killer Affair",
+    "review":"Detective Max is dragged to a store opening by her boyfriend Simon.  Simon forces her because the store is owned by her best friend Jen.  Jen started the store with the help of her husband Greg. The mask person warns her that he will bring deception to the light. She forgets about it until they get called on a new case.  Tom was found dead by strangulation with a charm around his neck.  During Max and Nick's interview with Tom's wife, Sarah, they learn he was a gambling addict.  When they interview the house manager, Francis, they learn Tom wasn't addicted to gambling.  He was addicted to sex.  As they continue the investigation, Max and the precinct get more video messages warning her about more killings.  Soon a pastor is found dead with slash all over his body.  He was whipped to death and a charm put on his body.  His wife said her husband loved BDSM and hired women to hit him frequently. The next message from the Charm Killer is directed specifically at Max.  She tries to brush it off until her ex-husband, Ryan, is found dead. With the personal message and killing, Max's boss believes the Charm Killer is someone she knows.<br/><br/>This is a different type of Lifetime movie.  It is a full murder mystery with kinky sex, adultery, an intriguing killer with an distinctive signature, and a horrible friend. Though Max is strong in her relationship, her friendship is toxic. However, that toxic friendship in necessary for the plot.  So before you yell at Max, bite your tongue and watch.  It will be a good pay off.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theirkilleraffair.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theirkilleraffair.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"How Not to Propose",
+    "review":"Travis, a printer salesman, is ready to propose to his long-time girlfriend Lena.  He bought the ring and has the weekend planned until his boss calls Travis into his office. His boss has decided to let Travis go.  To make matters worse, another publishing company has turned down Travis's book.  With no job and little money, Travis's weekend plans are canceled.  He still wants to propose but doesn't have a place.  His brother, Cedric, suggests he takes Lena glamping. Then, he can ask Lena to marry him.  Lena is a pediatric resident doing her rounds for a local pediatrician.  She was offered an opportunity to work for a research program in Munich. She decides to tell Travis on their weekend away.  When Lena's friend, Michelle, asks about a potential proposal, Lena shuts down the possibility. Lena believes they are still growing as individuals so marriage is out of the question.  With Lena's new job offer and Travis's proposal, this weekend will decide the fate of their relationship.<br/><br/>Although Lena has a secret, this movie focuses on Travis bumbling through his proposal.  He has issues with missing items, unwelcome guests, misreading coupons, and losing the ring before he can propose.  If that's not complicated enough, Travis and Lena run into Lena's ex-boyfriend Warren.  Warren is a successful, mountain man that makes unemployed Travis question his relationship along with his proposal. As Travis and Lena are figuring out their relationship, they accidentally help develop a new one.  After Cedric loses his WiFi, Travis is worried about him.  Lena suggests sending Michelle to help Cedric.  Cedric and Michelle discover they are two unknown peas in a pod.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hownottopropose.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hownottopropose.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Psycho Yoga Instructor",
     "review":"Justine and Tom received bad news, their adoption agency hasn't found a match for them.  Justine is crushed but Tom thinks its a sign to take a break from the process.  Justine is dumbfounded by his change of heart.  She blames it on his promotion and new work schedule.  She feels off-balanced, belittled, and ignored.  Her friend, Ginnie, tells Justine to take a yoga class.  The handsome instructor, Dominic, will take her mind off Tom and her adoption problems. After a great class, Justine tries to convey to Tom what she learned but he blows her off.  She arrives at the next class early but Dominic allows her to set up.  He can see she is hurting and offers to give private instructions. As Justine and Dominic get closer, she pulls further away from Tom.  She doesn't know the danger lurking around the corner. Justine is just the next name on his list.<br/><br/>This movie, originally titled The Perfect Pose, should have not been renamed.  When Lifetime fans see Psycho in the title, their bloodlust rises to an exponential level. For this movie, the first death isn't mentioned until 1.5 hours into the movie.  This is why most fans are let down.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Deadly Second Chances",
     "review":"D. A. Yasmine let her past get to her while she was questioning a witness.  The defendant, Timothy, is accused of killing his wife.  When Yasmine was younger, her abused mother and her drunken father got into an argument.  During the argument, her mother slipped and fell down the steps.  She died. When the case is dropped because of jury tampering, Yasmine has an outburst and faints.  Later, Yasmine's boss, Joan Winters, tells Yasmine to take a sabbatical while they prepare to try the case again. Yasmine packs up and heads to her sister's home with her daughter, Paige.  Her sister, Laura, remembers the abuse but has chosen to forgive their father.  While Yasmine is staying with her, Laura hopes she can convince Yasmine to reconcile with their father too. With so much going on at home, Yasmine doesn't see trouble lurking around the corner.  Timothy is out to get her and everyone she cares about.  He will do anything to get his kids back and stay out of jail.<br/><br/>This movie seems to lose direction.  At first, it feels like a piece about domestic violence.  Then, it jumps to forgiveness.  Next, PTSD.  Then, a mystery of who can Yasmine trust.  And back again.  It's hard to understand if you are watching a mystery, a thriller, or a statement. This movie tries to cover too much in two hours.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"My Husband's Deadly Past",
     "review":"Karen is having vivid nightmares of killing a woman she has never met.  She tells her husband, Otto, that her dreams a scaring her.  Otto, a psychotherapist, tells her to relax. The next morning, Karen sees a missing flyer for a young woman named Gina.  Karen recognizes Gina as the girl in her dreams and starts to panic. Otto suggests Karen try hypnosis to unlock her memories of Gina.  She agrees.  When she opens her eyes, she is on top of the dam and can't move.  Dr. Hugh pulls over to talk her down from what he believes is a suicide attempt. She begs Hugh to take her home instead of the hospital for observation.  He complies.  Karen is shocked to come home and see police everywhere.  They found Gina dead in the pool shed and Otto recorded Karen's confession to the murder. She tries to maintain her composure until police find her fingerprints on the plastic tarp that contained Gina's remains.   With no one on her side, Karen will bleed, fight, and banter to find out what really happened to Gina.<br/><br/>This movie starts crazy, gets insanely funny, and gives you a nutsy good ending.  You will be engaged with the mystery of what happened to Gina.  The title does give something away but you have no idea how bad her husband is until he goes too far. In the midst of all these wild accusations, Karen finds Hugh.  They have the best on-the-run banter ever seen in a movie.  Get comfortable and hold on tight.  You are in for a great movie.<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myhusbandsdeadlypast.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myhusbandsdeadlypast.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Groomzilla",
-    "review":"Allysa and Tucker bumped into each other and sparked a romance. After Allysa finished dental school, she expected Tucker to take their relationship seriously.  But when he misses her big award, she runs to the ladies' room to cry.  Remembering what his Grandma G.G. told him earlier, he proposes in the bathroom.  Allysa happily says yes.  As their family gathers for an engagement party, Allysa believes Tucker has turned a new leaf.  However, he disappoints her again and shows up extremely late.  Tucker goes to console Allysa and overhears her talking to Courtney.  She wishes Tucker would take their marriage seriously. He will do everything to make her dream wedding come true.  Now, he is a Groomzilla.<br/><br/>O.M.G.! Tucker goes off the deep end.  Often, brides-to-be complain about their future spouse not being involved or not caring about the wedding.  Well, Tucker's action will make you thankful for those spouses.  He plans the date, duties, sends announcements, and goes way over budget for Allysa's big day.  The only thing Allysa can do is hold on for the ride. This man is beyond a Bridezilla, he is a true Groomzilla.  You will love to watch him.  And don't mess with Grandma G.G..  She has something special in her purse for you.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/groomzilla.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/groomzilla.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Captive Nanny",
-    "review":"Rob and Chloe plead their case to an adoption agency. After hearing their counselor's assessment, Chloe worries the agency will say no.  Then she gets the shock of her life, Rob is happy to hear it.  He is having second thoughts about having a baby.  She is dumbfounded and breaks off their 8-year relationship.  To get her mind off her heartbreak, Chloe finds her next job as a nanny.  She is hired by Emily and Michael to watch their son Tommy. They tell Emily that Tommy his homeschooled and can't go outside for security reasons. She agrees to their terms and moves into her room.  On her first day, she breaks the rules and takes Tommy outside.  When they come home, Tommy hears something in the basement and Chloe calls the police.  Emily comes upstairs and tells Chloe to relax, the sounds she heard were from her workroom.  While Chloe is calmed, Emily is upset Chloe called the police.   Emily explains her ex-boyfriend may have the police on his side.  It is not safe for her family as long as her ex-boyfriend is stalking her.  Chloe, with extreme confusion, agrees to their demands again.  After hearing about disappearing nannies, seeing well-drafted dossiers, and learning about a rock star stalker, Chloe will look for the truth and save Tommy.<br/><br/>Reminiscent of The Au Pair Nightmare, this movie redeems itself by having great actors, chilling moments, and a strong climax.  You will spend most of the movie yelling at the screen or looking for spilled tea. Emily consistently says she wants to keep her family together and safe.  And she will starve, confine, and interrogate Chloe to do it.  While there are a lot of similarities to the other movie, give this one a chance.  It will give you all the thrills and chills you need.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecaptivenanny.png",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecaptivenanny.png"
     }
   }
 ];
