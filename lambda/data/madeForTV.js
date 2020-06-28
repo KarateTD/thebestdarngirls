f6122113d2c0dd5969eb0ff2fe6e9c44d398a3d7
Tiffany Gray <KarateTD@yahoo.com>
2020-06-28 16:10:02 -0400

    add 4 tv and 1 vod

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,49 +1,49 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Midway to Love",
+    "review":"Dr. Rachel is a successful T.V. psychotherapist.  After wrapping the last show before her hiatus,Rachel's assistant, Samantha, tells Rachel the producers fired Bill. The boss calls Rachel into his office and confirms the bad news.  Bill is gone.  Deborah will be the new producer and has a lot of ideas.  Deborah wants to attract a younger audience.  So she wants to pair Dr. Rachel with a famous YouTuber named Alden. Alden is excited, but Rachel is disappointed.  Rachel's boss tells her to go home during the hiatus and let the changes grow on her. Once in Midway, Rachel sees her dad at his therapy practice. At a local coffee shop, Rachel bumps into Mitchell.  She and Mitchell dated in the past, but Rachel broke it off to work in Hollywood.  Rachel will use her time in Midway to discover her purpose at work and in life.<br/><br/>This movie takes the long way to the point.  It's well-written that Rachel comes to her professional decision without using Mitchell as a factor.  But it's easy for the viewer to see how that choice will resolve itself.  You start to lose interest in the story.  A cute kid saves the plot, but the connection has little screen time. The movie may be sweet, but it will not be a favorite.<br/><br/>2 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/midwaytolove.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/midwaytolove.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Beat Don't Stop",
+    "review":"Go-Go music is the official sound of DC.  But that declaration didn't happen overnight.  First, Chuck Brown noticed the crowd's reaction to a percussion beat with no break while playing in the Maverick room.  This musical break was the first step into Go-Go music.  With the godfather of Go-Go introducing the classic sound, other groups like EU, Junkyard Band, Rare Essence, and others added Gospel, Funk, and R&B to make the style their own.  While the globe posters and street crews help spread the word of Go-Go shows, the mainstream media couldn't capture or understand the magic in the live performance. Withstanding government sanctions, the crack epidemic, and a change in sound, Go-Go is facing its greatest adversary: gentrification.<br/><br/>Now, this documentary is for fans and non-fans alike.  It takes a loving and critical view of the music native to DC.  While Go-Go has tried to remain fun and lighthearted, it was forced into the political realm when politics came for the music.  Now, Go-Go has found a voice in the movement for culture and recognition, but never acceptance.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebeathdontstop.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebeathdontstop.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Mechanics of Love",
+    "review":"Years ago, young Jake saw a young mechanic named Matti working in her father's garage and was smitten.  Today, Matti is an engineer working with combustion engines.  She is headed back to her hometown for her sister, Claire's wedding. Back at home, Claire's serious side shocks Matti.  Claire is worried about the cost of the wedding.  Even with their father's help, Claire and her fiance, TJ, can't afford it. With the wedding only weeks away, Claire still hasn't booked a band, picked flowers, or a color scheme.  Bewildered by Claire's struggles, Matti goes to her father's autobody shop.  Jake is his right-hand man. So they decide to put Matti's family name to good use and ask people her father has helped in the past with wedding items.  During the deal-making, Jake feels the connection with Matti.  But Matti loves Devin.  It will take a bachelor party, a ruined iPad, and a promotion to help Matti see the truth.<br/><br/>While this movie has a familiar ending, the destination to get there is slightly different.  Matti is very outspoken when it comes to her job. But her romantic relationship is another matter. This contrast between personal and professional will resonate with the viewer.  Both Clarie and Matti are struggling with who they were, who they are, and who they want to be.  You are engrossed as each woman figures it out.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mechanicsoflove.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mechanicsoflove.jpg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Love Under the Olive Tree",
+    "review":"Frank Cabella rightfully hands his olive oil company to his granddaughter Nicole.  Nicole is beyond excited and starts working on her big plans immediately.  She wants to add a new tasting facility. With their new Cabella Olive Oil, she believes they can win the Olive Oil competition.  When Nicole goes to the new site, she sees Jake Bradini looking at some paperwork.  Jake Bradini is the grandson of Raphael Brandini, the owner of a competing olive oil company.  Frank and Raphael were partners but had a falling out, and the feud spreads to each generation. Jake left his job in San Fransicso after Raphael broke his arm.  Now, Jake must learn the business to help his grandfather and win the competition.  When he sees Nicole's plan for a new tasting facility, he holds his paperwork in front of her.  The property she wants to build is on his family's land. Jake files an injunction to stop construction, so they go to court.  During the trial proceedings, the judge gets a bright idea.  The winner of the Olive Oil Competition should win the land.  Now, the competition is in high gear.  When love follows, can it survive the family feud?<br/><br/>This movie is a Romeo and Juliet story with a splash of olive oil.  Your respect for the olive oil making process with grow while watching this movie. They have races, fruit picking, and other contest that will make you laugh.  It's during these competitive moments that Nicole and Jake's love for each other grows.  Seeing it progress during the competition makes their love believable.  While the conclusion is anticlimactic, the love story still has promise.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveundertheolivetree.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveundertheolivetree.jpg"
+    }
+  },
+  {
+    "option":"5",
     "mtitle":"Birthmother's Betrayal",
     "review":"Tara is envious of her best friend, Jenny.  Jenny met her birth mom, who lets her do whatever she wants.  Now, Tara wants to meet her birth mom.  Tara's adoptive mom says no because the adoption agency warned her to keep Tara away from her birth mom.  After a medical scare, both Tara and Amy are curious and start the process in secret.  Tara takes a DNA test to find her mother while Amy calls the adoption agency.  Tara gets her results and meets with her birth mother, Grace, in secret.  Tara mentions Amy contacted the adoption agency for records, putting Grace on notice.  So, Grace finds the agent and kills her. Amy calls Grace and sets up a meeting.  Amy and Grace talk until Amy is comfortable introducing Grace to Tara.  A few days later, Amy has a surprise for Tara.  She introduces Tara to her birth mother, Grace. Tara pretends it's the first time.  They sit, look at photos, and laugh, but Grace tries to make a quick exit and has a fainting spell.  Amy insists that Grace spends the night, then the madness begins.<br/><br/>Lifetimers, you get a shovel and a great plot twist.  For the first thirty minutes, you believe you know where the plot is going.  So you will be tempted to divert your attention.  It is a huge mistake.  This movie saves itself from the Lifetime cliches by giving you the twist early.  You become so preoccupied with the plot that you can't turn away.  This movie deserves your full attention with tea to sip and pearls to clutch.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/birthmotherbetrayal.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/birthmotherbetrayal.jpg"
     }
-  },
-  {
-    "option":"2",
-    "mtitle":"Deadly Transaction",
-    "review":"Erica and Riley are having fun with her copier.  Erica is printing $100 bills and using the fake money to buy high-end items. After a shopping spree, Riley gets an idea.  She takes a fake hundred and goes to the currency exchange.  Erica starts to freak out. Erica is amazed when Riley receives 85 euros.  But Riley's plan doesn't stop there.  She takes the euros to another exchange and gets real American money back.  With a new level of crime, they need better prints.  Erica researches on YouTube and discovers all the items they need are in their art class.  Riley flirts with Mr. Sylvester, the art teacher, to get a duplicate of the classroom key. Erica believes everything will be fine if they keep their operation small.  Just one problem.  Mr. Sylvester found one of their phony bills in his class.  He is in debt with some dangerous men, so he comes up with a plan.  Riley and Erica must pay Mr. Sylvester $50,000, or he will turn them into the FBI.<br/><br/>Great job on keeping it creepy!!!! In these movies, you always have the rebel clashing with a level-headed criminal.  It doesn't take long for viewers to see that level-headed Erica and rebel Riley will fall apart. While these girls are dangerous, it's Mr. Sylvester that will make your skin crawl.  He is spine-chilling as he threatens and manipulates these girls.  This movie is a fun watch with a well-written, honest, and focused ending.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlytransaction.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlytransaction.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Apple Mortgage Cake",
-    "review":"Angela Logan is a divorced mother of three who works and volunteers to help her community. In 2009, Angela's car broke down, so she walks home.  Mrs. Row confronts her about the unfinished home repairs.  If Angela doesn't get them finished, the city will issue fines.  Angela tells Mrs. Row she will get it done and starts to bake for the Lincoln High Casino Night Fundraiser.  At the fundraiser, Angela runs into her old friend Melvin George, and later they ride home together. She stumbles and sees the mail.  She has gotten a final foreclosure notice.  She has ten days to pay them back $4000, or she and her sons will lose their home. Then her sons come up with a great idea.  She should sell her Apple Cakes and charge $40 a cake.  At that price point, she will need to sell 100 cakes in 10 days.  She renames them Apple Mortgage Cake and gets orders instantly.  When the cake orders stop, Angela will have to do something she has never done before. Rely on others.<br/><br/>Based on a true story, Angela Logan went from struggling mom to local heroine to national news in 10 days. However, Angela never lets her boys forget that other people are going through harder times than them. This movie stresses one issue that Angela had to learn.  It's ok to ask for help.  Angela gets help from family, friends, the community, The Hilton, and Bake Me a Wish to become an entrepreneur in 10 days.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/applemortgagecake.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/applemortgagecake.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Timeless Love",
-    "review":"At 7:05 am, Megan wakes up next to her husband, Thomas.  As they admire each other, their children Isabel and Dylan run in their parents' bedroom with their imaginations in full gear. Afterward, Megan goes downstairs to find her family.  But she can't find them anywhere.  Megan looks at the clock and notices it still reads 7:05 am.  She opens her eyes to the shock of her nurses.  For two months, Megan has been in a coma after a horrible accident. Confused, her parents responded she is single with no kids.  The news crushes Megan's spirit.  She tries to piece her life together with family, therapy, and a temp job.  She is awestruck to see Thomas is her boss. Well, this Thomas is a bit different.  Thomas goes by Tommy, doesn't know her, and has a girlfriend.  Convinced this is the man in her dreams, Megan has to find a way to tell Thomas the truth without scaring him off.<br/><br/>You will be sucked into the movie the moment Megan learns her husband and children aren't real.  The actress handles this like they died because she has lost everyone she loved in a moment. The second tear-jerker moment, for the audience, is seeing Megan in front of what she thought was her home.  She falls apart because the house is still under construction.  So it solidified that her dream isn't real and her family is gone.  From that point forward, she is fighting for the family she lost. The writing and acting will leave you fully invested.  Not just in their love but for Megan's hopes and dreams to come true.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/timelesslove.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/timelesslove.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Sisters of the Groom",
-    "review":"Jason Quinn is a success in his father, William's company, but he wants to quit and work as a landscaping architect.  He plans on giving William the news after his brother, Tim's, wedding. During their talk, Jason and Tim's sisters interrupt.  Amanda and Lisa double-check with Jason and Tim about the wedding week. Amanda and Lisa have something up their sleeve for Jason.  They worry about him being single for the wedding and secretly invited his ex-girlfriend Cassie to the wedding. Jason prepares for an interview with a potential chef for their spas and resorts.  Molly has an interview with Williams Spa and Resort, but her van breaks down on the way.  She asks her sister, Sarah, to run to the meeting and give the presentation. The moment Sarah and Jason lay eyes on each other, their tempers flare.  At 2 am the night before, she was cooking, singing, and dancing in heels right above Jason's head.  Jason, tired and angry, begrudgingly came upstairs to give her a piece of his mind.  Now, Sarah gives the presentation, but Jason feels her food isn't healthy enough for his clients. Sarah tells Molly she lost the interview, so they will have to come up with another plan.  The plan will come from Jason.  Worried about what his sisters have in store for him, Jason offers Sarah a deal.  If she goes to all the wedding activities and the wedding with him as his date, he will give them the final interview with his father.  For her sister, Sarah says yes. While keeping this secret, they fall in love.<br/><br/>With a plotline set up for a lot of funny moments, the movie gets serious with adoption woes, a marriage falling apart, and self-doubt.  The biggest downfall is too many characters. If you remove Molly's storyline, it will not affect the plot.  Also, Amanda and her husband, Henry, are arguing about preparation before their adoption confirmation. While this is a heartfelt addition, it feels like a time filler.  The next issue is hair and make-up.  The actresses for Lisa, Cassie, Amanda, and Nicole look too similar. Changes in hair and make-up would make it less confusing for viewers.  Don't be mistaken; the story was strong.  It was the subplots that cluttered the screen.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sistersofthegroom.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sistersofthegroom.jpg"
-    }
-  }
+  } 
 ];
 
 module.exports = madeForTV;
