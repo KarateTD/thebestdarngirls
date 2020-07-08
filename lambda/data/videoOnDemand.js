97f2997bf06711cf6bcf9e1a3cb1eb2f445820d5
Tiffany Gray <KarateTD@yahoo.com>
2020-07-08 00:06:32 -0400

    updating must buy and vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,24 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"The Outpost",
+		"review":"A new but experienced group of soldiers deploy to PRT Kamdesh in 2006.  When they land, they see the dire situation in the terrain.  In a valley, three mountains surround the men.  At any moment, the Taliban can perch on a hill and strike.  Every day they are met with attacks and wonder when the big one will come.  While defending themselves, the men must convince the locals to put down their weapons and fight against the Taliban. With little supplies, no plumbing, and worried family members at home, these men keep their orders and country in their hearts. They deal with spies, customs, and an alarmist interpreter before the Battle of Kamdesh.<br/><br/>Based on The Outpost: An Untold Story of American Valor by Jake Tapper, The Outpost is a gripping movie.  It uses the first hour to get to know the men and their comradery.  The second hour is the battle with no relief. You will be on edge with every move these soldiers make on know and unknown terrain.  If you or a loved one has from war-induced PTSD, The Outpost may be a trigger.  Otherwise, this movie is worth the rental money.<br/><br/>5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theoutpost.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theoutpost.jpg"
+		}
+	},
+	{
+		"option":"2",
+		"mtitle":"The Banker",
+		"review":"After listening and taking notes while shining shoes outside of a bank, Bernard Garrett has learned a lot about finance.  Now, the married father of one wants to take his knowledge to the world of investment real estate. Bernard moves his family to Los Angeles.  He finds a property but doesn't have enough for the renovations.  His wife, Eunice, suggests he find a black investor to get the money.  She introduces Bernard to her old boss, Joe Morris.  Joe one several bars and juke joints around Los Angeles.  After one meeting, Bernard wants no part of Joe, so he meets with Patrick Barker. To guarantee success, Barker becomes the face of the business while Beranrd stays in the background. When Barker dies, his widow does not want to work with the likes of Bernard. With only a verbal agreement, Bernard has nothing to back him up.  When he fails to get a bank loan to help, Bernard gets an idea.  He should buy the bank's building. But he needs money.  So, he goes to Joe.  Joe agrees to work with him, but they both know their skin color will be a problem. They hire Matt.  Matt is a young white man who is willing to play the part of an intelligent investor and experienced golfer.  Soon, Joe and Bernard are the most successful real estate investors in Los Angeles. Bernard wants to take this Los Angeles's idea to Texas.  Joe warns Bernard this move isn't business, its social activism.  This decision will have Bernard and Joe on the right side of history but the wrong side of the law.<br/><br/>.<br/><br/>4.5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebanker.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebanker.jpg"
+		}
+	},
+	{
+		"option":"3",
 		"mtitle":"Irresistible",
 		"review":"Democratic strategist, Gary Zimmer is reeling from Hilary Clinton's loss in 2016.  He decides they need a new rural friendly message.  He has found his guy.  One of his aides played Gary a viral video of Colonel Jack Hastings arguing in a town hall meeting in Deerlaken, Wisconsin.  Jack argues against showing IDs to receive public assistance. Gary thinks he can turn Jack into a Democrat, then change the community by getting Jack elected mayor of Deerlaken.  Gary wants Jack to be a fresh face of the Democratic Party with his gruff exterior and left ideas. At first Jack says no, but changes his mind.  Jack has decided to run for mayor. Gary thinks this will be an easy win, but one semicolon will strike fear in his heart.  Republican strategist, Faith Brewster, is in town.<br/><br/>This movie shows the dirty side of politics on both sides.  It poignantly describes how one party leads with fear while the other leads with shame.  While both argue they are morally right, it all comes down to money.  With focus groups, statisticians, and super PACs, the money is the only focus and concern. This movie provides plenty of laughs, satirical undertones of the horrible nature of both parties, and false endings like Vice.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -9,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"4",
 		"mtitle":"You Should Have Left",
 		"review":"Theo is having intense nightmares about his past; although he is trying a meditation app to help, the dreams are not getting better.  He goes to his wife, Suzie's, movie set and the security guard won't let him past the gate. But Theo can hear Suzie performing a sex scene. He tells her that's not what's bothering him.  He believes the security guard recognized him.  Years ago, Theo was the suspect of his first wife, Caroline's, bathtub drowning.  Theo declares his innocence.  And Suzie believes him.  She rents an Airbnb in Wales so they can have some time away with their daughter, Ella. Theo notices the house looks a lot bigger on the inside than it does on the outside.  The house, while beautiful, is having the wrong effect on Theo. His nightmares are intensifying while his trust issues are growing.  He opens his journal to write his feelings.  Inside it reads, \"You Should Have Left.  Now It's Too Late\".  They didn't choose the house; the house chose them.  And this house will never let them go.<br/><br/>Based on the novel You Should Have Left by Daniel Kehlmann, You Should Have Left is more of a thriller with a mix of a mystery than a horror movie.  It does drag but uses nightmares to scare the audience and foreshadow events to come.  While the house is weird with interior 5 inches bigger than the exterior, reappearing doorways, and endless hallways, it feels more like the Relativity lithograph than an engaging movie. Nevertheless, this movie is not worth the home premiere price tag.  Buying this movie is not recommended until it's in Red Box, then rent it on Vudu.<br/><br/>2.5 out of 5 stars.",
 		"image":{
@@ -18,30 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"5",
 		"mtitle":"The King of Staten Island",
 		"review":"Scott is a 24-year-old man with a dream of being a tattoo restaurant owner. However, his A.D.D. and manic-depression stand in his way.  Seventeen years ago, Scott's father, a firefighter, died in the line of duty. He hangs with his friends, does drugs, and plays video games.  His family and friends all worry about him.  While giving his friend a tattoo, a young boy asks to get one too.  Scott draws one line, then the boy runs away in pain. Later, the boy's father, Ray, confronts Scott's mom, Margie, about the illegal tattoo. Margie offers to pay for the removal, but Ray insists on talking to Scott's father.  Margie informs Ray that she is a widow.  Ray reveals he is a firefighter and knew her husband.  Ray asks Margie on a date the very next day.  For a few weeks, Margie and Ray keep their relationship a secret from Scott.  After they reveal the truth, Margie tells Scott it's time for him to move out. He has nine months to find a new place to live.  But he knows who to blame for his mother's change of heart, Ray.  Scott is out for revenge and will do anything to get Ray out.<br/><br/>This movie has the humor of Juno, the relationships of Baby Boy, and the perseverance of 8 Mile.  With so much promise, the movie feels too long. Although this review is missing many parts and characters, they are unnecessary for the overall point of the story.  Nevertheless, it is a beautiful love letter to Scott Davidson.  The lead actor's real-life father who died in the line of duty.  This movie is about a loveable burnout who finds inner peace and self-confidence.  Then, you are given an indie film resolution.  Streaming is the perfect platform for The King of Staten Island.<br/><br/>3 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekingofstatenisland.jpg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekingofstatenisland.jpg"
-		}
-	},
-	{
-		"option":"4",
-		"mtitle":"The High Note",
-		"review":"Maggie has been Grace Davis's assistant for 3 years.  Grace Davis is a Grammy award-winning legend in the music world but her last album didn't chart as well.  Her long-time manager, Jack, wants Grace to consider doing a Las Vegas residency.  However, Maggie thinks she should record a new album. During Grace's meeting with a producer, Maggie opens up and gives her opinion.  Maggie points out everything the producer did wrong and plays Grace a mix she created.  Grace loved it but told Maggie to never pull a stunt like that again. While Jack thought the producer did a horrible job, Jack tells Maggie she ruined any opportunity Grace had to work with that producer in the future.  Feeling defeated, Maggie goes on a shopping trip for Grace and is interrupted by David.  They share some playful banter about music and he invites her to a party at his house later.  When Maggie leaves the store, she sees David get on the stage and sing. While she loves his vocals, she believes the mix is awful.  She lies and tells him she is a producer and can help him produce a great song.  While Maggie is juggling Grace's life as her assistant, she is juggling David's life as his producer.  She must do everything to keep the two worlds apart. With her current life and future on the line, everything comes full circle to reveal a long con.<br/><br/>With a strong soundtrack, including new and old music alike, this movie will have you bobbing your head and creating mental notes about the music you've overlooked. While the story does lag in the middle with spoiled diva outbursts and emotional breakdowns, it picks up with a strong reveal near the end.  Along with a decent story, The High Note provides a few good laughs thanks to Gayle and Jack.  This movie comes to one blaring commonality: insecurity kills dreams. So, what's holding you back? History, opinions, position, or insecurity.<br/><br/>3 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thehighnote.jpg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thehighnote.jpg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"Body Cam",
-		"review":"Officer Renee Lomito has been cleared for active duty. Back on the force, she is paired with rookie Officer Danny Holledge.  On the beat, they hear a call over the radio.  One of their fellow officers, Kevin Ganning, failed to report back after calling in a suspicious driver.  Renee and Danny check it out.   At the scene, they find Kevin's car with flat tires, blood, and teeth.  As Danny investigates the scene, Renee looks at the dashcam footage.  It's choppy but she can see a green van without a license plate get pulled over.  Kevin sees something in the car, draws his weapon, and orders the woman out of the car.  After she gets out of the car, something snatches him into the air and violently slams him onto his car repeatedly.  Kevin screams, then disappears. Danny and Renee find Kevin hanging on a fence with his body split in half up to his chest.  When an investigator interviews Renee, they blame her for the dashcam footage disappearing.  Renee knows she didn't delete it.  Renee thinks something killed Kevin, she just doesn't know who, what, or why.  Renee looks for the van.  When she tracks it down, she traces it to a nurse named Taneesha and her son Demarco.  In her investigation, Renee will learn about the evil all around her.<br/><br/>Reminiscent of Candyman, this movie provides a few jump-scares with social commentary.  Horror fans won't be surprised at any of the scary moments because they are accompanied by an anticipation score. You watch to understand why this entity is attacking cops as Renee pull the facts together.  If you are a mystery fan, this is a better movie for you.  It does offer some gory moments when you see the after-effects of the attacks. Great job to the makeup and art department for creating ghoulish scenes and victims.  This is a decent mystery to rent if you need a few scares and a satisfying ending.<br/><br/>3 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bodycam.jpg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bodycam.jpg"
 		}
 	}
 ];
