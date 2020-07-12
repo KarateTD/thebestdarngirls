3f50d19f3a2055431ebb1b92096f514c844bc716
Tiffany Gray <KarateTD@yahoo.com>
2020-07-11 23:33:12 -0400

    add new npm and 3 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Obsession: Escaping My Ex",
+    "review":"It's been six months since Nick, formerly Blake, went to jail.  But he has no plans to stay there.  On his way to prison, he escaped transport and killed the driver.  He contacts his sister, Lisa, and gets shocking news.  The mental hospital released her, and her old boyfriend let her move into his home. Two hundred miles away, Madison has put Nick in the past.  She accepted an offer for an internship, set her social media to private, and repaired her bond with her sister Evie.  Evie filed for divorce, went back to real estate, and changed to her maiden name.  All feels great until Evie gets a call from Detective Johnson about Nick's escape. Now, both women are on high alert, but not high enough.  While leaving her job, Madison feels a gun on the back of her head.  Nick found her. Nick calls and tells Evie to wire $1,000,000 to an offshore bank account in 24 hours, or he will kill Madison. She begs for more time, so he gives her 48 hours instead.  In 48 hours, Evie has to get the money or Madison has to free herself, before Nick leaves the country with the love of his life, Madison, and the money.<br/><br/>Like most sequels, this didn't pack the punch the first movie did.  However, it feels like a setup for the third movie in the installment.  You can see all the mistakes Nick, Madison, and Evie are making along the way.  So, you aren't surprised by everyone's plan starts to fall apart. Even with those mistakes, he still manages to outsmart Madison.  If you started the Thrillogy, watch this movie to get ready for number three.<br/><br/>3 out of 5 stars.",
+    "iamge":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/obsessionescapingmyex.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/obsessionescapingmyex.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Obsession: Stalked by My Lover",
+    "review":"Madison, an orphaned college student, is looking for a new roommate.  Her current roommate, Rachel, suggests she try a roommate app, and, with a few clicks, she has three options, two women and one man.  After an interview and a tour, Madison picks the handsome man named Blake.  Even with the deposit and rent money in hand, Madison's boyfriend, Dylan, hates the idea. Madison believes Dylan is ridiculous about Blake's intentions.  Blake has a girlfriend and a good job, he needs a place to stay.  To celebrate the new living arrangement, Madison invites Blake to her estranged sister, Evie's home.  After dinner, Blake and Evie have a conversation.  She believes her husband, Kirk, is cheating, so Evie asks Blake to do a job.  Blake uses his security expertise to catch Kirk in the act. While Evie thinks she is paying a reputable security agent, Blake is using her.  He is a robber on the run. He plans to extort Evie and rob her neighbors.  But he wants to keep Madison by his side. He won't let anyone stand in the way of his money or his Madison.<br/><br/>The first installment of the Obsession Thrillogy has a murderous start.  It shares the focus between Madison's and Blake's perspectives.  You are watching Blake and Madison playing a cat-and-mouse game.  And sometimes both are the cat and the mouse.  This dichotomy of storytelling is compelling to watch.  One thing for sure, this installment will leave you excited to see the next Obsession installment.<br/><br/>4 out of 5 stars.",
+    "iamge":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/obsessionstalkedbymylover.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/obsessionstalkedbymylover.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Driven to the Edge",
+    "review":"Tess tries to get into the wrong Roller car when Jaye stops her because it's her car.  Jaye offers to share because Tess in a rush to get to a job interview.  The two women ride together.  Their driver's arrest surprises Jaye and Tess.  They get another Roller car, so Tess makes her meeting.  Before Tess's meeting, Jaye asks if they can be friends.  Taken aback by her offer, Tess says yes.  The two women meet and discover they have a lot in common. Soon, Tess introduces Jaye to her friend Olive and Olive's new boyfriend, Isaac.  Isaac swears he knows Jaye from somewhere.  On his way to the bathroom, Isaac bumps into Jaye and remembers their connection.  He works for Roller, so they go their cars inspected at the same time.  Jaye convinces Isaac that he made a mistake, but Isaac doesn't know this identification will be his doom.  Jaye is not who everyone perceives her to be.  She is a psychopath on the edge who uses Roller to kill her rude, dismissive, and disgusting passengers.  If Tess doesn't play by the rules, Jaye will make her the next victim.<br/><br/>First, Jaye is a great psychopath.  She talks to herself, compares herself to other killers, and practices smiling in the mirror.  However, the overall story is slow and overdone.  It's not until the end that you get a pretty good twist which piques your interest.  But that's the end of the movie.  Don't worry, it has plenty of gore to satisfy the Lifetimers bloodlust, but not enough story to whet the appetite.  This movie is background noise.<br/><br/>2 out of 5 stars.",
+    "iamge":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/driventothedge.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/driventothedge.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Midway to Love",
     "review":"Dr. Rachel is a successful T.V. psychotherapist.  After wrapping the last show before her hiatus,Rachel's assistant, Samantha, tells Rachel the producers fired Bill. The boss calls Rachel into his office and confirms the bad news.  Bill is gone.  Deborah will be the new producer and has a lot of ideas.  Deborah wants to attract a younger audience.  So she wants to pair Dr. Rachel with a famous YouTuber named Alden. Alden is excited, but Rachel is disappointed.  Rachel's boss tells her to go home during the hiatus and let the changes grow on her. Once in Midway, Rachel sees her dad at his therapy practice. At a local coffee shop, Rachel bumps into Mitchell.  She and Mitchell dated in the past, but Rachel broke it off to work in Hollywood.  Rachel will use her time in Midway to discover her purpose at work and in life.<br/><br/>This movie takes the long way to the point.  It's well-written that Rachel comes to her professional decision without using Mitchell as a factor.  But it's easy for the viewer to see how that choice will resolve itself.  You start to lose interest in the story.  A cute kid saves the plot, but the connection has little screen time. The movie may be sweet, but it will not be a favorite.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -9,41 +36,14 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"The Beat Don't Stop",
     "review":"Go-Go music is the official sound of DC.  But that declaration didn't happen overnight.  First, Chuck Brown noticed the crowd's reaction to a percussion beat with no break while playing in the Maverick room.  This musical break was the first step into Go-Go music.  With the godfather of Go-Go introducing the classic sound, other groups like EU, Junkyard Band, Rare Essence, and others added Gospel, Funk, and R and B to make the style their own.  While the globe posters and street crews help spread the word of Go-Go shows, the mainstream media couldn't capture or understand the magic in the live performance. Withstanding government sanctions, the crack epidemic, and a change in sound, Go-Go is facing its greatest adversary: gentrification.<br/><br/>Now, this documentary is for fans and non-fans alike.  It takes a loving and critical view of the music native to DC.  While Go-Go has tried to remain fun and lighthearted, it was forced into the political realm when politics came for the music.  Now, Go-Go has found a voice in the movement for culture and recognition, but never acceptance.<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebeathdontstop.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebeathdontstop.jpg"
     }
-  },
-  {
-    "option":"3",
-    "mtitle":"Mechanics of Love",
-    "review":"Years ago, young Jake saw a young mechanic named Matti working in her father's garage and was smitten.  Today, Matti is an engineer working with combustion engines.  She is headed back to her hometown for her sister, Claire's wedding. Back at home, Claire's serious side shocks Matti.  Claire is worried about the cost of the wedding.  Even with their father's help, Claire and her fiance, TJ, can't afford it. With the wedding only weeks away, Claire still hasn't booked a band, picked flowers, or a color scheme.  Bewildered by Claire's struggles, Matti goes to her father's autobody shop.  Jake is his right-hand man. So they decide to put Matti's family name to good use and ask people her father has helped in the past with wedding items.  During the deal-making, Jake feels the connection with Matti.  But Matti loves Devin.  It will take a bachelor party, a ruined iPad, and a promotion to help Matti see the truth.<br/><br/>While this movie has a familiar ending, the destination to get there is slightly different.  Matti is very outspoken when it comes to her job. But her romantic relationship is another matter. This contrast between personal and professional will resonate with the viewer.  Both Claire and Matti are struggling with who they were, who they are, and who they want to be.  You are engrossed as each woman figures it out.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mechanicsoflove.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mechanicsoflove.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Love Under the Olive Tree",
-    "review":"Frank Cabella rightfully hands his olive oil company to his granddaughter Nicole.  Nicole is beyond excited and starts working on her big plans immediately.  She wants to add a new tasting facility. With their new Cabella Olive Oil, she believes they can win the Olive Oil competition.  When Nicole goes to the new site, she sees Jake Brandini looking at some paperwork.  Jake Brandini is the grandson of Raphael Brandini, the owner of a competing olive oil company.  Frank and Raphael were partners but had a falling out, and the feud spreads to each generation. Jake left his job in San Francisco after Raphael broke his arm.  Now, Jake must learn the business to help his grandfather and win the competition.  When he sees Nicole's plan for a new tasting facility, he holds his paperwork in front of her.  The property she wants to build is on his family's land. Jake files an injunction to stop construction, so they go to court.  During the trial proceedings, the judge gets a bright idea.  The winner of the Olive Oil Competition should win the land.  Now, the competition is in high gear.  When love follows, can it survive the family feud?<br/><br/>This movie is a Romeo and Juliet story with a splash of olive oil.  Your respect for the olive oil making process will grow while watching this movie. They have races, fruit picking, and other contest that will make you laugh.  It's during these competitive moments that Nicole and Jake's love for each other grows.  Seeing it progress during the competition makes their love believable.  While the conclusion is anticlimactic, the love story still has promise.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveundertheolivetree.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveundertheolivetree.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Birthmother's Betrayal",
-    "review":"Tara is envious of her best friend, Jenny.  Jenny met her birth mom, who lets her do whatever she wants.  Now, Tara wants to meet her birth mom.  Tara's adoptive mom says no because the adoption agency warned her to keep Tara away from her birth mom.  After a medical scare, both Tara and Amy are curious and start the process in secret.  Tara takes a DNA test to find her mother while Amy calls the adoption agency.  Tara gets her results and meets with her birth mother, Grace, in secret.  Tara mentions Amy contacted the adoption agency for records, putting Grace on notice.  So, Grace finds the agent and kills her. Amy calls Grace and sets up a meeting.  Amy and Grace talk until Amy is comfortable introducing Grace to Tara.  A few days later, Amy has a surprise for Tara.  She introduces Tara to her birth mother, Grace. Tara pretends it's the first time.  They sit, look at photos, and laugh, but Grace tries to make a quick exit and has a fainting spell.  Amy insists that Grace spends the night, then the madness begins.<br/><br/>Lifetimers, you get a shovel and a great plot twist.  For the first thirty minutes, you believe you know where the plot is going.  So you will be tempted to divert your attention.  It is a huge mistake.  This movie saves itself from the Lifetime cliches by giving you the twist early.  You become so preoccupied with the plot that you can't turn away.  This movie deserves your full attention with tea to sip and pearls to clutch.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/birthmotherbetrayal.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/birthmotherbetrayal.jpg"
-    }
-  } 
+  }
 ];
 
 module.exports = madeForTV;
