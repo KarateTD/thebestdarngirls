6c427501d35f7cc3da35482d1e2f3f0554882d4c
Tiffany Gray <KarateTD@yahoo.com>
2020-07-19 23:47:35 -0400

    add 1 vod and fix made for tv

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Fatal Affair",
+		"review":"Recent empty-nesters, Ellie and Marcus, left the city and moved to a gorgeous home with a private beach. She attends her last meeting at the firm, but the presence of an old college friend, David, catches her off guard.  David works for a technology firm and plans to use his expertise to gain access to a corporate spy's emails.  Ellie planned on meeting her friend, Courtney, for drinks and decides to invite David to come too.  He happily obliges.  After they get to the club, Courtney calls with disappointing news.  She can't make it, leaving Ellie and David alone. Ellie learns that David divorced his wife and has no children.  Ellie admits, although she loves Marcus, she feels unsatisfied in her marriage.  David invites her to the dancefloor, and they almost kiss.  She retreats to the bathroom and tries to cool off.  David runs after her, and they have a passionate make-out session in the bathroom.  Just before they start having sex, Ellie stops David dead in his tracks. David text the next day for forgiveness, but Ellie ignores his calls and blocks his number.  With her life back to normal, Ellie invites Courtney for dinner, and Courtney brings a plus one: David.  Stunned, Ellie isn't sure what to do, but David pretends like they are meeting for the first time. She tries to warn Courtney about David, but its too late.  David took a preemptive strike and told her Ellie was the aggressor in the bathroom.  With a bit of investigation, Ellie will uncover David's dark past, dirty secret, and long-time obsession.<br/><br/>Fatal Affair is a mix of The Perfect Guy, Enough, The Good Son, and Fatal Attraction.  It provides two cliffhangers and some fun close calls.  So, how does the movie differ from a Lifetime movie? There isn't a nosy best friend, co-worker, or neighbor, just one woman who made a wrong decision.  Also, the cops are not morons. This story of an obsessive stalker isn't new, but it's acted very well.<br/><br/>3.5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fatalaffair.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fatalaffair.jpg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"The Outpost",
 		"review":"A new but experienced group of soldiers deploy to PRT Kamdesh in 2006.  When they land, they see the dire situation in the terrain.  In a valley, three mountains surround the men.  At any moment, the Taliban can perch on a hill and strike.  Every day they are met with attacks and wonder when the big one will come.  While defending themselves, the men must convince the locals to put down their weapons and fight against the Taliban. With little supplies, no plumbing, and worried family members at home, these men keep their orders and country in their hearts. They deal with spies, customs, and an alarmist interpreter before the Battle of Kamdesh.<br/><br/>Based on The Outpost: An Untold Story of American Valor by Jake Tapper, The Outpost is a gripping movie.  It uses the first hour to get to know the men and their camaraderie.  The second hour is the battle with no relief. You will be on edge with every move these soldiers make on known and unknown terrain.  If you or a loved one has war-induced PTSD, The Outpost may be a trigger.  Otherwise, this movie is worth the rental money.<br/><br/>5 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"The Banker",
 		"review":"After listening and taking notes while shining shoes outside of a bank, Bernard Garrett has learned a lot about finance.  Now, the married father of one wants to take his knowledge to the world of investment real estate.  Bernard moves his family to Los Angeles.  He finds a property but doesn't have enough for the renovations.  His wife, Eunice, suggests he find a black investor to get the money.  She introduces Bernard to her old boss, Joe Morris.  Joe owns several bars and juke joints around Los Angeles.  After one meeting, Bernard wants no part of Joe, so he meets with Patrick Barker. To guarantee success, Barker becomes the face of the business while Bernard stays in the background. When Barker dies, his widow does not want to work with the likes of Bernard. With only a verbal agreement, Bernard has nothing to back him up.  When he fails to get a bank loan to help, Bernard gets an idea.  He should buy the bank's building. But he needs money.  So, he goes to Joe.  Joe agrees to work with him, but they both know their skin color will be a problem. They hire Matt.  Matt is a young white man who is willing to play the part of an intelligent investor and experienced golfer.  Soon, Joe and Bernard are the most successful real estate investors in Los Angeles. Bernard wants to take this Los Angeles idea to Texas.  Joe warns Bernard this move isn't business, its social activism.  This decision will have Bernard and Joe on the right side of history but the wrong side of the law.<br/><br/>Because the United States didn't feel the impact of Bernard and Joe for several years after, we can easily forget their history and how it impacted the housing market.  The Banker tells the story of how two men used an unjust system and bent it to their will.  By out conning a con, Bernard and Joe owned the largest building in Los Angeles for years. When Bernard and Joe challenged nepotism, racism will play a part in continuing the world order.  Sit back and enjoy this history lesson with your teenagers.<br/><br/>4.5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Irresistible",
 		"review":"Democratic strategist, Gary Zimmer is reeling from Hilary Clinton's loss in 2016.  He decides they need a new rural friendly message.  He has found his guy.  One of his aides played Gary a viral video of Colonel Jack Hastings arguing in a town hall meeting in Deerlaken, Wisconsin.  Jack argues against showing IDs to receive public assistance. Gary thinks he can turn Jack into a Democrat, then change the community by getting Jack elected mayor of Deerlaken.  Gary wants Jack to be a fresh face of the Democratic Party with his gruff exterior and left ideas. At first Jack says no, but changes his mind.  Jack has decided to run for mayor. Gary thinks this will be an easy win, but one semicolon will strike fear in his heart.  Republican strategist, Faith Brewster, is in town.<br/><br/>This movie shows the dirty side of politics on both sides.  It poignantly describes how one party leads with fear while the other leads with shame.  While both argue they are morally right, it all comes down to money.  With focus groups, statisticians, and super PACs, the money is the only focus and concern. This movie provides plenty of laughs, satirical undertones of the horrible nature of both parties, and false endings like Vice.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"You Should Have Left",
 		"review":"Theo is having intense nightmares about his past; although he is trying a meditation app to help, the dreams are not getting better.  He goes to his wife, Suzie's, movie set and the security guard won't let him past the gate. But Theo can hear Suzie performing a sex scene. He tells her that's not what's bothering him.  He believes the security guard recognized him.  Years ago, Theo was the suspect of his first wife, Caroline's, bathtub drowning.  Theo declares his innocence.  And Suzie believes him.  She rents an Airbnb in Wales so they can have some time away with their daughter, Ella. Theo notices the house looks a lot bigger on the inside than it does on the outside.  The house, while beautiful, is having the wrong effect on Theo. His nightmares are intensifying while his trust issues are growing.  He opens his journal to write his feelings.  Inside it reads, \"You Should Have Left.  Now It's Too Late\".  They didn't choose the house; the house chose them.  And this house will never let them go.<br/><br/>Based on the novel You Should Have Left by Daniel Kehlmann, You Should Have Left is more of a thriller with a mix of a mystery than a horror movie.  It does drag but uses nightmares to scare the audience and foreshadow events to come.  While the house is weird with interior 5 inches bigger than the exterior, reappearing doorways, and endless hallways, it feels more like the Relativity lithograph than an engaging movie. Nevertheless, this movie is not worth the home premiere price tag.  Buying this movie is not recommended until it's in Red Box, then rent it on Vudu.<br/><br/>2.5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/youshouldhaveleft.jpg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/youshouldhaveleft.jpg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"The King of Staten Island",
-		"review":"Scott is a 24-year-old man with a dream of being a tattoo restaurant owner. However, his A.D.D. and manic-depression stand in his way.  Seventeen years ago, Scott's father, a firefighter, died in the line of duty. He hangs with his friends, does drugs, and plays video games.  His family and friends all worry about him.  While giving his friend a tattoo, a young boy asks to get one too.  Scott draws one line, then the boy runs away in pain. Later, the boy's father, Ray, confronts Scott's mom, Margie, about the illegal tattoo. Margie offers to pay for the removal, but Ray insists on talking to Scott's father.  Margie informs Ray that she is a widow.  Ray reveals he is a firefighter and knew her husband.  Ray asks Margie on a date the very next day.  For a few weeks, Margie and Ray keep their relationship a secret from Scott.  After they reveal the truth, Margie tells Scott it's time for him to move out. He has nine months to find a new place to live.  But he knows who to blame for his mother's change of heart, Ray.  Scott is out for revenge and will do anything to get Ray out.<br/><br/>This movie has the humor of Juno, the relationships of Baby Boy, and the perseverance of 8 Mile.  With so much promise, the movie feels too long. Although this review is missing many parts and characters, they are unnecessary for the overall point of the story.  Nevertheless, it is a beautiful love letter to Scott Davidson.  The lead actor's real-life father who died in the line of duty.  This movie is about a loveable burnout who finds inner peace and self-confidence.  Then, you are given an indie film resolution.  Streaming is the perfect platform for The King of Staten Island.<br/><br/>3 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekingofstatenisland.jpg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekingofstatenisland.jpg"
 		}
 	}
 ];
