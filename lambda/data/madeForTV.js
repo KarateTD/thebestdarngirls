589897ff0a0f30ef8ff45d9f3e2a2818b81ed467
Tiffany Gray <KarateTD@yahoo.com>
2020-07-26 19:29:29 -0400

    add 3 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Stalked by My Husband's Ex",
+    "review":"Kristen is happy.  Her boyfriend, Ryan, has proposed.  She will gain a husband and a precious little girl named Lisa.  Then, Ryan has something to confess.  He believes his ex-wife, Nina, is calling him from a blocked number.  When Lisa was a baby, Nina drove drunk with baby Lisa in the car. Nina was declared unfit, and he got sole legal and physical custody. Ryan thinks he should see Nina and calm her down, but Kristen convinces him that's a bad idea.  She believes it will all blow over with time like she has done with ex-boyfriends in the past.  Kristen couldn't be more wrong.  During their engagement party, Nina throws a drink in Kristen's face and maces Ryan.  Now, the couple is on high alert.  Just one problem: Nina is secretly messaging Lisa through Instagram.  Nina is hell-bent on getting Lisa back, even if it kills her.<br/><br/>This one is worth the watch because there are no stupid people, bratty kids, or moronics cops.  The main characters have an unlucky sequence of events happening to them, so you get a compelling storyline with characters you care about and a fun twist. This movie is a mix of today's social media and classic stalking. Stalked by My Husband's Ex isn't your typical Lifetime movie with the same characters.  And the end puts a satisfying conclusion to each character.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stalkedbymyhusbandsex.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/stalkedbymyhusbandsex.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Coins for Love",
+    "review":"Madison is still living paycheck to paycheck.  She got a job as an editor's assistant, but the pay will be low during her 6-month probationary period.  Until then, she is still driving a rideshare car and doing volunteer work when she can.  Alec got the job as a sports commentator on Hard Balling.  However, he is butting heads with his co-host and overspending.  With his increasing schedule, his life is harder to manage. So, Alec calls Madison and asks her to work as his assistant again.  She lays down the terms, and he agrees.  They start getting closer and almost kiss, but Madison puts an end to the love and keeps it professional.  Then, their past comes calling.  Alec's ex-fiance is back to win his love and to fire Madison.  Madison's ex-husband Jake is ready to be a good man to her and a father to his kids. It all comes down the word 'acrimonious' and a gunshot.<br/><br/>The sequel for Coins for Christmas builds off the last movie and gives the audience some potential for a third.  It's great because the characters' internal struggles didn't get solved in the first movie. They are rebuilding their lives and finding the flaws in their critical thinking. By the way, it helps to see the first, but it's not required.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/coinsforlove.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/coinsforlove.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Mile High Escorts",
+    "review":"Lauren is taking a break from school and working as a flight attendant.  She hopes to earn money for graduate school and attend with her boyfriend, Chris, next year.  Her best friend friend, Ashley, takes a job as a flight attendant too.  After a few flights, the job doesn't live up to the hype. After missing her hotel shuttle, Lauren gets a ride from another flight attendant named Hannah.  Hannah wines and dines her and puts Lauren in an expensive room.  Hannah explains she works for a private jet company, and the flight attendants offer concierge services.  These services include shows, dinners, and nightcaps. Lauren's pay will be handsome, and she can get tips, which can include cash, jewelry, and clothes.  Lauren turns Hannah's offer down but keeps Hannah's business card. Then, Lauren's life falls apart.  First, her hours are cut in half. Next, she catches Chris cheating, and he kicks her out of his home.  Then, she discovers her father is having money trouble.  Finally, she still needs money for grad school.  She decides to take Hannah on her offer and Ashley is right by her side. Hannah introduces them to the owner, Franklin, and his son, Eric. With a few flights behind them, Lauren can cash in without taking her clothes off.  But when Franklin dies and flight attendants disappear, Lauren is in unspeakable danger.<br/><br/>Lauren's character is different because she doesn't jump into the business headfirst.  She plans on being a regular flight attendant until she has so many obstacles in her way she sees no way of making the money she needs faster. She gets sucked into the fast-money lifestyle quick.  This movie does push a \"happy hooker\" narrative but changes pace with the changing of the guard.  Franklin, a man who saw the protection of his \"girls\" as his top priority, dies, and his son, Eric, a man who sees money as his top priority, takes over, and the business and clientele changes overnight.  Then, the reality of what these women do is at the forefront of this movie and shatters the \"happy hooker\" stereotype.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/milehighescorts.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/milehighescorts.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Murder in the Vineyard",
     "review":"Emma gave up her company and headed back to her old stomping grounds to take over her grandfather's vineyard.  She enrolls her daughter, Beatrice, in high school and runs into her high school sweetheart, Luke.  The principal assigned Chloe to show Beatrice around the school.  During the tour, a loose ball bumps Beatrice.  Soccer player, Bryan runs to get the soccer ball.  The next day, Emma focuses on winemaking while Beatrice walks into chemistry class.  The teacher makes Bryan her lab partner.  Bryan's girlfriend, April, is enraged.  She tells Bryan he can never be alone with Beatrice, so Bryan breaks up with April and starts dating Beatrice.  April decides to make a slam site about Beatrice.  At first, Beatrice stays level-headed and goes to a party.  But, the party turns sinister.  Beatrice is drugged, then provocative photos are taken with the soccer team and posted on the slam site.  Emma tries to defend her daughter, but the cops blame Beatrice.  They worry more about the players' reputation than Beatrice's safety.  Now, Beatrice is in the stalker's crosshairs, just like a girl who committed suicide two years ago.<br/><br/>This storyline is fascinating to watch.  In the first minute, you see a woman running away before someone grabs her.  Then, it becomes a Hallmark movie for 15 minutes.  Next, it becomes an H.M.M. movie.  The plot introduces you to several suspects with convincing motives.  You are reminded it's a Lifetime movie when they address cyberbullying, rape culture, and the stalker's reason for the crime.  With a compelling mystery, the plot gets your attention.  However, the ending will leave you wanting a better understanding of the stalker.  By the way, the website is real.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"The Wrong Wedding Planner",
     "review":"An excited Ashley and Brad are planning their wedding. During her friend, Clarissa's wedding reception, Ashley bumps into the wedding planner, Mandy. Ashley hires Mandy and uses Clarissa's reception as the only reference needed.  They start to work together, and Ashley promptly opens her home to Mandy. When Brad meets Mandy, Ashley can see something is wrong.  Brad looks as if a truck hit him.  He brushes it off as bad manners.  But, after several strange incidents, Brad comes clean.  Brad and Ashley were engaged and expecting their first child.  However, he broke off the engagement when he learned the pregnancy was a lie.  Since then, Mandy has been stalking him. And she won't let anyone stop her.<br/><br/>The next movie in \"The Wrong\" series offers an intriguing antagonist but not much else to the story.  You don't care enough about Brad or Ashley to feel an urgency for their lives. It follows \"The Wrong\" format, so the movie offers nothing new.  It has a few good one-liners but that's not enough to redeem it.<br/><br/>2.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewrongweddingplanner.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewrongweddingplanner.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Psych 2: Lassie Come Home",
-    "review":"While recovering from 6 gunshot wounds and a stroke, Chief Carlton \"Lassie\" Lassiter saw ghosts and dead bodies from his recovery room. If others on the force hear about Lassie's vision, he could lose his job for good.  He tells Shawn and Gus, but makes them promise to keep it quiet.  At first, Gus and Shawn decide not to take Lassie's case, but they change their mind when Lassie's dog, a gift from Shawn and Gus, finds a human severed hand.  While keeping that case a secret, Juliet is covertly investigating Lassie's shooting.  She shows Lassie a picture of the suspect in custody, but something in Lassie's face tells her she has the wrong guy. With two secret investigations, the pressure mounts when Shawn finds a positive pregnancy test in Jules's car.<br/><br/>Psych 2 is what you would expect from the creators of Psych.  It's funny, heartfelt, surprising, and full of nicknames while being true to its core fans and continuing where the first movie left off.  Chief Vick has her eye on a Commissioner position, Juliet and Shawn are adapting to married life, and Selene and Gus are still together and trying to find boundaries.  Psych 2 has a special meaning for all the Psychos.  After Timothy Odmunson suffered a stroke, the writers wrote the stroke in the script and allowed Psych fans into the trials and tribulations of recovery.  It's the most honest script ever written for the show.  Psych 3 can't come fast enough.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psych2lassiecomehome.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psych2lassiecomehome.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Obsession: Her Final Vengence",
-    "review":"Madison moved to Chicago, got a promotion, and joined a support group.  In the support group, she talks about the trauma Nick, her ex-boyfriend, put her through when he held her captive.  After the support group, Courtney approaches Madison and introduces herself.  Months ago, Courtney received a phone call.  Her brother's girlfriend murdered him. The two strike up a quick friendship. She doesn't know that Courtney is Lisa, Nick's sister.  She is furious at Madison. She gets close to Madison to find out why Nick fell for her.  Then, Lisa plans on ending Madison's life.  She will manipulate, drug, and steal to get her revenge and it all comes down to a WiFi password.<br/><br/>The best advice: root for the killer.  Lisa is a very cunning adversary.  She uses modern technology and simple gaslighting to mess with Madison's head. Unlike most Lifetime movies, this movie comes to a satisfying conclusion for all the characters.  It's easy to see why producers split this movie into a Thrillogy.  This third installment redeems the second.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/obsessionherfinalvengence.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/obsessionherfinalvengence.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Love in Harmony Valley",
-    "review":"Emma is a bundle of nerves returning home to Harmony Valley.  She knows pain and recovery await her.  The last time she visited, Emma and her best friend, Tracy, were in the car together, and a truck struck them.  While Emma had a few bumps and bruises, Tracy was in the ICU for thirteen days and had three surgeries. After some prompting from her grandmother, Rose, Emma goes to the recovery center to visit Tracy. While waiting, Emma sees Tracy's brother, Will, arrive and he doesn't want to see her.  He is upset Emma left right after the accident and abandoned his sister.  No one knows that while Emma doesn't have a reminder of physical scars, she has emotional scars.  She has panic attacks and night terrors of the crash.  Emma discovers Will and his friend, Flynn's proposal for a new vineyard. When Flynn and Will present their idea to the board, Rose and the other committee members notice the plan is missing key elements like the town's ice rink, bike path, bandshell. She and the board members turn it down.  Emma, trying to get in Will's good graces, makes Rose an offer.  Let Flynn and Will redo their proposal and include the town's landmarks and needs, then present the new plan in a few weeks.  Emma and Will's newfound partnership will spark forgiveness, love, and healing.<br/><br/>Based on the novel Dandelion Wishes by Melinda Curtis, this movie is the first of an eleven book series, so it takes a while to reveal the plot.  Like any first book, the storyline focuses on character development and intricate relationships.  The first hour will be very slow-paced, but the second hour picks up.  During this time, this movie gives good advice on forgiveness and strength.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveinharmonyvalley.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveinharmonyvalley.jpg"
     }
   }
 ];
