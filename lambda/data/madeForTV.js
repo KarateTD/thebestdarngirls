e76f99de4c9f5ce491e5a150ce9378b16398ccc1
Tiffany Gray <KarateTD@yahoo.com>
2020-08-08 00:54:14 -0400

    add 3 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"A Murder to Remember",
+    "review":"Robin and Sam walk into the police station.  Robin walks to Sheriff Watkins and explains Sam accidentally shot her husband, Javier while hunting.  Sam came to her aid and saved her from dying in the woods.  Sheriff Watkins takes their statement.  Sam goes to a local motel, while Robin goes home with her mother.  Robin starts to experience panic attacks and remembers more about what happened.  Sam purposely shot Javier and kept her hostage in the woods for several days.  He tortured, brainwashed, and raped her over 5 days.  He agreed to let her go after she promised to lie to the police. Watkins invites Sam to come back and tell his side of the story.  He offers to take a lie detector test. Their results come back and everyone is floored.  Sam passed, but Robin failed. It will take on fax to get to the truth of what happened in the woods.<br/><br/>Inspired by true events, you must remember these events happened in the 70s.  People were a lot less skeptical of those around them.  Robin naively instructs Javier to follow Sam to the best fishing lake in the area.  After driving in circles, Javier wants to abandon the plan but Robin wants to keep going. Also, the script alters the real story for the sake of the plotline.  It will help the viewer to watch the real survivor talk about the events that led up to the kidnapping.  The acting and directing were spot on but the script needed more work.  Don't forget to tip your hat to the best mom on Lifetime, Celeste.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/amurdertoremember.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/amurdertoremember.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Love by Accident",
+    "review":"As the townspeople of Greenwood go about their day, an RV crashes into a car.  Daphne stumbles out and asks good samaritans to check on her godmother, Fran.  Fran is terminally ill with pancreatic cancer.  They were headed on a road trip west to scatter Fran's husband's ashes.  During the drive, Fran became dizzy and crashed.  The hospital puts Fran on bedrest, leaving Daphne stranded.  Outside the hospital, she sees Matt. Matt calms her and says he is the town's mechanic.  He has her RV and has no problem with her sleeping there.  The next morning, Matt works on the RV while Daphne writes her postgraduate thesis. A few days later, Fran's arrival at the RV dumbfounds Daphne.  Fran confesses she left early to continue their trip. Daphne doesn't believe Fran is ready to drive.  After her parents and best friend died in a car accident, Daphne has vehophobia.  Now, she can't drive.  Daphne must come up with a plan to make Fran stay in Greenwood and off the road.  She knows Fran wanted her to find the love she's dreamed about from the novels. So, Daphne kisses Matt passionately.  Fran is ready to stay.  As this fake romance becomes real, Matt and Daphne will need each other to remain triumphant in the painful times ahead.<br/><br/>Based on Coming Home to You by M. K. Stelmack, this movie will pull at all of your heartstrings.  Both Daphne and Matt have to come to terms with their past to create a future together.  Daphne has felt like a burden to Fran. Matt's father abandoned him and his sister, Brittney, shortly after Brittney was born. They lean on each other during these tough times and create a bond fit for an Austen novel.  Keep this one on your DVR.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lovebyaccident.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lovebyaccident.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Romance in the Air",
+    "review":"Eden tanked her marketing proposal, so her boss asked her to take a break to recharge.  She heads to her parents' summer home in Lake Tahoe with her best friend, Kate.  As they settle in, Kate notices a man in the backyard. Eden walks over to introduce herself and realizes he is her old friend Riley. Riley owns a hot air balloon company and offers to give the ladies a ride while they are in town. On the hot air balloon ride, Eden learns that business is hard for Riley, so she offers her marketing services to help drum up some new business.  As she works with Riley, she finds her passion.  But Kate wonders if she's found her groove with marketing or with Riley.  As soon as Eden thinks she has things figured out, her boyfriend, Michael, comes to Lake Tahoe for a surprise visit.<br/><br/>This movie doesn't get good until the end.  It tries to throw in a twist with Michael showing up after Riley realizes he has feelings for Eden.  But you know Michael is a non-factor in this story.  This movie is the typical plot of going to your old town to find yourself and finding love with an old flame.  With the predictable plotline, it doesn't make you laugh, cry, or gasp to break up the monotony of the story.  However, Eden's selfless act at the end is the saving grace of this movie.<br/><br/>2 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/romanceintheair.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/romanceintheair.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Sleeping with Danger",
     "review":"Flight attendant Grace Tanner doesn't believe she will fall in love.  She is 38 and wants real love with a real connection.  Since her love life is on the rocks, Grace finds a new way to better her life.  She meets with a nutritionist named Dr. Paul.  Later that night, Dr. Paul calls, not with a nutrition plan, but with a game plan for her heart.  Within a few dates, Grace is head over heels for Dr. Paul. Their relationship is going great, so she starts to believe in love again.  At a party, Dr. Paul sees a man touch Grace's arm.  He pulls her aside and starts chastising her, questioning her fidelity, and roughing her up. To ease his mind, she promises her commitment and loyalty to him.  Even with that promise, he is quick to anger, tracks her movements, and alienates her from her friends. She reduces her flight schedule and works with him on his new business venture of selling nutrition shakes.  With business booming, they move into a house together.  As she unpacks his boxes, she finds legal paperwork.  She finds sexual harassment, peeping, and stalking charges filed from ex-girlfriends and patients. After a push, Paul slaps her.  Before Grace calls it abuse, she will be on the wrong side of a gun, in hiding, and writing a letter of regret and warning.<br/><br/>Inspired by Ann Rule's Mortal Danger and Other True Crime Cases, this movie shows how the stigma of abuse will force its victims to keep quiet and make excuses for their abuser.  Grace doesn't want to call Paul's actions abuse because she doesn't consider herself a victim. When she sees the truth, Paul is pure evil. This movie is one to watch and discuss with those you love.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"The Wrong Stepfather",
     "review":"Sarah Woodley is in a social media war with Marissa over Sarah's ex-boyfriend, Jason.  After arguing on-line becomes a fight in the real world, Principal Higgins calls Sarah to her office.  Soon Sarah's mother and teacher, Ms. Woodley is scolding Sarah about her behavior.  Ms. Woodley hopes to get Sarah back on track.  So, Ms. Woodley has hired a college counselor to guide Sarah through college admissions.  Ms. Woodley has left out one detail: she's dating the counselor, Craig Edgars.  The relationship is easygoing until Sarah finds a college essay that Craig wrote for her.  He wrote about how her stepfather, himself, has been the most influential person in her life. Now, Sarah doesn't trust him as her mother's boyfriend or her college counselor.  Sarah has no idea how right she is.  Craig is obsessed with having a family and will kill to get it.<br/><br/>Craig's paternal clock is ticking, but its ticking with crazy.  He will do anything to create the perfect family, even kill one of the members.  It's hard to see Ms. Woodley make so many excuses for his bad behavior.  But fear not Lifetimers, Sarah's bravery offsets her mother's misplaced trust.  The truth is a bad dad mixed with a college admission scandal all rolled into one Lifetime movie.  Part two is possible.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewrongstepfather.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewrongstepfather.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Upside-Down Magic",
-    "review":"For six years, 13-year-old Nory waited for this day.  She is going to Sage Academy to practice magic with her best friend, Reina.  Nory can shapeshift into any combination of animals.  Reina can shoot fire from her hands. As they wait for the bus, Mr. Skriff appears from the woods. Mr. Skriff leads them through a portal, and they enter the well-manicured lawn of Sage Academy. The headmaster, Ms. Knightslinger, introduces herself and the Sage Way of magic.  At the academy, they have five schools of study.  Each student must take a placement test.  The test will determine their school selection and their level.  After the placement test, Professor Argon chooses Reina for Honors Flares.  Nory's report says UDM. Underground, three students and a professor wait for Nory.  Knightslinger explains the students failed their placement test because their magic is upside-down. In the past, a UDM, named Reginald Roberts, was consumed by shadow magic and tried to take over the school.  Sage Academy will not allow the UDMs to practice magic.  They must stay underground with Professor Skriff until their magic dies. While they are trying to hinder the UDMs, Shadow magic found its next victim and plans to destroy the school once and for all.<br/><br/>Based on the Upside-Down Magic Book Series, this movie isn't about changing yourself to conform to others' standards.  The UDMs (Upside-Down Magic) embrace their differences as a strength.  Not a weakness for evil.  This movie gives a great lesson for kids today.  Being different isn't wrong.  Doing different isn't wrong.  It's forcing others to change their differences that become detrimental, not only to them but to the world too.  Let's celebrate the Upside-Down in all of us.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/upsidedownmagic.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/upsidedownmagic.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Her Deadly Groom",
-    "review":"Brenna wants Alison to put herself out there after her divorce from George.  So she creates a dating profile for Alison on a phone app.  To spite George, Alison publishes the profile.  When George leaves, Alison begs Brenna to delete it because Brenna set up the app profile with her email address and password.  Brenna refuses and goes on vacation.  A few days later, Alison still wants to delete the profile, but Alison sees a lot of great guys liked her picture.  When she remembers one of them, Brenna swipes right and the two match.  His name is Vincent. They go on a date and have a great time.  Vincent gives her $50,000 for her business and buys her a home.  So when he asks for her hand in marriage, Alison says yes.  Alison doesn't know that Vincent is too perfect.  Someone is setting up Alison, and it could cost her her life.<br/><br/>Be prepared to be frustrated.  Alison misses all the clues and falls in love too quickly.  Her friends, family, daughter's boyfriend, and associates try to warn her that something is off.  So, root for Vincent in this movie.  He stays a step ahead, talks to a dead body, and refuses to let anyone outsmart him.  This movie does leave a possibility for a part 2.  But do you want one?<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/herdeadlygroom.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/herdeadlygroom.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Stalked by My Husband's Ex",
-    "review":"Kristen is happy.  Her boyfriend, Ryan, has proposed.  She will gain a husband and a precious little girl named Lisa.  Then, Ryan has something to confess.  He believes his ex-wife, Nina, is calling him from a blocked number.  When Lisa was a baby, Nina drove drunk with baby Lisa in the car. Nina was declared unfit, and he got sole legal and physical custody. Ryan thinks he should see Nina and calm her down, but Kristen convinces him that's a bad idea.  She believes it will all blow over with time like she has done with ex-boyfriends in the past.  Kristen couldn't be more wrong.  During their engagement party, Nina throws a drink in Kristen's face and maces Ryan.  Now, the couple is on high alert.  Just one problem: Nina is secretly messaging Lisa through Instagram.  Nina is hell-bent on getting Lisa back, even if it kills her.<br/><br/>This one is worth the watch because there are no stupid people, bratty kids, or moronic cops.  The main characters have an unlucky sequence of events happening to them, so you get a compelling storyline with characters you care about and a fun twist. This movie is a mix of today's social media and classic stalking. Stalked by My Husband's Ex isn't your typical Lifetime movie with the same characters.  And the end puts a satisfying conclusion to each character.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stalkedbymyhusbandsex.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/stalkedbymyhusbandsex.jpg"
     }
   }
 ];
