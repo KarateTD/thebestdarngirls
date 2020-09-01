40fe551a37e9d999e29a0712dc4d0a6784c15d26
Tiffany Gray <KarateTD@yahoo.com>
2020-09-01 03:58:56 -0400

    adding made for tv and early screening

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Sorority Secrets",
+    "review":"After the college defunded her scholarship, Cassie is looking for an alternative to stay in school. She walks past the Kappa Eta Lambda sorority exhibit and overhears the sorority offers housing and tuition assistance.  She approaches them for a flyer, but they tell her no.  Cassie walks away, but runs into a classmate named Lisa.  Lisa says 'no' was a test.  The Lambdas expect her to come back and ask again.  She goes back, and they give her a flyer.  Soon, Cassie is the newest Lambda, and Monica is assigned to be her big sister. When Wendy, the house mom, sees Cassie's interest in the social committee, she invites Cassie to the white party to meet with benefactors. When Cassie arrives at the sorority house, Wendy has astonishing news.  One of the benefactors liked her and wants to sponsor her.  Wendy wants Cassie to join the social committee.  Cassie's life on campus is debt-free, and in the house is chores free.  Cassie doesn't know the social committee is a front for prostitution.  After several sisters die, Cassie vows to put an end to it all.<br/><br/>Give this movie a chance.  With the setup, it's easy to dismiss it as background noise.  During the sting, it becomes a full-blown mystery, twice.  This movie will pleasantly surprise you with its compelling twist.  Cassie will blossom from a timid, naive girl to an intelligent, strong woman within two hours.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sororitysecrets.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sororitysecrets.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Ruthless Realtor",
+    "review":"Meg shows Annie and Ralph Savage around a gorgeous home. As they leave, Lynette, shows up on the doorstep, demanding Meg to give her some answers.  Meg promised Lynette that home and kept it off the market for her.  Meg doesn't believe Lynette is the right fit for the house; however, the Savages are the perfect fit. A few weeks later, the Savages are moving in, and to their surprise, Meg is there with treats in hand.  They feel Meg is out of order, but try to keep up appearances and be kind to her. The next day, Meg is cooking in their kitchen and bearing gifts.  She mounted Annie's pregnancy test.  They can't get her out of the house fast enough and agree to keep her out.  Meg comes to Annie's job to apologize, but things quickly get out of hand, and Meg is pulled away by security.  She starts to send threatening texts to Annie and warns Annie the police will never find her.  Next, Meg reveals another secret.  She lied so they can get the house. nnie and Ralph will put their lives on the line for the home of their dreams.<br/><br/>Keep your eye on this movie.  It takes you down a familiar path of Lifetime movies but gives the experienced watcher a fun twist.  If you are paying attention, you will hear the truth the plot sprinkles along the way.  The actors did a great job of misleading the audience by playing into the Lifetime characters we know, love, and hate.  All Lifetime watchers will enjoy this movie.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ruthlessrealtor.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ruthlessrealtor.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Secrets in the Basement",
     "review":"Barbara found the home of Delilah and Shawn's dreams. Delilah sees all the markings of a basement but doesn't see one.  Barbara tells Delilah that she will double-check the plans and let her know if there is a basement.  During a trip to the hardware store, Delilah runs into Jay, the man who designed her home.  He is curious to see what she has done to the house.  Delilah allows Jay to follow her home.  During the tour, the two start to click, and Delilah wants to work for him.  After a few short moments, Jay agrees to allow Delilah to work with him. As work starts to mount, Delilah is collapsing under the pressure.  Delilah sees figures moving, unknown decor appearing, and cabinets opening on their own. As the old saying goes, just because you're crazy, it doesn't mean they aren't after you. Someone is out to get her, and they want her to pay for a huge mistake.<br/><br/>Secrets in the Basement isn't what you would expect from the title, but it does cover the Lifetime basics.  You have the over-trusting lead going crazy, the husband trying to help, and the nosy best friend that never disseminate all the necessary information at one time.  While the movie does try to add suspense for the person in the basement, experienced Lifetimers will be able to figure it out in 5 minutes.  The killer's motives are what drives viewers to watch.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Psycho Sister-in-Law",
     "review":"At his father's funeral, Nick spots a young woman standing far away but engaged.  He recognizes the woman is his sister Zara.  Zara is the illegitimate child of Nick's father, Gavin, and Zara's mother. Before he passed away, Gavin proposed to Callie, and he happily celebrated Nick and Hailey's first pregnancy.  Now, Nick and his wife, Hailey, are allowing Zara and Callie to stay in the home until the will reading.  In the will, Gavin left $100,000 for Zara, $600,000 for Callie, and everything else to Nick and his unborn offspring. Zara can't believe Gavin treated Callie better than her and Nick is treating Callie like family too.  Zara has a plan in store.  She will take over the home and the money.  To see her master plan through, she needs one thing: Nick and Hailey's unborn baby.<br/><br/>Thank goodness for backhanded remarks and twist.  The infighting between the women will have you clutching your pearls and laughing out loud.  While having a twist is expected, the twist itself is a compelling reveal.  Be forewarned: you have to catch this movie from the beginning to see Zara's first kill.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Wedding Every Weekend",
     "review":"Ginny encourages her friend, Brooke, to make this year the year of Brooke.  Over the next month, Brooke will attend a different wedding.  To avoid the singles' table, old Brook would stay in her boring relationship with Colin.  Now, Brooke will enjoy the single life and focus on creating a physical therapy practice.  During Gretchen and Scott's wedding, Brooke runs into one of her complicated patients, Nate.  Nate restores cars and loves DIY projects, including medical care.  She sees him limping around the wedding and asks to see his knee.  With pseudo pride, he shows the horrible tape job he did.  After a stroll, Brooke learns Nate is avoiding the dating world too.  He had a bad break up and wants to focus on his auto body shop.  The next weekend, Brooke and Nate are surprised to see each other at another wedding.  They realize they will be attending two more weddings in 2 weeks.  Each encounter will bring camaraderie and romance for the pair.  But their love doesn't come without challenges from their past and their present.<br/><br/>Congratulations to everyone involved in this movie.  This movie is one of the most original Hallmark storylines in a long time.  It is well-acted, well cast, well design, well-directed, and enjoyable from beginning to end.  Every character, every wedding, and the overall love story will have you well invested in the movie. Be prepared to smile, cry, and fall in love while watching.  Wedding Every Weekend is the instant classic Hallmark movie that Hallmarkies have wanted, needed, and deserved.<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/weddingeveryweekend.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/weddingeveryweekend.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Beware of Mom",
-    "review":"After the police catch her daughter, Kylie, at a college party, Tonya grounds her for two weeks and tells her to stay away from her friends.  The next day, Tonya sees the new neighbors moving in and politely suggests Kylie helps them.  The new neighbors are Anna and her daughter Jessie.  Nine months ago, Jessie lost her sister and dad in a house explosion, so they moved to get a new start.  Kylie understands loss because her dad died in Afghanistan. Also, Kylie discovers Anna is an influencer. Kylie is in awe of Jessie's mom.  After several attempts to invite Kylie to their home for dinner, Anna approaches Tonya and asks to keep an eye on her while Tonya works. Tonya likes the idea and says yes.  So, Tonya is dismayed when she comes home early and sees a party at Anna's house and Kylie's attire. Kylie is furious and wishes her mom was more like Anna.  This change of heart brings pure, unadulterated joy to Anna.  She wants Kylie to be her new daughter.  No matter the cost.<br/><br/>Talk about child abuse.  Jessie deserves your full and unbridled sympathy.  She is manipulated, controlled, and demeaned by Anna to indulge Anna's sick desires.  In the end, Jessie is astute enough to understand she has control over her actions and takes blame for her role.  It's Jessie that becomes an adult during this ordeal.  This movie has the Lifetime plotline you love, but it gives you a fresh dynamic on the mother-daughter relationship.  Beware of Mom is a fun watch.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bewareofmom.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bewareofmom.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Win, Lose, or Love",
-    "review":"Clara is a hard-working, divorced, dedicated mom of one.  She is very organized and works for the Parent's Group at her son's, Oliver, school.  Clara steers him towards activities like chess club and drama club to keep him from getting hurt. Alex is a hard-working, disorganized, dad of one.  His daughter, Emma, loves to play soccer but misses her mom.  Years ago, Alex, his wife, and his friend started an advertising business.  Since she died, he works day and night to keep the business running.  He pitches an idea to Hal of Hal's Sporting Goods. Since most consumers are moms, Hal should focus on gaining their trust.  Hal doesn't like the campaign and starts to walk out, then Alex gets a better idea.  He can shoot his next commercial at his daughter's school's Sports Day.  And it would look better if Hal donated $5,000 in equipment to the sports program. Alex attends the next Parent Group meeting to tell them about the idea.  While some parents like the idea, Clara is quick to rebuke.  She believes that sports get enough money, so the money should go to other activities.  Also, Sports Day should change to include all school activities. They are electing a new president tonight, so Margaret decides not to vote on the matter. Clara and Alex are nominated.  The Parents Group takes a vote, and the tally is tied.  Alex and Clara will be co-presidents until the next meeting, where they will vote again. The next meeting is the day before Sports Day. While Alex and Clara butt heads, they will learn about their children, themselves, and find love.<br/><br/>Talk about competition.  As Alex says, for someone who doesn't like sports, Clara truly is competitive. When discussing the children, the movie is quick to point out Alex's faults along with Clara. Oliver focuses on soccer with Alex, giving Emma a chance to focus on drama and poetryThis movie has the Lifetime plotline you love, but it gives you a fresh dynamic on the mother-daughter relationship.  Beware of mom is a fun watch with Clara. When they see each other's children, they understand another point of view.  It's heartfelt moments like these, that will bring you into the story.  And the conclusion will leave a smile on your face.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/winloseorlove.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/winloseorlove.jpg"
     }
   }
 ];
