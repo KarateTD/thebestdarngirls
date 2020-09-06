04171a96b6f9b472195410778360d503854a169b
Tiffany Gray <KarateTD@yahoo.com>
2020-09-06 01:17:29 -0400

    add 2 made for tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"A Chance in the World",
+    "review":"Steve has dreams about being cold, being hurt, and seeing the world whirl by him.  But he doesn't know if those dreams are real or his imagination.  When he wakes, he awakens to a nightmare.  His adoptive parents have rules to belittle and berate him. Betty and Willie are his adoptive parents, and they have three biological children, Eddie, Lisa, and Reggie. One day Willie tells Steve to come downstairs and explain himself.  Mr. Sykes, a counselor at Steve's school, wants to meet Steve during the summer. Betty reminds Steve to keep his mouth shut, or there will be hell to pay.  At school, Mr. Sykes gives Steve good news.  He did very well on his placement test.  With his scores, he can skip a few grades and go to college.  First, his mother will have to sign a form permitting him to be in the Upward Bound Program.  The program will give him weekly tutoring, college planning, and financial help.  Excited, he runs home to bring the forms to Betty to sign.  She grabs a pen, looks at Steve, and then rips up the application. She calls Mr. Sykes and Mrs. Holt, the program director, and tells them Steve doesn't want to join.  Mrs. Holt asks to speak to Steve, who reiterates Betty's words under extreme direst.  When Mrs. Holt insists on visiting the home to talk to Steve, Betty relents and asks her to give Steve new forms, and she will sign them.  This program is Steve's path to freedom, a chance to prove the world wrong, and a line to his past.<br/><br/>Based on A Chance in the World: An Orphan Boy, A Mysterious Past, and How He Found a Place Called Home by Steve Pemberton, this movie will draw a quiet rage in you.  Knowing parents such as the Robinsons exists will make your blood boil.  However, your heart will break during Steve's hospital visit.  Everyone can see Steve's bruises, but he won't tell anyone what happened. With one promise, she holds him hostage.  With all his pain, Steve found his escape in books like Encyclopedia Brown and Watership Down.  He found the beauty in his pain, and through that, his family.  Watch this one today and donate to A Chance in the World Foundation.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/achanceintheworld.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/achanceintheworld.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Secrets in the Woods",
+    "review":"After months of missed opportunities, Brant asks Sandra to spend time with him at his family's cabin. They drive up a deserted dirt road, and Brant gets out of the car.  He explains they have to walk the rest of the way.  Sandra loves the cabin; however, she is slightly crestfallen to learn her cell doesn't work. They enjoy hikes, sitting by the fire, and ax throwing.  On these trips, she lost her phone and her clothes.  Brant suggested she should wear his mom's clothes for the time being.  Sandra felt weird, but Brant convinced her it was ok.  Later, Sandra waited in the bed for Brant wearing sexy lingerie but lost focus when she heard a commotion outside.  Sandra walked out to check on Brant but found another man in the woods.  She tried to run away; however, she didn't get far.  She wakes up with a bruise on her head and a chain cuffed to her ankle.  Brant introduces Sandra to his father, Langley. If she doesn't follow Langley's rules, he will kill her.  If that's not bad enough, Brant horrifies Sandra when he tells her she is a gift for his father.  She will replace his mother and be Langley's new wife.  With no phone, no people within miles, and no internet, how will Sandra escape these sick and evil men?<br/><br/>This movie puts the creepy factor on 11.  You will be grossed out and entertained. At first, it's unclear why Brant continues with his father's plan.  The writers explain everything, but you have to stick to the movie. With that aside, it's eye-opening to ascertain how Brant picked Sandra.  While her looks were just one aspect, she had a lot of read flags for him to read.  He read her profile and saw a woman unsatisfied with her life, little family, and desperate to create a new family. While entertaining, watch with the lights on, and stay out of the woods.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretsinthewoods.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretsinthewoods.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Sorority Secrets",
     "review":"After her college defunded her scholarship, Cassie is looking for an alternative to stay in school. She walks past the Kappa Eta Lambda sorority exhibit and overhears the sorority offers housing and tuition assistance.  She approaches them for a flyer, but they tell her no.  Cassie walks away, but runs into a classmate named Lisa.  Lisa says the Lambdas' 'no' was a test.  The Lambdas expect her to come back and ask again.  She goes back, and they give her a flyer.  Soon, Cassie is the newest Lambda, and Monica is assigned to be her big sister. When Wendy, the house mom, sees Cassie's interest in the social committee, she invites Cassie to the white party to meet with benefactors. When Cassie arrives at the sorority house, Wendy has astonishing news.  One of the benefactors liked Cassie and wants to sponsor her.  Wendy wants Cassie to join the social committee.  Cassie's life on campus is debt-free and chores-free.  Cassie doesn't know the social committee is a front for prostitution.  After several sisters die, Cassie vows to put an end to it all.<br/><br/>Give this movie a chance.  With the setup, it's easy to dismiss it as background noise.  During the sting, it becomes a full-blown mystery, twice.  This movie will pleasantly surprise you with its compelling twist.  Cassie will blossom from a timid, naive girl to an intelligent, strong woman within two hours.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Ruthless Realtor",
     "review":"Meg shows Annie and Ralph Savage around a gorgeous home. As they leave, Lynette, shows up on the doorstep, demanding Meg to give her some answers.  Meg promised Lynette that home and kept it off the market for her.  Meg doesn't believe Lynette is the right fit for the house; however, the Savages are the perfect fit. A few weeks later, the Savages are moving in, and to their surprise, Meg is there with treats in hand.  They feel Meg is out of order, but try to keep up appearances and be kind to her. The next day, Meg is cooking in their kitchen and bearing gifts.  She mounted Annie's pregnancy test.  They can't get her out of the house fast enough and agree to keep her out.  Meg comes to Annie's job to apologize, but things quickly get out of hand, and Meg is pulled away by security.  She starts to send threatening texts to Annie and warns Annie the police will never find her.  Next, Meg reveals another secret.  She lied so they can get the house. Annie and Ralph will put their lives on the line for the home of their dreams.<br/><br/>Keep your eye on this movie.  It takes you down a familiar path of Lifetime movies but gives the experienced watcher a fun twist.  If you are paying attention, you will hear the truth the plot sprinkles along the way.  The actors did a great job of misleading the audience by playing into the Lifetime characters we know, love, and hate.  All Lifetime watchers will enjoy this movie.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Secrets in the Basement",
     "review":"Barbara found the home of Delilah and Shawn's dreams. Delilah sees all the markings of a basement but doesn't see one.  Barbara tells Delilah that she will double-check the plans and let her know if there is a basement.  During a trip to the hardware store, Delilah runs into Jay, the man who designed her home.  He is curious to see what she has done to the house.  Delilah allows Jay to follow her home.  During the tour, the two start to click, and Delilah wants to work for him.  After a few short moments, Jay agrees to allow Delilah to work with him. As work starts to mount, Delilah is collapsing under the pressure.  Delilah sees figures moving, unknown decor appearing, and cabinets opening on their own. As the old saying goes, just because you're crazy, it doesn't mean they aren't after you. Someone is out to get her, and they want her to pay for a huge mistake.<br/><br/>Secrets in the Basement isn't what you would expect from the title, but it does cover the Lifetime basics.  You have the over-trusting lead going crazy, the husband trying to help, and the nosy best friend that never disseminate all the necessary information at one time.  While the movie does try to add suspense for the person in the basement, experienced Lifetimers will be able to figure it out in 5 minutes.  The killer's motives are what drives viewers to watch.<br/><br/>2 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretsinthebasement.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretsinthebasement.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Psycho Sister-in-Law",
-    "review":"At his father's funeral, Nick spots a young woman standing far away but engaged.  He recognizes the woman is his sister Zara.  Zara is the illegitimate child of Nick's father, Gavin, and Zara's mother. Before he passed away, Gavin proposed to Callie, and he happily celebrated Nick and Hailey's first pregnancy.  Now, Nick and his wife, Hailey, are allowing Zara and Callie to stay in the home until the will reading.  In the will, Gavin left $100,000 for Zara, $600,000 for Callie, and everything else to Nick and his unborn offspring. Zara can't believe Gavin treated Callie better than her and Nick is treating Callie like family too.  Zara has a plan in store.  She will take over the home and the money.  To see her master plan through, she needs one thing: Nick and Hailey's unborn baby.<br/><br/>Thank goodness for backhanded remarks and twist.  The infighting between the women will have you clutching your pearls and laughing out loud.  While having a twist is expected, the twist itself is a compelling reveal.  Be forewarned: you have to catch this movie from the beginning to see Zara's first kill.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psychosisterinlaw.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psychosisterinlaw.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Wedding Every Weekend",
-    "review":"Ginny encourages her friend, Brooke, to make this year the year of Brooke.  Over the next month, Brooke will attend a different wedding.  To avoid the singles' table, old Brook would stay in her boring relationship with Colin.  Now, Brooke will enjoy the single life and focus on creating a physical therapy practice.  During Gretchen and Scott's wedding, Brooke runs into one of her complicated patients, Nate.  Nate restores cars and loves DIY projects, including medical care.  She sees him limping around the wedding and asks to see his knee.  With pseudo pride, he shows the horrible tape job he did.  After a stroll, Brooke learns Nate is avoiding the dating world too.  He had a bad break up and wants to focus on his auto body shop.  The next weekend, Brooke and Nate are surprised to see each other at another wedding.  They realize they will be attending two more weddings in 2 weeks.  Each encounter will bring camaraderie and romance for the pair.  But their love doesn't come without challenges from their past and their present.<br/><br/>Congratulations to everyone involved in this movie.  This movie is one of the most original Hallmark storylines in a long time.  It is well-acted, well cast, well design, well-directed, and enjoyable from beginning to end.  Every character, every wedding, and the overall love story will have you well invested in the movie. Be prepared to smile, cry, and fall in love while watching.  Wedding Every Weekend is the instant classic Hallmark movie that Hallmarkies have wanted, needed, and deserved.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/weddingeveryweekend.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/weddingeveryweekend.jpg"
     }
   }
 ];
