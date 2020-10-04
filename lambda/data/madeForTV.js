017aac8d2f4ce8f58633074cdadbd3c6a8296128
Tiffany Gray <KarateTD@yahoo.com>
2020-10-03 22:13:59 -0400

    add four tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Dying to Be a Cheerleader",
+    "review":".<br/><br/>* out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dyingtobeacheerleader.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dyingtobeacheerleader.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Deadly Double Cross",
+    "review":"Best friends since kindergarten, Kelly and Amber start their new school year with heightened anticipation.  They both have boyfriends, Kelly is dating Zander and Amber is dating Alex. After school, Amber meets up with Alex, and they go for a date in the park. They start to kiss, but he tries to go too far.  Amber stops him and tells him she wants to go home.  Alex calls her a prude and breaks up with her.  Seeing her daughter is sad about the breakup, Lisa tells Amber to go to a back to school party and have some fun with her friends.  Upon arrival, Amber sees Kelly, and they begin to chat.  Kelly offers to introduce Amber to Zander, and Amber his happy to meet him.  Until she sees his face.  It's Alex.  Amber tells Kelly immediately, and they both storm off. The next day, Kelly blows Amber off, and Amber is unsure why. At school, Amber grabs Kelly's phone and sees she is still texting Zander, and they fight in the school's courtyard.  Since this is the girls' first offense, the principal gives them a slap on the wrists.  But the law won't be so kind after someone finds Zander at the bottom of the stairs with a large gash in his head. After Zander dies and Amber ends up in jail, Lisa will fight to save her daughter, and Heather is by her side.<br/><br/>With a riveting premise, the movie never makes the watcher feel a sense of urgency or panic.  Also, the trailers ruined the twist ending, but the 'why' is a mystery.  While the actresses did great, the script was slow, and the victim was repugnant. This movie is background noise for your daily work.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlydoublecross.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlydoublecross.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"JL Family Ranch: The Wedding Gift",
+    "review":"Patriarch and widower, John, loves the ranch, its tranquility, and all its wonders, but he loves his three adult children, Reagan, Becca, and Brady more. Reagan is turning the family ranch into a Bed and Breakfast. With the Bed and Breakfast, she can generate more money for the ranch.  To generate buzz about the Bed and Breakfast, Reagan invited reviewers to come and stay.  And to her surprise, Claudia and Frederick, came a day early.  Becca is a single mother of one and dating Sheriff Henry. While she is happy for her mother, Lynnie wants to find her father, Caleb.  Caleb left town before she was born, which caused a rift between Caleb's family and Becca's family. Lynnie knows John and Becca don't want to discuss Caleb, but she is determined to find her dad. With two failed proposals, one lost ring, and an unwelcome blast from the past, this family will have to put everything behind them to save a ranch, but not their ranch.<br/><br/>The second installment of JL Family Ranch packs the heart and drama expected from the HMM channel.  The first proposal will make you cry tears of laughter as Tad, Brady, and John grab their guns and prepare for battle.  The second proposal will have your jaw on the floor as an unexpected guest arrives.  When Henry calls off the wedding, the drama amplifies. When the movie ends, you will be ready for the third installment.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/jlfamilyranchtheweddinggift.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/jlfamilyranchtheweddinggift.jpg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Falling for Look Lodge",
+    "review":"Lily takes a bike ride around Look Lodge.  She bikes up the driveway, and Noah opens his car door without looking.  Lily swerves to avoid hitting the door and hits the ground instead.  She starts to chastise him but stops after realizing he is a guest.  She does an about-face and apologizes. The activities director quit with very little notice, and he didn't leave the activities for the wedding party.  Lily wants to prove she is ready for the job, so she asks her sister, Charmaine, who is also the owner, to plan the wedding party's activities for the week.  Charmaine agrees to give her the job if the wedding party gives good feedback.  Lily meets with the bride and groom, Justine and Greg, his parents, Alice and Russell, and Greg's daughter, Beatrice. Lily tries to coax the plans out of Justine, but Justine left the planning up to the former director and her brother, Noah.  Justine turns to Noah to get the information, but she gets a blank stare. Lily jumps into to his rescue.  She offers to work with Noah to plan everything and get him away from his computer. She wants to bring them all together.  But Noah can see Lily isn't fulfilling her true passion, owning an adventure tour company.  They both set a plan in motion to save each other.<br/><br/>This movie feels obvious but goes in an entirely different direction.  It focuses more on Noah rebuilding his relationship with Justine while creating new bonds with Greg's family.  You will instantly fall in love with Beatrice and Alice. With its fair share of laughs, awkward moments, and fun, you will enjoy it with ease.  And let's not forget Kate finding love too.  Sit back and get comfortable, because you will love this movie.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fallingforlooklodge.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fallingforlooklodge.jpeg"
+    }
+  },
+  {
+    "option":"5",
     "mtitle":"Her Deadly Sugar Daddy",
     "review":"Bridget moved across the country with her best friend, Lindsey. Bridget's mother, Jolene, tells Bridget to get a job to pay the bills.  Bridget did apply to the Swerve blog, but the boss didn't believe she had enough life experience. At dinner with Lindsey and Jolene, Bridget catches the eye of Anthony.  But she believes he was looking at Jolene.  Without her permission, Lindsey puts Bridget on a dating app, and she has a match. While waiting on her date, Anthony sits down and introduces himself.  He tells Bridget she was the woman who caught his eye.  Then, Anthony gives Bridget his well-made business card and offers her a job as his assistant.  After waiting for hours, Bridget's date finally appears, and he is obnoxious.  Bridget leaves, but her date follows and harasses her.  Anthony stops the attack and offers her a ride home.  With her white knight's kind gesture, Bridget takes the job.  She stops in her tracks to learn the job pays $10,000 a month. At dinner with a client, the client starts to get handsy with Bridget.  She attests and tells Anthony to make the client stop.  Anthony says its part of the deal, and she doesn't have to do more than what she is willing to do. With a new sense of misplaced power, Bridget keeps the job and uses it to write her blog.  While Bridget thinks Anthony is a sugar daddy, she has no idea Anthony is a pimp, and he has killed before.<br/><br/>Bridget talking about feeling empowered by being pimped is the saddest moment in this movie. Her misguided empowerment parallels her misguided need for life experience for her blog.  Her saving grace is the strongest woman in this movie: Jolene.  Jolene doesn't fall for the nonsense Bridget spews about her new job.  Jolene quickly calls B.S. and tells her daughter to find a new line of work.  After that doesn't work, Jolene expresses her deep disappointment in a way only a mother can. If you plan on watching this movie, watch for Jolene.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/herdeadlysugardaddy.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/herdeadlysugardaddy.jpg"
-    }
-  },
-  {
-    "option":"2",
-    "mtitle":"Follow Me to Daisy Hills",
-    "review":"Jo prepares her general store for the morning when her sister, Sophie, walks in.  Jo takes a moment to razz Sophie about her first published article in the Daisy Hills newspaper, but gets serious about the mail.  Jo saw a large letter from Iowa State addressed to Sophie.  Jo had no idea Sophie was applying to school out of state, and she worries that Sophie won't come back. Jo and Sophie's father, Mr. Mason, has to resolve the store's finances.  Without changes to the business, the general store will have to close its doors and soon.  So, Mr. Mason calls Blake, who lived in Daisy Hills, but left town for college and a job in New York, to help him keep the store.  At Sophie's graduation party, Mr. Mason tells Jo he has a surprise for her.  Awestruck, Jo sees it's Blake, her ex-boyfriend. Angry, Jo lets her feelings known to Blake but abruptly stops when Mr. Mason experiences chest pains.  To ease Mr. Mason's stress, Jo agrees to listen to Blake's three ideas.  First, create an online presence so people can shop online instead of the small town.  Second, rearrange the store based on social engineering.  Third, update the store with modern technology to save on energy costs.  Jo says no to all of Blake's ideas. To save the store, Blake and Jo have to compromise on how to update the store but within the Daisy Hills's small-town feel.<br/><br/>Are you ready Hallmarkies? This movie features the first-ever drunken tweet in a Hallmark movie.  And it's a doozy.  Jo's fear of technology comes from the fact that she doesn't know how to use it.  You will know real horror as you watch Jo unknowingly post her message to the whole world.  To make matters funnier, after the drunken post, she leaves a drunk voicemail.  That one scene is worth the entire movie. This movie can become a weekly Hallmark series.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/followmetodaisyhills.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/followmetodaisyhills.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Secret Life of a Celebrity Surrogate",
-    "review":"Within hours, Liv loses her job, her apartment, and her boyfriend.  She puts a post on social media to find a job that will get her out of New York immediately.  Any job will do.  Moments later, Cassidy replies with an intriguing opportunity.  Cassidy works for Hayden and Ava.  Hayden and Ava are a high-profile celebrity couple. Cassidy tells Liv they want to hire a surrogate to carry their child.  They will provide room and board.  When the baby is born, they will give Liv $150,000. Liv calls Cassidy and says she will do it.  She flies to LA and meets the couple.  While they are eccentric, they are kind, loving, and shower Liv with gifts.  So, Liv says yes to being their surrogate.  Soon, Liv is making trips to the doctor, making trips to the pharmacy, getting shots, and being weighed everyday.  Liv believes she is doing the right thing until she sees a fight between Hayden and Ava get physical and Ava's abuse of drugs and alcohol.  Cassidy attempts to assure Liv, but its too little too late.  Liv wants answers, but can she handle them?<br/><br/>Talk about a fun twist! With so many storylines involving surrogacy, it's hard to differentiate them.  However, this movie does just that.  It becomes a mystery with breadcrumbs throughout the story to give the viewers clues to Ava's motives. You will clutch your pearls when Ava and Hayden make Liv public enemy number one, terminate the contract, and punish Liv.  This movie is worth the watch.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretlifeofacelebritysurrogate.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretlifeofacelebritysurrogate.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Sinfidelity",
-    "review":"Angela is a successful, married, mother of one.  While celebrating her husband's, Greg, pending merger, she gives her company's wine as parting gifts.  At the party, Angela notices Greg getting cozy with his secretary Lisa. Angela confides in her best friend Claire.  With little evidence, Claire tells Angela to keep her eyes open.  Then, Angela meets Franco, Greg's accountant during the merger and photographer at the party.  Angela mentions financial struggles in her company, and Franco offers to find someone to help her. Franco explains his friend is busy, but, to help, he will do it for free.  Angela is happy to hire him.  At night, Angela picks up Greg's phone and sees unsettling texts from Lisa.  Angela's fears are confirmed, Greg is cheating. Claire tells her to get even and have an affair. Angry, but in financial need, Angela keeps the appointment and meets Franco to discuss her books.  The discussion moves to his apartment.  The night ends in Franco's bed.  The next morning, Angela comes home to get some of her belongings.  With pain, anger, and fear, Greg waited for her.  Greg comes clean about all his secrets. If he doesn't get the merger, they will lose the house.  And Greg asked Lisa to hide his biggest secret, a trip to Italy for their 10th anniversary.  He never had an affair.  She tells Franco she's staying with her husband and he erupts.  He threatens to ruin Greg's merger if she doesn't leave Greg.  If she tells Greg, she could lose her family.  How far will Franco go to make Angela come clean?<br/><br/>The biggest twist in this movie is Greg.  While he cheated in the past, he did not cheat this time. But the secret Greg keeps puts his family's financials in danger and Angela trapped.  At first, she thinks she can ignore Franco but he dangles his merger power over her to pull her back in.  She has no idea how dangerous he is.  But you do, in the first 5 minutes.  This movie is a predictable watch, but fun.  So, sit back and have your tea ready.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sinfidelity.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sinfidelity.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Deranged Granny",
-    "review":"After losing their child during childbirth, Ethan and Lucy's marriage fell apart.  Ethan's mom, Barbara, thought another child was a solution, but Lucy wouldn't consider it.  After the divorce, Ethan cut Barbara out of his life.  He felt her overbearing ways strained his marriage.  Two years later, Ethan meets Kendall.  She has two children, Amy and Bobby, and co-parents with her ex-husband, Calvin. After months of dating, Ethan asks Kendall to marry him. So, Kendall finds Barbara and, without Ethan's knowledge, invites Barbara over their home for lunch.  Ethan decides to bury the hatchet and lets Barbara back into his life.  Barbara meets Amy and Bobby and can't wait to be their grandmother.  She dotes on and spoils them with trips, treats, and technology.  With the kids on Barbara's side, she will manipulate, threaten, and poison anyone who tries to take them away.<br/><br/>Barbara is truly a deranged grandma.  Covered under the guise of normal grandma activities, Barbara can infiltrate Ethan and Kendall's life and turn the children against them.  With linguistic programming, Barbara creates secrets, drives wedges, and makes divisive comments to accomplish her goal of dominating the family.  The plot gives the viewer a glimpse of why Barbara is fixated on having children but doesn't delve too much more. Be ready to be on the edge of your seat.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/derangedgranny.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/derangedgranny.jpg"
     }
   }
 ];
