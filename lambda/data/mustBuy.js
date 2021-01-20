1ae4ab6adf5327aaca2314c4bfe76c45b37aac35
Tiffany Gray <KarateTD@yahoo.com>
2021-01-19 19:54:06 -0500

    add 2 must buy

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,24 @@
 var mustBuy = [
   {
     "option":"1",
+    "mtitle":"The Kid Detective",
+    "review":"As a child, Abe Applebaum had a nose for mysteries.  His principal came to Abe with his first case when Abe was 13.  It was the case of the missing fundraiser money.  Abe picks a suspect, and the principal recovered the money.  Soon, Abe was running a business out of his treehouse, and the police even called to consult him on bigger cases.  When someone chopped down his treehouse, the thankful town got him an office on Main Street.  The mayor's daughter, Grace, was his receptionist, and he paid her with soda.  His business was booming, and Abe was in the newspaper every week, but that all changed when Grace went missing.  The town and Abe expected Abe to find her, but when he couldn't, Abe's town and Abe fell into a depression.  Today, at 32, Abe is self-loathing, solves mediocre mysteries, and has more debt than cases. As he walks home from work, he sees police combing through the river. The police found Caroline's 15-year-old boyfriend stabbed to death in the river, and they have no leads. So, Caroline asks him to find who murdered her boyfriend.  He asks if she is sure because the truth can be shocking.  Caroline is sure and hires Abe.  This murder case will be his first adult case and a chance to clear Grace's case from his soul.<br/><br/>This movie goes from Encyclopedia Brown to 8mm in less than two hours.  It's a mix of Criminal Minds' unsubs with Psych's hijinks and wit.  You will laugh at the inappropriateness of Abe as he hides in a little girl's bedroom after spying on a teenage boy.  This story is perfection because Abe carries the weight of the town on his shoulders, and you can see the moment the weight lifts.  If you have ever gone through a hard time, you will connect with Abe's tears. When Abe finds the murderer, he will learn the truth about his town and himself.  This movie will become a cult classic.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekiddetective.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekiddetective.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Climb",
+    "review":"In a few weeks, Kyle will marry Ava, so his best friend, Michael, takes him on a bike ride to help him lose weight.  As they bike uphill, Michael admits to sleeping with Ava before Kyle knew her and continuing the affair after the engagement.  Kyle tries to catch up to Michael but can't. At the top of the hill, Kyle sees Michael fighting a motorist and watches.  Kyle takes Michael to the hospital, and Michael promises to end the affair with Ava.  Kyle walks away to get more information from a doctor and comes back to find Ava and Michael kissing.  Ava admits she loves Kyle but doesn't want to marry him.  Three years later, Kyle walks to a gravesite to pay his respects and watch Michael have a breakdown over Ava's casket. They try to be civilized, but their conversation ends in blows.  Months later, Kyle spends time with his family for the holidays.  He's dating Marissa, a woman he broke up with in high school, and his family doesn't like her.  Then, Kyle drops the bomb that he and Marissa are getting married, and his family is less than enthused.  Kyle's mom counters his bomb and tells Kyle she invited Michael. This invite will stir up old wounds and create new ones.<br/><br/>In six parts, this movie will make you laugh at the most horrible moments in Kyle's life.  This toxic friendship spans years with little resolution for the outside influences affected by it. While they should stay away from each other, Kyle's family continues to meddle and attempts to fix the friendship.  Like most independent movies, they bookend the start and the conclusion.  While you will want more for Kyle, you will realize this is the only solution for him and Michael.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theclimb.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theclimb.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Honest Thief",
     "review":"Tom met Annie a year ago at a storage facility where she worked, and he wants to continue his future with her without looking over his shoulder every day.  The FBI knows Tom as the In-and-Out Bandit. Tom buys a house for him and Annie, then he goes to a motel and calls the FBI.  Agent Sam Baker picks up the phone and doesn't believe Tom's story. So, he gives the phone to his partner, Detective Meyers. Over the years, he has stolen $9 million.  He is willing to give it all back if they give him what he wants.  Tom wants to be held for a maximum of two years in a minimum-security prison with full visitation rights.  Meyers still doesn't believe him, so they hand the case to Agent Ramon Hall and Agent Jason Nivens. After meeting Tom, they believe Tom is another liar, so Tom tells them how he committed the crimes.  Tom gives them the location of $3 million.  They go to the storage unit expecting to find nothing.  However, they find the motherload.  John gets an idea.  They can take the money, kill Tom, and tell Baker and Meyers it was all a hoax. Ramon hangs his head and says yes. Ramon and John walk into the hotel room, and John is prepared to kill Tom, but there is a knock at the door.  It's Agent Baker.  Baker walks inside but can tell something is off, then John shoots him point-blank in the head. Tom jumps in Annie's car and drives away.  Tom has to come clean with the woman he loves, clear his name of murder, and peacefully turn himself in before John and Ramon kill him and Annie.<br/><br/>Yes, this is another Liam Neeson kills-everyone movie, but that's why it's fun.  Tom never claims to be perfect or a good guy.  However, he tells everyone he is honest. It's interesting to contrast what drives each pair to fight together.  Tom and Annie find strength through love and the future.  And for Myers, he fights to get justice for Baker and find the person who killed him.  For Ramon and John, their partnership stands on blackmail and money.  Seeing the pairs work together or for each other changes throughout the film, and it will be their downfall or salvation.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"TENET",
     "review":"TENET was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access",
     "image":{
@@ -18,32 +36,14 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Unhinged",
     "review":"Rachel got some bad news; her soon-to-be-ex-husband wants the house.  Exhausted from a late-night, Rachel tells her lawyer, Andy, she will handle it later because she is running late to take her son, Kyle, to school and go to work.  During the drive, Rachel gets a call from her client.  When the client hears Rachel will be late again, the client fires Rachel over the phone. She decides to get off the freeway and take Kyle to school using the backstreets.  At the light, she sits behind a pickup truck, but the driver doesn't move during the green light.  Rachel lays on the horn, then drives around.  At the next light, the pickup truck driver asks Kyle to roll down his window.  He asks if Rachel knows what a courtesy honk is and why she didn't use it.  Frustrated, she tells the man he was in the wrong.  The man apologizes for being upset and ignoring the light.  In turn, the man wants Rachel to apologize too.  She refuses to apologize.  The man vows to make her see what a bad day is.  Then, he starts to follow her down the street, leaving accidents and injuries in his wake. Rachel gets gas and sees the man behind her car.  She alerts the gas station clerk and a good Samaritan. The man hits the Samaritan and stalks Rachel on the road. Rachel doesn't know the man killed his ex-wife and her new husband just hours prior.  He is a monster with pure road rage, and Rachel is in his crosshairs.<br/><br/>Unhinged is a thriller, suspense, and horror movie all rolled into one.  You must be in your seat during the opening credits to see news clips of people losing it on the road.  By witnessing the unhinged man kill in the beginning, it helps you understand that Rachel isn't the driving force behind his actions.  She is just the next victim of his rage. This hard-hitting movie will make you jump in your seat, shock you, and leave you with a lasting impact.  See it in theaters today.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/unhinged.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/unhinged.jpg"
     }
-  },
-  {
-		"option":"4",
-		"mtitle":"Disney's Mulan",
-		"review":"Hua Zhou sees his daughter, Mulan, who has a strong connection to the qi inside her, practicing her martial arts skills. However, he knows tradition says the qi is for men.  If Mulan uses it, she will be labeled a witch and shunned.  So, he tells Mulan to hide her gifts and bring honor to the family by marrying.  Years later, Mulan has an appointment to see the matchmaker. Mulan, connecting to her qi, catches the tea set, but she slips and drops it.  The matchmaker tells Zhou the Hua family has failed to raise a good daughter, and Mulan brought shame to his family.  Shamed, Mulan leaves the matchmaker with Xiu and her mother, Li, and sees the Imperial Army entering their village.  Bori Khan, a Rouran warrior, is on a mission to destroy Imperial China to avenge his father's death.  He has Xhin Lang protecting him.  Xhin Lang was labeled a witch and cast out of her village because she connected with her qi. The Army delivers the Emperor's decree that every family should give one son to fight the war.  Now, Zhou, a decorated and injured soldier, steps up because he has no sons. Zhou's volunteerism is accepted.  Mulan tries to convince her father not to go, but he boldly and abruptly stops her plea. As her family sleeps, Mulan makes a choice.  She takes her father's armor, horse, sword, and decree scroll and heads for the woods.  She will pretend to be her father's son, Hua Jun, and take her father's place in the Army. Zhou prays for their ancestors to protect Mulan on her journey to become a legend.<br/><br/>Based on Disney's Mulan and The Ballad of Mulan, the live-adaptation of Mulan has some glaring differences from the original movie.  However, it is a bold addition to the franchise.  With glorious landscapes, fantastic wardrobes, and magnificent fight scenes, Mulan will captivate you from beginning to end. With a few changes, viewers get a new story but the same message, never hide your gifts.  This movie classic isn't updated; it's renewed.  And it's enjoyable for the whole family.<br/><br/>4.5 out of 5 stars.",
-		"image":{
-		  "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/disneysmulan.jpeg",
-		  "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/disneysmulan.jpeg"
-		}
-	  },
-  {
-		"option":"5",
-		"mtitle":"Bill and Ted Face the Music",
-		"review":"After playing their famous concert, Wyld Stallyns hasn't had a hit record in decades.  In turn, they haven't written their foretold song to unite the world.  Ted is married to Princess Elizabeth, and they have a daughter named Wilhelmina, also known as Billie.  Bill is married to Princess Joanna, and they have a daughter named Theodora, also known as Thea.  Billie and Thea have a deep passion for music of all genres, and they believe in their dad's ability to unite the world in song.  However, their wives, less so.  Bill and Ted learn their wives are unhappy and are contemplating a divorce.  The men leave the women to talk to the counselor and head home. That's when Kelly, Rufus's daughter, appears.  Kelly takes Bill and Ted to the future to see the Great Leader.  Time and reality are folding on itself.  If Bill and Ted don't write their song, the world will end.  And they must write the music and play it at 7:17 pm at MP 46, which is in 77 minutes.  Bill and Ted decide to steal the telephone booth, go to the future, and steal the song from themselves to save the world.  The Great Leader has lost faith in Bill and Ted and sends a robot to kill them. In fear, Kelly travels back to 2020 to warn Bill and Ted of the Great Leader's alternate plan, but she finds Billie and Thea instead.  Billie and Thea decide to help Bill and Ted by gathering the best musicians in time to back up their dad's band.  With three missions throughout time, who's mission will save the world?<br/><br/>The third installment of Bill and Ted's Excellent Adventures is everything you would expect from the franchise while coming full circle.  As Bill and Ted travel to meet future and worse off versions of themselves, Billie and Thea take the viewers on a musical journey. With their dad's laid-back nature and love of music, they embody everything you love about Bill and Ted while righting their wrongs.  This movie is a love letter to the cult classic fans.<br/><br/>4 out of 5 stars.",
-		"image":{
-		  "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/billandtedfacethemusic.jpg",
-		  "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/billandtedfacethemusic.jpg"
-		}
-	}
+  }
 ];
 
 module.exports = mustBuy;
@@ -97,16 +97,6 @@ module.exports = mustBuy;
   }
 
 {
-    "option":"4",
-    "mtitle":"The Climb",
-    "review":"In a few weeks, Kyle will marry Ava, so his best friend, Michael, takes him on a bike ride to help him lose weight.  As they bike uphill, Michael admits to sleeping with Ava before Kyle knew her and continuing the affair after the engagement.  Kyle tries to catch up to Michael but can't. At the top of the hill, Kyle sees Michael fighting a motorist and watches.  Kyle takes Michael to the hospital, and Michael promises to end the affair with Ava.  Kyle walks away to get more information from a doctor and comes back to find Ava and Michael kissing.  Ava admits she loves Kyle but doesn't want to marry him.  Three years later, Kyle walks to a gravesite to pay his respects and watch Michael have a breakdown over Ava's casket. They try to be civilized, but their conversation ends in blows.  Months later, Kyle spends time with his family for the holidays.  He's dating Marissa, a woman he broke up with in high school, and his family doesn't like her.  Then, Kyle drops the bomb that he and Marissa are getting married, and his family is less than enthused.  Kyle's mom counters his bomb and tells Kyle she invited Michael. This invite will stir up old wounds and create new ones.<br/><br/>In six parts, this movie will make you laugh at the most horrible moments in Kyle's life.  This toxic friendship spans years with little resolution for the outside influences affected by it. While they should stay away from each other, Kyle's family continues to meddle and attempts to fix the friendship.  Like most independent movies, they bookend the start and the conclusion.  While you will want more for Kyle, you will realize this is the only solution for him and Michael.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theclimb.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theclimb.jpg"
-    }
-  }
-
-{
     "option":"1",
     "mtitle":"Let Him Go",
     "review":"George and Margaret work on their Montana ranch with the help of their son, James, and James's wife, Lorna, and their baby, Jimmy.  Margaret looks outside the window and sees James's horse, but no James.  George finds James dead on a pile of rocks next to a creek.  Three years later, George and Margaret attend Lorna and Donnie Weboy's wedding. A few days later, Margaret sits in her car outside the grocery store and sees Donnie, Lorna, and Jimmy walking while eating ice cream cones.  Then, Donnie laughs at Jimmy because he dropped his ice cream.  When Jimmy refuses Donnie, Donnie hits Jimmy.  Lorna comes to Jimmy's defense, and Donnie hits her too.  They leave.  Margaret doesn't tell George what she saw at the grocery store.  Instead, she makes a cake and takes it to Donnie's home, but the family is gone. Margaret comes home and prepares for a trip.  When George gets home, he knows something is wrong. Margaret believes Lorna can't defend herself and their grandson.  So Margaret will get Jimmy back. George closes his suitcase, and they head down the road.  George and Margaret don't know the Weboy Family is powerful in North Dakota.  The matriarch, Blanche, rules with an iron fist, shotgun, and a lit cigarette. She will never let any of the Weboys walk away again, including Jimmy.<br/><br/>Based on Let Him Go by Larry Watson, this movie is a slow burn but picks up at the end.  Just a warning, some scenes can be pretty gruesome.  But what you are witnessing is three mothers fighting for the boy they love. In the end, it all comes down to Jimmy and how much people are willing to sacrifice to keep him. Viewers get a Kill Bill Vol. 2 moment, and it's worth it.<br/><br/>4 out of 5 stars.",
@@ -116,15 +106,7 @@ module.exports = mustBuy;
     }
   }
 
-  {
-    "option":"4",
-    "mtitle":"The Kid Detective",
-    "review":"As a child, Abe Applebaum had a nose for mysteries.  His principal came to Abe with his first case when Abe was 13.  It was the case of the missing fundraiser money.  Abe picks a suspect, and the principal recovered the money.  Soon, Abe was running a business out of his treehouse, and the police even called to consult him on bigger cases.  When someone chopped down his treehouse, the thankful town got him an office on Main Street.  The mayor's daughter, Grace, was his receptionist, and he paid her with soda.  His business was booming, and Abe was in the newspaper every week, but that all changed when Grace went missing.  The town and Abe expected Abe to find her, but when he couldn't, Abe's town and Abe fell into a depression.  Today, at 32, Abe is self-loathing, solves mediocre mysteries, and has more debt than cases. As he walks home from work, he sees police combing through the river. The police found Caroline's 15-year-old boyfriend stabbed to death in the river, and they have no leads. So, Caroline asks him to find who murdered her boyfriend.  He asks if she is sure because the truth can be shocking.  Caroline is sure and hires Abe.  This murder case will be his first adult case and a chance to clear Grace's case from his soul.<br/><br/>This movie goes from Encyclopedia Brown to 8mm in less than two hours.  It's a mix of Criminal Minds' unsubs with Psych's hijinks and wit.  You will laugh at the inappropriateness of Abe as he hides in a little girl's bedroom after spying on a teenage boy.  This story is perfection because Abe carries the weight of the town on his shoulders, and you can see the moment the weight lifts.  If you have ever gone through a hard time, you will connect with Abe's tears. When Abe finds the murderer, he will learn the truth about his town and himself.  This movie will become a cult classic.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekiddetective.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekiddetective.jpg"
-    }
-  }
+  
 
 	
 {
