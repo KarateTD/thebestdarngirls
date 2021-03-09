cbbe999f7c354da7cb89f38dad262a1de85f81e7
Tiffany Gray <KarateTD@yahoo.com>
2021-03-09 09:23:28 -0500

    3 in theater movie

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Chaos Walking",
+    "review":"In 2257 AD, only men and the Spackle populate the new world.  The noise is a condition that allows others to see their thoughts, and it afflicts only men.  Years ago, the Spackle killed all the women, including Todd Hewitt's mother.  Now, Todd is the youngest member of Prentisstown, and Ben and Cillian raise him.  While doing chores, Todd sees someone coming out of the barn, but he notices they don't have noise.  Todd chases the figure to a crash site and sees the person is a girl.  Todd tries to control his thoughts while running to tell Mayor Prentiss, but Todd can't keep his noise down. The mayor finds the girl and brings her back to his headquarters.  The mayor tries to get information, but the girl won't talk. Hearing the girl has escaped their grasp, the men look for her again, but Todd goes home.  Ben and Cillian hear Todd's noise about the girl.  They open a map and stab it to mark Farbranch. They tell Todd to save the girl from the mayor by going to Farbranch.  With a motorcycle and a horse, Todd and the girl, Viola, run.  Todd will discover the truth about Prentisstown on his noble journey.<br/><br/>Based on The Knife of Never Letting Go by Patrick Ness, this movie doesn't end like the books, so it's hard to believe there will be a part 2.  The visual noise effect is interesting to see, but hearing it can be jarring.  The fight scene between Viola and the preacher is anticlimactic for such a promising build-up.  You won't be bored watching this movie, but you won't have an urge to read the book.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chaoswalking.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chaoswalking.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Boogie",
+    "review":"After his father, Mr. Chin, got out of jail, Mr. Chin made Boogie change schools to join a different basketball program.  If he beats the best, Monk, then college scouts will give Boogie a full athletic scholarship for college. Mr. Chin believes Boogie can make it to the NBA, but his mother feels something different.  His mother, Mrs. Chin, can see that Boogie has an attitude problem that spills over on the basketball court. When Boogie's attitude causes another school to offer him a spot on the team without a scholarship, his new manager, Melvin, has a new offer with the CBA.  If Boogie accepts this offer, he will have to leave his team, give up college, and leave his new girlfriend, Eleanor, behind. Or he can keep trying for a college athletic scholarship that may not come.  With centuries of tradition weighing heavily on his shoulders, what will Boogie choose?<br/><br/>This movie speaks a lot about culture, responsibility, and family, but you never feel connected to a character.  This movie tries to be romantic as Love and Basketball, culturally relevant like White Men Can't Jump, and emotionally connected like The Farewell with an independent movie ending.  But the plot comes up short in comparison to all its counterparts. One watchable aspect is the movie's charming lead.  He is someone you can cheer for because he was counted out and counted on since his mother gave birth to him.  Seeing him fight to find his place between two worlds is worth the matinee price.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/boogie.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/boogie.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"My Zoe",
+    "review":"Isabelle and James battle in a courtroom weekly over custody of their daughter, Zoe. Since Isabelle will be out of town one of her weekends, she asks to pick Zoe up early to have time with her.  James says no and calls Isabelle a bad mother for going out of town.  When Isabelle returns, she picks up Zoe from James's house and notices that Zoe sneezes.  Zoe doesn't feel warm, so Isabelle believes it's a common cold and takes Zoe home. The next morning, Isabelle tries to wake Zoe up, but Zoe won't open her eyes.  The paramedics rush Zoe to the hospital, and doctors discover Zoe has a bleed on her brain.  Sadly, she will never recover.  Unable to deal with the trauma, Isabelle learns a doctor may have an unorthodox and illegal solution.<br/><br/>This movie moves from heart-wrenching to simply absurd to boring beyond belief.  The plot dragged on after it reached the climax.  With such a unique premise, it landed on cheap religious and political propaganda. Wait for free tv if you want to see this movie.<br/><br/>1 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myzoe.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myzoe.png"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Tom and Jerry: The Movie",
     "review":"Tom and Jerry are two animals without a home.  They start to fight, and Tom bumps into Kayla, a delivery woman, causing her to drop a customer's clean laundry in the street.  This encounter is the second time Kayla screwed up on the job, so her boss suspends her, but she quits instead.  Kayla walks to the Royal Gate Hotel, starts scavenging their free cookies, and sits next to Linda.  Kayla pretends to be an employee undercover to interview Linda and convinces Linda that she didn't make the cut. Kayla walks into Mr. Dubros's office with Linda's resumé.  Mr. Dubros wants to give Kayla a try, but Terence, the event manager, has reservations about her because he thinks she is lying. Social media influencers Preeta and Ben chose the Royal Gate for their extravagant wedding.  The press from this wedding will attract people from all over the world to stay at the hotel, so everything must be perfect.  As Terence gives Kayla the five-cent tour, they hear a scream come from the kitchen.  Chef Jackie's sous chef saw a mouse in the kitchen and can't cook with a rodent in the hotel.  Kayla offers to catch the mouse, and Mr. Dubros extends a permanent position if she does. As she hunts for the mouse, she hears a commotion in a hotel room.  Kayla opens the door to find Tom in a destroyed hotel room.  Then she remembers him from the park and makes him an offer.  If he kills the mouse, Kayla will buy him whatever he wants.  Tom agrees.  Now, Kayla has to trust that Tom can catch Jerry.  Then she can keep the job she stole before Mr. Dubros becomes the wiser.  She and Tom have no idea the fight Jerry will give them.<br/><br/>Based on Tom and Jerry by William Hanna and Joseph Barbera, this movie has all the hijinks you love from the original cartoon while melding it into the real world like Who Framed Roger Rabbit.  The writers pull this tale of wits into the 21st century by adding social media and clout to the plot. Blending the past with the future allows parents and kids to enjoy this movie together. So get the family together and have a welcome giggle at this classic cat versus mouse story.<br/><br/>3 out of 5 stars",
     "image":{
@@ -9,39 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Nomadland",
     "review":"After the USG Corporation plant in Empire Nevada closed down in 2012, Nevada discontinued their zip code.  While many moved on and created connections in a new town, newly widowed Fern decided to take her life in a different direction.  She wasn't ready to retire, but home felt lonely without her husband.  So Fern packed up her van, added some hidden features, and started her journey down the road.  She takes odd jobs to pay for her overnight parking spots, food, and laundry mat fees.  At the Amazon distribution center, Linda Mae tells Fern about the RTR or Rubber Tramp Rendezvous. The rendezvous is for people like Fern, who are not homeless but choose to be without a home.  They meet in the desert and exchange tips about living out of vans, RVs, and cars.  After the rendezvous, Fern heads down the road.  Fern has no idea she will see these people again, and they will help, impact, and define her and her choices in life.<br/><br/>Based on Nomadland: Surviving America in the Twenty-First Century by Jessica Bruder, at first, it's hard to figure out the plot of the movie.  However, this movie is all about Fern opening up and allowing people to glimpse inside her world.  The nomads she meets at the RTR try to welcome her into the fold but she declines.  But after a flat tire and a few cold nights, Fern discovers she needs these social connections. This film is a calming watch, but you won't have the urge to buy it.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nomadland.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nomadland.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Judas and the Black Messiah",
-    "review":"In 1968 Chicago, Bill O'Neal stands outside Leon's bar and notices a cherry red GTO.  He goes inside, flashes an FBI badge, and starts body-searching the patrons.  When he finds the keys inside a man's pocket, Bill arrests the man for grand theft auto.  Another man looks Bill in the eye and recognizes him.  He knows Bill is a con-artist, not FBI.  Bill grabs the keys, jumps in the car, locks the doors, and drives off as the patrons try to stop him.  Later, the police catch Bill in the stolen car.  They arrest Bill and put him in a room for questioning.  FBI Special Agent Roy Mitchell tells Bill he will do 18 months for the robbery and five years for impersonating an FBI agent.  Or he can pick another option. Mitchell offers to drop Bill's charges if he becomes an FBI informant to gather intel on Frederick Hampton.  Fred is the chairman of the Black Panther Party of Chicago.  Under constant threat of arrest, Bill is the lynchpin to one of the darkest days in FBI, Black Panther organization, and American history.<br/><br/>While the title seems extreme, the story of Bill and Fred mirror that of Judas and Jesus.  Bill pretended to be a friend, traded information for money and died like Judas. You will notice that when Bill is the focal point, the camera follows behind him. When he is with others, the camera is always in front or on the side of him.  This change in camera angle helps the viewer change perspective mentally and visually.  The music, setting, makeup, technology, and clothing were in line with the period.  This movie and its dismal conclusion for everyone involved will stick with you.  Not only will you watch again, but you will also start researching the names that history tries to forget.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/judasandtheblackmessiah.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/judasandtheblackmessiah.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Land",
-    "review":"Edee follows her sister's advice and talks to a therapist.  After losing her son and husband, Edee considers taking her life.  Then Edee gets an idea to leave her life for a new existence.  She buys supplies, an old fashion cabin, and a trailer. For a few days, she tries to catch fish, hunt, and grow food, but the wildlife thwarts her.  Then a harsh winter comes, and Edee stops fighting, lays down, and prepares to die.  She wakes up with Miguel and Nurse Alawa by her side and an I.V. bag in her arm.  They nurse her back to health but don't seek further medical attention because Edee refused.  Alawa goes back to work, but Miguel stays. So Miguel offers to teach Edee how to survive off the land. By learning to survive the wilderness, Edee discovers how to fight for her life.<br/><br/>With lots of quiet contemplation scenes like Cast Away, this movie makes the most out of silence.  For 30 minutes, Edee is alone with her thoughts, struggles, and pain. You never know what happened to her husband and son, only that they died.  And you won't learn Miguel's need to help her until the very end.  The theater is a great place to enjoy the full majesty of the nature shots.  While this is an intriguing movie, you only need to see it once to appreciate it.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/land.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/land.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The World to Come",
-    "review":"In 1856 New York, Abigail tends to her chores and helps keep track of the family's income.  Her husband, Dyer, is a farmer and has a ledger of the crops, animals, debts, and earnings. However, when they have a daughter, Nellie, Abigail puts her writing focus on her child's needs.  At the age of five, Nellie died from an illness.  Heartbroken, Abigail pours her emotions into her quill and refuses to have any more children. As Abigail works in the garden, a carriage on its way to church passes her, and she sees a woman sitting up front that grabs her attention.  A few days later, the woman introduces herself as Tallie.  She is married to Finney with no children, and he runs the general store. Each time they meet, Abigail writes about the new feelings growing inside of her and the hold Tallie has on her mind.  While Dyer is curious about their relationship and Abigail's small lies, it's Finney that pieces it all together, and he will do anything to keep his wife and her wifely duties to himself.<br/><br/>While this movie has breathtaking cinematography, the plot bounces back and forth.  At first, this film starts with a forbidden love story.  Then, it switches to a murder mystery that isn't a mystery.  With a letter, it turns into a Lifetime movie.  Finally, it descends deeper into grief and madness. This movie is exemplary for its cinematography, costumes, and romance, but the overall story fails.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theworldtocome.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theworldtocome.jpeg"
     }
   }
 ];
