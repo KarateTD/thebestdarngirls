34e7fd5d7f1b8739f28f1b6c8192de9d419aad72
Tiffany Gray <KarateTD@yahoo.com>
2021-03-22 23:41:14 -0400

    add 2 theater movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"The Courier",
+    "review":"In 1960, Oleg finds two American students, hands them a piece of paper, and tells them to take it to the American embassy. Oleg's information makes it into the hands of a CIA agent.  The CIA agent brings the info to an MI6 agent, and they discuss who to use as Oleg's courier. They find Greville Wynne.  A meek, unassuming businessman from Great Britain. The agents tell Greville that he will go to Moscow.  He will have a meeting with his usual sales pitch.  After the meeting, Oleg will approach Greville and assess Greville's ability to do the job.  Once the two have met, Oleg determines Greville is adequate and gives Greville more information. This hand-off was one of many. And if Greville wanted to give his family a fighting chance to survive a Cold War, he will continue to courier Oleg's information.  Oleg and Greville's partnership leaves a lasting impact on the world we live in today.<br/><br/>It's hard to believe Oleg and Greville's impact on the world then, the world today, and the world to come. These family men form an unbreakable bond and save the world.  They dodge the KGB, death threats, and poisonings to stand up for the world they want for their children. The acting, costumes, hair, and make-up were flawless.  The movie's pace slowed down as the script delved into their private lives, but delving allowed the viewer to see Oleg's and Greville's unique motivation for spying and treason.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecourier.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecourier.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Long Weekend",
+    "review":"Bart ignores calls from his therapist and his ex-fiancé.  After losing his job and apartment, Bart's depression starts to creep back into his life.  His friend, Doug, and Doug's wife, Rachel, invite Bart to stay in their garage until he gets his life in order. After his job interview, Bart goes to the movies and hides alcohol under his coat.  The ticket taker doesn't see Bart, but Vienna does. Soon, Bart feels Vienna jolting him awake and saying the movie is over.  Bart rises from his seat and leaves the theater. Vienna is new in town and wants to know where she can get alcohol like his.  Bart points out a hipster bar and a dive bar.  Vienna invites him to go with her.  They have a great night, but Bart notices some strange things about his new companion.  First, Vienna doesn't talk about herself.  Second, Vienna carries at least $10,000 in her bag at all times.  Third, and the weirdest thing is Vienna doesn't own a phone. Doug and Rachel believe that Vienna is weird but not as bad as his ex-fiancé.  They tell Bart to demand to know what secrets Vienna keeps.  On their next date, Bart convinces Vienna to admit the truth when they are alone and safe.  When she tells her story, Bart will end up in the hospital twice with an unbelievable tale.  But was it ever true?<br/><br/>With a rom-com clumsiness, these two meet, fall in love, and sleep together on the same day.  The on-screen couple's visible connection makes this plot undeniable.  It feels like the typical boy meets girl story until Vienna releases her secret.  Don't be surprised if you roll your eyes during her reveal. By the end of the movie, you will either be happy or confused.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/longweekend.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/longweekend.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"The Father",
     "review":"Anne rushes to her father, Anthony's flat because he caused a mess again.  He insulted his nurse and threatened her.  The nurse quit, and Anne must find someone else to care for him.  She tells Anthony that she found a new nurse named Laura. Anne met a man and wants to move to France to be with him.  She needs to make sure her father will be safe while she is gone, but she promises to visit him on the weekends.  Anthony laughs and refuses the help because he doesn't think he will need it. Anthony walks into the living room and sees a man he doesn't know sitting in his chair.  The man introduces himself as Anne's husband and says the place is his flat, not Anthony's.  Then Anne walks through the front door, and it's a different woman.  Anthony believes Anne is trying to trick him into giving up his flat. Can he hold it all together?<br/><br/>Based on the play Le Père by Florian Zeller, this movie gives the viewer a first-person perspective into dementia.  The flat, people, and scenery change by the minute, leaving the viewer just as disoriented as Anthony.  For a moment, you will wonder if someone is gaslighting Anthony to convince him to move or get help.  But at the end, you will see the pure sadness of a man lost in his mind.  While the story is compelling and well shot, you won't have an urge to buy it or watch it again.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Chaos Walking",
     "review":"In 2257 AD, only men and the Spackle populate the new world.  The noise is a condition that allows others to see their thoughts, and it afflicts only men.  Years ago, the Spackle killed all the women, including Todd Hewitt's mother.  Now, Todd is the youngest member of Prentisstown, and Ben and Cillian raise him.  While doing chores, Todd sees someone coming out of the barn, but he notices they don't have noise.  Todd chases the figure to a crash site and sees the person is a girl.  Todd tries to control his thoughts while running to tell Mayor Prentiss, but Todd can't keep his noise down. The mayor finds the girl and brings her back to his headquarters.  The mayor tries to get information, but the girl won't talk. Hearing the girl has escaped their grasp, the men look for her again, but Todd goes home.  Ben and Cillian hear Todd's noise about the girl.  They open a map and stab it to mark Farbranch. They tell Todd to save the girl from the mayor by going to Farbranch.  With a motorcycle and a horse, Todd and the girl, Viola, run.  Todd will discover the truth about Prentisstown on his noble journey.<br/><br/>Based on The Knife of Never Letting Go by Patrick Ness, this movie doesn't end like the books, so it's hard to believe there will be a part 2.  The visual noise effect is interesting to see, but hearing it can be jarring.  The fight scene between Viola and the preacher is anticlimactic for such a promising build-up.  You won't be bored watching this movie, but you won't have an urge to read the book.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Boogie",
     "review":"After his father, Mr. Chin, got out of jail, Mr. Chin made Boogie change schools to join a different basketball program.  If he beats the best, Monk, then college scouts will give Boogie a full athletic scholarship for college. Mr. Chin believes Boogie can make it to the NBA, but his mother feels something different.  His mother, Mrs. Chin, can see that Boogie has an attitude problem that spills over on the basketball court. When Boogie's attitude causes another school to offer him a spot on the team without a scholarship, his new manager, Melvin, has a new offer with the CBA.  If Boogie accepts this offer, he will have to leave his team, give up college, and leave his new girlfriend, Eleanor, behind. Or he can keep trying for a college athletic scholarship that may not come.  With centuries of tradition weighing heavily on his shoulders, what will Boogie choose?<br/><br/>This movie speaks a lot about culture, responsibility, and family, but you never feel connected to a character.  This movie tries to be romantic as Love and Basketball, culturally relevant like White Men Can't Jump, and emotionally connected like The Farewell with an independent movie ending.  But the plot comes up short in comparison to all its counterparts. One watchable aspect is the movie's charming lead.  He is someone you can cheer for because he was counted out and counted on since his mother gave birth to him.  Seeing him fight to find his place between two worlds is worth the matinee price.<br/><br/>2.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/boogie.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/boogie.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"My Zoe",
-    "review":"Isabelle and James battle in a courtroom weekly over custody of their daughter, Zoe. Since Isabelle will be out of town one of her weekends, she asks to pick Zoe up early to have time with her.  James says no and calls Isabelle a bad mother for going out of town.  When Isabelle returns, she picks up Zoe from James's house and notices that Zoe sneezes.  Zoe doesn't feel warm, so Isabelle believes it's a common cold and takes Zoe home. The next morning, Isabelle tries to wake Zoe up, but Zoe won't open her eyes.  The paramedics rush Zoe to the hospital, and doctors discover Zoe has a bleed on her brain.  Sadly, she will never recover.  Unable to deal with the trauma, Isabelle learns a doctor may have an unorthodox and illegal solution.<br/><br/>This movie moves from heart-wrenching to simply absurd to boring beyond belief.  The plot dragged on after it reached the climax.  With such a unique premise, it landed on cheap religious and political propaganda. Wait for free tv if you want to see this movie.<br/><br/>1 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myzoe.png",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myzoe.png"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Tom and Jerry: The Movie",
-    "review":"Tom and Jerry are two animals without a home.  They start to fight, and Tom bumps into Kayla, a delivery woman, causing her to drop a customer's clean laundry in the street.  This encounter is the second time Kayla screwed up on the job, so her boss suspends her, but she quits instead.  Kayla walks to the Royal Gate Hotel, starts scavenging their free cookies, and sits next to Linda.  Kayla pretends to be an employee undercover to interview Linda and convinces Linda that she didn't make the cut. Kayla walks into Mr. Dubros's office with Linda's resumé.  Mr. Dubros wants to give Kayla a try, but Terence, the event manager, has reservations about her because he thinks she is lying. Social media influencers Preeta and Ben chose the Royal Gate for their extravagant wedding.  The press from this wedding will attract people from all over the world to stay at the hotel, so everything must be perfect.  As Terence gives Kayla the five-cent tour, they hear a scream come from the kitchen.  Chef Jackie's sous chef saw a mouse in the kitchen and can't cook with a rodent in the hotel.  Kayla offers to catch the mouse, and Mr. Dubros extends a permanent position if she does. As she hunts for the mouse, she hears a commotion in a hotel room.  Kayla opens the door to find Tom in a destroyed hotel room.  Then she remembers him from the park and makes him an offer.  If he kills the mouse, Kayla will buy him whatever he wants.  Tom agrees.  Now, Kayla has to trust that Tom can catch Jerry.  Then she can keep the job she stole before Mr. Dubros becomes the wiser.  She and Tom have no idea the fight Jerry will give them.<br/><br/>Based on Tom and Jerry by William Hanna and Joseph Barbera, this movie has all the hijinks you love from the original cartoon while melding it into the real world like Who Framed Roger Rabbit.  The writers pull this tale of wits into the 21st century by adding social media and clout to the plot. Blending the past with the future allows parents and kids to enjoy this movie together. So get the family together and have a welcome giggle at this classic cat versus mouse story.<br/><br/>3 out of 5 stars",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/tomandjerrythemovie.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/tomandjerrythemovie.jpeg"
     }
   }
 ];
