d20bc77ff58e5d7cbcef6504fed2b4ccb1c052c0
Tiffany Gray <KarateTD@yahoo.com>
2021-03-23 20:04:29 -0400

    add must buy

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,15 @@
 var mustBuy = [
   {
-    "option":"1",
+		"option":"1",
+		"mtitle":"Disney Pixar Soul",
+		"review":"Joe works as a middle school music teacher in M.S. 70.  During class, the principal calls Joe to the hallway.  They can offer him a full-time teaching position with time off and benefits, no more part-time work for him.  He goes to his mother's tailor shop, and Libba tells him that he must take the job.  Libba wants Joe to have a steady income with benefits, not an unstable gig, so Joe tells Libba he will take the job.  Joe gets a call from Curley, a former student, and Curley is a drummer for Dorothea Williams, a famous local jazz saxophonist.  Dorothea needs a new pianist. Joe runs downtown as fast as he can and meets with Dorothea.  She tells Joe to sit at the piano and join them.  At first, Joe is confused because he doesn't know the song they are playing, but then he gets into a zone.  When he opens his eyes, the band watches in awe.  Dorothea tells him to get a suit, come back at 9 pm tonight, and congratulations.  Joe made the band.  Excited, he runs home, talking on his phone, and falls into an open sewer. Now, Joe is a soul headed to the Great Beyond.  He runs from the Great Beyond, falls off the bridge, and falls into the Great Before.  All the soul counselors introduce themselves as Jerry.  It's their job to give souls their personality, but one spot remains open.  Once the soul fills that spot by going to the Hall of Everything, they get an Earth pass and start their life.  Jerry assumes Joe is a mentor for souls looking for the spark to fill the last spot.  After several failed attempts to return to earth, Joe sees he is not dead but in a coma at the hospital.  Joe decides to pretend to be a mentor and steal an Earth pass to get back.  Jerry assigns Joe to 22. Twenty-two doesn't see the point of living if dying is the result.  Joe and '22' make a promise, if he helps her find her spark, '22' will give Joe her Earth pass.  He will live, and she will never be born.<br/><br/>As usual, Pixar delivers the concept of life, death, and the beyond in a touching, palatable, and beautiful way.  To parents, there are plenty of jokes for you to enjoy.  But one concept that will pull you in is the land of Lost Souls.  Some of these lost souls aren't dead.   They are so focused on one thing that they stopped living their life. Putting an image to this concept will strike a chord with you.  This movie is a film the entire family can enjoy, and it's a strong entry to close out the year.<br/><br/>5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/disneypixarsoul.png",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/disneypixarsoul.png"
+		}
+	},
+  {
+    "option":"2",
     "mtitle":"Promising Young Woman",
     "review":"One night a week, Cassie goes to a club and pretends to be falling-down drunk.  She waits for a guy to pick her up, take her home, and try to rape her.  Before the assault, Cassie tells the guys, 'I'm sober, and I could hurt you.'  At work, a man recognizes her from college.  His name is Ryan, and he's a pediatric surgeon.  They were both pre-med at Forrester University until Cassie dropped out.  Ryan wants to know why, but Cassie won't divulge.  As they talk, he mentions he still talks to their old classmates, including Al Monroe.  That name makes Cassie extremely curious and angry because she thought Al was in London.  Ryan explains Al is back in town to get married, and he became an anesthesiologist after college.  She goes home and looks up Al and several other people, and then creates a Kill Bill worthy revenge list.  And she does it all for Nina.<br/><br/>It doesn't take much to figure out who Nina is and what happened to her.  So, the plot doesn't take the time to explain that.  It follows Cassie's path of rage as she plays pied piper to so-called nice guys.  But her point isn't to murder, but to teach and warn them. But she never has a real plan until she hears Al's name.  She goes from devious to cunning.  She doesn't only go after the man who attacked Nina, but Cassie goes after the ones who let him get away with it.  And once she gets to Al, she has so much more planned for him, and you will love it.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Croods: A New Age",
     "review":"Guy, much to Grug's chagrin, joined the Croods as they search for Guy's Tomorrow. Grug worries Guy will pull Eep away from the pack, but Ugga tells him that's the natural order. Guy and Eep confirm Grug's fears after he overhears them in the sleep-pile.  Grug gives Eep a rock with their picture and asks her to join him, and start a new pack.  Eep says yes, and Grug leaves the sleep-pile and finds something new.  He wakes up the family and brings them to a high fence.  They knock a hole in the barrier and see rows of fresh food and beautiful flowers.  Before the family gorges, Grug welcomes them to Tomorrow, and now, the pack can stay together, forever.  Then Grug sees something he hasn't seen in a long time, a sweet fruit he called a banana.  Just as he grabs it, it sets off a trap, and a net pulls the entire Crood family up a tree.  Two figures emerge and poke the family with sticks but see similarities. Then, introduce themselves as Phil and Hope Betterman.  After further inspection, they see Guy and hug him.  Before Guy's parents died, the Bettermans were friends with his family, but after the tar killed them, the Bettermans lost track of Guy.  They bring the Croods to their treehouse and introduce the Croods to their daughter, Dawn.  As Grug sees the family settling into the Betterman lifestyle, he fears losing his entire pack to the Betterman way.  And Grug is correct to worry.  The Bettermans believe Guy should be with them, not the Croods. Soon, Ugga and Grug unlock their plan, and it will cause a chain reaction of bad decisions, all starting with bananas.<br/><br/>Sequel to The Croods, this movie expands their world, your imagination, and the artistry of computer animation.  Parents don't come to this movie expecting to get a nap because you will be laughing from beginning to end.  And don't come late because the first 5 minutes show the audience what happened to Guy's parents, and it shows the first time he met Eep from his perspective.  As the Croods and Bettermans come across punch monkeys, bee stings, spider wolves, and the Thundersisters, they learn to stop judging one another and work together.  The lessons that shine clear in this film is differences should be celebrated and growth is good.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Greenland",
     "review":"Greenland was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access'",
     "image":{
@@ -27,21 +36,12 @@ var mustBuy = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Let Him Go",
     "review":"George and Margaret work on their Montana ranch with the help of their son, James, and James's wife, Lorna, and their baby, Jimmy.  Margaret looks outside the window and sees James's horse, but no James.  George finds James dead on a pile of rocks next to a creek.  Three years later, George and Margaret attend Lorna and Donnie Weboy's wedding. A few days later, Margaret sits in her car outside the grocery store and sees Donnie, Lorna, and Jimmy walking while eating ice cream cones.  Then, Donnie laughs at Jimmy because he dropped his ice cream.  When Jimmy refuses Donnie, Donnie hits Jimmy.  Lorna comes to Jimmy's defense, and Donnie hits her too.  They leave.  Margaret doesn't tell George what she saw at the grocery store.  Instead, she makes a cake and takes it to Donnie's home, but the family is gone. Margaret comes home and prepares for a trip.  When George gets home, he knows something is wrong. Margaret believes Lorna can't defend herself and their grandson.  So Margaret will get Jimmy back. George closes his suitcase, and they head down the road.  George and Margaret don't know the Weboy Family is powerful in North Dakota.  The matriarch, Blanche, rules with an iron fist, shotgun, and a lit cigarette. She will never let any of the Weboys walk away again, including Jimmy.<br/><br/>Based on Let Him Go by Larry Watson, this movie is a slow burn but picks up at the end.  Just a warning, some scenes can be pretty gruesome.  But what you are witnessing is three mothers fighting for the boy they love. In the end, it all comes down to Jimmy and how much people are willing to sacrifice to keep him. Viewers get a Kill Bill Vol. 2 moment, and it's worth it.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lethimgo.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lethimgo.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Kid Detective",
-    "review":"As a child, Abe Applebaum had a nose for mysteries.  His principal came to Abe with his first case when Abe was 13.  It was the case of the missing fundraiser money.  Abe picks a suspect, and the principal recovered the money.  Soon, Abe was running a business out of his treehouse, and the police even called to consult him on bigger cases.  When someone chopped down his treehouse, the thankful town got him an office on Main Street.  The mayor's daughter, Grace, was his receptionist, and he paid her with soda.  His business was booming, and Abe was in the newspaper every week, but that all changed when Grace went missing.  The town and Abe expected Abe to find her, but when he couldn't, Abe's town and Abe fell into a depression.  Today, at 32, Abe is self-loathing, solves mediocre mysteries, and has more debt than cases. As he walks home from work, he sees police combing through the river. The police found Caroline's 15-year-old boyfriend stabbed to death in the river, and they have no leads. So, Caroline asks him to find who murdered her boyfriend.  He asks if she is sure because the truth can be shocking.  Caroline is sure and hires Abe.  This murder case will be his first adult case and a chance to clear Grace's case from his soul.<br/><br/>This movie goes from Encyclopedia Brown to 8mm in less than two hours.  It's a mix of Criminal Minds' unsubs with Psych's hijinks and wit.  You will laugh at the inappropriateness of Abe as he hides in a little girl's bedroom after spying on a teenage boy.  This story is perfection because Abe carries the weight of the town on his shoulders, and you can see the moment the weight lifts.  If you have ever gone through a hard time, you will connect with Abe's tears. When Abe finds the murderer, he will learn the truth about his town and himself.  This movie will become a cult classic.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekiddetective.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekiddetective.jpg"
     }
   }
 ];
