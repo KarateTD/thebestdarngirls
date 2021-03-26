0acc19b4eb8cc327dd1fbaf0bd419f7c4d783182
Tiffany Gray <KarateTD@yahoo.com>
2021-03-25 23:30:02 -0400

    add nobody and update must buy:

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Nobody",
+    "review":"Hutch Mansell is a married father of two.  One night, Hutch couldn't sleep and checked on his children when he heard a noise downstairs.  It is a man and a woman in ski masks attempting to rob his home.  The woman holds the gun at Hutch's head and demands his money, and Hutch points to a bowl on the counter. Then, Hutch's son, Blake, tackles the man.  The woman turns around and points the gun at Blake.  Hutch grabs a nearby golf club, but he doesn't hit the woman. The man and woman leave but not before punching Blake in the face. Hutch could see that the woman's gun wasn't loaded, so they didn't pose a threat.  At home, Hutch continues, but his daughter, Abby, can't find her kitty kat bracelet.  She left it in the bowl with all the money.  So, Hutch grabs his bus pass and leaves his home. It's not long before he finds Lupita and Luis and tells them he wants the bracelet.  Lupita gives him the watch, but she swears they don't have the bracelet. Hutch leaves and gets on the bus.  After he closes his eyes, the bus comes to a hard stop after the driver witnesses an accident.  The drunk drivers leave their stalled car and board the bus.  This moment was the opportunity Hutch needed all night because it awakens his beast, and Hutch leaves a pile of broken bodies on the bus.  But one of the men, Teddy, is related to Yulian Kuznetsov. Yulian wants revenge for the man that attacked his brother.  But he should be careful because Hutch is a Nobody.<br/><br/>So imagine mixing John Wick's action, Guardians of the Galaxy's music arrangement, and Kill Bill's wit, and you have Nobody.  With bone-crushing and bloody action, this movie proves the mild-mannered man is something to be feared.  As his victims slowly die, Hutch explains who he was, who he became, and why his life changed.  Unlike John Wick, Hutch is a family man with a father in a nursing home. This movie will be a favorite for action enthusiasts.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nobody.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nobody.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"The Courier",
     "review":"In 1960, Oleg finds two American students, hands them a piece of paper, and tells them to take it to the American embassy. Oleg's information makes it into the hands of a CIA agent.  The CIA agent brings the info to an MI6 agent, and they discuss who to use as Oleg's courier. They find Greville Wynne.  A meek, unassuming businessman from Great Britain. The agents tell Greville that he will go to Moscow.  He will have a meeting with his usual sales pitch.  After the meeting, Oleg will approach Greville and assess Greville's ability to do the job.  Once the two have met, Oleg determines Greville is adequate and gives Greville more information. This hand-off was one of many. And if Greville wanted to give his family a fighting chance to survive a Cold War, he will continue to courier Oleg's information.  Oleg and Greville's partnership leaves a lasting impact on the world we live in today.<br/><br/>It's hard to believe Oleg and Greville's impact on the world then, the world today, and the world to come. These family men form an unbreakable bond and save the world.  They dodge the KGB, death threats, and poisonings to stand up for the world they want for their children. The acting, costumes, hair, and make-up were flawless.  The movie's pace slowed down as the script delved into their private lives, but delving allowed the viewer to see Oleg's and Greville's unique motivation for spying and treason.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Long Weekend",
     "review":"Bart ignores calls from his therapist and his ex-fiancé.  After losing his job and apartment, Bart's depression starts to creep back into his life.  His friend, Doug, and Doug's wife, Rachel, invite Bart to stay in their garage until he gets his life in order. After his job interview, Bart goes to the movies and hides alcohol under his coat.  The ticket taker doesn't see Bart, but Vienna does. Soon, Bart feels Vienna jolting him awake and saying the movie is over.  Bart rises from his seat and leaves the theater. Vienna is new in town and wants to know where she can get alcohol like his.  Bart points out a hipster bar and a dive bar.  Vienna invites him to go with her.  They have a great night, but Bart notices some strange things about his new companion.  First, Vienna doesn't talk about herself.  Second, Vienna carries at least $10,000 in her bag at all times.  Third, and the weirdest thing is Vienna doesn't own a phone. Doug and Rachel believe that Vienna is weird but not as bad as his ex-fiancé.  They tell Bart to demand to know what secrets Vienna keeps.  On their next date, Bart convinces Vienna to admit the truth when they are alone and safe.  When she tells her story, Bart will end up in the hospital twice with an unbelievable tale.  But was it ever true?<br/><br/>With a rom-com clumsiness, these two meet, fall in love, and sleep together on the same day.  The on-screen couple's visible connection makes this plot undeniable.  It feels like the typical boy meets girl story until Vienna releases her secret.  Don't be surprised if you roll your eyes during her reveal. By the end of the movie, you will either be happy or confused.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"The Father",
     "review":"Anne rushes to her father, Anthony's flat because he caused a mess again.  He insulted his nurse and threatened her.  The nurse quit, and Anne must find someone else to care for him.  She tells Anthony that she found a new nurse named Laura. Anne met a man and wants to move to France to be with him.  She needs to make sure her father will be safe while she is gone, but she promises to visit him on the weekends.  Anthony laughs and refuses the help because he doesn't think he will need it. Anthony walks into the living room and sees a man he doesn't know sitting in his chair.  The man introduces himself as Anne's husband and says the place is his flat, not Anthony's.  Then Anne walks through the front door, and it's a different woman.  Anthony believes Anne is trying to trick him into giving up his flat. Can he hold it all together?<br/><br/>Based on the play Le Père by Florian Zeller, this movie gives the viewer a first-person perspective into dementia.  The flat, people, and scenery change by the minute, leaving the viewer just as disoriented as Anthony.  For a moment, you will wonder if someone is gaslighting Anthony to convince him to move or get help.  But at the end, you will see the pure sadness of a man lost in his mind.  While the story is compelling and well shot, you won't have an urge to buy it or watch it again.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Chaos Walking",
     "review":"In 2257 AD, only men and the Spackle populate the new world.  The noise is a condition that allows others to see their thoughts, and it afflicts only men.  Years ago, the Spackle killed all the women, including Todd Hewitt's mother.  Now, Todd is the youngest member of Prentisstown, and Ben and Cillian raise him.  While doing chores, Todd sees someone coming out of the barn, but he notices they don't have noise.  Todd chases the figure to a crash site and sees the person is a girl.  Todd tries to control his thoughts while running to tell Mayor Prentiss, but Todd can't keep his noise down. The mayor finds the girl and brings her back to his headquarters.  The mayor tries to get information, but the girl won't talk. Hearing the girl has escaped their grasp, the men look for her again, but Todd goes home.  Ben and Cillian hear Todd's noise about the girl.  They open a map and stab it to mark Farbranch. They tell Todd to save the girl from the mayor by going to Farbranch.  With a motorcycle and a horse, Todd and the girl, Viola, run.  Todd will discover the truth about Prentisstown on his noble journey.<br/><br/>Based on The Knife of Never Letting Go by Patrick Ness, this movie doesn't end like the books, so it's hard to believe there will be a part 2.  The visual noise effect is interesting to see, but hearing it can be jarring.  The fight scene between Viola and the preacher is anticlimactic for such a promising build-up.  You won't be bored watching this movie, but you won't have an urge to read the book.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chaoswalking.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chaoswalking.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Boogie",
-    "review":"After his father, Mr. Chin, got out of jail, Mr. Chin made Boogie change schools to join a different basketball program.  If he beats the best, Monk, then college scouts will give Boogie a full athletic scholarship for college. Mr. Chin believes Boogie can make it to the NBA, but his mother feels something different.  His mother, Mrs. Chin, can see that Boogie has an attitude problem that spills over on the basketball court. When Boogie's attitude causes another school to offer him a spot on the team without a scholarship, his new manager, Melvin, has a new offer with the CBA.  If Boogie accepts this offer, he will have to leave his team, give up college, and leave his new girlfriend, Eleanor, behind. Or he can keep trying for a college athletic scholarship that may not come.  With centuries of tradition weighing heavily on his shoulders, what will Boogie choose?<br/><br/>This movie speaks a lot about culture, responsibility, and family, but you never feel connected to a character.  This movie tries to be romantic as Love and Basketball, culturally relevant like White Men Can't Jump, and emotionally connected like The Farewell with an independent movie ending.  But the plot comes up short in comparison to all its counterparts. One watchable aspect is the movie's charming lead.  He is someone you can cheer for because he was counted out and counted on since his mother gave birth to him.  Seeing him fight to find his place between two worlds is worth the matinee price.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/boogie.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/boogie.jpg"
     }
   }
 ];
