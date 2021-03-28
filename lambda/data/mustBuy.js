797b99b4718988cfc974ebcd9e2332e5a6999aaf
Tiffany Gray <KarateTD@yahoo.com>
2021-03-28 10:37:27 -0400

    add tv movies

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -115,4 +115,24 @@ module.exports = mustBuy;
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/judasandtheblackmessiah.jpg"
     }
   }
+
+  {
+    "option":"1",
+    "mtitle":"Nobody",
+    "review":"Hutch Mansell is a married father of two.  One night, Hutch couldn't sleep and checked on his children when he heard a noise downstairs.  It is a man and a woman in ski masks attempting to rob his home.  The woman holds the gun at Hutch's head and demands his money, and Hutch points to a bowl on the counter. Then, Hutch's son, Blake, tackles the man.  The woman turns around and points the gun at Blake.  Hutch grabs a nearby golf club, but he doesn't hit the woman. The man and woman leave but not before punching Blake in the face. Hutch could see that the woman's gun wasn't loaded, so they didn't pose a threat.  At home, Hutch continues, but his daughter, Abby, can't find her kitty kat bracelet.  She left it in the bowl with all the money.  So, Hutch grabs his bus pass and leaves his home. It's not long before he finds Lupita and Luis and tells them he wants the bracelet.  Lupita gives him the watch, but she swears they don't have the bracelet. Hutch leaves and gets on the bus.  After he closes his eyes, the bus comes to a hard stop after the driver witnesses an accident.  The drunk drivers leave their stalled car and board the bus.  This moment was the opportunity Hutch needed all night because it awakens his beast, and Hutch leaves a pile of broken bodies on the bus.  But one of the men, Teddy, is related to Yulian Kuznetsov. Yulian wants revenge for the man that attacked his brother.  But he should be careful because Hutch is a Nobody.<br/><br/>So imagine mixing John Wick's action, Guardians of the Galaxy's music arrangement, and Kill Bill's wit, and you have Nobody.  With bone-crushing and bloody action, this movie proves the mild-mannered man is something to be feared.  As his victims slowly die, Hutch explains who he was, who he became, and why his life changed.  Unlike John Wick, Hutch is a family man with a father in a nursing home. This movie will be a favorite for action enthusiasts.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nobody.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nobody.jpeg"
+    }
+  }
+
+  {
+    "option":"2",
+    "mtitle":"The Courier",
+    "review":"In 1960, Oleg finds two American students, hands them a piece of paper, and tells them to take it to the American embassy. Oleg's information makes it into the hands of a CIA agent.  The CIA agent brings the info to an MI6 agent, and they discuss who to use as Oleg's courier. They find Greville Wynne.  A meek, unassuming businessman from Great Britain. The agents tell Greville that he will go to Moscow.  He will have a meeting with his usual sales pitch.  After the meeting, Oleg will approach Greville and assess Greville's ability to do the job.  Once the two have met, Oleg determines Greville is adequate and gives Greville more information. This hand-off was one of many. And if Greville wanted to give his family a fighting chance to survive a Cold War, he will continue to courier Oleg's information.  Oleg and Greville's partnership leaves a lasting impact on the world we live in today.<br/><br/>It's hard to believe Oleg and Greville's impact on the world then, the world today, and the world to come. These family men form an unbreakable bond and save the world.  They dodge the KGB, death threats, and poisonings to stand up for the world they want for their children. The acting, costumes, hair, and make-up were flawless.  The movie's pace slowed down as the script delved into their private lives, but delving allowed the viewer to see Oleg's and Greville's unique motivation for spying and treason.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecourier.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecourier.jpeg"
+    }
+  }
 */
