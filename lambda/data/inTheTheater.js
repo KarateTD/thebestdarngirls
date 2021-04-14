e39811e6538af16263562e52ca4867ad6dc17408
Tiffany Gray <KarateTD@yahoo.com>
2021-04-13 21:20:44 -0400

    add 2 in theater movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Voyagers",
+    "review":"Earth is heating up, and humans must find another hospitable planet.  They find a suitable second Earth, but it will take 83 years to travel there.  Richard suggests that scientists breed a crew from other scientists, raise them indoors and in isolation, and train them to sustain the ship.  The crew's grandchildren will be the ones who reach the new Earth. Richard launches into space with a crew of kids whose grandchildren will make it to the new Earth.  For ten years, everything goes well until the computer tests positive for toxic chemicals in the plant water.  Since the plant water is filtered urine, Christopher wonders what's wrong with them.  As the crew sleeps, Christopher and Zac search the database for the unknown compound in their urine causing the positive reading.  They learn the compound blocks their desires and makes them dull and docile. They determine the chemical compound is in their daily-required drink that they call \"The Blue.\"  Zac and Christopher decide to stop drinking it, and they feel everything for the first time, including anger, jealousy, and lust. When Zac sees Richard touching Sela's shoulder, he is furious. The next day, Richard and Zac prepare to go outside to repair the spacecraft, but recent unacceptable behavior makes Richard choose Christopher to help him.  During the repair, a shadowy figure moves past Richard, and he screams in pain.  Christopher pulls Richard inside, and the medical team tries to help him, but it's too late. As the new captain, Christopher's act is to find out what happened to Richard.  Zac thinks it's an alien, but Christopher isn't so sure.  On the hunt for the truth, the crew will divide into two factions: the believers and the non-believers.  After blood and accusations, only one will come out on top.<br/><br/>This futuristic rendition of Lord of the Flies takes some time to get interesting, but it does after the crew breaks down.  As the movie progresses, it changes from an alien plotline to a whodunit.  With well-crafted sets and costumes, you will dive into the plot.  But parents, if you are watching with your teens, be prepared to be uncomfortable.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/voyagers.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/voyagers.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"French Exit",
+    "review":"Frances sits with her advisor as he delivers dreadful news: all her money is gone.  She has no idea what to do next.  He tells her to sell everything in the home and use the cash to start a new life.  She drinks her woes away when her son, Malcolm, walks into the room to find her drunk. He got engaged to Susan, and they want to live together. But Frances tells Malcolm her news first, and, much to Susan's chagrin, Malcolm halts from telling the truth about his engagement.  When Malcolm tries to tell Frances again, she cuts him off with more news: a friend offered to let Frances and Malcolm stay in her apartment in France.  Malcolm keeps quiet. Frances, Malcolm, their cat, and thousands of dollars get on a cruise headed to France.  Before Frances' \"French Exit,\" she will allow a private investigator, a clairvoyant, a new friend, an old flame, and a fiancé to stay in this cozy apartment.  She will connect to her husband and reveal a horrific end for her vacation.<br/><br/>Based on French Exit by Patrick deWitt, this dark comedy provides unusual laughs at uncomfortable situations.  You will dream of having Frances' bravado in everyday life.  While this movie is entertaining, you won't have an urge to buy it.  But if you don't like dark comedies or rich people's problems, this isn't the movie for you.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/frenchexit.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/frenchexit.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Godzilla vs. Kong",
     "review":"It's been years since Kong defeated King Ghidorah, and now Kong continues his life on Skull Island. Monarch, with Dr. Ilene's guidance, monitors Kong's movements, diet, and attitude.  Also, Dr. Ilene cares for a little girl named Jia.  Jia is the last surviving member of the Iwa tribe. Others warn Ilene that Kong has outgrown the enclosure.  They need to move him, or they won't be able to contain him.  Ilene worries that moving Kong will awaken Godzilla.  In Pensacola, Florida, Apex Cybernetics has a mole with a Titan Conspiracy podcast named Bernie.  He downloads files and uses them to expose the work at Apex. As he delves deeper into the files, Godzilla attacks the Apex compound. Outside, no one understands why Godzilla is attacking Apex.  In the past, Godzilla only attacks when provoked. Madison listens to Bernie's latest broadcast about the attack and runs to her father, Mark Russell, who lost his wife, Emma, in the previous Monarch attack.  Madison tells Mark about the podcast and Godzilla's history, but he doesn't listen.  Madison decides to find Bernie to track Godzilla's reason for attacking Apex.  But Apex's CEO, Walter Simmons, doesn't want to know why; he wants to stop Godzilla.  He goes to Dr. Nathan and requests his help.  Walter wants Nathan to go to Hollow Earth, the Titan's home. Walter created the Hollow Earth Aerial Vehicle or HEAV for short, and it can withstand the reverse-gravitational effect.  And to find the Titan's source of power, they will need a titan, Kong.  With Kong as a divining rod, Ilene, Jia, and Nathan will travel to Hollow Earth to save the world from Godzilla.  But Bernie, Madison, and Madison's friend, Josh, discover that Godzilla isn't the biggest dog in the yard.  Who will come out on top?<br/><br/>Talk about a movie that gets to the point.  You came to see two titans fight, and you won't be waiting for long.  First, they battle at sea, then on land. With so much focus on the titans, the humans were subplots and obstacles for the hero to overcome. The graphics become cartoonish when the titans are on screen alone.  But when they interact with each other or humans, the CGI gets better.  See this movie on the tallest screen possible, whether that's in a theater or at home, and don't stay past the end credits.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Nobody",
     "review":"Hutch Mansell is a married father of two.  One night, Hutch couldn't sleep and checked on his children when he heard a noise downstairs.  It is a man and a woman in ski masks attempting to rob his home.  The woman holds the gun at Hutch's head and demands his money, and Hutch points to a bowl on the counter. Then, Hutch's son, Blake, tackles the man.  The woman turns around and points the gun at Blake.  Hutch grabs a nearby golf club, but he doesn't hit the woman. The man and woman leave but not before punching Blake in the face. Hutch could see that the woman's gun wasn't loaded, so they didn't pose a threat.  At home, Hutch continues, but his daughter, Abby, can't find her kitty kat bracelet.  She left it in the bowl with all the money.  So, Hutch grabs his bus pass and leaves his home. It's not long before he finds Lupita and Luis and tells them he wants the bracelet.  Lupita gives him the watch, but she swears they don't have the bracelet. Hutch leaves and gets on the bus.  After he closes his eyes, the bus comes to a hard stop after the driver witnesses an accident.  The drunk drivers leave their stalled car and board the bus.  This moment was the opportunity Hutch needed all night because it awakens his beast, and Hutch leaves a pile of broken bodies on the bus.  But one of the men, Teddy, is related to Yulian Kuznetsov. Yulian wants revenge for the man that attacked his brother.  But he should be careful because Hutch is a Nobody.<br/><br/>So imagine mixing John Wick's action, Guardians of the Galaxy's music arrangement, and Kill Bill's wit, and you have Nobody.  With bone-crushing and bloody action, this movie proves the mild-mannered man is something to be feared.  As his victims slowly die, Hutch explains who he was, who he became, and why his life changed.  Unlike John Wick, Hutch is a family man with a father in a nursing home. This movie will be a favorite for action enthusiasts.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Courier",
     "review":"In 1960, Oleg finds two American students, hands them a piece of paper, and tells them to take it to the American embassy. Oleg's information makes it into the hands of a CIA agent.  The CIA agent brings the info to an MI6 agent, and they discuss who to use as Oleg's courier. They find Greville Wynne.  A meek, unassuming businessman from Great Britain. The agents tell Greville that he will go to Moscow.  He will have a meeting with his usual sales pitch.  After the meeting, Oleg will approach Greville and assess Greville's ability to do the job.  Once the two have met, Oleg determines Greville is adequate and gives Greville more information. This hand-off was one of many. And if Greville wanted to give his family a fighting chance to survive a Cold War, he will continue to courier Oleg's information.  Oleg and Greville's partnership leaves a lasting impact on the world we live in today.<br/><br/>It's hard to believe Oleg and Greville's impact on the world then, the world today, and the world to come. These family men form an unbreakable bond and save the world.  They dodge the KGB, death threats, and poisonings to stand up for the world they want for their children. The acting, costumes, hair, and make-up were flawless.  The movie's pace slowed down as the script delved into their private lives, but delving allowed the viewer to see Oleg's and Greville's unique motivation for spying and treason.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecourier.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecourier.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Long Weekend",
-    "review":"Bart ignores calls from his therapist and his ex-fiancé.  After losing his job and apartment, Bart's depression starts to creep back into his life.  His friend, Doug, and Doug's wife, Rachel, invite Bart to stay in their garage until he gets his life in order. After his job interview, Bart goes to the movies and hides alcohol under his coat.  The ticket taker doesn't see Bart, but Vienna does. Soon, Bart feels Vienna jolting him awake and saying the movie is over.  Bart rises from his seat and leaves the theater. Vienna is new in town and wants to know where she can get alcohol like his.  Bart points out a hipster bar and a dive bar.  Vienna invites him to go with her.  They have a great night, but Bart notices some strange things about his new companion.  First, Vienna doesn't talk about herself.  Second, Vienna carries at least $10,000 in her bag at all times.  Third, and the weirdest thing is Vienna doesn't own a phone. Doug and Rachel believe that Vienna is weird but not as bad as his ex-fiancé.  They tell Bart to demand to know what secrets Vienna keeps.  On their next date, Bart convinces Vienna to admit the truth when they are alone and safe.  When she tells her story, Bart will end up in the hospital twice with an unbelievable tale.  But was it ever true?<br/><br/>With a rom-com clumsiness, these two meet, fall in love, and sleep together on the same day.  The on-screen couple's visible connection makes this plot undeniable.  It feels like the typical boy meets girl story until Vienna releases her secret.  Don't be surprised if you roll your eyes during her reveal. By the end of the movie, you will either be happy or confused.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/longweekend.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/longweekend.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Father",
-    "review":"Anne rushes to her father, Anthony's flat because he caused a mess again.  He insulted his nurse and threatened her.  The nurse quit, and Anne must find someone else to care for him.  She tells Anthony that she found a new nurse named Laura. Anne met a man and wants to move to France to be with him.  She needs to make sure her father will be safe while she is gone, but she promises to visit him on the weekends.  Anthony laughs and refuses the help because he doesn't think he will need it. Anthony walks into the living room and sees a man he doesn't know sitting in his chair.  The man introduces himself as Anne's husband and says the place is his flat, not Anthony's.  Then Anne walks through the front door, and it's a different woman.  Anthony believes Anne is trying to trick him into giving up his flat. Can he hold it all together?<br/><br/>Based on the play Le Père by Florian Zeller, this movie gives the viewer a first-person perspective into dementia.  The flat, people, and scenery change by the minute, leaving the viewer just as disoriented as Anthony.  For a moment, you will wonder if someone is gaslighting Anthony to convince him to move or get help.  But at the end, you will see the pure sadness of a man lost in his mind.  While the story is compelling and well shot, you won't have an urge to buy it or watch it again.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thefather.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thefather.jpeg"
     }
   }
 ];
@@ -56,23 +56,6 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Voyagers",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/voyagers.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/voyagers.jpg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"French Exit",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/frenchexit.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/frenchexit.jpeg"
-  }
-}
+
 */
 
