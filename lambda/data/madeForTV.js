3f1309502e7791586a738b138a22eec5f0b3d88c
Tiffany Gray <KarateTD@yahoo.com>
2021-07-09 01:13:25 -0400

    initial branch add

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Framed by My Husband",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/framedbymyhusband.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/framedbymyhusband.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Keeping Up With the Joneses: The Wrong Family",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongfamily.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongfamily.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"A Date With Danger",
     "review":"Nikki moves to a small town with her daughter, Brooke, and gets lost.  As they try to recalculate their GPS, Gavin, the coffee shop owner, helps them. Brooke sees a cute dress in a store window and convinces Nikki to go inside in try it on.  After putting her foot in her mouth, Nikki gets a job offer from the store's owner, Liz. Liz has a 15-year-old daughter named Anna.  On Nikki's first day, Liz takes Nikki out to talk, and they run into Gavin. Liz tells Nikki that she dated Gavin and warns Nikki to be careful of his clingy ways. As she leaves, Nikki spies Liz arguing with Anna's father, Dan, about custody arrangements. The next day, Gavin asks Nikki on a date, but she worries about Liz's feelings.  Liz gives Nikki the greenlight, and Gavin and Nikki go out.  When Gavin tries to kiss Nikki, she pumps the brakes.  She thinks they are moving too fast, and he agrees to slow down.  But that doesn't last long.  He buys plane tickets, buys an expensive necklace, and asks Nikki to move in.  Soon, Nikki breaks things off.  One night, Anna comes to Nikki's house because Liz never picked her up. Nikki goes to the store to check and finds a mess behind an unlocked door.  Liz's phone is there, but Liz is missing.  Nikki will do anything to find her new friend, even put her life on the line.<br/><br/>This movie is a slow burn to a predictable ending.  The abductor has so many red flags that you will spend most of the time wondering how Nikki keeps missing them. And to make matters worse, you will fume when Nikki takes custody of Anna away from Dan.  That's just too far.  While the acting is decent, the script is a dud.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Picture Perfect Lies",
     "review":"Rachel loves the sweet sixteen party her parents, Angela and Spencer, threw for her.  They invited her friends, Ava and Adam, her aunt, Jenny, and their housekeeper, Desi.  At the party, Rachel notices Jenny slipping hard liquor into her drink, and she starts to worry.  She walks over and checks on Jenny when Jenny starts to reveal a secret.  Before Jenny tells Rachel her secret, Angela cuts her off to take a picture.  After the party, Rachel goes to the pool with Adam so they can have some time alone.  Before they kiss, Rachel sees Jenny floating in the pool with a head wound. The next day, Rachel tells Ava she has no idea who would want to kill Jenny, but Ava tells Rachel she saw Jenny arguing with Spencer. Ava suggests going to Jenny's home and looking around.  Under Jenny's bed, Rachel finds pictures of a little girl.  Before Rachel can get a closer look, someone else breaks into the home and attacks Ava. Rachel knows there is more to Jenny's murder, and she vows to find the truth.  With Rachel's tenacity, she will unlock a 12-year-old mystery and Jenny's killer.<br/><br/>This film is the type of movie that gives the entire plot in the trailer.  You know the story, twist, and attacker before the movie starts. So, why should you watch it? Well, you can watch Rachel unpack the mystery. This movie is background noise.<br/><br/>2 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"A Whirlwind Wedding",
     "review":"Before leaving town for her mother's wedding, Cassie gets a call from her mom, Bonnie.  Bonnie's wedding planner dropped out at the last minute for a reality T.V. show, and Bonnie needs someone to finish planning the wedding. Bonnie tells Cassie that she knows Cassie can do the job, so Cassie accepts.  Before going to Bonnie's home, Cassie stops at a restaurant.  A man asks to sit at her table and offers his fries as payment. Cassie arrives at Bonnie's home and meets Bonnie's fiancé, Henry, for the first time. First on Cassie's list is finding a caterer.  After calling the first caterer, Cassie asks for a face-to-face interview, and the caterer catches her off guard.  It's Kyle, the fries guy.  And while she likes his food, she doesn't think it's elevated enough for the wedding.  Kyle tells her he doesn't do elevated, so she leaves. Kyle's brother, Phil, tells Kyle to take the job because their restaurant isn't doing well.  Kyle agrees to cater the wedding and change the menu, but he will not be traditional.  She will do the menu his way. As they plan a dream wedding, they get to know each other.  But when Cassie's boyfriend, Gordon, comes down with an engagement ring, will Kyle be in the cold?<br/><br/>As you watch, you will want two things.  First, you will want a bag of french fries.  And second, to see how the wedding will turn out.  With so many changes and great finds, you want to see Cassie's vision come to life. But as she plans Bonnie's wedding, you will feel bad for Bonnie because Cassie isn't listening to Bonnie's suggestions.  Bonnie wants simple, not elevated. Cassie's need for elevated will turn off viewers along with the overdone plot.<br/><br/>2.5 out of 5 stars.",
     "image":{
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/awhirlwindwedding.jpeg",
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/awhirlwindwedding.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Sand Dollar Cove",
-    "review":"Elli arrives at Sand Dollar Cove for her company to scout a location for their new beachfront hotel.  Nana, the owner of the Bed and Breakfast, greets Elli with open arms.  After Elli unpacks, Nana tells Elli to check out the local cafe. Before going, Elli goes to the beach and takes pictures for her boss. She starts to take more pictures when a dog runs up to her.  It's not long before the owner, Brody, follows. He believes Elli is a tourist and tells her the pier is closed.  When she inquires about it, Brody tells Elli that it is the heart and soul of the town. During Elli's meeting with Mayor Conway, Brody walks in.  Elli wasn't expecting Brody, but Conway explains that Brody and his family own the beach.  Brody knows the town needs the hotel to raise tourism after the storm.  But he won't consider any offer that doesn't include the pier, and Elli agrees to talk to her boss to create a win-win deal. And each moment they spend together, they fall deeper in love.  Can Elli help save Sand Dollar Cove without losing Brody or her job?<br/><br/>Based on Sand Dollar Cove by Nancy Naigle, this film looks into a mature relationship that differs from most Hallmark movies.  Since neither plays games to win or reject the other's affection, their love flows naturally on the screen. Brody's extended and nosy family will intrigue you. The only flaw is the plot discusses the solution without showing it to the viewer.  Maybe we will get that reveal in part 2.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sanddollarcove.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sanddollarcove.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Doomsday Mom: The Lori Vallow Story",
-    "review":"Lori Vallow often worries about the biblical teachings of end days. While most brush her off, her husband, Charles, and her brother, Alex, believe as she does.  Lori has a daughter, Tylee, with her ex-husband and a son, J.J., with Charles.  During a yard sale, Lori finds a book written by Chad Daybell.  He discusses the end of days in great detail and prepping for them.  She decides to go to a preppers convention.  Lori doesn't like shooting guns, but she listens to Mel talk about motherhood.  They connect over having sons with autism.  As they talk, Mel tells Lori that she met Chad at a convention, which amazes Lori. The next day, Mel calls and tells Lori that Chad is coming to a local convention.  Both women attend, and Lori approaches him to get her book signed.  He asks to meet with Lori in private after the event, and she agrees.  Later, Chad tells Lori that they were married nine times before and fought for the Lord each time. This meeting will set off a chain of events that starts with a welfare check and ends with dead bodies.<br/><br/>You think you know Lori's story, but you don't.  She is an extreme apocalypticist which causes her to look for signs of evil in the most insignificant actions. But there is a lingering question that comes to mind.  Would Lori go to these depths of depravity if she never met Chad? Or was she always capable of doing this without Chad? This could be a case of Shared Psychotic Disorder.  Regardless of the answer, several people, including two innocent children, suffered the ultimate price.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/doomsdaymomthelorivallowstory.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/doomsdaymomthelorivallowstory.jpeg"
     }
   }
 ];
