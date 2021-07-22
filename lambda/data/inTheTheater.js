66f160fcb4e594bf377862dc7ab011981d308e4d
Tiffany Gray <KarateTD@yahoo.com>
2021-07-22 01:33:20 -0400

    add snake eyes

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,15 +1,6 @@
 var inTheTheaters = [
   {
     "option":"1",
-    "mtitle":"Snake Eyes",
-    "review":"<br/><br/>* out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/snakeeyes.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/snakeeyes.jpeg"
-    }
-  },
-  {
-    "option":"2",
     "mtitle":"Space Jam: A New Legacy",
     "review":"In 1998, LeBron missed the winning shot in a basketball game, and his coach, Coach C, tells him it's because he focused on a Game Boy and not the game.  Since then, LeBron has put every ounce of his focus into the four lines on the court.  And he wants his sons, Dom and Darius, to do the same. Dom wants to focus on the video game he is creating called Dom-ball. When LeBron checks out Dom-ball, he is impressed but confused.  The video game goes beyond the fundamentals and gives players style points. LeBron goes into a meeting with Warner Brothers and takes Dom with him to see their new technology.  The technology is called Warner Brothers 3000, and it will launch soon, and having LeBron promote it will make the technology skyrocket. While Dom loves the idea, LeBron thinks it's stupid.  LeBron doesn't know, but the creator of Warner Brothers 3000 sees and hears LeBron insult his work, and he is furious.  The creator is Al G. Rhythm, and Al is an algorithm that lives in the server-verse. When LeBron says no, Dom leaves the meeting, and LeBron goes after him.  Al redirects the elevator to the server room and sucks them into the server-verse.  Then, Al takes LeBron's phone, hides Dom, and gives LeBron an ultimatum.  LeBron can get Dom back if he wins a game of basketball. LeBron thinks it will be an easy win, but Al sends LeBron to the rejects to find teammates in Tune World.  The only person left is Bugs Bunny.  Al convinced all the Toons to leave, and Bugs is ready for his revenge.  Al didn't tell LeBron that he is not playing regular basketball, but LeBron will be playing Dom-ball with a worldwide, captive audience.<br/><br/>Based on Space Jam, this movie adds family love and makes fun of its plot for being similar to the first.  In the server-verse, viewers will see plenty of Warner Brothers classics like Harry Potter, Casablanca, Game of Thrones, and Star Trek, to name a few.  Spotting the Easter Eggs will keep adults focus on the movie more than the plot. Don't be fooled; you do get an exciting and Looney game of basketball with all the classic Looney Tunes you love.  And you do get Michael Jordan.  While you may not buy it in stores, you will get your money's worth watching on the big screen.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +9,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"2",
     "mtitle":"Black Widow",
     "review":"In Ohio 1995, Alexei came home to warn his wife, Melina, that it was time to leave.  Alexei and Melina grabbed their two daughters, Natasha and Yelena, and ran off under a hail of gunfire.  The family flies to Cuba, and Dreykov declares their mission complete. And guards come for Natasha and Yelena.  Seeing this, Natasha grabs a guard's gun and tells them that they can't take her little sister to the Red Room because she is only six.  Alexei tells the girls to stay tough and stay together.  Twenty-one years later, Natasha is on the run after violating the Sokovia Accords. In Morocco, Yelena's target is on the move, and Yelena goes after her.  Just as Yelena turns the target over for the kill strike, the target sprays gas in Yelena's face. Yelena mails to vials to Budapest.  At her safehouse, Natasha's secret handler gives Natasha her mail from the Budapest safe house.  She doesn't know, but one of the packages is the vials from Yelena. The Taskmaster hits Natasha's car and runs her off the road.  In a fight, Natasha realized the Taskmaster isn't after her but after the vials.  She gets away with them and heads to Budapest.  In Budapest, Yelena waits for Natasha.  After a bloody truce, Yelena tells Natasha that the Red Room and Dreykov still exist. After Natasha left, Dreykov tightened the reins on his Widows and gave them a mind-control agent.  One of the first recipients was Yelena.  Now, the Black Widows are conscious but can't make any decisions.  However, the vials contain an antidote.  Natasha and Yelena want to free the Widows while taking down Dreykov and the Red Room.<br/><br/>This movie is about repairing family and wiping the slate clean.  When they sit to talk, Melina and Alexei come to terms with how they ruined Natasha and Yelena's childhoods.  But Natasha has to come to terms with leaving her sister behind to become an Avenger. The actions scenes are some of the cleanest in the MCU because they don't look cartoonish like the Hulk or Spider-Man. With all the emotion and physicality, something still feels off about the movie.  It's the same feeling you get watching Ant-Man or the 2nd Thor movie. Consider this movie as an introduction to Yelena, and it will make a lot more sense.  While it's worth seeing on the big screen, don't pay extra for IMAX or 3D.  Of course, stay past the end credits.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"3",
     "mtitle":"The Boss Baby: Family Business",
     "review":"Thirty years since their last mission, Ted and Tim Templeton are all grown up.  Ted married Carol and became a stay-at-home dad for his two daughters, Tabitha and Tina.  Tabitha is a second-grader and accepted into the most prestigious elementary school in town, The Acorn Center. Ted is a CEO and doesn't come around much, but he does send lots of lavish gifts for Tim's family.  As Tim puts his girls to bed, Tabitha's new grown-up attitude leaves him feeling abandoned.  She doesn't want a song, hug, or a kiss good night, but she wants to be like her Uncle Ted.  As he leaves Tabitha's room, Tim hears a sound coming from Tina's bedroom and goes in to check it out.  It's Tina's toy phone, and Tim gets the shock of his life.  Tina can talk, and she is in the family business. Tina uses a recording teddy bear to manipulate Tim's voice and calls Ted feigning that Tabitha is injured.  Ted gets on his helicopter to come to his niece's aid.  When Tina gets Ted and Tim alone, she stuffs pacifiers in their mouths and takes them to Baby Corp.. Tina explains that Dr. Irwin Armstrong created a school that destroys children's childhoods.  Tim recognizes Dr. Armstrong because he runs The Acorn Center. She gives them a formula that makes them 30 years younger for 48 hours.  They will need to go to The Acorn Center and plant listening devices in Dr. Armstrong's office. While it sounds easy, Tim and Ted must overcome weird kids, detention, and their estrangement to save the children of the world.<br/><br/>Based on The Boss Baby and The Bossier Baby by Marla Frazee, the same message is inside a different plot.  As siblings get older, they often grow apart.  This change isn't because they stop loving each other. In their childhood states, they find each other, and Tim connects with Tabitha.  He sees that his little girl is growing up and pulling away, but she still needs him.  That moment will rock your core.  While it's not as funny as the first, it's an entertaining movie for the entire family. And you must see it from the beginning to get a recap of all the babies from the first movie.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -36,12 +27,21 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"5",
+    "option":"4",
     "mtitle":"Summer of Soul",
     "review":"Tony Lawrence heard about the upcoming Woodstock festival but realized it was missing key artists.  So, with his hustling skills and connections, he made half promises to managers and the city of Harlem about creating a festival at Mount Morris Park.  But with Maxwell House and Mayor John Lindsay backing him up, Tony Lawrence creates a festival with scores of acts and 300,000 attendees over six weeks. Learning that this cultural festival of jazz, funk, blues, pop, gospel, latin, and pop music would happen, several people came together to film it.  That film sat in a basement for 50 years, until now.  Watch as Questlove talks to spectators, culturalists, artists, and organizers to bring this history out of the darkness and into the light.<br/><br/>1969 was a historic year for all Americans because there were assassinations, political change, and a moon landing.  But there was a culture shift happening in Harlem.  The days of buttoned-up suits disappeared for dashikis and afros, and the music of this time reflected this shift within the small community.  And the Harlem Cultural Festival mirrored it all. Hopefully, with the popularity of this movie, the film's proprietors will release the concert in its entirety.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/summerofsoul.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/summerofsoul.jpeg"
+    }
+  },
+  {
+    "option":"5",
+    "mtitle":"Zola",
+    "review":"Zola works at a restaurant and waits on a petite blonde and her date.  The women openly flirt with each other before Zola goes to the back to put in the order.  Zola turns around to see Stefani, the petite blonde, standing in front of her. After a quick discussion, they remembered they danced at the same strip club.  They exchange numbers and social media accounts.  The next day, Stefani texts Zola about a club in Florida where her roommate made $5,000 in one night. When Zola acquires more about the trip, Stefani gives vague or no answers.  But Stephani convinces Zola to go.  Zola gets in a car with Stefani, Stefani's boyfriend, Derrick, and a driver, whose name Zola won't know for 48 hours. The driver leaves Derrick in the motel and drives the women to the club.  They dance and make good money.  Soon Stefani asks Zola to take a picture with her because they both look cute.  From that point on, Zola tells you to keep an eye on everything Stefani does.  Zola has no idea she has entered a life that she knew existed but never wanted.<br/><br/>Based on Zola Tells All: The Real Story Behind the Greatest Stripper Saga Ever Tweeted by David Kushner, this movie is a warning to listen to your inner voice.  Zola ignored several warning signs on her chase for money.  Soon, she enters into a world of pimps, abuse, and suicide.  And she is dependent on the danger to get her back to safety.  This movie is not for young children, tweens, or teens.  It displays an unadulterated and unfiltered view of nudity, abuse, and adult occupations. It's up to the viewer to determine if you believe Stefani or Zola.  It's hard to believe this world exists, but the 148 tweets back it up.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/zola.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/zola.jpeg"
     }
   }
 ];
