2fb89de63cc2ef27ec7c76f56d4e48fe308dcd3a
Tiffany Gray <KarateTD@yahoo.com>
2021-07-24 22:32:17 -0400

    add 5 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,54 +1,8 @@
 var madeForTV = [
   {
     "option":"1",
-    "mtitle":"Nobody Will Believe You",
-    "review":"After their move, Hannah and her mother, Melanie, have wildly different views about the town. Hannah's first day at school reminded her that she is an outcast.  The popular girls ignore her, and her music teacher, Mr. Kurtz, wasn't impressed by her audition for his intensive music program. The school's guidance counselor, Mr. Garrett, offers a sympathetic ear whenever Hannah needs it.  As Melanie and Hannah get pick-me-up mac and cheese, Hannah receives a text from a boy she bumped into at school.  His name is Tanner, and she develops a quick crush. After a social media fail, Hannah, saves Amber's life.  Amber is a popular cheerleader that makes everyone miserable. Next, Amber invites Hannah to a party.  Hannah's life starts to look up, but she feels like someone is watching her.  She tries to ignore the feeling until someone leaks nude pictures of her to the whole school.  Devastated, Hannah doesn't realize this is step one in someone's murderous plan.<br/><br/>While this movie offers a decent mystery for the price of admission, the how goes by in a blur.  The writers put more focus on exposing the killer's past than explaining how the killer framed Hannah. During the killer's monologue, the backing track drowns out the explanation for why the killer picked Hannah to frame.  This revelation is helpful to the overall plot. Even with all the Lifetime tropes, this movie is watchable.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nobodywillbelieveyou.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nobodywillbelieveyou.jpeg"
-    }
-  },
-  {
-    "option":"2",
-    "mtitle":"Keeping Up With the Joneses: The Wrong Letter",
-    "review":"Detective Murphy interviews each of the Jones women, individually, about Carlos's shooting but gets nothing from them.  Although the police backed down, Pam tells everyone to keep their eyes and ears open. Pam believes she knows one enemy, Web.  He sent threatening letters in the past after they didn't take his deal to buy their company.  But Pam has one more person she wants to keep an eye on, Robin.  With her controlling interest in the company, Robin only needs 2 of the Jones sisters on her side to sell the company, and it worries Pam. Carrie is more focused on the new man in her life, Lance Jenkins, an investment banker. Kayla can't focus on the business because she is keeping her attention on her fiancé, William. However, Tara took her eye off the ball and wants to quit school and doesn't want to attend college. With each sister having a high level of mistrust, they have no idea Robin's in trouble until they get a ransom demand.  Either the sisters pay the money, or they will kill Robin.<br/><br/>The second chapter in this saga keeps dishing the dirt on this Pacific Hills royalty.  With a recap, in the beginning, you don't need to see the first chapter to enjoy this installment. Each woman has a crooked or new person in her life, so you aren't sure who kidnapped Robin.  However, you will watch and scrutinized everyone's actions and reactions.  This part will have you chomping at the bit for part three.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongletter.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongletter.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"College Professor Obsession",
-    "review":"After losing her track scholarship due to an injury, Jenny starts college in the winter semester with a part-time job and goals to run again.  Her R.A. Kyle introduces Jenny to her roommates, Beth and Sara. At the student union, Jenny learns the tragic fate of Sara and Beth's previous roommate, Elaine.  Elaine fell off a roof during a party, but some believe she committed suicide because her grades dropped dramatically. When she and Beth come back to the dorm, they find a hooded man in their room.  Jenny stabs him in the shoulder, but he runs away. During her first class, Jenny's history teacher tells her that her participation isn't wanted.  Then, her psychology professor, Dr. Davrow, tells her to dress sexier if she wants good grades.  Jenny can't believe Dr. Davrow exchanges sexy outfits for marks.  When she tells Beth, Beth tells Jenny to play along as she does.  At first, Jenny says no, but she changes her mind to get a scholarship.  When Dr. Davrow goes too far, Jenny decides to report him.  Now her life is in danger.<br/><br/>Dr. Davrow will give you the creeps.  This professor goes beyond any level of decency and adds victim-blaming to his list of depraved actions.  He is a character you will love to hate.  The movie tries to point you in another direction, but the perpetrator's maneuvers will give them away. This movie won't be a favorite, but it's not a waste of time.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/collegeprofessorobsession.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/collegeprofessorobsession.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Crashing Through the Snow",
-    "review":"Maggie attends the Christmas pageant for her youngest daughter, Sophie, but feels out of place.  Her ex-husband, Jeff, brought his new girlfriend, Kate.  And Maggie's oldest daughter, Mia, would rather sit with them.  Maggie puts on a brave face because this is the first time she will be without her daughters during the holidays.  When Jeff lets this information slip, Kate invites Maggie to their Christmas vacation in Aspen so Maggie can be with Sophie and Mia.  Maggie says yes. Kate's dad, Ted, introduces himself, and Kate directs Maggie to a guest room, and the in-suite steam room astonishes Maggie.  After a quick steam, Maggie walks out and sees a man standing in the room.  He's Sam, Kate's brother, and he's back from Tokyo to surprise his family for the holidays. After a bumpy introduction, Kate tells Sam that Maggie creates campaigns, and he is intrigued.  Seeing the unspoken competitive nature between Maggie and Kate, Sam offers to help because he understands what it is like to be in Kate's shadow. He needs Maggie to create a pitch deck for him. She agrees to help. Their collaboration sparks a moment of love that everyone can see.  Will the mom competition get in the way?<br/><br/>While it's easy to believe that Maggie and Jeff will reconnect, the plot shuts this thought down at the beginning.  This movie is a love story between Maggie and Sam.  But the not-competition competition between Kate and Maggie can be confusing until you hear both women talk about their connection to the children. While it's a different story for the channel, it just doesn't grasp your attention.  Keep this one on the back burner for a rainy day.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/crashingthroughthesnow.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/crashingthroughthesnow.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Framed by My Husband",
-    "review":"Rick Lowe's novel just made the New York Times bestsellers list, and he wants to celebrate by starting a family with his wife, June.  However, June has aspirations of becoming an author too.  And she doesn't want to have children until she finishes her first novel. June tells her sister, Maggie, that Rick doesn't respect her, but Maggie thinks June is overly sensitive. One day, June gets the mail and finds two vital correspondences.  The first is Rick's bonus check, and the second is a blackmail letter.  A woman claims Rick sexually assaulted her.  And if Rick doesn't pay $50,000, she will tell the police and go public. At the designated drop-off, Rick tells June she should give the blackmailers the money. June agrees to do the drop.  At the location, June calls out to the waiting car, but no one comes out.  She opens the car door, and a dead woman falls out.  June grabs the body and sees a dead man in the passenger seat. With a coat covered in blood, June explains to Rick what happened.  She tells Rick she saw that woman in a hospital the day before with bruises.  The woman's name was Wendy Bach.  He tells her to relax and keep quiet. The next day, June preoccupies herself with the news coverage of the two bodies found in a parking lot with stab wounds.  The police identified the man as Wendy's husband, Simon.  Before June leaves to spend time with Maggie, the police come to the front door.  They have evidence that June had an affair with Simon and told him to leave his wife or else.  She tells them about the blackmail and gets the briefcase of money to back up her story.  But inside it is her bloody coat and the murder weapon.  Rick is the only one who knew about the bloody clothing, and she knows he set her up. June wakes up handcuffed to a hospital bed with questions from Maggie about the murders. With no one on her side, June breaks out of the hospital and crashes her car into a river.  The police declare June dead.  But she is not, and June will exact bloody revenge on Rick.<br/><br/>This movie is one of the best 'framed' movies on Lifetime.  Within the first 20 minutes, the writers lay down the plot and its full steam ahead. This film depicts acts of sexual assault, so there should be a trigger warning for survivors of this crime. With all the positives, the antagonist's monologue is ridiculous.  See if you can spot the fatal flaw in his plan.  However, if this movie won't trigger you, then be prepared to be very entertained.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/framedbymyhusband.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/framedbymyhusband.jpeg"
-    }
-  }
-  /*
-  {
-    "option":"1",
     "mtitle":"Next Door Nightmare",
-    "review":"<br/><br/>* out of 5 stars.",
+    "review":"Helen argues with her daughter, Kelly, and Kelly leaves in a rage. When Helen sees the police, she knows Kelly must have sent them.  But Helen is wrong.  Kelly and her unborn baby died in a car crash.  Distraught, Helen refuses to talk to the police and attacks an officer with a knife. A mental hospital labeled Helen with extreme codependency.  Helen spends nine months in a mental institution before her release.  It's been one month after Helen's release, and Grace, a social worker, comes over for a mandatory wellness check. It pleases Grace to see Helen taking responsibility, but Grace wants Helen to join a support group for women her age.  As Helen walks Grace out, she sees a new family moving into the home across the street and introduces herself.  The married couple is Sara and Kyle.  Three years into their marriage, they struggled with infertility, and Kyle cheated on Sarah with Angela. Sarah admits the whole sordid past to Helen while Kyle is on business.  Helen offers to be a shoulder for Sarah to lean on day or night, especially when Sarah finds out she is pregnant.  But this isn't an offer Sarah should take.  Kelly wasn't Helen's daughter.  Helen met Kelly once on the internet and fixated on Kelly and Kelly's unborn baby after Helen's real daughter, Layla, committed suicide.  Over the years, Helen has collected and killed several daughters.  And if Sarah isn't careful, she will be the next one on Helen's list.<br/><br/>If you watch this movie from Helen's point of view, it will entertain you with her trickery.  Whether she hires someone or gets her hands dirty, Helen will stop at nothing to get her daughter and grandchild by her side.  Blood or not. You have an idea of her capabilities, but not how deadly she can be to others.  This movie won't be a favorite.  But if you are rooting for the killer, you will keep watching.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nextdoornightmare.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nextdoornightmare.jpeg"
@@ -57,7 +11,7 @@ var madeForTV = [
   {
     "option":"2",
     "mtitle":"Dead in the Water",
-    "review":"<br/><br/>* out of 5 stars.",
+    "review":"Tara's photography blog isn't launching as planned, and she waitresses on the side to make ends meet. She vents to her friend, Amy, and Amy suggests they go to her family's lakehouse before her parents sell it.  As Tara hears the word lake, she flashes back to 10 years ago.  Her brother, John, verbally abused her about becoming a photographer.  He pushed her, and her camera fell off a bridge.  Tara walked to the bottom of the bridge but couldn't find the camera.  After Tara walked to the top of the bridge, she found her camera there, but John's body in the water. Amy tells Tara to leave the past behind and have fun with her life.  After some browbeating, Tara decides to go. During a photoshoot, Tara and Amy run into Lucas - a man in need of a charger - and Amy invites him to the lakehouse.  Soon the women are at each other's throats and flirting with Lucas.  At the end of this ill-fated trip, someone will be dead.<br/><br/>This movie is the definition of frenemies. As much as they apologize, they belittle each other.  It's all too easy for someone to come in and break them apart. As you watch Tara's current and past relationships, you will witness that she is an emotional punching bag for John, Derek, and Amy. After all of that, it's easy to understand who is stalking Tara, but it's why that will keep you watching.  And the 'why' is the creepiest reason in a Lifetime movie ever.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadinthewater.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadinthewater.jpeg"
@@ -66,7 +20,7 @@ var madeForTV = [
   {
     "option":"3",
     "mtitle":"Keeping Up With the Joneses: The Wrong Blackmail",
-    "review":"<br/><br/>* out of 5 stars.",
+    "review":"After Robyn's safe return, the Jones women get back to work.  But someone has the family in their crosshairs.  Pam, Kayla, Carrie, and Tara get messages containing blackmail.  Pam's message has evidence of her affair with William, and Kayla's message has pictures of her kissing another man.  Carrie's email contained IRS documentation, and Tara received a spreadsheet of her drug sales. Pam wants to handle it among the family because she doesn't know who to trust.  Tara takes the lead and uses her hacking skills to uncover who is behind it all. Uncovering the truth will have them looking into Theodore Jones's death, the company's past, and the library's microfiche.<br/><br/>The best of the series will leave you wanting more.  This movie series needs to become a TV series or a quarterly movie series.  The Jones women have so many more stories to tell. The ending brings the women together, but so much hangs in the balance to tear them apart.  This movie can't be the end to the Jones clan. LMN, bring these strong women back ASAP.<br/><br/>5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongblackmail.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongblackmail.jpeg"
@@ -75,7 +29,7 @@ var madeForTV = [
   {
     "option":"4",
     "mtitle":"Labor of Lies",
-    "review":"<br/><br/>* out of 5 stars.",
+    "review":"Lara feels alone in her pregnancy. So, she goes online to find a pregnancy support group, and Bump Buddies comes back as the first result.  Lara creates a profile, and it doesn't take long to get a match named Jamie.  Jamie extends an offer to video chat. They make plans to meet up at pregnancy yoga, and Lara is happy to have a comrade.  But Jamie holds many dark secrets.  First, she promised her unborn baby to a baby broker. The broker's bodyguard, Darryl, doesn't trust Jamie, so he decides to follow her.  Second, Jamie told her estranged husband, Tony, that the baby is his, but Tony demands a paternity test when he gets back from Paris.  The third and biggest lie, Jamie isn't pregnant. She has to get a baby, dodge Darryl, and fix a paternity test before Tony gets home.<br/><br/>This movie is a thriller because it starts where most Lifetime movies end. The number of lies Jamie tells to keep up the ruse is astounding.  She lies to Lara, Tony, the broker, Darryl, and herself. But it's only a matter of time until she gets discovered.  And that moment will come down to a phone call.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/laboroflies.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/laboroflies.jpeg"
@@ -84,12 +38,12 @@ var madeForTV = [
   {
     "option":"5",
     "mtitle":"Love in Whitbrooke",
-    "review":"<br/><br/>* out of 5 stars.",
+    "review":"Amber sits with her boss, Roland, to discuss her future with the company.  For years, the company had Amber travel around the world, but now they want her to stay in London. Amber promises to think it over.  Roland warns Amber that she has eight days to give him an answer, or they will have to go with another candidate.  When Amber gets home, she receives a phone call from her mom, Barbara.  Amber's father, Rick, sprained his ankle.  And with the Founder's Day Festival happening in a few days, Barbara needs help preparing for the festival and running the family's café. Amber books the next bus to Whitbrooke to assist her family.  Before she boards, the driver asks for her ticket. Seeing that Amber is out of sorts, he offers to let her ride without a ticket.  Grateful, Amber boards and heads to her parents' home. After a day of screwing up coffee orders, Amber relaxes at her parents' home.  Then, Jason knocks at their door.  Amber remembers him as the bus driver.  She doesn't know that he is one of the last two descendants of the Whitbrooke founders. Now, he needs help with the festival, and Amber offers her project management expertise.  Together, they will bring out the best and aspirational sides of each other.  With that comes love.<br/><br/>This movie displays one of the most mature relationships on UP TV.  Amber and Jason both yearn to do something more fulfilling in their lives. As they work together, Amber and Jason talk about what they want and how their lives would change if they went after it.  And her job offer deadline pushes their romance forward.  While the movie drags in the middle, the initial setup and ending are highly entertaining.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveinwhitbrooke.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveinwhitbrooke.jpeg"
     }
-  }*/
+  }
 ];
 
 module.exports = madeForTV;
