8c7c3bcc423686e1d9300543528e5eaedb3d06fb
Tiffany Gray <KarateTD@yahoo.com>
2021-07-25 00:24:56 -0400

    incomplete 07312021

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"You're Not Safe Here",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "smallImageUrl":"",
+      "largeImageUrl":""
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Malicious Motives",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "smallImageUrl":"",
+      "largeImageUrl":""
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Murder on Maple Drive",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/murderonmapledrive.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/murderonmapledrive.jpeg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Next Door Nightmare",
     "review":"Helen argues with her daughter, Kelly, and Kelly leaves in a rage. When Helen sees the police, she knows Kelly must have sent them.  But Helen is wrong.  Kelly and her unborn baby died in a car crash.  Distraught, Helen refuses to talk to the police and attacks an officer with a knife. A mental hospital labeled Helen with extreme codependency.  Helen spends nine months in a mental institution before her release.  It's been one month after Helen's release, and Grace, a social worker, comes over for a mandatory wellness check. It pleases Grace to see Helen taking responsibility, but Grace wants Helen to join a support group for women her age.  As Helen walks Grace out, she sees a new family moving into the home across the street and introduces herself.  The married couple is Sara and Kyle.  Three years into their marriage, they struggled with infertility, and Kyle cheated on Sarah with Angela. Sarah admits the whole sordid past to Helen while Kyle is on business.  Helen offers to be a shoulder for Sarah to lean on day or night, especially when Sarah finds out she is pregnant.  But this isn't an offer Sarah should take.  Kelly wasn't Helen's daughter.  Helen met Kelly once on the internet and fixated on Kelly and Kelly's unborn baby after Helen's real daughter, Layla, committed suicide.  Over the years, Helen has collected and killed several daughters.  And if Sarah isn't careful, she will be the next one on Helen's list.<br/><br/>If you watch this movie from Helen's point of view, it will entertain you with her trickery.  Whether she hires someone or gets her hands dirty, Helen will stop at nothing to get her daughter and grandchild by her side.  Blood or not. You have an idea of her capabilities, but not how deadly she can be to others.  This movie won't be a favorite.  But if you are rooting for the killer, you will keep watching.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Dead in the Water",
     "review":"Tara's photography blog isn't launching as planned, and she waitresses on the side to make ends meet. She vents to her friend, Amy, and Amy suggests they go to her family's lakehouse before her parents sell it.  As Tara hears the word lake, she flashes back to 10 years ago.  Her brother, John, verbally abused her about becoming a photographer.  He pushed her, and her camera fell off a bridge.  Tara walked to the bottom of the bridge but couldn't find the camera.  After Tara walked to the top of the bridge, she found her camera there, but John's body in the water. Amy tells Tara to leave the past behind and have fun with her life.  After some browbeating, Tara decides to go. During a photoshoot, Tara and Amy run into Lucas - a man in need of a charger - and Amy invites him to the lakehouse.  Soon the women are at each other's throats and flirting with Lucas.  At the end of this ill-fated trip, someone will be dead.<br/><br/>This movie is the definition of frenemies. As much as they apologize, they belittle each other.  It's all too easy for someone to come in and break them apart. As you watch Tara's current and past relationships, you will witness that she is an emotional punching bag for John, Derek, and Amy. After all of that, it's easy to understand who is stalking Tara, but it's why that will keep you watching.  And the 'why' is the creepiest reason in a Lifetime movie ever.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadinthewater.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadinthewater.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Keeping Up With the Joneses: The Wrong Blackmail",
-    "review":"After Robyn's safe return, the Jones women get back to work.  But someone has the family in their crosshairs.  Pam, Kayla, Carrie, and Tara get messages containing blackmail.  Pam's message has evidence of her affair with William, and Kayla's message has pictures of her kissing another man.  Carrie's email contained IRS documentation, and Tara received a spreadsheet of her drug sales. Pam wants to handle it among the family because she doesn't know who to trust.  Tara takes the lead and uses her hacking skills to uncover who is behind it all. Uncovering the truth will have them looking into Theodore Jones's death, the company's past, and the library's microfiche.<br/><br/>The best of the series will leave you wanting more.  This movie series needs to become a TV series or a quarterly movie series.  The Jones women have so many more stories to tell. The ending brings the women together, but so much hangs in the balance to tear them apart.  This movie can't be the end to the Jones clan. LMN, bring these strong women back ASAP.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongblackmail.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongblackmail.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Labor of Lies",
-    "review":"Lara feels alone in her pregnancy. So, she goes online to find a pregnancy support group, and Bump Buddies comes back as the first result.  Lara creates a profile, and it doesn't take long to get a match named Jamie.  Jamie extends an offer to video chat. They make plans to meet up at pregnancy yoga, and Lara is happy to have a comrade.  But Jamie holds many dark secrets.  First, she promised her unborn baby to a baby broker. The broker's bodyguard, Darryl, doesn't trust Jamie, so he decides to follow her.  Second, Jamie told her estranged husband, Tony, that the baby is his, but Tony demands a paternity test when he gets back from Paris.  The third and biggest lie, Jamie isn't pregnant. She has to get a baby, dodge Darryl, and fix a paternity test before Tony gets home.<br/><br/>This movie is a thriller because it starts where most Lifetime movies end. The number of lies Jamie tells to keep up the ruse is astounding.  She lies to Lara, Tony, the broker, Darryl, and herself. But it's only a matter of time until she gets discovered.  And that moment will come down to a phone call.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/laboroflies.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/laboroflies.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Love in Whitbrooke",
-    "review":"Amber sits with her boss, Roland, to discuss her future with the company.  For years, the company had Amber travel around the world, but now they want her to stay in London. Amber promises to think it over.  Roland warns Amber that she has eight days to give him an answer, or they will have to go with another candidate.  When Amber gets home, she receives a phone call from her mom, Barbara.  Amber's father, Rick, sprained his ankle.  And with the Founder's Day Festival happening in a few days, Barbara needs help preparing for the festival and running the family's café. Amber books the next bus to Whitbrooke to assist her family.  Before she boards, the driver asks for her ticket. Seeing that Amber is out of sorts, he offers to let her ride without a ticket.  Grateful, Amber boards and heads to her parents' home. After a day of screwing up coffee orders, Amber relaxes at her parents' home.  Then, Jason knocks at their door.  Amber remembers him as the bus driver.  She doesn't know that he is one of the last two descendants of the Whitbrooke founders. Now, he needs help with the festival, and Amber offers her project management expertise.  Together, they will bring out the best and aspirational sides of each other.  With that comes love.<br/><br/>This movie displays one of the most mature relationships on UP TV.  Amber and Jason both yearn to do something more fulfilling in their lives. As they work together, Amber and Jason talk about what they want and how their lives would change if they went after it.  And her job offer deadline pushes their romance forward.  While the movie drags in the middle, the initial setup and ending are highly entertaining.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveinwhitbrooke.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveinwhitbrooke.jpeg"
     }
   }
 ];
