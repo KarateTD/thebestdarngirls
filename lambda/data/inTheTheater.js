8eb6f60172a4da0e486ae81e9a7ca45bf6a77a7c
Tiffany Gray <KarateTD@yahoo.com>
2021-08-06 10:20:15 -0400

    5 tv movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,47 +1,47 @@
 var inTheTheaters = [
   {
     "option":"1",
-    "mtitle":"The Suicide Squad",
-    "review":"Amanda Waller needs to assemble a larger team for her new mission, Operation Starfish.  She gathers three members of the original Suicide Squad - Rick Flag, Harley Quinn, and Boomerang - and adds more firepower to the team.  She gets T.D.K, Savant, Black guard, Javelin, Mongal, and Weasel to join. While this team is a formidable opponent for any enemy, Waller created another without telling Flag.  This team consists of Nanaue, Bloodsport, Polka-Dot Man, Ratcatcher 2, and Peacemaker.  These teams have to travel to Corto Maltese and infiltrate the Jötunheim, a Nazi-era prison and laboratory.  The Thinker is conducting experiments with alien technology to overthrow the American government.  After Team A fails, leaving Harley Quinn and Rick Flag prisoners, Team B will go on a bloody rampage to complete the mission.<br/><br/>O.M.G! James Gunn added the wit of Deadpool to the blood and guts of The Kingsman with Tarantino storytelling.  This movie not only holds up to its name, but it gives you the darkness you would expect from a D.C. comic.  While you are enjoying the often-uncomfortable humor, you see awe-inspiring juxtaposition in many fight scenes. The camera angles and framing give the viewer a different view of each fight scene. Seeing the first movie isn't necessary to enjoy this film. This movie is worth the theater price but not for kids nor tweens.  And stay past the end credits for a glimpse of survivors.<br/><br/>5 out of 5 stars.",
+    "mtitle":"Psycho Storm Chaser",
+    "review":"<br/><br/>* out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesuicidesquad.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesuicidesquad.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psychostormchaser.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psychostormchaser.jpeg"
     }
   },
   {
     "option":"2",
-    "mtitle":"Disney's Jungle Cruise",
-    "review":"As Dr. Lily Houghton's brother, MacGregor, presents her research to the association, Lily sees they will deny their request for access to the arrowhead.  So, she sneaks around the building in hopes of finding it.  With a bit of maneuvering and ingenuity, Lily finds the box containing the arrowhead and takes it.  But Prince Joachim sees her and, unbeknownst to Lily, puts her life in danger. When she gives him the box, he releases her to her death, but MacGregor saves her.  Luckily, Lily removed the arrowhead from the box with a sleight of hand. Lily and MacGregor head to Belize to start their search for the Tears of the Moon.  A healing tree whose petals can heal any illness and lift any curse.  Upon arriving in Belize, Lily looks for Nilo, a famed harbormaster, to get to the Tree of Life but finds Nilo's door locked.  She hears a commotion behind the door and assumes it's Nilo, but Lily is wrong.  It's Frank.  Earlier that day, Nilo took Frank's engine because Frank defaulted on a loan.  Frank broke into Nilo's office to steal the key and unlock the crate with his engine inside. Frank opens the door and lets Lily believe he is Nilo.  At first, Frank doesn't want to go because it's dangerous, but when he sees the arrowhead around Lily's neck, he gets on board with the plan. But Prince Joachim is after them, and he will raise the dead to get to the Tears of the Moon and aid his army in the Great War.<br/><br/>You must come to this movie on time because a lot happens in the first five minutes. This movie also provides a bit of history with conquistadors, Prince Joachim, and British War Effort packed into an enthralling adventure. While the leads have great chemistry, you must remember the CGI is more cartoonish, like Pirates of the Caribbean.  This film is enjoyable whether you choose to watch it at home or in the theaters, and there is no need to stay after the end credits, but you get some platinum dad jokes.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Death She Wrote",
+    "review":"<br/><br/>* out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/disneyjunglecruise.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/disneyjunglecruise.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deathshewrote.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deathshewrote.jpeg"
     }
   },
   {
     "option":"3",
-    "mtitle":"Stillwater",
-    "review":"Bill retrieves items from his mother-in-law, Sharon, for his daughter, Allison, and makes the long trip from Stillwater to Marseille. In the morning, he sees Allison in the visiting room of a prison.  Five years ago, a judge convicted Allison of killing her roommate and girlfriend, Lina.  With four more years left on her sentence, Allison has new information and gives a letter to Bill. Bill presents the letter to the lawyer, but she tells Bill that the letter means nothing. Bill goes back to the prison and lies to Allison. Allison is ecstatic, so Bill gets to work.  He asks his hotel neighbor, who pretended not to be an Anglophile, Virginie, to translate the letter for him.  She reads the letter, and it states that someone told a professor that a man named Akim admitted to killing Lina. Bill's hunt for Akim will cost him a daughter, a family, and his view on the world.<br/><br/>Everything you heard about this movie is true.  The actors give stellar performances, and they are a shoo-in for award season.  The plot, however, drags because it has a love story in the middle of a rescue mission.  But don't worry, the love story has a greater meaning to the plot, so hang in there.  Unlike most movies in this genre, Bill doesn't find high-ranking officials to help his case, and he doesn't get help from an American lawyer.  Bill learns French, gets a job, and becomes friends with locals to help his daughter. The story and its twist will stick with you.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Deadly Mom Retreat",
+    "review":"<br/><br/>* out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stillwater.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/stillwater.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlymomretreat.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlymomretreat.jpeg"
     }
   },
   {
     "option":"4",
-    "mtitle":"Space Jam: A New Legacy",
-    "review":"In 1998, LeBron missed the winning shot in a basketball game, and his coach, Coach C, tells him it's because he focused on a Game Boy and not the game.  Since then, LeBron has put every ounce of his focus into the four lines on the court.  And he wants his sons, Dom and Darius, to do the same. Dom wants to focus on the video game he is creating called Dom-ball. When LeBron checks out Dom-ball, he is impressed but confused.  The video game goes beyond the fundamentals and gives players style points. LeBron goes into a meeting with Warner Brothers and takes Dom with him to see their new technology.  The technology is called Warner Brothers 3000, and it will launch soon, and having LeBron promote it will make the technology skyrocket. While Dom loves the idea, LeBron thinks it's stupid.  LeBron doesn't know, but the creator of Warner Brothers 3000 sees and hears LeBron insult his work, and he is furious.  The creator is Al G. Rhythm, and Al is an algorithm that lives in the server-verse. When LeBron says no, Dom leaves the meeting, and LeBron goes after him.  Al redirects the elevator to the server room and sucks them into the server-verse.  Then, Al takes LeBron's phone, hides Dom, and gives LeBron an ultimatum.  LeBron can get Dom back if he wins a game of basketball. LeBron thinks it will be an easy win, but Al sends LeBron to the rejects to find teammates in Tune World.  The only person left is Bugs Bunny.  Al convinced all the Toons to leave, and Bugs is ready for his revenge.  Al didn't tell LeBron that he is not playing regular basketball, but LeBron will be playing Dom-ball with a worldwide, captive audience.<br/><br/>Based on Space Jam, this movie adds family love and makes fun of its plot for being similar to the first.  In the server-verse, viewers will see plenty of Warner Brothers classics like Harry Potter, Casablanca, Game of Thrones, and Star Trek, to name a few.  Spotting the Easter Eggs will keep adults focus on the movie more than the plot. Don't be fooled; you do get an exciting and Looney game of basketball with all the classic Looney Tunes you love.  And you do get Michael Jordan.  While you may not buy it in stores, you will get your money's worth watching on the big screen.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Mystery 101: Deadly History",
+    "review":"<br/><br/>* out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/spacejamanewlegacy.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/spacejamanewlegacy.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mystery101deadlyhistory.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mystery101deadlyhistory.jpeg"
     }
   },
   {
     "option":"5",
-    "mtitle":"Black Widow",
-    "review":"In Ohio 1995, Alexei came home to warn his wife, Melina, that it was time to leave.  Alexei and Melina grabbed their two daughters, Natasha and Yelena, and ran off under a hail of gunfire.  The family flies to Cuba, and Dreykov declares their mission complete. And guards come for Natasha and Yelena.  Seeing this, Natasha grabs a guard's gun and tells them that they can't take her little sister to the Red Room because she is only six.  Alexei tells the girls to stay tough and stay together.  Twenty-one years later, Natasha is on the run after violating the Sokovia Accords. In Morocco, Yelena's target is on the move, and Yelena goes after her.  Just as Yelena turns the target over for the kill strike, the target sprays gas in Yelena's face. Yelena mails to vials to Budapest.  At her safehouse, Natasha's secret handler gives Natasha her mail from the Budapest safe house.  She doesn't know, but one of the packages is the vials from Yelena. The Taskmaster hits Natasha's car and runs her off the road.  In a fight, Natasha realized the Taskmaster isn't after her but after the vials.  She gets away with them and heads to Budapest.  In Budapest, Yelena waits for Natasha.  After a bloody truce, Yelena tells Natasha that the Red Room and Dreykov still exist. After Natasha left, Dreykov tightened the reins on his Widows and gave them a mind-control agent.  One of the first recipients was Yelena.  Now, the Black Widows are conscious but can't make any decisions.  However, the vials contain an antidote.  Natasha and Yelena want to free the Widows while taking down Dreykov and the Red Room.<br/><br/>This movie is about repairing family and wiping the slate clean.  When they sit to talk, Melina and Alexei come to terms with how they ruined Natasha and Yelena's childhoods.  But Natasha has to come to terms with leaving her sister behind to become an Avenger. The actions scenes are some of the cleanest in the MCU because they don't look cartoonish like the Hulk or Spider-Man. With all the emotion and physicality, something still feels off about the movie.  It's the same feeling you get watching Ant-Man or the 2nd Thor movie. Consider this movie as an introduction to Yelena, and it will make a lot more sense.  While it's worth seeing on the big screen, don't pay extra for IMAX or 3D.  Of course, stay past the end credits.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Waking Up to Danger",
+    "review":"<br/><br/>* out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackwidow.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackwidow.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/wakinguptodanger.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wakinguptodanger.jpeg"
     }
   }
 ];
