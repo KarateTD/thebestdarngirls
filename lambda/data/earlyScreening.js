50aaeee5fbacbd69eabed97c76f43aa3e3da9d34
Tiffany Gray <KarateTD@yahoo.com>
2021-08-17 16:47:18 -0400

    add 2 in stores

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -1,4 +1,13 @@
 var earlyScreening = [
+/*  {
+    "option":"1",
+    "mtitle":"The Protégé",
+    "review":"<br/><br/>* out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theprotege.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theprotege.jpg"
+    }
+  },*/
   {
     "option":"1",
     "mtitle":"CODA",
@@ -19,15 +28,6 @@ var earlyScreening = [
   },
   {
     "option":"3",
-    "mtitle":"The Hitman's Wife's Bodyguard",
-    "review":"After graduating from therapy, Michael takes a much-needed vacation from bodyguarding and goes to Italy.  As he sits and listens to music, Sonia Kincaid rips off his headphones and pulls him out of the line of fire.  She tells Michael that her husband, Darius, took a job on their honeymoon, and thugs kidnapped Darius. Michael and Sonia go on a bloody mission and find Darius with mobster Carlos. The group fights their way out of Carlos's headquarters, but they are ambushed and dragged into a van.  Agent Bobby pulls the bag off their heads and tells Sonia, Michael, and Darius that they messed up his mission.  Carlos was Bobby's informant against Aristotle.  Aristotle is a Greek national who hates the EU's financial sanctions against his country, so he bought a computer virus.  With one upload to a power grid, Aristotle can take out an area of any size he chooses.  Carlos's job was to buy the case and give it to INTERPOL instead of giving it to Aristotle.  With Carlos's death, Bobby has to go to plan B: Michael, Sonia, and Darius.  Sonia will play Carlos's mistress, and Michael will be her bodyguard.  Darius will maintain a safe distance and watch over them. If they get the virus, Darius and Sonia can get some of their crimes forgiven, and Michael can get his bodyguard license back. With no time to make love, Sonia will never get the family she wants.  This mission should be easy for the murderous triad, but an exploding bracelet, amnesia, and gelato will make it difficult.<br/><br/>A sequel can rarely be better than the first, but The Hitman's Wife's Bodyguard gets it right.  It gives you wit and action from beginning to end. In the first movie, viewers got an introspective look into the life of Darius.  This time, we get a look at Sonia's beginnings as a con-woman and Michael's beginning as a bodyguard.  While Michael is on a mission to get his license back, Sonia focuses on creating the family of her dreams. With nods to Overboard, Tim McGraw, and Armageddon, you don't have to see the first movie to enjoy the sequel.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thehitmanswifesbodyguard.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thehitmanswifesbodyguard.jpeg"
-    }
-  },
-  {
-    "option":"4",
     "mtitle":"Songbird",
     "review":"Four years after COVID-19, COVID-23 is airborne, affects the brain, and causes rapid death.  Anyone without an immunity bracelet must stay inside at all times and do a 9 a.m. health scan with their phone.  If they fail the scan, the sanitation department comes to their door with armed guards and escorts them to the Q Zone, also known as the quick death zone.  Some would consider Nico one of the lucky few because he has an immunity bracelet. During the worldwide lockdown, he met Sara. Sara lives with her grandmother and sees people pulled away by Emmett and the sanitation department all the time. On the other side of town, Piper and William raise their daughter, Emma.  While they are immuned, Emma has a weak immune system and could get sick with ease. To make money, Piper and William sell black market immunity bracelets to powerful and wealthy people.  As Piper takes every precaution, William leaves in a suit and breathing mask to meet his mistress, May.  May sings online under the handle MayDay, and people watch and donate money. Their intertwined lives will come to a deadly end when someone gets sick and fails a health check.  They will need an immunity bracelet or die in the Q Zone.<br/><br/>While setting the current pandemic four years in the future, this movie is a mix between Crash and Mission Impossible.  This movie does cover a lot of topics like immuned people passing a disease, disease mutation, and proper quarantine techniques. However, at the core of this movie are three love stories and one obsession story.  The pandemic causes and affects each of these stories.  Seeing the satisfyingly bloody end will entice you to keep watching.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -36,7 +36,7 @@ var earlyScreening = [
     }
   },
   {
-    "option":"5",
+    "option":"4",
     "mtitle":"Come Away",
     "review":"Alice sits on a bed and reads a story to her children about her childhood.  Years ago, Alice had two brothers, Peter and David.  The children love to play in the woods a pretend to have sword fights, throw arrows, and battle pirates. Their mother, Rose, gives Alice a tinker's bell.  She tells Alice she can summon fairies with it. David is the smartest of the three children and never wants to grow up.  Peter, like his father, Jack, is an excellent carver.  Without Jack's knowledge, Eleanor enrolled David in boarding school.  Before David can converse with Rose about the matter, Rose explains that she has accepted Eleanor's offer.  All three children are upset David is leaving. David and Peter went into the woods to play, and David slipped into the water and drowned. Amid his pain, Jack goes to the local underground casino and creates a massive debt.  CJ wants his money tonight, and Jack writes an IOU on a napkin.  He goes home and believes he is even with CJ.  But, someone throws a brick through Jack's window with the napkin attached.  Alice and Peter take it upon themselves to help their dad.  They have no idea the wheels they will set in motion, linking their father's past, their imagination, and divided futures.<br/><br/>Based on Alice's Adventures in Wonderland by Lewis Carroll and Peter Pan by J. M. Barrie, this isn't the tale you know.  It's the tale of how Peter and Alice came to be.  One event gave Alice and Peter the same pain, but they handled it in different ways. With the intertwining stories, it's the special effects merging the children's fantasies with their reality that will make you do a double-take.  It's reminiscent of Rugrats. Sit back and enjoy with children six and up.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -44,6 +44,7 @@ var earlyScreening = [
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/comeaway.jpg"
     }
   }
+
 ];
 
 module.exports = earlyScreening;
