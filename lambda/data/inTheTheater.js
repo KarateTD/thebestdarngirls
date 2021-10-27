1f76771667b55d6d4b36a56c6e0b7b974503eead
Tiffany Gray <KarateTD@yahoo.com>
2021-10-27 02:12:27 -0400

    one theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Dune",
+    "review":"Paul Atreides wakes up from another dream about a young Freman woman.  While he lies to his mother, Lady Jessica, about the visions, he practices his voice with her.  Paul, like Jessica, has telepathic abilities, but they have to find the right tone to command people to do what they say.  After the lesson, Paul meets with his mentor Duncan.  Paul requests to go on Duncan's next mission, but Duncan says no. Later, Paul tells his father, Leto, the ruler of Caladan, that he is not ready to lead. Paul attends Leto's takeover of the Arrakis from the House of Harkonnen, ruled by Baron Vladimir.  Leto's faction will have control of the 'spice' produced by Arrakis.  'Spice' extends the human lifespan and is a critical element for interstellar space travel. While Vladimir hands over Arrakis freely, he does it with a trick up his sleeve.  Vladimir weakened the spice harvesters, created a plan to kill Paul, and added a mole in the House of Atreides. Paul will have to step into Leto's shoes and use all of his training to save Atreides, his family, 'spice,' and the Fremen.<br/><br/>Based on Dune by Frank Herbert, this remake is one of the finest visual movies of all time.  Viewers become engrossed in Frank's world with the costumes, scenes, creatures, and machinery. Seeing a film of this scale on a large screen is a must.  You can not be late for this movie.  The first 10 minutes cover the entire backstory for each house and the importance of spice.  Now what they gave the audience in visual effects, they lost in some of the audio.  The internal voices in Paul's mind become hard to understand when several voices overlap. Understandably, producers wanted the fighting scenes and sound effects to be grand, but it shouldn't come at the cost of dialog.  While you won't pay to see it again, you will be glad you saw it in theaters. But don't wait too long, because part 2 is in the works.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dune.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dune.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"The Last Duel",
     "review":"Marguerite de Carrouges sits stone-faced at the dinner table with her husband, Sir Jean de Carrouges, and mother-in-law. Once in the bedroom, he tries to have sex, but Marguerite refuses. Before he left, Jean told Marguerite not to leave the house nor be alone because it wasn't safe.  However, Marguerite's mother-in-law departed and took all the servants.  A man Marguerite knew came to the door, and she let him in.  But she didn't see Jacques Le Gris standing by the door.  When Marguerite opened it, both men came in.  Jacques declared his love for her, but she refused him.  Then, Jacques raped her. Jacques used his power to gain the land promised in Marguerite's dowry and took Jean's captain position under Count Pierre d'Alençon. Jean thinks Jacques raping his wife is another slight against him.<br/><br/>Jean knows that Pierre will reject any charges because Jacques is his friend.  So, Jean uses gossip to have the charges tried in a church trial.  During the proceedings, Jean lays down his glove to declare a duel to the death to prove Marguerite is telling the truth, and Jacques accepts. The judge tells Marguerite that she will be chained with an iron collar, tied to a post, whipped, and burned alive. With her life in the hands of Jean, Marguerite declares that she told the truth and she will suffer any fate of the duel.<br/><br/>Based on The Last Duel: A True Story of Crime, Scandal, and Trial by Combat by Eric Jager and inspired by actual events, the writer tells this plot in three chapters.  The first chapter is Jean's truth. The second chapter is Jacques's truth. The final chapter is Marguerite's truth. Watching the same events from different perspectives allows you to see the events through someone else's eyes. So pay attention.  With that said, this movie should come with a trigger warning for sexual assault survivors.  It depicts Marguerite's rape twice. While the costumes and performances were accurate, you won't leave the movie feeling like you want to see it twice.  But the gruesome battles and duels make the film worth seeing on the big screen.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"No Time to Die",
     "review":"After taking down Spectre, Bond retired the spy's life for a quaint life with Madeleine. Bond goes to Vesper Lynd's tomb but quickly finds himself under attack.  One of his attackers says that Madeleine is the daughter of Spectre. Five years later, Felix leaves a clue for Bond to find him.  Bond locates Felix, and Felix tells Bond that someone kidnapped a Mi6 scientist named Valdo Obruchev. Bond says no and leaves, but his car won't start.  A woman, whom he bumped into at the club, offers him a ride.  At his home, the woman reveals her name is Nomi, and she is the new Double-O-7. While Bond believes Spectre and Blofeld are behind the plot, they are merely puppets.  The puppet master will collide Bond's past with his future while uncovering Madeleine's secrets.<br/><br/>The follow-up to Spectre picks up where the last movie left off.  Bond enjoys a peaceful existence without love.  When Bond gets back into action, he is petal to the metal.  But he has the problem every seasoned vet deals with: Fresh Recruits.  They are fawning or attempting to squash his mythical accomplishment.  While the movie is entertaining and puts a chef's kiss on several characters, it's too long.  The editors could remove 30 to 45 minutes of this film and still create an entertaining movie. If you are a fan of this series, you will thoroughly enjoy this film at maximum cost and IMAX.  Otherwise, see it at the matinee price.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"The Many Saints of Newark",
     "review":"Richard Jr. watches his dad, Hollywood Richard, get off the ship with his new girlfriend, Giuseppina.  Hollywood is a soldier in the DiMeo crime family and a made guy.  Richard and his friend, Johnny Soprano, are members of the family too.  After some time, Richard Jr. sees Giuseppina suffer the way his mother did and deduces that Hollywood is abusing her.  Richard snaps and kills Hollywood.  Richard comes up with an idea to get rid of the body. With the riot still taking over the area, Richard goes to the community, puts the body in an abandoned building, and lights the building on fire. With Hollywood gone, Richard steps up and takes over the family, but he needs guidance along the way.  He considers Johnny, but Johnny gets pinched for an assault and weapons charge. With Johnny gone, Richard needs help from someone with experience.  So he goes to the prison to see Sally, his father's twin brother.  Richard tells Sally he wants to do good deeds, and Sally laughs.  But Richard knows where to start: Johnny's son, Tony Soprano.<br/><br/>Narrated by Christopher Moltisanti, this prequel to the critically acclaimed Sopranos is entertaining for fans but falls flat for anyone else.  The story is historically accurate by displaying riots, growing issues between African Americans and Italian Americans, and integration.  While producers market this as a Tony movie, it's not.  This film gives insight into the people around him who facilitated his ill-advised behavior. You get the violence, sex, and gore that you would expect from the genre, but it's nothing new; just adjusted for the times.  If you are a fan of the show, watch it on HBO Max.  Otherwise, wait till it's free to watch.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"The Addams Family 2",
     "review":"Wednesday Addams stands on the stage waiting to hear the teacher call her name as the science fair winner for her intelligence swap experiment. They made everyone the winner so no one would feel like a loser.  However, Wednesday thinks that without a winner, everyone is a loser.  Dr. Cyrus Strange agrees with Wednesday. He wants to see her research, but Wednesday says no because her formulas are a closely guarded family secret.  At home, Wednesday refuses to speak to her family because they embarrass her. Gomez can't stand the thought of disconnecting from his family.  So he plans a cross-country trip that will end at the happiest place on earth: Death Valley. Before they drive off, a lawyer, Mr. Mustela, approaches Morticia and Gomez Addams for his clients.  His clients believe the hospital switched their child at birth, and Wednesday is their biological daughter. Morticia and Gomez think Wednesday hired him. When Morticia and Gomez realize Wednesday didn't hire Mr. Mustela, they quickly get in their RV and drive away. The Addams will need to be extra kooky and creepy to stay one step ahead of Mr. Mustela without tipping off Wednesday.<br/><br/>In this sequel, Wednesday is the epitome of pre-teen angst.  While they are mysterious and spooky, the Addams are a loving family at their core. As Wednesday travels through the country, she learns the meaning of family and being an Addams.  And your children will laugh during her growth.  Expect the typical cartoon violence with a few adult jokes slipped in for parents.  This movie is no better or worse than the first, but it's worth the matinee price.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theaddamsfamily2.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theaddamsfamily2.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Venom: Let There Be Carnage",
-    "review":"Venom must snack on chickens and chocolate because Eddie won't go looking for bad guys to eat.  etective Mulligan calls Eddie into the precinct because the infamous serial killer, Cletus Kasady, wants to talk to Eddie only. At the prison, Cletus asks Eddie to print a message on the front page.  Eddie agrees before he leaves, but Venom slows Eddie down.  Venom notices something on the walls of Cletus's cell.  In the drawing, Venom discovers the burial site of several of Cletus's victims. The city lifts the moratorium on executions, and the state will execute Cletus in a few days.  An upset Cletus writes a postcard to Eddie. Cletus blames Eddie for his execution and wants Eddie to witness it. Face to face, Cletus and Eddie get into an argument, and Cletus angers Venom.  Venom attacks Cletus, and Cletus bites Eddie.  Cletus can taste something different about Eddie's blood and ingest it.  At the execution, Cletus's body starts to fight the chemicals, and he lives. Cletus and Carnage come up with a quid-pro-quo.  Cletus can kill Venom, but only if Carnage helps Cletus find one person: Frances.  With Cletus on the loose, Eddie will need Venom to fight.  Too bad he and Venom broke up.<br/><br/>This movie continues with all the laughs and violence you expect from Venom.  While the plot strengthens the relationship between Venom and Eddie, it offers another love story with a bloody third wheel and a red wedding.  And let's not forget Venom's coming out party. The sound may take some deciphering, but the visuals are top-notch.  It never gets too cartoonish or too realistic. With so much action in this twisted love story, viewers will get a glimpse into part three.  And only stay past the first set of end credits for a universal collision.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/venomlettherebecarnage.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/venomlettherebecarnage.jpeg"
     }
   }
 ];
@@ -55,15 +55,7 @@ module.exports = inTheTheaters;
 */
 
 /*
-{
-  "option":"1",
-  "mtitle":"Dune",
-  "review":".<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dune.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dune.jpeg"
-  }
-}
+
 
 */
 
