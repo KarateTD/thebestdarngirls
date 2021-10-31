efaeda141085700207b0abf1aebaa94f2681b2e1
Tiffany Gray <KarateTD@yahoo.com>
2021-10-30 23:51:46 -0400

    fix typo

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,7 +1,7 @@
 var madeForTV = [
   {
     "option":"1",
-    "mtilte":"Coyote Creek Christmas",
+    "mtitle":"Coyote Creek Christmas",
     "review":"Feeling unappreciated at her job, Paige can't wait to go to her parents' Coyote Creek Inn. However, Paige's parents tell her that Christmas fell by the waist side this year.  Paige won't hear it and declares to do the job in half the time.  Before Paige arrives, Dylan arrives earlier than expected with his son, Noah.  Dylan works for an acquisition firm with his brother. They decided to buy Coyote Creek Inn. However, they don't want to tell Paige until after the holiday.  So they make Dylan promise to keep their secret.  When Paige arrives, she is full steam ahead with ideas.  After hearing people in the lobby speaking a different language, Paige decides to have 'Christmas Around the World' as her theme.  She goes to the library to find books about Christmas traditions and runs into Dylan and Noah.  When Dylan discovers Paige's lineage, he keeps her parents' secret.  Each time they meet, Paige tells Dylan and Noah about the Coyote Creek traditions, and Noah wants to join in. Later, Paige tells Dylan she is not happy at her job and looks forward to taking over the inn when her parents retire.  Dylan wants to reveal the truth, but can she handle it? And will he lose her?<br/><br/>This movie follows the Hallmark holiday format, but it offers a precocious kid with a growing vocabulary and a sneaky marmot.  And for the first time, it shows the single parent struggle. Seeing this realness is a step in the right direction for Hallmark and makes the characters' love believable.  Sit back and have your peppermint fudge, pfeffernusse, and other holiday cookies ready.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/coyotecreekchristmas.jpeg",
@@ -10,7 +10,7 @@ var madeForTV = [
   },
   {
     "option":"2",
-    "mtilte":"Deadly Due Date",
+    "mtitle":"Deadly Due Date",
     "review":"Rachel dreads going to her high school reunion because people may ask if she and her husband, Bill, have kids.  Rachel has had several miscarriages and no children. At the reunion, Bill and Rachel recognize Claire.  Claire is eight months pregnant and alone.  After they catch up, Bill and Rachel offer Claire a ride back to her hotel. Later that night, Rachel gets a call from the hospital.  Someone attacked Claire.  So they bring Claire home to keep her safe.  In the morning, Claire makes Bill's favorite breakfast and tells them her plan.  She wants to give her baby boy up for adoption.  Bill pulls Rachel aside and tells her that they should adopt Claire's baby. She relents, and they tell Claire and allow her to move in.  The revelation leaves Claire overjoyed.  Not because her baby will have a good home.  But because she got the first step in her plan accomplished.  Claire has been in love with Bill since high school. She will kidnap, manipulate, and kill to keep Bill and her baby.<br/><br/>This story is crazy on top of crazy from beginning to end.  When you get the opening scene of a man chasing a girl in the woods, you will roll your eyes.  But don't change the channel.  This plot makes its first deviation from a Lifetime format at that point. While this movie will not be at the top of your Shocktober list this year, it finds a way to entertain you with an over-the-top spectacle.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlyduedate.jpeg",
@@ -19,7 +19,7 @@ var madeForTV = [
   },
   {
     "option":"3",
-    "mtilte":"Christmas in Harmony",
+    "mtitle":"Christmas in Harmony",
     "review":"Harmony walks into work with a smile and cookies.  After her company's merger, she expects to get promoted to Senior Director of Acquisitions.  Instead, she gets fired. Furious, Harmony leaves with some choice words and goes home.  Harmony arrives home to find her smiling mother, Mama Matthews, and sister, Jennifer, waiting for her. She comes to church to see Deacon James attempting to lead the choir.  But he falls short.  Mama Matthews thinks Harmony should sing in the choir.  Harmony says no to singing but yes to directing. With a dozen pies in hand, Harmony bumps into Kyle and smears pie on both of them.  Kyle is Harmony's high school sweetheart.  They were supposed to go to New York together and work on their respective music careers.  But Kyle never got on the plane.  Kyle tells Harmony he came to church to direct the choir. As Mama and James contend verbally, Kyle and Harmony do it musically.  Their sweet sounds give James an idea, co-directors.  To prove their musical abilities, they concede to the arrangement.  After auditions, James and Mama find out that the building's owner wants to sell the church.  To keep the church, they need to make $10,000 by the Holiday Hallelujah.  Kyle and Harmony will need to pull together a performance with a Christmas star to get a miracle.<br/><br/>Get your holiday playlist ready.  This movie will have you dancing in your seat with holiday classics and original songs.  While the vocal performances are astounding, the dubbing was a bit off and became distracting.  With so many subplots, it's hard to keep track of the main focus. Give this movie your full attention, or you will lose track of what's going on.  If you do get disoriented, keep track of the snowglobe.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/christmasinharmony.jpeg",
@@ -28,7 +28,7 @@ var madeForTV = [
   },
   {
     "option":"4",
-    "mtilte":"The Santa Stakeout",
+    "mtitle":"The Santa Stakeout",
     "review":"Tonya is a rookie detective at the police force and shares desk space with Ryan, a veteran detective. The captain calls them into his office because someone robbed several museums after their Christmas parties.  While looking at the photos, Tonya notices all the museums used the same Santa for their parties.  Ryan calls the agency and gets Santa's real name: Frances.  Tonya looks up Frances in the database and finds he has a police record.  While they try to one-up each other, the captain decides to put them on the case together. They sit across the street and stake out Frances's home.  When they try to get a closer look at Frances's delivery, a neighbor notices them.  The neighbor thinks they are a newlywed couple looking at the house for rent.  Tonya and Ryan go with the flow and pretend to be newlyweds, Tasha and Rupert.  As they investigate Frances's home and ongoings, Tonya and Ryan get into the holiday spirit, fall in love, and solve the case.<br/><br/>This movie merges the mystery of H.M.M. movies with the love story of Hallmark films while mixing in Christmas.  And both fans will be happy. While the holiday spirit will warm some, the mystery will feed the intrigue in others.  And thankfully, the love story doesn't sacrifice the mystery.  It's funny, thought-provoking, and charming.  Don't be surprised if you watch this film twice and it becomes your favorite of the holidays.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesantastakeout.jpeg",
@@ -37,7 +37,7 @@ var madeForTV = [
   },
   {
     "option":"5",
-    "mtilte":"Danger Next Door",
+    "mtitle":"Danger Next Door",
     "review":"After being mugged, Robin wants to move to a quiet suburb.  And raise her unborn baby with her husband, Ben.  After some prodding, Ben agrees and moves into a quaint neighborhood.  On their first day, Sharon and Guy come to the house with a basket of goodies.  Sharon sees Robin is pregnant and offers lots of unwanted advice. The next day, Robin starts her new job working for Amanda. She was friends with Sharon and Guy's daughter, Riley.  As they got older, Amanda and Riley grew apart because Riley started hanging with a rough crowd.  When Riley got pregnant, she called Amanda, but Amanda ignored the call.  Later, Riley, her boyfriend, and their unborn baby died in a car accident.  Robin can't imagine the loss Sharon and Guy feel, and she decides to take it easy on them.  The decision to change will leave one person dead, have one person locked in a basement, and give the worst gender reveal in history.<br/><br/>If you are wearing pearls, be ready to clutch them.  This Shocktober entry isn't shocking because of fear.  It's because of Sharon and Guy's audacity.  They enter Rachel and Ben's home without asking, creep around their house at night, and tell the couple what to eat. They start at level ten and stay there. The plot is far-fetched, but Sharon and Guy make it worth it.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dangernextdoor.jpeg",
