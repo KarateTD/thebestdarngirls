76fbcd308b7b7c51896813ac86036ffa14fa3718
Tiffany Gray <KarateTD@yahoo.com>
2021-11-15 19:49:39 -0500

    add 5 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
-    "mtitle":"Highway to Heaven",
-    "review":"Angela materializes with a mission from her boss and a stellar resumé.  Principal Bruce thinks her credentials go beyond their needs and reminds Angela that the guidance counselor job is temporary. A teacher walks into the interview demanding Bruce help him with an unruly student, Cody. Angela talks sense into Cody, but he doesn't want to go to counseling.  Bruce gives Cody a choice: counseling or detention.  In counseling, Cody tells Angela he doesn't need a fix. Angela asks Cody to call his dad, Jeff, but Jeff doesn't answer.  So, Angela walks Cody home to meet Jeff in person.  Angela discovers that Cody nor Jeff have been the same since their matriarch, Melissa, died.  When Jeff gave up on the restaurant, Cody lost again.  Angela asks Jeff about his extended family, but they are spread out all over the world.  And Jeff and Cody were Melissa's family.  Angela must repair this fractured family, or Cody's world will fall apart.  Lucky for Cody, Angela's an angel.<br/><br/>Based on the T.V. show, this movie is a jumping point to a holiday series and stays close to the original.  As Angela works her magic, she makes miracles.  Although, it will feel as if she doesn't know her boss's plan either. At times, Angela's interference leaves Cody with a broken heart. Angela doesn't give Cody what he wants.  Instead, she gives him the tools to cope.  And that will last a lifetime.  Be ready for the next movie in this series.<br/><br/>4 out of 5 stars.",
+    "mtitle":"My Christmas Family Tree",
+    "review":"A few weeks ago, Vanessa and her friend, DeeDee, took a DNA test. When Vanessa looks at her test, she discovers things about her ancestry.  But then Vanessa reads that she has a paternal match.  Vanessa's mother, Patty, died when Vanessa was young, and Vanessa never knew her father. His name is Richard, and Deedee looks him up on social media.  Richard is married with three young kids.<br/><br/>Vanessa takes a chance and leaves Richard a voicemail. Richard immediately calls Vanessa to meet at a local park. Richard tells Vanessa that he never knew about her, but he wants to make up for lost time. He offers to have his friend, Chris, pick up Vanessa on Chris's way to see the family. Vanessa agrees, and Chris picks her up. When Vanessa gets to the house, she meets Richard's wife, Pauline, and their three children, Aidan, Emilia, and Caitlin. Just as Vanessa finds her place in the family and Chris's heart, she gets a call from the DNA company.  There was a mix-up with the test, and Richard is not her father.  Can she bear to tell Richard, his family, and Chris the truth before Richard's entire family comes to meet her?<br/><br/>Have your tissues ready.  This movie will put you through every emotion possible.  You will leap for joy when Richard and Pauline accept Vanessa with open arms.  Next, experience admiration as Vanessa and Chris fall in love.  Then, you will weep as she learns the truth. And it's the last 10 seconds of this film that will leave you in tears.  This movie is an instant classic to add to your collection.<br/><br/>5 out of 5 stars.",
     "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/highwaytoheaven.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/highwaytoheaven.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myfamilychristmastree.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myfamilychristmastree.jpeg"
     }
   },
   {
     "option":"2",
-    "mtitle":"Her Fatal Family Secret",
-    "review":"Abby packs up her desk and says goodbye to her job working for the district attorney to join the most prestigious law firm in the state.  Among her farewells is Abby's estranged husband, Damon. While saying goodbye to Damon, Abby gets a phone call.  Abby and Damon's daughter, Sophie, got suspended again. When Abby gets Sophie home, Abby gets a text message.  Her father, Everett, died, and the police suspect it was murder. Abby tells Sophie that she doesn't talk to that side of her family since college.  Abby puts that behind her and focuses on her new job.  On her first day, Abby's boss, Brice, gives her their top priority case. The client is her estranged sister, Adrianna.  After Everett's nurse, Bianca, stated she saw Adrianna in Everett's room, the police arrested Adrianna. Now Abby has to face her past, rumors, and a murderer.<br/><br/>Now, this movie is one to watch with two twists.  The first is a bit predictable, but the second is not, even for the most experienced Lifetime viewer.  It does take a moment to grab your attention because viewers start to see the typical Lifetime setup: bratty teen, cheating husband, and estranged family.  With preconceived notions that you know the movie, it's easy to walk away.  But that would be a mistake.  When the story gets going, you won't be able to look elsewhere.  DVR this movie so you can skip the commercials.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"A Snowy Christmas",
+    "review":"Kelly comes home to find her mother's reservation book empty. When her father died, Sheila couldn't keep up with the maintenance.  And the B and B deteriorates.  Kelly talks to her sister, Grace, about selling it, but Grace refuses. Grace and Kelly agree to disagree, and they go outside to build a snowman.  As they create it, Kelly talks about she wants in a husband and dresses up the snowman.<br/><br/>With a puff of magic snow dust, Kelly and Grace walk away from the snowman.  After a family leaves the B and B due to cold water, Kelly wants to hire a contractor to fix some issues. Then, someone knocks on the door.  It's Forrest, and he is a contractor. So he came to help.  Kelly smiles and gives Frank a tour of the property.  Over their next few meetings, Kelly notices that Frank checks all the boxes on her snowman list. Kelly wonders if she got a Christmas miracle and her snowman came to life.  When a buyer comes with an offer, Kelly sees the name on the contract and thinks her Christmas miracle is a tragedy.  Has Kelly lost the love that she made?<br/><br/>This movie is slow and uneventful until the last 30 minutes.  It starts as a mythical story about a snowman and becomes a conglomerate taking over a family-owned business plotline. While it's still early in the Merry Movie Christmas season, this may be one of your least favorites.<br/><br/>1.5 out of 5 stars.",
     "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/herfatalfamilysecret.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/herfatalfamilysecret.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/asnowychristmas.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/asnowychristmas.jpeg"
     }
   },
   {
     "option":"3",
-    "mtitle":"Gingerbread Miracle",
-    "review":"Mia moved back to her parent's home with the hopes of being there for six months.  Two years later, she is still there with her sister, Sydney.  She applies to big law firms and uses the Casillas Panadería as her office.  Legend says if a member of the Casillas family makes you a personalized gingerbread cookie, you can make a wish, and it will come true.  The owner, Luis, makes Mia a red briefcase cookie.  She makes a wish 'to guide me where I'm supposed to be' and eats it.  Then, Luis tells Mia he wants to sell the Panadería to someone who will keep it a family business.<br/><br/>Mia goes through the paperwork, and the telephone rings.  It's Alex, Luis's nephew.  Alex got permission to work remotely and decided to come into town for Christmas. Before she left for school, she put a letter in Alex's pocket professing her love.  But he never called. Alex comes home and steps right into the kitchen. Then, Alex discovers that Luis is selling the Panadería and offers his services.  Mia tells Luis that she doesn't need another lawyer looking over the paperwork, but Alex wants to approve the buyers based on their integrity.  What started as a marriage of convenience becomes a launching pad for love, only if they would talk to each other.<br/><br/>If Alex and Mia acted like adults, this movie wouldn't have a plot. With communication being essential, it's also an adult decision.  It becomes frustrating that childish behavior is the premise of a movie.  With that aside, the thought of wishing on gingerbread is charming for the holidays.  This predictable film is background noise to the holiday season.<br/><br/>2.5 out of 5 stars.",
+    "mtitle":"Strike Her Dead",
+    "review":"Jenna's mom, Sally, got a new job, and they had to move from Detroit. Jenna goes to soccer practice to try out for the team.  With someone playing forward, Coach Hanover tells Jenna to give the goalie position a try. Jenna tells the coach that she has a scholarship for forward, and playing goalie could affect it. Either Jenna is a first-string goalie or a second-string forward, and Jenna chooses the goalie and makes the team.  At school, Jenna sees Lilly's missing person flyers and asks her teammates about Lilly.  They tell Jenna that they were celebrating their state championship when Lilly left and never came back. At Alicia's pool party, Jenna sees that Maria is by herself and decides to sit next to her. Then Jenna watches Stacey and Grace go upstairs, but Grace comes down fast and hard.  Before everything comes to light, one girl is dead, one is missing, and the other is in a coma.  And Jenna is the police's number one suspect.  Jenna will need Sally to clear her name before she becomes the next victim.<br/><br/>Have you ever seen a Lifetime movie make fun of others? Well, this one does with one line.  This mystery does an intriguing job of misdirecting your attention as it follows others acting suspiciously. In the end, you will realize everything you missed from the killer's mouth was a clue. This movie is a fun watch if you need a Christmas movie break.<br/><br/>4 out of 5 stars.",
     "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/gingerbreadmiracle.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/gingerbreadmiracle.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/strikeherdead.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/strikeherdead.jpeg"
     }
   },
   {
     "option":"4",
-    "mtitle":"Secrets of an Escort",
-    "review":"Connie works as a waitress during the day.  But she doesn't make enough to care for her daughter, Christine, and cover her husband's debt. While she kept Christine in the dark about Jason, Connie is an involved and loving parent.  But waitressing doesn't pay the bills, so Connie got a night job as Veronica, the escort. She works with a bartender, Leo, who introduces her to clients.  While Leo wants to be more, Connie refuses to mix business with pleasure.  Connie sees a regular, William, and he tells her his woes.  William's wife, Liz, left him.  Now, he wants to start a life with Connie.  At work, Connie's boss, Mia, introduces her to the new girl, Alison.  They instantly recognize each other from college. As Connie shows Alison the procedures, Jason walks in and threatens her. Connie gives the details to her sister, Detective Terry, but doesn't tell Terry about her night job. When Christine doesn't come home in the morning, Connie panics.  Then she gets another text threatening Christine's life.  Someone kidnapped Christine.  Can Connie find Christine without exposing her scandalous life?<br/><br/>With so many questionable characters in Connie's life, she tries to work through the list without help but has more questions than answers.  Connie slides down a razor's edge while she tries to navigate a double life for her family and clients. Experienced Lifetimers can pick out the kidnapper halfway into the movie.  Others may need more time, but it's a deadly reveal.<br/><br/>3.5 out of 5 stars.",
+    "mtitle":"Open By Christmas",
+    "review":"Nicky comes home to celebrate the holidays, but her parents expect her to stay longer.  They are retiring this year and want to sell their house.  They have everything in motion to put the house on the market, and Derrick is their realtor. As Nicky decides what to keep or throw away, Simone comes for a visit. Nicky can't wait to see Simone go down the aisle and marry her high school sweetheart, Jeremy. Then, Nicky picks up her old college book and finds a Christmas card inside.  Inside the card, someone wrote about Nicky's kind words and how they affect them.  Then, they wrote a stanza from Romeo and Juliet to profess their love.  From the letter, Nicky and Simone know the person played in a winter game and liked literature.  Since Nicky is single, Simone convinces Nicky to find the admirer.  Nicky has no idea the wild ride ahead of her.<br/><br/>This Hallmark movie serves the tea like no other before.  With a saucy mother-in-law, handwriting expert, and an epic graduation speech, you will watch Nicky go down the rabbit hole to find her high school suitor.  With a compelling mystery, this movie proves to be as heartfelt as it is funny. Also, it's a Hallmark movie, so you know the admirer in an instant.  But enjoy Nicky's journey along the way.<br/><br/>4.5 out of 5 stars.",
     "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretsofanescort.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretsofanescort.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/openbychristmas.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/openbychristmas.jpeg"
     }
   },
   {
     "option":"5",
-    "mtitle":"Christmas Sail",
-    "review":"Liz and her daughter, Hannah, go back to Portside after Liz's father, Dennis, injures himself saving a child. When they arrive at the docks, Liz sees her old friend Luke.  Liz inquires about the 25,000 dollar grand prize banners.  Luke tells Liz that the town added the grand prize to their Festival of Lights boat decorating contest to draw in more tourists. But he tells Liz that Dennis could never enter with his boat.  Liz stops in her tracks because Dennis said he sold his boat.<br/><br/>Liz looks at it, and it is in shambles. He never got around to selling, but he will soon.  In the morning, Liz walks around town for Christmas decor for Dennis's home.  Then, Hannah sees the house in a real estate flyer.  Dennis ran late on his payments because of his wife's medical bills.  If he doesn't come up with the money, he will lose his home. Liz has a settlement check coming, but it's not enough to cover the debt.  Liz gets a crazy idea to enter the Christmas contest.  If Liz gets the boat together, she could save Dennis's home.  But Liz will need help.  Good thing she has Hannah to convince Luke to join their team.<br/><br/>This movie feels like the stereotype Hallmark movie, which it is, but it's the last 5 minutes that will make you reach for the tissues.  A loving tribute to Christmas, a family, love, and a boat sail bring a family and relationship together. With a charming child softening a Scrooge, this film gives you all the feels in a predictable format.<br/><br/>3 out of 5 stars.",
+    "mtitle":"An Ice Wine Christmas",
+    "review":"Camila, a sommelier, impresses her boss, Roberto, so much he has a promotion for her. However, Roberto wants Camila to think it over before she accepts the position.  Camila drives to Evergreen but goes to the local winery first.  At the winery, Henry makes ice wine that Camila calls Christmas in a bottle. Before Camila heads into town, Henry warns her that Main Street is different.<br/><br/>As Camila walks to her mother's restaurant, she notices a man aimlessly walking with inappropriate weather attire.  He introduces himself as Declan and wants directions for Sonny's restaurant.  Camila explains that during the Christmas season, Sonny's restaurant becomes Snowman's restaurant.  They walk to the restaurant and greet her family.  Camila's family sits down and discusses the changes.  First, Henry wants to walk away from the winery and sell it.  Next, Henry hired Declan to analyze the crop and predict the harvest.  Finally, the Christmas festival will be simple, unlike in previous years.  The next day, Declan reveals that he wants to add automation to the ice wine process.  With this, they can sell ice wine all year round.  But, Camila believes if they sell the wine all year, then it won't be unique.  Camila tells Declan to tap into his Christmas spirit, and he laughs.  His family didn't decorate the tree, because they went on trips.  So, Camila decides to give Declan an Evergreen Christmas experience to help him understand how significant ice wine is to the holiday.  This Christmas tour guide will offer Declan a connection to her heart too.<br/><br/>One thing is for sure, watching this film will make you appreciate the ice wine process.  Now this movie separates itself from others because Camila doesn't put down Declan's family tradition. Instead, she wants to add to it.  Now, Declan gets to enjoy a traditional American Christmas with Camila's family and friends.  However, you do get a technology versus instinct story that follows the Hallmark design.  Watch this film with our favorite ice wine or ice cider.<br/><br/>3.5 out of 5 stars.",
     "image":{
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/christmassail.jpeg",
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/christmassail.jpeg"
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/anicewinechristmas.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/anicewinechristmas.jpeg"
     }
   }
 ];
@@ -55,50 +55,6 @@ module.exports = madeForTV;
 */
 
 /*
-{
-  "option":"1",
-  "mtitle":"My Christmas Family Tree",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myfamilychristmastree.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myfamilychristmastree.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"A Snowy Christmas",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/asnowychristmas.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/asnowychristmas.jpeg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"Strike Her Dead",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/strikeherdead.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/strikeherdead.jpeg"
-  }
-},
-{
-  "option":"4",
-  "mtitle":"Open By Christmas",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/openbychristmas.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/openbychristmas.jpeg"
-  }
-},
-{
-  "option":"5",
-  "mtitle":"An Ice Wine Christmas",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/anicewinechristmas.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/anicewinechristmas.jpeg"
-  }
-}
+
 
 */
