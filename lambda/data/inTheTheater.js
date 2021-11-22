7ff4caa8ec31bfc8d38673fa3968c0a7ce9193f9
Tiffany Gray <KarateTD@yahoo.com>
2021-11-22 13:56:54 -0500

    add 2 theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"King Richard",
+    "review":"While Venus and Serena Williams study and practice, their father, Richard, passes out brochures to everyone at the local country club. Before Venus and Serena's birth, Richard wrote a detailed plan for their tennis career.  While he believes in his daughters, Richard can't get someone to coach the girls for free. With his brochure getting little traction, Richard packs up Venus and Serena and drives them to see Paul Cohen.  Richard talks Paul into watching the girls hit a few balls, and Paul is impressed.  But he tells Richard that he can only coach Venus for the junior tournaments.  This moment is the first time something hasn't gone according to the plan. Now, Richard has to make a lasting decision that will affect Venus and Serena's lives forever.  Heavy is the head that wears the crown.<br/><br/>King Richard tells the story of a complicated man in a battle with himself and the world for what he pre-ordained. At the moment, it's hard to understand if Richard is doing this for pride or love.  Often, Brandy calls him out on his pride privately. Although he is instrumental in the girls' success, as Richard steps back, the girls fly. Sit back and watch history in the making.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/kingrichard.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/kingrichard.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Ghostbusters: Aftermath",
+    "review":"Callie gets her kids, Trevor and Phoebe, ready to meet with an estate lawyer.  Her father died and left Callie a farmhouse in his will. Inside the home, they find books to the ceiling, numbers on the crown molding, and a future riddled with repairs.  Then Janine walks into the home and tells Callie that her father only had the decrepit house, worthless land, and debt. The next day, Callie drops Phoebe off at summer science camp, meets Phoebe's teacher, Gary Grooberson.  Then, Callie drops Trevor off at work.  At work, Trevor meets his crush, Lucky.  She invites Trevor to go to the old mine with other co-workers to have fun.  At the mine shaft, they see a glowing figure and hear noises.<br/><br/>At school, Gary puts on Kujo for his students.  Then, he steps back into his office.  Phoebe walks in, sees his seismic activity map, and asks Gary about it.  Gary explains that Summerville isn't on a fault line, but it shakes every day. When Phoebe gets home, she notices a chess piece moved. She grabs the PKE meter she found earlier and goes through the house.  Under the floor, Phoebe discovers a ghost trap.  She takes it to Gary, and he assumes it's a replica.  But when he touches it, the ghost trap starts to spark. When the trap doesn't open, Gary taps it and releases a hell on earth not seen since 1984.<br/><br/>With a Revelations 6:12 warning, this movie builds up new characters while bridging the gap to the previous ones. With so many technological advancements, the director made a smart move by not changing the look or sound of the proton fire or the slime ghost's look.  However, newer graphics are clean and not distracting from the storyline.  With this careful distinction, the director invited new fans to the franchise without alienating the cult-like following of the 1984 classic.  And stay for the mid-credit and post-credit scenes.  They have fun with the past and build towards the future.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ghostbustersafterlife.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ghostbustersafterlife.jpeg"
+    }
+  },
+  {
+    "option":"1",
     "mtitle":"Eternals",
     "review":"Professor Sersi enters her classroom to take over for her makeshift substitute and boyfriend, Dane.  In the middle of class, there is a small earthquake that worries Sersi.  Later, Dane and Sersi go out with Sersi's young companion, Sprite. Dane tries to understand her rejection when a creature comes out of the ground and attacks Sersi.  As Sersi and Sprite fight back, Ikaris enters and helps them fight the beast.  When they defeat the creature, Sersi explains to Dane her true origin.  She and her friends are Eternals from a planet called Olympia.  The Celestial Arishem sent them to Earth with superpowers to defeat the Deviants, creatures sent to destroy all human life. Sersi believed they killed all the Deviants 1500 years ago.  But she was wrong.  And Sersi and Sprite notice the Deviant has increased strength and could heal itself, like their mentor Ajak. They enter Ajak's home in South Dakota but can't find her.  But they discover her dead in the backyard because a Deviant killed her and sucked out her healing powers.<br/><br/>Soon a sphere comes out of Ajak and into the Sersi because Ajak chose Sersi to lead the Eternals.  Now she has a direct link to Arishem.  Arishem warns Sersi that the Emergence is coming.  Years ago, Arishem implanted the Celestial Tiamut on Earth. But for Tiamut to be born, the Earth and its people must die.  Sersi loves the people and Earth.  She decides to get all the Eternals together to save the planet and humans.<br/><br/>You can not be late for this film because the opening crawl sets up the history of the Eternals, like Star Wars.  And the story is not linear. This movie breaks a lot of ground by having the first deaf, Korean, and openly gay superheroes. Understand that even with better graphics, Real 3D only enhances a few scenes. Of course, as you watch, you know this film is a setup for something bigger.  And it doesn't disappoint with two end credit scenes.  While the movie entertains and connects itself to the earlier MCU, it ran too long. Also, you will get ten origin stories in one movie. Finally, the last battle feels a bit anti-climatic.  It's a little too early to say if this is your favorite in Phase 4, but it could be a contender based on the costumes, jokes, and internal conflict.  And parents, there is one fifteen-second love-making scene in this film.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -56,24 +74,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"King Richard",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/kingrichard.jpeg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/kingrichard.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Ghostbusters: Aftermath",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ghostbustersafterlife.jpeg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ghostbustersafterlife.jpeg"
-  }
-}
+
 
 */
 
