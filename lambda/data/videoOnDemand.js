4de7d749887382c63f7b99b3990a4e6ad0de98e0
Tiffany Gray <KarateTD@yahoo.com>
2021-12-27 11:12:39 -0500

    add 1

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Being the Ricardos",
+		"review":"Lucille Ball waits for her husband, Desi Arnaz, with the morning paper in her hand.  When he comes through the door, she wants an explanation.  It shows him enjoying drinks and details a wild night out.  Desi looks at the paper and tells her it's an old picture of an event she with him attended months ago.  Lucy remembers and kisses Desi.  As they kiss, a news report comes over the radio.  A well-known T.V. star pledges her allegiance to the communist party.  Lucy is the most-popular woman or man on television.  They know the radio host is talking about Lucy.<br/><br/>Monday, Lucy and Desi go to the executives to convince them of the truth.  Lucy tells them that 20 years ago, she lived with her grandfather, who fought for the little guy.  She checked the communist box to please him.  Desi and Lucy leave the office and head to the table-read for episode 204 of I Love Lucy.  They believe the show is ok, but this will be the most trying week of their marriage, T.V. show, staff, and cast.<br/><br/>It might shock people today, but I Love Lucy was a groundbreaking and provocative show for its time.  Lucy played a dopey woman on T.V., but she was a comedic genius battling tooth and nail for her art.  However, this movie doesn't make Lucy look like an angel.  Her strong demeanor came at the cost of her writing staff and her co-stars, Vivian and Bill.  You don't need to know much about Lucy, Desi, or the show to enjoy this movie.  But you will want to watch the show afterward.<br/><br/>4.5 out of 5 stars",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/beingthericardos.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/beingthericardos.jpeg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"Psych 3: This is Gus",
 		"review":"With his baby on the way, Gus wants to do the honorable thing and marry Selene with his family and friends before the birth.  However, Selene won't give Gus information to divorce her estranged husband. The next day, Shawn goes to Jules for help doing a police search on Selene, but Jules refuses. Then, Shawn goes to Chief Karen Vick for help.  Karen doesn't help but knowingly leaves her computer unlocked with Shawn in the room.  Shawn looks for Selene in the database and finds nothing. With Gus's track record of dating killers, Shawn wants to investigate to protect his friend.  Gus wants to prove Shawn wrong. Will the real Selene please stand up?<br/><br/>Wait for it! This movie feeds its Psychos yet again while allowing the characters to grow.  Gus is the perfect groomzilla, with Shawn being the worst man and best friend at the same time.  Chief Lassie questions his ability to be a cop after his stroke in Psych 2.  And Karen battles with work-life balance after making it to the top.  It's great to see these characters stay grounded while their cases remain crazy. Yes, it helps to view the other movies and TV show because they make jokes from the source material.  Also, keep watching until the post-credits because fans get a glimpse of Psych 4.<br/><br/>5 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Red Notice",
 		"review":"Detective John Hartley is a criminal profiler with the FBI.  He is on the hunt for Nolan Booth, the most wanted thief in the world.  Interpol got a tip that Booth wants to steal Cleopatra's second egg. When John proves the display egg is fake, he spots Booth and chases him.  Before John can get a hand on Booth, Booth slips away to his safe house.  Exactly where John is waiting for him. John believes his work is complete until someone says the egg is fake.  John thinks it was Bishop who swapped it, but Interpol gets another tip and arrests John.  They lock John up in the same cell as Booth.  Before the fight, the captain pulls Booth and John out of their cell to talk.  The person sitting across from them is the mythical Bishop.  She has the first egg and expects to get the 2nd in 48 hours.  She wants to know the location of the 3rd egg and questions Booth.  He says he doesn't know, but Bishop knows Booth is a liar. So Bishop leaves.  John tells Booth that if Booth helps him catch Bishop, then Booth will be the #1 thief in the world and John can clear his name.  These strange bedfellows go on a worldwide mission.  But in a world of cons, who can you trust to act like a pro.<br/><br/>Let's be honest.  Within the first 15 minutes of Booth and John on the screen, you know there is more than meets the eye. With that aside, the comedy and comedic timing is gold. You will be happy to watch, but once is enough.  With the success of this movie and the ending, be ready for Red Notice 2.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Home Sweet Home Alone",
 		"review":"As potential buyers go through their home, Pam and Jeff answer their questions.  While Pam is helpful, Jeff sabotages.  Jeff lost his job months ago and hasn't been able to find work.  They can no longer afford their home. When Jeff goes upstairs, he sees Maxwell come out of the bathroom.  Maxwell's mom, Carol, pretended to be interested in the home so Maxwell could use the restroom.  Maxwell sees Jeff pull a box of dolls from the closet and annoyingly asks questions. One doll is an ugly little boy with an upside-down head.  Carol, looking for Maxwell, comments that the dolls could be worth a lot of money.  After everyone leaves, Jeff looks up the dolls on eBay.  And the little boy doll is a rare mistake worth $200,000.  Jeff runs to the box, but the doll is missing.  He believes Maxwell stole it. At Carol's home, there is a lot of commotion because her entire family is there.<br/><br/>However, in the holiday melee, the airline split Carol's family into two flights. Upset with the crowded house, Maxwell escapes to the garage, gets in a car, and falls asleep watching TV.  In the morning, Jeff comes to Carol's house.  But the family says Carol and Maxwell left.  When the family leaves, Jeff considers breaking in but changes his mind.  Until he looks inside and sees Maxwell's coat with a noticeable bulge; the doll is in Maxwell's coat pocket. When Jeff tells Pam, she believes he has lost his mind.  But she changes her tune when she thinks about all the Christmases her family spent in that house.  Pam and Jeff go to an empty home to steal back the doll, but they discover someone is still inside.  It's Maxwell. He overhears Jeff and Pam talking about stealing the ugly little boy and selling him to an old lady.  He assumes they mean him.  With the boy in the house, Pam and Jeff need to devise another plan to get the doll, but Maxwell will be ready for them.<br/><br/>The sixth installment to the Home Alone series changes the script.  The kid is ultimately the bad guy in this story.  The change in dynamic makes you sympathize each time Pam and Jeff get hurt.  They are desperate people trying to save their homes.  Something a lot of people understand. While it's not the classic, you can use this film to get your kids to watch the first two.<br/><br/>3 out of 5 stars.",
 		"image":{
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"The Harder They Fall",
 		"review":"Years ago, two men walked into Nat Love's home and killed his mother and father.  The man with the scorpion tattoo held Nat down while the other man took out a knife and carved a cross on Nat's forehead.  Today, Nat is a grown man, and he killed the man with the scorpion tattoo.  With the carver, Rufus Buck, in prison for life, Nat is ready to live the rest of his days with Mary. Nat crosses the bouncer, Cuffee, the door and heads to Mary.  As they talk, a fight breaks out, and Cuffee breaks it up.  But U.S. Marshall Bass Revees arrest Nat for the standoff.  Bass tells Nat that Rufus got a pardon with the help of Trudy and Cherokee Bill. With the man who made him an orphan free, Nat finishes his revenge with a trail of blood, bodies, and money behind him.<br/><br/>While the story is fiction, the people are not.  As this bloody western tale of revenge plays, don't be surprised if you look them up online and see their torrid past.  Using made-up stories with real characters isn't new.  But this movie delivers. However, it's the mix of modern and western slang that throws the movie off.  With so much attention to detail for the costumes and scenery, the dialect, not the dialog, feels neglected. Even if the story isn't true, writers need to have respect for the period.<br/><br/>4.5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thehardertheyfall.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thehardertheyfall.jpeg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"Passing",
-		"review":"After shopping, Irene takes a cab to a restaurant to escape the sweltering heat.  Inside, Clare spots Irene, but Irene doesn't recognize Clare. They have one thing in common; they are black women who 'pass' for white.  While Irene does it around predominately white towns, Clare does it every day.  Her white husband, John, doesn't know the truth.  Clare invites Irene up to her hotel room, and the two women catch up. When John comes to the room, Irene sits on pins and needles. Before John can say anything else, Irene makes a hasty exit. Clare writes Irene a letter stating that she hopes they can spend time together in Irene's home in Harlem.  Irene ignores the letter and continues with her life.  As Irene gives instructions to the maid, she gets an unannounced visitor.  It's Clare. As Clare spins more time with Irene and her family, Irene goes into a downward spiral of self-reflection, bitterness, and jealousy.  Can these women survive when Irene's reality clashes with Clare's truth?<br/><br/>Be prepared because Passing is a dialogue-heavy movie.  If you favor something fast-paced and funny, you will be disappointed.  When Irene speaks of Clare, backhanded comment or a polite disregard accompanies it. At first, you think Clare's constant passing repulses Irene.  However, you see the women both envy each other.  Based on Passing by Nella Larsen, this film has an ending that will stun you.<br/><br/>3.5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/passing.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/passing.jpeg"
 		}
 	}
 ];
