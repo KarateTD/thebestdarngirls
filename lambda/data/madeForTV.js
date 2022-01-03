253ea8408af96f2e3f36c48562b4b21189c59eae
Tiffany Gray <KarateTD@yahoo.com>
2022-01-02 23:08:43 -0500

    add 2 tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Deadly Infidelity",
+    "review":"After three months in a coma, Grace wakes up with her husband, Paul, and mother by her side.  The doctor believes Grace made a remarkable recovery until Paul mentions their daughter, Taylor, canceled her date to see Grace.  Grace laughs because her 12-year-old can't be dating.  Then Paul tells Grace that Taylor is 17.  Grace lost five years of her life.  Grace comes home and tries to get reacquainted with her life.  Paul tells Grace that her sister, Lisa, has taken care of things since Grace was in a coma.  And they are all happy to have Grace back.  Days later, a man comes into Grace's restaurant and tries to kiss her.  Grace pulls out a knife.  He tells her they've had an affair for over nine months.  His name is Kyle, and he left his wife to be with Grace.  He tells her to check the secret messaging app on her phone for proof.  Grace goes on the app and sees messages for months, and Lisa confirms the affair.  Grace tries to break it off, but Kyle swears never to let Grace go.  Can Grace keep Kyle away from her marriage, or will her newfound world implode?<br/><br/>Wow, what a ride.  When you think you know the plot, it provides another twist.  No matter what you think, it's right and wrong.  With a wife, a sister, and a lover, Grace's picture-perfect life spins out of control quickly, and you will love every minute of it.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlyinfidelity.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlyinfidelity.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"A Deadly Grudge",
+    "review":"Lindsey's dad, Jim, goes over the menu for her and Gavin's wedding reception, but she reminds him to focus on other things.  As they laugh about the future, Lindsey's mom, Annie, goes on a run.  Then Jim gets a call from the police.  Annie was a victim of a hit and run and didn't survive.  At the funeral, Lindsey sees a woman in the distance but ignores her.  Later, the same woman shows up at Annie's wake.  Jim tells Lindsey the woman is Sarah, her half-sister.  Before Jim met Annie, he met Sarah's mom, Franny, and got her pregnant.  Franny kept Jim a secret until she died.  Now, Jim and Sarah want to reconnect.  Lindsey feels confused, betrayed, and suspicious.  And she has every right to be.  Sarah wants Lindsey's fiancé, business, and dad.  Can Lindsey convince her family of Sarah's ill intentions?<br/><br/>'Only on Lifetime' is a sentiment you will repeat while watching this movie.  Sarah has the family wrapped around her finger with an allergy, two hit-and-runs, and a family business to manipulate.  You don't watch this to figure out who.  Instead, you want to know how Sarah did it and when Lindsey will uncover it.  Both questions will have an answer.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/adeadlygrudge.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/adeadlygrudge.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Writing Around the Christmas Tree",
     "review":"As Mikaela shares her wisdom in a writing workshop, one participant, Levi, asks intrusive questions about her love life and mother.  Mikaela quickly wraps up the session and plans her next steps with her agent, Gigi, and assistant, Keifer.  But Levi approaches Mikaela wanting more information.  He mentions her mother's writing retreat.<br/><br/>Mikaela tells Keifer to cancel all her holiday plans.  She must finish the second half of her book, or her publishers will back out.  She calls to cancel dinner with her dad and mentions Levi's idea.  He thinks reviving the retreat is magnificent and offers to cook as he did in the past.  Mikaela tells Kiefer to collect résumés for the retreat, and she will pick four applicants.<br/><br/>Levi gets a phone call from Tracy.  She commissioned him to write a biography about Mikaela's mother.  Tracy got wind of the retreat and now believes it would be a good opening for Levi to get closer to Mikaela.  Levi says he applied for the writing retreat but hasn't heard back.  Filled with the holiday spirit, Mikaela accepts him on a trial basis.  Her change of heart will bring romance and make her creative juices flow.  When she finds out about the biography, will it end their storybook romance before it starts?<br/><br/>The first interaction between Levi and Mikaela will pull you into the story.  He interrogates her like a hard-nosed reporter, but you aren't sure why because he admits he never read her books.  As the plot weaves in Mikaela's book, they foreshadow her romance with Levi.  The viewer gets a subtle message about why Levi and Mikaela belong together.  Watch this movie with your chestnut soup and Turkey Po'Boy sandwich in hand.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Hot Chocolate Holiday",
     "review":"Colette tolls night and day to recreate her grandmother's hot chocolate recipe into a powder mix.  Colette needs to get the recipe right and read the contract so grocery chains can sell her mix.  At the Christmas Kick-off, Colette meets Marcus.  He's renting the store next to hers and opening a bakery.<br/><br/>Colette orders a brownie with hot chocolate from Marcus's shop the next day.  She takes one sip and recognizes it as her recipe.  Colette thinks he stole it and wants to gather proof.  When her investigation doesn't get results, she tells him to stop selling it.  But he won't because it's his recipe.  Working on her grocery store deal, Frank heard about her competitor.  He gives her a warning.  If Marcus claims it's his recipe, it can kill her contract.  As she devises a plan, a fire destroys everything but the truth, the past, and their love.<br/><br/>While the plot follows the T.V. romance format, it solves the mystery of the recipe sooner than you would expect.  Instead of dragging it on for two hours, viewers learn the truth in the first hour.  Rebuilding their connection is the focus of the second half.  It gives the main characters more time to connect and heal.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Rebuilding a Dream Christmas",
     "review":"Abbey plans on a solo Christmas working on her new assignment, but Karen from the historical society calls.  A tree fell and broke the window at her house.  She needs to get a permit to fix it and present it in person.  Abbey drives to Weston, her hometown, and sees the house she inherited from her grandmother needs a lot of work. At the local cafe, Abbey runs into her best friend, Ellen. Ellen is close to achieving her dream of owning a Christmas store.  But she doesn't understand the rental agreement.  Ellen begs Abbey to look at it. But between work and finding a contractor, Abbey won't have time.  Ellen calls her brother, Josh, and convinces him to look at the window.  When they meet, Abbey explains she is in town for the day. But something will keep her in town: Josh's son, Noah.  Noah, asked Santa for a mom for Christmas.  No matter how hard Abbey tries, a storm, a dead battery, and a reindeer will keep her in Weston.  Is Noah's wish stronger than Karen's law?<br/><br/>When Ellen calls Josh, you know the plot from beginning to end.  The only aspects you don't consider are Noah and Santa.  But if you take them out of the movie, Abbey only had bad luck. This film adds nothing to the holiday season but a great charity: Trees For Troops.<br/><br/>1.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rebuildingadreamchristmas.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rebuildingadreamchristmas.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"it Takes a Christmas Village",
-    "review":"Mayor Alex Foster stands at the edge of a traffic jam and fixes the detour sign.  As cars turn around, Frank asks Alex how long the construction work will take. She quotes the contractor and says soon.  Frank reminds Alex that the construction blocks Main Street, and his business suffers every day. Alex's assistant, Gina, tells Alex the contractor is on vacation in Jamaica, and hiring another contractor is a breach. Alex sits with her dad, Art, the former mayor, to pick his brain, and he gives her a great idea: a Christmas Market.  All the Main Street stores will have a booth and recoup any losses during construction. Alex and Gina need to find a spot large enough with ample parking.  Then, Alex spots Hawkins Mill. But Gina reminds Alex that the Foster and Hawkins families have been at war for years since the patriarch closed it. When Alex mentions all the family members left, Gina states their son, Darcy, still lives on the property.<br/><br/>Alex marches to Darcy's home and sees his woodworking in the yard.  She asks for permission to use the old mill, and he gives an emphatic no. Darcy's grandmother, Mary, invites Alex inside to talk.  Mary mentions moving to her dream apartment someday before Darcy cuts her off. She and Gina look into the apartments Mary mentioned.  They find an application for a building permit. So, Alex offers Darcy a deal.  She will present his plan to the board if Darcy lets them use the mill for the Christmas Market. Will Alex and Darcy's sparks derail their deal?<br/><br/>This family at war story brings out the romance or bitterness of everyone involved.  Alex's ex-boyfriend, Brent, says their romance clouds her judgment. But Gina and Mary are in their corner. This movie follows the typical romance script, but Alex's passioned plea saves the film.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ittakesachristmasvillagge.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ittakesachristmasvillagge.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Song and Story: Amazing Grace",
-    "review":"Iris walks into the alley and requests Tanner create a picture for her. Tanner charges $25 with a deposit.  Iris returns to her boutique and tells her friend, Beverly, about her troubles. Beverly tells her that developers want to invest in stores in the area.  Iris tells Beverly to give them her information.<br/><br/>Days later, Iris finds Tanner for the completed drawing.  Iris sees Tanner's paintings and asks Tanner to turn her art into an oil painting.  Iris agrees, but it will take more time.  Tanner walks away, and Baker steps out of the shadows.  Tanner owes Baker money. If she runs credit card and phony money scams for him around town, then he will let her off the hook.  She tells him she will get the money and walks to a store. Iris walks from the back, and Tanner uncovers the store belongs to Iris.  Beverly calls the police, and the officers take Tanner away.  Iris sees the fear in Tanner's eyes and convinces D.A. to drop the charges if Tanner works in her store. She doesn't understand why Iris would help her.  Iris says it's what her mother would have done. Baker wants Tanner to run the scam in Iris's store.  These women will work on their grace while healing each other.  But can that grace survive their linked past?<br/><br/>This film is a caring movie about understanding and redemption that feels forced sometimes.  Both women are in the throws of losing everything important to them. Iris can see that Tanner has it rough, but Tanner has no idea what Iris goes through.  They need each other to grow, and you watch their development in awe.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/songandstoryamazingrace.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/songandstoryamazingrace.jpeg"
     }
   }
 ];
