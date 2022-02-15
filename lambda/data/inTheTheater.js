7a87b68de525f0d16cce8c1b60ac9bbaf5fd21b2
Tiffany Gray <KarateTD@yahoo.com>
2022-02-15 03:04:45 -0500

    add 3 tv movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Death on the Nile",
+    "review":"Hercule Poirot sits at a jazz club and watches a love story develop.  Jacqueline introduces her fiancé, Simon, to her friend, Linnet.  To celebrate, Jacqueline asks Simon to dance with Linnet.  Later, Hercule tries to enjoy dessert in front of a sphinx.  Then he sees a man flying a kite from the middle of the pyramid.  Hercule runs to the man to admonish him but stops when Hercule sees it's Bouc.  Bouc takes Hercule to meet his mother, Euphemia, and they invite Hercule to a wedding party.<br/><br/>At the party, Hercule sees Simon with his new bride, Linnet.  Stunning Linnet wears a 2 million dollar Tiffany necklace and a smile for her guests.  But a hush falls over the crowd when Jacqueline walks into the room.  During each leg of their trip, she appears unwanted to spoil their fun.  At the market, Hercule spots Jacqueline and attempts to plead to her better nature.  Jacqueline tells Hercule that she will never give up, and she has the perfect thing to fix a broken heart: a 22.  Hercule tells Simon and Linnet that they should end their trip, go home, and stay on the property until Jacqueline calms down.  But Simon refuses to let Jacqueline ruin their vacation, and he decides to gain distance by renting a ship, The Karnak, to travel the Nile.  And they invite Hercule to come aboard.<br/><br/>They travel to the rock temples at Abu Simbel, and Simon grabs Linnet for some alone time.  Then a rock comes close to killing the couple, but Hercule doesn't think it is an accident caused by the sand storm.  They board the boat in fear and find a shock.  Jacqueline awaits their return.  After night falls, Linnet takes a pill and resigns to her room.  Simon grows tired of Jacqueline's actions and makes his feelings known.  Jacqueline grabs her gun and shoots Simon in the leg.  Others pull them apart and Linnet's school friend, Rosalie, kicks the gun under the couch.  Linnet's ex-fiancé, Dr. Linus, tends to Simon's leg, and Linnet's godmother's nurse, Mrs. Bowers, calms Jacqueline down with morphine.<br/><br/>In the morning, Linnet's maid, Louise, finds Linnet dead with a gunshot to the head and her necklace missing.  At first, everyone blames Jacqueline, but Mrs. Bowers says she never left Jacqueline's side.  Hercule swears to find the killer before they hit land.<br/><br/>Based on Death on the Nile: A Hercule Poirot Mystery by Agatha Christie, this movie doesn't pick up exactly where the first one left off, but it does bring the films together.  You must watch from the beginning because the writer explains Hercule's famous mustache and wartorn start.  Within 30 minutes, the plot introduces all the characters and their connection to Linnet.  This chapter in the Hercule series is better than the first and will make you want more.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deathonthenile.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deathonthenile.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Marry Me",
+    "review":"Popstar Kat Valdez will marry singer Bastian in front of 20 million fans, performing their hit song Marry Me.  Kat's manager, Colin, tries to keep Kat's mind focused on the show and her endorsements.  And her assistants, Tyra and Melissa, keep Kat looking gorgeous and on film for social media.<br/><br/>Charlie is a single dad to Lou and works as a math teacher at Lou's school.  In between classes, Charlie's friend, Parker, finds him watching over Lou in the hallway.  Parker has a way to make him the fun dad.  Parker broke up with her girlfriend and obtained three tickets to Kat's show.<br/><br/>At the concert, Parker holds a sign that says 'Marry Me,' and Lou is having the time of her life.  Backstage, Kat changes costumes when she notices everyone looking at their phones.  She grabs Colin's phone and sees an article on Page Six.  It shows Bastian cheating with Tyra.  As the gravity of the betrayal hits Kat, her platform lifts her to center stage.  Parker gives her a sign to Charlie and grabs her phone to record the wedding.  She points to Charlie's sign and says yes.  They pull Charlie on stage, and the two get married.<br/><br/>Later, Colin wants Charlie to sign an agreement and annul the marriage.  But Kat wants to stay married so she won't be a punchline.  So he agrees to go with Kat's wishes.  But how long can they keep up the act before it becomes real? And will it last after a Grammy nomination with a surprise performance with Bastian?<br/><br/>Based on Marry Me by Bobby Crosby, this film is about Jennifer Lopez being herself.  That's why this movie feels honest and authentic.  Also, Owen Wilson's self-deprecating yet charming sense of humor makes this love story believable.  This storyline is nothing new to Hallmark fans because it follows the network's format.  The only difference is it's a visual album for a catchy soundtrack.  If you are a fan of J Lo or Owen, see it at the matinee price. Otherwise, catch it on Peacock.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/marryme.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/marryme.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Blacklight",
+    "review":"Travis completed another extraction for FBI Director Gabe and got the undercover agent out safely.  When Travis meets with this daughter, Amanda, at his granddaughter's birthday party, Travis promises to quit his dangerous job.  Amanda is not comfortable with this.  She believes his dangerous job impacts his OCD, and it's having a lasting impact on Natalie's mental state.<br/><br/>Mira is a reporter that found a story about social activist and political hopeful Sofia Flores.  A few weeks ago, Sofia was a hit-and-run victim.  But Mira believes there is more to the tale.  The police say they have a suspect but won't release the name.  However, Mira discovered that the uber driver gave a fake name, and the uber's license plates were fake.  One day, Dusty calls Mira with evidence to prove her suspicions are correct.  But four police officers stop his call.  They take him down and to the precinct.  Travis bails Dusty out but tells the police to keep it off the books.  Dusty escapes and runs to Mira when Travis is distracted by his duties as a grandfather.  Mira comes outside to see Travis chasing Dusty.<br/><br/>Travis reports Dusty's escape and contact with Mira to Gabe.  Travis tells Mira to stay away and politely threatens her, but Mira can't forget the story.  Soon, Dusty contacts Mira again, and they meet up.  After a chase, two agents kill Dusty in front of Travis and Mira and walk away.  Travis knows something is wrong and confronts Gabe.  When Gabe doesn't give him information, Travis quits.  However, Gabe won't let him and hides Travis's family.  Travis searches high and low for his family but comes to an abrupt halt when he gets a call from Mira.  She believes someone murdered her boss and sullied Dusty's name.  She needs Travis to help her find the truth behind Dusty's last operation.  And it all revolves around Sofia Flores.<br/><br/>This movie is exciting, but the overall conclusion falls flat.  You assume a twist about Mira's father, Amanda's estranged husband, or Natalie's stranger.  This plot throws out a lot of open-ended information but offers no conclusion for them.  It's an action movie, masking as a political drama, and should have found a home on Paramount+.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blacklight.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blacklight.jpeg"
+    }
+  },
+  {
+    "option":"1",
     "mtitle":"Moonfall",
     "review":"In 2011, astronauts Brian, Jocinda, and Marcus went on a mission to fix a satellite.  Everything was normal until Brian and Marcus saw a black swarm in space. While Brian and Jocinda were safe, nobody heard from Marcus again.  On earth, Brian tells them about the swarm.  But no one listens.  NASA concludes that Brian's error caused Marcus's death, and they fire Brian.  Today, Brian is a disgrace, and Jocinda works for NASA.<br/><br/>At the University of Californa Irvine, K.C. sneaks into a professor's office to obtain orbital data.  K.C. is a conspiracy theorist who believes the moon is a megastructure built by aliens.  And his obtained data prove the moon is out of orbit and in a crash course with the earth.  K.C. goes to the museum to make Brian pay attention to his warnings.  However, K.C.'s information falls on deaf ears, and Brian has the police escort K.C. out.  K.C. throws his findings at Brian and screams at Brian to read them.  When Brian sees the news about the moon, he finds K.C.'s podcast.  He tracks K.C. down for more information.<br/><br/>Back at NASA, the acting chief left Jocinda in charge.  She is determined to save the world and sends a team to investigate the swarm.  As the home base watches, they see it kill the flight crew.  Jocinda learns that energy attracts it.  Jocinda needs someone who can fly it without power.  And Brian is the only name that comes to mind.<br/><br/>This sci-fi extravaganza is similar to Independence Day and Geostorm.  It is fun to watch all these characters come together for the greater good.  However, this film adds nothing to the genre while hinting at a sequel.  Don't be dismayed.  Although the plot is redundant, the overall script is funny, keeping with the times, and engaging.  This film is an entertaining matinee movie for people older than 10.<br/><br/>3.5 out of 5 stars",
     "image":{
@@ -56,6 +83,31 @@ module.exports = inTheTheaters;
 
 /*
 
+{
+  "option":"1",
+  "mtitle":"Death on the Nile",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deathonthenile.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deathonthenile.jpeg"
+  }
+},
+{
+  "option":"2",
+  "mtitle":"Marry Me",
+  "review":"<br/><br/>3 out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/marryme.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/marryme.jpeg"
+  },
+  {
+  "option":"3",
+  "mtitle":"Blacklight",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blacklight.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blacklight.jpeg"
+  }
 
 
 */
