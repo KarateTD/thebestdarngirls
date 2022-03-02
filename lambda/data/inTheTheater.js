fbebf04c3a8b36624da0745bfce0c7b8c05de84e
Tiffany Gray <KarateTD@yahoo.com>
2022-03-02 02:49:48 -0500

    add batman

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"The Batman",
+    "review":"It's Halloween.  And while most of Gotham celebrate candy and debauchery, Bruce Wayne uses his mask for vengeance against the city's underbelly.  Batman has looked over the citizens with Lieutenant Gordon and his signal for two years.  Someone killed mayor Don Mitchell, wrapped his head in tape, cut off his thumb, and wrote 'No More Lies' on him.  The killer, The Riddler, left a card for 'The Batman' with a cipher inside.  The Riddler vows to kill more people and unmask the corruption in Gotham.  Batman uncovers pictures of the mayor with a young woman, that is not his wife, coming out of the Iceberg Lounge.<br/><br/>During questioning, a waitress walks into the room.  She freezes when she sees the photos.  The waitress goes to her apartment that she shares with the young woman, Annika, from the images.  The waitress promises to help Annika getaway.  After changing clothes, the waitress gingerly leaves the apartment through a window.  She goes to the mayor's home, an active crime scene, finds a hidden safe and cracks it.  She pulls out Annika's stolen passport when Batman grabs her.  The waitress is Selina Kyle, Catwoman.  Batman worries about the cat burglar and follows Selina back to her apartment.  Annika and their phones are gone.  Selina tells Batman that she will find answers in the 44 Below Club.  It's a private club within the Iceberg Lounge where the crooked politicians and cops hang out.  Batman knows this will be a target-rich environment for the Riddler, so he devises a plan for him and Selina to work together.  In 7 days, Batman will learn that his mask can inspire hope, create resentment, and reveal dark aspirations.  And all roads lead to Arkham.<br/><br/>This film is one of the best Batman movies since The Dark Knight.  It provides action, mystery, a few jump-scares, and intensely emotional moments.  This movie assumes you know Batman's backstory.  While the story and acting were impressive, the running time made the movie drag.  Don't be surprised if you wonder 'is this the end' several times.  The end credits will tempt you to stay, but there are zero spoilers.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebatman.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebatman.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Uncharted",
     "review":"Fifteen years ago, Nate and his brother, Sam, tried to steal Magellan's map.  It was the first map of the world with a secret to where Magellan and his crew hid billions in gold.  A security guard catches them and turns them over to the nuns at the orphanage.  This is Sam's third strike, and he must leave the orphanage to meet with a judge.  Sam goes upstairs to get his things, and Nate follows.  Nate sees Sam about to escape, and Nate begs him not to leave him.  Sam gives Nate a ring, a lighter, a letter, and a promise to return.<br/><br/>Today, Nate is a bartender with a lot of historical knowledge and sticky fingers.  He lifts a bracelet from a customer when a man, Sully, gives him praise.  Nate refuses to work with Sully, and Sully leaves.  Nate checks his pockets, and Sully's business card is in the bracelet's place.  Nate breaks into Sully's home and finds Sully waiting for him.  Sully notices that Nate stopped at the map on the wall before looking for the bracelet.  Then, Sully tells Nate the map is authentic, like the treasure.  To get it, they need two crosses because they're the key.  Sully already has one, but the other will be in an auction.  Nate doesn't want to be a part of the plan, but Sully mentions Sam.  Sully thinks that finding the treasure will lead to Sam.  Nate will quickly learn that there is no honor among thieves.<br/><br/>Based on Uncharted for Playstation, this movie is how game adaptations should be from this point forward.  It was funny, captivating, and suspenseful.  This film was Indiana Jones and National Treasure with a bit of Goonies.  It's no surprise that the mid-credits scene gives sequel hints.  Some action sequences feel like watching a high-resolution video game.  The film never gets slow or bogged down with unnecessary love stories.  Enjoy this adaption on the big screen for maximum viewing pleasure.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Dog",
     "review":"Jackson Briggs works in a gas station's sandwich shop while waiting to hear about his diplomat security application.  Jackson suffered a traumatic brain injury that may result in headaches, blurred vision, and seizures.  Although he got a clean bill of health, Jackson can't connect with Commander Jones on the phone to confirm his work detail.  He gets a call from Jones with unfortunate information that his fellow ranger, Riley, died.  Jackson goes to a local pub to celebrate Riley's life, drink, and talk to someone in charge.  Jones accuses Jackson of buying his medical clearance and drives away.<br/><br/>In the morning, Jones finds Jackson, grabs Jackson by the ankles, and pulls Jackson out of his truck.  Jones tells Jackson to come to Fort Lewis for a mission.  If Jackson completes it, Jones will approve Jackson's application.  Riley's family wants his K-9, LuLu, to attend his funeral.  Jackson remembers LuLu as a badass that loved Grey's Anatomy.  He steps into the cage to pet a muzzled LuLu.  At first, LuLu is calm.  Then, Jackson touches her ears, and she snaps.  This behavior makes LuLu a poor candidate for adoption.  So after the funeral, the military will put LuLu down.  Jackson gets LuLu into the car and starts the 1500 mile journey to Riley's funeral and LuLu's death.<br/><br/>The movie opens with a veteran pushing to get back into duty.  LuLu is a mirror for Jackson's inner turmoil.  She has extreme anxiety, which causes her to pant heavily.  As Jackson accepts LuLu's trials and tribulations, he forgives his own.  The director wonderfully shot moments between Jackson and LuLu to convey all possible emotions.  Enjoy this movie about a veteran and a handler.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Death on the Nile",
     "review":"Hercule Poirot sits at a jazz club and watches a love story develop.  Jacqueline introduces her fiancé, Simon, to her friend, Linnet.  To celebrate, Jacqueline asks Simon to dance with Linnet.  Later, Hercule tries to enjoy dessert in front of a sphinx.  Then he sees a man flying a kite from the middle of the pyramid.  Hercule runs to the man to admonish him but stops when Hercule sees it's Bouc.  Bouc takes Hercule to meet his mother, Euphemia, and they invite Hercule to a wedding party.<br/><br/>At the party, Hercule sees Simon with his new bride, Linnet.  Stunning Linnet wears a 2 million dollar Tiffany necklace and a smile for her guests.  But a hush falls over the crowd when Jacqueline walks into the room.  During each leg of their trip, she appears unwanted to spoil their fun.  At the market, Hercule spots Jacqueline and attempts to plead to her better nature.  Jacqueline tells Hercule that she will never give up, and she has the perfect thing to fix a broken heart: a 22.  Hercule tells Simon and Linnet that they should end their trip, go home, and stay on the property until Jacqueline calms down.  But Simon refuses to let Jacqueline ruin their vacation, and he decides to gain distance by renting a ship, The Karnak, to travel the Nile.  And they invite Hercule to come aboard.<br/><br/>They travel to the rock temples at Abu Simbel, and Simon grabs Linnet for some alone time.  Then a rock comes close to killing the couple, but Hercule doesn't think it is an accident caused by the sand storm.  They board the boat in fear and find a shock.  Jacqueline awaits their return.  After night falls, Linnet takes a pill and resigns to her room.  Simon grows tired of Jacqueline's actions and makes his feelings known.  Jacqueline grabs her gun and shoots Simon in the leg.  Others pull them apart and Linnet's school friend, Rosalie, kicks the gun under the couch.  Linnet's ex-fiancé, Dr. Linus, tends to Simon's leg, and Linnet's godmother's nurse, Mrs. Bowers, calms Jacqueline down with morphine.<br/><br/>In the morning, Linnet's maid, Louise, finds Linnet dead with a gunshot to the head and her necklace missing.  At first, everyone blames Jacqueline, but Mrs. Bowers says she never left Jacqueline's side.  Hercule swears to find the killer before they hit land.<br/><br/>Based on Death on the Nile: A Hercule Poirot Mystery by Agatha Christie, this movie doesn't pick up exactly where the first one left off, but it does bring the films together.  You must watch from the beginning because the writer explains Hercule's famous mustache and wartorn start.  Within 30 minutes, the plot introduces all the characters and their connection to Linnet.  This chapter in the Hercule series is better than the first and will make you want more.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Marry Me",
     "review":"Popstar Kat Valdez will marry singer Bastian in front of 20 million fans, performing their hit song Marry Me.  Kat's manager, Colin, tries to keep Kat's mind focused on the show and her endorsements.  And her assistants, Tyra and Melissa, keep Kat looking gorgeous and on film for social media.<br/><br/>Charlie is a single dad to Lou and works as a math teacher at Lou's school.  In between classes, Charlie's friend, Parker, finds him watching over Lou in the hallway.  Parker has a way to make him the fun dad.  Parker broke up with her girlfriend and obtained three tickets to Kat's show.<br/><br/>At the concert, Parker holds a sign that says 'Marry Me,' and Lou is having the time of her life.  Backstage, Kat changes costumes when she notices everyone looking at their phones.  She grabs Colin's phone and sees an article on Page Six.  It shows Bastian cheating with Tyra.  As the gravity of the betrayal hits Kat, her platform lifts her to center stage.  Parker gives her a sign to Charlie and grabs her phone to record the wedding.  She points to Charlie's sign and says yes.  They pull Charlie on stage, and the two get married.<br/><br/>Later, Colin wants Charlie to sign an agreement and annul the marriage.  But Kat wants to stay married so she won't be a punchline.  So he agrees to go with Kat's wishes.  But how long can they keep up the act before it becomes real? And will it last after a Grammy nomination with a surprise performance with Bastian?<br/><br/>Based on Marry Me by Bobby Crosby, this film is about Jennifer Lopez being herself.  That's why this movie feels honest and authentic.  Also, Owen Wilson's self-deprecating yet charming sense of humor makes this love story believable.  This storyline is nothing new to Hallmark fans because it follows the network's format.  The only difference is it's a visual album for a catchy soundtrack.  If you are a fan of J Lo or Owen, see it at the matinee price. Otherwise, catch it on Peacock.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/marryme.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/marryme.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Blacklight",
-    "review":"Travis completed another extraction for FBI Director Gabe and got the undercover agent out safely.  When Travis meets with this daughter, Amanda, at his granddaughter's birthday party, Travis promises to quit his dangerous job.  Amanda is not comfortable with this.  She believes his dangerous job impacts his OCD, and it's having a lasting impact on Natalie's mental state.<br/><br/>Mira is a reporter that found a story about social activist and political hopeful Sofia Flores.  A few weeks ago, Sofia was a hit-and-run victim.  But Mira believes there is more to the tale.  The police say they have a suspect but won't release the name.  However, Mira discovered that the uber driver gave a fake name, and the uber's license plates were fake.  One day, Dusty calls Mira with evidence to prove her suspicions are correct.  But four police officers stop his call.  They take him down and to the precinct.  Travis bails Dusty out but tells the police to keep it off the books.  Dusty escapes and runs to Mira when Travis is distracted by his duties as a grandfather.  Mira comes outside to see Travis chasing Dusty.<br/><br/>Travis reports Dusty's escape and contact with Mira to Gabe.  Travis tells Mira to stay away and politely threatens her, but Mira can't forget the story.  Soon, Dusty contacts Mira again, and they meet up.  After a chase, two agents kill Dusty in front of Travis and Mira and walk away.  Travis knows something is wrong and confronts Gabe.  When Gabe doesn't give him information, Travis quits.  However, Gabe won't let him and hides Travis's family.  Travis searches high and low for his family but comes to an abrupt halt when he gets a call from Mira.  She believes someone murdered her boss and sullied Dusty's name.  She needs Travis to help her find the truth behind Dusty's last operation.  And it all revolves around Sofia Flores.<br/><br/>This movie is exciting, but the overall conclusion falls flat.  You assume a twist about Mira's father, Amanda's estranged husband, or Natalie's stranger.  This plot throws out a lot of open-ended information but offers no conclusion for them.  It's an action movie, masking as a political drama, and should have found a home on Paramount+.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blacklight.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blacklight.jpeg"
     }
   }
 ];
@@ -56,14 +56,6 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"The Batman",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebatman.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebatman.jpeg"
-  }
-}
+
 */
 
