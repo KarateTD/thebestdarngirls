b915f7c8aea4314218ed9d0096fdab9e23a5b737
Tiffany Gray <KarateTD@yahoo.com>
2022-04-03 12:28:47 -0400

    add 4 tv movies

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -44,6 +44,14 @@ module.exports = earlyScreening;
 *  small image 720w x 480h (in pixels)
 *  large image 1200w x 800h (in pixels)
 
-
+{
+  "option":"1",
+  "mtitle":"Sonic the Hedgehog 2",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sonicthehedgehog2.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sonicthehedgehog2.jpeg"
+  }
+}
 
 */
--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Fallen Angels Murder Club: Friends to Die For",
+    "review":"The Fallen Angels book club convenes and discusses their latest book selection.  While this looks like an ordinary book club to some, the club members have one thing in common: their parole officer, Jerry.  Hollis and her friends, Rena, Gene, Rory, Abby, and Miller, are ex-cons.  But Hollis proclaims that she took the fall for her ex-husband, Bill.  While the outside world judges them, they believe in each other.<br/><br/>After discussing their latest book, Rory invites everyone to his home for cigars and drinks.  Everyone but Hollis says yes, but Rory persuades her to join them.  They laugh, smoke, and drink, but something about Rory's most expensive cigars catches Hollis's eye.  They remind Hollis of the cigars Bill smoked.  Hollis demands to know how Rory got the cigars, but he refuses to answer.  Then he politely but firmly tells Hollis to go home.<br/><br/>Hollis, Gene, Rena, Abby, and Miller wait for Rory at their next book club.  So they are happy when the doors open but shocked to see who comes through.  It's Detectives Faber and Lincoln.  They came to find information and clues about who poisoned and killed Rory.  The information dumbfounds Rory's friends.  After the detectives question all of them and retreat, Hollis remembers that Rory's murder mirrors the one from the book they're reading.  Hollis and the Fallen Angels book club will need to work together to clear their name and find justice for Rory.<br/><br/>This film can be the start of an intriguing series.  Like several H.M.M. franchises, they have compelling connections and a drive to solve crimes.  However, these characters have more complex backstories that only Lifetime can provide.  With interesting anti-heroes, this movie attracts anyone who needs or believes in second chances.  Also, the mystery gives viewers lots of suspects, red herrings, a decent body count, and the start of the next chapter.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fallenangelsmurderclubfriendstodiefor.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fallenangelsmurderclubfriendstodiefor.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Dangerous Methods",
+    "review":"Lacy got a call from her boss, Sharon.  Sharon needs Lacy to fill in for a high-profile job.  Famous actor Desmond Gage needs an assistant while preparing for a role.  Desmond is a method actor, and he needs to live the part to give it life.  Lacy takes the job because she wants to make it big in Hollywood.  On her first night, Lacy introduces herself and prepares for the night.  But she starts to dream about Desmond.  During the day, Desmond is sweet, caring, and aloof.  At night, he is passionate and unhinged.  Desmond's script has his character falling madly in love with a woman.  What will Lacy do when his obsession becomes real and dangerous.<br/><br/>Lacy is one of the most unsympathetic characters in a Lifetime movie.  She keeps making the same naive mistake, and it's infuriating.  At certain moments, you start to think Lacy is unstable.  The saving grace in this film is the last 10 minutes.  Desmond gets an explanation about his actions.  Watch this movie at the risk of your sanity.<br/><br/>1.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dangerousmethods.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dangerousmethods.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"True to the Game 3",
+    "review":"A bruised and battered Gena wakes up to see Quadir next to her.  He explains that he woke up in a private hospital after flatlining.  The only people who knew he was alive were his mother, Carlos, and his nurse, Maria.  Quadir planned to get better, have Carlos fly Gena to L.A., and watch her until he healed.  After that, Quadir wanted to meet her and fly away together.  But that plan came to an end when he saw Jerrell attack Gina.  Now, Quadir desires to stay in hiding until Gena's bruises heal.  Then, they can leave together with the money.<br/><br/>In Philadelphia, Jerrell's brother, Terrell, got out of jail.  Terrell decides to walk back to the neighborhood and take over Jerrell's operation.  While in L.A., Jerrell asked Saleem to find the snitch.  Saleem takes a hard look at the crew but still wants payback for the robbery.  Knowing something is wrong, Alia and Shoog beg Bria to cut Dalvin out of her life.  But when Dalvin comes to the house with Gucci shoes and Twizzlers, Bria can't say no.<br/><br/>Titus is the snitch, and he is working with Detective Joe.  Joe owes $40,000 to dangerous people.  Joe has 48 hours to get the money, or he will die.<br/><br/>In a state of bliss, Gena has no plans to return until Bria reaches out for help.  After a fight, Bria accidently shot Dalvin.  She calls Gena to help her.  Gena decides to return but tells Quadir not to come for his safety.  Quadir adheres to her request but says he will join her if she doesn't return in 72 hours.  Gena's presence will be the gasoline on an inferno that will leave a trail of ashes.<br/><br/>Based on True the Game Part III by Teri Woods and the sequel to True to the Game 2, this movie has all the blood and torture of an old-school gangster film.  You get every tooth-pulling detail set to the juxtaposition of Cameo playing in the background.  While the music choices are reminiscent of Reservoir Dogs, the story leaves viewers with more questions than answers.  This film represents the last book in the franchise.  With fans' questions, will it be the last movie?<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/truetothegame3.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/truetothegame3.png"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"A Second Chance at Love",
+    "review":"Years ago, Alicia had to sit back and watch her parents, Jack and Brenda, divorce.  They had the perfect marriage, but work and pride separated them.  Now, Alicia and Arnold will celebrate their 5th wedding anniversary.  But Arnold isn't getting the time that he wants with Alicia.  Every night, Brenda comes over to eat and talk.  And Alicia worries about Jack's girlfriend of the week.  After another interruption, Alicia decides to talk to her parents about using Arnold's app, Organic Chemistry.  The app blurs users' photos until both parties have had several conversations.  Jack says yes, but Brenda says no.  However, Brenda sees her mother, Mema, have a successful date.  So, Brenda decides to give the app a try.  It's not long before Brenda and Jack connect, but they want to keep it a secret from Alicia.  When Alicia focuses more on her parents' relationship than hers, will she lose Arnold?<br/><br/>With a stunning backing track, this movie will move your heart and your feet.  You must watch the beginning of this movie because it explains the divorce's impact on Alicia.  Arnold does everything he can to be understanding and accommodating, but Alicia won't meet him halfway.  Arnold is ready to start a family and will bend over backward to start the next chapter in their life.  But she is still in her parents' book.  The love and frustrations will have you locked into this film.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/asecondchancelove.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/asecondchancelove.jpeg"
+    }
+  },
+  {
+    "option":"5",
     "mtitle":"The Lost Girls",
     "review":"Angie sits at home with her little sister, Maddie, and waits for their parents.  Once again they are late, so Angie has to miss the party.  The next day, Angie walks with Maddie.  Maddie says she wants ice cream, so Angie pulls out her ukelele and sings for money.  Mario walks up to her and requests a song for $5.  When Mario spots Angie in the park again, he tells her he has an uncle that hires singers.<br/><br/>After her school recital, Angie's parents forget to attend or pick her up.  So Angie calls Mario.  Mario tells Angie he will take her home, but his uncle resides down the street and wants to hear her sing.  Angie walks into a room with Deacon and Ida .  Ida puts some makeup on Angie to add a pop of color, and Deacon walks her to the back.  As Angie starts to play for Rob, Deacon turns on a camera and leaves the room.  Rob gets up and sexually assaults Angie.  When Angie wakes up, Ida names her Angel and threatens to expose her assault if Angie tries to leave.  Angie is a victim of human trafficking, and escaping will only be 10% of the battle.<br/><br/>For survivors, some scenes can be triggering.  This movie stands out because it doesn't give a flowering end to escaping this modern-day slavery.  You witness survivors struggling with reconnecting with family, working with the police, and finding their voice.  And with physical or mental threats, some go back to their captors.  This movie is honest because everyone doesn't have a happy, clean-cut ending.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thelostgirls.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thelostgirls.jpeg"
-    }
-  },
-  {
-    "option":"2",
-    "mtitle":"Don't Hang Up",
-    "review":"Tracy reminds her husband, Chris, that their daughter, Sarah, will attend a spiritual retreat.  The next day, Chris finishes his lecture on mouth breeders and gets in his car.  He gets a call from an unknown number and ignores it.  But he picks it up the second time.  The caller tells Chris that they have his daughter.  Chris must pay $10,000 to get Sarah back unharmed.  If Chris alerts anyone or hangs up, they will kill Sarah.  Over the next 24 hours, Chris will go to hell and back for Sarah while trying to keep Tracy in the dark.<br/><br/>This movie shines a light on a dark phenomenon that has gained momentum over the years.  This crime happens because so much information is readily available with a mouse click.  While it's easy to play armchair detective while watching, being in this situation in real-time is different.  This film may seem outlandish, but it happens to people every day.  So don't disregard the fear and panic Chris emotes on the screen.  Watch this movie and make a plan with your family.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/donthangup.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/donthangup.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"The Wedding Fix",
-    "review":"Gwen co-hosts Wake Up Washington.  And during the week-long wedding segment, Gwen's producer, Miranda, hands her a bouquet.  Upon closer inspection, Gwen sees a ring attached to it.  Gwen's co-hosts signals for Gwen's boyfriend, Brad, to step up and propose, but he is too busy on the phone.  He got traded and wants to change his plans.  The changes don't include Gwen or the proposal.<br/><br/>The next day, Miranda pulls Gwen into her office.  A jogger filmed the breakup and posted it online.  It has thousands of views in a few hours, and Gwen feels humiliated on top of being heartbroken.  Miranda knows Gwen scheduled a week off to help with the wedding of her best friend, Rachel.  Miranda tells Gwen to leave early and take time to heal.<br/><br/>Gwen arrives at a town known as Weddingsville, gives her keys to the valet, and talks to the concierge.  He tells her the hotel has no vacancies and can't let her check in two days early.  So, Gwen tells him to have the valet bring her car around.  Confused, the concierge tells Gwen the hotel doesn't have a valet.  She runs outside to see Josh exiting her vehicle.  Josh is the tour guide, but he took her keys and parked her car to be kind.  Rachel invites Gwen to stay at her mother's home.  Rachel's mother, Diane, is happy to have help.  Diane tells Gwen she won't do it alone.  Some of the work is a two-person job.  Rachel's fiancé, Pete, asked his best man, Josh, to lend Gwen a hand.  They joke about knowing each other and get started on the list.  While on a path to love, they will find their dreams and learn from each other.  But will Gwen's plus one leave Josh in the dust?<br/><br/>This movie offers a surprise wedding, maritime law, and a final to-do list for the ages.  The romance isn't what viewers will focus on when watching.  You want to see how Diane, Josh, and Gwen will overcome hurdles for Rachel and Pete's wedding.  Between broken legs, quintets, and peacocks, anything will happen.  Come for the romance and stay for the wedding plan flubs.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theweddingfix.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theweddingfix.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Your Boyfriend is Mine",
-    "review":"Ben works at an animal shelter by day and a gentleman service by night.  At his second job, women know him as Lance, the boyfriend for hire.  A bachelorette party hired Ben and his friend, Harrison, to mingle, dance, and be eye candy.  Ben sits down with a woman named Amanda, and they talk all night.  Ben leaves, but Amanda wants to see him again and proves it with a 1000 dollar tip.<br/><br/>Amanda meets with her brother, Walker, and asks him to collect information about Lance.  She pretends to bump into Ben and calls him Lance.  Amanda offers Ben a job as her house manager because he knew what she wanted when they met the first time.  The job pays $100,000, but Callie worries Amanda wants more.  Ben tells her the gentleman rules still apply, and he will be on the lookout for any odd behavior.<br/><br/>Ben shows up at Amanda's home early and is ready to work.  Amanda tries to get Ben to rub her feet, but Ben declines and goes home to Callie.  When they get comfortable in bed, Ben receives a phone call from Amanda.  Someone attempted to break into her home, and she doesn't feel safe alone.  She begs Ben to come over, and he does.  Amanda convinces Ben to remain until she gets a security system installed.  But she paid the installer to work slowly.  Ben has no idea he is in a black widow's web.  And she will do anything to anyone to make Ben her boyfriend.<br/><br/>This woman is brilliant, manipulative, and crazy.  Amanda can spot a weakness and use it to further her agenda.  She shackles, attacks, and drugs Ben to take him from Callie.  And when that's not enough, Amanda threatens Callie and Harrison's life in real-time.  Be ready to sit on the edge of your seat and talk to the screen.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/yourboyfriendismine.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/yourboyfriendismine.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Sins in the Suburbs",
-    "review":"Heather calls Jess with a smile because she got the painting restoration job and $5,000.  Jess demands a celebration, and they go to a local bar.  They have a few drinks, and Heather walks up to the bartender, Marisol, for another round.  Heather never saw Tyler sitting at the bar, but Marisol thinks she remembers him from her past.<br/><br/>As Heather restores the painting, she sees her new neighbor, Tyler.  He comes over to ask for a glass of water because his system is not on.  Jess comes in and invites Tyler to brunch with them.  Things are going great until Marisol calls Tyler Robert. Marisol is an inconvenient hurdle in Tyler's plan to win Heather's affection.  Tyler will deal with Marisol or anyone else that gets in his way.<br/><br/>Tyler has some decent plans, but they are not well thought out.  This movie has cringe-worthy moments but in a good way.  You are supposed to cringe, cover your eyes, or gasp in horror.  And let's not forget Jess's funny one-liners.  By the way, you get a shovel.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sinsinthesuburbs.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sinsinthesuburbs.jpeg"
     }
   }
 ];
@@ -56,41 +56,6 @@ module.exports = madeForTV;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Fallen Angels Murder Club: Friends to Die For",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fallenangelsmurderclubfriendstodiefor.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fallenangelsmurderclubfriendstodiefor.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Dangerous Methods",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dangerousmethods.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dangerousmethods.jpeg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"True to the Game 3",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/truetothegame3.png",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/truetothegame3.png"
-  }
-},
-{
-  "option":"4",
-  "mtitle":"A Second Chance at Love",
-  "review":"Years ago, Alicia had to sit back and watch her parents, Jack and Brenda, divorce.  They had the perfect marriage, but work and pride separated them.  Now, Alicia and Arnold will celebrate their 5th wedding anniversary.  But Arnold isn't getting the time that he wants with Alicia.  Every night, Brenda comes over to eat and talk.  And Alicia worries about Jack's girlfriend of the week. After another interruption, Alicia decides to talk to her parents about using Arnold's app, Organic Chemistry.  The app blurs users' photos until both parties have had several conversations. Jack says yes, but Brenda says no.  However, Brenda sees her mother, Mema, have a successful date.  So, Brenda decides to give the app a try.  It's not long before Brenda and Jack connect, but they want to keep it a secret from Alicia.  When Alicia focuses more on her parents' relationship than hers, will she lose Arnold?<br/><br/>With a stunning backing track, this movie will move your heart and your feet.  You must watch the beginning of this movie because it explains the divorce's impact on Alicia.  Arnold does everything he can to be understanding and accommodating, but Alicia won't meet him halfway. Arnold is ready to start a family and will bend over backward to start the next chapter in their life.  But she is still in her parents' book.  The love and frustrations will have you locked into this film.<br/><br/>4.5 out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/asecondchancelove.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/asecondchancelove.jpeg"
-  }
-}
+
 
 */
