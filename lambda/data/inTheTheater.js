83930d453ef3a1a17caaa16a95faaa06415643e9
Tiffany Gray <KarateTD@yahoo.com>
2022-05-13 21:53:48 -0400

    add firestarter

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Firestarter",
+    "review":"Andy wakes from a nightmare to see his daughter, Charlie, sitting in the kitchen, playing with a lighter.  She tells him that she is experiencing weird feelings again and can't sleep.  When his wife, Vicky, wakes up, Andy makes them pancakes.<br/><br/>Later, Charlie plays dodgeball with her classmates, and her bully, Gavin, slams a ball at the back of her head.  Charlie runs to the bathroom and turns on all the faucets.  Then, an explosion comes from Charlie's stall.  The principal and the police want answers about how Charlie caused the explosion.<br/><br/>Andy starts to pack their things to go on the run again.  He worries that DSI will come after them after Charlie's explosion.  Charlie overhears the conversation and wants to know about DSI.  During college, Vicky and Andy participated in an experiment for a chemical called LOX-6.  It gave Andy the ability to control people's minds and Vicky the power to move objects.  Charlie becomes increasingly upset and accidently sets Vicky's arms on fire.  Andy bandages Vicky's arms and makes plans to leave the next day.  To calm Charlie down, Andy takes Charlie out for ice cream.<br/><br/>They come home after their sweet tooth run, and Charlie admits that she meant to hurt Andy.  When Andy sees pictures off the wall and glass on the floor, Andy knows something is wrong.  He runs into the living room and sees Rainbird behind a pendant light with a knife at Charlie's throat.  Rainbird tells Andy that DSI sent him, and they want Charlie alive.  Then Vicky's deceased body falls to the floor.  Charlie becomes enraged and creates a blast to knock Rainbird to the ground.  Andy and Charlie go on the run, but Dr. Jane, Rainbird, and DSI aren't through with them yet.<br/><br/>Based on Firestarter by Stephen King and a remake of the 1984 cult classic Firestarter, the producers marketed this film as a horror, but it doesn't have the jump scares you expect.  With time, this remake has better graphics and special effects.  Unlike the original, the parents don't agree on rearing young Charlie.  One person wants Charlie to contain her abilities, while the other wants her to train and hone them.  While this won't be another cult classic, it has compelling special effects and solid acting from a young actress.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/firestarter.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/firestarter.jpg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Doctor Strange in the Multiverse of Madness",
     "review":"Doctor Strange and America Chavez run towards the Book of Vishanti with a demon chasing them.  Doctor Strange realizes the demon is too strong to stop.  There is only one way to win and save the multiverse, kill America.  Then, he wakes up in bed.  Doctor Strange is accustomed to these nightmares and brushes them off.  Today, Christine will marry Charles.<br/><br/>At the reception, Dr. Strange hears car crashes and people screaming.  He looks outside to see cars slamming by themselves.  He uses a spell to reveal a monster chasing a girl.  After Doctor Strange and Sorcerer Supreme Wong save the girl, Doctor Strange remembers her from his dream.  America swipes his sling ring and runs away.  Strange and Wong catch up to her, get the sling ring, and ask why the monster came after her.  She explains a demon desires to steal her power because she has the ability to jump through universes. Doctor Strange's dreams weren't dreams.  However, they were connections to the other Doctor Stranges in different universes.  Wong remembers markings on the monster, but they didn't look like any ruins they know.  They looked like witchcraft.  Doctor Strange decides to consult the strongest witch he knows: Wanda.<br/><br/>As Strange discusses the multiverse with Wanda, she says America's name.  Strange freezes because he never said it.  Wanda sent the creatures after America.  Wanda acquired the Darkhold and used it to move between universes to steal America's powers.  Once she has America's abilities, she can be in another universe with Billy and Tommy forever.  She gives Doctor Strange until sundown to bring America to her, or Scarlet Witch will come for them.<br/><br/>This movie kicks down any preconceived notions about the movie or the multiverse.  Before watching this film, you should watch What If, WandaVision, Spider-Man: No Way Home, and Doctor Strange.  While you won't get that awe-inspiring, stand up and cheer moment fans get with the other movies, you have a good time. You should know this is one of the most ruthless and bloody films in the MCU.  The graphics are stunning and look better in 3D.  And there are mid and post-credit scenes.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Memory",
     "review":"After finishing his last contract early, Alex goes to Mauricio with plans to retire.  Mauricio laughs and reminds Alex that contract killers like them don't stop.  Like his older brother, Alex has Alzheimer's, causing him to make detrimental mistakes on the job.  But Mauricio doesn't know and signs Alex up for another hit.  After completing the job, Alex looks at the list and sees someone added a young woman, Beatriz.  When he goes to kill her, Alex sees she is a young girl and leaves the room.  He tells the contractor to call it off, but it's too late.<br/><br/>FBI agents Vincent and Linda work in tandem with a Mexican police officer, Hugo.  They save exploited children at the border.  Vincent kills Papa, Beatriz's father and trafficker, in a sting operation.  Vincent believes Beatriz knows more than she's telling them and wants to put her into witness protection.  Gerald agrees, but only if she cooperates with the investigation.<br/><br/>Within 24 hours, a newscaster reports Beatriz's murder.  Alex worries he could be the killer, but his date confirms he spent the night with her.  Alex decides to go after Beatriz's killer.  Vincent, Linda, and Hugo feel betrayed by their governments and choose to find the person or persons in charge of killing Beatriz.  These four people are on a collision course of justice.<br/><br/>This movie highlights corruption at the highest level of both governments and the dark side of a mother's love.  The story is hard to follow during Alex's episodes, but it helps the viewer understand his confusion.  If this is your cup of tea, see it at the matinee price.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"The Northman",
     "review":"King Aurvandill returned from war and walked into the arms of his wife, Queen Gudrún, and son, Prince Amleth.  Aurvandill worries about his wounds and thinks it's time to prepare Amleth to take the throne.  Gudrún thinks Amleth is too young, and Aurvandill's brother, Fjölnir, should take it.  But Aurvandill disagrees and goes through with the ceremony.  Afterward, a group of masked men attacks Aurvandill.  Amleth watches as Fjölnir takes off his hood, declares the throne his, and kills Aurvandill.  Amleth runs to find Gudrún, but he is too late.  He runs to the river, gets in a boat, and paddles away. He declares that he will avenge his father, save his mother, and kill Fjölnir.<br/><br/>Years later, Vikings saved and raised Amleth.  After a brutal win, the Vikings take the women and men hostage for enslavement and kill the children.  Amleth learns the Vikings will take the slaves to Fjölnir to enrich his kingdom.  Amleth has a vision that his revenge nears.  He gives himself the mark of a slave and sneaks onto the boat heading for Iceland.  Olga, an enslaved woman, knows Amleth is a Viking.  He tells her his noble origin, and she tells him she is a sorceress.  She will use her gifts to aid Amleth if he frees her.  Amleth is on a path to revenge served bloody.<br/><br/>Based on the legend of Amleth, this film depicts the horrific events that inspired Hamlet.  Amleth learns that one's legacy isn't always linear.  It hides ugly truths and generational pain, and the plot foreshadows Amleth's dark legacy but through the eyes of a witch.  The set designers' and customers' meticulous attention to detail is noticeable.  However, with that level of attention, the disgusting horrors of war are painfully accurate.  Skip this film if depictions of torture, assault, and child abuse trigger you.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"The Bad Guys",
     "review":"Mr. Wolf is the mastermind behind a gang of thieves.  Mr. Snake is the safe-cracker of the crew with no fingers.  Ms. Tarantula is the hacker, and everyone calls her Web.  Mr. Shark is a master of disguise.  Mr. Piranha is the craziest member because he is willing to fight no matter how many people stand in his way.  Since the world sees them as bad, they act accordingly by robbing banks, pick-pocketing, and running long cons.  Mr. Wolf watches the news coverage, and Governor Foxington's comments make him angry.  Governor Foxington feels sorry for them because they are playing into a stereotype, and she knows the police will catch them soon.  Mr. Wolf wants revenge and decides to loot the Good Samaritan Award, The Golden Dolphin.  The legendary Crimson Paw came close to stealing it but went into hiding afterward.  Mr. Wolf convinces Mr. Snake to take the job by showing him the recipient: Professor Marmalade, a guinea pig.  Mr. Snake can't resist because guinea pigs are his favorite food.<br/><br/>After a wild, well-executed, stinky heist, the team is ready to walk away scot-free.  Then, Mr. Wolf sees an old lady going down the stairs with an open purse full of money and jewels.  He tries to take it, but she starts to fall down the stairs.  He decides to pull her back and help her.  She pats Mr. Wolf on the hand and calls him a good boy.  It has an instant effect on him.  After exposure, Chief Misty arrests the bad guys and throws them in the police van.  Prof. Marmalade speaks on behalf of The Bad Guys.  He offers to reform them at his private residence instead of jail time.  Mr. Wolf agrees on behalf of the entire crew.<br/><br/>Inside the police van, Mr. Wolf's agreement confuses the crew.  They want to know why Mr. Wolf agreed to go 'good.'  Mr. Wolf explains that the award will be at Prof. Marmalade's home.  If they get inside, they will have a second chance to take it.  The Bad Guys will become The Good Guys to be The Bad Guys.<br/><br/>Based on The Bad Guys by Aaron Blabey, this animated Leverage with Logan Lucky comedy style will have adults and kids laughing.  The basis of the heist is realistic, but for children, the animation adds a lot of fun and creative dramatics.  Without it, you can remove the cartoon, add real people, and the script would work.  Adults may want to watch it twice with all the added plot twists.  Yes, there are several within the con.  This movie teaches kids to be more than a stereotype.  That's a lesson everyone can learn.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebadguys.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebadguys.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Father Stu",
-    "review":"Stuart Long is an amateur boxer looking for his big break, but he is in the hospital again.  The doctor tells Stu to quit boxing.  Wallowing in self-pity, Stu goes to his brother's grave and drinks.  The cops pull him over and arrest him.  When his mother, Kathleen, bails Stu out of jail, he has a new goal: go to Hollywood and become an actor.<br/><br/>Stu arrives in Hollywood with dreams of becoming famous.  He works in a butcher's shop at the back of a convenience store until then.  At work, Stu sees a beautiful woman, Carmen, and tries to ask her out.  She turns him down and walks out of the store.  A co-worker tells Stu that Carmen posted a flyer for a local church.  Stu gets dressed up and goes to church to impress her.<br/><br/>After a tumultuous day with his father, Bill, Stu rides his motorcycle down the street.  A car almost side-swipes him.  While dodging out of the way, Stu runs a red light.  He gets hit by a car, falls into the street, and gets run over by another vehicle.  As he lays there dying, Stu has a vision of Mary.  She tells him he has more in store for his life, and he will inspire others.  After a few surgeries, Stu feels back to his old self.  He calls Carmen on a date. She thinks Stu is going to propose.  Instead, Stu tells her he wants to become a priest.  Stu plans to enter theology school, become a priest, and lead a flock.  As they say, 'If you want to hear God laugh, tell him your plans.' Stu will inspire others in a way he could never imagine.<br/><br/>Based on a true story, this movie is about redemption and grace.  Don't expect the wholesome story that viewers get this time of the year.  This film doesn't try to sugarcoat Stu's life or actions.  He, like everyone else, is a flawed man that struggles each day.  The burden of being a living failure weighs heavily on Stu's soul.  But each downfall becomes a triumph and helps him point his flock onto the right path.  See this one, but not with the whole family.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fatherstu.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fatherstu.jpeg"
     }
   }
 ];
@@ -56,15 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Firestarter",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/firestarter.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/firestarter.jpg"
-  }
-}
+
 
 */
 
