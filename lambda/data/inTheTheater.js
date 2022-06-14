b26faaaae8592984ade1e6613db40544ed1cd35f
Tiffany Gray <KarateTD@yahoo.com>
2022-06-14 17:35:45 -0400

    add 2 must buy

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -56,7 +56,15 @@ module.exports = inTheTheaters;
 
 /*
 
-
+{
+  "option":"1",
+  "mtitle":"Lightyear",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lightyear.jpg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lightyear.jpg"
+  }
+}
 
 */
 
