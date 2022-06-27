41496d03a1e5537b43a77f9dbd3289ad62203bc5
Tiffany Gray <KarateTD@yahoo.com>
2022-06-26 21:55:20 -0400

    add 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Elvis",
+    "review":"Colonel Tom Parker sets up for his next show with Hank Snow, and Hank's son, Jimmie, puts a song on the album player. His father tells him to turn it off because it's black music, but Billy says the singer is white. Tom wants to find the singer immediately and manage his career. The singer, Elvis Presley, hits the stage, and the girls go wild. And Tom believes it's time for the Show-Man to meet the Snow-Man. Tom creates Elvis Presley Enterprises and puts Elvis's dad, Vernon, in charge. Now, Elvis is ready to take the world by storm with Tom by his side. Tom will give his accounts of Elvis, Elvis's military service, money, marriage, and downfall for you to judge.<br/><br/>What happens when the tamer loses control of the lion? Tom answers that question loud and clear. Tom narrates the story to redeem himself after suffering a heart attack. As the story of their relationship unfolds, history plays in the background. As you listen to the story, you wonder what Tom is hiding because he refuses to let Elvis go International. However, he got Elvis the biggest deal in history, the first satellite concert, and a residency in Vegas. Would Elvis be Elvis without Tom? That's up to you to decide.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/elvis.jpeg",
+      "largeImageHrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/elvis.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Black Phone",
+    "review":"Finny plays baseball but fails to strike out the opposing team's best pitcher, Bruce. After the game, Bruce compliments Finny, and the teams leave the field. Sometime after, Finny walks to school with his sister, Gwen, and they see a missing person's flyer up for Bruce. Gwen believes The Grabber got Bruce, and he's never coming back.<br/><br/>Finny hides in the bathroom at school, and three bullies come in. They name-call him until Robin walks into the bathroom. As Robin washes Moose's blood off his fist, he warns them not to miss with Finny. At the same time, the principal calls Gwen into his office. She told someone that she had dreams of the missing boys and black balloons. Gwen admits that she has visions in her dreams that come true. The detective gives Gwen his business card in case she has another. Those dreams won't help because The Grabber got Robin.<br/><br/>After school, Gwen leaves to visit a friend, and Finny walks home to look after their dad. Finny sees a man drop his groceries and offers to help. The man snatches Finny off the street using black balloons as a diversion. The man sprays something in Finny's mouth, puts Finny in the car, and drives away.<br/><br/>Finny wakes up on a mattress in a soundproofed concrete room. Finny takes in his surroundings and notices a black phone on the wall. He picks it up to make a call for help but hears nothing. That is, until the phone rings. Finny picks up, but the caller doesn't recall his name. The caller is Bruce. He warns Finny to get out, or The Grabber will kill Finny just like him.<br/><br/>This psychological thriller is not a horror movie as advertised. It goes beyond that and becomes a tale of triumph and strength. Just to let you know, this movie doesn't depict child molestation. However, it displays child abuse. The moment Finny squares his shoulders, you will cheer for him. There are a few jump scares but nothing grotesque. Luckily, the plot doesn't try to get into The Grabber's psyche or make viewers feel empathy for the monster. This story is about Finny growing into a confident young man with help from past victims.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theblackphone.jpg",
+      "largeImageHrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theblackphone.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Lightyear",
     "review":"The ship, also known as the turnip, wakes up Buzz from hypersleep. The ship's sensors detected life on the planet nearby. Buzz wakes Commander Hawthorne to take a closer look at the planet's inhabitants and collect soil samples. They make it back to the ship and try to fly away, but Buzz crashes and cracks their fuel cell, and they don't have another. Til they can create another fuel cell that can get them into hyperspace, the crew can't leave the planet.<br/><br/>The crew created a colony and a new fuel cell within a year, and Buzz boards XL-1 for a four-minute test flight around the sun and back. Buzz gets 60% into hyperspace before the fuel cell dies, and he lands. The trip lasted 4 minutes for Buzz, but it's four years for the colony. To help Buzz acclimate, Hawthorne gives Buzz a robotic therapy cat, Sox. Sox worries about Buzz, but Buzz tells the cat to help by fixing the fuel. Sox says ok.<br/><br/>Fifteen missions later, Buzz comes home to find that Hawthorne died. She left a video message for Buzz with her granddaughter, Izzy, sweetly interrupting. Commander Burnside walks into Hawthorne's old barracks and tells a weeping Buzz that the fuel mission is over. The colony decided to end the space ranger program, stay on the planet, take Sox, and create a laser dome to keep out the planet's dangerous inhabitants. Buzz tells Sox the mission is over, and Sox is disappointed because he figured out the formula. So Buzz grabs Sox, creates the fuel cell, and steals a ship. Buzz and Sox accomplish their mission and land on the planet but can't land on the colony because of the laser dome. Buzz finds the safest landing spot, and someone in a suit approaches him and tells Buzz to be quiet. The person in the suit sees Buzz's name tag and tells him that her name is Izzy, Hawthorne's granddaughter. It's been 22 years since Buzz left.<br/><br/>Since then, an enemy spacecraft came to the planet with a legion of robots. Their commander is the evil Emperor Zurg. After Zurg arrived, his army waged a constant attack on the laser dome. Izzy remembers the stories about Buzz and knows he is the missing link to Operation Surprise Party. They will take out the mothership controlling the robots to save the colony. But they need Buzz to get them to Zurg's spacecraft. Buzz respects Izzy's lineage, but he doesn't work with rookies. While Buzz doesn't want their help, he will need it to save the colony, defeat Zurg, and redefine his mission.<br/><br/>In 1995, Andy got a toy inspired by his favorite movie. Lightyear is the movie. So this adaptation isn't a remake or a sequel; it's a prequel for Andy's world in Toy Story 1. Pixar animators did a stunning job creating a colonized world on a dangerous planet. And their space animation is second to none. While creating a world of space travel, animators included geometry. Yes, Buzz is a mathlete. And let's not forget the quiet love letter Pixar sent to classic Nintendo fans. While this is an animation, the complex storyline is for 1995 Toy Story fans, not their kids. However, its action and jokes will entertain parents and their children.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Jurassic World: Dominion",
     "review":"After Maisie freed the dinosaurs, the humans and animals must find a way to co-exist. Claire forms the Dinosaur Protection Group to save them from poachers, breeders, and dinosaur fighters. She reports the violators to the authorities, who send the animals to Biosyn. Biosyn created a sanctuary in Italy for the animals and now researches them to develop pharmaceuticals.<br/><br/>Doctor Ellie Sattler arrives at a cornfield infested with locusts. The bugs are enormous and carry a genome from the Jurassic era. Ellie uncovers the locusts didn't ravage the neighbor's crops and ask why. The farmer tells Ellie the neighbor uses Biosyn seeds.<br/><br/>At a dig site, Ellie catches up with Doctor Alan Grant. He looks over the insect after catching up with Ellie. He determines that her suspicions are correct and the bug is genetically modified. Ellie believes Biosyn created the insects to manufacture a global food crisis with their crop being the only food source. Ellie wants to go to Biosyn and investigate for evidence. Ellie tells Alan not to worry because she has a friend on the inside: Dr. Ian Malcolm.<br/><br/>On the outskirts of Alaska, Claire and Owen privately raise Maisie because the world knows she released the dinosaurs. Maisie sees a small velociraptor in the woods with Blue's markings. It's Blue's child, but Maisie can't figure out how Blue had a baby with no males around. She names the small velociraptor Beta and feeds it.<br/><br/>As Owen surveys the property, he hears Blue's cries for help. He sees Rainn, a poacher, kidnapping Beta. Owen looks up and sees a woman grabbing Maisie and pulling her into a van. Owen runs home to tell Claire that Beta and Maisie are gone, and they prepare to help. Owen explains to Blue that the kidnappers have his kid too. He promises to bring Beta back, and Blue roars and leaves. Rainn stole Beta and Maisie with a purpose. Doctor Henry Wu needs them and their genetic makeup to fix his mistake and save the planet. The past and the future will collide to reveal the evil plan behind the lies of benevolence that is Biosyn.<br/><br/>The screenwriters attempted a massive feat by bringing the franchise's past and present characters together along with the dinosaurs. It does help to watch Jurassic Park, Jurassic World, and Jurassic World: Fallen Kingdom to understand each character's backstory and the movie's easter eggs. With Now This News commentary, the film attempts to give a brief overview of the prequel's major plot points, but you don't get an understanding of the character's arcs. The CGI department created thrilling new dinosaurs and improved fan favorites. This movie falls flat because it's the same underlying story: two lost kids are inside a business with no respect for mother nature. This movie is thrilling, but nothing will top the first.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Firestarter",
     "review":"Andy wakes from a nightmare to see his daughter, Charlie, sitting in the kitchen, playing with a lighter.  She tells him that she is experiencing weird feelings again and can't sleep.  When his wife, Vicky, wakes up, Andy makes them pancakes.<br/><br/>Later, Charlie plays dodgeball with her classmates, and her bully, Gavin, slams a ball at the back of her head.  Charlie runs to the bathroom and turns on all the faucets.  Then, an explosion comes from Charlie's stall.  The principal and the police want answers about how Charlie caused the explosion.<br/><br/>Andy starts to pack their things to go on the run again.  He worries that DSI will come after them after Charlie's explosion.  Charlie overhears the conversation and wants to know about DSI.  During college, Vicky and Andy participated in an experiment for a chemical called LOX-6.  It gave Andy the ability to control people's minds and Vicky the power to move objects.  Charlie becomes increasingly upset and accidently sets Vicky's arms on fire.  Andy bandages Vicky's arms and makes plans to leave the next day.  To calm Charlie down, Andy takes Charlie out for ice cream.<br/><br/>They come home after their sweet tooth run, and Charlie admits that she meant to hurt Andy.  When Andy sees pictures off the wall and glass on the floor, Andy knows something is wrong.  He runs into the living room and sees Rainbird behind a pendant light with a knife at Charlie's throat.  Rainbird tells Andy that DSI sent him, and they want Charlie alive.  Then Vicky's deceased body falls to the floor.  Charlie becomes enraged and creates a blast to knock Rainbird to the ground.  Andy and Charlie go on the run, but Dr. Jane, Rainbird, and DSI aren't through with them yet.<br/><br/>Based on Firestarter by Stephen King and a remake of the 1984 cult classic Firestarter, the producers marketed this film as a horror, but it doesn't have the jump scares you expect.  With time, this remake has better graphics and special effects.  Unlike the original, the parents don't agree on rearing young Charlie.  One person wants Charlie to contain her abilities, while the other wants her to train and hone them.  While this won't be another cult classic, it has compelling special effects and solid acting from a young actress.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/firestarter.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/firestarter.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Doctor Strange in the Multiverse of Madness",
-    "review":"Doctor Strange and America Chavez run towards the Book of Vishanti with a demon chasing them.  Doctor Strange realizes the demon is too strong to stop.  There is only one way to win and save the multiverse, kill America.  Then, he wakes up in bed.  Doctor Strange is accustomed to these nightmares and brushes them off.  Today, Christine will marry Charles.<br/><br/>At the reception, Dr. Strange hears car crashes and people screaming.  He looks outside to see cars slamming by themselves.  He uses a spell to reveal a monster chasing a girl.  After Doctor Strange and Sorcerer Supreme Wong save the girl, Doctor Strange remembers her from his dream.  America swipes his sling ring and runs away.  Strange and Wong catch up to her, get the sling ring, and ask why the monster came after her.  She explains a demon desires to steal her power because she has the ability to jump through universes. Doctor Strange's dreams weren't dreams.  However, they were connections to the other Doctor Stranges in different universes.  Wong remembers markings on the monster, but they didn't look like any ruins they know.  They looked like witchcraft.  Doctor Strange decides to consult the strongest witch he knows: Wanda.<br/><br/>As Strange discusses the multiverse with Wanda, she says America's name.  Strange freezes because he never said it.  Wanda sent the creatures after America.  Wanda acquired the Darkhold and used it to move between universes to steal America's powers.  Once she has America's abilities, she can be in another universe with Billy and Tommy forever.  She gives Doctor Strange until sundown to bring America to her, or Scarlet Witch will come for them.<br/><br/>This movie kicks down any preconceived notions about the movie or the multiverse.  Before watching this film, you should watch What If, WandaVision, Spider-Man: No Way Home, and Doctor Strange.  While you won't get that awe-inspiring, stand up and cheer moment fans get with the other movies, you have a good time. You should know this is one of the most ruthless and bloody films in the MCU.  The graphics are stunning and look better in 3D.  And there are mid and post-credit scenes.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/doctorstrangemultiversemadness.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/doctorstrangemultiversemadness.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Memory",
-    "review":"After finishing his last contract early, Alex goes to Mauricio with plans to retire.  Mauricio laughs and reminds Alex that contract killers like them don't stop.  Like his older brother, Alex has Alzheimer's, causing him to make detrimental mistakes on the job.  But Mauricio doesn't know and signs Alex up for another hit.  After completing the job, Alex looks at the list and sees someone added a young woman, Beatriz.  When he goes to kill her, Alex sees she is a young girl and leaves the room.  He tells the contractor to call it off, but it's too late.<br/><br/>FBI agents Vincent and Linda work in tandem with a Mexican police officer, Hugo.  They save exploited children at the border.  Vincent kills Papa, Beatriz's father and trafficker, in a sting operation.  Vincent believes Beatriz knows more than she's telling them and wants to put her into witness protection.  Gerald agrees, but only if she cooperates with the investigation.<br/><br/>Within 24 hours, a newscaster reports Beatriz's murder.  Alex worries he could be the killer, but his date confirms he spent the night with her.  Alex decides to go after Beatriz's killer.  Vincent, Linda, and Hugo feel betrayed by their governments and choose to find the person or persons in charge of killing Beatriz.  These four people are on a collision course of justice.<br/><br/>This movie highlights corruption at the highest level of both governments and the dark side of a mother's love.  The story is hard to follow during Alex's episodes, but it helps the viewer understand his confusion.  If this is your cup of tea, see it at the matinee price.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/memory.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/memory.jpg"
     }
   }
 ];
@@ -56,24 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Elvis",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/elvis.jpeg",
-    "largeImageHrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/elvis.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"The Black Phone",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theblackphone.jpg",
-    "largeImageHrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theblackphone.jpg"
-  }
-}
+
 
 */
 
