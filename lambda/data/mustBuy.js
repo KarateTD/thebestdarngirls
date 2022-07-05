507beb3866081de7d55e97d3cb3b19c9c095492b
Tiffany Gray <KarateTD@yahoo.com>
2022-07-05 18:36:13 -0400

    add must buy

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,15 @@
 var mustBuy = [
   {
     "option":"1",
+    "mtitle":"Everything Everywhere All at Once",
+    "review":"Evelyn prepares the paperwork for their upcoming audit.  Her focus pulls her attention away from her husband, Waymond, when he tries to hand her divorce papers.  Her daughter, Joy, waits for Evelyn with Joy's girlfriend, Becky.  Evelyn brushes off Joy and concentrates on work.<br/><br/>Evelyn grabs her paperwork and goes to the IRS headquarters with Waymond and her father, Gong Gong.  As they ride the elevator up, Waymond deploys an umbrella to block the camera, grabs two earbuds, and puts them in Evelyn's ears.  He tells Evelyn that he is Waymond from another universe, and he needs her help.  If she wants to see him again, follow the instructions on the paper.<br/><br/>While talking with the IRS inspector, Deirdre, Evelyn looks at the paper.  It gives Evelyn 3 steps that land her in the janitor's closet with the other Waymond.  He tells her that the multiverse exists.  When another Evelyn discovered you could connect to another version of yourself, they used the power to gain skills.  But Jobu Tupaki wants to use this ability to destroy all the universes.  So, she is going through each one and killing Evelyns.  Waymond wants this universe's Evelyn to tap into the other Evelyns and defeat Jobu.  Then, Waymond and Deidre start fighting.  Within minutes, Evelyn has to learn how to tap into the multiverse, use her powers, and fight Jobu.  When she is face-to-face with her universe's Jobu, can Evelyn kill Jobu?<br/><br/>This film mixes the lessons of generational pain with some of the most hilarious, painful, and outrageous fight scenes ever crafted.  To tap into another universe's skills, they have to stimulate themselves. And it gets wild with fanny packs, awards, and dogs.  This 3-part film slows down towards the end and loses the audience for a while.  Be ready for a visual feast.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/everythingeverywhereallatonce.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/everythingeverywhereallatonce.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Fantastic Beasts: The Secrets of Dumbledore",
     "review":"Fantastic Beasts: The Secrets of Dumbledore was an Early Screening review for Premium Access members only.  The review and rating has been moved to the Library.  To have access to the review and rating say, 'I want to buy Premium Access.'",
     "image":{
@@ -9,7 +18,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Bad Guys",
     "review":"Mr. Wolf is the mastermind behind a gang of thieves.  Mr. Snake is the safe-cracker of the crew with no fingers.  Ms. Tarantula is the hacker, and everyone calls her Web.  Mr. Shark is a master of disguise.  Mr. Piranha is the craziest member because he is willing to fight no matter how many people stand in his way.  Since the world sees them as bad, they act accordingly by robbing banks, pick-pocketing, and running long cons.  Mr. Wolf watches the news coverage, and Governor Foxington's comments make him angry.  Governor Foxington feels sorry for them because they are playing into a stereotype, and she knows the police will catch them soon.  Mr. Wolf wants revenge and decides to loot the Good Samaritan Award, The Golden Dolphin.  The legendary Crimson Paw came close to stealing it but went into hiding afterward.  Mr. Wolf convinces Mr. Snake to take the job by showing him the recipient: Professor Marmalade, a guinea pig.  Mr. Snake can't resist because guinea pigs are his favorite food.<br/><br/>After a wild, well-executed, stinky heist, the team is ready to walk away scot-free.  Then, Mr. Wolf sees an old lady going down the stairs with an open purse full of money and jewels.  He tries to take it, but she starts to fall down the stairs.  He decides to pull her back and help her.  She pats Mr. Wolf on the hand and calls him a good boy.  It has an instant effect on him.  After exposure, Chief Misty arrests the bad guys and throws them in the police van.  Prof. Marmalade speaks on behalf of The Bad Guys.  He offers to reform them at his private residence instead of jail time.  Mr. Wolf agrees on behalf of the entire crew.<br/><br/>Inside the police van, Mr. Wolf's agreement confuses the crew.  They want to know why Mr. Wolf agreed to go 'good.'  Mr. Wolf explains that the award will be at Prof. Marmalade's home.  If they get inside, they will have a second chance to take it.  The Bad Guys will become The Good Guys to be The Bad Guys.<br/><br/>Based on The Bad Guys by Aaron Blabey, this animated Leverage with Logan Lucky comedy style will have adults and kids laughing.  The basis of the heist is realistic, but for children, the animation adds a lot of fun and creative dramatics.  Without it, you can remove the cartoon, add real people, and the script would work.  Adults may want to watch it twice with all the added plot twists.  Yes, there are several within the con.  This movie teaches kids to be more than a stereotype.  That's a lesson everyone can learn.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Ambulance",
     "review":"Veteran William Sharp sits on the phone arguing with insurance companies.  His wife, Amy, needs experimental surgery to save her life.  William hangs up the phone and tells Amy everything will be ok.  Then, he reminds her to turn on the alarm while he goes on a forklift driver interview.  Unbeknownst to Amy, William drives to Danny's chopshop.<br/><br/>As William attempts to ask for a 231,000 dollar loan, Danny collects his crew and cars for his next heist.  He gives William 5 minutes to decide if he is in or out.  If William joins, he will gain millions.  William gears up and jumps on the truck.  But there is a kink in the plan.  Officer Zack built up the nerve to talk to his bank teller crush, Kim.  Seeing Zack won't let up, Danny lets Zack inside.  Soon, Zack realizes something is going on, and all hell breaks loose.  Danny didn't know he was part of a task force sting operation.  They knew Danny wanted to hit a bank and planned to catch him after the robbery without hostages.  But Zack messed up their plan too.<br/><br/>After a fight in the parking garage, William shoots Zack.  They see EMT Cam and Scott loading Zack onto the ambulance.  William pulls Scott from the driver's seat, and Danny tries to force Cam out of the back.  But she will not leave her patient.  Danny agrees because killing a cop is a life sentence.  Also, Danny can use Cam and Zack as bargaining chips with the police.  With millions in their bag and two hostages, can Danny and William get home with the money?<br/><br/>A remake of Ambulancen, this film gives you a brief introduction to William's turmoil.  Then the action gets going.  The synopsis above is the first 20 minutes of the movie.  The tale of family bonds rivals all other films before it.  With a 60-mph surgery, unholy alliances, and sharpshooters, this film allows the audience to care about Cam, Danny, and William.  Watch this movie on the big screen only.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var mustBuy = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Father Stu",
     "review":"Stuart Long is an amateur boxer looking for his big break, but he is in the hospital again.  The doctor tells Stu to quit boxing.  Wallowing in self-pity, Stu goes to his brother's grave and drinks.  The cops pull him over and arrest him.  When his mother, Kathleen, bails Stu out of jail, he has a new goal: go to Hollywood and become an actor.<br/><br/>Stu arrives in Hollywood with dreams of becoming famous.  He works in a butcher's shop at the back of a convenience store until then.  At work, Stu sees a beautiful woman, Carmen, and tries to ask her out.  She turns him down and walks out of the store.  A co-worker tells Stu that Carmen posted a flyer for a local church.  Stu gets dressed up and goes to church to impress her.<br/><br/>After a tumultuous day with his father, Bill, Stu rides his motorcycle down the street.  A car almost side-swipes him.  While dodging out of the way, Stu runs a red light.  He gets hit by a car, falls into the street, and gets run over by another vehicle.  As he lays there dying, Stu has a vision of Mary.  She tells him he has more in store for his life, and he will inspire others.  After a few surgeries, Stu feels back to his old self.  He calls Carmen on a date. She thinks Stu is going to propose.  Instead, Stu tells her he wants to become a priest.  Stu plans to enter theology school, become a priest, and lead a flock.  As they say, 'If you want to hear God laugh, tell him your plans.' Stu will inspire others in a way he could never imagine.<br/><br/>Based on a true story, this movie is about redemption and grace.  Don't expect the wholesome story that viewers get this time of the year.  This film doesn't try to sugarcoat Stu's life or actions.  He, like everyone else, is a flawed man that struggles each day.  The burden of being a living failure weighs heavily on Stu's soul.  But each downfall becomes a triumph and helps him point his flock onto the right path.  See this one, but not with the whole family.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fatherstu.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fatherstu.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Batman",
-    "review":"It's Halloween.  And while most of Gotham celebrate candy and debauchery, Bruce Wayne uses his mask for vengeance against the city's underbelly.  Batman has looked over the citizens with Lieutenant Gordon and his signal for two years.  Someone killed mayor Don Mitchell, wrapped his head in tape, cut off his thumb, and wrote 'No More Lies' on him.  The killer, The Riddler, left a card for 'The Batman' with a cipher inside.  The Riddler vows to kill more people and unmask the corruption in Gotham.  Batman uncovers pictures of the mayor with a young woman, that is not his wife, coming out of the Iceberg Lounge.<br/><br/>During questioning, a waitress walks into the room.  She freezes when she sees the photos.  The waitress goes to her apartment that she shares with the young woman, Annika, from the images.  The waitress promises to help Annika getaway.  After changing clothes, the waitress gingerly leaves the apartment through a window.  She goes to the mayor's home, an active crime scene, finds a hidden safe and cracks it.  She pulls out Annika's stolen passport when Batman grabs her.  The waitress is Selina Kyle, Catwoman.  Batman worries about the cat burglar and follows Selina back to her apartment.  Annika and their phones are gone.  Selina tells Batman that she will find answers in the 44 Below Club.  It's a private club within the Iceberg Lounge where the crooked politicians and cops hang out.  Batman knows this will be a target-rich environment for the Riddler, so he devises a plan for him and Selina to work together.  In 7 days, Batman will learn that his mask can inspire hope, create resentment, and reveal dark aspirations.  And all roads lead to Arkham.<br/><br/>This film is one of the best Batman movies since The Dark Knight.  It provides action, mystery, a few jump-scares, and intensely emotional moments.  This movie assumes you know Batman's backstory.  While the story and acting were impressive, the running time made the movie drag.  Don't be surprised if you wonder 'is this the end' several times.  The end credits will tempt you to stay, but there are zero spoilers.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebatman.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebatman.jpeg"
     }
   }
 ];
@@ -93,16 +93,6 @@ module.exports = mustBuy;
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/coda.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/coda.jpg"
-    }
-  }
-
-  {
-    "option":"2",
-    "mtitle":"Everything Everywhere All at Once",
-    "review":"Evelyn prepares the paperwork for their upcoming audit.  Her focus pulls her attention away from her husband, Waymond, when he tries to hand her divorce papers.  Her daughter, Joy, waits for Evelyn with Joy's girlfriend, Becky.  Evelyn brushes off Joy and concentrates on work.<br/><br/>Evelyn grabs her paperwork and goes to the IRS headquarters with Waymond and her father, Gong Gong.  As they ride the elevator up, Waymond deploys an umbrella to block the camera, grabs two earbuds, and puts them in Evelyn's ears.  He tells Evelyn that he is Waymond from another universe, and he needs her help.  If she wants to see him again, follow the instructions on the paper.<br/><br/>While talking with the IRS inspector, Deirdre, Evelyn looks at the paper.  It gives Evelyn 3 steps that land her in the janitor's closet with the other Waymond.  He tells her that the multiverse exists.  When another Evelyn discovered you could connect to another version of yourself, they used the power to gain skills.  But Jobu Tupaki wants to use this ability to destroy all the universes.  So, she is going through each one and killing Evelyns.  Waymond wants this universe's Evelyn to tap into the other Evelyns and defeat Jobu.  Then, Waymond and Deidre start fighting.  Within minutes, Evelyn has to learn how to tap into the multiverse, use her powers, and fight Jobu.  When she is face-to-face with her universe's Jobu, can Evelyn kill Jobu?<br/><br/>This film mixes the lessons of generational pain with some of the most hilarious, painful, and outrageous fight scenes ever crafted.  To tap into another universe's skills, they have to stimulate themselves. And it gets wild with fanny packs, awards, and dogs.  This 3-part film slows down towards the end and loses the audience for a while.  Be ready for a visual feast.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/everythingeverywhereallatonce.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/everythingeverywhereallatonce.jpeg"
     }
   }
 
