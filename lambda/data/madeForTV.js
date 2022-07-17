299ea27872f582b07b28baf6e7b2315bfc7cbfbb
Tiffany Gray <KarateTD@yahoo.com>
2022-07-17 13:12:24 -0400

    5 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
-    "mtitle":"Flowers in the Attic: The Origin: Part 1",
-    "review":"Unmarried Olivia Winfield is unlike any woman of her time because she works outside the home and is a partner in her father's company. Mr. Winfield tells Olivia they will have a guest for dinner. The guest is the very wealthy and single Malcolm Foxworth. Olivia and Malcolm have a battle of wits. Impressed, Malcolm invites her to go on a walk.<br/><br/>Olivia is falling head over heels, but Malcolm isn't forthcoming about his family. It's not too long before the two get married. Malcolm tells Olivia that managing his grand-scale home will be a full-time job as they arrive. And he expects Olivia to handle it with grace and his standards. Olivia finds the house overwhelming but decides to handle it full steam ahead. Olivia's years of overseeing a company could never prepare her for Malcolm, Foxworth Hall, and the evil inside.<br/><br/>Based on Garden of Shadows by V.C. Andrews, this movie is the four-part prequel to Flowers in the Attic. Olivia tells her history in her last will and testament because she never could before. This movie should come with a trigger warning because it displays sexual assault. It will be uncomfortable to watch but allows viewers to see what caused Olivia to break mentally. And the cinematography of the home helps you see this beautiful mansion as a prison or gilded cage. Before watching this movie, you must see Flowers in the Attic.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"Flowers in the Attic: The Origin: Part 2",
+    "review":"Olivia puts on a fake belly and prepares the staff for the changes to the household. She informs them that she is pregnant, her step-mother-in-law, Alicia, is leaving for Philidelphia, and Alicia's son, Christopher, will stay. Olivia puts Alicia in the chamber maids' quarters with access to the attic. After childbirth, Olivia will pretend the baby is hers, and Alicia will quietly retreat with Christopher. Olivia falsely believes this will give her control over Malcolm and the household. Unfortunately, it will take 15 years for Olivia to see she never had power in Foxworth Hall. She is a victim and perpetrator of Malcolm's crimes and sinful nature.<br/><br/>Based on Garden of Shadows by V.C. Andrews, this segment continues where part one left off but jumps 15 years within the movie. All the children grow up and suffer at Malcolm's feet while they try to assert control over their money, love, and body. However, Olivia does the most in-depth soul searching and self-realization of any character. Unlike the first, this film ends on a predictable cliffhanger.<br/><br/>4.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/flowersintheattictheorigin.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/flowersintheattictheorigin.jpeg"
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/flowersintheattictheorigin.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/flowersintheattictheorigin.jpeg"
     }
   },
   {
     "option":"2",
-    "mtitle":"Keeping Up With the Joneses: The Wrong Nemesis",
-    "review":"Robin anxiously waits for the police to declare her husband, Web, dead so she can inherit his properties. Until then, Robin wants to focus on Carrie's Crystal Water Foundation and developing properties around the family's new golden resource: water. Kayla walks outside to check on Tara because Tara left her private school for community college. The move seems strange to Tara's family, but it's the ideal setting for her. Kayla is making moves in the dating world. She wants to keep her ex-fiancé in the past and enjoy her single life. But the family's name adds complication for all the women. Between a secret child, schemers, and eco-terrorist, this family has no shortage of enemies. They have so many on their heels that they have their eyes on the wrong nemesis.<br/><br/>Wow! Oh, wow! This movie takes the definition of twist and turns and throws it into a tornado. The plot carefully introduces new characters, establishes familiar faces, and gives you business classes in two hours. The storyline helps propel each character's story forward and gear you up for the next movie. And special applause goes to the wardrobe, hair, and makeup team. The women are runway and business ready with flawless style viewers will want to duplicate. Kudos on rebooting the series with more zeal, drama, class, and fashion than the first season.<br/><br/>5 out of 5 stars.",
+    "mtitle":"Keeping Up With the Joneses: The Wrong Murder",
+    "review":"The Jones women work on building around their water wealth while starting a charity, but Webb's death looms over them. Robin handles business dealings while answering questions about her involvement. Pam entices Stephen to look into the sabotage. Also, Pam asks Sheldon to look into Eve's story. Eve says she is Pam's long-lost child, but Pam desires proof. Kayla takes a bold leap into the dating world but can't shake off her ex-fiancé, William. Carrie works hard to get her family to accept and trust Lance. However, one person wants to take advantage of Lance's nefarious nature. Leo did some digging and discovered Lance's clever insider trading deal. Tara doesn't believe Eve, so Tara asks Rich to seduce Eve for information. Until then, Tara works overtime to make the family see her as an adult. The Jones women maintain appearances while gaining control of their professional endeavors. How can they do it with the wrong murder hanging over their head and enemies encroaching from every side?<br/><br/>Each installment widens Jones's world and enriches the main characters' storylines. Of course, new characters get a brief introduction, but established characters receive a more bountiful backstory. Don't be fooled by the title. This movie won't satisfy your morbid needs until the last 30 minutes. However, there is plenty of drama, fashion, and backstabbing to keep your interest. Have your wine ready for the next episode.<br/><br/>5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongnemesis.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongnemesis.jpeg"
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongmurder.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongmurder.jpeg"
     }
   },
   {
     "option":"3",
-    "mtitle":"Jailbreak Lovers",
-    "review":"Toby and her husband, Pat, are empty nesters, and Toby looks for something new to fill her time. Toby works at the local animal shelter, but a local news story inspires her. The program matches dogs with inmates to domesticate the animals so the rescue shelter can find the dogs a forever home.<br/><br/>Warden Jenkins approves the program, and Toby brings two dogs with her co-worker, Deb. Inmates Mateo and Darnell get dogs to train. As Toby gives training instructions, John walks over. He wants to join the program too. John is in jail for first-degree felony murder and robbery. He believes training dogs will bring him purpose behind bars. John gets approval, and Toby gives him a dog. After a few weeks, Mateo wants his sister-in-law to adopt his dog so he can check up on it. However, Toby can't get Mateo's family on the phone and has to give the dog to someone else. Mateo's anger boils over, and he gets in Toby's face. John steps in between them and calms Mateo down.<br/><br/>Toby meets with Warden Jenkins and requests a guard to protect her. Jenkins tells Toby that they are short-staffed and don't have the funding. John offers Toby protection and assistance bringing items from her van. Toby falls in love with John and gives him special treatment. Will she help him escape prison and leave her husband?<br/><br/>Based on the true story of Toby Dorr and John Maynard, the writers added some comedy and confessionals to this crazy love story. In the beginning, you learn that Toby lacks something in her mundane life. Helping dogs and prisoners become her mission, but you are unsure of John's motives. If you pay attention, John slowly tests her by asking for physical contact, adult pleasures, and a phone. If he can get her to bend small rules for the promise of a future, he could depend on her for more extensive favors. In honest comedic fashion, this couple's stupidity brings the story back full circle.<br/><br/>4.5 out of 5 stars.",
+    "mtitle":"Stranger Next Door",
+    "review":"Rochelle moved in with her father, Ernest, after losing her job, health, and husband, Michael. She worked on a sting operation and uncovered Michael, a cop, received dirty money. Then, someone shot her while speaking to her informant, Tyler. Now, Rochelle awaits to testify against Michael for the FBI and works in financial forensics. Rochelle calls for her father but can't find him in the house. Her neighbor, Keele, tells Rochelle to call the police, but they offer little to no help because they hold a grudge. Ernest resurfaces and doesn't understand the fuss. He was with her new neighbor, Jesse.<br/><br/>Ernest admits that he worries about Rochelle. Later, Jesse comes to the house with a misdelivered package and invites Rochelle to come out sometime. Keele coyly meets Jesse at the bar with Rochelle. Jesse starts a conversation with Rochelle, and she brings him home for a one-night stand. Rochelle discovers calm in the handsome stranger, but she doesn't know about the evil within.<br/><br/>First of all, Keele is on another level. Second of all, Rochelle is too trusting. Like Wayward, this movie gets gory. But it satisfies a bloodlust without alienating its audience. This movie follows the Lifetime format, but it's a fun watch.<br/><br/>3.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/jailbreaklovers.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/jailbreaklovers.jpeg"
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/strangernextdoor.jpg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/strangernextdoor.jpg"
     }
   },
   {
     "option":"4",
-    "mtitle":"Ice Road Killer",
-    "review":"Helen drives to pick up her daughter, Lauren, for winter break. While on the phone with Lauren's school to iron out financial issues, Helen almost hits a woman on the road. The woman, Carly, says she was desperate to get someone to stop for her. Carly needs a ride to town to meet with her family. Carly agrees and gets in Helen's car. Carly opens the glove box and spots a gun. Helen says it's for self-defense, and Carly quietly nods. Carly tries not to show Helen that she is freaking out over the gun, but Carly texts her boyfriend, Boyd, the truth. Carly wants to stop their plan, but Boyd says no.<br/><br/>Carly waits for Boyd to attack when Helen picks up Lauren, but he doesn't, and the women drive to their next stop. Carly doesn't know, but a trucker blocked Boyd's car before he could attack. Carly texts Boyd again, but the trucker messages back that Boyd is dead and she is next. Helen and Lauren ride with the vengeful trucker's next victim. And he won't hesitate to make Lauren and Helen collateral damage.<br/><br/>A lot of suspenseful events driven by stupid decisions are the best way to summarize this movie. At first, viewers will believe that Boyd and Carly are the killers, but they are the least of Helen and Lauren's worries. As he hunts down the couple, you don't sympathize with them. Also, you will have little empathy for Helen because she keeps making countless mistakes. Lauren is the only person using their brain. This movie is frustratingly captivating.<br/><br/>3 out of 5 stars.",
+    "mtitle":"Nightmare PTA Moms",
+    "review":"Gail and her husband, Rick, moved to a new town with their daughter, Chloe, for Rick's new job. When she drops Chloe off at school, Gail doesn't notice Vanessa spying on her from the bookshelf. Vanessa comes out and introduces herself as the president of the PTA. Vanessa encourages Gail to join for Chloe's education, the school, and fun.<br/><br/>At the meeting, Gail gets to know the other parents but asks about Lexie's memorial in the front office. Sonny tells Gail that Lexie was a member of the PTA and took her life after becoming PTA president, beating Vanessa. Vanessa got the position after Lexie's death. Afterward, everyone loves Gail's cookies, but no one touches Vanessa's treats. Vanessa's envy and jealousy boil over, and she wants Gail gone now.<br/><br/>It's hard to believe that this is over a Rice Krispy Treat. If Vanessa focused on her daughter, Serena, instead of being the queen bee, Gail would be just another PTA member. But Vanessa's meddling and insults raise Gail's interests. Also, Serena is a fruit from the poisonous tree. This film will feed your guilty pleasure.<br/><br/>3 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/iceroadkillers.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/iceroadkillers.jpeg"
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nightmareptamoms.jpeg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nightmareptamoms.jpeg"
     }
   },
   {
     "option":"5",
-    "mtitle":"Planning on Forever",
-    "review":"Emma presented her latest idea to her boss, Danielle. Danielle believes Emma is the perfect choice to head their up-and-coming Seattle branch. Danielle knows Emma has a break coming for Emma's sister's wedding. She tells Emma to use that time to think about the new job.<br/><br/>Liam restores bikes but dreams of working on vintage bicycles, and he is happy to be the best man at his best friend's wedding. Liam and the groom, Brett, have been friends for years. At the mixer, Miranda and Brett introduce Liam to the maid of honor, Emma. They know Emma and Liam had one date that didn't go well, but they hope the two can get along for the wedding. Liam and Emma agree to put the past behind them and get along for the wedding.<br/><br/>With six weeks until the wedding, Miranda and Brett get called to their job site for an emergency. They don't want to postpone the wedding because it falls on their 5th anniversary. So they ask Liam and Emma to be their stand-ins and plan the wedding for them. Miranda and Brett want their wedding to be a surprise. Liam and Emma work together to create Miranda and Brett's dream wedding while falling in love.<br/><br/>While the actors try their best, this movie is slow and uninteresting. Predictable movies should be funny, dramatic, or heart-wrenching to keep your attention. However, this film doesn't accomplish any of those goals. Leave this film as background noise for a busy day.<br/><br/>1.5 out of 5 stars.",
+    "mtitle":"Love on Fire",
+    "review":"Gia drives to her new home and spots a grill for free outside the firehouse. She stops to inquire about the grill, and firefighter Jim comes out to greet her. Jim tells Gia that they are replacing the old and malfunctioning grill, but it could work with some TLC.<br/><br/>Gia unpacks and hears Karen knocking on her back door. Karen is married to Jim's best friend, Mo. Gia says she moved to town for a fresh start and to build a playground. Karen mentions that she wishes it was an inclusive playground so her daughter, Julia, could play with her friends. Julia has Cerebral Palsy, and most aren't accessible for her. Gia scraps her original plans after researching the innovative play area and presents it to her boss.<br/><br/>A few days later, Jim's daughter, Lizzy, comes to Gia's house and invites Gia to the firefighters' BBQ. Jim impresses Gia with his grilling skills, but she bets she can do better. They make a grill-off bet, but Jim says Gia has to make her BBQ sauce from scratch and offers to teach her. The firefighters want a gym, but Mayor Don won't allow it because he cares about money. Gia works with Don and offers to help the firefighters. As Gia and Jim work together, a romance develops, but they deny it because both suffer from deep-seated loss. Can they work through the pain and love each other?<br/><br/>Yes, this movie follows the format, but it sparks immense feelings while opening your eyes. This film doesn't shy or laugh away the pain, but it leans into it and expresses each person's emotions in believable detail. You will get misty-eyed as Karen talks about Julia wanting to play with her friends. This movie will tug on your heartstrings and won't let go.<br/><br/>4.5 out of 5 stars.",
     "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/planningonforever.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/planningonforever.jpg"
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveonfire.jpg",
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveonfire.jpg"
     }
   }
 ];
@@ -56,50 +56,6 @@ module.exports = madeForTV;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Flowers in the Attic: The Origin: Part 2",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/flowersintheattictheorigin.jpeg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/flowersintheattictheorigin.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Keeping Up With the Joneses: The Wrong Murder",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keepingupwiththejonesesthewrongmurder.jpeg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keepingupwiththejonesesthewrongmurder.jpeg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"Stranger Next Door",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/strangernextdoor.jpg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/strangernextdoor.jpg"
-  }
-},
-{
-  "option":"4",
-  "mtitle":"Nightmare PTA Moms",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nightmareptamoms.jpeg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nightmareptamoms.jpeg"
-  }
-},
-{
-  "option":"5",
-  "mtitle":"Love on Fire",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveonfire.jpg",
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveonfire.jpg"
-  }
-}
+
 
 */
