3e2cbb62e6264baf1fdb7f68419822109a161f03
Tiffany Gray <KarateTD@yahoo.com>
2022-08-14 23:58:36 -0400

    add 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Bodies Bodies Bodies",
+    "review":"Sophie arrives at David's party with her new girlfriend, Bee. Alice jumps out of the pool and greets Sophie with open arms, but David, Emma, and Jordan keep their distance. When Sophie introduces everyone to Bee, she notices a new face in the pool. It's Alice's new boyfriend, Greg, and he is in his 40s. Sophie notices that Jordan's significant other, Max, is missing, but no one wants to talk about it.<br/><br/>The night continues while the group drinks alcohol, hits each other, and dances. Sophie recommends they play Bodies Bodies Bodies when she sees Jordan and Bee getting close. Sophie draws an X on a sheet of paper and cuts it up. The person with the X is the killer and taps people on the back. The person has to fall to the ground. Once someone discovers them, that person yells, 'Bodies, Bodies, Bodies,' and they try to deduce who the killer is amongst them.<br/><br/>During the first round, someone kills Greg. David leaves them all behind to cool off, and Greg goes to bed. After another round, Bee hears something hitting the window and turns to see. It's David with a gash on his neck. He falls to the ground and dies. They summarize that someone in the group turned the game into a reality, and there is a killer among them. But who?<br/><br/>This movie combines the erratic culture of Gen Z with a mystery. Don't be confused, because this isn't a horror or slasher film. It's a funny, introspective look at a generation of people with buzz words for their feelings with little coping skills on how to handle them. While the ending feels flat to other generations, it's the perfect exclamation point and most real conclusion for Gen Z.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bodiesbodiesbodies.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bodiesbodiesbodies.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Fall",
+    "review":"It's been 51 weeks since Becky lost her husband, Dan, in a free climbing accident, and her depression deepens. Becky's father, James, worries she is on the verge of taking her life, so he calls Becky's friend, Hunter, to help. Hunter appears on Becky's doorstep with a plan. She wants to climb the B67TB tower and spread Dan's ashes at the top.<br/><br/>They travel to the tower, and Becky starts to have a panic attack looking up, but Hunter calms her down. They start their ascent up the ladder surrounded by a metal cage. They continue to climb and make it to the top. Then, Becky grabs Dan's ashes and releases them with kind words. With their mission complete, Becky locks onto the climbing rope and starts climbing down. After a few steps, the ladder breaks away and falls backward. They laugh about almost dying, and Becky prepares to descend again. However, they realize the ladder is gone, the tower has no foot holes, and their supplies fell on a satellite below. They need to find a way down before the weather, vultures, and exhaustion takes them out.<br/><br/>If you are afraid of heights, skip this movie. It doesn't shy away from looking down. Once they realize they are stuck with no way down, they use ingenuity to MacGyver solutions but run into some bad luck each step of the way. While this isn't a horror movie, don't be surprised if you look away from the screen or cover your eyes. This movie is better for the theater, but you won't want to buy it later.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fall.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fall.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Bullet Train",
     "review":"Yuichi Kimura sits near a hospital bed, where his son, Wataru, is in a coma. As Wataru's grandfather, The Elder, walks in, Yuichi reports that someone pushed Wataru off a roof. Yuichi found a note. The note states that Wataru's attacker will be on the bullet train headed to Kyoto in seat B4. As he approaches B4 with a gun, Yuichi finds a young girl sitting there. He apologizes, and she stuns him. The young girl is known as The Prince and wants to use Yuichi to set up her target, The White Death.<br/><br/>Maria's called her best snatch-and-grab man, Ladybug, for a job after The Carver fell ill. After some time with his therapist, Barry, Ladybug is ready to return to work. Maria wants Ladybug to board the bullet train and find a silver briefcase with train stickers and get off at the next stop.<br/><br/>Lemon and Tangerine are twin brothers working for The White Death. The White Death's enemies kidnapped his son, The Son, and held him for ransom. Lemon and Tangerine faked a ransom drop, killed the kidnappers, and got The Son. They will ride the bullet train and return The Son and the briefcase of money to The White Death.<br/><br/>A mobster raised The Wolf after The Wolf's mother died. Now, The Wolf is a contract killer for the mob boss. Years later, The Wolf marries his girlfriend in a white tuxedo with the mob in attendance. The Wolf raised his glass of red wine just as a waiter bumped him, spilling wine on The Wolf's tux. The Wolf left to clean his suit and got a bloody shock. All his guests, including his wife, were bleeding from their eyes and mouths. They all died. Later, someone uncovered that The Hornet poisoned The Wolf's wedding, and now The Hornet will be on the bullet train. These killers are on the same train with a different mission. However, they will collide with a Fiji bottle, a barrette, a sticker, and one unfortunate incident. Oh, and there is a venomous snake loose on the train.<br/><br/>If you miss one second, go home. Based on Bullet Train by Kōtarō Isaka, this movie has surprises throughout the plot that will make you gasp, laugh, and scream. Between Ladybug's therapy wisdom and Lemon's Thomas the Tank Engine obsession, the audience will find quick laughs between the bloodshed. While the story isn't linear, the director placed the flashbacks in the right place to move the story forward. There is one mid-credit scene that will bring you to a visual zen. If you love Ambulance's action, John Wick's humor, and Death on the Nile's mystery, this is the right movie for you.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"DC League of Super Pets",
     "review":"Before Kal-El's parents sent him to earth, his puppy jumped in the spacecraft with him. Years later, Kal-El is Superman, who poses as the mild-mannered Clark Kent. And his puppy is a full-grown dog called Krypto, but his alter-ego is Bark Kent. They work together to fight crime, but Krypto has a bigger enemy, Lois Lane. Lois feels that Krypto is lonely, so she suggests Superman go to the pet store and get Krypto a friend.<br/><br/>When Superman walks in, the proprietor notices one dog, Ace, escaped. Superman uses his x-ray vision to see Krypto immobilizing the escapee. Superman believes Krypto isn't ready for a companion, so he takes the pup home. Superman opens the door, but Lois isn't there. It's a guinea pig named Lulu. She shoots Superman with Kryptonite and throws him out the window. Lulu hid kryptonite in a piece of cheese that Krypto ate, so now he is defenseless. Then, Krypto recognizes Lulu.<br/><br/>Lulu was a guinea pig in Lex Luthor's lab. And as he experimented, she watched and learned. Lex studied different types of kryptonite and discovered the orange one could give him powers. Before Lex could test his theory, Superman and Krypto stopped him and freed all the guinea pigs, putting Lulu in a pet shop with Ace. However, Lulu didn't want Krypto to save her. When orange kryptonite came into orbit, Lex grabbed it, but the Justice League stopped Lex from obtaining it. It gave Lulu the power to move things with her mind. So she went to Superman's house to start step one of her plan.<br/><br/>Krypto wonders aimlessly and without powers until she sees lightning shooting from the ground to the sky. He finds Ace, PB, Merton, and Chip. Lulu didn't know, but they all got powers from the shard. Krypto thinks they should work together to stop Lulu before she destroys Superman and the world.<br/><br/>Adults, don't be surprised if you laugh more than your children at this film. The jokes aren't sexual innuendoes, so don't worry about that. This movie, based on the DC Legion of Super-Pets: The Great Mxy-UP, has the dark humor of a DC comic movie wrapped in adorable pets. The film will strike a chord with pet owners while raising awareness about pet adoption, especially the older pets. The plot is a bit of Snyder's Cut mixed with The Secret Life of Pets. Be prepared to laugh while your kids punch in the air. And there are mid and post-credit scenes.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Nope",
     "review":"Otis Haywood and Otis Jr. prepare their horse, Ghost, for Ghost's next film role. During training, debris falls from the sky and gravely wounds Otis. Otis Jr., also known as OJ, tries to get Otis to the hospital, but it's too late.<br/><br/>Now, shy OJ has to run the business with his outspoken little sister, Emerald. While Emerald wants to be in front of the camera, she does all the talking on set. OJ takes the horse and Emerald to Jupiter's Claim, a small western-themed attraction in town. OJ sells his horse to the owner Jupe but hopes to repurchase it. Emerald can't believe OJ is selling the horses, but he doesn't want to hear her opinion because she isn't involved with the ranch. They make up over drinks and music when Emerald notices Ghost is in the arena. OJ goes to wrangle the horse but sees something moving in the clouds, causing anything electrical to go out.<br/><br/>OJ tells Emerald what he saw, and they go to the electronic store to get some cameras. The cashier, Angel, offers to help set it up as part of the store's deal. Angel sets up the cameras but becomes curious when OJ asks him to point the cameras at the sky. So he hacks in and watches. Angel notices one cloud never moves. He knows the Haywoods have a UFO or U.A.P.. With proof, Emerald calls the director, Antlers, from the shoot and asks for help to capture the Oprah shot. Emerald and OJ hope the footage will save their ranch, but they have no clue what they are dealing with in the sky.<br/><br/>Executives marked this film as a horror, but this Jordan Peele experience is more thriller and comedy, and it gives a brief history of cinema but focuses on the ranch. The script allows viewers to see the relationship between Emerald and OJ and OJ's growth. The juxtaposition between the sibling's personalities blares on screen. At times you want to cover your eyes, but Peele adds comedy to keep your eyes on the screen. The graphics on the UFO and its inner working are visually compelling. This movie has less social commentary than other works by Peele. So it won't be your favorite of his works, but it is fun to watch.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/nope.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/nope.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Where the Crawdads Sing",
-    "review":"On October 30th, 1969, in Barkley Cove, North Carolina, two boys riding their bikes along the marsh find Chase Andrews's body. They determine that Chase fell from the fire tower. They find red fiber on Chase and have a lab examine it.<br/><br/>The officers go to the bar, and everyone talks about the case. Tom, a retired lawyer, overhears the whispered commotion. There were rumors that Chase was secretly dating the 'Marsh Girl' and someone heard her threaten him.<br/><br/>The police go to see the Marsh Girl the next day. They look through the girl's things and unearth a red hat. They take her to jail while the town hurls insults. When the lab determines the hat matches the fibers found on Chase, they charge the Marsh Girl with murder.<br/><br/>Tom walks into the jail cell and introduces himself to the Marsh Girl. But he knows her as Catherine Danielle Clark, but her family calls her Kya. Kya resided alone on the marsh since she was a child. Her mother, Ma, left Kya and Kya's siblings with their abusive alcoholic father, Pa. Kya, a child then, decided to stay with him and be invisible. Tom offers to represent Kya in the murder trial but pleads for Kya to open up to him. Tom knows that the jury will use their preconceived notions against her. Tom needs to prove Kya's innocence and place in society. Or she will get the death penalty.<br/><br/>Based on Where the Crawdads Sing by Delia Owens, be ready to feel the words jump off the page. The director used the marsh, lagoon, and swamp to narrate pages of complex land descriptions. At first, the swamp looks daunting and frightening. As Kya grows into a young woman, the swamp is inviting, loving, and full of life. The mystery unfolds with flashbacks and courtroom questioning. At times, these two intertwining storylines conflict or compliment each other. It's a mystery that waits for the last 15 seconds to give you the killer and the reason.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/wherethecrawdadssing.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wherethecrawdadssing.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Paws of Fury: The Legend of Hank",
-    "review":"Ika Chu puts the finishing touches on the Shogun's palace before the Shogun arrives. There is a lot to do, but Ika Chu needs his cats to remove one eyesore blocking the perfect view. It's a small town called Kakamucho, protected by a samurai. When the samurai sees the mass amount of cat warriors heading to the town, the samurai runs away, leaving Kakamucho defenseless.<br/><br/>The citizens of Kakamucho demand the Shogun send a new samurai, and the Shogun delegates the job to Ika Chu. He sees a dog, Hank, facing execution for walking into cat territory. Hank admits to crossing the border only to receive samurai training. Ika Chu appoints Hank the new samurai because he knows the cats of Kakamucho will kill a dog on sight.<br/><br/>Ika Chu's prediction is correct, and the cats attack Hank. But they stop when Hank proves he is the new samurai. Hank meets Jimbo, a hesitant sensei with too much catnip, and convinces Jimbo to train him. Ika Chu can't fathom the cats accepting this dog, so he vows to destroy Hank and Kakamucho before Shogun arrives.<br/><br/>Loosely based on Blazing Saddles, this movie is hilarious. From beginning to end, the characters never forget it's a movie. It takes everyday activities, like tweeting, and transforms the tweet into an actual bird. This film is an animated, classic comedy you grew up watching. It's worth every penny, and you will enjoy it more than your children.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/pawsoffurythelegendofhank.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/pawsoffurythelegendofhank.jpeg"
     }
   }
 ];
@@ -56,24 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Bodies Bodies Bodies",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bodiesbodiesbodies.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bodiesbodiesbodies.jpg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Fall",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fall.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fall.jpg"
-  }
-}
+
 
 */
 
