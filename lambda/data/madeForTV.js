0af2595406cca7ad439e2eda70167efdc78fadba
Tiffany Gray <KarateTD@yahoo.com>
2022-08-18 00:28:23 -0400

    fix tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,4 +1,61 @@
 var madeForTV = [
+  {
+    "option":"1",
+    "mtitle":"In Love With My Partner's Wife",
+    "review":"Paul enters the home of his partner, Frank, and Frank's wife, Eve. Paul thanks Eve for the homemade lunch and leaves with Frank. Frank and Paul stake out Jeffery's home. Frank goes through the front while Paul enters the backyard. Paul stumbles across Jeffery and pleads with Jeffery to put down his knife to talk. Jeffery calms down and tells Paul there is a rat on the force. As they talk, Frank shoots Jeffery.<br/><br/>Paul enters Frank's house, ready for his free lunch and spirited conversation with Paul, but Paul's not there. Due to Addison's Disease, Eve has a dizzy spell, and Paul catches her, but he notices a bruise on her neck. Paul has Eve show all her bruises and learns his partner is abusive. Paul doesn't tell anyone else, but he confronts Frank at work. Frank tells Paul he will attend therapy, but that's a lie.<br/><br/>At their captain's party, Frank and Paul mingle with their co-workers. Frank takes Eve outside the bar and slaps her. Paul runs out to help Eve, punches Frank, and offers her a place to stay.<br/><br/>Paul gets a call from his captain telling him to come to work for an I.A.B. meeting. Paul wants to inform the captain about Frank's abuse. But the captain received a tip that Paul killed Jeffery in cold blood. But that's not all; Frank took Eve's pills and made it impossible to get more. With their lives on the line, Eve and Paul go on the run to clear their name.<br/><br/>Viewers will spot Frank's abusive nature when Eve puts the breakfast on the table. Frank needs constant flattery for any task, even if it's his boss or dad. Also, the movie doesn't end with the typical solution because Frank adds another wrinkle to their plan one hour into the film. So you will want to watch to see the conclusion. The film is watchable but never grabs your full attention.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/inlovewithmypartnerswife.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/inlovewithmypartnerswife.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"She Is Not Your Daughter",
+    "review":"Dr. Bob leaves his daughter, May, and wife, Liza, at the breakfast table for work. He finds a note on his car that states, 'she is not your daughter.' Bob goes to work and gets a phone call from an unknown caller. The caller tells Bob to meet at a local restaurant to discover who they are and what they want.<br/><br/>Bob goes to the café and sees Jill sitting in a booth. She regrets giving her daughter, May, to Bob and Liza. However, she threatens to tell Liza and May the truth unless Bob pays her $500,000 in 7 days. She gives Bob a phone to contact her later with more instructions.<br/><br/>With time winding down, Bob comes clean to Liza. Bob saw Liza was heartbroken after suffering a miscarriage. She gave birth later, but that baby died shortly after. While Liza was unconscious, Bob heard that Jill wanted to give her baby up for adoption. So he bought May from Jill and gave it to Liza before she woke up. Bob tells Jill that Liza knows everything and her blackmail scheme won't work. Jill turns to her sister, Faye, and explains to Faye the job is over. Faye won't take that lying down and hatches a new plan that involves kidnapping, murder, and $1,000,000.<br/><br/>Liza is the hero in this movie. She spots Bob's lies, protects May, and refuses to throw Bob in front of the bus. Also, Liza is quick on her feet and creates several stories to cover Bob's tracks with May, the police, and Bob's co-workers. While May makes an understandable mistake, she has the determination to keep fighting. This film won't be a favorite of the Stranger Danger series, but it's a compelling watch.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sheisnotyourdaughter.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sheisnotyourdaughter.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Twisted House Sitter",
+    "review":"After a public break-up with her boyfriend, Xander, Morgan decides to take an extended, tech-free vacation from her media company. In Morgan's haste, Morgan realizes that she forgot something, runs into her home, turns off the alarm, retrieves the item, and returns to the car. Her driver, Alicia, tries to make small talk, but Morgan focuses on her phone. Morgan arrives at the airport on time and hurries out of the car but forgets her keys. Alicia sees the keys in the back seat as she backs up the car. She drives to Morgan's house and lets herself in. Alicia takes a bath, cooks, and tries on Morgan's clothes. Alicia thinks she has a home, clothes, and money she deserves. When word gets back to Morgan, this inconvenience will become the house sitter from hell.<br/><br/>This movie will make you scream, 'the audacity' more than once as Alicia quickly makes herself at home in Morgan's house. Don't be fooled because it doesn't take long for Morgan to realize something is wrong and come home. At that moment, Alicia's cunning and Morgan's business sense battle it out. While the film has a great set-up, the final fight scene is comically bad.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/twistedhousesitter.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/twistedhousesitter.jpg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Big Sky River",
+    "review":"Tara takes one last horseback ride with her stepdaughter, Erin, before leaving New York for a 3-month vacation on a ranch. Erin begs Tara to stay because Erin doesn't want to deal with her dad, James, and James's new girlfriend, Bethany. Tara urges Erin to use this time to connect with Bethany and James.<br/><br/>Tara arrives in town and stops at a gas station. The sheriff, Boone, warns Tara not to leave her purse unattended. His friend tries to set Boone up on a date, but he refuses because the woman isn't ready to be a mother to his two young sons, Fletcher and Griffin.<br/><br/>Boone comes home to a welcome surprise; Tara will be his neighbor for the next three months. They start to connect, but Boone can see that Tara has one foot out the door. As Tara lets down her guard, she gets a call from Erin and later James. Erin discovers that Bethany wants to send her to summer camp and boarding school. Shortly, James calls and asks if Erin can stay on the ranch, and Tara says yes. Tara resolves that Erin's presence could pause her budding relationship. But it's just what she and Boone need to get honest with each other.<br/><br/>Based on Big Sky River by Linda Lael Miller, this movie takes a deep look into the life of a step-parent. When Tara finds her voice in Erin's life, you will cheer in your seat. Boone is the complete opposite of James. You will fall in love with this open-hearted cowboy. Sit back and enjoy.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bigskyriver.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bigskyriver.jpeg"
+    }
+  },
+  {
+    "option":"5",
+    "mtitle":"The Art of Passion",
+    "review":"Dr. Hope and her friend, Nina, attend a gallery show. They converse about the art and the exorbitant cost with a fellow patron. That patron is the artist, James, and both women are embarrassed. The next day, Hope sees a familiar face on her exam table. It's Maggie, an abuse victim, and she has bruises everywhere. Then Arnie enters the hospital and demands to see Maggie, but Hope won't back down.<br/><br/>Hope runs home and feels someone watching her. So she quickens her pace and unknowingly drops her phone. Then she jumps behind a fence and waits for the stalker to pass. James comes up and asks why she is sneaking into his backyard. James offers to help Hope because he has a cousin, Rita, that works as a detective. Hope says no and walks away from the handsome artist.<br/><br/>After not being able to find her phone, Hope gets a new one and updates her contacts. Then, she receives a message from Arnie. He has her phone and stands outside her hospital. He wants Hope to reveal his wife's location, or he will destroy her career and end her life.<br/><br/>Welcome back to 90s Lifetime movies. Luckily, Hope found a strong, sexy, and wealthy man to help her. He awakens something in Dr. Hope that she left behind for her career. This film is a sexy thriller that old-school Lifetime fans miss. And please remember to put a lock on your phone.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theartofpassion.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theartofpassion.jpeg"
+    }
+  }
+];
+
+module.exports = madeForTV;
+
+/* Rules
+*  do not user '&' use the word 'and'
+*  small image 720w x 480h (in pixels)
+*  large image 1200w x 800h (in pixels)
+*/
+
+/*
+
 {
   "option":"1",
   "mtitle":"Temptation Under the Sun",
@@ -44,18 +101,5 @@ var madeForTV = [
     "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lovegamematch.jpg"
   }
 }
-];
-
-module.exports = madeForTV;
-
-/* Rules
-*  do not user '&' use the word 'and'
-*  small image 720w x 480h (in pixels)
-*  large image 1200w x 800h (in pixels)
-*/
-
-/*
-
-
 
 */
