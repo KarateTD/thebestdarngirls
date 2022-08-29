65e27e517d68face505ff136b5cfc07e878b2284
Tiffany Gray <KarateTD@yahoo.com>
2022-08-29 02:14:15 -0400

    3 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Breaking",
+    "review":"Brian Brown-Easley stands outside Wells Fargo and thinks about his daughter and ex-wife. He walks inside and enters his information on a deposit slip. Rosa, a bank teller, calls Brian to her counter and makes small talk. He quietly asks Rosa for a piece of paper and writes a note. It reads, 'I have a bomb.' Rosa looks at her bank manager, Estel, with unbridled panic and fear in her eyes. They offer to give him money, but that's not what Brian wants. He wants Rosa and Estel to get news reporters and police on the scene. For months, Brian felt ignored and unheard. The VA and Lincoln College of Technology stole his money, and he wants the VA to pay it back.<br/><br/>Based on 'They Didn't Have to Kill Him': The Death of Lance Cpl. Brian Easley by Aaron Gell, this movie covers many topics plaguing US veterans, including homelessness, PTSD, job insecurity, and the VA.. It's painful to watch this kind man fight for the little money he had. For the soldiers that give everything for the US, it's disgusting that they have to jump through hoops to get anything back. Your heart will sink when Brian says the amount he's fighting over. Maybe, this movie will be the first step to righting some wrongs.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/breaking.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/breaking.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Invitation",
+    "review":"Evie finishes her shift for the Finding Yourself party, and her best friend, Grace, swiped some swag bags, and a DNA genealogy testing kit is inside. Evie takes the test and discovers she has a second cousin named Oliver Alexander from England. He is in New York and wants to meet her face to face.<br/><br/>Evie meets Oliver, and he explains that the Alexanders haven't heard from her side of the family since the scandal. Her great-grandmother had an affair with her great-grandfather, and she got pregnant. He ran with and raised the baby alone. Oliver says the family is having a wedding and invites Evie on an all-expenses paid trip to England. Evie isn't sure, but Oliver convinces her to go.<br/><br/>Upon arrival, Evie bumps into the staff setting up for the wedding and makes one of the women drop a box of glasses. The head butler, Mr. Fields, scolds the maid, but Evie defends her. Lord Walter walks up to Mr. Fields and explains that Evie is a guest. Evie finds the wedding host handsome and charming. Oliver explains that the Alexanders handle Walter's real estate holdings exclusively, and Walter isn't a relative of Evie. Evie can't wait to attend the wedding of the century, but she doesn't know that she will be center stage for a bloody scheme that's generations in the making.<br/><br/>Imagine if a Hallmark movie morphed into Ready or Not, and that's what you have. The trailers give you 50% of Walter's secrets. If you are a fan of classic horror, this isn't your film. The Invitation is more of a thriller than a horror. The wedding has one shocking factor, but it's all a part of Evie's plan. This film can be a good date movie because it has something for everyone.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvitation.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvitation.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Three Thousand Years of Longing",
+    "review":"Narratorlogist Alithea debarks her airplane and pushes her luggage cart through the airport before speaking at a conference. Alithea speaks to the crowd and faints when she sees a pencil-drawn man moving throughout the audience. Instead of taking it easy, Alithea goes to the local market and finds a bottle with a distinct blue swirl and burns. She buys the item and takes it to the hotel room to clean. After a couple of rubs with her toothbrush, a Djinn slinks out of the bottle and offers Alithea three wishes with ground rules. Alithea is only allowed three wishes, and Alithea can't wish for immortality or harm to others. Djinn asks for her heart's desires, and he will make them come true. Instead, she questions Djinn about his origin and other masters. This tale will explain Djinn's 3000 years of longing.<br/><br/>Based on The Djinn in the Nightingale's Eye by A.S. Byatt, this director made this film for an audience that loves fantasy and poetry. Alithea narrates the story like a fairy tale because you wouldn't believe her any other way. The graphics compliment her narration seamlessly. If you are into fantastical plots with metrical structures, you should check out this movie.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/threethousandyearsoflonging.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/threethousandyearsoflonging.jpg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Beast",
     "review":"Dr. Nate travels to a remote jungle in Africa with his two daughters, Norah and Meridith. Their mother died a year ago, so Nate wants to connect with them by showing them their mother's home. The three meet up with Martin, a childhood friend of their mother, and settle into his home.<br/><br/>On their first outing, Martin takes them to a large rock formation with a lion pride he studies. The girls watch in fear as the lion runs towards Martin, but they relax when the lion hugs him. Martin moves closer and sees a gash on one of the females' front paws. Martin wants to examine it, but she snarls a warning to stay away. Martin explains a poacher must have hurt her, plans to help later, and gets in his car. They drive to their mother's old village to find it empty. Nate goes inside and sees dozens of people slaughtered and mulled. They find a wounded man on the side of the road, and the man warns them the devil is in the jungle. Martin grabs his gun and heads into the brush to look for survivors.<br/><br/>Nate attends to the man, but he dies. Nate grabs the gun, tells his daughters to wait in the car, and goes to find Martin. Nate hears Martin screaming to get in the truck. Nate runs to the vehicle with the Beast behind him. Meridith grabs the wheel to drive away but crashes it. Nate gets a call on his walkie-talkie from Martin. Martin is severely injured but sees the lion staring at him from above. The lion is using Martin as bait. Nate swears to get Martin back and get his girls home safely. But with no car and an angry lion on the prowl, how can they survive the jungle?<br/><br/>First, THIS MOVIE DOES NOT GLORIFY POACHING. Secondly, parents beware. This film depicts disobedient children making dumb decisions and amplifying the problem. You will enjoy this film if you are a fan of Jaws or Cujo. The director increased the intensity with long tracking shots during critical moments, so you become a part of the hunt. Also, this movie discusses anti-poachers and the lion's importance to the local's economy. Yes, the script and fight scenes are farfetched, but they make for a good time in the theater. This movie won't win awards, but it's a much-needed escape with a few jump scares to get your adrenaline pumping.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Bodies Bodies Bodies",
     "review":"Sophie arrives at David's party with her new girlfriend, Bee. Alice jumps out of the pool and greets Sophie with open arms, but David, Emma, and Jordan keep their distance. When Sophie introduces everyone to Bee, she notices a new face in the pool. It's Alice's new boyfriend, Greg, and he is in his 40s. Sophie notices that Jordan's significant other, Max, is missing, but no one wants to talk about it.<br/><br/>The night continues while the group drinks alcohol, hits each other, and dances. Sophie recommends they play Bodies Bodies Bodies when she sees Jordan and Bee getting close. Sophie draws an X on a sheet of paper and cuts it up. The person with the X is the killer and taps people on the back. The person has to fall to the ground. Once someone discovers them, that person yells, 'Bodies, Bodies, Bodies,' and they try to deduce who the killer is amongst them.<br/><br/>During the first round, someone kills Greg. David leaves them all behind to cool off, and Greg goes to bed. After another round, Bee hears something hitting the window and turns to see. It's David with a gash on his neck. He falls to the ground and dies. They summarize that someone in the group turned the game into a reality, and there is a killer among them. But who?<br/><br/>This movie combines the erratic culture of Gen Z with a mystery. Don't be confused, because this isn't a horror or slasher film. It's a funny, introspective look at a generation of people with buzz words for their feelings with little coping skills on how to handle them. While the ending feels flat to other generations, it's the perfect exclamation point and most real conclusion for Gen Z.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bodiesbodiesbodies.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bodiesbodiesbodies.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Fall",
-    "review":"It's been 51 weeks since Becky lost her husband, Dan, in a free climbing accident, and her depression deepens. Becky's father, James, worries she is on the verge of taking her life, so he calls Becky's friend, Hunter, to help. Hunter appears on Becky's doorstep with a plan. She wants to climb the B67TB tower and spread Dan's ashes at the top.<br/><br/>They travel to the tower, and Becky starts to have a panic attack looking up, but Hunter calms her down. They start their ascent up the ladder surrounded by a metal cage. They continue to climb and make it to the top. Then, Becky grabs Dan's ashes and releases them with kind words. With their mission complete, Becky locks onto the climbing rope and starts climbing down. After a few steps, the ladder breaks away and falls backward. They laugh about almost dying, and Becky prepares to descend again. However, they realize the ladder is gone, the tower has no foot holes, and their supplies fell on a satellite below. They need to find a way down before the weather, vultures, and exhaustion takes them out.<br/><br/>If you are afraid of heights, skip this movie. It doesn't shy away from looking down. Once they realize they are stuck with no way down, they use ingenuity to MacGyver solutions but run into some bad luck each step of the way. While this isn't a horror movie, don't be surprised if you look away from the screen or cover your eyes. This movie is better for the theater, but you won't want to buy it later.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fall.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fall.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Bullet Train",
-    "review":"Yuichi Kimura sits near a hospital bed, where his son, Wataru, is in a coma. As Wataru's grandfather, The Elder, walks in, Yuichi reports that someone pushed Wataru off a roof. Yuichi found a note. The note states that Wataru's attacker will be on the bullet train headed to Kyoto in seat B4. As he approaches B4 with a gun, Yuichi finds a young girl sitting there. He apologizes, and she stuns him. The young girl is known as The Prince and wants to use Yuichi to set up her target, The White Death.<br/><br/>Maria's called her best snatch-and-grab man, Ladybug, for a job after The Carver fell ill. After some time with his therapist, Barry, Ladybug is ready to return to work. Maria wants Ladybug to board the bullet train and find a silver briefcase with train stickers and get off at the next stop.<br/><br/>Lemon and Tangerine are twin brothers working for The White Death. The White Death's enemies kidnapped his son, The Son, and held him for ransom. Lemon and Tangerine faked a ransom drop, killed the kidnappers, and got The Son. They will ride the bullet train and return The Son and the briefcase of money to The White Death.<br/><br/>A mobster raised The Wolf after The Wolf's mother died. Now, The Wolf is a contract killer for the mob boss. Years later, The Wolf marries his girlfriend in a white tuxedo with the mob in attendance. The Wolf raised his glass of red wine just as a waiter bumped him, spilling wine on The Wolf's tux. The Wolf left to clean his suit and got a bloody shock. All his guests, including his wife, were bleeding from their eyes and mouths. They all died. Later, someone uncovered that The Hornet poisoned The Wolf's wedding, and now The Hornet will be on the bullet train. These killers are on the same train with a different mission. However, they will collide with a Fiji bottle, a barrette, a sticker, and one unfortunate incident. Oh, and there is a venomous snake loose on the train.<br/><br/>If you miss one second, go home. Based on Bullet Train by Kōtarō Isaka, this movie has surprises throughout the plot that will make you gasp, laugh, and scream. Between Ladybug's therapy wisdom and Lemon's Thomas the Tank Engine obsession, the audience will find quick laughs between the bloodshed. While the story isn't linear, the director placed the flashbacks in the right place to move the story forward. There is one mid-credit scene that will bring you to a visual zen. If you love Ambulance's action, John Wick's humor, and Death on the Nile's mystery, this is the right movie for you.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bullettrain.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bullettrain.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"DC League of Super Pets",
-    "review":"Before Kal-El's parents sent him to earth, his puppy jumped in the spacecraft with him. Years later, Kal-El is Superman, who poses as the mild-mannered Clark Kent. And his puppy is a full-grown dog called Krypto, but his alter-ego is Bark Kent. They work together to fight crime, but Krypto has a bigger enemy, Lois Lane. Lois feels that Krypto is lonely, so she suggests Superman go to the pet store and get Krypto a friend.<br/><br/>When Superman walks in, the proprietor notices one dog, Ace, escaped. Superman uses his x-ray vision to see Krypto immobilizing the escapee. Superman believes Krypto isn't ready for a companion, so he takes the pup home. Superman opens the door, but Lois isn't there. It's a guinea pig named Lulu. She shoots Superman with Kryptonite and throws him out the window. Lulu hid kryptonite in a piece of cheese that Krypto ate, so now he is defenseless. Then, Krypto recognizes Lulu.<br/><br/>Lulu was a guinea pig in Lex Luthor's lab. And as he experimented, she watched and learned. Lex studied different types of kryptonite and discovered the orange one could give him powers. Before Lex could test his theory, Superman and Krypto stopped him and freed all the guinea pigs, putting Lulu in a pet shop with Ace. However, Lulu didn't want Krypto to save her. When orange kryptonite came into orbit, Lex grabbed it, but the Justice League stopped Lex from obtaining it. It gave Lulu the power to move things with her mind. So she went to Superman's house to start step one of her plan.<br/><br/>Krypto wonders aimlessly and without powers until she sees lightning shooting from the ground to the sky. He finds Ace, PB, Merton, and Chip. Lulu didn't know, but they all got powers from the shard. Krypto thinks they should work together to stop Lulu before she destroys Superman and the world.<br/><br/>Adults, don't be surprised if you laugh more than your children at this film. The jokes aren't sexual innuendoes, so don't worry about that. This movie, based on the DC Legion of Super-Pets: The Great Mxy-UP, has the dark humor of a DC comic movie wrapped in adorable pets. The film will strike a chord with pet owners while raising awareness about pet adoption, especially the older pets. The plot is a bit of Snyder's Cut mixed with The Secret Life of Pets. Be prepared to laugh while your kids punch in the air. And there are mid and post-credit scenes.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dcleagueofsuperpets.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dcleagueofsuperpets.jpeg"
     }
   }
 ];
@@ -56,33 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Breaking",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/breaking.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/breaking.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"The Invitation",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvitation.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvitation.jpg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"Three Thousand Years of Longing",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/threethousandyearsoflonging.jpg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/threethousandyearsoflonging.jpg"
-  }
-}
+
 
 */
 
