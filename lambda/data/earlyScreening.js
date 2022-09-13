c41d620d4ceb209579730728fa63c314a9e1b763
Tiffany Gray <KarateTD@yahoo.com>
2022-09-13 18:24:38 -0400

    add in stores

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -53,7 +53,15 @@ module.exports = earlyScreening;
 *  small image 720w x 480h (in pixels)
 *  large image 1200w x 800h (in pixels)
 
-
+{
+  "option":"1",
+  "mtitle":"Don't Worry Darling",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dontworrydarling.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dontworrydarling.jpeg"
+  }
+}
 
 
 */
