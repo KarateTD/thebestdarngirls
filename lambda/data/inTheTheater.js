da818921d90101a8470ba0733a99c5d5cca936e3
Tiffany Gray <KarateTD@yahoo.com>
2022-09-18 20:09:08 -0400

    add in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,33 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"See How They Run",
+    "review":"John Woolf bought the film rights to Agatha Christie's The Mousetrap play and picked Leo Köpernick to direct with Mervyn as the screenwriter. After the play's 100th showing, everyone gets together for a party. Leo tore up Mervyn's original script and asked for a rewrite with a new killer and ending. Then, Leo walks over to Sheila and tells her she needs to get close to him to get the lead because John promised it to Grace Kelly. John may be the producer, but the director chooses the stars. When Sheila's husband, Dicky, sees Leo getting chummy, he throws a punch at Leo. Leo lands on the cake and needs a change of clothes. Someone attacks and kills Leo while he's in the costume room, looking for a change of clean clothes. Commissioner Harold picks Inspector Stoppard and Constable Stalker to investigate the crime and find out who killed Leo.<br/><br/>This movie makes fun of the Agatha Christie formula while making fun of itself. It's charming and witty while providing a riveting mystery. However, the film hides nothing about the killer's activity from the viewer. Often the story divulges side conversations viewers never saw during the wrap-up. The viewers need to pay attention. While this mystery is comical and suspenseful, you won't want to buy it on DVD.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/seehowtheyrun.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/seehowtheyrun.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Woman King",
+    "review":"In 1823 West Africa, the Agojie of the Dahomey Kingdom returned from a fierce battle with their captured people and prisoners from Migan's tribe. King Ghezo instructs his people not to look at the Agojie. General Nanisca stands in front of trainees and tells them the rules of joining her elite warriors. Among the trainees is Nawi. Nawi's father tried to force her to marry an older man, but the man slapped her when she wouldn't speak to him. Nawi's father dragged her to Ghezo's palace and gave Nawi to Ghezo.<br/><br/>Ghezo and one of his wives sit in front of his council and discuss the future of Dahomey. Nanisca warns the king that trading prisoners into slavery for western weapons will weaken Africa. Another advisor tells the king that selling their enemies into slavery will strengthen their kingdom.<br/><br/>Migan enters Dahomey for Ghezo's tribute to the Oyo Empire. His kingdom controls the port Dahomey needs to keep the slave trade. Migan notices that the tribute is short and tells Ghezo that he wants 20 Agojie to make up for the shortfall. Instead of giving Migan 20 warriors, Nanisca declares war. Migan believes the Dahomey kingdom is getting bolder. So, he entices other tribes and western slavers to band together to bring them down. Nanisca will need to look her horrific past in the face to defeat the oncoming attack.<br/><br/>This movie is a warrior's tale with bits of history thrown into the plot. Viewers should use this film as a starting point to do more research on the Dahomey kingdom and the Oyo Empire. Within the story, viewers get revenge, love, jealousy, and excellent choreography. The astounding details on costumes, hair, and makeup are unforgettable. However, there is an audible difference between native African speakers and those from other countries. The flip in tone and dialect can be distracting.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewomanking.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewomanking.jpg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"God's Country",
+    "review":"Sandra walks out the door to attend work and sees a red pickup truck in her driveway. She leaves a note asking the owner not to park in her driveway and teaches her public speaking class. When she sees the truck again, Sandra approaches the driver, Nathan, and asks if he can park on the other side fifteen miles away. As they talk, Samuel gets out of the passenger side with a chip on his shoulder and challenges her. She tells them not to park there again and goes into the house. The next time they do, Sandra tows their truck to the other side. Then someone shoots an arrow at her front door. Sandra calls the acting Sheriff Wolf for help and prepares all the evidence into plastic Ziploc bags. Sandra deduces that Wolf knows the men. He confirms her suspicions and offers to talk to them. Sandra puts on her coat to go with him. She believes this will end the fight, but it's only begun.<br/><br/>This movie is a two-hour Fear Thy Neighbor episode. Something as small as a note becomes blood and tinder between these 3. Another aspect of this movie is the contrast between Samuel and Nathan. Nathan is a quiet, unassuming follower that sits with Sandra in the church to discuss the problem. Samuel is an angry alpha male that feels the need to put Sandra in her place. Both are cowards, but Samuel controls Nathan.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/godscountry.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/godscountry.jpeg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"After Ever Happy",
     "review":"Hardin and Tessa return to his mother's wedding reception after Christian reveal that he is Hardin's father. Hardin confronts his mother, Trish, and she quietly apologizes, but Hardin refuses to listen. Then, Tessa looks up and realizes Hardin is gone. She finds him at Trish's home, setting a fire. Tessa pleads with Hardin to stop, but he won't. Christian runs in as Hardin ignites the accelerant, snaps Hardin into reality, tells Hardin to leave, and takes the blame.<br/><br/>Tessa returns to the hotel with Hardin, but he leaves her with the airline tickets and goes to his friends' house. Tessa tracks Hardin down and finds him in the arms of another girl. Realizing he made a mistake, Hardin runs after Tessa. She is done with the back and forth and breaks things off.<br/><br/>In Washington, Tessa returns to Hardin's apartment with Landon to get her things. She notices the thermostat is on and looks around. She finds her father, Richard, dead with a needle by his side. After numerous attempts, Hardin's friend picks up, and Landon tells Hardin that Richard died. Hardin demands to speak to Tessa, but she won't talk to him. Hardin rushes to Washington to be by Tessa's side during the funeral. As things fall into place, Tessa drops three bombs on Hardin. First, she may not be able to get pregnant or carry a baby to term. Second, she decided to move to New York with Landon and apply to NYU. Third, Tessa doesn't want Hardin to come. Will they grow apart, or will pain draw Tessa and Hardin to each other again?<br/><br/>Based on the novel, After Ever Happy by Anna Todd and the sequel to After We Fell, this movie continues the roller coaster ride of this doomed romance. Tessa finally admits they only stop fighting to make love. Otherwise, they hurt each other emotionally. They are addicted to drama, trauma, and each other. Don't be surprised that the screenwriter rushed or skipped sections of the book. For fans of the book, the rumors are true. This film ends with 'to be continued.' There will be a fifth movie. No matter how frustrating this couple can be, it flows nicely with the other films.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Honk for Jesus. Save Your Soul",
     "review":"After facing a scandal, Pastor Lee-Curtis and First Lady Trinity plan to rebuild their congregation at Wander to Greater Paths Baptist Church. They want to start a grass-roots campaign to re-open their church doors on Easter Sunday. To help facilitate the re-opening, Lee-Curtis hired Anita, a filmmaker, to create a documentary about their rise. While filming, Trinity and Lee-Curtis discover their rivals, Pastor Keon and First Lady Shakura, are opening their new church location on the same day. Lee-Curtis and Trinity go to Keon's church to convince him and Shakura to change the date. But they refuse. So, Lee-Curtis decides to move up the re-opening to 5 days away, which is this Sunday. Trinity and Lee-Curtis become a two-person show in front of Anita. But behind the scenes, Trinity slowly breaks down. The church community and her family want Trinity to stand by her man to the detriment of herself, mind, body, and spirit.<br/><br/>If you watched the previews, this movie feels like a comedy. However, that feeling is only on the surface, like Trinity's emotions. She pretends to be happy and by Lee-Curtis's side when the cameras are on. However, this movie switches between documentary style and real life. You see Trinity battling with Lee-Curtis, hecklers, accusers, and family to re-open the church. Another sad reality is how the public treats Trinity and Lee-Curtis differently after the scandal. They accost, blame, and shame her but protect, love, and forgive him. Like most independent movies, you never get a conclusion. Just self-examination. No matter what side of the church you are on, this is one to watch on Peacock and discuss, but it is not for kids.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/honkforjesussaveyoursoul.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/honkforjesussaveyoursoul.jpg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Breaking",
-    "review":"Brian Brown-Easley stands outside Wells Fargo and thinks about his daughter and ex-wife. He walks inside and enters his information on a deposit slip. Rosa, a bank teller, calls Brian to her counter and makes small talk. He quietly asks Rosa for a piece of paper and writes a note. It reads, 'I have a bomb.' Rosa looks at her bank manager, Estel, with unbridled panic and fear in her eyes. They offer to give him money, but that's not what Brian wants. He wants Rosa and Estel to get news reporters and police on the scene. For months, Brian felt ignored and unheard. The VA and Lincoln College of Technology stole his money, and he wants the VA to pay it back.<br/><br/>Based on 'They Didn't Have to Kill Him': The Death of Lance Cpl. Brian Easley by Aaron Gell, this movie covers many topics plaguing US veterans, including homelessness, PTSD, job insecurity, and the VA.. It's painful to watch this kind man fight for the little money he had. For the soldiers that give everything for the US, it's disgusting that they have to jump through hoops to get anything back. Your heart will sink when Brian says the amount he's fighting over. Maybe, this movie will be the first step to righting some wrongs.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/breaking.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/breaking.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"The Invitation",
-    "review":"Evie finishes her shift for the Finding Yourself party, and her best friend, Grace, swiped some swag bags, and a DNA genealogy testing kit is inside. Evie takes the test and discovers she has a second cousin named Oliver Alexander from England. He is in New York and wants to meet her face to face.<br/><br/>Evie meets Oliver, and he explains that the Alexanders haven't heard from her side of the family since the scandal. Her great-grandmother had an affair with her great-grandfather, and she got pregnant. He ran with and raised the baby alone. Oliver says the family is having a wedding and invites Evie on an all-expenses paid trip to England. Evie isn't sure, but Oliver convinces her to go.<br/><br/>Upon arrival, Evie bumps into the staff setting up for the wedding and makes one of the women drop a box of glasses. The head butler, Mr. Fields, scolds the maid, but Evie defends her. Lord Walter walks up to Mr. Fields and explains that Evie is a guest. Evie finds the wedding host handsome and charming. Oliver explains that the Alexanders handle Walter's real estate holdings exclusively, and Walter isn't a relative of Evie. Evie can't wait to attend the wedding of the century, but she doesn't know that she will be center stage for a bloody scheme that's generations in the making.<br/><br/>Imagine if a Hallmark movie morphed into Ready or Not, and that's what you have. The trailers give you 50% of Walter's secrets. If you are a fan of classic horror, this isn't your film. The Invitation is more of a thriller than a horror. The wedding has one shocking factor, but it's all a part of Evie's plan. This film can be a good date movie because it has something for everyone.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theinvitation.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theinvitation.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Three Thousand Years of Longing",
-    "review":"Narratorlogist Alithea debarks her airplane and pushes her luggage cart through the airport before speaking at a conference. Alithea speaks to the crowd and faints when she sees a pencil-drawn man moving throughout the audience. Instead of taking it easy, Alithea goes to the local market and finds a bottle with a distinct blue swirl and burns. She buys the item and takes it to the hotel room to clean. After a couple of rubs with her toothbrush, a Djinn slinks out of the bottle and offers Alithea three wishes with ground rules. Alithea is only allowed three wishes, and Alithea can't wish for immortality or harm to others. Djinn asks for her heart's desires, and he will make them come true. Instead, she questions Djinn about his origin and other masters. This tale will explain Djinn's 3000 years of longing.<br/><br/>Based on The Djinn in the Nightingale's Eye by A.S. Byatt, this director made this film for an audience that loves fantasy and poetry. Alithea narrates the story like a fairy tale because you wouldn't believe her any other way. The graphics compliment her narration seamlessly. If you are into fantastical plots with metrical structures, you should check out this movie.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/threethousandyearsoflonging.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/threethousandyearsoflonging.jpg"
     }
   }
 ];
@@ -55,8 +55,6 @@ module.exports = inTheTheaters;
 */
 
 /*
-
-
 
 
 
