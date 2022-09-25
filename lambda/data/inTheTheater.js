0512e3ec3e96ed5092c272d6faf76989fd7b4d09
Tiffany Gray <KarateTD@yahoo.com>
2022-09-25 19:22:14 -0400

    add 1 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"On the Come Up",
+    "review":"Bri's father, Lawless, is a rap legend in Garden Heights, and she aspires to be a rapper like him. However, he died when she was a baby. Her aunt, Pooh, got Bri a spot at The Ring, a rap arena owned by DJ Hype. Bri sells candy at school to classmates to help her mom. Her friends, Malik and Sonny, believe Bri has what it takes to make it in the music industry.<br/><br/>At her first rap battle, Bri's opponent remarks about her father and mother. Bri can't shake the feeling, so she chokes and leaves The Ring. When an officer sees Bri take something out of her bag and exchange it for money, he demands to see her bookbag. Bri refuses, so the officer grabs her and slams her to the floor. Teachers say Bri is aggressive, but Jay wants to know if the SROs will suffer consequences. The principal says the SROs behavior was inexcusable, but the principal will suspend Bri for a week during the investigation.<br/><br/>Bri uses the time to write more rhymes and gets another shot at The Ring. This time, Bri defeats a locally known rapper, Milez, and Milez's father, Supreme, takes notice. Supreme managed her father until the day Lawless died, but Pooh tells Bri to stay away from Supreme. Pooh and Bri go to the parking lot on cloud nine, but their good mood dissipates when they see Pooh's friend in an altercation with a rival gang. Pooh pulls out a gun and refuses to back down, so the gang leaves.<br/><br/>Pooh and Bri return to The Ring for the next battle, but DJ Hype bans Pooh and Bri for bringing a weapon. Instead of leaving, Bri battles a rapper in the parking lot and wins. In desperation, Bri messages Supreme on Instagram. Bri gets in the booth, but producers hate her single. So she makes a song talking about the rival gang, her family, and the SROs. This song will be Bri's first hit, but she doesn't understand that it's a statement that will anger the streets, her family, and her school.<br/><br/>Based on On the Come Up by Angie Thomas, this movie is a coming-of-age story with prose. This film, at first, feels like 8 Mile, but winning the battle isn't the story's goal. Bri has to find her voice and fight through the pain. She must thrive, not just survive. Bri learns the power of song, its effect on others, and the perils of playing the role. In the end, Bri finds her voice using her pain and stands up for herself and her family.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/onthecomeup.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/onthecomeup.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"See How They Run",
     "review":"John Woolf bought the film rights to Agatha Christie's The Mousetrap play and picked Leo Köpernick to direct with Mervyn as the screenwriter. After the play's 100th showing, everyone gets together for a party. Leo tore up Mervyn's original script and asked for a rewrite with a new killer and ending. Then, Leo walks over to Sheila and tells her she needs to get close to him to get the lead because John promised it to Grace Kelly. John may be the producer, but the director chooses the stars. When Sheila's husband, Dicky, sees Leo getting chummy, he throws a punch at Leo. Leo lands on the cake and needs a change of clothes. Someone attacks and kills Leo while he's in the costume room, looking for a change of clean clothes. Commissioner Harold picks Inspector Stoppard and Constable Stalker to investigate the crime and find out who killed Leo.<br/><br/>This movie makes fun of the Agatha Christie formula while making fun of itself. It's charming and witty while providing a riveting mystery. However, the film hides nothing about the killer's activity from the viewer. Often the story divulges side conversations viewers never saw during the wrap-up. The viewers need to pay attention. While this mystery is comical and suspenseful, you won't want to buy it on DVD.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Woman King",
     "review":"In 1823 West Africa, the Agojie of the Dahomey Kingdom returned from a fierce battle with their captured people and prisoners from Migan's tribe. King Ghezo instructs his people not to look at the Agojie. General Nanisca stands in front of trainees and tells them the rules of joining her elite warriors. Among the trainees is Nawi. Nawi's father tried to force her to marry an older man, but the man slapped her when she wouldn't speak to him. Nawi's father dragged her to Ghezo's palace and gave Nawi to Ghezo.<br/><br/>Ghezo and one of his wives sit in front of his council and discuss the future of Dahomey. Nanisca warns the king that trading prisoners into slavery for western weapons will weaken Africa. Another advisor tells the king that selling their enemies into slavery will strengthen their kingdom.<br/><br/>Migan enters Dahomey for Ghezo's tribute to the Oyo Empire. His kingdom controls the port Dahomey needs to keep the slave trade. Migan notices that the tribute is short and tells Ghezo that he wants 20 Agojie to make up for the shortfall. Instead of giving Migan 20 warriors, Nanisca declares war. Migan believes the Dahomey kingdom is getting bolder. So, he entices other tribes and western slavers to band together to bring them down. Nanisca will need to look her horrific past in the face to defeat the oncoming attack.<br/><br/>This movie is a warrior's tale with bits of history thrown into the plot. Viewers should use this film as a starting point to do more research on the Dahomey kingdom and the Oyo Empire. Within the story, viewers get revenge, love, jealousy, and excellent choreography. The astounding details on costumes, hair, and makeup are unforgettable. However, there is an audible difference between native African speakers and those from other countries. The flip in tone and dialect can be distracting.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"God's Country",
     "review":"Sandra walks out the door to attend work and sees a red pickup truck in her driveway. She leaves a note asking the owner not to park in her driveway and teaches her public speaking class. When she sees the truck again, Sandra approaches the driver, Nathan, and asks if he can park on the other side fifteen miles away. As they talk, Samuel gets out of the passenger side with a chip on his shoulder and challenges her. She tells them not to park there again and goes into the house. The next time they do, Sandra tows their truck to the other side. Then someone shoots an arrow at her front door. Sandra calls the acting Sheriff Wolf for help and prepares all the evidence into plastic Ziploc bags. Sandra deduces that Wolf knows the men. He confirms her suspicions and offers to talk to them. Sandra puts on her coat to go with him. She believes this will end the fight, but it's only begun.<br/><br/>This movie is a two-hour Fear Thy Neighbor episode. Something as small as a note becomes blood and tinder between these 3. Another aspect of this movie is the contrast between Samuel and Nathan. Nathan is a quiet, unassuming follower that sits with Sandra in the church to discuss the problem. Samuel is an angry alpha male that feels the need to put Sandra in her place. Both are cowards, but Samuel controls Nathan.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"After Ever Happy",
     "review":"Hardin and Tessa return to his mother's wedding reception after Christian reveal that he is Hardin's father. Hardin confronts his mother, Trish, and she quietly apologizes, but Hardin refuses to listen. Then, Tessa looks up and realizes Hardin is gone. She finds him at Trish's home, setting a fire. Tessa pleads with Hardin to stop, but he won't. Christian runs in as Hardin ignites the accelerant, snaps Hardin into reality, tells Hardin to leave, and takes the blame.<br/><br/>Tessa returns to the hotel with Hardin, but he leaves her with the airline tickets and goes to his friends' house. Tessa tracks Hardin down and finds him in the arms of another girl. Realizing he made a mistake, Hardin runs after Tessa. She is done with the back and forth and breaks things off.<br/><br/>In Washington, Tessa returns to Hardin's apartment with Landon to get her things. She notices the thermostat is on and looks around. She finds her father, Richard, dead with a needle by his side. After numerous attempts, Hardin's friend picks up, and Landon tells Hardin that Richard died. Hardin demands to speak to Tessa, but she won't talk to him. Hardin rushes to Washington to be by Tessa's side during the funeral. As things fall into place, Tessa drops three bombs on Hardin. First, she may not be able to get pregnant or carry a baby to term. Second, she decided to move to New York with Landon and apply to NYU. Third, Tessa doesn't want Hardin to come. Will they grow apart, or will pain draw Tessa and Hardin to each other again?<br/><br/>Based on the novel, After Ever Happy by Anna Todd and the sequel to After We Fell, this movie continues the roller coaster ride of this doomed romance. Tessa finally admits they only stop fighting to make love. Otherwise, they hurt each other emotionally. They are addicted to drama, trauma, and each other. Don't be surprised that the screenwriter rushed or skipped sections of the book. For fans of the book, the rumors are true. This film ends with 'to be continued.' There will be a fifth movie. No matter how frustrating this couple can be, it flows nicely with the other films.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aftereverhappy.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aftereverhappy.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Honk for Jesus. Save Your Soul",
-    "review":"After facing a scandal, Pastor Lee-Curtis and First Lady Trinity plan to rebuild their congregation at Wander to Greater Paths Baptist Church. They want to start a grass-roots campaign to re-open their church doors on Easter Sunday. To help facilitate the re-opening, Lee-Curtis hired Anita, a filmmaker, to create a documentary about their rise. While filming, Trinity and Lee-Curtis discover their rivals, Pastor Keon and First Lady Shakura, are opening their new church location on the same day. Lee-Curtis and Trinity go to Keon's church to convince him and Shakura to change the date. But they refuse. So, Lee-Curtis decides to move up the re-opening to 5 days away, which is this Sunday. Trinity and Lee-Curtis become a two-person show in front of Anita. But behind the scenes, Trinity slowly breaks down. The church community and her family want Trinity to stand by her man to the detriment of herself, mind, body, and spirit.<br/><br/>If you watched the previews, this movie feels like a comedy. However, that feeling is only on the surface, like Trinity's emotions. She pretends to be happy and by Lee-Curtis's side when the cameras are on. However, this movie switches between documentary style and real life. You see Trinity battling with Lee-Curtis, hecklers, accusers, and family to re-open the church. Another sad reality is how the public treats Trinity and Lee-Curtis differently after the scandal. They accost, blame, and shame her but protect, love, and forgive him. Like most independent movies, you never get a conclusion. Just self-examination. No matter what side of the church you are on, this is one to watch on Peacock and discuss, but it is not for kids.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/honkforjesussaveyoursoul.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/honkforjesussaveyoursoul.jpg"
     }
   }
 ];
@@ -56,15 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"On the Come Up",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/onthecomeup.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/onthecomeup.jpeg"
-  }
-}
+
 
 */
 
