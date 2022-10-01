a84b301ba0c883544ab87c70a1b2abc4dc5b6aca
Tiffany Gray <KarateTD@yahoo.com>
2022-10-01 00:39:34 -0400

    fix numbers

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -9,7 +9,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"1",
+		"option":"2",
 		"mtitle":"Zombies 3",
 		"review":"In Seabrook, the humans, zombies, and werewolves created a community of understanding. Zed wants to be the first zombie to attend college and forge a path for all monster-kind. But first, he has to win the championship game to impress the recruiters. Before the game, Zed and his family meet the recruiters. However, an alien spacecraft hovers over Seabrook and beams extraterrestrials, Alan, Aspen, and Allie, to earth. The recruiters leave without offering Zed a spot in college. And the townspeople run for cover, except for Addison. While the aliens aren't here to hurt people, they keep their mission a secret. Years ago, their scout commander crash landed while looking for a livable planet. She found it and put the coordinates in the most precious thing in Seabrook. The aliens want to discover the item, get the location, and return to their new home.<br/><br/>After some time in Seabrook, the aliens think the moonstone is what they need. The aliens ask the mothership to beam them up, but it pulls up Addison too. Addison is an alien and thinks she has found her place in the world. But she doesn't have their spark. The aliens will need the entire town to get home while helping Zed get into college for monster-kind and his relationship.<br/><br/>This third installment has a mix of E.T., Bring it On, and Guardians of the Galaxy with a Disney Channel Soundtrack. Unlike the people of Seabrook, Addison wants to welcome the aliens with open arms because she still feels alone. Nevertheless, Addison is the linchpin for all creatures in Seabrook. The film hints about environmental issues, but that's not the plot. The story is about finding the purpose in conflict and differences.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -18,7 +18,7 @@ var videoOnDemand = [
 		}
 	 },
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Being the Ricardos",
 		"review":"Lucille Ball waits for her husband, Desi Arnaz, with the morning paper in her hand.  When he comes through the door, she wants an explanation.  It shows him enjoying drinks and details a wild night out.  Desi looks at the paper and tells her it's an old picture of an event she with him attended months ago.  Lucy remembers and kisses Desi.  As they kiss, a news report comes over the radio.  A well-known T.V. star pledges her allegiance to the communist party.  Lucy is the most-popular woman or man on television.  They know the radio host is talking about Lucy.<br/><br/>Monday, Lucy and Desi go to the executives to convince them of the truth.  Lucy tells them that 20 years ago, she lived with her grandfather, who fought for the little guy.  She checked the communist box to please him.  Desi and Lucy leave the office and head to the table-read for episode 204 of I Love Lucy.  They believe the show is ok, but this will be the most trying week of their marriage, T.V. show, staff, and cast.<br/><br/>It might shock people today, but I Love Lucy was a groundbreaking and provocative show for its time.  Lucy played a dopey woman on T.V., but she was a comedic genius battling tooth and nail for her art.  However, this movie doesn't make Lucy look like an angel.  Her strong demeanor came at the cost of her writing staff and her co-stars, Vivian and Bill.  You don't need to know much about Lucy, Desi, or the show to enjoy this movie.  But you will want to watch the show afterward.<br/><br/>4.5 out of 5 stars",
 		"image":{
@@ -27,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Psych 3: This is Gus",
 		"review":"With his baby on the way, Gus wants to do the honorable thing and marry Selene with his family and friends before the birth.  However, Selene won't give Gus information to divorce her estranged husband.  The next day, Shawn goes to Jules for help doing a police search on Selene, but Jules refuses.  Then, Shawn goes to Chief Karen Vick for help.  Karen doesn't help but knowingly leaves her computer unlocked with Shawn in the room.  Shawn looks for Selene in the database and finds nothing. With Gus's track record of dating killers, Shawn wants to investigate to protect his friend.  Gus wants to prove Shawn wrong.  Will the real Selene please stand up?<br/><br/>Wait for it! This movie feeds its Psychos yet again while allowing the characters to grow.  Gus is the perfect groomzilla, with Shawn being the worst man and best friend at the same time.  Chief Lassie questions his ability to be a cop after his stroke in Psych 2.  And Karen battles with work-life balance after making it to the top.  It's great to see these characters stay grounded while their cases remain crazy.  Yes, it helps to view the other movies and TV show because they make jokes from the source material.  Also, keep watching until the post-credits because fans get a glimpse of Psych 4.<br/><br/>5 out of 5 stars.",
 		"image":{
@@ -36,7 +36,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"Red Notice",
 		"review":"Detective John Hartley is a criminal profiler with the FBI.  He is on the hunt for Nolan Booth, the most wanted thief in the world.  Interpol got a tip that Booth wants to steal Cleopatra's second egg.  When John proves the display egg is fake, he spots Booth and chases him.  Before John can get a hand on Booth, Booth slips away to his safe house.  Exactly where John is waiting for him.  John believes his work is complete until someone says the egg is fake.  John thinks it was Bishop who swapped it, but Interpol gets another tip and arrests John.  They lock John up in the same cell as Booth.  Before the fight, the captain pulls Booth and John out of their cell to talk.  The person sitting across from them is the mythical Bishop.  She has the first egg and expects to get the 2nd in 48 hours.  She wants to know the location of the 3rd egg and questions Booth.  He says he doesn't know, but Bishop knows Booth is a liar.  So Bishop leaves.  John tells Booth that if Booth helps him catch Bishop, then Booth will be the #1 thief in the world and John can clear his name.  These strange bedfellows go on a worldwide mission.  But in a world of cons, who can you trust to act like a pro.<br/><br/>Let's be honest.  Within the first 15 minutes of Booth and John on the screen, you know there is more than meets the eye.  With that aside, the comedy and comedic timing is gold.  You will be happy to watch, but once is enough.  With the success of this movie and the ending, be ready for Red Notice 2.<br/><br/>3.5 out of 5 stars.",
 		"image":{
