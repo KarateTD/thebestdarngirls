63bad8c085a2df03d42d18680632ad28a0a0e7e3
Tiffany Gray <KarateTD@yahoo.com>
2022-10-11 23:53:17 -0400

    add 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Amsterdam",
+    "review":"During World War I, Officer Harold Woodsman refuses to work with his squad leader because Harold believes he is a Jim Crow doctor that won't save him or his fellow soldiers. Their commander puts Dr. Burt Berendsen in charge of the battalion. They both get hit by shrapnel during a firefight in France, and Nurse Valerie works on them. Burt lost an eye in the firefight, so Valerie takes him to the best glass eye makers in town, Paul Canterbury and Henry Norcross. Harold and Burt can see that Paul and Henry are spies. They offer to help Paul and Henry in their missions. For a while, the three make a great team in Amsterdam. However, Burt wants to return to New York to be with Beatrice. Valerie and Harold follow, but Valerie leaves Harold with a Dear John letter days after they arrive.<br/><br/>Harold is a lawyer by 1933, and Burt is a veteran's doctor. Burt takes the cases that no one wants because the soldiers need less than reputable medication. Harold enters Burt's office and asks him to perform a back alley surgery for their captain, Senator Bill Meekins. Bill's daughter, Elizabeth, begs Burt to take the case. Elizabeth thinks someone poisoned Bill and wants Burt to perform an autopsy with Harold's forged paperwork. Elizabeth was right. When Harold and Burt try to tell her, Elizabeth runs away. Voze told Elizabeth that looking into Bill's death could get her killed. They try to get more information, but a hitman shoves Elizabeth onto the street, and a car runs her over, killing her. They go to the Voze's home and find Valerie. She is a Voze and is suffering from a nerve disorder. The three band together to find Bill and Elizabeth's killer. However, they will find a plan to bring down America.<br/><br/>Excellent movie with amazing actors. However, it is too long. You can cut an hour out of this film and get an understanding of the plot. Designers expertly select costumes, medical equipment, slang, and scenery for the age period. Viewers will leave this movie connecting the evildoer's goal and politics today. It's all about power and control, no matter who is at the top.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/amsterdam.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/amsterdam.jpg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Lyle, Lyle, Crocodile",
+    "review":"Hector sneaks into the Show Me What You Got auditions and tries to wow the judges but fails. Hector goes to the pet store and asks for an exotic and cute but cheap pet. Then, Hector hears something singing in the back. Hector finds a singing crocodile, buys him, and takes it home. Hector names the crocodile Lyle and practices with Lyle day and night. Then, Hector begs for a stage. Hector owes the producer, so Hector puts up his house as collateral. Lyle knows every word and step. But when he gets on stage for the first time, Lyle freezes. Hector leaves Lyle in his attic and finds a new show on the road.<br/><br/>Eighteen months later, Josh moves into Hector's home with his parents, Mr. and Mrs. Primm. He hears singing coming from the vent. Josh walks into the attic and sees Lyle dancing and singing. Josh screams, and Lyle jumps out of the window and into the street. Before Lyle can run again, Josh asks Lyle to help him find his home. The pair form a lasting bond while eating and singing. Mrs. and Mr. Primm discover Lyle, and Lyle instantly persuades them with his talent and caring nature.<br/><br/>Then, the Primms get a knock at the door. It's Hector. When he gave his home as collateral, he added a clause. He can live in the house 15 days out of the year. He sees Lyle has newfound confidence after singing in front of the Primms. So, Hector starts booking shows. But when animal control finds out about Lyle, can Lyle save himself, or will his talent go to waste?<br/><br/>Based on the book series by Bernard Weber, this movie is a cute, wholesome film for the family. You will dance in your seat and laugh at a crocodile out of the water. However, the songs are forgettable outside the context of the movie. The graphic designers made Lyle look authentic but not scary. Their design choices are visible when they add Lyle to the scene with real crocodiles. Younger viewers will learn about being brave and friendship.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lylelylecrocodile.jpg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lylelylecrocodile.jpg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Bro's",
     "review":"Podcaster Bobby celebrates his 1,000,000th subscriber on his channel, The 11th Brick. Also, he is the executive director of the 1st LGBTQ+ Museum in New York City. Bobby meets hook-ups on Grindr and has a cynical view of relationships. Bobby goes to a bar after spending time with friends and sees Aaron on the dance floor. Aaron is a probate lawyer that loves CrossFit but doesn't use dating apps. During the middle of their discussion, Aaron ghosts Bobby in the club, but they meet at the bar. When Aaron disappears again, Bobby finds him and confronts him. Aaron tries to give Bobby his number, but Bobby walks away.<br/><br/>After a trying day at work, Aaron text messages Bobby. They have a flirty but awkward exchange and start to date but never make it official. Bobby doesn't feel like he is in Aaron's league. Aaron feels boring next to Bobby's thought-provoking ways. Bobby and Aaron are two strong men with strong opinions but plagued with self-doubt. Can they overcome themselves to love each other?<br/><br/>This movie is a raunchy and funny rom-com that makes fun of and plays into stereotypes. Aaron and Bobby belong to an ignored community, finding their footing within the world and themselves. With that said, Bobby calls out a significant point. There is a lot of LGBTQ+ history wiped out of American history. Some only hear about Stonewall Riots, but historical events should include every aspect of the person. Also, the writers openly chuckle at Hallmark pandering to the LGBTQ+ community. This film is an easy laugh with some hot and wild scenes for the open-minded.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"On the Come Up",
     "review":"Bri's father, Lawless, is a rap legend in Garden Heights, and she aspires to be a rapper like him. However, he died when she was a baby. Her aunt, Pooh, got Bri a spot at The Ring, a rap arena owned by DJ Hype. Bri sells candy at school to classmates to help her mom. Her friends, Malik and Sonny, believe Bri has what it takes to make it in the music industry.<br/><br/>At her first rap battle, Bri's opponent remarks about her father and mother. Bri can't shake the feeling, so she chokes and leaves The Ring. When an officer sees Bri take something out of her bag and exchange it for money, he demands to see her bookbag. Bri refuses, so the officer grabs her and slams her to the floor. Teachers say Bri is aggressive, but Jay wants to know if the SROs will suffer consequences. The principal says the SROs behavior was inexcusable, but the principal will suspend Bri for a week during the investigation.<br/><br/>Bri uses the time to write more rhymes and gets another shot at The Ring. This time, Bri defeats a locally known rapper, Milez, and Milez's father, Supreme, takes notice. Supreme managed her father until the day Lawless died, but Pooh tells Bri to stay away from Supreme. Pooh and Bri go to the parking lot on cloud nine, but their good mood dissipates when they see Pooh's friend in an altercation with a rival gang. Pooh pulls out a gun and refuses to back down, so the gang leaves.<br/><br/>Pooh and Bri return to The Ring for the next battle, but DJ Hype bans Pooh and Bri for bringing a weapon. Instead of leaving, Bri battles a rapper in the parking lot and wins. In desperation, Bri messages Supreme on Instagram. Bri gets in the booth, but producers hate her single. So she makes a song talking about the rival gang, her family, and the SROs. This song will be Bri's first hit, but she doesn't understand that it's a statement that will anger the streets, her family, and her school.<br/><br/>Based on On the Come Up by Angie Thomas, this movie is a coming-of-age story with prose. This film, at first, feels like 8 Mile, but winning the battle isn't the story's goal. Bri has to find her voice and fight through the pain. She must thrive, not just survive. Bri learns the power of song, its effect on others, and the perils of playing the role. In the end, Bri finds her voice using her pain and stands up for herself and her family.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"See How They Run",
     "review":"John Woolf bought the film rights to Agatha Christie's The Mousetrap play and picked Leo Köpernick to direct with Mervyn as the screenwriter. After the play's 100th showing, everyone gets together for a party. Leo tore up Mervyn's original script and asked for a rewrite with a new killer and ending. Then, Leo walks over to Sheila and tells her she needs to get close to him to get the lead because John promised it to Grace Kelly. John may be the producer, but the director chooses the stars. When Sheila's husband, Dicky, sees Leo getting chummy, he throws a punch at Leo. Leo lands on the cake and needs a change of clothes. Someone attacks and kills Leo while he's in the costume room, looking for a change of clean clothes. Commissioner Harold picks Inspector Stoppard and Constable Stalker to investigate the crime and find out who killed Leo.<br/><br/>This movie makes fun of the Agatha Christie formula while making fun of itself. It's charming and witty while providing a riveting mystery. However, the film hides nothing about the killer's activity from the viewer. Often the story divulges side conversations viewers never saw during the wrap-up. The viewers need to pay attention. While this mystery is comical and suspenseful, you won't want to buy it on DVD.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/seehowtheyrun.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/seehowtheyrun.jpg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"The Woman King",
-    "review":"In 1823 West Africa, the Agojie of the Dahomey Kingdom returned from a fierce battle with their captured people and prisoners from Migan's tribe. King Ghezo instructs his people not to look at the Agojie. General Nanisca stands in front of trainees and tells them the rules of joining her elite warriors. Among the trainees is Nawi. Nawi's father tried to force her to marry an older man, but the man slapped her when she wouldn't speak to him. Nawi's father dragged her to Ghezo's palace and gave Nawi to Ghezo.<br/><br/>Ghezo and one of his wives sit in front of his council and discuss the future of Dahomey. Nanisca warns the king that trading prisoners into slavery for western weapons will weaken Africa. Another advisor tells the king that selling their enemies into slavery will strengthen their kingdom.<br/><br/>Migan enters Dahomey for Ghezo's tribute to the Oyo Empire. His kingdom controls the port Dahomey needs to keep the slave trade. Migan notices that the tribute is short and tells Ghezo that he wants 20 Agojie to make up for the shortfall. Instead of giving Migan 20 warriors, Nanisca declares war. Migan believes the Dahomey kingdom is getting bolder. So, he entices other tribes and western slavers to band together to bring them down. Nanisca will need to look her horrific past in the face to defeat the oncoming attack.<br/><br/>This movie is a warrior's tale with bits of history thrown into the plot. Viewers should use this film as a starting point to do more research on the Dahomey kingdom and the Oyo Empire. Within the story, viewers get revenge, love, jealousy, and excellent choreography. The astounding details on costumes, hair, and makeup are unforgettable. However, there is an audible difference between native African speakers and those from other countries. The flip in tone and dialect can be distracting.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewomanking.jpg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewomanking.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"God's Country",
-    "review":"Sandra walks out the door to attend work and sees a red pickup truck in her driveway. She leaves a note asking the owner not to park in her driveway and teaches her public speaking class. When she sees the truck again, Sandra approaches the driver, Nathan, and asks if he can park on the other side fifteen miles away. As they talk, Samuel gets out of the passenger side with a chip on his shoulder and challenges her. She tells them not to park there again and goes into the house. The next time they do, Sandra tows their truck to the other side. Then someone shoots an arrow at her front door. Sandra calls the acting Sheriff Wolf for help and prepares all the evidence into plastic Ziploc bags. Sandra deduces that Wolf knows the men. He confirms her suspicions and offers to talk to them. Sandra puts on her coat to go with him. She believes this will end the fight, but it's only begun.<br/><br/>This movie is a two-hour Fear Thy Neighbor episode. Something as small as a note becomes blood and tinder between these 3. Another aspect of this movie is the contrast between Samuel and Nathan. Nathan is a quiet, unassuming follower that sits with Sandra in the church to discuss the problem. Samuel is an angry alpha male that feels the need to put Sandra in her place. Both are cowards, but Samuel controls Nathan.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/godscountry.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/godscountry.jpeg"
     }
   }
 ];
@@ -56,6 +56,15 @@ module.exports = inTheTheaters;
 
 /*
 
+{
+  "option":"1",
+  "mtitle":"Till",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/till.jpg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/till.jpg"
+  }
+}
 
 */
 
