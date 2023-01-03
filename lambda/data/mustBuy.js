a67419f41ad652c95770cc086eb572cf81c7b0c6
Tiffany Gray <KarateTD@yahoo.com>
2023-01-03 17:45:21 -0500

    add black adam

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,18 @@
 var mustBuy = [
   {
     "option":"1",
+    "mtitle":"Black Adam",
+    "review":"Five thousand years ago, the city of Kahndaq was the first self-governed city of its kind. However, King Anh-Kot enslaved the people and forced them to look for Eternium. He wanted to use the crystal to create the Crown of Sabbac. When a slave finds the crystal, the other people descend upon him. A boy tells them to back off because they shouldn't fight amongst themselves. The boy runs away with the Eternium and starts an uprising. The king kills the boy to make an example of him, but the gods save him and give him the power of Shazam. The boy, now Teth Adam, returns to save Kahndaq, but the king has the crown. They have a battle that destroys the once-luxurious city.<br/><br/>Today, the people of Kahndaq live under the rule of the Intergang. Amon learned from his mother, Adrianna, that Intergang pillages their resources and pollutes their land while oppressing the people. Karim waits in the truck as Adrianna, Ishmael, and Samir explore the caves. Adrianna finds the crown, but Intergang comes in with guns. With a gun to her head, Adrianna tells the troops she will surrender the crown. However, she reads the inscription on the ground and summons Teth Adam. Teth Adam kills all but one of the Intergang troops and saves Karim and Adrianna. The surviving troop loads an Eternium-powered rocket and shoots Teth Adam. It injures him, and Adrianna brings Teth Adam and the crown home.<br/><br/>Teth Adam's presence alerted Amanda Waller, who calls Hawkman to capture Teth Adam and imprison him. He called Doctor Fate, a man who can read the future and perform sorcery with the magical Helmet of Fate. Next, Hawkman reached out to Atom Smasher. The last member of the team is Cyclone, and she is a tornado with a 167 IQ. The newly-formed JSA flies to Kahndaq to stop Teth Adam. Yet, Teth Adam isn't their only problem. They have a deadlier force looking for the crown, and they are willing to die and kill for it. It is their birthright.<br/><br/>Congratulations to DC for making the first adult superhero movie viewers can see. Teth Adam is one of the deadliest superheroes or antiheroes the D.C.E.U. has produced. Most of the CGI is well done and flows naturally with the script. However, the smaller Adam is distracting because the head is comically too big for the body. For viewers, being at the beginning sets up most of the surprises. The dialogue and slow-motion scenes have great comedic timing and accurately display everyone's strengths. This movie has one mid-credit sequence but no post-credits. Also, it sets up the future of the D.C.E.U. and is worth watching.<br/><br/>5 out of 5 stars.",
+    "asin":{
+      "re":"B0BJ8FMBQP"
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackadam.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackadam.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"The Woman King",
     "review":"In 1823 West Africa, the Agojie of the Dahomey Kingdom returned from a fierce battle with their captured people and prisoners from Migan's tribe. King Ghezo instructs his people not to look at the Agojie. General Nanisca stands in front of trainees and tells them the rules of joining her elite warriors. Among the trainees is Nawi. Nawi's father tried to force her to marry an older man, but the man slapped her when she wouldn't speak to him. Nawi's father dragged her to Ghezo's palace and gave Nawi to Ghezo.<br/><br/>Ghezo and one of his wives sit in front of his council and discuss the future of Dahomey. Nanisca warns the king that trading prisoners into slavery for western weapons will weaken Africa. Another advisor tells the king that selling their enemies into slavery will strengthen their kingdom.<br/><br/>Migan enters Dahomey for Ghezo's tribute to the Oyo Empire. His kingdom controls the port Dahomey needs to keep the slave trade. Migan notices that the tribute is short and tells Ghezo that he wants 20 Agojie to make up for the shortfall. Instead of giving Migan 20 warriors, Nanisca declares war. Migan believes the Dahomey kingdom is getting bolder. So, he entices other tribes and western slavers to band together to bring them down. Nanisca will need to look her horrific past in the face to defeat the oncoming attack.<br/><br/>This movie is a warrior's tale with bits of history thrown into the plot. Viewers should use this film as a starting point to do more research on the Dahomey kingdom and the Oyo Empire. Within the story, viewers get revenge, love, jealousy, and excellent choreography. The astounding details on costumes, hair, and makeup are unforgettable. However, there is an audible difference between native African speakers and those from other countries. The flip in tone and dialect can be distracting.<br/><br/>4.5 out of 5 stars.",
     "asin":{
@@ -12,7 +24,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Gigi and Nate",
     "review":"Gigi and Nate was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
     "asin":{
@@ -24,7 +36,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Top Gun: Maverick",
     "review":"Top Gun: Maverick was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
     "asin":{
@@ -36,7 +48,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Breaking",
     "review":"Brian Brown-Easley stands outside Wells Fargo and thinks about his daughter and ex-wife. He walks inside and enters his information on a deposit slip. Rosa, a bank teller, calls Brian to her counter and makes small talk. He quietly asks Rosa for a piece of paper and writes a note. It reads, 'I have a bomb.' Rosa looks at her bank manager, Estel, with unbridled panic and fear in her eyes. They offer to give him money, but that's not what Brian wants. He wants Rosa and Estel to get news reporters and police on the scene. For months, Brian felt ignored and unheard. The VA and Lincoln College of Technology stole his money, and he wants the VA to pay it back.<br/><br/>Based on 'They Didn't Have to Kill Him': The Death of Lance Cpl. Brian Easley by Aaron Gell, this movie covers many topics plaguing US veterans, including homelessness, PTSD, job insecurity, and the VA.. It's painful to watch this kind man fight for the little money he had. For the soldiers that give everything for the US, it's disgusting that they have to jump through hoops to get anything back. Your heart will sink when Brian says the amount he's fighting over. Maybe, this movie will be the first step to righting some wrongs.<br/><br/>4.5 out of 5 stars.",
     "asin":{
@@ -45,18 +57,6 @@ var mustBuy = [
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/breaking.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/breaking.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Silent Twins",
-    "review":"The Silent Twins was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
-    "asin":{
-      "re":"B0BHM3XLCX"
-    },
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesilenttwins.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesilenttwins.jpeg"
     }
   }
 ];
@@ -76,7 +76,7 @@ module.exports = mustBuy;
     "mtitle":"Till",
     "review":"In Chicago, Illinois, Mamie walks into a department store with new shoes to find her son, Emmett, looking at wallets. Emmett pleads to get the wallet with a woman's picture inside. Mamie agrees to buy Emmett the wallet for his vacation to Money, Mississippi. Before he goes, Mamie warns Emmett that the rules in Mississippi are different than in Chicago. He must be on his best behavior, not anger white people, and remain small.<br/><br/>As Emmett boards the train with his Uncle Preacher and his cousins, Mamie worries because this two-week vacation is the longest they have ever been apart. In Money, Emmett plays around while his cousins work the cotton fields. They stop at a local store to hang out and get some refreshments. Emmett goes inside, buys two pieces of gum, gives the cashier, Carolyn, the money, and compliments her. Emmett's cousins see Carolyn run for her gun, so they get into their cars and drive away. Two men come into Preacher's home and threaten him at gunpoint to hand over Emmett. They load Emmett onto the flatbed and have Carolyn confirm Emmett's identity. As several men hold Emmett down, they take him away.<br/><br/>In Chicago, Mamie gets a call from Uncle Preacher saying that Emmett is missing. Then, a friend comes in because a reporter called her with the news. Police found Emmett's body in a river, and he is dead. After fighting with Mississippi, Mamie meets Emmett's body at the same train station she waved goodbye to him. She falls apart.<br/><br/>Mamie and Gene meet the mortician, and Mamie demands to see Emmett's remains. The mortician suggests making Emmett look better, but Mamie tells the mortician that Emmett is perfect. She goes outside to the awaiting media and makes a call to action. Mamie decides to have a public viewing of Emmett, so everyone will know what happened and light a fire under Mississippi to charge the men responsible. This act is the first step of Mamie becoming a civil rights activist and sharing a mother's pain for future generations.<br/><br/>This movie isn't just about Emmett's injustice. It's about the pain, scrutiny, and humiliation Mamie suffered while bravely sitting and testifying during Emmett's murder trial. The director made a bold choice not to show Emmett's murder. However, viewers get a full devastating view of the aftermath. Everything, from hair to clothes to eyewear, was spot on for the period and backed up by photographs. Listen carefully to the news reports in the background. They report about the lynching of Reverand George Washington Lee and his verdict. While Emmett is in Mississippi, you see that Mamie has a nagging feeling that clinches around her. That feeling only a parent knows. This movie will move you to tears and shock you as Mamie gets assistance from T. R. M. Howard, Medgar Evers, and Myrlie Evers and ushers in the Emmett Till Antilynching Act.<br/><br/>5 out of 5 stars.",
     "asin":{
-      "re":""
+      "re":"B0BKRYRZ8S"
     },
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/till.jpg",
@@ -110,23 +110,15 @@ module.exports = mustBuy;
     }
   }
 
-  {
-    "option":"5",
-    "mtitle":"Black Adam",
-    "review":"Five thousand years ago, the city of Kahndaq was the first self-governed city of its kind. However, King Anh-Kot enslaved the people and forced them to look for Eternium. He wanted to use the crystal to create the Crown of Sabbac. When a slave finds the crystal, the other people descend upon him. A boy tells them to back off because they shouldn't fight amongst themselves. The boy runs away with the Eternium and starts an uprising. The king kills the boy to make an example of him, but the gods save him and give him the power of Shazam. The boy, now Teth Adam, returns to save Kahndaq, but the king has the crown. They have a battle that destroys the once-luxurious city.<br/><br/>Today, the people of Kahndaq live under the rule of the Intergang. Amon learned from his mother, Adrianna, that Intergang pillages their resources and pollutes their land while oppressing the people. Karim waits in the truck as Adrianna, Ishmael, and Samir explore the caves. Adrianna finds the crown, but Intergang comes in with guns. With a gun to her head, Adrianna tells the troops she will surrender the crown. However, she reads the inscription on the ground and summons Teth Adam. Teth Adam kills all but one of the Intergang troops and saves Karim and Adrianna. The surviving troop loads an Eternium-powered rocket and shoots Teth Adam. It injures him, and Adrianna brings Teth Adam and the crown home.<br/><br/>Teth Adam's presence alerted Amanda Waller, who calls Hawkman to capture Teth Adam and imprison him. He called Doctor Fate, a man who can read the future and perform sorcery with the magical Helmet of Fate. Next, Hawkman reached out to Atom Smasher. The last member of the team is Cyclone, and she is a tornado with a 167 IQ. The newly-formed JSA flies to Kahndaq to stop Teth Adam. Yet, Teth Adam isn't their only problem. They have a deadlier force looking for the crown, and they are willing to die and kill for it. It is their birthright.<br/><br/>Congratulations to DC for making the first adult superhero movie viewers can see. Teth Adam is one of the deadliest superheroes or antiheroes the D.C.E.U. has produced. Most of the CGI is well done and flows naturally with the script. However, the smaller Adam is distracting because the head is comically too big for the body. For viewers, being at the beginning sets up most of the surprises. The dialogue and slow-motion scenes have great comedic timing and accurately display everyone's strengths. This movie has one mid-credit sequence but no post-credits. Also, it sets up the future of the D.C.E.U. and is worth watching.<br/><br/>5 out of 5 stars.",
-    "asin":{
-      "re":""
-    },
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackadam.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackadam.jpeg"
-    }
-  }
+  
 
   {
     "option":"3",
     "mtitle":"Devotion",
     "review":"Lieutenant Tom Hudner enters the locker room to greet his fellow soldiers and hears someone mumbling to themselves. Before Tom can investigate, his airmen walk in, say hello, and rib him. One soldier, Ensign Jesse Brown, is standoffish, but Tom shakes his hand. Tom attempts to get closer to Jesse, but Jesse keeps his guard up. Until Tom offers Jesse a ride home because Jesse's car broke down. Tom meets Jesse's wife, Daisy, and their daughter, Pam. Daisy invites Tom in for a beer. She worries because the U.S. government gave the airmen their first mission. They will lend air support to the Korean War. Pam makes Tom promise to watch over Jesse. A new aircraft, some backward attitudes, and heavy firefights will test Tom's devotion to his promise.<br/><br/>Based on Devotion: An Epic Story of Heroism, Friendship, and Sacrifice by Adam Makos, this movie stands out from other historical tellings because there isn't a bad guy. Although Jesse receives media attention for his race and naval status, he doesn't want that to define him. Only his dedication to the country. Tom is a true definition of an ally. He quickly points out others' wrongdoings and listens to Jesse with an ear of compassion, not pity. This movie will leave a tear in your eye and gives these men their due.<br/><br/>4 out of 5 stars.",
+    "asin":{
+      "re":"B0BMJS881K"
+    },
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/devotion.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/devotion.jpeg"
