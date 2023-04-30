fbd6e595f385a7130ff36fadd6139179cd4bd7cd
Tiffany Gray <KarateTD@yahoo.com>
2023-04-30 12:41:14 -0400

    add 2 in theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Big George Foreman: The Miraculous Story of the Once and Future Heavyweight Champion of the World",
+    "review":"Nancy moved her children to Houston, Texas's fifth ward housing community. She splits a sandwich between her four children and tells her son, George Foreman, not to fight in school. When a kid picks on George for being poor, George stands up and punches the kid in the face. That anger and resentment followed George into his teens. George walks down the street and sees a TV commercial for the Job Corps. He begs Nancy to let him join.<br/><br/>George keeps his anger in check and makes a friend, Desmond, but things change when someone steals George's Chuck Taylor sneakers. George spots a cadet wearing them and chases the person on campus. George breaks down a door, hangs the man upside down from a window, and takes the shoes off the man's feet. Doc sees George's handy work and tells George that he must leave school. George begs to stay. So Doc offers George a choice: Train to become a boxer or leave school. George agrees, gets in the ring, and learns that punching isn't enough. After training for a year, George wants fancy cars and a large home. Doc doesn't think George will be ready for the 1968 Olympics in one year and wants George to wait five years. George sets out to prove Doc and the rest of the world wrong for the next 50 years.<br/><br/>This movie is three comeback stories in one. The costume department did a tremendous job using archival photos to recreate George's look. This attention to detail cemented the era. Also, Khris brings the nuances of the heavyweight champion to life through his physical appearance and iconic voice. While fans of the champ won't learn anything new, this movie is worth a big-screen viewing based on the performances alone.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/biggeorgeforeman.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/biggeorgeforeman.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Sisu",
+    "review":"During the Lapland war, a lone gold miner finds gold nuggets in his pan and cordons the area. He digs underground and finds a mound of gold. He breaks it into pieces and puts it in his bag. On the way to the bank, he crosses Nazis with a caravan, including a tank, liquor, and captured women. The Nazis consider shooting the old miner on horseback but leave him alone because he is heading to a Nazi checkpoint. When the miner arrives, the guards find his gold and attempt to kill him. But the miner brutally murders all but one of them and leaves with most of his gold. The caravan hears the gunshots in the distance and turns around. They pull a nugget from the guard's body and decide to find the miner. Their leader, Bruno, knows the war will end soon, and the Allies will hang them for their crimes. Bruno believes the gold will help them buy their way to freedom. They catch up to the miner in a minefield and find a dog tag. They uncover that the miner is a Winter War veteran and legend, Aatami. Aatami is an unhinged, one-person death squad. One of the enslaved women, Aino, knows the tale of the immortal one. Aino knows Aatami refuses to die because he has Sisu. Bruno doesn't let Aino's warning stop him. He wants the gold at any cost.<br/><br/>This film combines John Wick with history, told in seven chapters. As a warning, be ready for gruesome violence. Don't be surprised if you must look away from the screen and cringe. Also, this movie depends heavily on the action to narrate the story. The lead character doesn't speak until the end of the film. You must be on time because the opening crawl explains the meaning of Sisu and Aatami's backstory. This film is entertaining for a one-time watch in theaters but not for the faint of heart.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sisu.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sisu.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Chevalier",
     "review":"George de Bologne Saint-Georges took his illegitimate son, Joseph, from Joseph's enslaved mother, Nanon, and brought Joseph to a Jesuit boarding school in Angoulême. Joseph, age 7, stood up and started to play his violin. The headmaster accepted Joseph into the school, and George left him there.<br/><br/>Twelve years later, Joseph battled Alexandre Picard, a fencing master, after Alexandre made derogatory comments about Joseph's mulatto background. Joseph beat Alexandre with a publicly divided audience watching. Queen Marie Antoinette stood up and named Joseph an officer of the king's bodyguard and a chevalier.<br/><br/>During a party, Joseph relays his ambitions to the queen. He wants to be the director of the Paris Opera. Singer Marie-Madeleine Guimard overhears Joseph's ambition and pulls him aside for a dance. She can put Joseph in their favor if he sleeps with her. Joseph turns down her advances, and Marie-Josephine's singing voice pulls him away. Joseph and Marie-Josephine, the wife of Marc René, talk about her voice, the opera, and his talents. Her cousin, Madam de Genlis, pulls Marie-Josephine away to make connections.<br/><br/>Later, Joseph meets with Madam de Genlis to produce his opera, but she's unsure he can pull it off. Joseph agrees to do it, but he wants Marie-Josephine to be the singer. Marie-Josephine comes to the opera house and decides to sing because Marc-René will be away for a year. Joseph is a man with the world at his feet and talent in a bow. However, Ancien Régime will take it all away because he is not one of them, merely a tourist.<br/><br/>This piece of reclaimed history has something for everyone. It starts with drama at the opera after Joseph interrupts Mozart as Mozart takes audience requests. This prelude to the revolution has bold music, stunning costumes, and meticulous detail to social etiquette. Viewers will watch Joseph navigate a tightrope of customs and rituals in French society. By pushing Joseph out, they make him a spear in the revolution. If you are not an opera fan, this film has something for you too. And the conclusion explains how Joseph's history faded away until now.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Guy Ritchie's The Covenant",
     "review":"Master Sergeant John Kinley lost his last interpreter after a checkpoint bombing and returned to base to select a new one. John learns that Ahmed is the best but has a problem with authority. John picks Ahmed. Ahmed spots a traitor in John's group, and he saves the team from an ambush.<br/><br/>John's team gets intel about an Afghan I.E.D. manufacturing station. John, Ahmed, and the platoon fight the guards but come under heavy fire. Only John and Ahmed survive and escape. However, John's injuries leave him with immense pain, a concussion, and unable to walk. He has to get John to safety 100 klicksvaway through rugged terrain.<br/><br/>John wakes up in a hospital with papers to return home to his family. After a few weeks, Sergeant Declan O'Brady comes to John's home to check on John's progress. He tells John that Ahmed, Ahmed's wife, Basira, and their three-month-old baby had to flee and hide underground. Ahmed and his family face execution if they can't get Special Immigrant Visas and a flight out. He realizes phone calls aren't enough. John must return to a warzone to save the man who risked everything to rescue him.<br/><br/>This movie is a collection of stories from interpreters who helped American troops while risking their lives and the lives of their families. America promised these brave interpreters and employees visas for their sacrifice. However, political and government red tape left many behind to suffer, run, and die. The action can be intense and cause flashbacks if you have war-related PTSD. The combat sequences are gritty, heart-pounding, and graphic. This movie is a call to action that these troops will make sure America keeps its promise: No One Left Behind.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"Renfield",
     "review":"Real estate lawyer Robert Montague Renfield helped Count Dracula purchase a castle. Renfield hoped the deal will allow him to enter upper society and provide a better life for his family. However, he becomes Dracula's familiar for centuries. While drinking humans makes Dracula stronger, Renfield draws his from bugs.<br/><br/>Renfield attends a Dependent Relationships Addiction Anonymous Group, a.k.a DRAAG, in present-day New Orleans. Renfield never speaks during these meetings but helps the members by killing their abusive partners. After the group, Renfield follows an abusive boyfriend to his lair. He and his friends stole drugs from the most feared crime family in New Orleans, the Lobos. Renfield kills everyone inside as Teddy Lobo brings a hired assassin to get payback for the theft. Renfield beheads the hired gun, and Teddy flees the scene with kilos of drugs in his car. Teddy drives to a DUI checkpoint and sees Officer Rebecca Quincy working. Frustrated with her department's unwillingness to help, Rebecca returns to the scene and finds a pen with a restaurant logo. She goes to the restaurant to see if Teddy is there committing a crime.<br/><br/>Teddy's mother, Bella, has grown tired of Teddy's antics. She doesn't believe he has what it takes to lead the family. She wants Teddy to kill Rebecca and demonstrate that he can run the Lobos.<br/><br/>Renfield brings his latest kill to Dracula, who is still healing from an attack, but Dracula is unhappy. The blood isn't innocent and pure. Then Renfields sees Dracula's plan for world domination. Renfield goes to his favorite restaurant and prays for relief as each of Dracula's requests enters, including Rebecca. Renfield walks to the door to leave but sees Teddy and his henchman running toward the entrance with guns. Renfield hides with the other patrons, and Teddy puts a gun to Rebecca's head. Rebecca's courage inspires Renfield. He eats a bug and helps Rebecca take down Teddy's gang. Renfield runs to the group and begs for guidance. He needs to focus on himself. Dracula won't let Renfield leave without a fight and will destroy everything Renfield cares about, including Rebecca, to make Renfield return and suffer.<br/><br/>Based on vampire lore, this movie is funny and gory. The movie's core is surviving unhealthy relationships. Like most abusive relationships, Dracula retaliates when Renfield attempts to leave. He attacks Renfield's self-esteem and friends. Outside the plot, this film has fun, bloody fight scenes. Don't be fooled. Rebecca doesn't sit around screaming for help. She kills and fights her way out of situations. This new twist on a classic has something for everyone. This movie is for the M3GAN and Cocaine Bear fans.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/renfield.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/renfield.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"AIR",
-    "review":"Sonny Vaccaro looks through game tapes to find the next athletic sponsor for Nike. The CEO, Phil Knight, authorized $250,000 for three players. Sonny has plenty on his shoulders because Nike's basketball division is floundering. Sonny sits in a marketing meeting with its head, Rob Strasser, and looks through the 1984 NBA draft picks. Sonny wants them to consider Michael Jordan, but the staff knows Jordan is expensive and prefers Adidas. Sonny leaves the meeting upset and looks over more college basketball tapes. While watching, he sees an Arthur Ashe commercial for tennis rackets and gets an idea. Instead of having a player put on their shoes, they design them for the player. The player and the shoe will become a brand within themselves.<br/><br/>Sonny flies to LA to talk to his friend, George Raveling, because George coached Michael Jordan in the Olympic tournament. George believes Sonny's instincts are right about the rookie but warns Sonny that Michael's parents, James and Deloris, are strongholds in their son's career. Sonny returns to Nike's headquarters and pitches his idea to Howard White. Howard signed Moses Malone to a Nike deal that remains their best acquisition. Howard admits he called Moses's mom to finalize the deal. Sonny takes George and Howard's advice and meets with Jordan's parents. After a revealing conversation with Deloris, Jordan's manager, David, calls Sonny with a flurry of insults and a Monday meeting. Sonny runs to Peter Moore and asks him to design the best shoe Nike has ever created. Sonny doesn't know, but his decision to pursue Michael Jordan will change Nike, athletic sponsorships, and Michael Jordan's finances forever.<br/><br/>A lot of care went into making this film. The director ensures this isn't a Michael Jordan movie. It's about Sonny, Deloris, and Nike. Sonny is the man behind the curtain. He understood the psychology behind the sport, its players, and its coaches. However, the driving force behind this deal was Deloris Jordan. She knew her son's worth to the game and wouldn't compromise his future. The music and technology will transport you back to 1984. Also, this film gives recognition to Peter's genius. Sneakers weren't just a fashion statement for him. They were a work of art. While the language is a bit much for younger viewers, audiences will love this film from start to finish.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/air.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/air.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Super Mario Bros Movie",
-    "review":"Mario and Luigi quit their job at Spike's Wrecking Company to start their plumbing business. They watch their new commercial in a restaurant, and Spike laughs at them. Mario comes home to a family that loves mushrooms and criticizes his career choice because Mario spent his life savings on the commercial. However, Luigi believes in Mario. During their conversation, Mario hears a news report on TV. A main water pipe burst in Brooklyn, and it's flooding. Mario decides to save Brooklyn.<br/><br/>Mario and Luigi drive to Brooklyn, but Mario can see the workers are digging in the wrong spot. Mario opens a maintenance hole cover and jumps underground with Luigi. They are pulled into a Warp Pipe but get separated along the way. Mario sees Luigi heading to darkness and fire, but he lands in the Mushroom Kingdom.<br/><br/>He attempts to touch one, and a mushroom jumps and introduces himself as Toad. Mario describes where Luigi landed, and Toad knows it well. It's the Dark Lands. Koopa King Bowser took over the area and stole the Super Star. Toad knows that Princess Peach is planning a preemptive strike to keep King Bowser's reign from spreading. So Toad takes Mario to Princess Peach to get her help.<br/><br/>After talking to her counsel, Princess Peach knows she will need an army to defeat King Bowser. The closest army is in the Jungle Kingdom, and King Cranky Kong rules it. King Cranky Kong refuses to let Princess Peach uses his army, but Mario won't take no for an answer because he wants to save Luigi. So King Cranky Kong will let Princess Peach use his army, but only if Mario can beat his son, Donkey Kong. Mario will fight with power-ups, flying fists, and a catsuit. However, they have Bowser all wrong. He doesn't want to take over the Mushroom Kingdom. He stole the star to impress and marry Princess Peach.<br/><br/>This feast for the eyes combines Super Mario Bros, Mario Kart, and Donkey Kong. It has hidden easter eggs throughout the movie for long-time gaming fans. The colors are vibrant. They teleport you to a universe you may or may not know. And video gamers, be ready for the training montage. It will make your hands twitch for the controller. The movie provides humor for kids and adults without being raunchy or sneaky. The soundtrack includes music from all genres and decades. And make sure you stay for the mid and post-credit scenes.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesupermariobrosmovie.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesupermariobrosmovie.jpeg"
     }
   }
 ];
@@ -56,24 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Big George Foreman: The Miraculous Story of the Once and Future Heavyweight Champion of the World",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/biggeorgeforeman.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/biggeorgeforeman.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"Sisu",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sisu.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sisu.jpeg"
-  }
-}
+
 
 {
   "option":"1",
