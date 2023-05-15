ff7b66ca082a1941cc44768424567941cd0d5897
Tiffany Gray <KarateTD@yahoo.com>
2023-05-14 22:16:02 -0400

    add 1 theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Hypnotic",
+    "review":"Danny Rourke finishes his counseling session and meets his partner, Nicks, for their shift. Detective Nicks tells Danny that they got an anonymous call about a bank robbery that will happen today. And the robber will take box number 23. Danny gets in the surveillance van, and a man sitting on a bench catches Danny's eye. The man, Dellrayne, gets up, talks to the guards, and enters the bank. Danny believes Dellrayne is the mastermind and the guards are part of the plan. As a commotion rumbles outside, Danny follows Dellrayne inside the bank. Danny pretends to want a safety deposit box, distracts the bank employee, and opens box 23. He finds a polaroid of his daughter, Minnie, with the words 'Find Lev Dellrayne' written at the bottom. He panics because Lyle kidnapped Minnie 4 years ago. Although the courts convicted Lyle, they never recovered Minnie.<br/><br/>Dellrayne gets the box, and Danny chases Dellrayne to a roof. Dellrayne opens the box to discover it's empty. Danny pulls the picture out of his pocket and demands to know Minnie's location. Dellrayne tells Danny's backup they have the wrong guy, and they turn their weapons at Danny. Dellrayne escapes by jumping off the roof.<br/><br/>Danny returns to the van with Detective Nicks, and Detective Nicks tells Danny that the techs traced the call to a dime-store psychic, Diane. Danny interviews Diane about Dellrayne, and she is fearful. Diane says Dellrayne is a powerful man who wants to kill her.<br/><br/>Danny takes Diane to police storage to keep Diane safe. Diane explains that she and Dellrayne are Hypnotics. Hypnotics can alter someone's mind through a psychic bandwidth and make people do or think anything they desire. A government organization called Division wanted to turn Hypnotics into weapons. However, Dellrayne became too powerful. So Division created a tool, Domino, to stop him. Dellrayne stole Domino when he escaped, hid it, and wiped his memory. Dellrayne left clues behind to find Domino and reclaim his memory. Diane believes that Minnie is the key to finding Domino. Danny tells Diane that he believes Minnie is dead. So they decide to find Domino instead. Danny can't trust anything he sees or hears in a world of hypnotics.<br/><br/>This movie quickly picks up the pace. The effects aren't mind-blowing, but they are effective. These altered moments are evident when the director removes them and exposes the bare bones behind the movie's magic. During the recap, they playback with the beginning with and without Hypnotic power, and it will leave you speechless. Also, one mid-credit scene leaves this movie open for part two. This film is fun, but you won't be in a rush to buy it later. However, it's worth the theater if you are into mind-bending thrillers.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hypnotic.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hypnotic.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Love Again",
     "review":"Mira kisses her boyfriend, John, goodbye and watches him walk across the street while waving to her. Then, a car hits and kills him in front of her eyes. Two years later, Mira's writing career is at a standstill, and her publisher threatens to sue her for her advance. Mira's sister, Suzy, convinces Mira to move out of their parent's home and with her. Later, Suzy moves in boxes from the automobile and finds one labeled John. They find a recording of Orpheus and Eurydice, a record player, and an engagement ring. Mira sobs because John never got a chance to ask her. And she puts on the engagement ring. That night, she prepares two glasses of wine and attempts a conversation with John. However, talking to air feels silly, and Mira turns to their favorite form of communication: texting.<br/><br/>Rob works at the New York Chronicle and wants to focus on his podcast. However, his boss, Richard, tells Rob to document the resurgence of Celine Dion. Rob sighs and listens to her music for preparation. Rob's co-worker, Lisa, gives Rob a company phone. Rob doesn't understand why he needs one, but Lisa and Billy urge him to take it. Rob goes home and watches a basketball game. Then, his work phone sends him a text notification from an unknown number. He reads these messages with expressions of deep longing. He wants to know more but doesn't respond. Rob has a one-on-one meeting with Celine, and she encourages her to go after the mystery woman and find love. Rob uses Mira's text to learn she will be at the opera. He waits for her and engineers a meet-cute. He decides not to tell Mira about the text, but will her heart go on when she discovers the truth (sorry for the pun)?<br/><br/>Based on the novel SMS für dich by Sofie Cramer, and an English remake of SMS für dich, this movie has the setup of a Hallmark original without the return to the small hometown. It doesn't hold back from the pain and devastation death can cause. Also, Rob became disillusioned by love when his fiancée left him one week before their wedding. Viewers know it is a matter of time before Mira discovers the truth, but you wait for the 'how.' This plot has the classic rom-com setup and ending but has a lot of heart, music, and pain. This film won't make your Vudu queue, but it's worth the matinee.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Guardians of the Galaxy Vol. 3",
     "review":"The Guardians of the Galaxy have set up their headquarters on Knowhere. Quill drinks himself into oblivion daily to forget the woman he lost twice. After Quill passes out, Nebula puts him into bed. Adam crashes into Rocket and rips through Knowhere. Adam flies away as the Guardians tend to Rocket's wounds. Quill wakes from his stooper, blames himself, and puts a medic pack on Rocket. However, it doesn't heal Rocket. It causes him to convulse. Nebula demands they take the pack off and bring Rocket to the ship.<br/><br/>Nebula links herself to Rocket's circuits and scans him. She discovers a kill switch on his heart, but they need a passkey to remove it. Nebula uncovers that Orgo Corp created the kill switch on the Orgosphere planet. Nebula knows it has a force field and calls a contact to enter the planet's atmosphere. They enter, and Quill speaks to The Ravagers because he wears their flames. However, Stakar isn't Nebula's contact; Gamora is. She is with Stakar's faction and wants 100,000 units from the Guardians to help. Nebula agrees to the terms, and the team sets on their mission to get the passkey to save Rocket's life.<br/><br/>Adam returns to his mother, Ayesha, without 89P13, and the High Evolutionary is furious. The High Evolutionary, Dr. Herbert Wyndham, reminds Ayesha of his strength because they and their birthing pods are his creation. Ayesha vows Adam will get 89P13 and ensures her promise by coming with Adam. Herbert knows exactly where 89P13 and his friends will go to save him and gives Ayesha and Adam the coordinates.<br/><br/>While in a coma, Rocket thinks about his time in a cage before Herbert started experiments on him. 89P13 was Herbert's first shining light of success. He wanted to extract Rocket's brain to study what made Rocket special. Rocket escaped, but Herbert never stopped looking for Rocket. Herbert sends Ayesha and Adam to get Rocket back but has a backup plan to guarantee he gets his ultimate prize.<br/><br/>Fans watch this ragtag bunch become a team in the first movie. In the second, diehards see them become a family. Now, prepare to watch the Guardians discover themselves. You will get eye-popping graphics, funny commentary, and assorted genres of music in this Guardians' farewell, along with a new opener dedicated to your beloved favorites. Viewers should watch Guardians of the Galaxy Volume 1, Volume 2, Avengers: Infinity Wars, and Avengers: End Game. The third installment makes jokes about the past, brings up previous events, and returns old characters for a final send-off. Every Guardian gets an end to their tale, and audiences will leave the theater knowing that the Guardians will be alright. This film has mid- and post-credits scenes. And the final message will make Marvel fans smile.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Big George Foreman: The Miraculous Story of the Once and Future Heavyweight Champion of the World",
     "review":"Nancy moved her children to Houston, Texas's fifth ward housing community. She splits a sandwich between her four children and tells her son, George Foreman, not to fight in school. When a kid picks on George for being poor, George stands up and punches the kid in the face. That anger and resentment followed George into his teens. George walks down the street and sees a TV commercial for the Job Corps. He begs Nancy to let him join.<br/><br/>George keeps his anger in check and makes a friend, Desmond, but things change when someone steals George's Chuck Taylor sneakers. George spots a cadet wearing them and chases the person on campus. George breaks down a door, hangs the man upside down from a window, and takes the shoes off the man's feet. Doc sees George's handy work and tells George that he must leave school. George begs to stay. So Doc offers George a choice: Train to become a boxer or leave school. George agrees, gets in the ring, and learns that punching isn't enough. After training for a year, George wants fancy cars and a large home. Doc doesn't think George will be ready for the 1968 Olympics in one year and wants George to wait five years. George sets out to prove Doc and the rest of the world wrong for the next 50 years.<br/><br/>This movie is three comeback stories in one. The costume department did a tremendous job using archival photos to recreate George's look. This attention to detail cemented the era. Also, Khris brings the nuances of the heavyweight champion to life through his physical appearance and iconic voice. While fans of the champ won't learn anything new, this movie is worth a big-screen viewing based on the performances alone.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Sisu",
     "review":"During the Lapland war, a lone gold miner finds gold nuggets in his pan and cordons the area. He digs underground and finds a mound of gold. He breaks it into pieces and puts it in his bag. On the way to the bank, he crosses Nazis with a caravan, including a tank, liquor, and captured women. The Nazis consider shooting the old miner on horseback but leave him alone because he is heading to a Nazi checkpoint. When the miner arrives, the guards find his gold and attempt to kill him. But the miner brutally murders all but one of them and leaves with most of his gold. The caravan hears the gunshots in the distance and turns around. They pull a nugget from the guard's body and decide to find the miner. Their leader, Bruno, knows the war will end soon, and the Allies will hang them for their crimes. Bruno believes the gold will help them buy their way to freedom. They catch up to the miner in a minefield and find a dog tag. They uncover that the miner is a Winter War veteran and legend, Aatami. Aatami is an unhinged, one-person death squad. One of the enslaved women, Aino, knows the tale of the immortal one. Aino knows Aatami refuses to die because he has Sisu. Bruno doesn't let Aino's warning stop him. He wants the gold at any cost.<br/><br/>This film combines John Wick with history, told in seven chapters. As a warning, be ready for gruesome violence. Don't be surprised if you must look away from the screen and cringe. Also, this movie depends heavily on the action to narrate the story. The lead character doesn't speak until the end of the film. You must be on time because the opening crawl explains the meaning of Sisu and Aatami's backstory. This film is entertaining for a one-time watch in theaters but not for the faint of heart.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sisu.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sisu.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Chevalier",
-    "review":"George de Bologne Saint-Georges took his illegitimate son, Joseph, from Joseph's enslaved mother, Nanon, and brought Joseph to a Jesuit boarding school in Angoulême. Joseph, age 7, stood up and started to play his violin. The headmaster accepted Joseph into the school, and George left him there.<br/><br/>Twelve years later, Joseph battled Alexandre Picard, a fencing master, after Alexandre made derogatory comments about Joseph's mulatto background. Joseph beat Alexandre with a publicly divided audience watching. Queen Marie Antoinette stood up and named Joseph an officer of the king's bodyguard and a chevalier.<br/><br/>During a party, Joseph relays his ambitions to the queen. He wants to be the director of the Paris Opera. Singer Marie-Madeleine Guimard overhears Joseph's ambition and pulls him aside for a dance. She can put Joseph in their favor if he sleeps with her. Joseph turns down her advances, and Marie-Josephine's singing voice pulls him away. Joseph and Marie-Josephine, the wife of Marc René, talk about her voice, the opera, and his talents. Her cousin, Madam de Genlis, pulls Marie-Josephine away to make connections.<br/><br/>Later, Joseph meets with Madam de Genlis to produce his opera, but she's unsure he can pull it off. Joseph agrees to do it, but he wants Marie-Josephine to be the singer. Marie-Josephine comes to the opera house and decides to sing because Marc-René will be away for a year. Joseph is a man with the world at his feet and talent in a bow. However, Ancien Régime will take it all away because he is not one of them, merely a tourist.<br/><br/>This piece of reclaimed history has something for everyone. It starts with drama at the opera after Joseph interrupts Mozart as Mozart takes audience requests. This prelude to the revolution has bold music, stunning costumes, and meticulous detail to social etiquette. Viewers will watch Joseph navigate a tightrope of customs and rituals in French society. By pushing Joseph out, they make him a spear in the revolution. If you are not an opera fan, this film has something for you too. And the conclusion explains how Joseph's history faded away until now.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/chevalier.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/chevalier.jpeg"
     }
   }
 ];
@@ -56,17 +56,17 @@ module.exports = inTheTheaters;
 
 /*
 
+
+
 {
   "option":"1",
-  "mtitle":"Hypnotic",
-  "review":"<br/><br/>* out of 5 stars.",
+  "mtitle":"Fast X",
+  "review":"<br/><br/>* out of stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/hypnotic.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/hypnotic.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fastx.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fastx.jpeg"
   }
 }
-
-
 
 
 */
