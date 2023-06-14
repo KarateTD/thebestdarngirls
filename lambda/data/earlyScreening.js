bd4e924416d0a8dab6fdd66877999ff95dd1e16d
Tiffany Gray <KarateTD@yahoo.com>
2023-06-13 23:57:35 -0400

    add earling screening

--- lambda/data/earlyScreening.js
+++ lambda/data/earlyScreening.js
@@ -1,6 +1,15 @@
 var earlyScreening = [
   {
     "option":"1",
+    "mtitle":"The Blackening",
+    "review":"Morgan and Shawn rented a cabin in the woods for a Juneteenth celebration. Shawn finds a game room as Morgan decorates for the festivities. He gets Morgan to follow him to the room, and they find a game called The Blackening. It tells them to name one black character in a horror movie that lived. If they can't in 60 seconds, one of them will die. They say the wrong answer, and Shawn gets shot with an arrow. Morgan runs, but the killer grabs her.<br/><br/>Dewayne, Allison, and Lisa travel to the cabin, but Allison and Lisa have a secret. Dewayne doesn't care about their secret as long as Lisa's ex-boyfriend, Nnamdi, doesn't come. King and Nnamdi greet them when they arrive, much to Dewayne's chagrin.<br/><br/>Shanika pulls over for gas and gets Rap Snacks from the market. She bumps into Clifton but chooses not to remember his name. He explains that his phone died on the way to the cabin, and he doesn't have his charger. Shanika begrudgingly invites Clifton to follow her. They arrive late and find the group settling into a game of spades. The lights cut off, and the seven friends look for a fuse box. They find The Blackening. Believing it's a joke, they start to play. The game tells them to answer all the questions correctly, or Morgan dies. A TV turns on to show Morgan begging for her life. They must play, but will they all survive?<br/><br/>This movie is funny from beginning to end, but don't forget the profound questions it asks. First, what does it mean to be black? And next, is there a way to be black enough? This film is the fly on the wall. However, it plays up the laughs to ease a complex conversation. While it provides a few jump scares, this movie won't give you nightmares. And there is one mid-credit scene. This movie is for you if you enjoy M3GAN and Scary Movie.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theblackening.png",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theblackening.png"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Book Club: The Next Chapter",
     "review":"Sharon, Carol, Vivian, and Diane continue their book club during the pandemic via Zoom. While some deal with technical difficulties, they encourage each other to thrive through their ups and downs. Sharon retired from the bench but now dispises requests to officiate weddings. Diane lives with Mitchell but keeps her deceased husband's ashes inside a coffee can in the closet. Carol lost her business, while her husband, Bruce, survived a heart attack. Vivian shares the grandest news when the pandemic ends, and the women can see each other face to face. Arthur asked for her hand in marriage, and they are looking for venues. As they discuss The Alchemist, Carol pulls out her college dream book. Inside, she wrote about traveling to Italy and seeing her cooking schoolmate and his meatballs again. Sharon, Vivian, and Diane all say no. As the weeks go, each woman's obligation falls to the waist side, and Arthur wants them to take the trip. So these four women go on an Italian eat, pray, shop trip with jail, robbery, and a surprise.<br/><br/>This Book Club sequel is funnier than the first. However, you should see the first movie because it doesn't bother giving you a synopsis of the first. The women's characters are well-developed, so, as the name says, this is the next chapter of their lives. It's lovely to see a healthy female friendship in a film. While you won't run to Amazon to buy it, fans of the first should see it in theaters.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var earlyScreening = [
     }
   },
   {
-  "option":"2",
+  "option":"3",
   "mtitle":"Are You There God? It's Me Margaret",
   "review":"Eleven-year-old Margaret Simon returns from summer camp to the open arms of her mother, Barbara, and grandmother, Sylvia. Margaret notices boxes sprawled across her parents' New York apartment. Sylvia blurts out the truth. Margaret and her family are moving to New Jersey. Margaret's dad, Herb, got a promotion. Barbara will be home more and help at PTA meetings like all the other moms. Margaret sighs and prepares for New Jersey by praying to God.<br/><br/>As the Simons move into their new home, a young girl, Nancy, knocks on the door. She invites Margaret to run through the sprinklers outside her home. Margaret can't find her bathing suit, so Nancy offers to let Margaret borrow one of hers. Nancy brings Margaret to her house to change and notices that Margaret hasn't started wearing a bra yet. Nancy tells Margaret to come to school without socks to meet her friends.<br/><br/>With her Achilles tendons covered in blisters, Margaret walks into her class and sits with Nancy and Nancy's friends, Gretchen and Janine. Nancy invites Margaret to their secret meeting after school, and Margaret is ecstatic. Nancy, Janine, Margaret, and Gretchen sit at a tea party and discuss the club's rules. First, everyone must wear a bra. Second, they have to tell the group when they get their period. Third, they have to keep a boy book with the name of the boys they like. Finally, they have to share the boys' names. They all agree to the terms.<br/><br/>Margaret begs Barbara to take her bra shopping and wears it to the first meeting. Nancy says they will get their period soon because they are wearing bras. Margaret runs home, gets on her knees, and prays to God to get her period. Margaret's year of womanhood will bring family strife, religious insecurities, and two minutes in heaven.<br/><br/>Based on Are You There God? It's Me, Margaret by Judy Blume, this movie makes a confusing time funny, heartfelt, and charming. Margaret's womanhood isn't her only struggle. She grows up in an interfaith family without religion because her maternal grandparents disowned Barbara for marrying a Jewish man. Margaret doesn't know what she believes but prays with joy, love, and frustration. Margaret learns about frienemies, bullying, and cute jerks within a year. If you haven't read the book, this movie will have you running for a copy. If you have read the book, this movie was long overdue and worth the wait.<br/><br/>5 out of 5 stars.",
   "image":{
@@ -18,7 +27,7 @@ var earlyScreening = [
   }
 },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Magic Mike's Last Dance",
     "review":"Mike lost his furniture business during the pandemic and stopped dancing. Now, he works as a bartender at private parties. Mike's latest job is at a charity pool party thrown by Maxandra Mendoza, formerly Rattigan. Kim, a former client, remembers Mike as he serves her a drink and tells Max about him. Max calls Mike into her home and asks how much for a dance. As a joke, Mike says $60,000. She calls his dance silly and offers $6,000 instead. To prove his dance isn't a joke, Mike pours Max a drink, rearranges the furniture, and gives her the wildest dance of her dreams. She tells him to come to London on her private plane but won't tell him why. She will pay him the $60,000 he quoted earlier if he does.<br/><br/>After a shopping trip, Max brings Mike to The Rattigan Theater. The theater's running play is Isabel Ascendant, but Max hates it because it gives an overused premise: will a woman choose love or money? Max announces the play is on hiatus for four weeks while she creates a new show with her new director, Mike. She wants something new and revolutionary.<br/><br/>As Mike and Max iron out the details, Zadie, Max's daughter, curtly walks up to them. Zadie rolls her eyes and tells Mike that Max's therapist described Max as the 'Queen of the 1st Act.' She has big ideas and loves to start them but never finishes them. Later Mike uncovers that Max is under the thumb of her soon-to-be ex-husband, Roger. He bought the theater for her when they married. She must stay on Roger's good side to keep it. Mike will direct his first play with an exotic show under Max while staying in Roger's good graces.<br/><br/>The third installment of the franchise makes fun of the factitious play while being the play. Like Isabel, Max has to decide between her estranged husband, Roger, or Mike. So, labeling the play as shallow and sexist but having the movie follow the same plot is contradicting. The final dance is entertaining, and the sexy dancers are immensely talented. And there is a sweet 10-second intermission and a cameo from your favorites. If you enjoyed the first two, see this one at the matinee price on a regular screen with simple surround sound. Anything else is a waste.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -35,15 +44,7 @@ module.exports = earlyScreening;
 *  small image 720w x 480h (in pixels)
 *  large image 1200w x 800h (in pixels)
 
-{
-  "option":"1",
-  "mtitle":"The Blackening",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theblackening.png",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theblackening.png"
-  }
-}
+
 
 {
   "option":"1",
