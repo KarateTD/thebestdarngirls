56e20a8596c6141180c814080002ab6bcde52da6
Tiffany Gray <KarateTD@yahoo.com>
2023-07-02 01:49:05 -0400

    4 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"My Professor's Guide to Murder",
+    "review":"Miles signs autographs for adoring fans after completing an in-store reading of his latest novel. His publisher begs Miles not to take time off to teach. Miles will teach a murder mystery writing class for bachelor students.<br/><br/>Ashley meets with her advisor about attending the lower-level writing class. He doesn't feel that it is appropriate because Ashley is a master's student who is close to completing her thesis, which is a novel. Miles is Ashley's favorite author, and she hopes to pick his brain about her book. Her advisor decides that Ashley should be Miles's teaching assistant. Ashley accepts the job.<br/><br/>Ashley and Miles meet before class. He demonstrates some of the aspects of writing and gives his students an assignment. He wants them to write the perfect murder. The students take this assignment with enthusiasm. But one person will take it too far and make someone's words a reality. Will Ashley be a victim, survivor, or perpetrator?<br/><br/>This movie has an overdone premise with compelling characters that make dumb decisions. The title gives away the film, and the twist is nonexistent. While the acting is well done, it can't help a dull script. Interestingly, a movie about writing a mystery doesn't have an enigma to solve.<br/><br/>2.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myprofessorsguidetomurder.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myprofessorsguidetomurder.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Sins of The Preacher's Wife",
+    "review":"Beth and Dan move to Willowvale with their daughter to open Dan's practice. Dan found a home to Beth's specifications, but she's unhappy. She feels the woods are dangerous and thinks someone is watching her. Beth goes grocery shopping, and a man warns her to leave town. Beth tells Dan about the rude man and her panic attack after. Dan tells Beth to seek therapy.<br/><br/>Beth leaves the therapy session early and almost runs into traffic. Marion snatches Beth back in the nick of time. Marion is married to the town's preacher, Jim, and runs the church's daycare center. Marion senses something is bothering Beth and encourages Beth to open up. Marion is Beth's guiding light through Beth's career, marriage, and motherhood stresses. Beth is correct to be fearful. Someone is lurking in the dark, waiting to strike.<br/><br/>This plot is one of the most disjointed in Lifetime history. The opening 20 minutes introduces Beth as a whining nag to the audience. Beth's big trauma reveal has nothing to do with the overall story and feels like a waste of time. The writer should have written this plot from Marion's perspective to give the story more depth and entertainment value. Skip this movie.<br/><br/>1.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sinsofthepreacherswife.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sinsofthepreacherswife.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Love At First Lie",
+    "review":"Vivian catches her friend, Kate, scrolling through the latest dating app. As they debate online dating, Kate matches with Walker and sets up a date. Kate meets Walker at a restaurant, and they have a stimulating conversation. Walker deals in antiquities and travels the world. Kate thinks the worldly man is the one, but she is tumbling down a dark rabbit hole of lies, money, and mayhem.<br/><br/>This plot will feel unbelievable. But if you watch true crime docu-series, you know it has happened. These love scams are well-documented in Bad Vegan, Tinder Swindler, and Love Fraud. The person has a wild story about their life. Something awful happens to them. And they desperately need money to get help. Gradually the money they need increases, the story gets more outlandish, and the victim falls deeper in love. Take this movie as a lesson about revealing too much information and multifactor authentication.<br/><br/>2 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveatfirstlie.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveatfirstlie.jpeg"
+    }
+  },
+  {
+    "option":"4",
+    "mtitle":"Make Me a Match",
+    "review":"Vivi and Neela attend a company party. They reached 20 million subscribers for the Data Match app. She and her boyfriend, Kenny, are proud results of the app's match. Kenny steps away from Vivi during the party when his phone gets a text. She sees its flirty text from a woman hidden under his boss's name. She solely blames him and ends the relationship. She avoids embarrassment by taking two months off work.<br/><br/>Vivi returns to work to see that she and Kenny are still the face of the company. Vivi is determined to figure out what went wrong with her app. She and Neela talk to the analyst, and they say only 20% of their customers have long-lasting relationships. Neela suggests Vivi hate-watch Housewives reality TV. During a commercial break, they see an ad for Raina. She is an old-fashion matchmaker with a 95% success rate. Vivi gets an idea and contacts Raina for a face-to-face meeting. Raina enters Vivi's office, and her son, Bhumesh, follows moments later. Vivi wants to shadow Raina's tactics and incorporate them into the app. Bhumesh likes the plan. He wanted Raina to go digital for years. Raina agrees to the partnership but doesn't want Vivi to observe only. She wants Vivi to become a client. Vivi relents and starts the first step to finding the love of her life: Bhumesh.<br/><br/>This typical Hallmark plot has an old-school core. Raina states that her methods seem old but prove to have long-lasting results. Viewers may consider incorporating them into their dating lives. For instance, the families meet each other. Yes, they have chaperoned dates, but it halts the honeymoon phase to delve into questions about passions, hopes, and dreams as individuals and a couple. Vivi walks into Raina's world with an open mind, respect, and carefully-worded questions. This movie will be a staple this season.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/makemeamatch.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/makemeamatch.jpeg"
+    }
+  },
+  {
+    "option":"5",
     "mtitle":"Keyshia Cole: This is My Story",
     "review":"Keyshia Cole runs home after spending time with her boyfriend, Jamal, in 1997. Her adoptive mother, Yvonne, scolds Keyshia for being out with a boy while her adoptive father, Leon, keeps his distance. She sees money in Keyshia's hands and knows what Keyshia wants to do. Keyshia leaves under a veil of threats and looks for her mother, Frankie. Later, Keyshia finds her mother and takes her to dinner to ensure Frankie spends money on food, not drugs. Keyshia returns home, but Yvonne refuses to let Keyshia back inside.<br/><br/>After months of living with her sister, Neffe, Keyshia goes to a church and asks the congregation to pray for her career. Keyshia receives a check from the state for $14,000 and decides to use it to go to Los Angeles to pursue her dream. Keyshia looks for Frankie and begs Frankie to come with her. Frankie tells Keyshia to get settled first, then send for her. Keyshia heads to Los Angeles with a dream of recording an album. Keyshia's dream will come true, but she will suffer through family issues, reality tv, anxiety, and failed relationships.<br/><br/>This movie follows Keyshia through her struggles in Los Angeles. Each time she wanted to give up, something beautiful came her way. But nothing was easy. This movie needed another hour to focus on Keyshia and her career. Like her reality tv shows, the film's plot centered on the family. This story is as much about Frankie as it is about Keyshia. However, it helps viewers gain compassion for families coping with a loved one's addiction.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/keyshiacolethisismystory.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/keyshiacolethisismystory.jpeg"
-    }
-  },
-  {
-    "option":"2",
-    "mtitle":"My Husband's Worst Mistake",
-    "review":"Amy and Kent decide to skip the dinner after a grueling real estate seminar and hit the bar. They sleep together after too many drinks and agree to keep it quiet. They could lose their jobs and spouses. They go to their homes in the evening, and Amy's husband, Brad, questions Amy. Amy lies about her whereabouts and receives a text from 'K.' Brad demands to see the message and reads it. It breaks her lie apart, and Amy tells Brad about the affair with Kent. Enraged, Brad pushes Amy down the stairs and kills her.<br/><br/>Kent goes to a sullen workplace and asks his co-worker, Miguel, what's wrong. Miguel tells Kent their boss, Jackie, emailed about Amy's death. Later, Kent finds a compromising photo of him and Amy on the windshield of his car. Kent runs to Miguel's office to confront him. Miguel blackmails Kent to give up his promotion, or Miguel will release the photo. Kent arrives home to admit his affair and the blackmailing scheme to his wife, Sarah, with Lucy unknowingly listening. Sarah decides to focus her energy on her job and Lucy. But Brad won't let that happen. Brad believes Kent should pay for Amy's death and is on the road to vengeance.<br/><br/>Once again, every character sucks in this film. Amy and Kent are pathetic for cheating. Brad is a controlling killer, and Lucy doesn't know how to stay in a child's place. Although, she does save the day. Surprisingly, this movie has an intelligent cop for 15 minutes. Sarah is the only redeemable character in this film. Viewers will want Sarah to leave this family and never return. The plot is wild, but the ending is flatter than paper. This movie is a way to kill 2 hours, but you shouldn't waste space on your DVR.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myhusbandsworstmistake.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myhusbandsworstmistake.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"The Secret Sauce",
-    "review":"Laura recommended her company, Superior, sponsor J and P's barbecue contest to her boss, Gary, months ago. Gary says J and P agreed to the sponsorship, and the conference is to introduce Laura as the spearhead. Gary walks away, and Laura jumps into a meeting room to happy dance while singing her praises. She hears someone behind her clear his throat and sees a man on the conference monitor. He introduces himself as Jim from J and P, and Laura assumes he works in marketing. Laura pleads with Jim not to tell Gary about the dance or hopeful promotion. He vows to keep her secret.<br/><br/>Gary formally introduces Laura to Jim, the CEO of J and P.. Gary wants Laura to travel to St. George and discover what made J and P and their secret sauce go viral. Uncomfortably, Jim agrees to show Laura around and learn about the contest. After disconnecting the call, Gary tells Laura to learn J and P's secret sauce recipe so they can mass produce it under the Superior brand. If they get the sauce, they will compensate Jim, his restaurant, and his future contests.<br/><br/>Jim tells his hostess, Tara, that Laura is coming to oversee the contest. Tara spots Laura outside and invites Laura to a meal. She loves it and asks for the recipe, but Jim says no because it's a family secret. Then, Laura asks Jim to put Superior's sponsorship banner outside the restaurant. Jim is apprehensive about change. Jim says he will consider Laura's idea, but she must learn to appreciate the beauty of barbecue: low and slow. Can Laura get Jim to reconsider, or will Gary have other plans for Jim and his sauce?<br/><br/>This movie starts and continues with the expected plot. A high-fashion, big-city, and job-oriented woman comes to a small town to change a stern man and his family's business. However, love changes them both for the better. The audience shouldn't give up on this movie because the resolution makes Laura a boss with the moves of a CEO. And that slight twist saves the movie.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesecretsauce.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesecretsauce.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"The Wedding Contract",
-    "review":"It's been months since teacher Rebecca tripped ad executive Adam at the park. They fell head over heels in love, and now Rebecca wants Adam to come to Shabbat Dinner to meet her family. Rebecca thinks Adam is the one and hopes he will propose. Adam comes to Sadie and Jacob's home bearing gifts for the whole family. He enjoys dinner with her family because he lost his family's traditions after his widowed mother, Dianne, remarried. Today, Dianne is divorced and focuses on her career. During dinner, Sadie and Jacob have a playful tiff, and she reminds Jacob of his agreement in their wedding contract. They explain its purpose to Adam and hope Rebecca will keep the tradition. A few days later, Adam proposes to Rebecca, and they look forward to getting married and spending their lives together. But are they ready? Planning their wedding will test their relationship, in-laws, and careers.<br/><br/>Happily ever after is only in fairy tales. Rebecca and Adam come down from their love high after meeting with the Rabbi and answering questions about their relationship. Then Dianne arrives and clashes with Sadie. Rebecca deals with the dueling mothers while Adam works. Finally, Adam gets a promotion that will force him to move to L.A. after the wedding. While this movie is hilarious, it has authentic issues in a marriage. The plot isn't the usual Hallmark fluff. It focuses on the challenges of becoming a couple maturely. The audience should watch and use the film to spark a necessary conversation.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theweddingcontract.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theweddingcontract.jpeg"
-    }
-  },  
-  {
-    "option":"5",
-    "mtitle":"Mary J. Blige's Strength of a Woman",
-    "review":"Fifteen years after Kendra left Ben in her rearview mirror, she became a successful photographer with high-profile clients. She married Kevin, settled in Chicago, and maintained her friendship with Terry and Terry's son, Miles. Kendra and Kevin want a baby, but his surgery schedule keeps Kevin busy.<br/><br/>Kendra sees Ben at the fundraiser. They try to have a civilized conversation, but Kendra gets upset when Ben says he misses her, and she leaves. Kendra settles back into her life again but receives a devastating phone call. Her father died. Kendra travels to North Carolina for the funeral. Kendra meets Ben on the back porch, and he admits he and Connie are legally separated. Kendra was the woman he wanted, then and now. Kendra refuses to listen and exits.<br/><br/>Kendra eats alone and gets a notification from her ovulation tracker. She decides to surprise Kevin with evening delight. She pulls up to the hospital and finds Kevin kissing another woman. Kendra decides to stay at Terry's house to blow off some steam. Terry tells Kendra that Kevin treats Kendra like an afterthought. Kendra believes that Kevin loves her. She wants to display her strength by holding on and making her marriage work. Kendra will learn how strong she can be when she lets go.<br/><br/>Inspired by Strength of a Woman, this film covers Kendra's growth through marriage, friendship, and loss. It doesn't take long to see Kevin's deplorable behavior towards Kendra. Unlike the first movie, Kendra is in a downward spiral of her making. She continues to make the wrong choices to appear strong through commitment, but it weakens her spirit and self-respect. Don't give up on her. Kendra rises from the ashes and leaves the door open for part 3.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/maryjbligesstrengthofawoman.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/maryjbligesstrengthofawoman.jpeg"
     }
   }
 ];
@@ -58,38 +58,20 @@ module.exports = madeForTV;
 
 {
   "option":"1",
-  "mtitle":"My Professor's Guide to Murder",
+  "mtitle":"V.C. Andrews' Dawn",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myprofessorsguidetomurder.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myprofessorsguidetomurder.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vcandrewsdawn.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vcandrewsdawn.jpeg"
   }
 },
 {
   "option":"2",
-  "mtitle":"Sins of The Preacher's Wife",
+  "mtitle":"The Kept Mistress Killer",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sinsofthepreacherswife.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sinsofthepreacherswife.jpeg"
-  }
-},
-{
-  "option":"3",
-  "mtitle":"Love At First Lie",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveatfirstlie.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveatfirstlie.jpeg"
-  }
-},
-{
-  "option":"4",
-  "mtitle":"Make Me a Match",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/makemeamatch.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/makemeamatch.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thekeptmistresskiller.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thekeptmistresskiller.jpeg"
   }
 }
 
