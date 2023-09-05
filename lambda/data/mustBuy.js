0a6128db9373b0df6ab24c6993a5247b8feb6e58
Tiffany Gray <KarateTD@yahoo.com>
2023-09-05 17:22:16 -0400

    test new pipeline

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -84,6 +84,18 @@ module.exports = mustBuy;
     }
   }
 
+    {
+    "option":"1",
+    "mtitle":"Gran Turismo",
+    "review":"Gran Turismo was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
+    "asin":{
+      "re":"B0CFDHL2QM"
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/granturismo.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/granturismo.jpeg"
+    }
+  }
 
    {
     "option":"5",
