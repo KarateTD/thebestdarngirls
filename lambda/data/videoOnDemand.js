ca8524557a09a64ce743e0cec07a0ec92c3c7da5
Tiffany Gray <KarateTD@yahoo.com>
2023-09-06 23:29:15 -0400

    bars/cake

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Sitting in Bars with Cake",
+		"review":"Socially awkward Jane takes a break from studying for her LSAT to bake a cake for her best friend, Corrine. Corrine decides to sing karaoke before dinner, so Jane takes the cake with them. Partygoers ask Jane to see her cake at the bar and marvel at its decadence. Corrine asks the bartender for a knife and shares the cake with everyone. Corrine stands back while Jane becomes the life of the party.<br/><br/>Corrine comes up with an idea to pull Jane out of her shell. They will go to one bar weekly with a different cake for a year. Then, Corrine will judge the cake by its results. The more guys Jane gets, the better the cake. Jane agrees and starts baking.<br/><br/>After weeks of successful cakes, Corrine and Jane come home to celebrate Jane's latest conquest and Corrine's promotion. But Corrine's head throbs. Then Jane hears a bang and finds Corrine having a seizure. Corrine has a brain tumor that requires chemo and radiation. Jane wants to put cake barring on hold, but Corrine makes Jane promise to keep it going. No matter what.<br/><br/>Corrine is a daring wild child but knows when to be responsible. Jane exists to serve others, not just Corrine but her parents. Jane and Corrine seem like opposites, but they find what they need from each other. This movie makes awkward moments fun and sad ones teachable. They have an honest relationship with fights, laughs, and forgiveness. They call each other out and take accountability for their actions and words. Based on Bars with Cake: Lessons and Recipes from One Year of Trying to Bake My Way to a Boyfriend by Audrey Shulman, this movie is a tear-jerker but displays a friendship worth celebrating. Oh, be sure to bring cake!<br/><br/>4.5 out of 5 stars",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sittinginbarswithcake.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sittinginbarswithcake.jpeg"
+		}
+	 },
+	{
+		"option":"2",
 		"mtitle":"Hocus Pocus 2",
 		"review":"As the town of Salem gears up for another Halloween, Becca and Izzy plan to celebrate Becca's 16th birthday with a trip to Gilbert's magic shop, some spells in the woods, and a horror movie fest. Becca, Izzy, and Cassie were best friends until Cassie started dating the popular jock, Mike.<br/><br/>Becca and Izzy visit Gilbert's magic shop and gleam at his story about the Sanderson sisters and their return in 1993. Gilbert says Becca and Izzy are his favorite customers and tempts them the buy Angelica Leaves, and because it's Becca's birthday, he gives her a candle. Becca and Izzy go to their secret place in the woods, light the candle, and say an incantation. When they hear singing in the woods, they blow the candle out. However, the flame reignites and the Sanderson sisters, Winnie, Sarah, and Mary, appear before them. But the girls tell the sisters their 40, idolize them, and want to help them. The sisters want to know what potions they use to look so young, so Izzy and Becca take the sisters to Walgreens. After looking down a few aisles, the sisters realize Izzy and Becca tricked them, so they go to their old home, now Gilbert's shop. Izzy and Becca run to the Gilbert's, and the sisters put them in a dungeon. They look around Gilbert's shop and see a re-election flyer for Mayor Jefry. Winnie can see he is a descendant of the dreadful Reverand Traske.<br/><br/>On Winnie's 16th birthday in 1653, the Reverand decided that Winnie should marry his son. Winnie refused because she was in love with Billy. He decided to take her sisters, Mary and Sarah, away, so Winnie wouldn't be a bad influence. They fought and ran into the forbidden woods. There, a witch saw them and could tell Winnie was something special. The witch gave Winnie Book and told her not to say the all-powerful spell, Magicae Maxima.<br/><br/>Today, for Winnie, living forever isn't enough. She wants to be the most powerful witch ever so she can kill Jefry and destroy Salem. Winnie uses Gilbert to get the ingredients for the potion. One ingredient is the blood of her enemy. Winnie's enemy is Mayor Jefry because he is the descendant of the Reverand, and he is Cassie's dad. Overhearing everything, Izzy and Becca must find Jefry and Cassie, stop the sisters from becoming all-powerful, and destroy them for good. Or the Sandersons will demolish Salem and Jefry's bloodline.<br/><br/>The sequel to Hocus Pocus packs all the drama, slap-stick comedy, and singing fans expect and need. The script takes the sisters out of their element and puts them into Walgreens. Fans will delight seeing them interact with the doors, adoring fans, and Roombas. Hocus Pocus focused on their youth and children, but this movie focuses on their sisterhood. A fact lost in the first movie with constant insults and gut punches. For newbies, you don't have to see the first movie to enjoy this sequel. The plot gives a brief synopsis of what happened. To the lovers of the cult classic, you will have this one in your Halloween rotation every year.<br/><br/>4.5 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Zombies 3",
 		"review":"In Seabrook, the humans, zombies, and werewolves created a community of understanding. Zed wants to be the first zombie to attend college and forge a path for all monster-kind. But first, he has to win the championship game to impress the recruiters. Before the game, Zed and his family meet the recruiters. However, an alien spacecraft hovers over Seabrook and beams extraterrestrials, Alan, Aspen, and Allie, to earth. The recruiters leave without offering Zed a spot in college. And the townspeople run for cover, except for Addison. While the aliens aren't here to hurt people, they keep their mission a secret. Years ago, their scout commander crash landed while looking for a livable planet. She found it and put the coordinates in the most precious thing in Seabrook. The aliens want to discover the item, get the location, and return to their new home.<br/><br/>After some time in Seabrook, the aliens think the moonstone is what they need. The aliens ask the mothership to beam them up, but it pulls up Addison too. Addison is an alien and thinks she has found her place in the world. But she doesn't have their spark. The aliens will need the entire town to get home while helping Zed get into college for monster-kind and his relationship.<br/><br/>This third installment has a mix of E.T., Bring it On, and Guardians of the Galaxy with a Disney Channel Soundtrack. Unlike the people of Seabrook, Addison wants to welcome the aliens with open arms because she still feels alone. Nevertheless, Addison is the linchpin for all creatures in Seabrook. The film hints about environmental issues, but that's not the plot. The story is about finding the purpose in conflict and differences.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	 },
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Being the Ricardos",
 		"review":"Lucille Ball waits for her husband, Desi Arnaz, with the morning paper in her hand.  When he comes through the door, she wants an explanation.  It shows him enjoying drinks and details a wild night out.  Desi looks at the paper and tells her it's an old picture of an event she with him attended months ago.  Lucy remembers and kisses Desi.  As they kiss, a news report comes over the radio.  A well-known T.V. star pledges her allegiance to the communist party.  Lucy is the most-popular woman or man on television.  They know the radio host is talking about Lucy.<br/><br/>Monday, Lucy and Desi go to the executives to convince them of the truth.  Lucy tells them that 20 years ago, she lived with her grandfather, who fought for the little guy.  She checked the communist box to please him.  Desi and Lucy leave the office and head to the table-read for episode 204 of I Love Lucy.  They believe the show is ok, but this will be the most trying week of their marriage, T.V. show, staff, and cast.<br/><br/>It might shock people today, but I Love Lucy was a groundbreaking and provocative show for its time.  Lucy played a dopey woman on T.V., but she was a comedic genius battling tooth and nail for her art.  However, this movie doesn't make Lucy look like an angel.  Her strong demeanor came at the cost of her writing staff and her co-stars, Vivian and Bill.  You don't need to know much about Lucy, Desi, or the show to enjoy this movie.  But you will want to watch the show afterward.<br/><br/>4.5 out of 5 stars",
 		"image":{
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"Psych 3: This is Gus",
 		"review":"With his baby on the way, Gus wants to do the honorable thing and marry Selene with his family and friends before the birth.  However, Selene won't give Gus information to divorce her estranged husband.  The next day, Shawn goes to Jules for help doing a police search on Selene, but Jules refuses.  Then, Shawn goes to Chief Karen Vick for help.  Karen doesn't help but knowingly leaves her computer unlocked with Shawn in the room.  Shawn looks for Selene in the database and finds nothing. With Gus's track record of dating killers, Shawn wants to investigate to protect his friend.  Gus wants to prove Shawn wrong.  Will the real Selene please stand up?<br/><br/>Wait for it! This movie feeds its Psychos yet again while allowing the characters to grow.  Gus is the perfect groomzilla, with Shawn being the worst man and best friend at the same time.  Chief Lassie questions his ability to be a cop after his stroke in Psych 2.  And Karen battles with work-life balance after making it to the top.  It's great to see these characters stay grounded while their cases remain crazy.  Yes, it helps to view the other movies and TV show because they make jokes from the source material.  Also, keep watching until the post-credits because fans get a glimpse of Psych 4.<br/><br/>5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/psych3thisisgus.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/psych3thisisgus.jpeg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"Red Notice",
-		"review":"Detective John Hartley is a criminal profiler with the FBI.  He is on the hunt for Nolan Booth, the most wanted thief in the world.  Interpol got a tip that Booth wants to steal Cleopatra's second egg.  When John proves the display egg is fake, he spots Booth and chases him.  Before John can get a hand on Booth, Booth slips away to his safe house.  Exactly where John is waiting for him.  John believes his work is complete until someone says the egg is fake.  John thinks it was Bishop who swapped it, but Interpol gets another tip and arrests John.  They lock John up in the same cell as Booth.  Before the fight, the captain pulls Booth and John out of their cell to talk.  The person sitting across from them is the mythical Bishop.  She has the first egg and expects to get the 2nd in 48 hours.  She wants to know the location of the 3rd egg and questions Booth.  He says he doesn't know, but Bishop knows Booth is a liar.  So Bishop leaves.  John tells Booth that if Booth helps him catch Bishop, then Booth will be the #1 thief in the world and John can clear his name.  These strange bedfellows go on a worldwide mission.  But in a world of cons, who can you trust to act like a pro.<br/><br/>Let's be honest.  Within the first 15 minutes of Booth and John on the screen, you know there is more than meets the eye.  With that aside, the comedy and comedic timing is gold.  You will be happy to watch, but once is enough.  With the success of this movie and the ending, be ready for Red Notice 2.<br/><br/>3.5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/rednotice.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/rednotice.jpeg"
 		}
 	}
 ];
@@ -56,14 +56,6 @@ module.exports = videoOnDemand;
 
  	/*
 
-	 {
-		"option":"",
-		"mtitle":"Sitting in Bars with Cake",
-		"review":"<br/><br/> * out of 5 stars",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sittinginbarswithcake.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sittinginbarswithcake.jpeg"
-		}
-	 }
+	 
 	
 	*/
