57238c8f04d1ff2611e76e60005486ba991c8d23
Tiffany Gray <KarateTD@yahoo.com>
2023-09-22 19:17:02 -0400

    expendables 4

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"The Expendables 4",
+    "review":"Barney goes to the home of Christmas's girlfriend, Gina, and patiently waits while they argue inside. Barney needs Christmas's help because he lost his lucky ring in a thumb wrestling match the night before. Christmas makes quick work of their rivals, and Barney exits with it. Then, they meet up with the crew for a new mission. Gunner and Toll Road prep the weapons, and Barney introduces the team to the new guys: Easy Day and Galgo's son, Galan. CIA agent Marsh strolls into the room after taking over for Drummer. A mercenary, Suarto Rahmat, plans to steal nuclear warheads in Libya. They think he is working with the elusive Ocelot. Ocelot is Barney's ghost. Twenty-five years ago, Barney and his team had a job to find Ocelot, but Ocelot killed Barney's entire crew. This time, Barney will return the favor.<br/><br/>After a hail of gunfire, blood, and dismemberment, Christmas has a choice. He can save Barney or get the warheads. Barney tells Christmas to get the explosives because Ocelot could use them to start World War III. Christmas disobeys the order and tries to save Barney, but Rahmat shoots Barney out of the sky and kills him. Christmas runs over to the plane and finds Barney's charred remains with Barney's signature ring.<br/><br/>The crew goes to the bar to celebrate Barney's life, and Gina meets Christmas there. She pulls him aside to talk. Christmas wants to know when they are going to avenge Barney. However, Marsh saunters in and tells Christmas he is out of the Expendables for disobeying a direct order. Also, Gina is taking Barney's place. Christmas gifts Gina his knife and asks Gina to kill Rahmat with it. Christmas wakes up in the morning, and Gina is gone. She and the team, along with Lash and Marsh, have Ocelot and Rahmat in their sight. Unfortunately, Rahmat knows they are on the way. Good thing Christmas is coming.<br/><br/>The fourth movie in The Expendables franchise has everything you expect: guns, fights, quips, and explosions. Most of your favorite leading men are gone, and the replacements have no backstories. Also, this movie has more sex and sexual tension than the others. With that said, this installment is funnier and gorier than the others. It pulls no punches with the devastating gunshots and lethal hand-to-hand combat. If you love the others, see this one in theaters. But casual fans should see it at the matinee price.<br><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/expend4bles.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/expend4bles.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"A Haunting in Venice",
     "review":"Hercule Poirot retires in Venice and avoids new cases with the help of his bodyguard, Vitale Portfoglio. Vitale runs to Hercule's rooftop with a message from a friend. Then, Vitale shows Hercule an apple, and Hercule knows that author Ariadne Oliver is there. An actress, Rowena Drake, hired a medium, Joyce Reynolds, to communicate with her deceased daughter, Alicia, in a séance. Alicia committed suicide after her fiancé, Maxime, left her and six months later became engaged to another woman. Ariadne was able to uncover fakes before but believes Joyce is the real deal. Ariadne is writing a book about Joyce, and proving Joyce wrong will make the book a best-seller.<br/><br/>Hercule, Ariadne, and Vitale arrive at Rowena's home and hear it's haunted history. Rowena opens her doors to let the children play on Halloween, but Leopold wants to read his book instead. Leopold doesn't want to participate in the activities because his father, Dr. Ferrier, suffers from trauma. Joyce arrives with her assistant, Desdemona Holland, wearing a cloak and mask. Before everyone walks into the salon, Rowena's housekeeper, Olga Semioff, screams at the front door. Maxime received an invitation to the séance and demands to attend.<br/><br/>Joyce puts a typewriter in the corner of the room, and Desdemona stands next to it. Joyce has Olga, Ariadne, Dr. Ferrier, Leopold, and Rowena surround her as she calls Alicia to speak from beyond the grave. Hercule stops the séance and exposes Joyce's fraud. Desdemona's half-brother, Nicholas, hid in the fireplace and signaled the typewriter. As Hercule readies to put the case to bed, windows fly open, and Joyce spends around uncontrollably speaking with Alicia's voice. She comes out of the trance and offers more information about Alicia's killer in the morning.<br/><br/>Privately, Hercule confronts Joyce for using Rowena's desperation for financial gain. Joyce he puts her mask and cloak on Hercule and tells him to do better than her. Hercule indulges in a children's game of bobbing apples, and someone pushes his face into the water. He wakes up to Vitale standing over him. Then, they all hear a scream and find Joyce impaled after someone threw her off a balcony. Hercule locks everyone inside the house and plans to investigate. Hercule doesn't believe in ghosts or God but starts to see and hear things around the house. Can Hercule uncover the killer before losing his mind?<br/><br/>Based on Hallowe'en Party by Agatha Christie, this movie starts with Hercule happily leaving the mystery world behind. The house has a horrid history with a plague, causing doctors and nurses to leave children to die. This tale leaves the plot open to ghosts with targets in their crosshairs. This film has plot points from Murder on the Orient Express and Death on the Nile. Hercule fans know to keep their eyes and ears open for every clue. And this time, you may spot the nugget of truth hidden in a brief exchange. This movie isn't the best, but you will leave the theater satisfied.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Outlaw Johnny Black",
     "review":"Outlaw Johnny Black rides into Cheyanne because Brett Clayton and the Ruff Riders will rob the bank. Over two decades ago, Brett killed Johnny's father, Reverand Bullseye Black. Then, Johnny sees a group of men harassing an indigenous couple, and Johnny runs to their rescue. During the melee, the sheriff runs out, and Johnny shoots off his hat, causing the sheriff to have a heart attack. They blame Johnny for the sheriff's death and compare Johnny to his wanted poster. They decide Johnny's punishment will be hanging, but the indigenous couple helps Johnny escape.<br/><br/>Johnny wanders the desert until he collapses, and Reverand Percy saves him. Percy will travel to Hope Springs and take over for the town's recently deceased Reverand. However, Percy has more waiting for him. Percy and Betsy correspond via letters for two years, and Percy is ready to marry her. Betsy is beautiful, but Percy admits that Betsy has never seen an image of him.<br/><br/>Johnny prepares Percy's horse and buggy to travel to Hope Springs, and someone shoots Percy with an arrow to the heart. Johnny gets in the buggy and runs away to a madame's house. Marshall Cove doesn't trust Johnny and questions him. Johnny says his name is Reverand Percy as Betsy vouches for him. The congregation burst with pride and gushes about the new Reverand's sermon on Sunday. They bring Johnny to the Reverand's quarters but sadly say that Johnny can't move into the Reverand's home because Tom Sheally took it. Tom negotiates with Jessie Lee to buy the town, but she won't budge. Tom isn't a patient or fair man. So, he will kill for the town.<br/><br/>After everyone leaves Johnny's room, he tries to jump out of the window to escape. Deacon Fry enters and gives Johnny the church's security box key. Fry tells Johnny the box has $5,000 inside. Johnny wants the money, but the bank won't open until Monday. So Johnny has to stay in town and preach a sermon before he can get the money and leave. This fateful decision will put Johnny on a path of love, God, and Brett.<br/><br/>This film is a parody of old-fashioned westerns and blaxploitation. It covers a lot of issues while staying light-hearted and fun. Be prepared for fancy shooting, gun-slinging, and karate. The script stayed within the period with costumes and technology but constantly broke the fourth wall to include the audience in the joke. While the movie was entertaining, it ran too long.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"After Everything",
     "review":"Hardin has a best-selling book, but he lost his muse. Tessa left after he published a book about their stormy yet steamy love affair. Today, Hardin is a year behind for his next novel. Hardin parties and drinks all night but can't write one word without Tessa. He texts her, but she tells him it's over.<br/><br/>Hardin meets with his parents, Trish and Christian. They want him to stop drinking and focus. Trish tells Hardin that she has been in contact with Natalie. Natalie is another scorned woman from Hardin's past. Trish says Natalie lives in Lisbon and works in a dress shop. Hardin decides to go to Lisbon and make amends to Natalie. Hardin hopes fixing himself will be the first step to winning Tessa's heart.<br/><br/>Based on The After Series by Anna Todd, this movie is about Hardin, his abyss, and finding forgiveness from the first woman he hurt. The audience peeks at Hardin's downward spiral before Tessa. And it shows the parallel between how he treated Natalie and Tessa. This installment is an hour and fifteen minutes of Hardin's self-loathing, 2nd novel syndrome, and alcoholism. Then, he is face-to-face with Tessa. While giving you a somewhat redeeming view of Hardin during his growth, this movie puts a bow on Hardin and Tessa's story.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"My Big Fat Greek Wedding 3",
     "review":"Toula and Ian lost their fathers over the years. Toula tells Maria she and Ian are going to Greece to give her father's journal to his three best friends. The village of Toula's father, Gus, is having a reunion, and Toula hopes Gus's friends will be there. Toula and Ian arrive at the airport to find their daughter, Paris, waiting for them. Toula calls her brother, Nick, and ensures he is on his way with their aunts, Voula and Theia. As they board the plane, Paris tries to distance herself from her nosey family, but they invite her ex-boyfriend, Aristotle, to join them.<br/><br/>The family lands and instantly becomes awestruck with Greece. Mayor Victory meets them with coffee and brings them to Gus's hometown. Toula loves Gus's village, but only six people live their now. Mayor Victory sent an invitation to every family, but only the Portokaloses came. The reunion will be in a few days, but Victory believes everyone will come. The empty town discourages Toula, but Ian and her family give her faith to keep going. Toula vows to find Gus's friends and deliver the journal before returning to the States.<br/><br/>In case you were wondering, Yes. This movie has a Greek wedding in Greece. Like most adults, Toula enters the next phase of her marriage to Ian. They must care for their parents and pass the traditions to the younger generation. But Toula must balance between Gus's wishes and Ian's vacation. Ian must refocus her energy to have fun and not worry about Paris, the family, or the secrets. Yes, there are explosive secrets for the Portokaloses and the Millers. The family packs joy with their open, loving, and caring nature. They love each other no matter if they are shaving at the dinner table, swimming nude, or drinking. This film doesn't try to one-up the previous movies. It gels with them perfectly. For fans of the Greeks, see this movie in theaters.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mybigfatgreekwedding3.jpg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mybigfatgreekwedding3.jpg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Bottoms",
-    "review":"PJ wants this year to be different. So, she will approach her crush, Brittany, and ask her out. PJ suggests that her best friend, Josie, do the same with her love interest, Isabel. PJ convinces Josie to attend the school fair. Hazel runs into Josie and PJ at the fair and inquires about Josie's arm in a sling. PJ jokingly lies about being in juvie with Josie. They ignore Hazel when they see Isabel and Brittany. Josie stumbles over her words and invites Josie to interject. Josie can't put a coherent sentence together and runs away. PJ spots Isabel and Jeff fighting while Isabel walks away from Jeff. They offer Isabel a safe ride home, and Isabel gets in the car. Jeff jumps in front of the vehicle to stop them. When he doesn't move, Josie barely taps Jeff with her bumper. He falls to the ground, screaming in pain.<br/><br/>Jeff comes to school on crutches the next day, and the entire school believes Josie hit Jeff. They are the talk of the classroom. Principal Meyers calls them into his office to expel them. Josie falsely states that they are part of a self-defense club for female empowerment. Josie ponders the idea and thinks it's the best way to meet cute girls in the school, especially Brittany and Isabel.<br/><br/>The number of attendees for their first meeting underwhelmed Josie and PJ. But they keep up the lie and train. After the first punch, Hazel tells them they need an advisor. Josie and PJ elect their history teacher, Mr. G. because they assume he won't come. He makes it to the second meeting, along with Isabel and Brittany. A football player, Tim, worries the club is taking the cheerleaders' attention away from the football team. So, he tells Josie and PJ to shut it down before he exposes their lies and motives. Josie and PJ choose to ignore Tim. And their choice will have bloody, explosive, and deadly consequences.<br/><br/>This dark satire is a mix of Booksmart and Not Another Teen Movie but not as funny or raunchy. Viewers will spend more time being shocked than laughing out loud. The script exposes misguided views of feminism, the misunderstood differences of sexuality, and the vast subsets of feminity. It starts as a raunchy statement piece but becomes increasingly weird and disjointed. The final fight scene feels like a desperate attempt to save the film and create a believable ending for the leads. While critics raved, it's best to view this film at home.<br/><br/>2.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/bottoms.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/bottoms.jpeg"
     }
   }
 ];
@@ -56,7 +56,33 @@ module.exports = inTheTheaters;
 
 /*
 
-
+{
+  "option":"1"
+  "mtitle":"The Creator",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecreator.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecreator.jpeg"
+  }
+},
+{
+  "option":"2"
+  "mtitle":"Dumb Money",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dumbmoney.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dumbmoney.jpeg"
+  }
+},
+{
+  "option":"3"
+  "mtitle":"PAW Patrol: The Might Movie",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/pawpatrolmightymovie.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/pawpatrolmightymovie.jpeg"
+  }
+}
 
 */
 
