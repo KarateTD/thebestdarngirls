24b2668eaf5b1361e10f6d3eafceeb78e1049543
Tiffany Gray <KarateTD@yahoo.com>
2023-10-31 14:17:36 -0400

    add vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Stay Out",
+		"review":"Donovan kisses his wife, Simone, before she leaves the house for her flight. Donovan sits at his desk and converses with his co-workers, Lauren and Graham. After their meeting, Donovan gets a phone call from a lawyer. His aunt Velma passed away ten years ago. Donovan hasn't seen his Uncle Rufus and Aunt Velma since he was young. He knew Rufus died in 1968 but had no idea about Velma's passing. Rufus and Velma had property filled with personal items. So, Donovan has to visit Georgia to finalize the estate.<br/><br/>Donovan arrives in Blue Ridge, Georgia, and most locals don't hide their disdain. Donovan decides to sell the home but can't find a real estate agent to take on the property. He visits the property and finds his old toys, a box of toothpicks, and a chicken's foot. Donovan looks up and sees Rufus in front of him. Rufus possesses his body. In 1968, two men lynched Rufus because he wouldn't give up his home. Before Rufus died, he put a voodoo protection spell on his family. Now, his spirit has taken over Donovan's body to seek revenge on those who killed him and their children. Is anyone safe from Rufus's wrath?<br/><br/>This movie isn't a horror or a thriller. It's a paranormal revenge story that allows you to cheer for the 'villain.' Rufus's targets are deserving so you won't have a problem with the bloodshed. If you watch closely, Rufus isn't the only ghost seeking vengeance. Velma wants justice. If you want something to watch for the Halloween season but with less gore, this is your film.<br/><br/>3 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/stayout.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/stayout.jpeg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"If You Were The Last",
 		"review":"For three years, astronauts Adam and Jane aimlessly float in space. Jane doesn't believe that NASA is looking for them but Adam has hope. They have a goat, chickens, pop tarts, and weed to keep them busy. Jane questions Adam about a milky substance she stepped on next to the garden. He apologizes because he thought he cleaned it up. She admits to pleasuring herself, but only in her bedroom, not in front of the chickens and their food. He tosses out the idea of them sleeping together. She laughs, says no, and gives three reasons why not. Will they give in, or will they return first?<br/><br/>This movie is The Martian, with simple graphics and two people. And it's The Mountain Between Us but funny. Adam convinces Jane to watch The Princess Bride and listen to country music while line dancing. And Jane gets him to watch Alien in outer space and sing Lionel Richie. The ending is predictable, but the journey is thrilling. The director displayed their relationship through a series of montages and running gags. The comedy will keep you watching, but the sweet predictability loses your interest by the conclusion.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"God's Grace: The Sheila Johnson Story",
 		"review":"Sheila Johnson sips and paints with her friend, Adriene, when she notices a weird sensation in her hand. After decades of military service with the Air Force, they promoted Sheila to Senior Master Sergeant of the Technology Department. Sheila's father, Dillard, and daughter, Janaia, have flowers ready when Sheila gets home. Sheila smells flowers, sneezes, and feels a burning sensation in her ribs. She visits the base hospital for tests.<br/><br/>The doctor gives Sheila the standard exam. Then. he brushes off Sheila's feelings until he reads her chart and discovers her mother died of breast cancer five years ago. He decides to draw her blood and get an ultrasound. Sheila has stage 4 metastatic breast cancer, like her mother, Grace. She thinks this is the end of her journey, but it's the first step of her testimony.<br/><br/>Wow! This film will put you through it. Sheila was on cloud nine before her diagnosis. She has a successful career, a college-bound daughter, a doting father, and a sister-like friendship. Then, her world does a 180 with one diagnosis. She does research and discovers that misrepresentation litters support groups, diagnoses, and clinical trials. She starts SWAGG, encourages women to get a 2nd opinion, and motivates women to demand an ultrasound. Viewers get a glimpse of the actual Sheila and an introduction to her book, A Survivor's Lens: A Metastatic Breast Cancer Survivor's Focus on Faith and God's Word.<br/><br/>5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"The Burial",
 		"review":"After his 75th birthday, Jeremiah O'Keefe gets a visit from the State Insurance Commission. Jeremiah runs eight funeral homes and sells insurance but doesn't have the required reserve in his account. The commission warns Jeremiah to replenish his account or face heavy penalties. Jeremiah sits with his lawyer, Mike, to plan how to fix the problem. Mike reminds Jeremiah that Ray Loewen from The Loewen Group bought funeral homes around town and can make Jeremiah a fair offer.<br/><br/>Mike and Jeremiah meet at the airport to fly to Vancouver to converse with Ray, but Jeremiah has a surprise. Jeremiah hired Hal, a young attorney, to accompany them. They meet with Ray on his boat, and Ray offers to buy three funeral homes but never sell insurance in southern Mississippi. Ray and Jeremiah shake on the deal, but almost a year later, Ray hasn't signed it. Hal thinks Ray is purposefully stalling to bankrupt Jeremiah. Jeremiah wants to sue Ray. Hal doesn't think Mike is the right lawyer to try the case. Hal puts a videotape of Willie Gary into Jeremiah's VCR.<br/><br/>Jeremiah watches Willie work a case and loves his delivery and bravado. So, Jeremiah and Hal go to Willie to hire him. Willie declines the case, and Hal and Jeremiah leave Willie's office. Hal tells Willie that winning this case will make him internationally famous because Jeremiah is the tip of the iceberg. Willie takes the case and travels to meet Jeremiah's team.<br/><br/>Mike meets with Willie and discloses an offer he presented to Ray for $9 million. Willie says the deal is off the table because he wants $100 million. Ray's team looks at the area demographics and don't think they can win without finding a lawyer to appeal to the jury. Ray has a lawyer in mind: Mame Downes. Willie's desire to win the case will uncover a disgraceful misuse of trust, discriminatory pricing, and long-overdue justice.<br/><br/>Based on The Burial by Jonathan Harr, this movie packs drama and fun to lighten the mood about racism and classism. When you watch, remember that this film wasn't in the 1960s. It's 1995. The Loewen Group overcharged people during their worst moments after selling worthless insurance. They aligned themselves with the local churches to gain trust in the community. The movie also depicts how Ray and Jeremiah started in the same position. They inherited businesses from their father and worked hard to grow them. The final judgment will shock you. Stay until the mid-credits to get a glimpse of the real-life Willie.<br/><br/>4.5 out of 5 stars.",
 		"image":{
@@ -27,23 +36,14 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"Wake",
 		"review":"Dr. Sharita finishes her lesson and relaxes in her office. Her watch alarms her because her heart rate is dropping. Before Sharita passes out, she sees a bloody figure coming towards her. Sharita wakes up on the couch with her cousin, Eddie, caring for her. He wants Sharita to get out more and have fun, but she worries about her narcolepsy. She confesses to her doctor that she copes with sleep paralysis and hallucinations, and the doctor suggests increasing her medication dosage.<br/><br/>Preston, going by the rap name P-Mac, walks off stage, and his assistant, Jay, tells P-Mac to stop doing drugs and take his bipolar medication. P-Mac doesn't want to take them because they impede his writing. P-Mac pushes everyone out of his dressing room when his girlfriend, Shontay, requests time alone. She is pregnant and wants to end their relationship because of his abuse. He pretends to agree while slipping drugs into her water. She dies of a drug overdose next to her car.<br/><br/>Sharita talks to her friend, Bryn, about the new neighbor moving next door. Sharita hacks into a database to find information on P-Mac. Later, Sharita strides to P-Mac's home to formally introduce herself. He questions her cameras pointing at his home, and she assures him they are for her condition, not surveillance. He laughs, but something about P-Mac rubs Sharita the wrong way. When she sees him commit a heinous crime, can Sharita trust her eyes, or is it another hallucination?<br/><br/>This movie sets viewers up because both parties struggle with a mental condition. So, the audience is unsure if they can trust what they see. And that makes this plot a pure thriller. Also, the script doesn't take itself too seriously with Eddie's commentary on their dumb actions. And a murder happens while the killer wears a 'good vibes' hoodie. It's comic relief during a brutal scene. This film is for people who aren't horror fans but like a mind-bender.<br/><br/>4 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/wakebet.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wakebet.jpeg"
 		}
-	 },
-	{
-		"option":"5",
-		"mtitle":"First Lady of BMF: The Tonesa Welch Story",
-		"review":"Tonesa's mother, Carol, and stepfather, Emerson, agree to watch her son, Little Marlon, while she goes to a club with her friends, Carla and Lisa. Tonesa switches into something more provocative and heads out for a fun night. As one man approaches Tonesa, another, Harvey, scares him away. Harvey offers Tonesa some champagne, and they have an instant spark.<br/><br/>Tonesa sneaks out to meet Harvey and leaves Little Marlon behind. Harvey showers Tonesa with money, gifts, and attention. Tonesa dumps her long-distance boyfriend, Big Marlon, for Harvey and moves in with him. One day, Harvey comes home and puts his gun on the table. She confesses that she wants to be a part of his crew and sell drugs. But Harvey says no because he has another plan for her. He wants her to write bad checks.<br/><br/>Tonesa does great scamming for Harvey, but it's not enough. So Harvey teaches her the drug trade, and they expand their territory. They move into a new home, and Tonesa comes up with a new idea. She wants to increase their market to the West because they have money to burn. Harvey ponders her idea and slaps her. Harvey offers Tonesa drugs to dull the pain. Despite her parents' warning, Tonesa willingly slides down a razor's edge into a life of abuse, drugs, money, and jail.<br/><br/>Tonesa matures throughout this film. At first, she blames her parents for holding her back. Then, she accuses her husband and boyfriends. Finally, Tonesa takes a long look at herself. She had something special that made her the first lady of BMF: her tenacity and woman's intuition. Tonesa earns a legitimate living today and works to help other recently released prisoners return to society. The film uses technology, wardrobe, slang, and hair to recreate the period. And it doesn't glamorize her past. It provides an honest look at her decisions, life, consequences, and redemption.<br/><br/>5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fistladyofbmf.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fistladyofbmf.jpeg"
-		}
-	}
+	 }
 ];
 
 module.exports = videoOnDemand;
@@ -55,8 +55,6 @@ module.exports = videoOnDemand;
 */
 
  	/*
-
-
 	 
 	
 	*/
