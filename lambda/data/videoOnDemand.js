150f91f40244aed6d7e7e22d0c8fefbdea86717e
Tiffany Gray <KarateTD@yahoo.com>
2023-11-22 18:41:22 -0500

    add vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Good Burger 2",
+		"review":"Dex invites investors to his home to present his new product, Burn No More. If you spray it on anything, it will become fireproof. And Dex uses his home to prove the product works. However, he burns his home to the ground, and the investors pull out of the project. Dex returns to his office and finds an eviction notice. Dex finds a yo-yo and thinks of his best friend, Ed. He calls Ed and asks to see him. Ed is ecstatic.<br/><br/>Dex asks for his old job at Good Burger back, and Ed promises to put a good word in with the manager, Mr. Jenson. Mr. Jenson hires Dex without an interview because the owner, Ed, vouched for him. Dex didn't know Ed owns Good Burger because they haven't spoken in over five years.<br/><br/>A lawyer, Cecil, pulls Ed aside during Dex's first shift. Dex overhears Cecil trying to get Ed to sell Good Burger. But Ed turns him down. Then, Cecil invites Dex to his home. He tells Dex that Megacorp doesn't want to buy Good Burger from Ed. Instead, they want to franchise. If Dex convinces Ed to sign the documents, Megacorp will make Dex a partner. Dex arranges a meeting, and Ed signs because Dex asks him to do it.<br/><br/>Dex has a massive party for the Good Burger employees and promises a raise in pay for the staff. Cecil stops the party. Ed and Dex didn't read the contract. They are closing the flagship Good Burger and opening copycat restaurants worldwide. Ed and his employees are jobless. Dex feels guilty. So he vows to get Good Burger from Megacorp and return it to Ed.<br/><br/>This movie caters to new and old fans equally. It has a modern plotline that references the first movie. Also, All That cast members make noteworthy cameo appearances. The core of Ed's personality hasn't changed. Ed is a husband with seven children, a thriving business, and a famous sauce. Dex is single with no kids or business. However, Dex isn't jealous of Ed but tries to use him. Dex sees his misgivings and attempts to be a better friend. The movie gives a synopsis of Good Burger, so fans and newbies don't have to watch it. This film stays faithful to the original while reeling in new fans, and it's enjoyable for the whole family.<br/><br/>4 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/goodburger2.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/goodburger2.jpeg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"Dashing Through The Snow",
 		"review":"Eddie is a crisis counselor who works in concert with the police department. After talking to a homeless man and getting him off a Christmas tree, he sees that man on the news. Congressman Harf is working hard to keep the local mission open. Eddie hates this time of the year. Eddie's dad came home early, and so did the mall Santa. But he came to rob them.<br/><br/>Eddie's estranged wife, Alison, drops off their daughter, Charlotte, to spend Christmas Eve with him. She tells Eddie to keep a positive attitude because Charlotte loves the holiday. Charlotte reminds Eddie to feed the vacationing neighbor's cat. Eddie walks to the door, grabs the mail, and calls for the feline. Then, Eddie hears a voice coming from the fireplace. It's Nick, and he came to deliver the toys. Eddie assumes Nick is mentally unstable and offers to get Nick help. Nick grabs his tablet to double-check the naughty list but can't unlock it. He realized he got the wrong tablet at Congressman Harf's house. Harf sent 3 of his goons after Nick to retrieve it. Eddie thinks Nick is a common thief, but Charlotte knows he is real. Can Nick get Eddie to believe and save Christmas?<br/><br/>This film plays with the belief storyline. Eddie rejects the holiday, and it has a direct effect on Nick and his magic. Viewers will get some Santa facts while adding new ones for holiday lovers. It answers children's questions and keeps the dream alive. It's what you expect from Disney but without the constant product placements.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Sworn Justice: Taken Before Christmas",
 		"review":"After years of deep cover, Sophie loses her badge after she gets too close to her mark, Nico. She couldn't shoot Nico, and Nico stabbed her boss, Dave, in the neck. Nico vows to get his revenge from prison. Today, Sophie rides the desk to get her badge back. This year, Sophie has more time to spend with her mother, Laila. Laila checks if Sophie will make the Christmas Sunday service tomorrow. And Sophie says yes.<br/><br/>Sophie gets a phone call from her lover, District Attorney Ty. She worries he wants to define the relationship before the holiday. However, it's a digitized voice on the phone. The caller says they have Ty and will kill him if Sophie doesn't deliver $5,000 per their instructions.<br/><br/>Ty leaves work until he gets a phone call from an unknown caller. They have Sophie, and they will kill her if he doesn't pay $5,000. And they send a photo of Sophie drugged on a mattress to prove it. These two are on a mission to save the other and don't know it. Will they discover the truth or give in?<br/><br/>This film is something you have to watch from the beginning. There are several moving parts, back-handed dealings, and several bad guys. The fight scenes are well choreographed and use everyday objects to make it believable. And be prepared for a cliffhanger that will leave some stunned. But fans of the genre will suspect it.<br/><br/>3.5 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"Christmas Angel",
 		"review":"Pop singer Angel St. James wraps up her performance and interview on The Tamar Braxton Show and rushes home to meet her fiancé, super producer E.J., for their anniversary. Angel enters her house to find it empty. E.J. forgot about their anniversary and can't leave. He tells Angel to buy whatever she wants at his expense, and Angel hangs up. She tears up her tickets to Aspen and gets in the car to drive to her hometown.<br/><br/>Angel attempts to send a text and runs off the road. A tow driver, Calvin, pulls over to assist her. She offers to pay extra to have him fix it now, but he can't because her car is electric. He knows an Airbnb she can use until he fixes her car. Angel agrees and gets comfortable. Calvin and his daughter, Liz, visit Angel in the morning. Angel learns that Calvin is a widower and paralegal with several properties, including her Airbnb. His sister-in-law, Natalie, helps with Liz, but Liz still misses her mom. Angel gets inspiration from Liz and considers ending her relationship with E.J.. But the break-up could have a devastating effect on her career. Is Angel willing to risk it all to have everything?<br/><br/>This movie starts with the typical rom-com plot. However, the script adds more drama to wet your thirst. Natalie blackmails, E.J. returns, and Calvin rages. Also, Liz acts her age and isn't a brat. This film also provides catchy and original music. This movie is fun but for teens and up.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"Heart for the Holidays",
 		"review":"Rachel meets with her assistant, Jenna, to plan Rachel's hectic schedule. Rachel leaves to meet with a client but has a heart attack in the client's lobby. Rachel's doctor gives her grim news. She has myocarditis and needs a transplant. Later, Rachel's doctor tells Rachel that they found a heart. A woman, Angela, was in a car accident in Cheverly and won't make it. Rachel calls a car to take her to Cheverly Medical Center.<br/><br/>Rachel walks around the hospital for a cell signal to call Jenna. She gets one bar but doesn't realize she is in a woman's hospital room. It's Angela. Rachel holds Angela's hand, and Angela asks Rachel to promise that Rachel will love deeply and with all her heart. Rachel promises, and Angela dies. Rachel wakes up with Angela's heart beating in her chest.<br/><br/>Rachel continues to make phone calls as she gets better. She takes her daily trips around the hospital but nearly falls. A man, Scott, runs to her aid and fixes her medical I.V. pole. After a revealing meet-cute, Rachel runs into another transplant patient, Amy. Amy received Angela's kidney. When Amy learns Rachel has Angela's heart, Amy considers Rachel her sister. Scott told Amy that Rachel took his breath away. However, Rachel wants to leave Cheverly and return to LA for work. Rachel tries to escape, but her heart flutters. Something keeps holding Rachel back, and it's more than love.<br/><br/>The beginning will bring you to tears as Angela drifts away with a few parting words to a woman she will save but never know. Afterward, Rachel's connection to Amy garners the viewer's attention, but the film is the typical rom-com plot. Audiences will phase out when Rachel and Scott are on because you know their ending. However, you will melt when Rachel and Amy bond over the carnival, Angela, and hair. Their sisterly relationship is the storyline's driving force.<br/><br/>3.5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/heartfortheholidays.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/heartfortheholidays.jpeg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"A Wesley Christmas Wedding",
-		"review":"This year, the Wesleys have a lot to celebrate. Sylvia and Bryan made time for each other and traveled throughout the year. Chris and Trina added a beautiful baby boy to their family, and Ryan and Todd got engaged. Marcus moved in with Gloria while they attended group therapy for veterans. Cydney is ready to leave the road and return home to celebrate the holiday and nuptials.<br/><br/>Trina and Chris's daughters, Kennedy and Taryn, arrive from college with a surprise. Kennedy brought her new boyfriend, Rico, to spend the holidays with them. Taryn dislikes Rico but doesn't speak up to their parents. If Taryrn tells their parents Rico is a bum, Kennedy will spill Taryn's secret.<br/><br/>The Wesley women meet at the ballroom before Todd and Ryan's engagement party. They can't believe Ryan's mother, Diana, changed the decorations. During the party, Diana focuses the party on her but hates the ghastly holiday-themed decor. The MC calls for friends and family to make toast for the engaged, and Todd's ex-wife, Janelle, grabs the microphone to introduce herself as Todd's wife. She admits that she never signed the papers and deserves everything Todd is doing for Ryan. Ryan overhears them and gives Todd his ring back. Will there be a Wesley Christmas Wedding?<br/><br/>This sequel provides drama and hilarious moments with the Wesley Bunch and DC Go-Go. The heads of the family have found their groove, but Sylvia must deal with another woman in the family. Diana has a vision for Ryan's wedding and won't let anyone, including Ryan, stand in her way. The script covers PTSD and postpartum depression honestly and respectfully. This movie, like its predecessor, balances these series topics with pearl-clutching humor. You should watch the first before viewing the second to familiarize yourself with the family and their friends.<br/><br/>5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/awesleychristmasweding.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/awesleychristmasweding.jpeg"
 		}
 	}
 ];
@@ -58,6 +58,16 @@ module.exports = videoOnDemand;
 	 
 
 
+
+{
+	"option":"1",
+	"mtitle":"So Fly Christmas",
+	"review":"<br/><br/>* out of 5 stars.",
+	"image":{
+		"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/soflychristmas.jpeg",
+		"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/soflychristmas.jpeg"
+	}
+}
 
 	
 	*/
