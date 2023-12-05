c0dee8ae7ad3bc680d21e200569376093d37c76e
Tiffany Gray <KarateTD@yahoo.com>
2023-12-04 23:19:59 -0500

    add 2 vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,24 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"The Christmas Ringer",
+		"review":"Nicole started singing in her father's church. Today, she is a has-been R and B star with a memorable Christmas song. Nicole performs for a local woman's birthday party but freaks out when they ask her to sing her biggest hit. She refuses to sing and bumps into the birthday woman when she leaves. Nicole returns to her holiday set and finds the crew packing up. The stage director informs Nicole that her check bounced.<br/><br/>Nicole and her manager look over her accounts. They are empty or overdrawn. Nicole realizes her fiancé, Cliff, took the money and ghosted her. Also, someone filmed Nicole knocking down the woman, and it went viral. Nicole's manager suggests Nicole leave LA and visit her hometown. She should film herself doing charitable acts to change the public's perception.<br/><br/>Nicole returns home to an unenthusiastic welcome. The church fell on hard times because the angel investor stopped donating three months ago. Nevertheless, Nicole's mother has an idea. The church won the lottery to host The Christmas Choir Classic. The grand prize is $25,000. Nicole's mother asks Nicole to lead the choir. Can Nicole nab the choir a win without them discovering she is using them for clickbait?<br/><br/>This movie focuses on mending old wounds. The largest one Nicole has to fix is within herself. She doesn't want to sing her trendiest song because it dredges old memories. Today, Nicole sees that avoiding the past doesn't make it go away. This film has some astounding singers and catchy holiday songs.<br/><br/>4 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thechristmasringer.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thechristmasringer.jpeg"
+		}
+	},
+	{
+		"option":"2",
+		"mtitle":"A Royal Christmas Surprise",
+		"review":"Riley and Mandla are successful in their personal and professional lives. Their relationship is more loving than ever because Mandla's parents, King Chidike and Queen Nobantu, accepted Riley as a future wife, princess, and queen. Riley and Mandla decided to throw an engagement party to announce their upcoming nuptials to their friends. Also, they will use the party to introduce Mandla's parents to Riley's relatives, Uncle Ray and Aunt Gina.<br/><br/>Chidike and Nobantu fly to Georgia and travel to Ray and Gina's home. Ray and Chidike greet each other like old friends, but Nobantu and Gina size up each other. Gina remembers how Nobantu made Riley feel in Africa, so she plans to pay Nobantu back. Nobantu won't take Gina's disrespect. She will fight fire with fire while keeping her head held high. They will go for the jugular, and Riley must play peacemaker. Can she keep it up when Mandla drops a bomb on her?<br/><br/>This sequel is a wildly different movie than the first, and that's a good thing. It's funnier, wilder, and has more heart. Don't be surprised if you must pause the TV because you are laughing too hard to hear the dialog. Nobantu and Gina don't pull punches, and their is plenty of collateral damage. Amid this craziness, Mandla keeps a life-altering secret from Riley. You laugh while waiting with bated breath for the other shoe to drop. Viewers will look forward to film three if this series keeps its trajectory.<br/><br/>4.5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aroyalchristmassurprise.png",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aroyalchristmassurprise.png"
+		}
+	},
+	{
+		"option":"3",
 		"mtitle":"So Fly Christmas",
 		"review":"Wyvetta stands at the Atlantic City United Church and Casino, waiting for her fiancé Floyd to enter. When he doesn't show, Wyvetta's best friend, Dione, takes Floyd's place. Instead of a wedding in holy matrimony, they cement their friendship forever.<br/><br/>On their podcast, Di and Wy So Fly, Dione questions callers about when they take down their Christmas trees. Wyvetta ignores the topic and says she is 'Done with Love.' She decides to say no to love and romance, but not sex. Dione gives up love and relationships too.<br/><br/>Eleven months later, Dione and Wyvetta have branded their 'Done with Love' message, and their friendship is strong. Wyvetta spends the evening with her parents, Harriet and Myron, while Dione meets her daughter, Sierra, at a diner. Sierra leaves earlier, and a man, Clarence, approaches Dione, but she turns him down before he can speak. Clarence corrects Dione because he wants her extra ketchup. T He invites Dione to eat alone with him. The spark between Dione and Clarence grows, but Dione wants to keep him a secret from Wyvetta. Dione continues to engage in the 'Done with Love' movement. What will happen when Wyvetta learns the truth?<br/><br/>This movie isn't a rom-com in the classic way. It's a friendship comedy. The bond between these women takes center stage. Dione and Wyvetta support each other during their movement. However, Dione has to hide her romance around Wyvetta. Everyone has been a Dione, a Wyvetta, or both. Like these friends, you can grow and be better.<br/><br/>5 out of 5 stars.",
 		"image":{
@@ -9,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"4",
 		"mtitle":"Good Burger 2",
 		"review":"Dex invites investors to his home to present his new product, Burn No More. If you spray it on anything, it will become fireproof. And Dex uses his home to prove the product works. However, he burns his home to the ground, and the investors pull out of the project. Dex returns to his office and finds an eviction notice. Dex finds a yo-yo and thinks of his best friend, Ed. He calls Ed and asks to see him. Ed is ecstatic.<br/><br/>Dex asks for his old job at Good Burger back, and Ed promises to put a good word in with the manager, Mr. Jenson. Mr. Jenson hires Dex without an interview because the owner, Ed, vouched for him. Dex didn't know Ed owns Good Burger because they haven't spoken in over five years.<br/><br/>A lawyer, Cecil, pulls Ed aside during Dex's first shift. Dex overhears Cecil trying to get Ed to sell Good Burger. But Ed turns him down. Then, Cecil invites Dex to his home. He tells Dex that Megacorp doesn't want to buy Good Burger from Ed. Instead, they want to franchise. If Dex convinces Ed to sign the documents, Megacorp will make Dex a partner. Dex arranges a meeting, and Ed signs because Dex asks him to do it.<br/><br/>Dex has a massive party for the Good Burger employees and promises a raise in pay for the staff. Cecil stops the party. Ed and Dex didn't read the contract. They are closing the flagship Good Burger and opening copycat restaurants worldwide. Ed and his employees are jobless. Dex feels guilty. So he vows to get Good Burger from Megacorp and return it to Ed.<br/><br/>This movie caters to new and old fans equally. It has a modern plotline that references the first movie. Also, All That cast members make noteworthy cameo appearances. The core of Ed's personality hasn't changed. Ed is a husband with seven children, a thriving business, and a famous sauce. Dex is single with no kids or business. However, Dex isn't jealous of Ed but tries to use him. Dex sees his misgivings and attempts to be a better friend. The movie gives a synopsis of Good Burger, so fans and newbies don't have to watch it. This film stays faithful to the original while reeling in new fans, and it's enjoyable for the whole family.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -18,30 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"5",
 		"mtitle":"Dashing Through The Snow",
 		"review":"Eddie is a crisis counselor who works in concert with the police department. After talking to a homeless man and getting him off a Christmas tree, he sees that man on the news. Congressman Harf is working hard to keep the local mission open. Eddie hates this time of the year. Eddie's dad came home early, and so did the mall Santa. But he came to rob them.<br/><br/>Eddie's estranged wife, Alison, drops off their daughter, Charlotte, to spend Christmas Eve with him. She tells Eddie to keep a positive attitude because Charlotte loves the holiday. Charlotte reminds Eddie to feed the vacationing neighbor's cat. Eddie walks to the door, grabs the mail, and calls for the feline. Then, Eddie hears a voice coming from the fireplace. It's Nick, and he came to deliver the toys. Eddie assumes Nick is mentally unstable and offers to get Nick help. Nick grabs his tablet to double-check the naughty list but can't unlock it. He realized he got the wrong tablet at Congressman Harf's house. Harf sent 3 of his goons after Nick to retrieve it. Eddie thinks Nick is a common thief, but Charlotte knows he is real. Can Nick get Eddie to believe and save Christmas?<br/><br/>This film plays with the belief storyline. Eddie rejects the holiday, and it has a direct effect on Nick and his magic. Viewers will get some Santa facts while adding new ones for holiday lovers. It answers children's questions and keeps the dream alive. It's what you expect from Disney but without the constant product placements.<br/><br/>3.5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dashingthroughthesnow.jpeg",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dashingthroughthesnow.jpeg"
-		}
-	},
-	{
-		"option":"4",
-		"mtitle":"Sworn Justice: Taken Before Christmas",
-		"review":"After years of deep cover, Sophie loses her badge after she gets too close to her mark, Nico. She couldn't shoot Nico, and Nico stabbed her boss, Dave, in the neck. Nico vows to get his revenge from prison. Today, Sophie rides the desk to get her badge back. This year, Sophie has more time to spend with her mother, Laila. Laila checks if Sophie will make the Christmas Sunday service tomorrow. And Sophie says yes.<br/><br/>Sophie gets a phone call from her lover, District Attorney Ty. She worries he wants to define the relationship before the holiday. However, it's a digitized voice on the phone. The caller says they have Ty and will kill him if Sophie doesn't deliver $5,000 per their instructions.<br/><br/>Ty leaves work until he gets a phone call from an unknown caller. They have Sophie, and they will kill her if he doesn't pay $5,000. And they send a photo of Sophie drugged on a mattress to prove it. These two are on a mission to save the other and don't know it. Will they discover the truth or give in?<br/><br/>This film is something you have to watch from the beginning. There are several moving parts, back-handed dealings, and several bad guys. The fight scenes are well choreographed and use everyday objects to make it believable. And be prepared for a cliffhanger that will leave some stunned. But fans of the genre will suspect it.<br/><br/>3.5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/swornjusticetakenbeforechristmas.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/swornjusticetakenbeforechristmas.jpeg"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"Christmas Angel",
-		"review":"Pop singer Angel St. James wraps up her performance and interview on The Tamar Braxton Show and rushes home to meet her fiancé, super producer E.J., for their anniversary. Angel enters her house to find it empty. E.J. forgot about their anniversary and can't leave. He tells Angel to buy whatever she wants at his expense, and Angel hangs up. She tears up her tickets to Aspen and gets in the car to drive to her hometown.<br/><br/>Angel attempts to send a text and runs off the road. A tow driver, Calvin, pulls over to assist her. She offers to pay extra to have him fix it now, but he can't because her car is electric. He knows an Airbnb she can use until he fixes her car. Angel agrees and gets comfortable. Calvin and his daughter, Liz, visit Angel in the morning. Angel learns that Calvin is a widower and paralegal with several properties, including her Airbnb. His sister-in-law, Natalie, helps with Liz, but Liz still misses her mom. Angel gets inspiration from Liz and considers ending her relationship with E.J.. But the break-up could have a devastating effect on her career. Is Angel willing to risk it all to have everything?<br/><br/>This movie starts with the typical rom-com plot. However, the script adds more drama to wet your thirst. Natalie blackmails, E.J. returns, and Calvin rages. Also, Liz acts her age and isn't a brat. This film also provides catchy and original music. This movie is fun but for teens and up.<br/><br/>4 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/christmasangel.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/christmasangel.jpeg"
 		}
 	}
 ];
@@ -56,24 +56,7 @@ module.exports = videoOnDemand;
 
  	/*
 
-	{
-		"option":"1",
-		"mtitle":"The Christmas Wringer",
-		"review":"<br/><br/>* out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thechristmasringer.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thechristmasringer.jpeg"
-		}
-	},
-	{
-		"option":"2",
-		"mtitle":"A Royal Christmas Surprise",
-		"review":"<br/><br/>* out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aroyalchristmassurprise.png",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aroyalchristmassurprise.png"
-		}
-	}
+	
 
 
 	{
@@ -81,8 +64,28 @@ module.exports = videoOnDemand;
 		"mtitle":"Candy Cane Lane",
 		"review":"<br/><br/>* out of 5 stars.",
 		"image":{
-			"smallImageUrl":"",
-			"largeImageUrl":""
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/candycanelane.jpg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/candycanelane.jpg"
+		}
+	}
+
+	{
+		"option":"1",
+		"mtitle":"Never Alone For Christmas",
+		"review":"<br/><br/>* out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/neveraloneofchristmas.jpeg",
+			"laregImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/neveraloneofchristmas.jpeg"
+		}
+	}
+
+	{
+		"option":"1",
+		"mtitle":"Mr. Monk's Last Case: A Monk Movie",
+		"review":"<br/><br/>* out of 5 stars."
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mrmonkslastcaseamonkmovie.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mrmonkslastcaseamonkmovie.jpeg"
 		}
 	}
 	
