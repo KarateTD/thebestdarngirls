0ad30ec7084f11dc5c4927841cc244f4498d1a49
Tiffany Gray <KarateTD@yahoo.com>
2023-12-08 17:01:07 -0500

    update vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option":"1",
+		"mtitle":"Mr. Monk's Last Case: A Monk Movie",
+		"review":"Adrian Monk's publisher reads his extensive manuscript and cancels Monk's book deal. She tells Monk to return his advance, and he panics. Monk's stepdaughter, Molly, took care of Monk during the pandemic. And in exchange, he would pay for her wedding. Now, he has to tell Molly he can't offer her a lavish nuptial. Monk leaves the publisher's office and looks out a window. He considers ending his life as he unlocks it, but Trudy stops him.<br/><br/>Monk and Molly wait at the airport for Natalie and Randy to arrive. Molly pulls Natalie aside and warns her that Monk is worse. He was better after solving Trudy's case. But the pandemic set him back. She thinks working will help, but Monk refuses to take on new cases.<br/><br/>The day before the wedding, Molly's fiancé, reporter Griffin, measures his bungee cord for his semi-annual jump. She accompanies Griffin to the jump point but doesn't watch. Molly hears spectators cheering, then screaming. She runs to check. Someone tells her Griffin's cord was 6 feet too long. Griffin is dead.<br/><br/>A week later, Monk, Natalie, and Randy attend the coroner's ruling with Molly. The coroner rules Griffin's passing as death by misadventure. Molly doesn't believe it. Someone threatened Griffin recently. He interviewed the wealthiest man in the world, Rick Eden. Griffin had evidence that Rick killed his partner to inherit his half of Eden Industries. Then, Rick threatened Griffin's life. Molly knows Rick did this but needs Monk to leave retirement and prove it. Monk is on the case. You'll thank him later.<br/><br/>Dear Monkaphiles, are you ready to have your minds blown and hearts melted? The movie returns to the first episode as the publisher reads Monk's manuscript. However, your heart breaks because Monk is counting down to the end. For him, this is candidly his last case. Fans will get the hand gestures, ticks, and 'here's what happened' that we love. The conclusion is satisfying, respects the fans, and expresses Monk's importance. The post-credit scene gives this modern-day Sherlock Holmes the Watson he needs. You will want to watch this movie again and again.<br/><br/>5 out of 5 stars.",
+		"image":{
+			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mrmonkslastcaseamonkmovie.jpeg",
+			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mrmonkslastcaseamonkmovie.jpeg"
+		}
+	},
+	{
+		"option":"2",
 		"mtitle":"Never Alone For Christmas",
 		"review":"Lauren stays home for Christmas after the airline cancels her flight. Her friend and her friend's new husband are having a Christmas party. So, Lauren will spend the holiday with them.<br/><br/>Lauren walks to the bar, and the bartender makes her a heavily spiked eggnog. Lauren asks who ordered it, and the bartender points to Miles. He is a veterinary technician who wants to help pets in poor communities. Also, Miles loves to tell jokes.<br/><br/>Within a year, Lauren and Miles fall in love, move in together, and get engaged. Lauren prepares for the wedding, but Miles won't take it seriously. He makes jokes and sets up pranks all the time. Lauren confesses to her friend, Dina, that everything isn't a joke, and she's tired of Miles's antics. Then, their boss, Preston, knocks on the door. Lauren checks her makeup while Dina lets him in. He wants Lauren to create a new pitch for their app, Rebound to Love. Lauren enthusiastically takes the assignment.<br/><br/>Lauren comes home and decides to end her relationship with Miles. He wants to try counseling, but it's too late for Lauren. Lauren uses her relationship failure to create a campaign and contest for the app. The contest is for matched members to have a vacation at Ms. Ramona's Cajun Christmas in New Orleans. Lauren thinks it would be a brilliant idea for her and Preston to go and moderate the contest. He agrees with a smile.<br/><br/>Lauren and Preston arrive at Ms. Ramona's and check into rooms across the hall. Then, Miles exits the room next to hers. Miles explains that he and his date, Raven, won the trip, and he is not leaving. Lauren decides to focus her romantic attention on Preston while working. Lauren will learn that what you want isn't what you need.<br/><br/>With a world based on Instagram love, it's commonplace to see people play pranks on their significant other. How far is too far? Lauren loves Miles's jokes but wants him to be serious about their wedding, future children, and career. Miles discovers funny is good, but not all the time. He needs to do the work and show he can be serious when it counts. This movie is funny and worth a watch.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"2",
+		"option":"3",
 		"mtitle":"Candy Cane Lane",
 		"review":"Chris creates his lawn ornaments by hand for the Candy Cane Lane competition. However, his neighbor, 4-time winner Bruce, relies on inflatables. Chris refuses to change and chooses quality over quantity. Chris and his wife, Carol, prepare their children for school. He grabs his daughter's backpack and finds a Notre Dame brochure. Joy admits she doesn't want to attend her family's alma mater, USC. Carol asks their son, Nick, about his math grades. Nick doesn't care about math because he wants to pursue a music career. And Chris worries his youngest daughter, Holly, doesn't believe anymore.<br/><br/>Chris's boss, Lee, calls Chris into his office. Lee sold the company. Now, Lee will let go of half the sales department. Chris is on the wrong side and has to pack up his office.<br/><br/>Chris and Carol walk to Bruce's house for a party, but Chris stops them before they reach the door. He tells Carol he got fired, and she lifts his spirits. During the party, they watch news about the Candy Cane Lane competition on Bruce's TV. The reporters announce that the competition has sponsors, and this year's winner will get $100,000.<br/><br/>Chris wants to buy decorations to get a leg up on the competition. Chris and Holly ride through several detours. They find a shop, Kringles, under a bridge. Shop owner Pepper jumps out of the shadows and shows them around the shop but warns Holly not to play with the dolls in the dollhouse. Chris purchases several items, including a 12 Days of Christmas tree, and Pepper prints the receipt. She tells him to ignore the fine print and sign. Chris signs his name and heads home with his items.<br/><br/>He follows the unusual instructions and assembles the tree. After a rough start, Chris undoubtedly wins the competition. Chris wakes up to see the characters from the song are missing from the tree. Chris runs to Kringles to get spare parts for the tree. He walks to the dollhouse and discovers the dolls are alive. They know Chris signed the receipt like they did. They tell Chris to remember the song Pepper sang because she gave Chris a task. Chris remembers Pepper told him to find the golden rings. And he has until Christmas to uncover them all. But they warn him that Pepper likes to change the rules so she can always win.<br/><br/>This movie switches from real-world issues to a fantasy film within 30 minutes. Then, the script gets wild with an evil elf, Santa, talking ornaments, and a strange milkmaid. However, the conclusion brings the plot back to earth. Chris finds the meaning behind the holiday and understands his children need to follow their chosen paths. This realization saves the story.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"3",
+		"option":"4",
 		"mtitle":"The Christmas Ringer",
 		"review":"Nicole started singing in her father's church. Today, she is a has-been R and B star with a memorable Christmas song. Nicole performs for a local woman's birthday party but freaks out when they ask her to sing her biggest hit. She refuses to sing and bumps into the birthday woman when she leaves. Nicole returns to her holiday set and finds the crew packing up. The stage director informs Nicole that her check bounced.<br/><br/>Nicole and her manager look over her accounts. They are empty or overdrawn. Nicole realizes her fiancé, Cliff, took the money and ghosted her. Also, someone filmed Nicole knocking down the woman, and it went viral. Nicole's manager suggests Nicole leave LA and visit her hometown. She should film herself doing charitable acts to change the public's perception.<br/><br/>Nicole returns home to an unenthusiastic welcome. The church fell on hard times because the angel investor stopped donating three months ago. Nevertheless, Nicole's mother has an idea. The church won the lottery to host The Christmas Choir Classic. The grand prize is $25,000. Nicole's mother asks Nicole to lead the choir. Can Nicole nab the choir a win without them discovering she is using them for clickbait?<br/><br/>This movie focuses on mending old wounds. The largest one Nicole has to fix is within herself. She doesn't want to sing her trendiest song because it dredges old memories. Today, Nicole sees that avoiding the past doesn't make it go away. This film has some astounding singers and catchy holiday songs.<br/><br/>4 out of 5 stars.",
 		"image":{
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option":"4",
+		"option":"5",
 		"mtitle":"A Royal Christmas Surprise",
 		"review":"Riley and Mandla are successful in their personal and professional lives. Their relationship is more loving than ever because Mandla's parents, King Chidike and Queen Nobantu, accepted Riley as a future wife, princess, and queen. Riley and Mandla decided to throw an engagement party to announce their upcoming nuptials to their friends. Also, they will use the party to introduce Mandla's parents to Riley's relatives, Uncle Ray and Aunt Gina.<br/><br/>Chidike and Nobantu fly to Georgia and travel to Ray and Gina's home. Ray and Chidike greet each other like old friends, but Nobantu and Gina size up each other. Gina remembers how Nobantu made Riley feel in Africa, so she plans to pay Nobantu back. Nobantu won't take Gina's disrespect. She will fight fire with fire while keeping her head held high. They will go for the jugular, and Riley must play peacemaker. Can she keep it up when Mandla drops a bomb on her?<br/><br/>This sequel is a wildly different movie than the first, and that's a good thing. It's funnier, wilder, and has more heart. Don't be surprised if you must pause the TV because you are laughing too hard to hear the dialog. Nobantu and Gina don't pull punches, and their is plenty of collateral damage. Amid this craziness, Mandla keeps a life-altering secret from Riley. You laugh while waiting with bated breath for the other shoe to drop. Viewers will look forward to film three if this series keeps its trajectory.<br/><br/>4.5 out of 5 stars.",
 		"image":{
 			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aroyalchristmassurprise.png",
 			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aroyalchristmassurprise.png"
-		}
-	},
-	{
-		"option":"5",
-		"mtitle":"So Fly Christmas",
-		"review":"Wyvetta stands at the Atlantic City United Church and Casino, waiting for her fiancé Floyd to enter. When he doesn't show, Wyvetta's best friend, Dione, takes Floyd's place. Instead of a wedding in holy matrimony, they cement their friendship forever.<br/><br/>On their podcast, Di and Wy So Fly, Dione questions callers about when they take down their Christmas trees. Wyvetta ignores the topic and says she is 'Done with Love.' She decides to say no to love and romance, but not sex. Dione gives up love and relationships too.<br/><br/>Eleven months later, Dione and Wyvetta have branded their 'Done with Love' message, and their friendship is strong. Wyvetta spends the evening with her parents, Harriet and Myron, while Dione meets her daughter, Sierra, at a diner. Sierra leaves earlier, and a man, Clarence, approaches Dione, but she turns him down before he can speak. Clarence corrects Dione because he wants her extra ketchup. T He invites Dione to eat alone with him. The spark between Dione and Clarence grows, but Dione wants to keep him a secret from Wyvetta. Dione continues to engage in the 'Done with Love' movement. What will happen when Wyvetta learns the truth?<br/><br/>This movie isn't a rom-com in the classic way. It's a friendship comedy. The bond between these women takes center stage. Dione and Wyvetta support each other during their movement. However, Dione has to hide her romance around Wyvetta. Everyone has been a Dione, a Wyvetta, or both. Like these friends, you can grow and be better.<br/><br/>5 out of 5 stars.",
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/soflychristmas.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/soflychristmas.jpeg"
 		}
 	}
 ];
@@ -56,14 +56,6 @@ module.exports = videoOnDemand;
 
  	/*
 
-	{
-		"option":"1",
-		"mtitle":"Mr. Monk's Last Case: A Monk Movie",
-		"review":"<br/><br/>* out of 5 stars."
-		"image":{
-			"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mrmonkslastcaseamonkmovie.jpeg",
-			"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mrmonkslastcaseamonkmovie.jpeg"
-		}
-	}
+	
 	
 	*/
