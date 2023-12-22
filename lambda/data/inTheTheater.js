a01fe108a5f12af2ca65c97811d181ac65820a16
Tiffany Gray <KarateTD@yahoo.com>
2023-12-22 00:14:33 -0500

    add aquaman

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Aquaman and the Lost Kingdom",
+    "review":"Since Arthur became the King of Atlantis, he married and had a son, Arthur Jr.. However, he still declares himself Aquaman for the surface dwellers. Arthur loves being a dad, but the job of King disenchants him. He dislikes the political aspects of the job, and a council stops his ideas.<br/><br/>Dr. Stephen Shin works with David Kane to find Atlantis. Stephen travels to the icy corners of the globe and falls into a cavern. They follow a trail to a cavern pool and dive in. David finds a broken black trident and puts the pieces together. The creator of the trident promises to help David kill Aquaman, every person Aquaman loves, and everything Aquaman loves. But only if David frees the creator. He starts raiding ancient Atlantean artifacts and a fuel source called orichalcum. Soon, David raided all the stockpiles except for the one in Atlantis.<br/><br/>Arthur sees news reports for the surface world that the climate is haywire. Then, he goes to Atlantis and discovers something is poisoning the sea life. The council blames the surface, but Arthur assures them that the surface isn't to blame. Then, David strikes Atlantis and injures Aquaman's wife, Mera. He steals their stockpile of orichalcum before making his escape. Arthur's mother, Atlanna, and Mera's father, Nereus, uncover that David has been stealing the orichalum for five months. Using the fuel caused greenhouse gases to emit at an exponential level. Arthur wants to find David and stop him from destroying the plant, but only one man would know where David hides: Orm. Arthur must break his brother out of the Fisherman Kindom's prison undetected, find David, and save the world.<br/><br/>This sequel is more concise than its predecessor. It introduces viewers to new worlds while building on beloved characters. While the script doesn't state it, this film is a redemption story for Aquaman. The opening scene gives viewers a synopsis of the first film and swiftly introduces Arthur's updated roles. The dialog has a quick wit and doesn't force an unnecessary love story. The graphics have improved in this film, but 3D isn't necessary. There is one cringy mid-credit scene, but nothing in the post-credits. The conclusion opens the door for part three but doesn't promise it.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aquaman2.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aquaman2.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Wonka",
     "review":"Willy Wonka dreamed of being a magician while his mother worked by the river. She would bring him a cocoa bean every night and make him a personalized chocolate bar on his birthday. He thinks it's the best chocolate in the world, but his mother says the best chocolate is at the Galeries Gourmet. Willy wishes they could open a shop there and sell his mother's chocolates.<br/><br/>Willy sails into town with his travel chocolate factory, tattered clothes, 12 silver sovereigns, and dream of owning a chocolate store in Galeries Gourmet. It doesn't take long for Willy to spend and lose all his money. Then, Bleacher approaches to pull his dog away from Willy. He knows a place Willy can stay that's affordable. Mrs. Scrubitt owns a laundry and boarding house. She will let Willy stay one night if he pays one silver sovereign by 6 pm tomorrow. A girl pops out of the backroom and warns Willy to read the fine print. Willy writes his name on the dotted line.<br/><br/>Willy travels to the Galeries Gourmet and introduces his hover chocolates to the crowd for one silver sovereign. Before anyone can try a piece, chocolatiers Authur Slugworth, Prodnose, and Fickelguber slink to Willy and grab a candy. They hate it because they believe chocolate should be simple and for the rich. Slugworth calls the Chief of Police to remove Willy for selling chocolate without a store. The Chief of Police takes Willy's earnings and escorts him out of the market.<br/><br/>Willy returns to Mrs. Scrubitt to pay his tab because he has one silver sovereign left. However, his current bill is 10,000 silver sovereign. Willy admits he can't pay it. So, he will have to work it off. Willy meets Mrs. Scrubitt's indentured servants - accountant Abacus, plumber Piper, comedian Larry, telephone operator Lottie, and the little girl Noodle.<br/><br/>After a day of scrubbing, Noddles takes Willy to his sleeping quarters. Willy wants to leave to sell his chocolates but can't figure out how. She gives him an idea. Willy believes he will have the money to pay Mrs. Scrubitt and buy a shop soon. However, Slugworth, Prodnose, and Fickelguber, also known as the Chocolate Cartel, conspire against Willy. Willy will need more than his dream and friends to survive. He will need an Oompa-Loompa.<br/><br/>Inspired by Charlie and the Chocolate Factory, this musical prequel is a delight for the eyes and ears. The vibrant colors are for the most massive screen possible, and the music will spark your imagination. The plot provides long-time fans with Willy's background and pays homage to the original story. This musical is hilarious with a humous seduction, a thieving Oompa-Loompa, and a growing Chief of Police. Along the way, Willy makes fruitful and loving connections. And don't leave immediately, because Lofty will update the audience on everyone's whereabouts. This film is worth its weight in chocolate.<br/><br/>4.5 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"The Shift",
     "review":"Kevin watches the news report about his hedge fund company closing its doors and firing all its employees after the housing market crashes. He sits at a bar with a gold AA chip in one hand and a beer in the other. Molly walks up to him as he puts the glass to his lips and calls him the saddest, most pathetic man she has ever seen. She offers tea instead, and Kevin leaves his untouched beer behind.<br/><br/>Years later, Molly and Kevin's marriage falls apart, and Kevin works a dead-end job. Kevin drives and calls Molly on the phone. A truck hits him. Kevin is in the alleyway moments later, and a man slaps him. Kevin regains consciousness, and the man calls Kevin by name. Then Kevin notices the streets are empty. When Kevin tries to call the police, the man laughs. The man calls himself 'The Benefactor' and invites Kevin to eat.<br/><br/>The Benefactor explains that he took Kevin out of his world through a shift. The Benefactor has Kevins all over the multiverse working for him as Shifters. The Benefactor gives Kevin a choice. He can go back to his life with an unhappy wife and a horrible job. Or Kevin can become a Shifter and move people throughout the universe. Kevin immediately starts to pray to God. The Benefactor becomes furious and tells Kevin that God won't protect him. Then, The Benefactor disappears. He promises God that he will return to his universe and Molly no matter how long it takes. But can Kevin keep the faith in The Benefactor's chaotic and bleak world?<br/><br/>This movie is the science fiction telling of Job. It has futuristic devices like the deviator to transport people from one universe to another. The Benefactor tries to prove to Kevin that God abandoned him. But Kevin secretly passes the gospel to the people. The shift sequences are well-timed, and the movie packs an emotional punch for people who have lost children. While the movie is entertaining, you won't want to buy it on DVD.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Silent Night",
     "review":"Brian runs in an ugly Christmas sweater, a jingle bell necklace, and bloody hands. He finds the drive-by shooters in a high-speed chase. The driver gets out, beats Brian, and shoots Brian in the windpipe. Brian wakes up in the hospital. He has motor functions but has lost his ability to talk and needs an electrolarynx to speak.<br/><br/>After gaining the ability to walk with a cane, Brian returns home with his wife, Saya. Brian remembers his last moments with Taylor. They were playing in the front yard, enjoying the holiday. Then, two gang members drove down their street, shooting at each other, and a stray bullet hit Taylor. Brian lays in Taylor's bed and comes up with a plan. Brian will research, train, and stock supplies until Christmas Eve. That night, he will kill them all.<br/><br/>This film is an action-driven movie because there is zero dialog. There are a few lines in text messages, but you can count them on two hands. The battles are bloody and entertaining. Brian doesn't believe taking down the gang is enough. Instead, he must kill them to even the score. This movie is a nod to Joel's acting skills. He must convey depression, fury, and profound loss without a single word. While you won't want to buy this movie, it's worth seeing in the theaters for action fans.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Napoleon",
     "review":"Napoleon stands guard as the people of France behead Queen Marie Antoinette. Revolutionary leader Paul Barras asks Napoleon what the troops need on the battlefield. Then, Napoleon alerts Paul that Toulon is a port city. If they take siege of the port, France will control the city. Next, Napoleon and his army executed Maximilien Robespierre and ended the Reign of Terror to restore order in France.<br/><br/>Napoleon enjoys his new title of General at a party and notices a woman, Joséphine, playing cards. She boldly approached him as he stammered. Later, Joséphine sends her son to retrieve his deceased father's weapon. He takes pity on the boy and returns the weapon to see Josephine standing atop the staircase. Soon, Napoleon takes Joséphine as his bride. With love conquered, Napoleon only has one more thing to conquer: the world.<br/><br/>The rise of Napoleon is as astounding as it is complex. He was brilliant on the battlefield but didn't have the social graces of a politician. This movie covers 23 years of Napoleon's rise and fall. The director, writer, costume designer, and set designer paid great attention to detail. The beheading and battles were unadulterated bloodbaths that left nothing to the imagination. However, the movie begins to lull after Joséphine's death. The big screen is the best way to see this movie, but it's better for history buffs.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/napoleon.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/napoleon.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Wish",
-    "review":"Asha is a few months shy of her 18th birthday and gives tours of her homeland, Rosas. Rosas is famous. King Magnifico learned sorcery after wicked rulers destroyed his home. Then, he created Rosas and invited anyone to come. He uses his power to grant one wish every month in a ceremony. On a person's 18th birthday, the king asks for their wish. The person gives it to him and never remembers it. Today is the 100th birthday of Asha's grandfather, Sabino. Everyone thinks Magnifico will grant his wish in the ceremony.<br/><br/>Asha finishes another tour and runs to her friends for encouragement. She has an interview to be the king's apprentice. Queen Amaya enters the kitchen and asks Asha to speak to the king. He lets her enter the room of wishes. Asha looks at everyone's wishes in wonder and finds Sabino's. He wished to inspire people with music. Asha asks Magnifico if Magnifico will allow Sabino's wish, and Magnifico says no. He doesn't know if Sabino will motivate people to love Magnifico or overthrow him. Since Sabino's wish is too vague, he won't grant it. Asha tells Magnifico to return the wish if he won't allow it because remembering it will give Sabino hope. Magnifico refuses. He rejects Asha's application for the apprenticeship.<br/><br/>Asha tries to warn Sabino and her mother, Sakina, about Magnifico, but they don't want to hear it. Asha runs to her father's favorite spot and makes a wish upon a star. A bright light comes from the sky, and all of Rosas, including Magnifico, feels it. A star comes from the sky and gives Asha's goat, Valentino, the ability to talk. Asha decides to break into Magnifico's castle and free the wishes. Magnifico believes the unknown magic is a threat to his kingdom. And he will seek and destroy it no matter what.<br/><br/>The animation and songs are second to none in this film. King Magnifico is the classic Disney villain. Also, the film makes nods to Little Jon, Bambi, Thumper, and more beloved Disney characters. The trailer is tricky because it makes viewers think Asha wants Magnifico to grant their wishes. However, she wants Magnifico to return the wishes he won't allow to restore hope, imagination, and invention to the people. This film isn't saying to grant all wishes. It's saying wishes inspire hope, so you shouldn't forget them. That's the message for kids.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/wish.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wish.jpeg"
     }
   }
 ];
@@ -58,13 +58,30 @@ module.exports = inTheTheaters;
 
 {
   "option":"1",
-  "mtitle":"Aquaman and the Lost Kingdom",
+  "mtitle":"Migration",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aquaman2.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aquaman2.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/migration.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/migration.jpeg"
+  }
+},
+{
+  "option":"2",
+  "mtitle":"The Iron Claw",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theironclaw.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theironclaw.jpeg"
+  }
+},
+{
+  "option":"3",
+  "mtitle":"Anyone But You",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/anyonebutyou.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/anyonebutyou.jpeg"
   }
 }
-
 */
 
