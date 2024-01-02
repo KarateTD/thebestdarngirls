69c7b8671eaf85c14e1f2b45d62749274625070a
Tiffany Gray <KarateTD@yahoo.com>
2024-01-02 18:03:57 -0500

    fixes

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -148,4 +148,54 @@ module.exports = mustBuy;
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/wonka.jpeg"
     }
   }
+
+    {
+    "option":"1",
+    "mtitle":"The Color Purple",
+    "review":"Celie and her sister, Nettie, reminisce about their mother when Celie's water breaks. Celie gives birth to a baby boy and names him Adam. Then, her father, Alfonso, enters to give the baby to God. Celie begs for more time with him, but her father says no. It's his baby, and he can do what he pleases.<br/><br/>Nettie walks home from school, and Mister follows her on a horse while serenading her. She tells him to leave her alone. Mister follows Nettie home and asks Alfonso for her hand in marriage. Alfonso says no. However, Celie means nothing to him, but she can work. As the men barter, Nettie and Celie watch. Alfonso tells Celie to pack her bags because she will marry Mister.<br/><br/>Celie, on foot, follows Mister on horseback to his home. He introduces her to his kids and tells her to start cleaning. Celine prepares dinner, and Mister demands to know why she didn't bathe the kids. She suggests doing it in the morning, and he slaps her to the floor. Late at night, Celie lays in bed and sees a picture of Shug Avery. Mister admits that's the woman he should've married. As Mister climbs on Celie, Celie stares at Shug's picture and dreams about her.<br/><br/>A few days later, Celie hears a knock at the door. It's Nettie. Alfonso started touching her, so she ran away. Celie begs Mister to let Nettie stay. He says yes. Everything seems to be going well until Mister creeps into Nettie's room. She pleads with him to stop and fights him when Mister resists. He grabs his gun and drags her outside. He threatens to shoot Nettie if she ever returns. When Celie tries to leave, Mister pulls her back and refuses to let Celie go. Nettie promises to write Celie once a week to let Celie know she is alive. But the letters never come, and Celie falls deeper into a loveless marriage of abuse. What will it take for Celie to discover a way to love herself and triumph over her dire circumstances?<br/><br/>Based on The Color Purple: A New Musical, this movie doesn't change the story. It only adds music to the plot. Now, the writers shorten the beginning between Nettie and Celie talking about a man doing his business. Also, the writers removed most of the internal monologue. The movie passes the torch between the original Celie and the current. Also, the sets change from a practical set to a stage setting without taking the audience out of the moment. The vocal performances are second to none, with perfect comedic timing. Be ready to sing, dance, cry, and stand taller after seeing this gem come to life on the silver screen.<br/><br/>5 out of 5 stars.",
+    "asin":{
+      "re":"B0CQ6JK2D9"
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecolorpurple.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecolorpurple.jpeg"
+    }
+  }
+
+  {
+    "option":"2",
+    "mtitle":"The Boys in the Boat",
+    "review":"In 1936, Joe Rantz attended the University of Washington for engineering. He needs to pay for the 2nd half of this semester in two weeks. After engineering class, Joe's friend, Roger, tells him the rowing team will pay crew members and offer housing. Coach Al Ulbrickson introduces the men to the other coaches, including Coach Thomas Bolles and the boatbuilder George Pocock. The coaches will decide who will join the varsity and junior varsity 8-men teams.<br/><br/>After a grueling tryout, Al picks Joe, Roger, and the other men. The president says they need to win, and he can ask the former students for more money. But winning one race isn't enough. They need to win gold at the Olympics. After the JV team races in the regatta and beat the best time ever by seconds, Al gets an unorthodox idea. Instead of sending the varsity team to compete in Poughkeepsie, he can send JV. The president, donors, and varsity team angrily erupt after Al's announcement. However, he won't budge. Can the JV team prove Al right by making it to the Olympics and winning gold?<br/><br/>Based on The Boys in the Boat: Nine Americans and Their Epic Quest for Gold at the 1936 Berlin Olympics by Daniel James Brown, this film focuses on Al, Joe, and their obstacles. Al went with his gut and made an unpopular choice. And he suffered along the way for it. Each time the team hit a new impossible milestone, someone moved the goalpost. Joe raised himself since he was 14. He has to learn how to depend on others and become one. This movie will make you cheer in your seat, but you won't have the urge to delve deeper into this historic win.<br/><br/>4 out of 5 stars.",
+    "asin":{
+      "re":""
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theboysintheboat.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theboysintheboat.jpeg"
+    }
+  }
+
+  {
+    "option":"4",
+    "mtitle":"Migration",
+    "review":"Mack and Pam Mallard swim in their pond with their ducklings, Gwen and Dax. Mack tells the ducklings that life beyond the pond is dangerous and filled with duck-eating herons. Pam sees another flock fly above in a migration formation, and they land in their pond. Mack looks for his kids and sees Dax talking to a duck in the other flock named Kim. Kim tells Dax that her family will fly to Jamaica for the winter and see water that glows. A smitten Dax wants to go, but Mack says no. The flock flies away, and Pam and Mack put their kids to bed.<br/><br/>Mack accidentally sits on his Uncle Dan. Dan agrees with Mack to stay in the pond's safety. Dan says Mack can be secure and die alone like him. Mack quickly changes his mind and wakes up Pam. He wants to migrate to Jamaica, and everyone jumps for joy. They do their manual checks and fly south. However, they notice other flocks moving in the other direction. Mack and Pam decide to stay on their path. Adventure awaits.<br/><br/>This movie is charming and well-executed. The graphics are flawless as the Mallards fly through the sky from the pond to New York. If you get motion sickness, don't see this film in 3D. Unlike most Illumination movies, the script doesn't introduce the audience to the antagonist until the halfway mark. The Mallards open their eyes to a new world, new adventures, and new problems, but they hold solid as a family. This movie is a G version of the National Lampoon vacation films. And come early to see the Minions-Vector short.<br/><br/>4 out of 5 stars.",
+    "asin":{
+      "re":"B0CQ6MN2R1"
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/migration.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/migration.jpeg"
+    }
+  }
+
+  {
+    "option":"5",
+    "mtitle":"The Iron Claw",
+    "review":"Jack Von Erich exits the arena after a grueling match to find his wife, Doris, and two sons waiting for him outside. Doris asks Jack how much the new Cadillac attached to their mobile home costs. Jack tells Doris he has to look like the NWA champion to get the belt. Doris prays for guidance in the front seat. Doris believes God will protect the family from the Von Erich Curse, but Jack thinks it's professional wrestling.<br/><br/>Years later, Jack runs WCCW promotion with his son, Kevin, at the top of the card. Jack and Doris's other sons, Kerry, Mike, and David, beam with pride at Kevin's in-ring accomplishments. Jack lets his sons know he has favorites in a particular order, but they are subject to change. Kerry is gearing up for the 1980 Olympics in Moscow, and Kevin will wrestle Harley Race in a non-title match. Jack announces it's David's time to get into the squared circle and tag with Kevin.<br/><br/>Kerry returns home after the U.S. pulls out of the Olympics. Jack adds Kerry to the family business and makes Kevin, Kerry, and David compete in 6-man tag team matches. After the Von Erichs battle, Jack announces that David will go for the NWA championship. Kevin knows he has gone down a rank on Jack's list. The Von Erichs will fight to be at the top of Jack's list and the wrestling world while fighting the family curse.<br/><br/>This movie is a stunning tribute to the Von Erichs by bringing their story to a peaceful end. The casting was top-notch, and the actors embodied their roles. You can sense the deep love these brothers had for one another. The director overlays the brothers in the locker room, and viewers see their similar struggles and suffering. It's unbelievable that one family went through so much tragedy. This movie deserves to be seen on the big screen because it is honest perfection.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theironclaw.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theironclaw.jpeg"
+    }
+  }
+
 */
