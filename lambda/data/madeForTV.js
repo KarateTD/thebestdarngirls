5386773767ac4f36f8f67eea3e1d5c7e0983bcb1
Tiffany Gray <KarateTD@yahoo.com>
2024-01-07 03:41:31 -0500

    add 2 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Love on The Right Course",
+    "review":"Whitney competes in the Red Pines golf tournament, but everyone can see she has lost a step since her mother, Coach Alice, died two years ago. Whitney's long-time caddy, Andrew, acts as her Interim Coach, but she hasn't officially hired him. Whitney loses the tournament but qualifies for the Eastern European Open next month. Andrew wants to return to Arizona immediately to practice, but Whitney says no. She must travel to Budapest to spend time with her father, Marton, on his birthday. Whitney's lack of focus frustrates Andrew, and he quits.<br/><br/>In Budapest, Whitney plays at Marton's golf resort with noise-cancelling headphones. She doesn't hear Daniel drive by in his golf cart. Daniel has been the course pro for a year. He gives Whitney a quick tip and drives away. Whitney meets with Marton and encourages him to get more involved with the golf course and his friends. He wants to sell it. Whitney respects Marton's decision but needs him to think it through. As she opens Marton's mind to healing, Whitney will learn new techniques with Daniel. But will it end when she discovers Daniel is working against her too?<br/><br/>This movie is slow unless you like golf, and the pivotal conflict spikes in the last 30 minutes of the film. The lead characters don't have a romantic spark until the end. It's great to see Whitney come into her confidence, but viewers never worry about the golf course sale or Whitney's relationship with Andrew. If you want something to watch without using your full attention, this is your film.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveontherightcourse.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveontherightcourse.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Dying for Fame",
+    "review":"Juliet creates a workout story to inspire her followers and posts several pictures. Afterward, she walks to the bathroom for selfies and a shower. Juliet walks out to candles and rose petals. Her Romeo stalker, Holden, left them to impress her. She begs him to leave, but Holden won't give up. She fights him off and escapes.<br/><br/>Anna watches a TV starlet, Tessa, drunkenly stumble to the bathroom. Anna follows Tessa under the guise of helping but snaps photos of Tessa regurgitating in the toilet. Anna works for Hot Goss, an internet tabloid. She doesn't believe actors, singers, and influencers deserve their fame because they won the genetic lottery. Anna's boss, Lex, gives Anna an interview with Juliet. Anna asks her co-worker and friend, Artie, to investigate Juliet's followers. He uncovered that her followers didn't engage and followed each other. Juliet's followers are bot accounts.<br/><br/>Anna enters Juliet's home, and Juliet's sister, Meg, welcomes Anna. Then, Anna hears Juliet arguing with her boyfriend, Ryan, upstairs. After Ryan leaves, Anna interviews Juliet on Juliet's terrace. Anna questions Juliet about her followers. Juliet doesn't deny it but openly wonders why no one asks about her stalker. Anna considers stalkers the price of fame and doesn't care about that story. She takes Juliet's dodge as an admission that she bought followers. Anna writes the story, and fans cancel Juliet.<br/><br/>Anna wakes up to angry messages and comments because Juliet ended her life by jumping off her terrace. Juliet's family and friends accost Anna, so Anna shuts off her phone. Anna second-guesses putting out the article, but Lex says it's the right move. Lex offers Anna a drink and lets Anna leave for the day. Anna turns on her phone to find a message from Juliet moments before her death. Juliet forgave and thanked Anna for exposing her. Anna doesn't believe Juliet ended her life. She thinks Holden threw Juliet off the terrace. She decides to investigate for a new story. Can Anna handle her newfound fame and the public revenge?<br/><br/>Anna is a spider caught in a web she made. She doesn't care about people, only the story. While Anna starts as the villain, her rant may ring familiar to viewers. She looks down on celebrities and hate-watches them. She doesn't understand why people admire them but profit from their downfall. Viewers will instantly dislike Anna but continue to watch her evolve. Surprisingly, the script allows the audience to sympathize with Juliet. While the mystery is easy to solve, the plot holds a glaring mirror to society, influencer culture, and instafame.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dyingforfame.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dyingforfame.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Secret Love Triangle",
     "review":"Jess dreams about celebrating with her husband, Matt, and their two best friends, Robert and Robert's wife, Lori. Jess wakes up in her jail cell because her cellmate, Michelle, jostles her for saying Matt's name in her sleep.<br/><br/>Jess writes a letter to the Innocence Project, but Michelle tells her to give it up. Jess says Matt hired a private investigator to uncover new information. Jess swears she didn't kill Robert in a lover's rage. Matt visits Jess, and she asks if the P.I. found anything. Matt shakes his head no. Then, Matt drops a bombshell. He wants to call off the P.I., sell their home, and move out of state.<br/><br/>On work detail, Michelle, after having her early release rejected, breaks free of her shackles. Then she knocks out the two guards and jumps in the officers' van. She gives Jess one chance to go with her, and Jess jumps in the passenger seat. She tells Jess to run with her, but Jess wants to return to town and prove her innocence. Michelle hugs Jess and runs. Jess has no idea the tangled web she will unweave.<br/><br/>The mystery of why Jess is in jail is better than who killed Robert. The opening scene is the two couples at the party after Jess and Lori talk about the business. In the blink of an eye, Jess is in jail. This smooth revelation will give viewers whiplash and make them lean in. You will pick the antagonist after a few flashbacks and some Lifetime experience. This film starts strong but falls into the formula at the end.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"My Wife's Hidden Lover",
     "review":"Phoebe's husband, Ethan, reprimands her for not finding his coffee tumbler. Ethan leaves for his job as Mayor, and Phoebe runs to the attic. She kisses Lucas and brings him downstairs. Phoebe agreed to let Lucas secretly live in her attic for six months while he wrote his romance novel. She became his muse, and they fell in love.<br/><br/>As Phoebe juggles her husband and lover, Lucas's desperation grows. He wants Phoebe to commit to him. Later, Ethan admits to being a cruel husband and begs for a new start. Phoebe agrees, and Lucas overhears. Lucas threatens to expose their love affair, but Phoebe gives up. She tells Lucas to leave her home and exits to meet Ethan for an important announcement. Ethan screams at Phoebe not to attend the function. Phoebe returns home and runs upstairs, apologizing to Lucas. She finds Lucas dead on the attic floor with a blanket over his face. Ethan returns and expresses regret for his actions. He asks Phoebe to come again, but she says no. Ethan yells and walks out. Phoebe has nowhere to turn. Later, the body disappears, and Phoebe is pregnant. How can she evade this mess without ruining her future and Ethan's career?<br/><br/>Lifetimers take a deep breath because this story's protagonist is hard to find. Phoebe needs a backbone and therapy. She buckles no matter who shows her affection. Ethan puts her down and says, 'I'm sorry,' so she wants to start a new life with him. Lucas tries to be Ethan's opposite but slowly becomes controlling. This plot is an 'everyone sucks' storyline, except for Phoebe's best friend Jim. He is the only honest person in this film. This movie has a whodunit with unlikeable characters.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"A Christmas Letter",
     "review":"Leslie is a foster mom to 6 children - Sam, Wendell, Joey, Chucky, Shay, and Ariana. Siblings Wendell and Sam open their bedroom window. Sam climbs to the roof with her letter to Santa. She turns it into a paper airplane, prays, and pushes her letter into the wind. Leslie meets with her friend, Claudette, to talk about the repairs. Leslie has no idea how to come up with $50,000. She needs to get the repairs done, or the city won't give her a certificate of occupancy, causing the kids to return to the system.<br/><br/>David enters the study and finds his dad, Robert, sleeping with the window open. He shuts the window and updates Robert about his daily tasks before going to work at the post office. Robert wheels himself to the window and picks a letter. David grabs the letter from Robert's hand, assuming it is a to-do list. Then, he leaves for work.<br/><br/>Mr. Charles and Mrs. <lang xml:lang='fr-FR'>Milan</lang> Chatsworth rush into Leslie's home. They want to adopt Sam. However, Leslie doesn't want to separate Sam from her brother, Wendell. Then, Charles offers Leslie $50,000 to agree. She throws the Chatsworths out of her home. Soon after, David comes with the mail and offers to play hockey ball with the kids. Before leaving, David offers to give the kids a post office tour and let them put their letters to Santa on the mail truck to Santa's house. Wendell comes up with an idea. They will get on the mail truck and tell Santa their wish in person.<br/><br/>David takes the kids to the post office and introduces them to his co-worker, Hank. Hank shows them the mail truck is going to Santa's house and takes their letters. Leslie and the kids leave, and Hank puts the last bag into his vehicle. Then, Hank breaks his phone while he shuts the door.<br/><br/>Sam and Wendell get Joey and Chucky to squabble and distract Leslie on the walk home. The siblings run to the post office and hide in Hank's truck. Leslie realizes she is two kids short when she gets home. Shay reveals Sam and Wendell's plan. Leslie asks Claudette to watch the kids while she runs to the post office. She gets there, but Hank is gone. David knows Hank's route, so Leslie wants to follow Hank to retrieve her kids. While they follow the mail truck, Robert and Claudette brainstorm ways to save the foster home. They will need a Christmas miracle.<br/><br/>The best way to describe this film is cheesy but in the best possible way. This movie has the best balance of humor, romance, magic, community, and religion. It's for the entire family and will reaffirm your children's belief in Santa. Everyone has a wish and a greater purpose. This movie teaches that those may not be the same. One paper airplane will make your heart smile. So, sit back and enjoy this easy-going and delightful holiday film.<br/><br/>4 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/achristmasletter.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/achristmasletter.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Christmas Revisited",
-    "review":"Ida and Jo sit and chat in Nate's cabin while waiting for their old college friends to arrive and celebrate Christmas. Mer and Johnny are first, and Cameron follows. The group can sense that the tension between Jo and Cameron is thick. Nate's mother, Mrs. Carolyn, enters the cabin to greet them. Nate wanted them to get together before he passed away. Carolyn is happy to see his cabin full of life again. However, this friendly getaway will turn combative because everyone hides something upsetting about themselves. Also, every person holds an old grudge. These friends need to work out their issues or dishonor Nate's memory.<br/><br/>Nate was the lynchpin that held this group together. Without Nate, they wouldn't be friends. Since Nate died, their friendship broke at the seams. At first, they pretend everything is fine. Soon, they start seeing the cracks. It's easy for a viewer to connect with a character from this ensemble. And the script balances drama and comedy with ease. This movie is a marvelous way for OWN to end its 2023 holiday season.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/christmasrevisited.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/christmasrevisited.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Mom's Christmas Boyfriend",
-    "review":"Emma and her daughter, Lily, plan the seating arrangements for Emma's Christmas Eve dinner. Lily points out that everyone has a date but Emma. Emma and Lily go to the mall to buy donations for a needy family. A mall elf tells Lily about a Children's Christmas Wish contest. Kids write their wish, and the winner gets their wish granted and money for a charity of their choice. Lily throws caution to the win and writes a wish.<br/><br/>Zach retrieves the wish box from the mall elf and walks to his office but bumps into Emma along the way. They share a laugh, apologize, and retreat. Zach's co-worker, Brady, stresses over the new and predictable wishes because they haven't finished the last box. Their boss, Veronica, puts their jobs on the line. Zach stops at Lily's illustrated card as they read through the wishes. She wants her mom to have a boyfriend for the holiday and her charity family to receive the donation. Zach and Brady pick Lily as their winner.<br/><br/>Zach and Brady call Emma and Lily to announce Lily as the winner and get Emma's permission to film them. However, they leave out Lily's wish. They come to Emma's home with a camera crew and a reporter. The reporter asks Lily about her wish, and she tells the truth while Emma sits stunned. At the end of the interview, the reporter lets viewers know that they can email Emma directly through a link on the store's website. Emma selects the ruggedly handsome Clark. As Emma spends time with Clark, Zach follows for content and starts to fall for Emma. Will he reveal the truth or keep quiet for the wish promotion?<br/><br/>Thank goodness we have a child acting their age in a holiday movie. Lily is funny and sassy, thanks to her Aunt Britt. When viewers meet Clark, they will know he wants to be insta-famous. It's a stark contrast to Emma and her reserved nature. Also, the script lets viewers see the unbreakable bond between Lily and Emma. Lily is the shimmering glue that holds this predictable film together.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/momschristmasboyfriend.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/momschristmasboyfriend.jpeg"
     }
   }
 ];
@@ -58,20 +58,38 @@ module.exports = madeForTV;
 
 {
   "option":"1",
-  "mtitle":"Love on The Right Course",
+  "mtitle":"Girl in the Video",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveontherightcourse.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveontherightcourse.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/girlinthevideo.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/girlinthevideo.jpeg"
   }
 },
 {
   "option":"2",
-  "mtitle":"Dying for Fame",
+  "mtitle":"True Justice: Family Ties",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dyingforfame.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dyingforfame.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/truejusticefamilyties.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/truejusticefamilyties.jpeg"
+  }
+},
+{
+  "option":"3",
+  "mtitle":"The Wrong Life Coach",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thewronglifecoach.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thewronglifecoach.jpeg"
+  }
+},
+{
+  "option":"4",
+  "mtitle":"Romantic Friction",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/romanticfriction.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/romanticfriction.jpeg"
   }
 }
 
