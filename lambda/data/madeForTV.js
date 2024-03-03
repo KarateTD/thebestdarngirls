1a33969a593411898e956bb544b724885a322111
Tiffany Gray <KarateTD@yahoo.com>
2024-03-02 23:17:28 -0500

    add 3 tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Single Black Female 2: Simone's Revenge",
+    "review":"After watching her home burn down with Simone inside two years ago, Monica moves to Seattle with her best friend, Bebe, for a new job at Red Alert News. Bebe will be a hairdresser on set.<br/><br/>Monica and Bebe meet their new boss, Kendyl, and are immediately off-put by Kendyl's cold demeanor. Later, Bebe works on Monica's hair when the makeup artist, Layla, enters the dressing room. Monica tries to make small talk with Layla, but Layla isn't interested.<br/><br/>Monica sits at the anchor desk, and Kendyl takes her notecards away. Monica gracefully handles the change until Monica reads her life story to the audience. Monica regroups on air, interviews her guest, and finishes the segment. She can't escape her nightmares and feels Simone around her even though Simone is dead.<br/><br/>Sister Grace wishes she could remember her past but is grateful Sister Margaret saved her after she collapsed on the side of the road years ago. Grace works in a thrift store when she sees Monica talking about her stalker on the news. The report causes Grace's memories to flood back. She remembers her name is Simone and rages about Monica's report, slandering her name. Margaret encourages Simone to find the truth. She doesn't know that truth is deadly. Simone is out for revenge and won't stop until she gets it.<br/><br/>This sequel to Single Black Female ramps up the crazy quickly. Simone gets her memory back in the first 30 minutes and goes on a revenge rampage. Everything she does is slow and deliberate. Also, Monica isn't dealing with her psychological trauma from the attack. She has nightmares and sees Simone, but her fear is understandable. As a warning, it does take a while for the body count to start. But it quickly rises. And the ending will leave you ready for part 3.<br/><br/>4.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/singleblackfemale2simonesrevenge.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/singleblackfemale2simonesrevenge.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Lonely Crime Fanatic",
+    "review":"Ashley has another nightmare about her estranged husband, Paul, after listening to a crime podcast before going to bed. Her friend, Lily, warns Ashley that Ashley focuses on podcasts to avoid her divorce. When Ashley states she's heard them all, Lily mentions the Unsolved Files podcast starring David Bryant. Then, Paul uses his key to get inside the house. She pushes him out and declares she will only communicate with him through a lawyer.<br/><br/>Ashley researches David online and learns he has a book signing. She finds David talking to fans outside the store, and David politely corrects Ashley for fawning about his work. He wants her to remember that the podcast's subjects are real people. Then, he signs her book with his phone number and invites her to dinner. During dinner, David offers to show Ashley his studio and get her started in podcasting. Ashley believes this is the start of something beautiful. It will be a beautiful disaster.<br/><br/>This movie drags to an abrupt stop that you will beg for. No matter how much Lily tries to warn Ashley, Ashley keeps falling for distractions. And Ashley's final revelation feels like a critique on true crime podcasts, their listeners, and their subjects. It's a bit preachy for the wild setup. This film is a bore and, hopefully, the worst entry for the Wicked Revenge 2024 season.<br/><br/>1.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lonelycrimefanatic.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lonelycrimefanatic.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"The Soulmate Search",
+    "review":"Ellen hangs her latest commissioned installation in the Birchwood building as her cousin, Chloe, and agent, Angel, admires Ellen's work. Ellen tells them that an highly exclusive dating service, Eternity, delivered their famed black envelope. Eternity has a 100% success rate, but they have several rules. You must accept any date they send to you. Second, you have to stay on the date for 30 minutes. Third, Ellen can't ask who recommended her for Eternity, or she's out. And fourth, she has to recommend another candidate for Eternity once she makes her match. Ellen can never rejoin if Eternity pulls her from the program.<br/><br/>As Ellen hangs her painting, Will enters and criticizes her artwork. His family runs the building and the law firm on the top 2 floors. Attorney Will knows his opinion is the one that matters. Ellen ignores him as Will leaves.<br/><br/>Ellen gets a call from Eternity and accepts the dinner date. She goes to the restaurant, follows the host to her table, and sits. Ellen looks up and sees Will sitting across from her. Will stands to exit, but Ellen stops him. Ellen doesn't want to lose her membership. She reminds Will that they must stay for 30 minutes, so he sets a timer on his phone.<br/><br/>Ellen sadly reports to Chloe and Angel that she is Eternity's first failure. She wants to meet with them and have them explain why they thought Will would be a good match. Ellen's calls go unanswered, so she walks to the Eternity headquarters and finds Will standing outside. Will and Ellen realize Eternity conned them. Ellen tells her friends and wants to find the truth. However, Will desires to sue. So they begrudgingly decide to work together to bring Eternity down. Will they beat Eternity, or will Eternity win?<br/><br/>This movie needs your attention to see how everything comes together. As Ellen and Will investigate Eternity, they go on accidental micro dates. They dance, have a stroll, and eat. Ellen is a romantic, but Will thinks true love is a joke. Viewers will learn who nominated Ellen. However, Ellen did not officially choose the next candidate. So, the audience may get a part two.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesoulmatesearch.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesoulmatesearch.jpeg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Sense and Sensibility",
     "review":"The Dashwood sisters - Elinor, Marianne, and Margaret - stand by their father's deathbed with their mother. Mr. Dashwood makes Elinor promise to keep their family together. Then, their half-brother, John, enters. Mr. Dashwood will give the inheritance to John but makes John vow to take care of his half-sisters and stepmother. He accepts.<br/><br/>John comes to the property with his wife, Fanny. Elinor walks the ground with Edward, Fanny's half-brother, and he smittens her. Edward knows Fanny and his stepmother, Mrs. Ferrars, will never approve of their relationship because Elinor is below their station. Before they part, Edward gives Elinor a sketch pad as a gift.<br/><br/>Elinor wrote to relatives, and one agreed to let the family stay in their cottage. The women make themselves at home, and Marianne plays the piano for the Jennings. Then, they hear Colonel Brandon applauding in the background. Later, Marianne admits she likes him but doesn't want to be married to an officer because they aren't romantic.<br/><br/>As Marianne and Margaret walk the grounds, they see a man on a horse. Margaret runs towards him, and Marianne stumbles, chasing after her. Marianne hurts her ankle, and the man, John Willoughby, carries Marianne home. They learn that Mr. Willoughby is an unmarried wealthy man, and Marianne has caught his eye. Elinor knows how she feels because Elinor still dreams of Edward. Marianne has to choose between two men, while Elinor has to love beyond her station. Will they get the love they seek?<br/><br/>Based on Sense and Sensibility by Jane Austen, this movie follows the novel with stunning scenery, practical sets, and magnificent costumes and makeup. It will transport the viewer to a complex but romantic time. The script brings delight and pain to gossip and makes the viewers beg for more. This movie proves that Jane Austen's words transcend era, nationality, and ethnicity. It will stay on your DVR and become a favorite.<br/><br/>5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"CrimeTime: Freefall",
     "review":"After wrapping the last episode of her series, Crime Time, Hadley meets with her agent, Saunders. He hasn't been able to find more acting work for Hadley, but he negotiated a memoir deal for her. Hadley and her family will move to Mystic, Colorado. Hadley can visit her mother, Dee, in Mystic, Colorado while writing about her life and time on the show.<br/><br/>Hadley and her kids, Rain and Hendrix, drive to Mel's diner to meet with Hadley's real estate agent, Jay. They notice a man, Anson, posting missing flyers of his business partner, Sam Davis. Once inside, Hadley notices a man, Kirk, sitting at the bar with bruised knuckles while Mel serves him. Jay gushes over Hadley's accolades before taking her to the rental property.<br/><br/>Jay informs Hadley that the area is quiet and her closest neighbor, Frank, is miles away. Hadley tries to go to sleep but can't because of screaming and gunshots. Rain tells them the screaming is coming from Frank's goats, but she is unsure about the shots. Hadley calls the police. Detective Shawn Caden tells Hadley it's hunting season, and the mountains create an echo chamber.<br/><br/>Hadley leaves home to meet Dee at the diner and hears strange flapping. She finds a parachute on a tree with a dead jumper attached. The jumper is Sam. Shawn investigates, and the medical examiner makes a positive ID and declares the death a murder because Sam has a bullet wound. As Hadley updates Dee, Sam's mother, Steph, enters with a request. She wants Hadley to investigate the crime because Mystic police are short-staffed, and Hadley trained with actual officers. Hadley says yes and assembles her Clue Crew to help. They will look into Sam's life and use Hadley's expertise and hyperobservation skills to solve the crime, with Detective Shawn protecting and unknowingly guiding them.<br/><br/>This movie is an entertaining start to a new series. Hadley and her kids must learn about small-town life, a world of free parking, screaming goats, and no matcha martinis. The crime is intriguing and complex. But the wrap-up proves the clues were in front of the audience the whole time. Viewers will look forward to seeing these characters grow while solving more crimes as a Clue Crew.<br/><br/>4.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/crimetimefreefall.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/crimetimefreefall.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"The Ex Obsession",
-    "review":"Personal stylist Kim completes a reel for social media before heading to her husband's company picnic. Kim's husband, John, can't stop talking about his new co-worker, Grant. Kim thinks John has a man crush on Grant.<br/><br/>John introduces Kim to Grant. She says a quick hello and focuses on John's boss. As John plays cornhole, Grant glides to Kim and asks for an update since they broke up. She warns Grant that John doesn't know about their past and demands Grant keep quiet.<br/><br/>Grant comes to Kim's workplace, and they kiss. They meet a few more times before Kim professes her love for John and tells Grant to back off. Later, John returns home to find Grant and Kim sitting on the couch. She confesses they dated in the past but nothing recently. John doesn't believe her, flies into a rage, and attacks Grant. John walks away in horror, and Kim kneels next to Grant. Kim takes a needle, stabs Grant through the eye, and kills him. She convinces John that he killed Grant, and they bury Grant in the backyard. Now, they have to stay one step ahead to cover up Grant's murder, but can they trust each other?<br/><br/>This movie has one of the most gruesome murders. It will be hard to keep your eyes on the screen. Both John and Kim change after Grant's murder. You watch them predict how cops think while keeping an eye on each other. However, it's no surprise who comes out on top. But how they get there is the mystery.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theexobsession.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theexobsession.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"A Taste of Love",
-    "review":"Taylor finished her third and final season of her show, Q.E.D - Quick, Easy, and Delicious. Before she leaves, a network executive asks to meet with Taylor tomorrow, and Taylor agrees. Taylor's manager and friend, Jessica, can't contain her excitement.<br/><br/>Taylor meets with the executive, and they have marvelous news. They want to move Q.E.D. to prime time. When she reminds them about her upscale show, they don't want another show that audiences can't cook or relate to. They will increase Taylor's budget and pay. Taylor says no and flies to her hometown, Dunedin.<br/><br/>Taylor arrives at her parents' restaurant, Cici's Riders, to find it close. She finds her childhood friend, Jacob, delivering food. Taylor says she didn't know her parents, Glenn and Patty, were renovating, and Jacob teases her about the show. Taylor meets with her parents and tells them about the offer but not the rejection. Then, they come clean. They aren't renovating the restaurant. They tell Taylor to go to Jordan's birthday party to take her mind off the sale.<br/><br/>Taylor enjoys the festivities until she argues with Jacob about leaving years ago. When Jacob leaves, an old rival, Linda, approaches to gloat about her family's thriving restaurant. With Cici's Riders shutting down, Linda's Wild Irish won't have competition during the Food and Wine Contest, even though Linda's family has won every year. Next, Jessica calls. The network wants Taylor to take a second look at their offer with more money and a larger budget.<br/><br/>Glenn and Taylor go out for breakfast, and Linda walks over with a news article about the competition. Taylor entered into the contest without telling Glenn. She hopes winning the competition will save the restaurant and inspire her parents to keep it. But she will need help from Jacob to reconnect with the locals and create the perfect menu for their palate. Can Taylor save the restaurant and her friendship with Jacob?<br/><br/>This movie has an emotional core. It draws the viewer in by showing Taylor's past. Next, viewers have a methodical villain, Linda. Linda knows the right buttons to push to get under anyone's skin. Finally, we have loving parents. Glenn and Patty enter the contest for Taylor and aid in Taylor's job decision. This film will whet your emotional appetite while making you hungry. And Taylor's final dish is legendary.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/atasteoflove.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/atasteoflove.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Man in the Guesthouse",
-    "review":"Ashley quit her job and hasn't worked in a while. She suggests to her husband, Brandon, that they rent their A.U.D., Additional Dwelling Unit, to make up the income. Her friend, Shelley, supplements her income that way, and Ashley thinks it will help cover the mortgage until she finds work.<br/><br/>Ashley meets with Shelley after renovating the unit with Brandon. They list the property online and look through the candidates. They find Dan's profile. He is a college student looking for a long-term rental. Shelley tells Ashley that he will bring a steady income.<br/><br/>Dan meets Ashley and Brandon. He is an MBA student who needs a larger space to study. Also, he and his mother have started to aggravate each other. Ashley and Brandan allow Dan to settle in the unit, but Ashley feels something is off about Dan. He is too nice and friendly. Is she right?<br/><br/>Seriously, let's all promise to do a background check and learn about tenant's rights. It doesn't take long for Dan to overstep his boundaries and threaten Ashley and Brandon. Dan causes leaks, breaks into their home, and makes revenge porn. And the villain's monologue pieces the story together nicely. As you watch, try to uncover Dan's motives and enjoy the crazy.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/themanintheguesthouse.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/themanintheguesthouse.jpeg"
     }
   }
 ];
@@ -58,29 +58,47 @@ module.exports = madeForTV;
 
 {
   "option":"1",
-  "mtitle":"Single Black Female 2: Simone's Revenge",
+  "mtitle":"Hunting Housewives",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/singleblackfemale2simonesrevenge.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/singleblackfemale2simonesrevenge.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/huntinghousewives.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/huntinghousewives.jpeg"
   }
 },
 {
   "option":"2",
-  "mtitle":"Lonely Crime Fanatic",
+  "mtitle":"Blackmail, Lies, and Murder",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/lonelycrimefanatic.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/lonelycrimefanatic.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/blackmailliesandmurder.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/blackmailliesandmurder.jpeg"
   }
 },
 {
   "option":"3",
-  "mtitle":"The Soulmate Search",
+  "mtitle":"The Thundermans Return",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesoulmatesearch.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesoulmatesearch.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thethundermansreturn.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thethundermansreturn.jpeg"
+  }
+},
+{
+  "option":"4",
+  "mtitle":"Alone in the Dark",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aloneinthedark.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aloneinthedark.jpeg"
+  }
+},
+{
+  "option":"5",
+  "mtitle":"The Single's Guidebook",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thesingleguidebook.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thesingleguidebook.jpeg"
   }
 }
 
