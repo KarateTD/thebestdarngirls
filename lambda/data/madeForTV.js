79f9b4abbe7c1dae2cf55d0084c14a25597a6e2b
Tiffany Gray <KarateTD@yahoo.com>
2024-03-31 01:10:29 -0400

    add 3 tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"An Easter Bloom",
+    "review":"Amanda looks at the farm, sees zero flowers, and sighs. She has no idea how the farm will survive without a successful harvest. Amanda works in a coffee shop, and her mother, Grace, bakes to make side money. But it won't be enough.<br/><br/>Amanda prepares a coffee for Derrick, and he leaves a generous tip. Amanda's co-worker believes he was flirting. Derrick is the new pastor, but he hides himself from his parishioners. They don't know he loves extreme sports, and he worries they will judge him. Derrick prays for guidance, courage, and forgiveness. His mother, Lori, believes God will show Derrick the way.<br/><br/>Amanda goes home and prays over the harvest when she hears a crash. Amanda looks up and sees Lori stumbling out of her car. Lori identifies the flowers in Amanda's home and stuns Amanda. Lori admits she is a retired florist but never achieved her ultimate goal. Lori wanted 1st place at the annual Easter Flower Arranging Competition. Lori encourages Amanda to compete, but Amanda states she is a farmer, not a florist. However, the $20,000 prize changes Amanda's mind. Amanda enters the competition and sets herself on a path of love, friendship, and forgiveness.<br/><br/>This movie brings faith to the forefront and applies it to Amanda's harvest. While planting their crops, Amanda's parents strengthened and explained her faith and how to use prayer. These flashback scenes are great for younger viewers. However, the storyline stirs zero concern for the main characters. The film's shining light is Derrick's sermon. He says what all pastors should preach to their congregations, and the audience will welcome it.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aneasterbloom.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aneasterbloom.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"My Acting Coach Nightmare",
+    "review":"Sienna runs home with a brochure and gives it to her mother, Lisa. Sienna's friend, Amy, told Sienna about an acting class at the local community center. The teacher, Max, has movie credits and worked in Hollywood. Sienna begs to take the course, but Lisa is unsure. Lisa's new husband, Mark, encourages Lisa to let Sienna take the course. Lisa obliges.<br/><br/>During the first class, Max performs a mirroring exercise with Haley. Amy and Sienna can see that Haley is Max's favorite. After a few days, Haley isn't in class. Max explains Haley's absence and gives Sienna Haley's lead role in Romeo and Juliet. Sienna and Amy see Max arguing with Haley after class. Sienna decides to walk away, but Amy watches.<br/><br/>Max gives Sienna personalized attention, much to Amy's chagrin. He requests private sessions and sexy photoshoots. Lisa's concerns grow, and she asks Mark to speak to Max. Max states that Mark takes pictures of young women after class. Mark denies these accusations, but Sienna finds photos in Mark's truck. Lisa leaves Mark but keeps an eye on Max. She thinks he's dangerous, and she is right. Can Lisa save Sienna from a predator?<br/><br/>This movie uses today's headlines and conspiracies to engage the viewer. Sienna acts like a 13-year-old but is 18. She doesn't listen to Lisa's or Amy's warnings. Max slowly grooms her with attention and promises. Soon, he warps her mind and makes Sienna depend on him for validation. While this film may irritate adult viewers, a watcher can use it as a teaching tool for their tween, teen, or young adult.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myactingcoachnightmare.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myactingcoachnightmare.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"Shifting Gears",
+    "review":"Jess finishes restoring a vintage car and notices she is running late for her class. She teaches beginner auto repair courses to empower women at her shop, Barro's Auto. However, the office manager, Teri, sees Jess has issues with public speaking. After class, Jess's father, Ray, brings Jess a new car to restore.<br/><br/>Luke arrives at a strategy meeting late and apologizes to his father, Steven. Luke spots the slide on the display and asks for clarification. Steven says their company, Tune Up Whiz, plans to acquire Barro's Auto.<br/><br/>Luke sits at a bar with his friend, Pete, and confesses that he hates working in a boardroom. Luke leaves Pete at the bar for a date but forgets his keys. He returns to look for them, and Jess has them in her hand. She gives Luke his keys, and Luke exits. Afterward, Teri can sense the tension between Jess and Luke. Jess explains Luke is the infamous college boyfriend. Luke's grandfather wanted his son, Steven, and Jess's dad, Ray, to run Tune Up Whiz together. However, Steven changed the contract and cut Ray out of the deal. Jess assumed Luke would take her side. When he didn't, she ended the relationship.<br/><br/>Jess is working on a car when Wayne Larouche enters her shop. Wayne created a new auto competition program and wants Jess to be a part of it. Jess remembers her stage fright and says no. After Wayne informs her about the $50,000 prize, Jess offers to consider it.<br/><br/>Jess sits with a distressed Ray. He tells Jess the landlord wants them to prepay for a year or lose the family business. So, Jess signs up for the competition. On the first day, Jess sees Luke. He entered the competition after another contestant dropped out. Luke doesn't tell Jess that Steven wants to acquire Barro's Auto. For Jess, this show is about saving her family's heart, but she has to get past her stage fright. Can Jess win without learning Luke's secret?<br/><br/>This movie has saucy elements in a Hallmark package. Of course, Luke hides a secret from Jess. His family wants their shop, and Steven has a few tricks to help the process. Luke struggles internally between the man he wants to be and who he is. This Shakespeare-like romantic comedy has enough spice to keep viewers watching and entertained with a Hallmark wholesome ending.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shiftinggears.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shiftinggears.jpeg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"Surviving the Sleepover",
     "review":"Hannah and her mother, Jessica, reside in a hotel while contractors finish their home. Jessica tells Hannah to worry more about making friends than their living arrangements.<br/><br/>Hannah vlogs about coping with her father's passing and moving to another town. As she films while walking to school, Melissa honks her horn, causing Hannah to drop her phone. Abbie and Charlotte, recognized the new girl but didn't know she resided in a hotel.<br/><br/>Melissa arrives at school and sees her stepbrother, Cameron, watching Hannah's vlog. She warns Cameron to stay away from Hannah. Cameron pulls Melissa away from her friends and rages that they had a one-time fling.<br/><br/>Hannah and Abbie talk during Spanish class and become friends. Abbie encourages Hannah to ask Cameron to date but warns that Melissa is protective over him. Hannah takes Abbie's advice, and Cameron accepts. Before they can cement their plans, Melissa invites Hannah to a girls-only sleepover.<br/><br/>During the sleepover, Melissa tries to humiliate Hannah. They play the game 'Never Have I Ever.' Melissa's card asks if the reader ever had a crush on their stepbrother. Melissa flies into a rage and blames Hannah. Hannah tries to pass Melissa, but Melissa grabs a knife. Abbie, attempting to help Hannah leave, gets stabbed by Melissa. Melissa panics and guides Hannah and Abbie to a room so Abbie can rest and wait for an ambulance. However, Melissa locks them inside. She doesn't plan on Abbie or Hannah surviving the sleepover.<br/><br/>This movie is Mean Girls turned deadly. Melissa's obsession with Cameron is narcissistic and delusional. Charlotte is the consummate beta follower, but Abbie openly second-guesses Melissa. As Melissa's crazy slowly ramps up, her cunning nature evolves. However, the final 5 minutes destroy her character development. She caused so much havoc and didn't suffer any consequences. Instead of punishment, viewers get a group therapy session. It leaves the audience with a bad taste in their mouths.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"My Father's Murder in Greece",
     "review":"Allie visits her father, Giorgos, and grandmother, Yiayia, in Greece after Yiayia received a poor prognosis earlier. They stand at a street crossing, and an officer aggressively approaches and accosts Giorgos. Soon, Giorgos and the officer laugh. He is Giorgos long-time friend, Kostas.<br/><br/>Allie arrives in Yiayia's warm embrace and delights Yiayia by speaking Greek. Yiayia prepares dinner for Allie, Giorgos, and Kostas. The dinner is pleasant until Giorgos's brother, Theo, enters. Allie understands they are fighting over a woman named Iris. Theo slept with Iris while Giorgos was dating her. Theo blames Giorgos for leaving him alone to care for their mother. They continue to yell until Theo leaves.<br/><br/>Allie sleeps until she gets a phone call from her mother, Catherine. Allie hears a thud during their conversation and leaves her room to find the sound's origin. She discovers her father dead on the ground and cries over his body. Then, someone knocks her out before she can call for help. Kostas wants to declare Giorgos's death as a heart attack, but Allie believes he is wrong. She believes someone killed her father and rendered her unconscious. She thinks Theo is the culprit, but Yiayia can't imagine Theo killing Giorgos. Can Allie prove someone murdered Giorgos and that someone was Theo?<br/><br/>This movie has several twists, allowing viewers to journey with Allie. The mystery will throw you for a loop, but the lead's stupidity will keep you grounded. She goes on an unknown boat alone, privately meets with a stranger, and turns off her phone's locator. The audience slowly loses sympathy for Allie. Viewers should watch this movie for the mystery and to yell at the screen.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myfathersmurderingreece.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myfathersmurderingreece.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"Dying on Party Island",
-    "review":"Mel travels to a remote island to identify the remains of her sister, Georgia. The police believe Georgia died while cliff diving and washed ashore with her wallet. Then, Mel meets with Georgia's hosts, spouses Keaton and Paula. They give Mel Georgia's belongings, and Mel notices that Georgia's phone and passport are missing. Later, Mel calls her mother, Annie, to confirm Georgia's passing. She informs Annie that Georgia got another tattoo and the manner of death. Annie thinks those are out of character for Georgia. So, Mel decides to stay in town and asks questions about Georgia and her final days on the island. Mel's questions will bring a message from Georgia, put a gun in Mel's face, and uncover corruption.<br/><br/>This dark mystery adds reels and a phone call to keep viewers guessing. You wonder if Georgia is dead or alive. However, it doesn't take long to understand what's happening on this island. You can see familiar techniques and the blaring warning signs around Mel. This movie doesn't require your full attention but is a new twist on the 2024 Spring Breakdowns season.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dangeronpartyisland.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dangeronpartyisland.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Killing for Extra Credit",
-    "review":"Marybeth receives more disparaging text from classmates before the start of her senior year. Her best friend, Sophie, tells Marybeth to ignore the bullies and focus on this year. Last year, intimate photos Marybeth sent her boyfriend, Josh, were leaked to the school. Marybeth's friend, Ray, meets Marybeth and Sophie at school, and they walk in together but stop when they see Marybeth's presidential rival, Toby. Toby exposed Marybeth's photos in the school newspaper and now gloats about taking her job and being president.<br/><br/>Josh attempts to talk to Marybeth during class, but she refuses. She believes Josh gave her photos to Toby, but Josh vehemently denies it. The school gets a text message from someone calling themselves 'Extra Credit.' And this unanimous person exposes secrets throughout the day. Marybeth asks Sophie and Ray to help her expose 'Extra Credit.'<br/><br/>Marybeth gets the shock of her life one day. 'Extra Credit' tells the school that Sophie released Marybeth's pictures. Marybeth confronts Sophie, and Sophie cries and begs to explain. Marybeth refuses to listen. Later, Sophie's father can't find Sophie. Marybeth runs to their private meeting spot and finds Sophie dead. The police rule it a suicide, but Marybeth doesn't believe it. She wants to prove someone killed Sophie and that it was 'Extra Credit.' Can Marybeth uncover the culprit without losing her life?<br/><br/>This movie feels like a continuation of Friday Night Sext Scandal and has many moving parts. The plot slows as Marybeth and Ray look through Marybeth's life to uncover the events leading to her demise. However, the conclusion - well, the first - speeds up the movie ten times. But hang on because there is a fun twist. This movie is perfect for the Spring Break crowd, but adults may want more.<br/><br/>3.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/killingforextracredit.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/killingforextracredit.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"Sweetly Salted",
-    "review":"Alison critiques restaurants under the pseudonym Sweetly Salted for Chicago Today. She clings to anonymity until she gets a phone call from Olivia Sterling from Hudson Publishers. Olivia wants Alison to write a book because there are so few female critics with Alison's reach. However, they want to break her identity.<br/><br/>Alison goes to Isla Dulce with her friend, co-worker Jules. Alison has been looking forward to this outing because they serve Jibarito. The first time she tasted it, she gave the server a 20 dollar tip with 'thank you for the salty and the sweet' written on it. However, she rejects the food because it has Hawaiian rolls instead of plantains. She returns to the office and writes a scathing yet clever review.<br/><br/>Jules approaches Alison at work with a pitiful look and shows Alison the comments under her review. Alison's fans love Isla Dulce and turn against Alison. Alison's boss, Lexi, thinks Alison should take a sabbatical from the newspaper so the readers can miss her.<br/><br/>Alison goes to Isla Dulce to speak to the chef, Seb, and explain her review but overhears a conversation between Seb and his sous chef. The sous chef quits because a poor review from Sweetly Salted ended other establishments. Alison had no clue about the impact her comments had. And it gives her an idea. She will use her break to work at the Isla Dulce as a sous chef. Then she will write about it for her book. Alison convinces Seb to give her the job without revealing her inexperience in the kitchen, alter ego, or upcoming book. Can she continue to hide the truth while falling in love?<br/><br/>Putting the apple cheddar pie and pineapple basil salad to the side, Alison has a lot of finesse. However, Seb is desperate. Alison never considered her article's ripple effect on people's lives and livelihoods. This realization will make you watch, but the love story is foreseeable.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sweetlysalted.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sweetlysalted.jpeg"
     }
   }
 ];
@@ -58,29 +58,48 @@ module.exports = madeForTV;
 
 {
   "option":"1",
-  "mtitle":"An Easter Bloom",
+  "mtitle":"Secret Life of a Sorority Girl",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/aneasterbloom.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/aneasterbloom.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/secretlifeofasororitygirl.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/secretlifeofasororitygirl.jpeg"
   }
 },
 {
   "option":"2",
-  "mtitle":"My Acting Coach Nightmare",
+  "mtitle":"One Bad Apple: A Hannah Swensen Mystery",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myactingcoachnightmare.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myactingcoachnightmare.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/onebadapplehannahswensenmystery.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/onebadapplehannahswensenmystery.jpeg"
   }
 },
 {
   "option":"3",
-  "mtitle":"Shifting Gears",
+  "mtitle":"Million Dollar Lethal Listing",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shiftinggears.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/shiftinggears.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/milliondollarlethallisting.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/milliondollarlethallisting.jpeg"
+  }
+},
+{
+  "option":"4",
+  "mtitle":"Writing a Love Song",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/writingalovesong.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/writingalovesong.jpeg"
+  }
+},
+{
+  "option":"5",
+  "mtitle":"Living with My Mother's Killer",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/livingwithmymotherskiller.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/livingwithmymotherskiller.jpeg"
   }
 }
+
 */
