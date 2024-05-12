38a71c9c1755ebf66d2908582bd0a02626ce8232
Tiffany Gray <KarateTD@yahoo.com>
2024-05-11 23:58:42 -0400

    3 tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"Mommy Meanest",
+    "review":"Mia delivers an impassioned speech to her senior class during Maplewood Prep's Anti-Bullying Week. She suffered her first year because she wasn't rich like the other students. Her mother, Madelyn, got a job as a music teacher so Mia could attend. Josh asks Mia about attending the University of Northeast Florida in the fall. He got accepted and thought they could get to know each other before. Madelyn leaves her love interest, Scot, when she sees her ex-husband, Erik, exiting the auditorium. Erik rages about Madelyn driving a wedge between him and Mia. She informs Erik that Mia sets the boundaries, not her. Madelyn walks to her classroom when Scot finds her and ends their relationship.<br/><br/>Madelyn sits with her student, Zoey, and questions Zoey about her ongoing cyberbullying case. While it was disturbing, it brought her closer to her parents. Madelyn can't wait to snuggle under the covers with Mia and watch Gilmore Girls. However, Mia has plans with the 'Jet Set' and asks for a rain check. Madelyn obliges.<br/><br/>Mia returns home with exhilaration. Then, Mia gets a disgusting text message from an unknown number. She runs to Madelyn for comfort. Soon, Mia receives hundreds of texts from unknown numbers. And it's hurting her schoolwork, relationships, and spirit. At least she has Madelyn. Will they find Mia's cyberstalker before they end her life?<br/><br/>Inspired by actual events, this movie is wild, and you must give it time to cook. The principal and police take Mia's concerns seriously, and it's refreshing. Mia takes steps to defend herself by blocking the numbers and getting a new phone with an unlisted number. But nothing helps. As her life falls apart, her bully's life rises. This film highlights the new phenomena of Munchausen Syndrome by Proxy in the digital age. The plot slowly connects the audience to Mia and her descent into depression, isolation, and desperation over the harassment. That's when the conclusion gives you the gut punch: the cyberbully's monologue. This torment isn't the bully's first rodeo.<br/><br/>5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mommymeanest.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mommymeanest.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"The Manny",
+    "review":"Lani hosts a web cooking show called Kitchen Rebel from her home. She and her assistant, Mercedes, are excited because they are in final talks to bring their show to TV. Lani wants to hire a nanny because there will be more work over the coming weeks. Mercedes put an ad online but questions Lani about the nanny's gender. Lani states she wants a woman.<br/><br/>Morgan knocks on the door after Lani finishes recording. He came for the job, but Mercedes swears she rejected him earlier. Morgan believes his epicene name makes people overlook him. Then, Lani hears her ex-husband, Darren. He pulls Lani aside and demands she doesn't hire Morgan. Also, Darren needs to cancel his hiking trip with Jaylen. She is furious that Darren is trying to control her life. Lani is disappointed that Darren canceled his plans with Jaylen, but Morgan offers to take Jaylen hiking. Lani says no but agrees to let Mercedes do it. Morgan leaves but asks Lani to keep his information.<br/><br/>Mercedes had a bad fall after slipping on cooking oil on her apartment stairs. She broke her foot and now can't take Jaylen to the cabin. Lani calls Morgan's references and hires him after hearing glowing reviews. But nothing happens by accident. Morgan studied Lani and Jaylen to mold himself into the perfect manny. He wants to create his dream family and will kill to do it.<br/><br/>Morgan's deception is an art form. He stalks his prey online and parrots the information when they meet. Then, he offers helpful hints to show he cares while alienating Lani. Lani attempts to fire Morgan several times, but he alters the situation, so she needs him. This film is fun to watch but won't require a second viewing unless it's a rainy day. And the conclusion doesn't come with a pretty bow, just life lessons.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/themanny.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/themanny.jpeg"
+    }
+  },
+  {
+    "option":"3",
+    "mtitle":"My Life is on the Line",
+    "review":"Shannon argues with her ex-husband, Joel, over their son's afterschool pick-up. If Shannon doesn't get Dylan, Joel will bring it up at their next mediation meeting. She relents and promises to pick up Dylan. Shannon takes a deep breath and walks into work at the Southwest Credit Union call center. Her boss, Pilar, warns Shannon that she must make her survey quota or lose her job. Next, Shannon receives a call from the mediator, Debbie. Shannon's last drug test registered positive. Shannon tries to explain it's due to her medication, but Debbie won't hear it. Debbie warns Shannon to stop taking drugs, keep her job, and make her appointments. Or lose custody of and visitation rights to Dylan.<br/><br/>Nicholas calls Shannon from the hospital because Nicholas's wife, Maggie, needs emergency surgery, but the doctor won't perform it without a deposit of $20,000. Nicholas tried to charge it, but it bounced because of an overdraft. Shannon overrides it by entering Pilar's code without Pilar's approval. Nicholas leaves Shannon a glowing review. The charge goes through, and the fraud department immediately flags the account. Shannon watches her screen blink red in horror when Maggie calls. Shannon requests Maggie's location, and Maggie states she's home and her husband, Nicholas, is away on business. Shannon made a mistake. But Nicholas won't let up. Shannon gave him personal information during their call. He threatens her job and son when she refuses to release the money. Can Shannon keep Nicholas at bay without anyone uncovering her mistake?<br/><br/>Shannon's life starts on shaky ground. She could lose her son with one misstep. Now, everything in her life hinges on Nicholas, a con artist. During their first conversation, she gave him too much personal information about her divorce, job, and son. Now, he knows how to manipulate her. So, your anxiety and frustration will build at the same time. This movie is a roller coaster for your emotions, but you never feel bad for Shannon. She is in a hole, but she keeps digging. This film is a fun distraction, but you won't watch it twice.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mylifeisontheline.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mylifeisontheline.jpeg"
+    }
+  },
+  {
+    "option":"4",
     "mtitle":"A Deadly Threat to My Family",
     "review":"Reporters flank Serena as she exits the courthouse with her brother, Ryan. They interrogate her about The Builders of Ascension cult. Serena testified against the leader, Aleph, and he got 30 years in prison. The cult believed the world would end in five days. So they planned to take their life and rise to the next level.<br/><br/>Ryan brings Serena home to the arms of her adoring niece, Aria, and uneasy sister-in-law, Marielle. He offers Serena the guest house to give her much-needed privacy. Marielle worries about Serena's progress, but Ryan tells Marielle not to focus on the past.<br/><br/>Marielle, Serena, and Aria get into the car to take Aria to school and Serena to therapy. Marielle and Aria argue about Aria trying to impress new classmates. Serena warns Marielle to loosen the reigns on Aria, or someone will take Aria away. Marielle sees Serena's warning as a threat. Marielle should worry because a deadly threat creeps around her family, and she has 5 days to save them.<br/><br/>Can someone slap Ryan? You will come unglued every time he accuses Marielle of overreacting. This movie boils down to communication. If Serena told Ryan and Marielle about the cult's plans, everything would have been avoidable. The plot feels farfetched. However, NXIVM, Heaven's Gate, and Jonestowns prove this can happen. This movie won't be a favorite, but it's an entertaining distraction for two hours.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"5",
     "mtitle":"Our Mother's Secret Affair",
     "review":"Fran grabs a weapon to confront an intruder, Kasey. However, Kasey introduces herself as Fran's older sister. The police question Kasey as Fran's mother, Diana, and Diana's old co-worker, Troy, arrive. Diana explains that she and Troy had an affair. They called it off when his wife, Elle, discovered the truth. However, Diana became pregnant with Kasey. Since Elle couldn't have kids, Diana signed over her rights to Elle and Troy. Diana missed Kasey so much that she got pregnant with Fran by her husband.<br/><br/>Elle calls her mother, Gloria, in a frantic state. Elle's brother, Jake, wants to end Diana, but Gloria warns him to step back. He is on probation and could return to jail for the slightest mishap. Elle had a mental breakdown over Troy's affair, and caring for Kasey helped her through it. Gloria won't let Diana hurt Elle again. Gloria assures Jake that she has wheels in motion to handle Diana.<br/><br/>Across town, a woman gets a phone call as she stashes her cash and gun. The caller wants her to take care of two females. She agrees to do it and gets pictures of Diana and Fran in her email. The woman makes a profile named Lynda and will ingratiate herself to Fran. She plans to get into Diana's home and make Diana and Fran's deaths look like accidents. Will she succeed before Fran and Kasey connect?<br/><br/>Lifetimers must pull out a notepad to keep track of all the connections, dealings, and motives. With each new character, the script injects a new complexity. The writers risk the script with more sensationalism. The plot has so many moving parts it becomes a parody of itself. Honestly, Troy is the villain of this movie. His affair caused a ripple effect for decades.<br/><br/>3 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/ourmotherssecretaffair.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/ourmotherssecretaffair.jpeg"
-    }
-  },
-  {
-    "option":"3",
-    "mtitle":"The Replacement Daughter",
-    "review":"Jessica leaves school with her friend, Vanessa, and discusses how Shane stared at Jessica during school. Jessica runs to her mother's beauty salon. Jessica's mother, Liza, argues with her landlord, Frank, about the business space. Condo developers offered Frank money to sell, but now Liza refuses to move. Frank leaves in a huff, and Liza finishes Eva's nails. Liza's friend, co-worker Helen, suggests Liza get the cameras fixed because the cafe down the street had two break-ins. Liza and Jessica leave to spend the evening together. As Jessica takes a selfie, a car hits Liza.<br/><br/>Weeks after the accident, the police haven't found the person who hit Liza. Helen happily took Jessica into her home and now brings Jessica to visit Liza daily. Eva arrives at the hospital to see Liza and offers to take Jessica to dinner so Helen can pick up a delivery at the salon. Helen allows it.<br/><br/>During dinner, Eva says she overheard Jessica's dilemma about school. Jessica's top school accepted her, but with financial aid, she is $10,000 short of tuition. Eva has a surprise. She talked to the dean, and they are offering Jessica a full-ride scholarship. Jessica and Eva leave the restaurant so Jessica can go home with Helen. However, they find Helen dead in the alley. Feeling alone, Jessica inquires about living with Eva after a social worker suggests going to a temporary foster home. Eva informs the social worker that she is a foster parent, waiting to receive her first placement. Jessica eases into a life she never dreamed of, but Eva is a dangerous woman looking for a replacement daughter.<br/><br/>This movie is old-school Lifetime, and you will love it. Eva is unhinged but has the resources to make others fall in line. Eva quickly eliminates the competition and racks up a body count. And the hits come hard and fast. Jessica isn't a wilting flower. The moment she suspects something is wrong with Eva, she investigates and refuses to accept excuses. If you love a crazy villain, this is the film for you.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thereplacementdaughter.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thereplacementdaughter.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Branching Out",
-    "review":"Guitar newbie Ruby sits in class when her teacher, Miss Weaver, gives the class their next term project. She wants them to create a family tree and give a presentation about their heritage. After class, Ruby asks Miss Weaver for another project because Ruby is the product of IVF. Also, Ruby's mother, Amelia, is an only child with a deceased mother and an estranged father. Miss Weaver encourages Ruby to complete the project and discover her heritage.<br/><br/>Ruby questions Amelia about her father. Amelia remembers keeping the donor's information about him along with an audio recording. However, the donor wanted to remain anonymous until Ruby's 18th birthday. Amelia prepares dinner with her friend, Maura, and Maura understands Ruby's need to reach out. Maura suggests Amelia give Ruby an ancestry test. Ruby overhears and begs for the tests. Amelia says yes and waits for the results.<br/><br/>TJ is a struggling guitarist who works on his family's famous farm to make ends meet. His parents, Tomás and Sofia Cruz, run the Cruz Family Farm. They hint at TJ to start a family, but he is happy to play cool uncle.<br/><br/>Amelia gets an email with Ruby's result, and Ruby matches with Sofia Cruz. She decides to meet them. Amelia drives to the farm and approaches TJ. Within moments, she blurts out that TJ has a daughter and walks away. However, TJ wants to meet her. Amelia worries that TJ could break Ruby's heart as her father did to hers. Can she put her apprehension aside for Ruby's sake while falling in love with TJ? Will her concerns come to fruition when TJ gets his dream job?<br/><br/>This movie rests on the heart of one incredible young girl. Ruby starts with one project that looks simple, but for today's families, a tree can be a complex assignment. The script ensures that TJ and Amelia aren't the antagonists in the movie. Amelia knows how it feels to have a father disappoint her. TJ becomes a father overnight with responsibilities beyond the imagination. Luckily, TJ and Amelia have friends and family to hold them accountable and strive for Ruby to thrive. With one group chat, these strangers become a loving and caring family. This movie deserves your full attention with the whole family.<br/><br/>4.5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/branchingout.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/branchingout.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"A Chef's Deadly Revenge",
-    "review":"Chef Lucy focuses on plating meals when her stalker leaves an origami crane and private photos at the table. Lucy's friend, Sous Chef Megan, lets Lucy stay at her place. In the past, Lucy's ex-boyfriend, Ayden, invaded her privacy and stalked her. Lucy remembers from experience that without concrete evidence, the police can't help.<br/><br/>Lucy takes a break from work to clear her mind. She sits on a park bench and sees a man, Frank, with his dog. They politely converse until Frank talks about her being late for the lunch shift. Lucy never mentioned her job, so Frank's knowledge worries her. She believes Frank is her stalker. However, Frank has zero ties to Lucy. Megan worries that Lucy strings together coincidences and creates a fearful situation. Lucy focuses on work until she finds a mutilated mannequin with a scary note. Lucy sees this as a threat. Lucy is tired of being a stalking victim. Now, she will turn the tables and become a stalker.<br/><br/>This movie takes a while to get to the point or start a body count. However, it's about who can out-crazy the other. Frank is Lucy's stalker, but his infatuation isn't about romance. He is a Resentful Stalker. Lucy's response is to become a Revenge Stalker. In an attempt to destroy each other, Lucy and Frank emotionally and physically hurt people around them. The only victims in the film are Megan and Frank's wife, Emily.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/achefsdeadlyrevenge.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/achefsdeadlyrevenge.jpeg"
     }
   }
 ];
@@ -56,32 +56,6 @@ module.exports = madeForTV;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Mommy Meanest",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mommymeanest.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mommymeanest.jpeg"
-  }
-},
-{
-  "option":"2",
-  "mtitle":"The Manny",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"",
-    "largeImageUrl":""
-  }
-},
-{
-  "option":"3",
-  "mtitle":"My Life is on the Line",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mylifeisontheline.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mylifeisontheline.jpeg"
-  }
-}
+
 
 */
