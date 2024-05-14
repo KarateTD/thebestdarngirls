6b5e2539ce24a3489c9dbcc4390a4d9f2d98d54c
Tiffany Gray <KarateTD@yahoo.com>
2024-05-13 23:34:24 -0400

    add babe

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -85,6 +85,19 @@ module.exports = mustBuy;
   }
 
     {
+    "option":"1",
+    "mtitle":"Babes",
+    "review":"Babes was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
+    "asin":{
+      "re":""
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/babes.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/babes.jpeg"
+    }
+  }
+
+    {
     "option":"2",
     "mtitle":"Scrambled",
     "review":"Scrambled was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
