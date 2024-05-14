a295ee24ae5c349cb2153cc771f93dd3cd58fe92
Tiffany Gray <KarateTD@yahoo.com>
2024-05-14 16:27:36 -0400

    add dune

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,18 @@
 var mustBuy = [
   {
     "option":"1",
+    "mtitle":"Dune: Part Two",
+    "review":"Paul and his pregnant mother, Lady Jessica, hide among the Freman people in the desert as word of their death travels through the houses. Stilgar pulls Jessica aside because he and other southern Freman believe Paul is there to fulfill the prophecy. A Reverand Mother is dying, and they want Jessica to take her place. Jessica says yes and goes through the deadly ritual but doesn't tell them she's pregnant. With her new status, Jessica will convince non-believing Freman that Paul is the Messiah.<br/><br/>Paul desires to learn the ways of the Freman. Stilgar gladly teaches Paul, and southern Freman delights in Paul's triumphs. However, a northern Freman, Chani, isn't impressed. She doesn't subscribe to the prophecy because she thinks it's an enslavement tool. Paul confides in Chani about his apocalyptic dreams as the two become inseparable. Paul's dreams warn that traveling to Southern Arrakis will bring death to billions.<br/><br/>Glossu Rabban Harkonnen sends spice harvesters and soldiers to the northern part of the Arrakis. Since the death of Duke Leto, Baron Vladimir Harroken wants his nephew, Glossu, to regain control of spice production. Vladimir thinks Glossu is a doting foot soldier but doesn't believe Glossu is ready to lead. Vladimir wants his other nephew, Feyd-Rautha, to take the throne. Emperor Shaddam IV's Truthsayer, Gaius Helen Mohiam, agrees. Shaddam's daughter, Princess Irulan, disagrees because Feyd-Rautha is a psychopath. Irulan keeps a secret. She suspects that Paul and Jessica are alive but unsure of their location. Can Paul and the Fremans stop the Harroken with the Emperor and his people secretly working for them?<br/><br/>Based on the novel Dune by Frank Herbert and the sequel to Dune, this film deserves the most vivid screen and best sound an audience can find. The graphics are extraordinary and clean. The sound gives the script a proper build for its iconic moments. The story is captivating and dramatic. The script doesn't force its high-level wit on you and profoundly uses silence. On this surface, this movie is about empires fighting over a resource. However, this plot gently discusses religion and looting. This sequel is better than the first film and deserves your time and money. Its stunning fight scenes, jaw-dropping graphics, and compelling storyline are worth the running time.<br/><br/>5 out of 5 stars.",
+    "asin":{
+      "re":"B0CW3DTWNG"
+    },
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dunepart2.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dunepart2.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Mean Girls",
     "review":"Mean Girls was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
     "asin":{
@@ -12,7 +24,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Ordinary Angels",
     "review":"Five years ago, Ed and his wife, Theresa, welcomed their baby girl, Michelle, into this world. Now, Ed, Michelle, and Michelle's older sister, Ashley, attend Theresa's funeral. Ed lost his wife and now may lose Michelle. Michelle has biliary atresia and needs a liver transplant. Theresa died and left a staggering amount of medical debt. Now, Michelle's bills are piling up too.<br/><br/>Sharon wakes up in her friend's bed after a wild night of partying. So, her friend, Rose, forces Sharon to attend an A.A. meeting. Afterward, Sharon goes to the store to buy a six-pack and sees a newspaper article about Michelle. She feels compelled to attend Theresa's funeral. Ed thanks Sharon for coming and pulls his daughters away.<br/><br/>Sharon wakes up with a new passion. She raises over $4000 for the family and hand-delivers it to them. Ed's mother, Barbara, invites Sharon inside for dinner. Sharon looks over Ed's shoulder and sees a stack of unpaid bills. She returns home, calls his utility companies, and gets his balances. She can't believe Ed owes over $430,000. Sharon uses her business skills to help Ed's finances and get Michelle a liver. While focusing on the family, Rose worries it's another form of addictive behavior. Can Sharon help save Michelle's life without falling into despair?<br/><br/>Based on Ordinary Angels by Sharon Stevens Evans, this movie's core value is perseverance. Ed is a prideful man and has difficulty asking for and accepting help. Sharon gets inspiration during the A.A. meeting to be a service to others. However, she replaces one addiction with another because she hasn't acknowledged the problem. When Ed and Sharon work together, they rally a community to help Michelle. They can overcome a blizzard, summon planes, and clear debt. This movie will bring a tear to your eye and strengthen your faith in the community.<br/><br/>5 out of 5 stars.",
     "asin":{
@@ -24,7 +36,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"Lisa Frankenstein",
     "review":"Lisa Frankenstein was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
     "asin":{
@@ -36,7 +48,7 @@ var mustBuy = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"The Iron Claw",
     "review":"Jack Von Erich exits the arena after a grueling match to find his wife, Doris, and two sons waiting for him outside. Doris asks Jack how much the new Cadillac attached to their mobile home costs. Jack tells Doris he has to look like the NWA champion to get the belt. Doris prays for guidance in the front seat. Doris believes God will protect the family from the Von Erich Curse, but Jack thinks it's professional wrestling.<br/><br/>Years later, Jack runs WCCW promotion with his son, Kevin, at the top of the card. Jack and Doris's other sons, Kerry, Mike, and David, beam with pride at Kevin's in-ring accomplishments. Jack lets his sons know he has favorites in a particular order, but they are subject to change. Kerry is gearing up for the 1980 Olympics in Moscow, and Kevin will wrestle Harley Race in a non-title match. Jack announces it's David's time to get into the squared circle and tag with Kevin.<br/><br/>Kerry returns home after the U.S. pulls out of the Olympics. Jack adds Kerry to the family business and makes Kevin, Kerry, and David compete in 6-man tag team matches. After the Von Erichs battle, Jack announces that David will go for the NWA championship. Kevin knows he has gone down a rank on Jack's list. The Von Erichs will fight to be at the top of Jack's list and the wrestling world while fighting the family curse.<br/><br/>This movie is a stunning tribute to the Von Erichs by bringing their story to a peaceful end. The casting was top-notch, and the actors embodied their roles. You can sense the deep love these brothers had for one another. The director overlays the brothers in the locker room, and viewers see their similar struggles and suffering. It's unbelievable that one family went through so much tragedy. This movie deserves to be seen on the big screen because it is honest perfection.<br/><br/>5 out of 5 stars.",
     "asin":{
@@ -45,18 +57,6 @@ var mustBuy = [
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theironclaw.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theironclaw.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Color Purple",
-    "review":"Celie and her sister, Nettie, reminisce about their mother when Celie's water breaks. Celie gives birth to a baby boy and names him Adam. Then, her father, Alfonso, enters to give the baby to God. Celie begs for more time with him, but her father says no. It's his baby, and he can do what he pleases.<br/><br/>Nettie walks home from school, and Mister follows her on a horse while serenading her. She tells him to leave her alone. Mister follows Nettie home and asks Alfonso for her hand in marriage. Alfonso says no. However, Celie means nothing to him, but she can work. As the men barter, Nettie and Celie watch. Alfonso tells Celie to pack her bags because she will marry Mister.<br/><br/>Celie, on foot, follows Mister on horseback to his home. He introduces her to his kids and tells her to start cleaning. Celine prepares dinner, and Mister demands to know why she didn't bathe the kids. She suggests doing it in the morning, and he slaps her to the floor. Late at night, Celie lays in bed and sees a picture of Shug Avery. Mister admits that's the woman he should've married. As Mister climbs on Celie, Celie stares at Shug's picture and dreams about her.<br/><br/>A few days later, Celie hears a knock at the door. It's Nettie. Alfonso started touching her, so she ran away. Celie begs Mister to let Nettie stay. He says yes. Everything seems to be going well until Mister creeps into Nettie's room. She pleads with him to stop and fights him when Mister resists. He grabs his gun and drags her outside. He threatens to shoot Nettie if she ever returns. When Celie tries to leave, Mister pulls her back and refuses to let Celie go. Nettie promises to write Celie once a week to let Celie know she is alive. But the letters never come, and Celie falls deeper into a loveless marriage of abuse. What will it take for Celie to discover a way to love herself and triumph over her dire circumstances?<br/><br/>Based on The Color Purple: A New Musical, this movie doesn't change the story. It only adds music to the plot. Now, the writers shorten the beginning between Nettie and Celie talking about a man doing his business. Also, the writers removed most of the internal monologue. The movie passes the torch between the original Celie and the current. Also, the sets change from a practical set to a stage setting without taking the audience out of the moment. The vocal performances are second to none, with perfect comedic timing. Be ready to sing, dance, cry, and stand taller after seeing this gem come to life on the silver screen.<br/><br/>5 out of 5 stars.",
-    "asin":{
-      "re":"B0CQ6JK2D9"
-    },
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thecolorpurple.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thecolorpurple.jpeg"
     }
   }
 ];
@@ -226,19 +226,6 @@ module.exports = mustBuy;
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/americanfiction.jpeg"
     }
   }
-
-   {
-    "option":"1",
-    "mtitle":"Dune: Part Two",
-    "review":"Paul and his pregnant mother, Lady Jessica, hide among the Freman people in the desert as word of their death travels through the houses. Stilgar pulls Jessica aside because he and other southern Freman believe Paul is there to fulfill the prophecy. A Reverand Mother is dying, and they want Jessica to take her place. Jessica says yes and goes through the deadly ritual but doesn't tell them she's pregnant. With her new status, Jessica will convince non-believing Freman that Paul is the Messiah.<br/><br/>Paul desires to learn the ways of the Freman. Stilgar gladly teaches Paul, and southern Freman delights in Paul's triumphs. However, a northern Freman, Chani, isn't impressed. She doesn't subscribe to the prophecy because she thinks it's an enslavement tool. Paul confides in Chani about his apocalyptic dreams as the two become inseparable. Paul's dreams warn that traveling to Southern Arrakis will bring death to billions.<br/><br/>Glossu Rabban Harkonnen sends spice harvesters and soldiers to the northern part of the Arrakis. Since the death of Duke Leto, Baron Vladimir Harroken wants his nephew, Glossu, to regain control of spice production. Vladimir thinks Glossu is a doting foot soldier but doesn't believe Glossu is ready to lead. Vladimir wants his other nephew, Feyd-Rautha, to take the throne. Emperor Shaddam IV's Truthsayer, Gaius Helen Mohiam, agrees. Shaddam's daughter, Princess Irulan, disagrees because Feyd-Rautha is a psychopath. Irulan keeps a secret. She suspects that Paul and Jessica are alive but unsure of their location. Can Paul and the Fremans stop the Harroken with the Emperor and his people secretly working for them?<br/><br/>Based on the novel Dune by Frank Herbert and the sequel to Dune, this film deserves the most vivid screen and best sound an audience can find. The graphics are extraordinary and clean. The sound gives the script a proper build for its iconic moments. The story is captivating and dramatic. The script doesn't force its high-level wit on you and profoundly uses silence. On this surface, this movie is about empires fighting over a resource. However, this plot gently discusses religion and looting. This sequel is better than the first film and deserves your time and money. Its stunning fight scenes, jaw-dropping graphics, and compelling storyline are worth the running time.<br/><br/>5 out of 5 stars.",
-    "asin":{
-      "re":"B0CW3DTWNG"
-    },
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dunepart2.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dunepart2.jpeg"
-    }
-  },
 
    {
     "option":"5",
