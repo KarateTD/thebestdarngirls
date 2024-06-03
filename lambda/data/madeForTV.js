80594d64774270be66739bded15aae4399476d75
Tiffany Gray <KarateTD@yahoo.com>
2024-06-02 20:23:27 -0400

    2 movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option":"1",
+    "mtitle":"For Love and Honey",
+    "review":"Anthropologist Dr. Austin teaches his class about Jacques De Beauvoir and his writings. Austin came to Malta to start his research about Jacques but hasn't made significant strides. Austin's research partner, Michael, warns Austin that he needs to write and submit his research paper in a week. If he doesn't, Austin must return to the states. Austin waves goodbye to his friend and walks towards the Port Grace Library. He wanders in the street, and a motorcyclist, Eva, dodges to miss him. They blame each other before both leave in a huff.<br/><br/>Eva returns to her apiary and informs her Maltese Honey Bees of her close call with a rude tourist. Her sister, Chloe, interjects with unfortunate news. Chloe didn't get the promotion. Then, Chloe alerts Eva to a bee emergency at the library.<br/><br/>Eva arrives at the library, and her friend, Luna, informs her about the new handsome, single doctor. They hear him scream from outside because an unwelcome bee slipped inside. Eva traps the be inside a glass with a 400-year-old document. Austin cringes and begs her to be careful with the historical paper. She releases the bee outside to follow it back to its hive. They find the hive, and Austin pleads for the paper. She promises to return it if he never swats another bee. He agrees, and she hands him the document.<br/><br/>Later, Eva returns to remove the hive safely. Austin sees a painting underneath as she chips away at the bee wall. He discovers a mural that breathes new life into his research. The mayor, Matthew, comes for a closer look. They have a festival coming up, and Matthew wants a marque event. This finding is it. Eva doesn't want the wall to distract from the protected bees. Austin must protect the wall for his research. Matthew puts them in charge of each other. They must preserve the wall and the bees for the festival and their love.<br/><br/>This sweet movie is about two people who want to preserve history. Eva's family kept bees for centuries and is part of local legends. Austin wants to uncover lost history. They never thought that they would deeply connect. And watching their passions join them is charming. This film is a sweet way to pass the time with your favorite locally sourced honey and tea.<br/><br/>3 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/forloveandhoney.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/forloveandhoney.jpeg"
+    }
+  },
+  {
+    "option":"2",
+    "mtitle":"Daddy's Deadly Secret",
+    "review":"Richard and Caroline discuss expanding their family after putting Richard's daughter, Abigail, to bed. Caroline loves Abigail and wants to have another child with Richard. Also, Richard tells Caroline he is getting his affairs in order after giving his sister, Darla, a restraining order.<br/><br/>Richard and Caroline drop Abigail off at her mother's home, and Richard speaks with Abigail's stepfather, Doug. Richard finds Abigail's mom, Ellie, in the bathroom with puffy eyes. She blames it on allergies and swears that she is taking her medication.<br/><br/>On the drive home, Caroline sees Abigail cross the street with a group of girls bullying a younger girl. The crossing guard, Marianne, introduces herself to Caroline. Marianne confirms Caroline's fears. Abigail's new friend group isn't a good influence. Caroline visits her patient, Beth, at Beth's home. Caroline attempts to get Beth to visit her office, but Beth refuses.<br/><br/>Caroline returns home and gets a call from Richard. Abigail didn't return to Doug and Ellie's house. Caroline runs upstairs, but Abigail isn't their either. Caroline and Richard run to Doug and Ellie's and discuss what to do next. Ellie admits that Abigail's demeanor is changing. Richard's anger boils over at the revelation, and they call the police. Caroline prays this is the first step to finding Abigail. But it's the slippery slope to the truth.<br/><br/>This movie has smaller pieces that come together in the end. So those bits need your attention as the plot circles back. Caroline and Ellie have a pivotal and revealing moment, but their crying makes them incomprehensible. So, it's difficult to understand the antagonist's ultimate deception. Otherwise, this film is a wild ride with lies, murder, corruption, and greed. It has something for everyone.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/daddysdeadlysecret.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/daddysdeadlysecret.jpeg"
+    }
+  },
+  {
+    "option":"3",
     "mtitle":"Cruise Ship Murder",
     "review":"Olivia drives her Aunt Theresa and Theresa's husband, Ian, to the docks for Theresa and Ian's cruise. Theresa pulls Olivia aside and informs Olivia that this isn't a pleasure cruise. Before Theresa elaborates, they have to board. Olivia waves goodbye to them.<br/><br/>Days later, Olivia gets a call from Ian. Theresa fell overboard during a storm, and the ship's officers can't find her. Soon, Olivia's fears come true. The police found Theresa's remains. When Ian arrives home, Olivia meets with Ian to check on him. He is understandably upset. She comforts him and gets in her car. Then, Shelly, a reporter, knocks on Olivia's window. Shelly asks to interview Olivia about Theresa's death. Disgusted, Olivia rolls up her window and drives away.<br/><br/>Olivia stands at Theresa's gravesite and mourns as Ellen approaches her. Ellen was Theresa's lawyer and now wants to talk to Olivia. Olivia goes to Ellen's office and learns Theresa tried to change her will before she died. This fact raises concerns for Olivia, but Ellen doesn't know how much Ian knew. Olivia plans to ask Ian later.<br/><br/>After talking to Ian, Olivia feels disturbed by his answers and demeanor. She decides to put her skills to work and investigate Theresa's death. Soon, someone sends Olivia death threats, stalks her, and attacks her. Can Olivia uncover what happened on the cruise before she loses her life?<br/><br/>This movie takes time to ramp up, but the body count rises exponentially. Unlike this review, viewers see everything happening on the boat before Theresa falls overboard. There was a drugging, an affair, and a violent storm. It was the perfect background for a devious death. The audience should give the plot a chance to play out. The movie has enough twists and turns to spark your interest.<br/><br/>3.5 out of 5 stars.",
     "image":{
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option":"2",
+    "option":"4",
     "mtitle":"Cradle of Deception",
     "review":"Widow Erin visits her IVF clinic, and Dr. Jackson, a personal friend, puts her mind at ease. Jackson assures Erin that she will be a good mother and her baby boy is healthy. A few days later, Erin gives birth to Ben and breathes a sigh of relief.<br/><br/>Now, Ben is four months old and can't stop crying. Erin grabs a thermometer, and Ben has a 102-degree temperature. Erin rushes him to the hospital. They run several tests and discover Ben has SCID, a genetic disorder. The pediatrician, Dr. Alice, says Ben needs a donor. She and her mother, Ruth, are Ben's only living relatives.<br/><br/>Erin gets the idea to have a local paper run Ben's story. They could raise awareness and find a donor. A reporter, Charlie, calls Erin and asks her to stop by for an interview. Later, Charlie tells Erin that her story sounds familiar to another family who used IVF at her clinic. Days later, Erin reads Charlie's article, an exposé about Jackson and the clinic, and can't believe her eyes. Then, she gets a letter from the clinic threatening to sue her for defamation. She can't afford a legal battle, so she confronts Jackson at his office. Jackson offers a free DNA test to determine Ben's lineage. Erin hopes this will settle her concerns, but it will open Pandora's box.<br/><br/>This film gives My Child Has My Doctor's Face a body count using a similar plot. Jackson follows the advice of keeping your friends close and your enemies closer. He is a cunning villain who keeps you watching with a dutiful wife, Diana, by his side. Everything else is predictable.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option":"3",
+    "option":"5",
     "mtitle":"The Ice Rink Murders",
     "review":"Megan got a scholarship to ice skate from Coach Diane. Megan meets Carly. Carly is a top-tier skater and a social media guru with sponsors. Her last partner, Brian, left her to skate with another competitor overseas. Now, Carly skates with her hockey player brother, Trevor. Fellow skater, Lyndsey walks past Megan with a warning to stay out of her way. Lyndsey got injured, and now Megan is there to fill in until Lyndsey is ready to compete. Carly introduces Megan to their equipment manager, Blaze. Megan prepares to become a champion, but someone wants her out of the way. And they will resort to murder to get the job done.<br/><br/>Don't let the title fool you because the body count is low. The viewer doesn't need to pay attention to understand what's happening, and the killer is evident to experienced Lifetimers. The film picks up when the culprit delivers their monologue, and a friend exposes it in real time. If you skip this story, you won't miss much.<br/><br/>2.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theicerinkmurders.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theicerinkmurders.jpeg"
-    }
-  },
-  {
-    "option":"4",
-    "mtitle":"Everything Puppies",
-    "review":"Scarlett cracked the code for the perfect healthy puppy treat. She runs to her friend's store for marketing assistance. Her friend, business partner Gina, owns and operates Chew-That boutique. Scarlett needs Gina to create a logo and campaign so Scarlett can market her treats to local pet stores.<br/><br/>Gina created a campaign after her puppies demolished the treats. She named the treats Puppy Palate. Scarlett and Gina go to pet stores around town, but none have space for a new product with zero sales history. Also, they all carry Pup Chuck treats. Then, they remembered another store that opened last year called Paws and Wellness Emporium. They leave to meet with the general manager, Alex.<br/><br/>Before Alex says no, Scarlett suggests Alex try the Puppie Palate treat on his dog. He does and orders 100 boxes. Scarlett rashly agrees to make them by tomorrow.<br/><br/>Scarlett makes the order in time, and it sells out quickly. Alex asks her for a date, and she assumes it's business only. But Gina can see the attraction. Alex tells Scarlett he will ask the regional manager, Michelle, to hear Scarlett's pitch. Scarlett thinks they have a future outside of work.<br/><br/>Alex sits with Michelle, his ex-girlfriend, and discusses Puppy Palate. She hopes he is there to rekindle their relationship, but he is 100% professional. He pitches the idea, but Michelle doesn't think that the new product can repeat its success. However, she will consider it.<br/><br/>At Pup Chuck, the director of sales Paul wants a way to fatten the margin. Paul learns his sales are down at the Paws and Wellness Emporium and meets with Michelle. When he discovers Michelle is allowing a branch to carry a second brand, he threatens to pull his products from their shelves. Michelle calls Alex and tells him to end Scarlett's deal. Crestfallen, Michelle ends her relationship with Alex. Can Alex save his business and personal relationship with Scarlett without losing his store?<br/><br/>This dog lovers movie is cute and has an adorable ending with a 4-legged judge. The plot, as the title, depends on your love of puppies to keep viewers invested. Otherwise, it's the romantic comedy we all know. Scarlett and Alex's moment of triumph feels like a Leverage plot and makes the movie fun. Otherwise, save this movie for a rainy day with your pup.<br/><br/>3 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/everythingpuppies.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/everythingpuppies.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"The Bad Guardian",
-    "review":"Leigh repeatedly calls her father, Jason, while on vacation with her family. However, she can't get an answer. So, they cut their vacation short to drive to Jason's home. They find it empty with blood on the floor. They call the police, and Detective Haywood finds Jason at Shadyside Assistant Living facility.<br/><br/>Jason fell and got a concussion. Amongst the confusion, the hospital called a woman, Janet, to watch over Jason. Leigh meets Janet and Janet's brother, Dave. They explain guardianship to Leigh. Janet explains that Leigh has to go to court to remove guardianship.<br/><br/>After a few days, Leigh gets someone on the phone and learns Janet is applying for permanent guardianship today. Leigh runs to the hearing, but the judge rules in Janet's favor. She hopes to save up enough money to free Jason. However, the clock is ticking. Janet wiped out Jason's bank accounts, put his house up for sale, and auctioned off his belongings. Jason is a victim of Janet's greed. Will Leigh help him survive this scam with his life?<br/><br/>This movie spotlights the systems keeping this crime in place. Corporate greed, political gain, and fear keep loving families away and elders under a stranger's control. Janet has an intricate system of family and friends to maintain her control over her wards. Leigh has to navigate this while caring for her family, limited knowledge of the system, and working. This movie is a necessary call to action. Protect the people you love today and do everything to protect yourself tomorrow.<br/><br/>5 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thebadguardian.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thebadguardian.jpeg"
     }
   }
 ];
@@ -58,20 +58,38 @@ module.exports = madeForTV;
 
 {
   "option":"1",
-  "mtitle":"For Love and Honey",
+  "mtitle":"The Girl Locked Upstairs: The Tanya Kach Story",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/forloveandhoney.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/forloveandhoney.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thegirlockedupstairstanyakach.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thegirlockedupstairstanyakach.jpeg"
   }
 },
 {
   "option":"2",
-  "mtitle":"Daddy's Deadly Secret",
+  "mtitle":"Tipline Mysteries: Dial 1 for Murder",
   "review":"<br/><br/>* out of 5 stars.",
   "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/daddysdeadlysecret.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/daddysdeadlysecret.jpeg"
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/tiplinemysteriesdial1formurder.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/tiplinemysteriesdial1formurder.jpeg"
+  }
+},
+{
+  "option":"3",
+  "mtitle":"My Husband's Killer Affair",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myhusbandskilleraffair.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myhusbandskilleraffair.jpeg"
+  }
+},
+{
+  "option":"4",
+  "mtitle":"May the Best Wedding Win",
+  "review":"<br/><br/>* out of 5 stars.",
+  "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/mythebestweddingwin.jpeg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/mythebestweddingwin.jpeg"
   }
 }
 
