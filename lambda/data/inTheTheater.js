a559558fe1661e9c7901836b9fb3569db1dacf9c
Tiffany Gray <KarateTD@yahoo.com>
2024-06-06 23:36:29 -0400

    bad boys

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option":"1",
+    "mtitle":"Bad Boys: Ride or Die",
+    "review":"Marcus delivers a toast at a moment he thought would never happen: Mike's wedding. Marcus cries as he wishes Mike the best with his new bride, Christine. Marcus gets help from Captain Conrad's daughter, U.S. Marshall Judy, and granddaughter, Callie. Marcus dances the Wobble and suffers a heart attack. Captain Conrad assures Marcus that he won't die and gives Marcus a warning. Marcus wakes up with a new lease on life and is 100% fearless. As Marcus begs Mike to get him Skittles, Mike gets a news alert on his phone. The FBI found evidence that Captain Conrad was working with the drug cartels.<br/><br/>Mike and Marcus run to headquarters to clear their captain's legacy, but Captain Rita and her boyfriend, Attorney General Lockwood, must follow the evidence. Judy enters and demands they find the real culprits. Mike offers to help, but she refuses because Mike's son, Armando, killed her dad.<br/><br/>Mike and Marcus ride and contemplate their next steps when they get a video message from Conrad. He was looking into corruption after their ecstasy bust years ago. Now, he needs his Bad Boys to clear his name. He warns them to trust no one. Mike believes they must ask a cartel member to catch the corruption.<br/><br/>Mike and Marcus visit Armando in prison. Armando states that Conrad was clean. Also, his mother, Isabel, didn't put Conrad on the list; someone added him. Mike and Marcus find Conrad's secret case files and pull Armando out of jail to go through them. They believe they have an advantage, but the culprit is one step ahead. To get through this, Mike and Marcus will need fate, AMMO, Reggie, and alligators.<br/><br/>For newcomers to the franchise, it helps to see the three previous films before watching this 4th installment. This chapter of the buddy-action-comedy will introduce a new version of Bad Boys, tie up loose ends, and show you another side of Reggie. Unlike the previous films, Mike is cautious and has panic attacks. However, Marcus is fearless and wild. Their switch, along with the action-packed fight scenes, make this movie entertaining. Be prepared to laugh, wince, and cheer.<br/><br/>4 out of 5 stars.",
+    "image":{
+      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/badboysrideordie.jpeg",
+      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/badboysrideordie.jpeg"
+    }
+  },
+  {
+    "option":"2",
     "mtitle":"Summer Camp",
     "review":"Nora arrived at Camp Pinnacle because her parents wanted her to make friends. Nora hears Jane begging Mary to get out of the bathroom. Ginny barges into the bathroom and discovers Mary started her period. Ginny talks Mary through it and pulls Mary out. Later, Ginny sees Nora struggling at archery and assists. The counselors put Nora, Mary, and Ginny in a cabin together. The three remained friends.<br/><br/>It's their Camp reunion, and Ginny, a bestselling author, is excited to attend but worries her friends won't make it. Mary wanted to become a doctor. But she left school to become a wife and mother. She became an E.R. nurse later, but Ginny thinks Mary settled for her husband. Nora excelled in school and obtained multiple degrees. She dove deeper into work after her husband died. Ginny believes this reunion is what their friendship needs. It's so much more.<br/><br/>Each of these women stands their ground and finds their voice. Nora delves into work because it's safe but never ventures out for more. Nora learns her wants in marriage and friendship. Ginny assumes she has all the answers, but only because she doesn't let anyone else speak. After they grow and admit their faults, they ask one question: Am I The A. Hole. While the movie had the fun and raunchiness of camp, the bloopers were funnier than the film. The start and conclusion were entertaining, but the middle dragged. If this plot isn't for you, wait for streaming.<br/><br/>3 out of 5 stars.",
     "image":{
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"2",
+    "option":"3",
     "mtitle":"Furiosa: A Mad Max Saga",
     "review":"Furiosa plucks fruit with her sister, Valkyrie, when she sees scavengers in their woods. Furiosa's home is an oasis, The Green Place, in the wastelands, and she has to protect it. One of the scavengers captures and kidnaps Furiosa. Valkyrie warns their mother, Mary. Mary finds Furiosa but gets captured by Dementus, the biker leader. Dementus sees Furiosa is in good health and demands to know her origin. Both refuse to talk. So Dementus makes Furiosa watch as he kills her mother.<br/><br/>Dementus sees his men are starving and orders Immortan Joe, ruler of the Citadel, to give his people more food and water. When Joe doesn't listen, Dementus captures the ruler of Gastown and brings him to the Citadel. Joe gives in to some of Dementus's requests and makes Dementus the new ruler of Gastown. However, Joe wants Furiosa as a future wife because she has never been sick. Furiosa must escape Joe, get revenge for Mary, and return to The Green Place.<br/><br/>This prequel has the action you expect from the Mad Max series while answering fanatics' questions about Furiosa. This installment tells you who kidnapped her, what happened to her arm, the start of her face paint, and how she developed into a War Rig commander and Imperator. Also, this movie, unlike its predecessor, is told in four parts - The Pole Inaccessibility, Lessons from the Wasteland, The Stowaway, and Homeward. And the director brilliantly connected the two movies for die-hard fans in the last scene. When there isn't an explosion to rely on, the dialogue and storytelling dulls.<br/><br/>4 out of 5 stars.",
     "image":{
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"3",
+    "option":"4",
     "mtitle":"The Garfield Movie",
     "review":"Garfield waits for his father, Vic, to return but can't ignore his stomach after hours alone. Lonely, Jon looks out the window and sees Garfield's sad eyes. Jon opens the window and gives Garfield a few slices of pepperoni. Garfield decides to go with Jon because Vic never returned. Jon takes Garfield home and later adopts Odie.<br/><br/>Today, Garfield and Odie enjoy the good life, excluding Mondays. Garfield wakes up for his midnight snack and opens the fridge. Someone kidnaps Odie and Garfield before Odie cooks.<br/><br/>The kidnappers, Noland and Roland, hang Odie and Garfield by their feet and leave them there. Then, a masked cat cuts them down. Garfield recognizes the cat as Vic, his father. Garfield doesn't want to exit with Vic because Vic abandoned him in the alley. Jinx appears from the darkness and scares everyone. She ordered Roland and Noland to take Garfield and Odie to lure Vic out of hiding. When the city pound caught Jinx, Vic left her behind. Now, she wants Vic to steal a quart of milk - for each day she was in the pound. It will be a lot of milk because Jinx was in prison for over four years. She tells Vic to take Garfield and Odie with him to help. Can this heist repair this broken bond?<br/><br/>This movie has good intentions but doesn't spark laughter. It had physical, slapstick humor for kids. And it slipped in adult, non-sexual jokes. The plot's saving grace is explaining a father's love and sacrifice to the audience. Garfield feels abandoned, but he is never alone. However, that message loses its luster with the multiple product placements.<br/><br/>2.5 out of 5 stars.",
     "image":{
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option":"4",
+    "option":"5",
     "mtitle":"Sight",
     "review":"Dr. Ming Wang successfully restored the sight of another patient at the Wang Vision Institute. He credits his assistant, Ruth, and his surgical partner, Misha Bartnovsky. Ming gets a new patient named Kajal from Calcutta, India, days later. A nun found Kajal in the street and prayed someone could restore Kajal's vision. At first, Ming doesn't want to do the surgery. Then, Misha explains Kajal's brutal scarring. Her stepmother uncovered that people pay more to blind beggars on the street. So Kajal's stepmother poured sulfuric acid into Kajal's eyes. Ming gets a vision of an old schoolmate, Lili. Ming decides to help Kajal in a way he could never do for Lili. Ming will discover that there is more to sight than seeing.<br/><br/>Based on From Darkness to Sight: A Journey from Hardship to Healing by Dr. Ming Wang, M.D. Ph.D., this movie shows the viewer the beauty of tenacity, triumphs, and failures. Overcoming his past was a driving force in his career until this case. While this movie will spark joy and inspiration, the audience won't want to buy it. However, you may want to share it.<br/><br/>3.5 out of 5 stars.",
     "image":{
       "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sight.jpeg",
       "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sight.jpeg"
-    }
-  },
-  {
-    "option":"5",
-    "mtitle":"IF",
-    "review":"Elizabeth, better known as Bea, temporarily moves in with her grandmother during her father's treatment. For Bea, this brings back memories of when she lost her mother. Her mother encouraged her to tell stories, perform, and play make-believe. Her dad does the same with his joyful attitude.<br/><br/>After visiting her dad, Bea sees a young girl running in the hallway. Bea chases after her, but the girl runs into the top-floor apartment. Bea knocks, and a man answers without opening the door. The man answers that no girl lives there.<br/><br/>Bea finds an old camcorder and plays the tape inside. It's a recording of happier times with her mother. Then she sees the man and girl outside her window. Bea follows them down the street and watches the man enter a brownstone. Inside the home, the man, Cal, asks Blue to come out of the closet. Outside, Bea sees Blue's large purple hand open the bedroom window. She turns in disbelief and sees the girl from the apartment. But she is not human. Bea faints.<br/><br/>Bea wakes up in Cal's apartment, and the 'girl,' Blossom, makes Bea tea. Blue explains that they are IF, an acronym for Imaginary Friend. The kids that manifested them grew up. Cal is a matchmaker of sorts. He is helping them find new kids to match with. Bea gets an idea. They will locate the kids who created them as adults now. If they can get the adults to remember them, maybe they'll save the IFs. Bea, who tries to be an adult, will discover the beauty in fun and imagination.<br/><br/>This film is an astounding, heartfelt story that asks, 'What IF.' Viewers must catch this movie from the beginning. The script delivers the lovely relationship between Bea and her parents in a montage. Seeing the IFs helps Bea find joy in having fun again. The script shows the audience the importance of Imaginary Friends then and now. And the score will make your heart swell with excitement. You will want the DVD to uncover what they left on the cutting room floor. If you saw this movie, don't ruin the twist. But adults may see it coming.<br/><br/>4 out of 5 stars.",
-    "image":{
-      "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/if.jpeg",
-      "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/if.jpeg"
     }
   }
 ];
@@ -56,15 +56,7 @@ module.exports = inTheTheaters;
 
 /*
 
-{
-  "option":"1",
-  "mtitle":"Bad Boys: Ride or Die",
-  "review":"<br/><br/>* out of 5 stars.",
-  "image":{
-    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/badboysrideordie.jpeg",
-    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/badboysrideordie.jpeg"
-  }
-}
+
 
 */
 
