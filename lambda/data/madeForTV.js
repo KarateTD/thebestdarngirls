a84943ed31d7ad5a16687c612a447487eca11555
Tiffany Gray <KarateTD@yahoo.com>
2024-07-07 12:52:17 -0400

    add 3 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Amish Affair",
+    "review": "Hannah finds Aaron waiting for her at the train station. He takes her in his horse and buggy to his home that he shares with his wife, Sarah, and daughters, Rachel and Grace. Hannah inquires if the congregation knows about her sorted past. While the police cleared Hannah, her Amish community didn't. Aaron believes Sarah has ALS, but she refuses to see a doctor or take modern medicine. Hannah eats dinner and settles in for the night. She will take over Sarah's household duties while caring for Sarah.<br/><br/>Hannah is working in the barn when Aaron approaches her. She rejects his advances earlier but gives in to temptation. However, Hannah stops the affair after watching the tribunal shun Miriam for tempting a married man. Soon, Hannah meets two of Aaron's employees, Jacob and Laura. He asks Hannah out on a date, infuriating Aaron. Aaron tells Jacob about Hannah's past to coerce Jacob to break up with Hannah. To Aaron's surprise, Hannah still won't date Aaron while Sarah is alive. Days later, Sarah dies, and Aaron turns to Hannah for comfort. The police investigate after hearing about Hannah's past. When gossip spreads around the small community, Hannah must discover the truth or lose her freedom.<br/><br/>Inspired by actual events, this movie slowly entices viewers into a sexy murder plot. While the audience knows the killer, you will be unsure when the crime started. Aaron uses Hannah's fears to push her into a relationship with him. And her community solidifies her concerns by shunning Mariam. While the conclusion gives the characters a happy ending, it's obvious the community's mindset doesn't change about who they punish. This film will whet your appetite.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/amishaffair.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/amishaffair.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "Little Girl in the Window",
+    "review": "Paul returns home to find his wife, Iris, on the couch watching an unsolved crime show about a missing girl named Jennifer. Iris suffers from depression after a miscarriage. Paul gives Iris a professional camera to help Iris engage with the world.<br/><br/>Iris leaves her home for the first time in months and takes pictures of homes around town. She stumbles across a house but notices strange writing on the window. Iris snaps a few photos until a barking dog frightens her. Mrs. Norris chastises the pup and introduces herself. They converse, and Iris leaves.<br/><br/>Iris examines the photos and sees the writing on the window spelled HELP backward. She goes to Robert's home, and he catches Iris standing outside. As Robert extends his hand to greet Iris, Iris sees a little girl in the window and yelps. Iris takes her concerns to Detective Clark. Clark thinks Iris is mistaken but goes to Robert's home. Clark asks about the little girl, and Robert grabs a large doll. Clark says goodbye and contacts Iris. He threatens to file a police report against Iris if she makes false allegations again.<br/><br/>Iris doesn't accept Detective Clark's excuse and travels to Robert's home again. She sternly asks to see the doll, and Robert invites Iris inside. She tries to run, but Robert grabs her. Iris wakes up in a basement, chained to the floor. She sees two women chained at opposite ends of the room. Iris vows to escape, but how?<br/><br/>This movie will have you questioning reality for the first hour. You won't be sure if Iris is seeing things or not. It's easy to deduce that Iris is dealing with trauma from losing her child. There are two things viewers should notice. First, Robert screams a warning into a so-called empty house. And the final hint, Robert grabs Iris. After that moment, it becomes the standard Lifetime escape film.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/littlegirlinthewindow.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/littlegirlinthewindow.jpg"
+    }
+  },
+  {
+    "option": "3",
+    "mtitle": "Falling Like Snowflakes",
+    "review": "Photographer Teagan meets with her friend, gallery owner Charlotte, to discuss the Christmas Auction at Winslow Gallery. Teagan hopes her snowflake images will garner enough money to fix some of the Willow Creek Community Center's repairs. She prays for a Christmas miracle and drives to the center to meet her students. After class, Teagan sees Julie standing alone. Julie knows her father, Noah, will come, but he is plowing the recent snowfall. Teagan offers to drive Julie to Noah's plow garage to help. Teagan and Noah stammer at the other's appearance, and Julie notices. She asks about their history, and they explain they are old friends.<br/><br/>Charlotte asks Teagan to come to the gallery as soon as possible. Calvin, an art collector and hotel owner, is interested in Teagan's photographs. Calvin knows there are 35 types of snowflakes but sees 34 images. He wants a complete collection. Calvin offers to buy the collection if she gets the last one. Calvin's purchase will cover the center's repairs and make this show the most successful. Teagan agrees to try. After Teagan leaves, photographer Susan overhears the snowflake conversation. She decides to find the snowflake first to get in the show.<br/><br/>Teagan sees Noah in the town square and questions him about the weather. Noah openly wonders about her interrogation, and Teagan admits this impending storm is the perfect condition for her missing snowflake. This storm will be dangerous, so Noah insists on going in his snow plow. This pair will hunt for a mysterious snowflake, but will they find something more before Susan beats them to the Christmas Auction?<br/><br/>This movie has interesting facts about the weather and snowflakes, but the film won't need your full attention. Its saving grace is Susan's trickery. She feels like a Disney villain. You will love to hate her. And her character has more growth than others. Also, Julie's gift for Noah is incredible, and the reveal will make you misty-eyed. The plot is predictable and shows why Hallmark saved this film for July.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/fallinglikesnowflakes.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/fallinglikesnowflakes.jpg"
+    }
+  },
+  {
+    "option": "4",
     "mtitle": "The Killer Inside: The Ruth Finley Story",
     "review": "Ruth Finley prepares dinner for her husband, Ed, and mother, Fay, while watching the latest news about the BTK Killer. Ed turns off the TV. He's worried that Ruth is too engrossed with the case and knows more than the police. After Fay leaves, Ed discusses taking a trip to Europe instead of their typical vacation. Ruth is unsure.<br/><br/>Ruth tells her friend, co-worker Susan, about Ed's idea. Susan's shocked that Ruth didn't say yes but says it aligns with Ruth's predictable ways. Later, Ruth accepts the European vacation while she and Ed garden. Ed beams with joy until he has a heart attack and falls in the yard. They rush Ed to the hospital, and doctors care for him. Ruth wants to stay by his side, but Ed convinces her to leave and rest. Later, an unknown person calls Ruth. The caller has intimate knowledge about her previous attack. Ruth hangs up and doesn't sleep.<br/><br/>Ed returns home after days in the hospital. He feels good but sees something is bothering Ruth because she is relentlessly baking. Ruth brushes off his worries and focuses on her job. Then, Ed gets a call from Susan. Ruth left work for her lunch break and never returned. Two men kidnapped her, attacked her, and pulled her into the woods. She escaped. However, Ruth's torment doesn't stop there. The attacker continues to call and write threatening letters. The police believe it's BTK. Will Ruth survive, or will the killer succeed?<br/><br/>The movie starts with the brutal slaying of a Kansas woman in 1977 with her son in the home. It helps viewers understand the police's urgency to protect Ruth and find the killer. Ruth receives several threats via phone and mail. As the story follows Ruth's harassment, the plot slows. But one late-night moment will pull you back into this story and make you scream at the TV. This movie isn't fantastic, but it's intriguing.<br/><br/>3.5 out of 5 stars.",
     "image": {
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "5",
     "mtitle": "Devil on Campus: The Larry Ray Story",
     "review": "Talia Ray tells her friends, Claudia, Isabella, Santos, and Daniel, that her father, Larry Ray, is out of jail and outside their door. Larry enters with a bag of cash, a limo, and a story. He gives the college students money for food and offers to cook. He stays the night in their dorm room. However, Claudia's concerns grow when Larry doesn't leave and spends time in Isabella's bedroom. Claudia's parents, Tom and Christian, meet with Sarah Lawrence College officials about Claudia's living arrangements. However, the offical reminds Tom and Christian that the students are over 18 and parents can visit without limiting the duration. Unsatisfied, Tom and Christian leave the campus and offer to let Claudia stay home this semester. Claudia doesn't want to go. They smile, hug, and leave Claudia to stay in the dorm. It's a mistake they will all regret. Larry Ray is evil personified.<br/><br/>This movie will horrify and disgust you. But it should be a discussion topic because it is a true story. Larry Ray slowly crept into these students' lives and manipulated them. He blamed them for his mistakes, issues, or illnesses and never took accountability. Larry relished in his control while gaining millions from them. The school failed these students because they had the power to stop it. This film should have had two parts. Viewers should watch this movie with their older teens and first-year college students.<br/><br/>4.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deviloncampusthelarryraystory.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deviloncampusthelarryraystory.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "Discovering Love",
-    "review": "Archeology professor Skyler lost another student, Mary, to a better program with more opportunities. After class, the head of the archeology department, Iris, enters with glum news. The school wants to shut down the archeology department because they don't have enough students. They can stay if they can get their enrollment up before the next semester. Skyler assumes she has three months, but Iris corrects Skyler and says they have one.<br/><br/>Eric unearths a box but can't identify the origin. His benefactor, Charles, isn't happy because he wants his name in a museum. Eric reminds Charles that Eric doesn't have a degree in archeology. Charles threatens to pull Eric's funding but gets an idea. He contacts Dimitris and requests Dimitris find an archeologist to help Eric. Dimitris calls Iris.<br/><br/>Iris alerts Skyler that someone found a link to the Helen of Troy crown once owned by Princes Dafni. Iris wants to research the item in Greece, but the University won't fund it. Iris and Skyler decide to go to Greece with their money and find the crown in the next month. Finding the crown could rewrite history and prove legends true.<br/><br/>Skyler packs for the trip, and her friend, Janelle, gives Skyler some items. Janelle wants Skyler to use this research opportunity to meet someone. Then, Janelle shows Skyler a picture of Eric digging in Greece. Skyler cringes because Eric is a treasure hunter for the rich. She considers Eric a problem within the business.<br/><br/>Iris and Skyler check into their hotel before meeting Dimitris. Skyler goes to the meeting but rolls her eyes when she sees Eric enter Dimitris's office. Skyler and Eric protest, but Dimitris won't hear it. Charles paid for both of them to find the crown. The archeologist and treasure hunter must work together to save the archeology program. Will they uncover more?<br/><br/>This movie is Indiana Jones with a love story. Unlike other romcoms, this movie offers suspense because someone tracks our adventurers on the small island. Of course, Skyler is by the book, while Eric follows his gut. They collaborate by relying on Eric's instincts and Skyler's research. This movie isn't a favorite of the 2024 June Grooms season, but it's fun to watch.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/discoveringlove.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/discoveringlove.jpeg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Two Scoops of Italy",
-    "review": "Chef Danielle's restaurant has been closed for a month while she revamps her menu. Her investor, Owen, meets with Danielle to try the new menu, but it is underwhelming. He will give Danielle one month to update her menu. If she doesn't wow him, he will pull his funding. Danielle goes home to her sister, Sarah, who consoles Danielle with pizza. Sarah reminds Danielle about their cottage in Italy. Sarah hopes a change in culinary scenery will spark Danielle's creative engine.<br/><br/>Giancarlo gives Nico a sample of his latest gelato flavor. The young boy takes a bite, returns it, and asks for the strawberry. Giancarlo pleads with his father, Aldo, to let Giancarlo keep his late mother's traditions alive by creating new flavors. But Aldo says no. Then, Danielle enters their shop, asking for directions and coffee. Giancarlo offers her a sample of his new flavor, and Danielle gives Giancarlo an honest and harsh critique. Giancarlo abruptly ends their conversation and points Danielle in the right direction.<br/><br/>After playing games with the townspeople, Aldo approves of Giancarlo trying new flavors. But there is a catch. If Nico dislikes them, Aldo won't add them to the menu. Danielle walks in and asks for directions to several locations. Giancarlo offers to show her around Italy, but she has to help him create new geloto flavors. Danielle agrees to help Giancarlo in exchange for his guidance around Italy. Danielle hopes working with Giancarlo will spark her to create a new menu, but will she find more?<br/><br/>As viewers watch the movie, it will be easy to look away. However, this lapse in attention will leave you confused. Sporadically, Danielle reads a book, and the script acts out the scenes with Danielle and Mario. So watch the background. The games are fun, the scenery is beautiful, and the food sounds exquisite, but this film lacks passion. Viewers always know the ending for Hallmark plots, so the journey has to grab you. This movie doesn't.<br/><br/>3 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/twoscoopsofitaly.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/twoscoopsofitaly.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Bad Orphan",
-    "review": "Carl and Jessica watch their daughter, Rhiannon, leave with her boyfriend, Derek. Then, they get a call from their adoption specialist, Maya. Unfortunately, another mother rejected them for her child. However, Maya has an alternative. She has an 8-year-old girl, Gabby, with special needs looking for a forever home. Jessica and Carl ask for time to think about it. Later, Jessica reads the girl's life story and finds similarities. She and Carl decide to adopt the child.<br/><br/>Gabby takes a girl's family drawing and pays her with stolen money. Maya looks at Gabby's picture and tells Gabby that Carl and Jessica have a 15-year-old daughter. Gabby sighs deeply and corrects the drawing. Carl and Jessica meet Gabby and welcome Gabby to their home.<br/><br/>Jessica takes Gabby to an open house because Gabby hasn't enrolled in school. Gabby asks to go to a movie instead, but Jessica says no. Jessica prepares cookies for potential buyers, and Gabby offers to help. A couple enters the home because the wife left her cell phone. Jessica calls the phone to locate it and finds Gabby covering it in cookie dough. Gabby smiles and says she is helping. However, Jessica believes there's more to the story. Jessica reveals to a friend, Lani, that Gabby could be older than she appears. Can Jessica uncover the truth before Gabby takes over the family?<br/><br/>Inspired by the story of Natalie Grace, this movie's conclusion has a wild twist. Some viewers will love it, but it could turn off others. Either way, the script allows viewers to decide if Gabby is crazy, manipulative, or both. Did Jessica go too far to prove Gabby's age?This film is a crazy ride with a surprising twist that will divide viewers, but it needs part 2: The Bad Orphan: Jessica's Revenge.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thebadorphan.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thebadorphan.jpeg"
     }
   }
 ];
@@ -58,29 +58,38 @@ module.exports = madeForTV;
 
 {
 "option":"1",
-"mtitle":"Amish Affair",
+"mtitle":"Sister Wife Murder",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/amishaffair.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/amishaffair.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/sisterwifemurder.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/sisterwifemurder.jpg"
 }
 },
 {
 "option":"2",
-"mtitle":"Little Girl in the Window",
+"mtitle":"Signed, Sealed, Delivered: The Tale of Three Letters",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/littlegirlinthewindow.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/littlegirlinthewindow.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/signedsealeddeliverdataleofthreeletters.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/signedsealeddeliverdataleofthreeletters.jpg"
 }
 },
 {
 "option":"3",
-"mtitle":"Falling Like Snowflakes",
+"mtitle":"Deadly Wives Club",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/fallinglikesnowflakes.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/fallinglikesnowflakes.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlywivesclub.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlywivesclub.jpg"
+}
+},
+{
+"option":"4",
+"mtitle":"Couples Retreat Murder",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/couplesretreatmurder.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/couplesretreatmurder.jpg"
 }
 }
 
