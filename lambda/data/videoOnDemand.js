a5949c364df19c2a7feaca195e167a7b96b761dd
Tiffany Gray <KarateTD@yahoo.com>
2024-07-11 14:58:56 -0400

    divorce in the black

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Tyler Perry's Divorce in the Black",
+		"review": "Pastor Clarence preaches to Linda's surviving four sons at her eldest son's funeral. He warns the men not to follow in their older brother's footsteps, or they will follow his path to hell. Linda blames her daughter-in-law, Ava, for Clarence's remarks because Ava is Clarence's daughter. She orders her sons to remove the body from the casket. Afterward, Ava's friend, Rona, and Rona's husband, Jim, try to console Ava. However, Ava wants to find her husband, Dallas, and stand by his side during his time of need.<br/><br/>Ava arrives at Linda's home and receives an icy welcome. Ava sits in Dallas's truck and attempts to comfort him, but he forces her to leave. Dallas and his brothers go to a bar, drink, fight, and play pool. Ava finds Dallas and tells him to go with her because he has work in the morning. Ava waits in the parking lot and runs into her old crush, Benji. They converse until Dallas sees them together. He warns Benji to leave, or they will beat Benji worse than the last time. Ava drives Dallas home.<br/><br/>Ava comes home after working at a bank and finds Dallas drinking at the counter. He reminds Ava that they have dinner plans with Rona and Jim, and he wants to leave now. She comes downstairs after 40 minutes and finds Dallas gone. Dallas demeans Ava when she arrives and screams he wants a divorce. He exits, and Ava cries. Ava drives to her parents' home.<br/><br/>After a week, Ava gains new determination and reconnects with Benji. Dallas's brother, Morris, sends Dallas an image of Ava and Benji at a carnival. Dallas storms into Ava's workplace and rages about her relationship with Benji. Confused, Ava tells Dallas that she never cheated and the meeting with Benji was innocent. Dallas orders Ava to meet with his attorney in the morning and sign the papers.<br/><br/>Rona, Jim, and Dallas sit in a conference room with Dallas's lawyer. Ava enters in a sexy dress and wants to know where to sign. Then, Ava signs and leaves with Rona. Ava is ready to start a new chapter in her life. Dallas fumes because she didn't fight. He thinks Ava is disrespectful and an adulterer. He wants to give her a second chance to prove she can be a wife or kill trying.<br/><br/>This latest installment in the Tyler Perry library brings a better story to his formulated plots. It uses modern scenarios to freshen the storyline and increase the intensity. Ava is a pastor's daughter who doesn't want to disappoint her parents. So, she stayed in an unhealthy relationship and ignored the warning signs. Also, you will see Dallas's quick mental decline and why it happens. This movie has the believable ingredient missing from Tyler's latest projects. And the opening and final scenes will leave your jaw on the floor.<br/><br/>4 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/divorceintheblack.jpg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/divorceintheblack.jpg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Beverly Hills Cop: Axel F",
 		"review": "After apprehending burglars with Detective Mike Wood, Detective Axel Foley gets called into Chief Jeffery Friedman's office. Jeffery put in his retirement paperwork. However, Axel senses something is off. Axel deduces that Jeffery offered his job to save Axel's badge, and Axel feels awful.<br/><br/>In Beverly Hills, defense attorney Jane Saunders enters a prison to talk to inmate Sam Enriquez. She wants to handle his case for free because she thinks someone set up Sam. Sam rode in a car with Undercover Detective Copeland to sell drugs. Then, someone shot Copeland, and the police arrested Sam. Jane walks to the car, and masked assailants attack by dangling her vehicle off the edge of a parking garage with her inside. Private investigator Billy Rosewood begs Jane to leave the case and not act like her father. But Jane refuses. So, Billy calls Jane's father, Axel, and updates Axel on Jane's dire situation.<br/><br/>Axel races to Billy's office and finds two thugs tearing it apart. He convinces the thugs he is on their side and searches Billy's desk. He rips out a page from Billy's planner. Then, the hoodlums find Axel's picture on the wall. Axel escapes their grasp but breaks several laws. Detective Bobby Abbott charges Axel as Axel screams for Chief John Taggart, but John isn't there. Later, John pulls Axel into his office and introduces him to Captain Grant Cade from narcotics. Axel demands to know more about Sam and Jane. John tells Axel and Bobby to leave the case alone. Nothing will stop Axel from finding the people who threaten his daughter.<br/><br/>This fourth installment in the Beverly Hills Cop franchise returns with your favorites and sticks to the winning formula. The movie opens with a wild chase followed by a screaming chief. Then, a case brings Axel to Beverly Hills. The action starts when Axel lands and only family tension eases it. The writers took time to learn why fans love the franchise and provided the same laughs and high-energy action. To parents, this movie isn't for children. However, it will attract Beverly Hills Cop lovers while gaining new fans to watch the previous pictures.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "No Time to Spy: A Loud House Movie",
 		"review": "Lincoln runs to the door to greet his Pop Pop, Albert, and future Gran Gran, Myrtle. He loves hearing her stories about being a spy and dreams of becoming an operative like David Steele. The Louds scramble to pack for their family vacation for Albert and Myrtle's wedding. Myrtle reminds Lincoln that she's retired but has a dangerous mission for him: Operation Wedding Ring. Lincoln is the best man and must protect the rings until the nuptials.<br/><br/>Lincoln and the family board a cruise ship after a wild flight. Lincoln accidentally takes photos of a shady arms deal. The bad guys see him and swim to the boat. The goons jump on the ship and demand the camera. Myrtle takes them down before arriving on shore. She pulls Lincoln inside of a tent and calls X at headquarters. X asks Myrtle to come out of retirement and handle it because Myrtle is in the area. Myrtle surprises Lincoln when she says no. Myrtle leaves the tent to help Albert, and Lincoln grabs her phone. He secretly texts X from Myrtle's phone and accepts the mission. Lincoln will save the world with or without Myrtle's help. It's about to get LOUD!<br/><br/>Lincoln regales Clyde with stories about Myrtle's past but doesn't understand her sacrifice. It's not until his family is at risk that Lincoln understands Myrtle's concern. This installment in the Loud franchise is about family, love, and sacrifice. This family-centered comedy has action and is a joy for the whole family. The Louds will charm their core audience while adding to the family.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Turtles All The Way Down",
 		"review": "Aza's therapist considers several forms of therapy after Aza admits to opening her callus again. Aza confesses to taking her medication sporadically but won't consider Exposure Response Therapy for her Contamination OCD. Aza eats lunch with her best friend, Daisy, at school but runs out of the cafeteria because she thinks her stomach is too loud. Aza furiously washes her hands, removes her band-aid, opens her callus, and puts soap into the wound. Her thoughts ease. Daisy plans to meet Aza after school at the most posh restaurant in town: Applebee's.<br/><br/>As they eat their burgers, Aza and Daisy overhear a news report about Russell Davis Pickett. Russell is a fugitive billionaire with several white-collar crimes to his name. He left his two sons, Davis and Noah, behind. The police issued a $100,000 reward for information leading to his capture. Daisy remembers that Aza met Davis at a camp for children coping with loss. Aza remembers Davis got a web camera for his 13th birthday. They put it in a tree on his property. Daisy wants to sync to it and examine the photos for evidence.<br/><br/>Aza and Daisy find the camera and sync with it. However, a groundskeeper discovers them and brings them to the Pickett's home. Davis remembers Aza, and Daisy observes he's smitten. Daisy wants Aza to keep up the conversation with Davis. They can return to the belly of the beast, investigate Russell's disappearance, and receive the reward money. However, Aza worries about becoming close to Davis and potentially having his microbiome inside her. Can Daisy and Aza find Russell without Aza spiraling?<br/><br/>Based on the novel Turtles All The Way Down by John Green, this adaptation draws you into Aza's spiral while keeping her human. The script doesn't depict Aza as all-knowing or perfect. She has flaws, and her OCD impacts everyone around her. Later, Daisy's fan fiction feels harsh. But when Daisy questions Aza about her personal life, Aza flounders. It's a complex and necessary conversation for these friends and exposes the faults in their friendship. Also, viewers get a mystery. This film ends like most of John Green's books, with an honest, compelling, hopeful, and wholehearted conclusion. It will leave a mark and conversation for the audience.<br/><br/>4.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Shirley",
 		"review": "Shirley Chisholm joined the new class of the 91st Congress of the United States in 1968. She stands out because she is the 1st African American woman to achieve this goal. Representing New York's 12th district, Shirley wanted to encourage the marginalized to vote.<br/><br/>After a few years in Congress, Mac Holder and Arthur Hardwick Jr. visit Shirley and her husband, Conrad, with startling information. Shirley promised to put her name on the primary ballot if her Floridian staffers raised $5,000. Mac informs Shirley that they raised nearly $10,000. Arthur will handle finances, Conrad will cover security, and Mac will advise her. Shirley knows they are a formidable force. However, this is the first year people 18 to 20 can vote. So, Shirley hires her former intern, Robert Gottlieb, as her national student coordinator. Later, Shirley employs a young and unregistered Barbara Lee. Shirley's bold political moves will change California's voting landscape, media coverage, and the Democratic National Committee.<br/><br/>Shirley wanted the world to know she was more than an African American and more than a woman. She was proud of both things but never allowed it to become her only identity. She wanted to be a voice of people ignored by politicians. She refused to hear the word 'can't' and quickly learned political inner workings and backdoor dealings. She understood that making history was significant but not as much as change. By standing up for herself, Shirley proudly leveled the playing field for all. This film covers a few years of her life but beautifully embraces the impact she made.<br/><br/>4.5 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/shirley.jpeg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/shirley.jpeg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Road House",
-		"review": "Frankie walks to an underground fight club, looking for Carter. After watching Carter win, Frankie sees him quit after seeing his opponent, Dalton. Now, she eyes Dalton. She runs a roadhouse in Glass Keys, but lately, a local gang has harassed the establishment and patrons. She wants Dalton to keep the peace. Dalton says no, but Frankie writes down her phone number if he changes his mind later.<br/><br/>Dalton drives his car on the train tracks and waits. Dalton changes his mind before the train strikes, but the vehicle stalls. Dalton starts it in the nick of time and narrowly escapes death. He grabs Frankie's number on the dash and takes the job.<br/><br/>Dalton arrives in Glass Keys and visits the Road House. Soon, the local gang starts hitting patrons and breaking items inside. Dalton makes quick work of the goons and sends them to the hospital. He assumes he will never see them again. However, they work for Brandt, a shady land developer. The Road House is the only property standing in the way of his luxury housing development. Brandt puts a target on Dalton's back. Now, Dalton, the Road House, and the residents of Glass Keys are in danger. Can Dalton do the one thing he doesn't want to do again: kill?<br/><br/>This re-imaging of 1989's Road House keeps the humor and essence of Dalton's character. He doesn't want to fight unless necessary because 'No one wins in a fight.' While the film's heart is intact, the action scenes aren't smooth. The jump from live-action to computerized is evident and distracting. Dalton appears more like a superhero than a vigilante. This film is captivating for the action fanatic.<br/><br/>4 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/roadhouse.jpeg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/roadhouse.jpeg"
 		}
 	}
 ];
@@ -55,16 +55,6 @@ module.exports = videoOnDemand;
 */
 
 /*
-
-{
-"option":"1",
-"mtitle":"Tyler Perry's Divorce in the Black",
-"review":"<br/><br/>* out of 5 stars.",
-"image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/divorceintheblack.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/divorceintheblack.jpg"
-}
-}
 	
 {
 "option":"1",
