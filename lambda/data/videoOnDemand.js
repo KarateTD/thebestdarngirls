df2dafcd800c65ec584e4a7d65502b3ce79408df
Tiffany Gray <KarateTD@yahoo.com>
2024-08-16 14:03:47 -0400

    add the union

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "The Union",
+		"review": "Tom directs his team to extract a CIA turncoat, Derek. Roxy watches from above while Nick works on the ground with other operatives. Nick and Derek make it to the bridge and wait for further instructions. A van pulls up, shoots them, and steals their briefcase. Roxy is the only survivor.<br/><br/>Mike hangs at a bar with his friends when a familiar face, Roxy, walks through the door. Roxy asks if Mike wants to take the party to a better location, and he says yes. Roxy hugs Mike and admits she has an ulterior motive for bringing him to a remote location on a hill. She stabs Mike in the neck with a tranquilizer, knocks him out, and pushes him down.<br/><br/>Shocked, Mike wakes up in The Savoy Hotel in London. Mike exits the lobby and hears Tom call him over. Tom and Roxy explain they are part of a covert task force called The Union. They are a group of blue-collar workers who hide in the shadows and do the dirty work. They value street smarts over book smarts.<br/><br/>After breakfast, Tom updates Mike on their situation. Derek sold a list of every law enforcement officer, intelligence operative, Union member, and their families worldwide to a woman called the auctioneer. This woman, Juliet, will sell the list on the black market in 2 weeks. They want to buy access to the auction. They need a nobody to buy the device for access. Roxy thinks Mike is perfect for the job. Mike doesn't like being called a nobody but agrees to help. They will turn six months of training into two weeks. Then, Mike will meet Juliet to exchange a device for $5 million. They will use it to access the auction and buy the list to delete it. This job should be easy. Right?<br/><br/>Espionage movie fans know to look for the double-cross or the story within the story. Some viewers will get the person right, but others won't. Also, you feel that Roxy and Mike have an unresolved history. They act flirty and fun when they first meet, but things come to the surface when their lives are on the line. This film has fistfights, shootouts, and car chases. But it shocks when Roxy reveals her biggest secret to Mike and the audience. The film is entertaining and hints at a sequel. But it won't garner your undivided attention.<br/><br/>3.5 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/theunion.jpg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/theunion.jpg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Jackpot!",
 		"review": "Katie returns to LA to pursue acting after caring for her ailing mother. She checks into a shady AirBnB, hosted by DJ and Shadi. Katie will meet with a talent agent in the morning. She wakes up to a strange liquid dripping on her face. Shadi explains it's a sewage problem. Katie sees the same sewage dripping on her clothes. Shadi offers to rent her clothes to Katie for $40. It's her only option, so Katie agrees.<br/><br/>Katie arrived at the audition and reached for her monologue. She grabs Shadi's lottery ticket and presses her thumb against the registration button. The ticket snaps a photo and enters Katie in the lottery. After the manager makes creepy suggestions, Katie leaves. The ticket pings in her pocket and declares Katie the winner. Then, every actress waiting in line gets a ping from their tickets and attacks Katie. Noel bursts through the ceiling while Katie battles a karate and yoga class. Noel congratulates Katie for winning and offers her five free saves before he charges 10%.<br/><br/>Years ago, California created a grand lottery. If the winner is alive at sundown, then they keep the money. However, someone can kill the winner and keep the prize. The lottery is at an all-time high of $3.6 billion.<br/><br/>Noel hands Katie a contract to sign, and Katie does. She asks him if she can quit the lottery. Noel says the only way out is to leave the city limits. But she will forfeit the prize. Noel agrees to help her flee, even though he will get nothing. Katie isn't sure she can trust him, but he's her only choice. Can Katie survive winning the lottery?<br/><br/>What if the Purge and Hunger Games were a comedy? This film is your answer. The fight scenes are never grave, along with most of the dialogue. This storyline escapes the horror genre by having two confirmed kills. Then you have close calls and injuries. This picture is perfect for streaming and an acceptable date movie without romance.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "French Girl",
 		"review": "Sophie knocks on her boyfriend's classroom door to get his attention. Gordon exits the classroom. Sophie got a job interview for a Michelin-star chef's upcoming restaurant. Sophie knows the chef, Ruby, because Sophie worked as her sous chef for a few years before coming to New York. The interview is in Quebec, and they must cancel their trip upstate. Gordon considers the choice a no-brainer and tells Sophie to accept the interview.<br/><br/>Gordon runs to his father's house after school and asks his dad, Peter, for his family's heirloom wedding ring. He is ready to propose to Sophie and start their life as man and wife. Simultaneously, Sophie calls her family and informs them she is coming to town. They beam through the phone until they learn the job is with Ruby. They ask Sophie if she told Gordon the truth about their relationship, but she hasn't.<br/><br/>After a memorable flight, Gordon and Sophie land in Quebec, and Gordon shares awkward moments with her family. They sit for a family dinner and get to know each other better. Then, Ruby comes with a bottle of wine. Sophie informs her family she will tell Gordon about her past when the time is right.<br/><br/>Sophie leaves for her first day for her interview. Gordon takes this time to explore the family's home. He finds Sophie's old bedroom and looks around. Gordon discovers a scrapbook with S and R on the binder. He looks through it and finds an intimate Polaroid of Ruby and Sophie. Gordon runs to Sophie's brother, Junior, for advice. He says Ruby cheated on Sophie and broke Sophie's heart. Gordon thinks Ruby wants Sophie back, and the job is the bait. Gordon can see Ruby's deceitful ways, but can he convince Sophie?<br/><br/>This movie has standout points that will remind you of Bridesmaids, Meet the Flockers, and Weekend at Bernie's, but it's not a fresh idea. It only changes the interloper's gender. Sophie is an unassuming girlfriend with good intentions but secrets. Gordon is a kind but insecure boyfriend who bends backward to impress her family. Ruby is the arrogant past, putting on a false face. The studio made the correct choice by sending this picture to streaming.<br/><br/>2.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "The PA and the Manhattan Prince",
 		"review": "Personal Assistant Lucy arranges the garland in Ryanna's archway before Ryanna's autograph signing. Ryanna rips into the room and says she wants the orange flowers changed to red. After a successful signing, Ryanna hands Lucy a signed copy of her book and talks about her needs for tomorrow. Lucy cuts Ryanna off and reminds Ryanna that today is Lucy's last day.<br/><br/>Lucy finds a note on her door from her neighbor, Gloria, inviting her to dinner. Gloria tells Lucy to have fun, work less, and find love. Lucy wants to focus on her new client, Prince Rupert. His business could launch Lucy's dream. Lucy wants to start a personal assistant company.<br/><br/>Lucy arrives at Rupert's home, but no one answers the front door. Lucy walks around the house and knocks over a dessert tray. She kneels to clean and hears a man offering assistance behind her. Then, it dawns on Lucy that she is speaking to Prince Rupert. Prince Rupert's courtier, James, points Lucy towards the bathroom so she can clean up.<br/><br/>Lucy sits for an interview with James, and Rupert enters the room to listen. James wants Lucy to maintain Rupert's schedule, throw the first annual masked ball, and help Rupert find a princess. Rupert hires Lucy, but James warns her she is on probation until the ball. Later, James tells Lucy not to fall for Rupert's charms or schemes. Lucy heeds his advice and prepares for Rupert. But is Rupert ready for her?<br/><br/>This movie has the model plot for a woman who meets a prince and falls in love. Its standouts are the princesses and the fairy godmother, Gloria. They bring the necessary comedy to this rom-com. Viewers will smile when Lucy stands up for herself after Rupert's ridiculous request. This picture is for fans of the Hallmark plot with a regal twist. And pay attention to the subplot because it's beautiful.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "One Fast Move",
 		"review": "Wes has a high-speed chase with the police and spends time in prison. He loses his benefits after receiving a 'bad conduct' discharge from the military. A social worker offers Wes a construction certificate to work, but Wes doesn't want it. He only wants his bike.<br/><br/>Wes rides to a race track and finds Dean. He looks Dean in the eye and tells Dean his mother's name. Dean invites Wes for a beer at a local bar. Wes leaves, and Dean follows. Wes tells Dean he wants to race, but Dean doesn't think Wes is ready. He offers to tune up Wes's bike instead.<br/><br/>After thinking about it all night, Wes shows up at Legend's motorcycle shop. Dean introduces Wes to the owner, Coach Abel. Wes asks Dean to ride with him. They ride, but Wes makes a hot shot move that almost causes Dean to crash. Dean barrels toward Wes and demands an explanation because those moves ended Dean's career. Wes wants Dean to train him to race professionally. Dean agrees to train Wes to become a pro. But is Dean doing it for Wes or himself?<br/><br/>This film is an emotional rollercoaster, but it isn't captivating. The plot touches on the absent father-longing son dynamics with an elder guiding the way. Dean and Wes call the shots on the track, but Abel is the hero of this story. The subplot has a romance with Camila. However, the writers use it to stoke issues between Dean and Wes. This movie found the perfect platform but won't bring new Prime subscribers.<br/><br/>2.5 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/onefastmove.jpg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/onefastmove.jpg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "My Spy: The Eternal City",
-		"review": "JJ cares for 14-year-old Sophie while her mother, Kate, works in Rwanda. JJ is a whiz in the kitchen since giving up field work to care for Sophie. She goes to school and trains with JJ in the afternoon. However, Sophie doesn't appreciate JJ's hands-on approach. She reminds JJ that he may play dad but isn't her father.<br/><br/>JJ and Bobbi keep watch of Christina while Christina retrieves the location of handheld nuclear weapons. Christina gets the information, puts them on a thumb drive, and escapes before operative soldiers find her. David praises JJ for completing step one but wants JJ in the field for the next step. JJ won't break his pinky promise to nine-year-old Sophie and rejects the job.<br/><br/>Sophie sings in the school choir while mooning over the lead singer, Ryan. She dreams of asking him to the homecoming dance. Sophie asks her friend, Collin, for assistance and a confidence boost. Their choir director alerts the choir they are on the shortlist to sing at the G7 summit in Vatican City.<br/><br/>JJ trains Sophie, but she misses her backflip several times. She decides to leave, but JJ worries she's quitting. Then, Sophie gets a text from her choir director. They are going to Italy.<br/><br/>Before boarding the buses, Sophie gives JJ the rules for the trip. Then, Sophie and JJ watch David drop off his son, Collin. Collin doesn't know his dad heads the CIA. JJ vows to keep an eye on Collin for David. The new Vice Principal, Nancy, confronts JJ about his duties during the trip. He is a first-time chaperone, and she worries he is ill-prepared.<br/><br/>JJ gets a warning from the tripwire he installed in front of Ryan's door. He catches Ryan, Collin, and Sophie sneaking out of the hotel. JJ allows them to leave. But they must return before Vice Principal Nancy catches them. Sophie and Ryan are having a wonderful time together and take a selfie. During the picture, Sophie notices someone pull Collin into a van, and she chases after the kidnapper. A motorcyclist almost hits Sophie during her pursuit, but JJ saves her. JJ looks at Sophie's images and identifies the kidnapper as Bishop Crane. Bishop got the thumb drive from Christina. Now, Bishop's boss wants David to get the launch codes from a secure facility. If he doesn't, they will kill Collin. They need to save the world and Collin.<br/><br/>The sequel to My Spy navigates complicated relationships with a fun mystery, comical action sequences, and attack finches. JJ doesn't have a dreamy adolescent. She is a young woman trying to spread her wings. Bobbi and Sophie maintain their semi-hostile but respectable relationship during the mission. And the boss's reveal is surprising. This film builds on the original and brings back the characters you love. While watching the first film is unnecessary, the plot has two humorous callbacks to the original. This movie isn't for younger children, but you and your teens will laugh.<br/><br/>3.5 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/myspy2eternalcity.jpeg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/myspy2eternalcity.jpeg"
 		}
 	}
 ];
@@ -56,14 +56,6 @@ module.exports = videoOnDemand;
 
 /*
 
-{
-"option":"1",
-"mtitle":"The Union",
-"review":"<br/><br/>* out of 5 stars.",
-"image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/theunion.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/theunion.jpg"
-}
-}
+
 
 */
