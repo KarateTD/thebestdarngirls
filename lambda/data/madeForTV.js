0fdd53928983bc0e33ac0caf1cfe0484d878c52b
Tiffany Gray <KarateTD@yahoo.com>
2024-08-31 23:59:08 -0400

    3 movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Twisted Neighbor",
+    "review": "Colleen's mom, Vanessa, doesn't understand why Colleen left New York for a cul-de-sac called Sunny Vista. Colleen walks home from the market as the neighbors take notice of the newcomer. Her bag rips open, and the contents fall on the street. Quinn picks up an item while saying hello but leaves Colleen to finish the retrieval. Dan runs over with a box to contain the food as Jared watches. Jared's wife, Kimberly, chats about Colleen with her friend Ashton on the neighborhood app. They attempt to learn about Colleen's marital status and job. Jillian, the H.O.A. president, forcefully reminds Colleen that the H.O.A. is having a meeting tonight and suggests Colleen change clothes.<br/><br/>The H.O.A. meets at Theo's home, and everyone gives Colleen the third degree. After the meeting, Colleen gets notifications from the neighborhood app. As Colleen reads other messages, she sees everyone discussing Edith's unkempt property. Colleen bakes a pie and walks to Edith's to offer cleaning help. She finds Edith dead with a fireplace poker in her abdomen.<br/><br/>Colleen collects herself at home when she sees movement at her back door. She calls the police and grabs a rolling pin. To Colleen's surprise, it's Theo with a jar of sauce. He disclosed that he has mushrooms, and they get high. Colleen wants to tell Theo about her past, but he receives a message. An anonymous texter accuses him of selling smut. Theo says he knows the caller and leaves to confront them but promises to return. Colleen comes down from her high in the morning, but Theo never returns. Days later, Colleen finds Theo's mangled body. Neighbors worry that Colleen is the killer because she was the last person with Theo. Can Colleen clear her name and find the culprit before another person dies?<br/><br/>Where to start? You should watch this film if you want a funny and unhinged movie. The mystery is compelling and gives viewers an unforeseeable culprit. After Colleen takes drugs, you won't be sure if she's still hallucinating these events. And you have to assume Edith's killer is the person wearing the mask. They never explicitly say who killed her. This movie has drugs, affairs, murders, and BDSM. Viewers should watch this movie with a glass of wine and a good sense of humor.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/twistedneighbor.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/twistedneighbor.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "Deadly DILF",
+    "review": "Elysium lives with her Aunt Kendra after Elysium's attacker killed her father. She is attending college and on the track team. A football flies into her backyard, and Elysium walks over to the neighbors to return it.<br/><br/>Rio and Tori are married. He is studying business in college, and she runs a gym. Tori is the stepmother to Rio's son, Gunnar, and they recently moved into the neighborhood. Gunnar answers the door and smiles at Elysium. They introduce themselves, but there is an attraction between Rio and Elysium. Later, Elysium babysits Gunnar and helps Tori with the gym's social media accounts.<br/><br/>Rio and Elysium spend time privately, and she unknowingly watches him from afar. Rio drinks heavily and sleeps with Elysium. She awakens and dreams of a life with him. However, he wants to forget the night and Elysium. Elysium won't go without a fight. This DILF will need to become deadly before Elysium takes everything from him.<br/><br/>Elysium's attack at the movie's start doesn't get an explanation or a meaning. Fans assume they will revisit the beginning sequence, but we never do. Viewers will feel bad for Rio until you discover this is his second affair. The final sequence has many poor choices and doesn't explain Rio's outcome. Also, Elysium's conclusion doesn't feel fair. The ending starts with Elysium's letter to Rio, but you don't hear the letter. This film has a wild premise and action but leaves viewers with unanswered questions.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deadlydilf.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deadlydilf.jpg"
+    }
+  },
+  {
+    "option": "3",
+    "mtitle": "The Magic of Lemon Drops",
+    "review": "Lolly serves her father, Marty, avocado toast on German bread, but he won't try it. He hasn't changed the menu since his wife, Irene, died, and he won't now. Lolly goes through the diner's finances and fears they could lose it, but Marty keeps the faith while eating Lolly's organic popsicles.<br/><br/>Lolly goes to the market for produce and talks to her friend, Eve. Eve asks if Lolly remembers her old boyfriend, Rory, who is again coming to town for the season. Lolly plans to meet with Rory and apologize for the past.<br/><br/>Rory and Lolly go to their favorite spot. She apologizes to Rory for asking him to choose between her and his dream college program. To Lolly's surprise, he forgave her a long time ago. Although newly divorced, Rory works in sports medicine and has a wonderful daughter.<br/><br/>Lolly meets with her sister, Daphne, at Aunt Gert's home. They find Lolly's scrapbook with all her dreams and wishes. Lolly wanted her family, an upscale diner called Toast, and Rory. Daphne tells Lolly that she got into a yoga program in Bali, and Lolly is upset. Lolly doesn't think it's safe or the correct move for the family. Daphne leaves and yells that she doesn't want to be regretful like Lolly.<br/><br/>Lolly expresses her feelings to Gert, and Gert gives Lolly a gift. It's four magical lemon drops. She can make a wish on three drops and see how her life would have turned out if she made that decision. The 4th one is to choose the life she wants, but she will have no memory of her life now. What life will Lolly choose?<br/><br/>Based on The Magic of Lemon Drop Pie by Rachel Linden, this movie asks hard questions and takes Lolly on an emotional journey. She uncovers the good and bad of each decision. Lolly learns to appreciate what she has and discovers the beauty in loss. This film forces you to dream with introspection, not perfection. This beautiful film will make you want to read the book and have a lemon drop. Don't miss this Hallmark classic.<br/><br/>4.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/themagicoflemondrops.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/themagicoflemondrops.jpg"
+    }
+  },
+  {
+    "option": "4",
     "mtitle": "Terry McMillan Presents: Forever",
     "review": "Johnnie returns home after his wife, Tina, served him with divorce papers on base. Johnnie demands his car keys and drives away. Police officers Carlie and Levi pull Johnnie over. Levi recognizes Johnnie as a hometown baseball hero. He asks Carlie to let Johnnie off with a warning because his registration expired. She agrees, but he must drive straight home.<br/><br/>Johnnie orders and sees a young man, Tim, harassing a young woman, Leslie. Johnnie tells Tim to leave Leslie alone, or Johnnie will disqualify Tim from making the baseball team. Tim lunges for Lesley again when Leslie's mother, Carlie, enters. Carlie forces Tim to leave and grounds Leslie for poor grades. Then, she turns to Johnnie and questions him about not going home like he promised. Johnnie asks Carlie on a date. She says no because she has three daughters, Lesley, Courtney, and Tessa. While he's never dated a woman with kids, Johnnie isn't scared to try. He asks Carlie for a chance. This choice will be Carlie's forever.<br/><br/>This movie feels typical on the surface. However, it shocks like Terry's books. Carlie and Johnnie fall for each other but deal with integrating him into the family. As life eases, they get a gut punch that will leave your jaw on the floor. She wants Johnnie to feel welcome but needs her daughters to feel heard. Johnnie learns that dating a woman with kids comes with different priorities. Viewers should not give up on this film too soon because it will take you through the emotional gauntlet.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "5",
     "mtitle": "Nelly Knows Mysteries: A Fatal Engagement",
     "review": "Nelly reports to work and discusses her column with Fiona. Nelly cycles through her mail and finds a letter without a return address. The letter is from a woman who suspects her boyfriend of cheating. Nelly, from the small town of Babbleton, knows everyone and recognizes the hearts above the I's. The letter is from Dahlia, who is dating Blake. Nelly wants to handle this face-to-face.<br/><br/>Nelly arrives at Blake's workshop and finds the door open. She peers inside and finds Blake dead with $20,000 in his pocket. Nelly calls the police. Sheriff Burgi introduces Nelly to the new detective, Michael. Michael interviews Nelly about finding the body. Nelly reveals everything and hands Michael the cash. Burgi gives this case to Michael to prove himself, but she tells Michael to work with Nelly because Nelly knows the town. Dahlia says she spoke to Blake the night before, but he rushed her off the phone. She doesn't think he was cheating but keeping a secret. Nelly tells Dahlia to talk to Michael but convinces Michael to let her sit with Dahlia during the interview. Dahlia completes the interrogation, and Michael allows her to exit, but she remains a suspect. Nelly promises to clear Dahlia's name. Can Nelly and Michael find Blake's killer?<br/><br/>This plot has several moving parts and people with bad intentions. The night before Blake's murder, Blake had a poker game. Each player had a secret. Blake has a neighbor, Rumple, who dislikes him and his prior devious acts. Nelly is a new type of protagonist for Hallmark Mystery fans. She is unapologetically happy and keeps an earth-shattering grin on her face. However, you learn that her smile holds a lot of pain. The wrap-up pushes lots of information in 10 minutes and will require a second watch to keep up. Nevertheless, this film can become a series with a developing romance.<br/><br/>4 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/nellyknowsmysteriesafatalengagment.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/nellyknowsmysteriesafatalengagment.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "Bonds Will Be Broken",
-    "review": "Janine talks to Kelly about the 'Becoming You' seminar. Kelly promises it will lead Janine to enlightenment. Janine pays $300 and attends the meeting. Janine spends more time with Kelly before going to meet with Victor. Victor is an investigative journalist writing a report on a dangerous cult. Anna infiltrated the seminar with the name Janine. Anna has worked on the cult for three months with little progress. She wants to speed things up to find her sister, Nina. Nina went missing after going to a seminar. Anna plans to rescue and deprogram Nina, but Anna has to find Nina first. Anna will gain Kelly's trust and get closer to Ray. Then, Ray will bring Anna to their cabin. She will meet the leader, David, and hopefully, find Nina. Victor warns Anna to be careful and don't get in too deep. He believes this cult has killed and will kill again.<br/><br/>This film displays the BITE model and cult practices. He love-bombs them with acceptance after destroying their confidence. These moments don't compel you to watch. It's the second Nina sees Anna. Her response will floor you. It will make you sit up and watch until the end. This film won't be a favorite, but it's worth your time.<br/><br/>3 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/bondswillbebroken.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/bondswillbebroken.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Trapped in the Rocky Mountains",
-    "review": "Carrie won a contest for a one-night stay at a 4-star cabin in the Rocky Mountains. She rejoices in her prize as she drives to the cabin with her husband, Chef Jared. Carrie doesn't care about the cell service because she wants this weekend to focus on their marriage.<br/><br/>They notice strange things when they approach the cabin. The cabin's front door is ajar, the property is empty, and there is no WiFi. Carrie hears noises outside and sees two people walking towards the property. Jared recognizes them. It's Wes and his sister, Lizzie. Wes works for a travel magazine. The editor sent him to the cabin to write about it. He asked Lizzie to come.<br/><br/>They tour the cabin when they hear more voices in the sitting area. It's Harrison and Rayanne. Rayanne plans to meet a client tomorrow, and Harrison was there to meet a woman he met online. Jared tells Carrie they were friends in college but haven't seen each other in ten years. They consider leaving, but all decide to stay the night. After nightfall, Harrison decides to leave. Carrie reminds him that the next town is 2 hours away, but he takes the chance. Later, Harrison returns because someone locked the iron gate. They can't exit the property.<br/><br/>Carrie can see the tension rising within the group and asks Jared to explain privately. Jared tells Carrie that they came to a cabin like this ten years ago with their friend Ashley. Ashley decided to play Death Poker. The players are all in or fold. The last person would have to stay in the woods all night alone. Ashley lost. She went into the woods and went missing in the morning. Later, the police declared Ashley dead. Carrie talks to the group and uncovers critical information Jared didn't reveal. They didn't tell the police about the game and said she wandered off after hiking. Now, they think someone blames them and wants them to confess. They must find a way out or suffer, but who can they trust?<br/><br/>This movie starts with people making poor decisions. It's ill-advised for them but the perfect setup for the audience. The characters are people with a loose moral compass. During an intense conversation, the kid gloves come off, and they don't hold their punches. It is a popcorn and tea moment for the viewers. The story revisits old scenes that you missed during the recap. This picture is beyond wild and will enthrall you, but you have to get past their first set of poor choices.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/trappedintherockymountains.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/trappedintherockymountains.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "A Costa Rican Wedding",
-    "review": "Bonnie catches Emily daydreaming about Emily's ex-boyfriend, Shane. She cautions Emily against checking Shane's social media. Bonnie hopes Emily will use the upcoming trip to Costa Rica for Phoebe's wedding to detach from Shane. Emily shows Bonnie Phoebe's family heirloom wedding bands. Bonnie gives Emily her gift for Costa Rica: a first aid kit. Bonnie knows Emily will hurt herself.<br/><br/>Emily arrives and waits for Jamal's cousin to pick her up from the airport. Emily fumbles when Ryan calls her name. Jamal's uncle married Ryan's aunt, so now they are cousins and best friends. Emily grits her teeth and gets in Ryan's truck.<br/><br/>Emily meets Phoebe on the beach, and Phoebe asks Emily to clarify Emily's issues with Ryan. Emily won't divulge and chooses to focus on Phoebe and Jamal's wedding day. As Phoebe and Jamal's families arrive, they breathe a sigh of relief when they see Emily has the rings. Everyone knows that where Emily goes, disaster follows.<br/><br/>The women skip the bachelorette party and join the bridal party for a swim. Emily sees a monkey running off with her bag and first aid kit. She remembers the bag had a stealth pocket, and she put the rings inside but forgot to take them out. The bridal party laughs as the monkey runs away with it, and Emily quietly panics. She tells them she needs it because her phone's inside. Phoebe uses her phone to locate Emily's. Emily promises to return after finding her bag. Ryan knows the countryside and offers to go with Emily. Emily and Ryan expect to find the monkey and get Emily's bag back. If not, Emily will lose the heirloom rings forever and ruin Phoebe and Jamal's wedding.<br/><br/>This movie increases the comedy in a rom-com as Emily and Ryan bumble through the jungle. Their adventure includes run-ins with venomous snakes, mud pits, and thieving monkeys. However, it forces them to converse and work through their issues. You may view Emily as a stubborn whiner who causes her calamities. However, Ryan's speech saves the day. The scenery is as beautiful as the friendships. This film is for good-humored people with patience.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/acostaricanwedding.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/acostaricanwedding.jpeg"
     }
   }
 ];
@@ -58,29 +58,38 @@ module.exports = madeForTV;
 
 {
 "option":"1",
-"mtitle":"Twisted Neighbor",
+"mtitle":"Held Hostage in My House",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/twistedneighbor.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/twistedneighbor.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/heldhostageinmyhouse.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/heldhostageinmyhouse.jpg"
 }
 },
 {
 "option":"2",
-"mtitle":"Deadly DILF",
+"mtitle":"The Past Comes Knocking",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/deadlydilf.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/deadlydilf.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thepastcomeknocking.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thepastcomeknocking.jpg"
 }
 },
 {
 "option":"3",
-"mtitle":"The Magic of Lemon Drops",
+"mtitle":"The Daughter I Gave Away",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/themagicoflemondrops.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/themagicoflemondrops.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thedaughterigaveaway.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thedaughterigaveaway.jpg"
+}
+},
+{
+"option":"4",
+"mtitle":"Head Over Heels",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/headoverheels.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/headoverheels.jpg"
 }
 }
 
