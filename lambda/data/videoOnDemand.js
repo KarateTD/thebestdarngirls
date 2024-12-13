1b04f4ed04e5782833a2fa484ebed603655c6c30
Tiffany Gray <KarateTD@yahoo.com>
2024-12-13 18:30:26 -0500

    add 1 vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Too Many Christmases",
+		"review": "Jerome cuts hair with his brother, Bryson. Their cousin, Lavar, brags about the many women who bought him gifts for Christmas. Jerome and Bryson laugh at Lavar because they are in committed relationships. Bryson will finally bring his new girlfriend, Cree, to meet his family. For the first time, Jerome will bring his wife, Kayla. Newlyweds Jerome and Kayla typically spend the holidays with their respective families. Jerome can't wait for Kayla to spend time with his family and their traditions.<br/><br/>Kayla decorates her tree when her brother, LeeLee, calls. His tour got canceled, and he's home for the holidays. With LeeLee in town, Kayla knows their mother, Regina, will want them to spend the holidays together. LeeLee decides to ask Jerome to spend Christmas with her family instead of his.<br/><br/>Kayla prepares Jerome dessert for dinner, and he knows something isn't right. She tells him that her family expects her to be with them from the holidays. She asks Jerome to consider canceling his plans and spending time with hers. Jerome jokes that the only solution is for them to be in two places simultaneously. Kayla takes him seriously and creates a game plan. They will celebrate with both families without them knowing. They will make elaborate ruses to leave and return on a schedule. What could go wrong?<br/><br/>This comedy will hit home for new couples. Jerome and Kayla spend the holidays apart during their relationship. So, their families never expected things to change. It's a plausible plan, but they forgot one glaring factor: their families. Lavar has a history with Cree, Pastor Davis doesn't accept LeeLee, and Kayla bought the wrong cookies. The plot balances humor with compassion exceptionally. It will make you laugh, clutch your pearls, and hug a loved one. This film could become a series with the proper script.<br/><br/>4.5 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/toomanychristmases.jpeg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/toomanychristmases.jpeg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Brewster's Millions: Christmas",
 		"review": "Morgan Brewster leaves her successful business meeting to meet her stylist at home. Morgan's aunt, Mrs. Brewster, surprises Morgan when Morgan walks through the door. Mrs. Brewster discusses Morgan's spending and overindulgence. She questions Morgan's holiday plans, and Morgan cuts off her aunt. She will not celebrate with the Brewsters because she planned a trip around the world. Mrs. Brewster believes Morgan exiled herself because this is the first Christmas with her husband, Monty, Morgan's uncle. They took care of Morgan after her parents died and doted on her.<br/><br/>Morgan goes to lunch with friends, and the restaurant declines several of her credit cards. Morgan runs to Mrs. Brewster for an explanation. Mrs. Brewster cut off Morgan's credit cards and stipend. She is tired of Morgan's antics and is ready to retire. Mrs. Brewster wants Morgan to take over the company but doesn't believe Morgan's prepared. So, Uncle Monty created four tasks for Morgan to complete before Christmas. Morgan will get her inheritance and the company. Otherwise, Mrs. Brewster will cut her off. Mrs. Brewster has her COO, Tiffany, and Tiffany's right hand, Andrew, enter. While Tiffany assists Mrs. Brewster with daily operations, Andrew will keep a watchful eye on Morgan to ensure Morgan follows the rules. They are to create safe and clean long-term housing, give a child a unique gift that doesn't require money, restore her faith in the meaning of Christmas, and open her heart to love. Morgan can't use her name, money, connection, donations, or gifts to cheat. However, Tiffany will make it difficult. She doesn't believe Morgan is fit to lead and tells Andrew to place obstacles in Morgan's way. Will Morgan succeed, or will Tiffany lead?<br/><br/>Inspired by Brewster's Millions, this movie puts a holiday twist to Brewster's mission. However, she doesn't have to spend money but help others. Morgan learns the beauty of giving, sharing, and caring. She discovers how to make a home with little and see past what others lack. This film is funny and will bring a tear to your eye.<br/><br/>5 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "The Day Before Christmas",
 		"review": "Nia waits in the pick-up line for her daughter, Imani, while checking her to-do list. Nia has two catering jobs and a bank loan meeting tomorrow, Christmas Eve. Then, she sees another parent, Rashaad, jump the line, double park, and get out of his car. Nia confronts Rashaad for breaking protocol, and he tells her to relax. His son, Bryce, exits the school with Imani. Nia arrives home and finds a foreclosure notice on her door. She will thrive if she gets the bank loan tomorrow.<br/><br/>Nia cooks through the night. She jumps up, dresses, grabs her food, and drives to the school. The organizer informs Nia that they are late. Rashaad and Bryce arrive late too. Nia considers driving Imani to the trip, but Rashaad points at Nia's flat tire. She angrily sighs. Earlier, she took out the spare for the food. Rashaad calls a tow truck, but the car needs more than a tire. He offers to drive Nia to her first catering job.<br/><br/>Rashaad explains that today is vital for him too. He is a photographer, and the mayor requested Rashaad take his family portrait. If Rashaad does well, it will lead to more work with the city. Also, it will prove to Rashaad's ex-wife, Gabrielle, that he's a stable parent. Rashaad drops Nia and Imani off and drives away. Then, Nia remembers the food was in her car at the repair shop. She tries to call Rashaad, but her code doesn't work. Nia has Rashaad's phone. When Rashaad realized he had Nia's phone, Bryce told Rashaad to try Imani's birthday as the code. Rashaad calls Nia, and they exchange codes.<br/><br/>Nia and Imani hide in the bathroom until they create a plan. Nia asks Imani for the cookies in her backpack. Imani looks inside and finds a camera. Imani accidently grabbed Bryce's back with Rashaad's camera. Nia and Rashaad will have to cover for each other. Nia will lose her home, and Rashaad will lose Bryce if they fail. Will they gain more if they succeed?<br/><br/>Nia and Rashaad start as enemies. She has a type-A personality. Rashaad likes to let things flow and believes everything will work out. However, they have to do something new: trust. Once Nia and Rashaad gain mutual respect, they will do anything for each other. And the script doesn't vilify Gabrielle. She is a concerned mother with a heart. This film is a funny series of misfortunes that lead to love.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "A Wesley South African Christmas",
 		"review": "Todd goes through his business plan with the heads of Dlamini Enterprises siblings Themba and Gugu. Themba loves the proposal, but Gugu won't get off her phone. She politely exits the room, and Themba follows her. Themba wants to know why she is being so rude to his friend. She doesn't want to work with Todd because Todd doesn't understand their culture and background. So, Gugu doesn't trust Todd.<br/><br/>Later, Themba calls Todd and tells Todd about Gugu's issues with the deal. However, Themba has a plan. He invites Todd and the Wesleys to spend Christmas with his family in Durban South Africa. Todd is unsure because Christmas belongs to the family's matriarch, Sylvia. Todd knows they must secure this multimillion-dollar deal. Todd calls the family and convinces his father, Bryan, Sylvia, uncle Marcus, aunt Gloria, sister-in-law, Trina, sister, Cameron, and wife, Ryan, to come. Todd hopes his family will help him seal the deal with Gugu.<br/><br/>Watch out world! The Wesleys are on the move. However, they aren't the crazy family from the previous films. They are grounded and unified. It's the Dlaminis who need help. The Wesleys become a prime example of open communication and understanding. The audience will see the beautiful side of Africa, often ignored by mainstream media, in this film. As they guide the Dlaminis, the Wesley family grows and learns.<br/><br/>4.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Style Me for Christmas",
 		"review": "Tiffany alters a gown for her most lucrative client, Mrs. Beauvais, with the help of her assistant, best friend Ray'ah. Mrs. Beauvais calls for an update on her gowns. Tiffany asks for an extension. Mrs. Beauvais cancels her order. Tiffany sulks because Mrs. Beauvais's business is crucial to Tiffany's income. Tiffany is 3 months behind on her rent, $200,000 in debt, living in her boutique, and got a rent increase notice. She prays for a Christmas miracle.<br/><br/>Tedee Maxwell's manager, Sarah, enters his bedroom. Tedee's pre-sales for his Christmas album are abysmal. Sarah thinks his playboy image is working against the clean-cut music. She wants to manufacture a relationship to improve album sales.<br/><br/>Sarah walks into Tiffany's boutique and asks for a suit in Tedee's measurements. Tiffany doesn't know that Sarah wants the suit and her for Tedee. Sarah leaves with a great impression of the stylist.<br/><br/>Tiffany sees Tedee on TV as he talks about wearing her suit. Sarah calls Tiffany. Tedee lost his wardrobe for the Christmas album promotional tour. He needs a stylist to create looks. Ray'ah encourages Tiffany to say yes. After Tiffany and Ray'ah arrive, Sarah tells Tedee that Tiffany is the woman for the manufactured relationship. Tedee rolls his eyes because Tiffany isn't the type the paparazzi sees him with. But he decides to go with it, and his feelings grow. But will it change when Tedee's ex-girlfriend, Nicole, returns?<br/><br/>Ray'ah is the best friend we all need. Tiffany had a run of bad luck, so Sarah offering her a dream job is a prayer answered. However, this will test Tiffany's faith, patience, and confidence. During Tiffany's growth, Tedee learns a lot too. Tedee starts trying to attract Tiffany but builds a friendship instead. It's beautiful to watch. This film is a naughty and wild take on a holiday love story.<br/><br/>5 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/StyleMeForChristmas.jpeg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/StyleMeForChristmas.jpeg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "A Christmas Miracle",
-		"review": "Roz prepares for the Community Center's Christmas Jubilee when she gets a call. The government wants to make room for a new commercial complex. So, they are cutting funding from several programs. Roz packs the Christmas stuff away and heads home.<br/><br/>Dalton closes a big deal before the holidays. Now, he wants to focus on his family. He overhears his co-worker, James, on the phone with his superintendent. The pipe in James's apartment broke, and it's flooded. They won't be able to fix it until after the holidays. Dalton offers to let James stay with his family, but James declines the invitation.<br/><br/>James sits at a cafe calling hotels, but no one has a room. He orders a blueberry muffin and leaves the table to wash his hands. Seconds later, Lisa enters and sits at the empty table. The waitress assumes James left and gives Lisa the muffin for free. James sits down, corrects the waitress's mistake, grabs the muffin, and eats it. Lisa gets her things and angrily leaves. He takes Dalton's offer.<br/><br/>The McKenzie family returns for another week of Christmas fun. Later, the family eats and has a joyous celebration over dinner until James arrives. The family can feel the tension between the muffin man and Lisa. Roz leaves to get James a plate and hears an announcement on the TV. The city council won't stop at removing the Jubilee. They want to eliminate the community center. This family will pull together to create the Christmas Jubilee without the city council and prove the community center's worth. Can the McKenzies pull off a Christmas Miracle?<br/><br/>The third installment in the McKenzies franchise doesn't disappoint. They continue to be open, loving, and compassionate. They spread this message of joy and prosperity to their community. Yes, the pastor is back. Viewers should make the McKenzies a regular part of your holiday season.<br/><br/>5 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/achristmasmiraclebetplus.jpg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/achristmasmiraclebetplus.jpg"
 		}
 	}
 ];
@@ -56,14 +56,6 @@ module.exports = videoOnDemand;
 
 /*
 
-{
-"option":"1",
-"mtitle":"Too Many Christmas",
-"review":"<br/><br/>* out of 5 stars.",
-"image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/toomanychristmases.jpeg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/toomanychristmases.jpeg"
-}
-}
+
 
 */
