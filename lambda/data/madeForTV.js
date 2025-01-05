0b1d8d1ae7cd8846d00e1e8306de3eb86d170dcf
Tiffany Gray <KarateTD@yahoo.com>
2025-01-04 23:34:12 -0500

    2 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
-    "option": "1",
+    "option":"1",
+    "mtitle":"Vanished Out of Sight",
+    "review":"Claire gives her daughter, Briar, a bath before opening presents on Christmas morning. Briar questions Claire about swimming and going out. Claire says she swam when she was younger but now doesn't because she's blind. Later, Briar opens presents as they hear a knock on the door. It's Claire's boyfriend, Beck. He brought presents for Briar, including a bike, but he saved the best gift for Claire, an engagement ring. Claire doesn't accept his proposal. Beck knows Claire hides her past from him but wishes she would talk to him. Beck leaves to spend time with his uncle, while Claire and Briar spend the afternoon together.<br/><br/>Briar wants to ride her bicycle, so Claire calls her neighbor, Kim, to watch Briar while she works in the garden. Later, Briar asks to spend time with her friend, Kim's daughter Joyce, and Claire agrees. Later, Claire prepares dinner and calls Kim to bring Briar home. Kim pauses and says Joyce walked Briar home an hour ago. Within minutes, the neighbors and police form a search party for Briar. Officer Reece Miller questions Claire about Briar while Sheriff Walker Donley interrogates Beck outside. Walker enters, but Claire brushes him off. She dealt with him in the past, and he didn't find justice for her. Claire won't depend on the police or her neighbors for help. She will locate Briar in a world of darkness.<br/><br/>Claire solves two mysteries in her life while venturing into a world that she fears. She won't let anyone stop her from finding Briar. Claire's panic grows in this movie as more of her loved ones disappear. It amplifies the danger Briar is in. As you watch, you will see Claire make questionable choices, but remember that she is a traumatized woman on the edge. This film feels slow but allows your anxiety to rise with Claire's.<br/><br/>4 out of 5 stars.",
+    "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vanishedoutofsight.jpg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vanishedoutofsight.jpg"
+    }
+    },
+    {
+    "option":"2",
+    "mtitle":"Dressed to Kill",
+    "review":"Amy walks to Silver Fashion House with an armload of fabrics. The CEO's daughter, Blair, rushes past Amy and doesn't offer help. Amy stops by seamstress Wanda's desk and hands Wanda a fabric bundle. Wanda forgot to order it the night before. Wanda's divorce affects her work, but she promises to do better.<br/><br/>Blair sits in her mother's office while her mother, Vanessa, scolds her for partying in their latest fashion while intoxicated. Blair wants to create an affordable line, but Vanessa says no and demands that Blair exit. Amy enters, and Vanessa shocks her. She wants Amy to take over the company, not Blair. Then, Vanessa looks through dresses for their upcoming runway show, finds poorly-made stitching, and has an asthma attack. She asks for the person responsible, and Amy admits it's Wanda. Vanessa wants Amy to prove she has CEO skills and demands that she fire Wanda. Regretfully, Amy walks to Wanda's desk and lets her go.<br/><br/>Amy tells her boyfriend, Kevin, about Vanessa's vision for Silver. However, Kevin, a therapist, is underwhelmed. Amy begs Kevin to be more encouraging, and he relents.<br/><br/>Amy arrives at work and sees the coroner removing Vanessa's body from the premises. The police believe Vanessa had an asthma attack and died. Amy knows Vanessa would misplace her inhaler, but Vanessa never lost it. Amy asks the police about the security cameras, but earlier, someone turned them off before Vanessa's death. Amy pretends to accept the police's theory.<br/><br/>Blair sits at Vanessa's will reading when Amy enters. Vanessa left the property and 10% of the company to Blair. However, she left the rest of Silver to Amy. Blair rages. She believes Amy knew about the will change and killed Vanessa to take over. Amy swears she didn't do it and laughs at Blair's allegations until the police believe Blair. Can Amy prove her innocence while running the company?<br/><br/>This movie is a masterclass in misdirection. The plot establishes viable suspects with suspicious behaviors, motives, and leading clues. The story makes Amy look disoriented for a moment. Some viewers will wonder if she's the killer. With so much buildup, the culprit is less revealing. This film is like a roller coaster, a wild ride that ends predictably.<br/><br/>3.5 out of 5 stars.",
+    "image":{
+    "smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dressedtokill.jpg",
+    "largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dressedtokill.jpg"
+    }
+    },
+  {
+    "option": "3",
     "mtitle": "The Last Thing She Said",
     "review": "Kate finished her college degree and now worries about paying her student loan. Her friend, Hayley, tells Kate to relax because there is an opening at her job.<br/><br/>Eric interviews Kate, but her lack of work experience bothers him. She convinces Eric that her tenacious work ethic will replace her minuscule work history. A few days later, Eric offers her the job.<br/><br/>Kate works late, and the light turns off. She walks around the office to find a switch and runs into the cleaning lady, Maria, and Maria's son, Stephan. They have a cleaning business and care for the offices at night. Maria, Kate, and Stephan finish work simultaneously. So, Maria invites Kate to a late night out, and Kate agrees.<br/><br/>Kate loves the small bar and has a great conversation with Maria. Maria sees an off-duty officer, Josh, and waves. Josh moonlights at some of the same buildings she cleans. A few days later, Stephan asks Kate out on a date, and she politely rejects him. Eric overhears their conversation and fires Stephan and Maria.<br/><br/>After a long work day, Eric offers to drive Kate home. He asks to use her bathroom when they arrive, and she agrees. Kate opens her door and discovers her ex-boyfriend, Matt, waiting for her. Matt explains that he isn't over Kate, but she doesn't want to date him.<br/><br/>Kate and Eric finish their project, and he wants to celebrate. Eric makes advances toward Kate, and she fights him off. Maria yells at Eric, and he backs off. Kate leaves with Maria. Kate chooses to forget everything that happened because she needs the job.<br/><br/>Hayley enters Eric's office. Kate hasn't been to work and isn't home. Eric reports Kate to Josh as a missing person. Josh vouches for Maria and Stephan because they are kind people. Ann won't hear it. Can they find Kate and her kidnapper?<br/><br/>If you don't read the synopsis on other sites, this movie offers a mystery for 45 minutes. Kate has several unstable men in her life. Her ex-boyfriend, Matt, her boss, Eric, and Stephan. The kidnapper is foreseeable if you read another site's synopsis. However, the motive is disgusting but sadly believable. This movie doesn't require your undivided attention but is mildly entertaining.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "4",
     "mtitle": "Engaged by Christmas",
     "review": "Zoe finishes her final proposal for her boss, Penny, and is ready to start her company. She picks up the newspaper to see if her favorite column, Ask Adora, picked her letter. To her surprise, the advise columnist responded. Zoe wants to get married to her boyfriend, Cary. Adora tells Zoe to be direct, but she has to be willing to accept any answer. Zoe likes the advice and plans to use it at their dinner.<br/><br/>Zoe meets Cary at the restaurant. They speak simultaneously. She wants to get married, but he wants to break up. Zoe spirals. Previously, she planned the 12 blog posts. And the posts would include pictures of her and Cary. Zoe can't believe her plans are slowly going down the drain.<br/><br/>Zoe tries to write the first post, but her breakup is fresh on her mind. She grabs the newspaper to get inspiration from Adora and sees an address. Zoe walks to Adora's work to find Adora's office. Zoe finds a man, Adam, at the desk with a picture of Adora on the wall. Zoe deciphers the truth. Adam is Adora. Adam informs Zoe that he is a licensed marriage counselor who offers excellent advice. Then, Zoe gets an idea. Adam can be her pretend boyfriend in the posts. If he doesn't, she will expose his secret. He agrees to help her, but will they get more?<br/><br/>Zoe and Cary are the perfect example of a couple on the same page but in a different book. Adam hides behind the Adora persona and tries to help everyone. However, he's failing himself. Adam piles work on his plate to avoid more pressing issues. Zoe's drive pushes her to be perfect. However, she must learn that imperfection is wonderful and singlehood has its blessings. While the film has funny moments, you won't feel a need to rewind to see missed scenes. The script becomes dull as Zoe and Adam arrive at their foreseeable conclusion.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "3",
+    "option": "5",
     "mtitle": "North by North Pole: A Dial S Mystery",
     "review": "Zoey and Avery prepare to interview local Santas for the Christmas Festival. If they do well, the mayor will let them do the interior design for the new city building. While Zoey is excited, they have a problem. Someone keeps sending threats to end the festival. Avery walks to the front to bring in the first batch of Santa's but only finds one left. They interview and hire him.<br/><br/>Architect Dalton arrives late with coffee for his father's assistant, Wilson. Dalton presents his green initiative for the new city building. Dalton's father, Parker, likes the idea but not Dalton's tardiness. He wants Dalton to prove himself. The town's Christmas Festival will fund the new building. He wants Dalton to help the organizer. If he does well, then Parker will let Dalton design the building.<br/><br/>Zoey meets with the townspeople at the abandoned newspaper factory and asks them to volunteer. Gus, Alyx, and Mr. Amberson offer to volunteer their time and services. Dalton introduces himself and Wilson to Zoey. Zoey doesn't want his help but accepts when she discovers Dalton will be the architect for the building she wants to decorate. Dalton asks to view their Christmas decorations, and Zoey takes him to the back room. She opens the door and sees a mess. Someone destroyed the decorations. Zoey and Avery explain that someone is attempting to sabotage the festival. Zoey and Dalton will work together to uncover the culprit so they can keep their jobs.<br/><br/>This standalone sequel doesn't have the murder of a Hallmark Mystery but provides an intriguing enigma. Besides the mystery, viewers get three love stories. The leads are the first love story, like any holiday romance. Next is Avery and Wilson. Finally, you have Avery's mom, Phyllis, and her baking rival, Jim. Each love story slows down the mystery. That storyline separates this film from others this holiday season. Hopefully, the next installment will be fewer love stories and more Christmas mysteries.<br/><br/>2.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/northbynorthpoledialsmystery.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/northbynorthpoledialsmystery.jpeg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "24-Karat Christmas",
-    "review": "Trish's friend, Gia, sets Trish up on a blind date with Booker. When he's late, Trish pulls out a pen and notepad to sketch jewelry designs. Booker comes up behind Trish and scares her, causing her to spill her drink. Booker picks up the sketchbook as they clean the spill and admires her designs. The date doesn't go well. After Booker makes an ill-timed joke at Trish's expense, Trish leaves.<br/><br/>Trish finishes custom wedding rings for Bryce and Avery two days before Christmas Eve. Later that evening, the best man will pick them up for their Christmas Eve wedding. Trish boxes her orders when Gia enters the store.<br/><br/>Gia mentions the Jingle Ball Benefit and wants Trish to attend. Trish wants to spend the holidays relaxing at home. Gia thinks Trish's plans are sad and wants to set up Trisha on another blind date. Right now. Trish panics, and Gia shows Trish a few power poses. A man, Mason, enters and fumbles with Trish's jewelry. She grabs the ring box and places it on her desk while talking to Mason. She tries a power pose and knocks the boxes off her desk. The carrier appears. Trish grabs the boxes on the floor and hands them to the carrier.<br/><br/>Trish closes the shop when she hears a knock at the door. It's the groomsman, and he's late. She opens the door to reveal Booker. She looks for the rings and can't find them. Trish thinks the carrier mistakenly has the rings. Booker and Trish arrive at the carrier, but it's closed. Trish tells Booker she will return when they open and fix the situation. Since the package didn't have a shipping label, they can't ship it. So it must be in the warehouse.<br/><br/>Trish arrives in the morning, and Booker is there with coffee. He blames himself, so he will help Trish find the rings. Trish doesn't like his carefree attitude, but it has results. Booker thinks Trish can accomplish more if she relaxes. Can these opposites find the ring before the wedding without harming each other? Will they find more?<br/><br/>This film is everything you would expect from a holiday rom-com. Trish is a high-strung woman, and Booker is a relaxed man. His friends understand his carefree attitude, but Trish doesn't. Without this understanding, the audience will view Booker as irresponsible and disrespectful. However, his attitude stems from pain. She needs to relax a little, but not to Booker's level.<br/><br/>3 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/24karatchristmas.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/24karatchristmas.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Happy Howlidays",
-    "review": "Mia works at the Seattle Tourism Bureau for her friend, Isabel. Isabel empathizes with Mia's problems but believes Mia should move about the world again. Mia exhales and works. Mia walks home and finds a dog stuck in a fence. The dog follows her home, and she reluctantly lets the dog inside.<br/><br/>Mia wakes up to pee and goose feathers covering her living room. She takes the dog to Pup Town Rescue and sees the Rescue's owner, Max, interrogating a potential dog owner. He condemns Mia for trying to give the dog away and pushes her out of the Rescue. Max's assistant, Penny, tells Max he was too harsh. Mia brings the dog to work, and Isabel convinces Mia to name the dog. Mia names the dog Russell. Isabel gives Mia a new assignment to get her moving around town and discovering new adventures for tourists and locals. Also, Isabel encourages Mia to include Russell.<br/><br/>Mia takes Russell to the dog park, and he starts playing with another dog, Jules. Mia records it, and a man yells at her for recording his dog without permission. The man is Max. He gives her a list of other parks, and she happily resigns to go there. She posts the video to the website, and it goes viral. Commenters want to know more about Jules and the owner. Isabel demands that Mia find them and make new content.<br/><br/>Mia comes to the Rescue and explains the situation to Max. He says no until Penny reminds him that the Rescue is in financial trouble. Mia offers to foster Russell until they find an owner, and Max says yes. He agrees. Can they fall in love as fast as their dogs?<br/><br/>If you own a dog, this movie will be off-putting. Max is rude and condescending to customers, and it's no wonder his business is failing. Mia has a sad backstory that will pull at some viewer's heartstrings. It explains her depression. Hallmark wisely put this film at the end of their 2024 Countdown To Christmas slate.<br/><br/>2.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/happyhowlidays.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/happyhowlidays.jpeg"
     }
   }
 ];
@@ -58,20 +58,47 @@ module.exports = madeForTV;
 
 {
 "option":"1",
-"mtitle":"Vanished Out of Sight",
+"mtitle":"Terror Comes Knocking: The Marcela Borges Story",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/vanishedoutofsight.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vanishedoutofsight.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/terrorcomesknockingthemarcelaborgesstory.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/terrorcomesknockingthemarcelaborgesstory.jpg"
 }
 },
 {
 "option":"2",
-"mtitle":"Dressed to Kill",
+"mtitle":"My Fatal Social Following",
 "review":"<br/><br/>* out of 5 stars.",
 "image":{
-"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/dressedtokill.jpg",
-"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/dressedtokill.jpg"
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/myfatalsocialfollowing.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/myfatalsocialfollowing.jpg"
+}
+},
+{
+"option":"3",
+"mtitle":"The Jane Mysteries: A Deadly Prescription",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thejanemysteriesdeadlyprescription.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thejanemysteriesdeadlyprescription.jpg"
+}
+},
+{
+"option":"4",
+"mtitle":"Vanished in Death Valley",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vanishedindeathvalley.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/vanishedindeathvalley.jpg"
+}
+},
+{
+"option":"5",
+"mtitle":"Love of the Irish",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveoftheirish.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveoftheirish.jpg"
 }
 }
 
