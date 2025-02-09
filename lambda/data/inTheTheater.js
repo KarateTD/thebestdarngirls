721b72568e62b9b4e571863d8fe719aeceda5c05
Tiffany Gray <KarateTD@yahoo.com>
2025-02-09 15:19:26 -0500

    add 2 theater movies

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option": "1",
+    "mtitle": "Love Hurts",
+    "review": "Realtor Marvin bakes cookies before riding his bike to his next house showing. Marvin's assistant, Ashley, calls to remind Marvin about the office Valentine's Day party.<br/><br/>After the house showing, Marvin arrives with leftover heart-shaped cookies for the staff. He opens a red envelope with a note saying Rose is back. Dumbfounded, Marvin asks Ashley to give him some alone time in his office. Marvin walks through the door and wakes up. Marvin lifts his head to see assassin Raven sitting on the couch reading poetry. Raven only has one question for Marvin: where's Rose? Marvin says he doesn't know and fights. He asks Cliff to borrow his car and drives home.<br/><br/>Marvin quickly bags clothes and money. Then, hitmen King and Otis break in and attack. They want to know why Marvin didn't kill Rose as Marvin's brother, Knuckles, instructed. Years ago, Rose was Knuckle's accountant and kept his dirty money clean. However, Knuckles discovered she stole $4,000,000 from him. Marvin took the hit because Marvin secretly loved Rose. He told her to hide and never return. Marvin did the same. Marvin escapes Otis and King, but Rose shocks Marvin with a stun gun before he makes it to Cliff's car.<br/><br/>Marvin wakes up with a new shirt in the back of a club. He finds Rose serving drinks at the bar. She is tired of hiding because 'hiding ain't living.' Rose found information to prove she didn't steal Knuckle's money. So she sent Valentine's Day cards to Knuckles and everyone in his inner circle to announce her return. She wants Marvin to reclaim his hitman life, end Knuckle's crew, and control the empire. However, Marvin is happy with his new life. With Rose announcing her return, Knuckles wants his goons to bring them to him alive. What path will Marvin choose, and will he live to see it?<br/><br/>This film is a Valentine's Day movie that has something for everyone. It has true love, betrayal, and violence. The highly choreographed fights will remind the audience of Kill Bill. The director used interesting point-of-view angles from inanimate objects. However, the cuts make the stunt doubles and wire work obvious while taking away from the cinematography. The writers used the holiday to inject some much-needed humor into the script. While the film has flaws, it will entertain you and become your go-to Valentine's Day movie.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/lovehurts.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/lovehurts.jpg"
+    }
+  },
+  {
+    "option": "2",
     "mtitle": "Companion",
     "review": "Iris awakens after a long drive to a rustic small cabin in the woods with her boyfriend, Josh, in the driver's seat. Kat answers the door but gives Iris the cold shoulder. Iris and Josh greet Patrick as he cooks, and Patrick's boyfriend, Eli, offers Iris and Josh hugs. Later, Kat's boyfriend, Sergey, descends the stairs and welcomes everyone to his home.<br/><br/>After dinner, Iris sits with Kat and asks about her relationship with Sergey. Kat admits Sergey is a terrible boyfriend and husband, but he has money. She dislikes what Iris represents: Kat's replacement. The couples turn in for the night, and Josh and Iris make love. Afterward, Iris professes her undying love to Josh, and he tells her to go to sleep.<br/><br/>Iris wakes up and prepares for an evening on the beach with Josh. However, he doesn't want to go. Iris walks to the beach chairs and breathes in the morning air. She puts her hands in her pocket and pulls out a switchblade. Sergey joins Iris. Later, Iris returns to the cabin with a large blood stain on her shirt. Josh demands answers. Iris explains that Sergey attacked her, and she defended herself. Iris didn't care about dying but was afraid not to see Josh again. Josh tells Iris to go to sleep, and she powers down.<br/><br/>Josh and Patrick tie Iris to a chair, and Josh asks to be alone with Iris to say goodbye. He wakes Iris up and explains that she is his companion robot from Empathix Robotics. They will explain to the police that Iris killed someone. Empathix will label her defective and turn her off. Iris cuts herself free and escapes from the house with Josh's phone. Patrick sees Iris escape and warns the others. Josh says they must get Iris back. If Empathix examines her, they will see he jailbroke Iris to make her more aggressive and go against her factory settings. He and Kat planned Sergey's murder and asked Patrick and Eli to come to strengthen their alibi. Can Iris escape them before they destroy her?<br/><br/>This movie is leaps and bounds better than the trailer. The trailer gives viewers one plot twist. However, there are several. This plot leans into the warnings of artificial intelligence and the dehumanization of people while being bloody and funny. The script touches on the importance of autonomy, the pitfalls of entitlement, and why you should read the user agreement without being preachy. This film isn't a horror movie but falls in the same category as M3GAN. It's a thought-provoking, bloody, cringy good time. You can also take a date.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "2",
+    "option": "3",
     "mtitle": "Dog Man",
     "review": "Officer Knight and his K-9 Greg chase Petey the Cat all around town. The team continuously catches Petey. Petey knows Greg is clever but isn't strong, and Knight is powerful but not intelligent. Petey's assistant, Butler, thinks Petey should plan for when they combine. Petey fires Butler and creates a new devious scheme.<br/><br/>Knight and Greg believe they have Petey cornered, but Petey escapes and leaves them on top of an abandoned building with a bomb. The bomb detonates. The doctors tell Greg that his body is in bad shape and Knight's head is not fixable. They decide to put Greg's head on Knight's body. They create Dog Man. Mayor is tired of Petey's antics and wants Chief to catch Petey. However, each time he catches Petey, Petey always escapes. Mayor grows tired of Petey's antics. If Dog Man doesn't apprehend Petey for good, she will make Chief fire Dog Man.<br/><br/>Petey learns of the doctor's creation and screams. Butler says the only person who could work for Petey is Petey. Petey decides to clone himself. The clone, Lil' Petey, will take 18 years to develop. Petey brushes his clone aside and creates a new scheme. He will reanimate Chief's most diabolical enemy, Flippy, and program Flippy to destroy all do-gooders. Will Petey succeed?<br/><br/>Based on the Dog Man book series by Dav Pilkey, this movie accomplishes verbal and visual jokes without getting raunchy. The graphics are simple but contain eye-catching jokes in the background. The voice actors did a magnificent job of conveying the emotions of their characters. The conclusion is a beautiful lesson in co-parenting and breaking family cycles. This film is for kids who are Dog Man or Captain Underpants fans. However, parents should pay the matinee price.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "3",
+    "option": "4",
     "mtitle": "Flight Risk",
     "review": "Accountant Winston sits in the Igloo Motel in Alaska, warming his Cup Noodles in the microwave. Then, U.S. Marshalls bursts through the door. Deputy Madolyn Harris arrests Winston, and he requests an immunity deal to become a state's witness against his boss, Moretti.<br/><br/>Madolyn escorts a handcuffed Winston onto a private plane. The pilot, Daryl, jovially boards the plane. Daryl lifts the plane off the ground and flies through the air. Winston sits behind them as Daryl makes small talk.<br/><br/>Madolyn asks for an E.T.A. for their landing, and Daryl tells her to put on her headphones so they can talk. As Madolyn and Daryl converse, Winston sees a pilot's I.D. under the seat. He moves it with his foot and sees Daryl's name with another man's face. He tries to warn Madolyn, but she doesn't heed his warning.<br/><br/>Madolyn sees blood on Daryl and remembers suspicious things he said. She reaches for his gun, but he chokes her until she is unconscious. He plans on killing them slowly for Moretti. Daryl doesn't see Madolyn wake up and grab her taser. She shocks Daryl until he is incapacitated. They look through Daryl's pockets and find sheets of paper. The papers contain pictures of Winston, Madolyn, and the home of Winston's mother. Winston recalls he only told one person his mother's address. Madolyn deduces that there is a mole in the U.S. Marshall's office. She has to land the plane with a fugitive witness, a psychopath, and low fuel while investigating the mole. Also, Madolyn doesn't know how to fly. Can she save her witness, or will they all die?<br/><br/>This movie provides action and suspense with a dash of inappropriate humor. Winston, Madolyn, and Daryl are the main characters, but Madolyn talks to several people on her satellite phone. This banter helps the audience unpack the mystery. The tight space makes fight scenes challenging, but the actors pull it off. However, some shots lose continuity to save time. This film is the correct length for the plot but leaves the ending open for more. Flight Risk is a solid matinee movie.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "4",
+    "option": "5",
     "mtitle": "Nickel Boys",
     "review": "Elwood lives with his grandmother, Hattie, after losing his mother. He is attentive in school and dreams of going to college. His teacher, Mr. Hill, doesn't teach American History from the textbooks. He encourages his students to be free-thinkers, question authority, and demand change in the Jim Crow South. Mr. Hill sees promise in Elwood and gives Elwood a pamphlet for a free college. Elwood decides to hitchhike to the school and apply. A man pulls over and offers Elwood a ride. Later, police pull the man over. They arrest the man and Elwood for stealing the car. The judge sends Elwood to Nickel Academy, a segregated reform school for boys.<br/><br/>It doesn't take long for Elwood to uncover that the staff treats black and white students differently. He considers changing the dynamics until he meets Turner. Turner warns Elwood that there are four ways to leave Nickel Academy: do your time, age out, run away, or die. Elwood believes there's a 5th way: change it. Decades later, the state is investigating the academy. It triggers panic attacks in Elwood because the state needs witnesses. Can Elwood testify and come to terms with his past?<br/><br/>Based on the novel The Nickel Boys by Colson Whitehead, the director intentionally shot the movie from different perspectives. It gives Elwood's point of view from his childhood through his teens. The point of view switches when Elwood meets Turner. Once Elwood enters adulthood, the camera changes to a birds-eye view. This director does this to shock the audience during the final reveal. This film features abuse, but you don't see it. You feel it. This film is an extraordinary watch if you can handle it.<br/><br/>5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/nickelboys.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/nickelboys.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Den of Thieves 2: Pantera",
-    "review": "Donnie and his crew ambush the security at the Antwerp International Airport tarmac. They break into one of the shipping containers and steal the diamonds inside. Some days later, they meet in Nice France, and Jovanna hands Donnie his cover story. He will act as a diamond dealer and use the stolen gems to get access to the World Diamond Center. Donnie funds the operation and collects information about the diamond volt. No one has ever successfully robbed the WDC, and Donnie plans to be the first.<br/><br/>Detective Nick finalized his divorce and custody agreement with his ex-wife in Los Angeles. And he can't get the representatives of the Federal Reserve to admit someone robbed them. Nick grabs Merrimen's girlfriend and threatens her life if she doesn't tell him where Donnie hid the money. She admits the money is in Panama. Only one account catches his eye, and someone has made regular withdrawals from France. Nick flies to France and uses his old U.S. Marshall's badge to gain access to the French police department. The police show Nick footage around the WDC, and Nick spots Donnie on security tapes. However, he tells the police it's not Donnie and leaves the precinct.<br/><br/>Donnie returns to his French apartment after spending time at the WDC. He hears something moving throughout and grabs his gun. Nick is eating a meal and enjoying a drink. He wants to join Donnie's crew and get paid. If Donnie doesn't include Nick, then Nick will arrest Donnie. So Donnie relents. Donnie and Nick think they can rob the WDC and leave without a trace. However, Donnie doesn't know that the diamonds he originally stole belong to a mafia syndicate called Octopus. The leader put a tracker on his diamonds and wants them back now. Can Donnie and Nick complete the heist before the Octopus gets a hold of them?<br/><br/>This sequel, inspired by the Antwerp Diamond Heist, has all the heart-pounding action of the first movie while telling a fresh story. It puts the audience in the middle of the heist with several ticking clocks, low-tech solutions, and high-tech gadgets. And the heist isn't the end of the story. The script recaps the first film, so you don't need to see it to enjoy this picture. There are no mid- or post-credit scenes, but the ending hints at part 3. So be ready to hold your breath and root for the bad guy because this film will take you on a wild ride.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/denofthieves2pantera.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/denofthieves2pantera.jpeg"
     }
   }
 ];
@@ -57,6 +57,16 @@ module.exports = inTheTheaters;
 
 /*
 
+
+{
+"option":"1",
+"mtitle":"Captain America: Brave New World",
+"review":"<br/><br/>* out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/captainamericabravenewworld.jpeg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/captainamericabravenewworld.jpeg"
+}
+}
 
 
 */
