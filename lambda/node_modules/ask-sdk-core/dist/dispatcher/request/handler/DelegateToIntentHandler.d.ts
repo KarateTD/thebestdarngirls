865abed178aaaa55f0c463019da3ee1c597c0a12
Tiffany Gray <KarateTD@yahoo.com>
2025-03-04 19:50:26 -0500

    1st commit

--- lambda/node_modules/ask-sdk-core/dist/dispatcher/request/handler/DelegateToIntentHandler.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'ask-sdk-model';
-import { CustomSkillRequestHandler } from './CustomSkillRequestHandler';
-import { HandlerInput } from './HandlerInput';
-export declare class DelegateToIntentHandler implements CustomSkillRequestHandler {
-    canHandle(input: HandlerInput): boolean;
-    handle(input: HandlerInput): Response | Promise<Response>;
-}
