8b7c379427159677b5f1901dcffed91d3c002537
Tiffany Gray <KarateTD@yahoo.com>
2025-04-10 00:14:26 -0400

    add drop

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -84,6 +84,19 @@ module.exports = mustBuy;
     }
     }
 
+    {
+    "option": "1",
+    "mtitle": "Drop",
+    "review": "Drop was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
+    "asin":{
+      "re":""
+    },
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/drop.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/drop.jpeg"
+    }
+  }
+
    {
     "option": "1",
     "mtitle": "Paddington in Peru",
