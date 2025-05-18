56aef8ea21d5eaa9e8fbd776b4eede77e9eca463
Tiffany Gray <KarateTD@yahoo.com>
2025-05-18 19:34:45 -0400

    final dest

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option": "1",
+    "mtitle": "Final Destination Bloodlines",
+    "review": "Stefani awakens in the middle of class with a scream. For two months, she has had the same nightmare about her grandparents, Iris and Paul, every day. Stefani believes returning home and speaking to her father, Marty, about Iris will ease her mind. Her roommate agrees.<br/><br/>Stefani arrives home and greets Marty and her brother, Charlie. Stefani asks her father about a box of Iris's things that her mother, Darlene, kept. Marty tells Stefani not to talk about Iris and that the box is with Stefani's uncle Harold. Charlie wants to visit his cousins, so Stefani offers to drive.<br/><br/>They arrive at Harold's home and greet their aunt, Brenda, and cousins, Erik, Bobby, and Julia. Stefani questions Harold about Iris. He says that Iris is a crazy woman, and Child Protective Services had to save him and his sister, Stefani's estranged mom, Darlene, from Iris. Stefani finds old letters with Iris's address and drives there.<br/><br/>Stefani arrives at a heavily fortified home. She tells Iris about her dreams, and Iris says it's her premonition. Fifty years ago, Iris and her boyfriend, Paul, went to the grand opening of the Skyline Tower. Paul and Iris went to the observation deck. During Paul's proposal, Iris had a premonition that everyone would die. She and a little boy, JB, would be last. She warned everyone and saved their lives. Since that day, everyone and their descendants died in the order they would have that night. Death is still after Iris and her heirs.<br/><br/>Iris runs after Stefani but stops at the doorway. Iris chooses to risk it and begs Stefani to take the book. Stefani grabs it, but Iris can still see disbelief in Stefani's eyes. Iris takes three steps back and lets Death take her. Stefani rushes to her family to warn them, but it is too late. A member met a gruesome end. Death is coming for them, but can Stefani make her family believe before it's too late?<br/><br/>This Final Destination installment doesn't disappoint. The deaths are grisly and cringe-inducing, and you are guaranteed to have a new fear unlocked. The writers injected more humor into this chapter with added gore. Unlike its predecessors, this film didn't rely too heavily on the signs. Stefani got a bad feeling and acted accordingly. This film doesn't offer mid or post-credit scenes. This tribute to Tony Todd is a bloody, good time with lots of laughs and guts.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/finaldestinationbloodlines.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/finaldestinationbloodlines.jpg"
+    }
+  },
+  {
+    "option": "2",
     "mtitle": "Shadow Force",
     "review": "Isaac inserts his hearing aids while his son, Ky, questions Isaac about his scars. Isaac answers honestly and tells his son they came from bad guys—the same bad guys keeping Ky's mother, Kyrah, away. Ky admits that he misses Kyrah. Isaac promises that Kyrah will come home when she can.<br/><br/>Isaac takes Ky to the bank, and several armed people enter. He asks the robber to let Ky walk to him, but the robber points his gun at Ky's head. Isaac removes his hearing aids and unleashes his wrath on the bank robbers. He grabs Ky and exits the bank while taking stock of the cameras.<br/><br/>The bank footage goes viral. Newly-appointed Secretary General Cinder views it and goes into a rage. He briefs the heads of several governments about an elite group of assassins he created called Shadow Force. They operated with one rule: they couldn't leave. Kyrah and Isaac defected when she got pregnant. Cinder will double the bounty on Isaac and Kyrah.<br/><br/>Kyrah sits alone and watches the video. She thinks back to the day she left Isaac and Ky. She knew Cinder wouldn't stop hunting them. So, she vowed to hunt each member of the Shadow Force. Kyrah knew they wouldn't see her coming because no mother would leave their child. With Isaac's cover blown, she must locate her family and protect them from Cinder. If she fails, she will lose everything.<br/><br/>An audience wouldn't expect an action film on Mother's Day, but this film is a bloody hug to moms everywhere. Ky is the unexpected comic relief in this film. The plot overreaches for its runtime. The script attempts to add a twist but gives the audience too much foreshadowing. The fight scenes are intentionally blurry but add comedy to distract. While the script and action have flaws, it's an endearing action movie with old-school love and solid family values, but not for younger viewers.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "2",
+    "option": "3",
     "mtitle": "Sinners",
     "review": "Guitarist Sammie leaves the cotton fields for home and searches for his guitar. Pastor Jebediah admits to taking it. He fears evil will prey on his son if Sammie continues to play the devil's music. Sammie won't let Jebediah's worries deter him. He takes his guitar and exits the church. His cousins, twins Elijah 'Smoke' and Elias 'Stack' Moore, wait for him.<br/><br/>Everyone in Clarksdale, Mississippi, knows the Smokestack Twins are not to be trifled with. They fought in World War 1 and worked with mobsters in Chicago. They recently bought a sawmill and will turn it into a juke joint tonight. Smoke and Stack want Sammie to play at their establishment.<br/><br/>The crowd gathers at the juke joint for fun, singing, and dancing. Soon, Stack gets word that his ex-girlfriend, Mary, is in attendance. Stack tells her to leave, but Mary refuses.<br/><br/>During the performances, three people approach the juke joint. Remmick, Bert, and Joan heard Sammie's performance and wanted to play. However, the twins don't believe they will be comfortable and could cause trouble, so they don't invite the trio inside. Mary discovers the twins haven't made enough money and wants to meet the trio to determine trustworthiness. The juke joint is about to dance with the devil. Will they survive?<br/><br/>This movie turns the overworked vampire genre into something special with the 1930s backdrop and spectacular score. The film opens with a brief introduction to a shared folklore. There's music so enchanting that it can pierce the veil between the past and future, the living and the dead, and invite evil. As the tune plays, the director adds past and present music styles along with different types of dancers. It makes the opening sequence come to life and transports you into the world in between. Also, the scenes with the twins interacting are nearly flawless, but the blood scenes are a bit over the top. This movie is captivating, educational, and a tad scary. However, stay for the mid and post-credit scenes. The director, writers, and composers made this film for the big screen.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "3",
+    "option": "4",
     "mtitle": "Thunderbolts",
     "review": "Yelena completes another mission for CIA Director Valentina but doesn't feel fulfilled. So, she visits her father, Alexei, the Red Guardian, for help. He tells her the best way to feel fulfilled is to do good for her country. Yelena leaves Alexi and calls Valentina. She agrees to do one more job and then become a public figure. Valentina tells Yelana to go to an Ox Corp black site and destroy everything inside, including a target.<br/><br/>Valentina stands before Congress for her impeachment trial. She warns them that the Avengers are gone. They must create new heroes to protect the Earth. Valentina leaves the hearing with her assistant, Mel. She tells Mel to clean everything they can use against her in the hearing.<br/><br/>Congressman Bucky Barnes attends a Battle of New York exhibit with Valentina and Congressman Gary. Bucky runs into Mel and gives her his business card. Then, Bucky pulls Gary aside. He tells Gary that Mel may switch sides. However, Gary wants Bucky to focus on his job.<br/><br/>Yelena arrives at the black site. Then, John Walker hits her. She retaliates but stops because he is not her target. However, she is his. During their battle, Ava, the Ghost, appears and attacks John because Valentina hired her to kill him. They battle, and Taskmaster appears. Ava kills Taskmaster in the battle. The remaining three continue to fight and accidentally hit a button on a box. The box opens and releases Bob. Ava, John, and Yelena argue until they realize that Valentina set them up to kill each other and wipe her slate clean.<br/><br/>Mel second-guesses Valentina's leadership when Valentina instructs Mel to kill her operatives. However, Mel alerts Valentina that they survived and released Bob. She demands that Mel get her to the site and save Bob, but everyone else can die. Valentina and Mel don't know that Alexei is their driver. He drops them off and rushes to warn Yelena. Mel calls Bucky and gives him the details. He rushes to find the three and brings them to the hearing as evidence of Valentina's crimes. Yelena, Bucky, Red Guardian, Ava, and John Walker will form the Thunderbolts. They assume their enemy is Yelena. However, they will battle the void inside all of them.<br/><br/>Marvel fans, you asked for a more story-driven and gritty MCU. This movie delivers. The opening sequence changed. It only includes pages from the comic books. Then, the music and image go dark. Most of the audience has experienced feelings of loneliness, nothingness, sadness, shame, and even depression. This film is the physical manifestation of that. The film's quips are original, funny, and perfectly acted and timed. The only downfall is the first battle scene between Bob and the Thunderbolts. It's reminiscent of Hulk and Loki. It's cartoonish and overly computerized at a time of seriousness. If this is the new direction of the MCU, then they need to stay on this path. Viewers should remain for the mid and post-credit scenes.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "4",
+    "option": "5",
     "mtitle": "The Amateur",
     "review": "Sarah walks to the garage to spend quality time with her husband, Charlie, before her business trip. She will spend five days in London. Charlie works for the CIA in the Decryption and Analysis department. His contacts send him top-secret information. Charlie sees Sarah off before going to work.<br/><br/>Charlie walks to his desk and gets an encrypted message from Inquiline. Inquiline sends Charlie several files. They prove that Charlie's boss, CIA Director Alex Moore, falsifies drone strikes as suicide bombings to hide their political influence. Charlie is unsure what to do with the information and goes home.<br/><br/>Charlie arrives at work and sees colleagues at his desk. Charlie worries as he enters Alex's office. Alex and the CIA Director Samantha O'Brien have distressing news for Charlie. There was a terror attack in London, and a terrorist killed Sarah.<br/><br/>Charlie comes to work the next day and surprises everyone. He researched all night and uncovered the four assailants: Mishka Blazhic, Ellish, Gretchen Frank, and Horst Schiller. Horst, the most elusive, killed Sarah. Charlie shows Alex and CIA Supervisor Caleb, and they are not shocked. Charlie demands field training, cash, and a new identity so he can eliminate Sarah's killers. They tell Charlie to report to Henderson at the training facility. Henderson will prepare Charlie for his tasks, but Alex wants Charlie to become a victim. Can Charlie get his revenge before Alex silences him?<br/><br/>Based on The Amateur: A Novel of Revenge by Robert Littell, this globe-trotting revenge plot pits brain against brawn. Charlie has an affinity for field work but doesn't possess the strength or callousness. However, he uses his skills and knowledge to stay one step ahead. The graphics are gritty but not shocking or overly bloody. The trailers promise a high-octane story. However, the writing contains several lulls and an unnecessary 'will they, won't they' aspect. Viewers should see this picture in theaters, but you won't want to buy it.<br/><br/>3.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/theamateur.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/theamateur.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Warfare",
-    "review": "A U.S. Navy SEAL team enters a home in Ramadi, and translators Farid and Noor secure the family living inside. The team sets up camp in the house and monitors the market across the street for suspicious activity.<br/><br/>In the morning, the team views a lookout on the roof across the street. Then, Ray and Elliot see increased activity and guns but can't get a clear shot. As they report their findings, someone throws a grenade into the window. They call over the radio for a Bradley and wait. The Bradley arrives, and the team sends the translators first. The team joins them, and an I.E.D. detonates. One of the translators is gravely wounded, while Sam and Elliot are severely injured. The team radios for two Bradleys and an extraction team to help Sam and Elliot. It will take 10 minutes. It will be the longest 10 minutes of their life.<br/><br/>Wow! Based on the team's memories, this movie covers a gruesome battle in a warzone. The film feels slow and tedious as this young team carries out their mission. Then, hell comes to them. The graphics department didn't spare any horrific detail. The audience sees blood, guts, and dismemberment. This film should have a trigger warning for anyone with war-related PTSD. You will also see images of some of the team during the end credits. The action makes this movie worth the 'big screen' price, but one viewing is enough.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/warfare.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/warfare.jpeg"
     }
   }
 ];
@@ -56,16 +56,6 @@ module.exports = inTheTheaters;
 */
 
 /*
-
-{
-"option": "1",
-"mtitle": "Final Destination Bloodlines",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/finaldestinationbloodlines.jpg",
-  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/finaldestinationbloodlines.jpg"
-}
-}
 
 {
 "option": "1",
