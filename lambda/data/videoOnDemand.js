8b1e6cffa2da9abd95912db8d3431f19fd85aff5
Tiffany Gray <KarateTD@yahoo.com>
2025-07-31 10:24:59 -0400

    add dutch

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Dutch III: International Gangster",
+		"review": "Craze grabs food for Angel and Goldilocks as they await Dutch's instruction. She wants to settle the score with Zoom once and for all. Craze pleads with Angel to stay low, but questions Goldilock's loyalty. Angel assures Craze that Goldilocks is ride or die. Angel doesn't know Goldilocks is in communication with Detective Yates. She promises to put Angel and Goldilocks in witness protection if Goldilocks can prove Dutch is alive.<br/><br/>Dutch partners with Renato in Italy. Renato sells everything, but diamonds are his specialty. Mancuso stole Renato's rarest diamond, and Renato sent Dutch on a mission to find it. He eliminated Mancuso's crew and recovered weapons. However, Mancuso and the diamond weren't there. Dutch needs time and his crew. Renato obliges. Dutch has his interpreter, Carmella, request phony passports for Angel, Craze, and Zoom. Angel's rage boils over when she sees Zoom's passport. She vows to kill Zoom on sight. So, Craze and Angel fly to Italy without Zoom.<br/><br/>In Newark, Zoom's brothers forgive him for his past transgressions. Sugar Ray takes Zoom to the Sammy Education Center. He wants to open the doors soon, but lacks funding. Zoom wants to help, but he's living off his savings. Later, Carmella propositions Zoom with a video of Dutch, a bag of money, and a promise for more. Dutch needs his whole family to finish the diamond heist. Can this crime family turn Dutch into an international gangster, or will the family implode?<br/><br/>Based on Dutch III: International Gangster by Teri Woods, this gritty crime drama is a far cry from the source material. This review will focus solely on the film. The third installment of the Dutch franchise starts exactly where the 2nd film ends. You must see both films before diving into the third. Viewers will crowd the screen because Dutch must be five steps ahead while maintaining peace in the family. This film doesn't have the body count of the second but contains gripping storytelling. The director chose a wild juxtaposition of two couples coming to terms with their relationship. This emotional yo-yo will leave you satisfied with their decisions. The film contains Italian dialogue with English subtitles. This addition makes the setting feel authentic and raw. While the picture is captivating, the ending doesn't feel complete. This conclusion will make you hope for a part 4 to complete everyone's tale. However, the book series is a trilogy.<br/><br/>4 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/dutchiii.jpeg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/dutchiii.jpeg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "War of the Worlds",
 		"review": "Will Radford logs into his computer at the Department of Homeland Security. He surveils people and their phone calls for potential terrorist threats. One threat is the Disruptor. He is an online vigilante who threatens to expose secrets about a data collection program called Goliath. DHS members reiterate that it doesn't exist. However, they are hunting the Disruptor because it has breached their firewalls and stolen top-secret information. Will uses his tools to keep an eye on his family. His daughter, Faith, and Faith's boyfriend, Mark, are pregnant with their first child. Will spies on Faith at a café and scolds her for not eating enough protein. Then, Will contacts his son, Dave. He urges Dave to use his degree to get a job and move out. However, Dave says his work is video games. Dave believes Will changed after their mom, Erika, died.<br/><br/>Will gets a solid lead on the Disruptor's location. He obtains an arrest warrant and sends it to FBI Agent Jeffries. They don't find the Disruptor during the raid, and Will is furious until he sees meteors fall from the sky. Will receives media notifications that fallen meteors are a worldwide phenomenon. Then, the meteor opens, and a large alien appears. Will calls Faith and finds her hiding in her office. He alerts Mark to Faith's location before an alien strikes her building. Faith survives the blast but has an object embedded in her leg, and the wound is bleeding heavily. Dave tries to contact Will, but Will focuses his concern on Faith. Dave yells that there is more to the aliens than Will knows. Will doesn't want to listen to Dave's conspiracy theories. He must save the world and Faith.<br/><br/>This apocalyptic thriller utilizes powerful screenlife storytelling. It warns about data collection and government surveillance. The introduction gives you a glimpse into Will's life. The film has enough suspense to keep the viewers watching with interest. The script isn't where the picture fell short. The graphics are laughable, and the plot relies on technology that you hear about in spy novels. It removes the reality of the circumstances and makes the dire situations laughable. War of the Worlds found a proper place on Prime Video, but don't start a subscription to watch it.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Madea's Destination Wedding",
 		"review": "District Attorney Brian waits at a high-end restaurant with his ex-wife, Debrah, and son, BJ. Brian inquires why their daughter, Tiffany, chose this restaurant instead of something more affordable, like Red Lobster. Since Debrah has a wealthy husband, she will never eat at establishments like that again.<br/><br/>Amongst the discord, Tiffany enters with her boyfriend, Zavier. She and Zavier have been dating for over a year, and they have since gotten engaged. Brian believes Zavier should ask for his permission, but Zavier says no. And to add to Brian's stress, Tiffany says she and Zavier want to get married in two weeks. Brian is speechless.<br/><br/>Tiffany calls a family meeting at Madea's home. Madea wonders why Tiffany wants to get married so fast. Debrah, Tiffany, and Zavier enter, and Zavier refuses to introduce himself. The family is appalled until Tiffany makes a declaration. She and Zavier will wed in the Bahamas. The family doesn't believe they can afford it. Debrah says Dennis will cover it. However, she states that the father of the bride should do that. Joe sees Brian about to speak and tells Brian to shut up. Brian states he will pay for the wedding and for everyone to attend. The family celebrates, but Madea can't shake the feeling that something is wrong. And she will get to the bottom of it with a slap, a punch, and a gunshot.<br/><br/>This film is precisely what you would expect from Tyler Perry. It places your favorite characters, Madea, Bam, Mr. Brown, and Joe, in different environments to add laughs and hijinks. They get louder, prouder, and wilder. You will look forward to the family scenes but not the drama. Of course, this movie has a lesson about tolerance, prejudice, acceptance, and true love. It takes up the last 15 minutes but isn't a dominant theme. This film offers a good chuckle and could become a guilty pleasure for some. Otherwise, it's nothing spectacular.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Dora and the Search for Sol Dorado",
 		"review": "Dora and Diego walk through the jungle with their Abuelo. He tells them about the legendary Sol Dorado treasure. It will grant its finder any wish. Abuelo knows Dora and Diego will become great explorers and gives Dora a blank map to guide her way. Later, Dora grew up watching Camila the Crusader. Dora dreams of being like Camila and finding lost treasure.<br/><br/>Now, teens Dora and Diego search the jungle for Sol Dorado. Diego and Dora go to the cave and find a sun tablet encased in the ground. Dora swaps her Camila compass for the tablet and smiles. Then, she sets off a trap. Diego saves Dora, but she loses her map in a fire. She clings to Diego for support. He sighs and pulls out an envelope from his pocket. However, Diego doesn't notice that he also grabbed a flyer for Camila's theme park. Dora sees it and assumes Diego wants to work there. She says yes.<br/><br/>Dora searches for the office and bumps into Camila. Camila praises Dora's adventurous spirit and gives her an ID badge. Dora meets her co-worker, Naiya, and Naiya's brother, Sonny. After a long day at the park, Dora notices she left her ID on the tram. Dora hides in the shadows as she watches Camila and her gang haggle about Sol Dorado. Camila sees her and sends her gang after them. They escape with Naiya and Sonny into the jungle. Now, Dora wants to find Sol Dorado before Camila and her goons exploit it. But can Dora do it without her map?<br/><br/>This new cinematic adaptation of Dora the Explorer combines elements of Spy Kids with Indiana Jones. This version of Dora is different because she is older and missing a key aspect of her character: the map. The storyline is enthralling and packs enough action to keep your kids entertained. This film will attract a slightly older audience without alienating its die-hard fans. This latest Dora adaptation lives up to the Nickelodeon hype and is fun for the entire family.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Heads of State",
 		"review": "Prime Minister Sam lies awake at night because one of his MI-6 officers, Noël, went missing. He gets up early to meet with his team to prepare for his next press conference with U.S. President Will. Sam detests Will because Will is an actor-turned-politician. Will believes Sam doesn't respect him. They attend the press conference, but Will's harsh words about the U.S. involvement against terrorism strike Sam the wrong way. Their handlers devise an idea for them to eat at a pub together while the press takes pictures. It will give Will credibility while making Sam likable. They agree to do it and board Air Force One to discuss strategy.<br/><br/>After boarding Air Force One, Will and Sam enter a private room. Will spots a plane flying too close and walks to his security for answers. The waiter attacks the security, and Will's bodyguard pulls them to safety. He gives Sam and Will a parachute and provides them with the directions to the safe house. They land safely, but Sam warns Will to destroy his phone. The world thinks they are dead, including their enemies. Will and Sam must arrive at the NATO summit before anyone discovers they are alive.<br/><br/>This action-comedy offers nothing new for the genre, actors, or special effects. Even with the star power, it's not enough to pull your attention away from anything else. The movie offers Bullet Train moments when it gives a comical recap, but provides few laughs. The secondary characters are more entertaining and well-written than the leads. It's no wonder the producers put it on Prime.<br/><br/>2.5 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/headsofstate.jpg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/headsofstate.jpg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Deep Cover",
-		"review": "Hugh acts tough to connect with his co-workers, but fails miserably. After the cool guy overlooks him again, Hugh desires to elevate his confidence. He walks and sees a sign for an improv class. It promotes the class as a way to build your voice.<br/><br/>Marlon auditions for a commercial but overacts the part. His agent, Terry, gets Marlon a job flipping signs, dressed as an elf. Marlon takes the part because he needs the money. He joins an improv class to hone his skills.<br/><br/>Acting teacher Kat applauds as two students complete an improv scene. As she describes the 'yes and' technique, Hugh knocks something over. She calls Hugh to the stage and searches the room for a partner. She sees Marlon standing at the bar and asks him to join Hugh on stage.<br/><br/>Two students told Kat that an agent would be in the audience for their show. Kat takes the stage but doesn't impress him. She drowns her sorrows in alcohol when a man, Billings, approaches her about a job. Agent Billings works for law enforcement and wants to hire improv actors to infiltrate a criminal enterprise. He is willing to pay $200 a day if Kat finds two more actors. She hears Hugh drop something and looks over to Marlon. She convinces them to join the team.<br/><br/>Billings needs them to enter a convenience store that sells counterfeit cigarettes. They must buy it while wearing recording devices and bring it to him. Kat and Marlon enter the store while Hugh waits by the door. However, the actors are too good. The store owner assumes they are drug dealers and takes the trio to his boss, Fly. Fly likes the way Kat does business, so he hires them for a bigger job. Kat, Marlon, and Hugh carefully rush out of Fly's lair and return to Billings. He demands that they stay in and get closer to Fly. These three will go to the underbelly of the criminal world, but will they return alive?<br/><br/>This action-comedy has its highs and lows. The action is enjoyable, but nothing new or groundbreaking for the genre. The comedy will make you lightly chuckle rather than laugh out loud. The script did an impressive job with each character's motivation. Viewers can see themselves as one of the protagonists as they fall into this spiral of madness. This picture is similar to Rough Night or Date Night. The setup is outlandish but gripping. While the film is charming, it isn't worth a Prime subscription.<br/><br/>3 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deepcover.jpeg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deepcover.jpeg"
 		}
 	}
 ];
@@ -55,19 +55,6 @@ module.exports = videoOnDemand;
 */
 
 /*
-
-
-
-{
-"option": "1",
-"mtitle": "Dutch III: International Gangster",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/dutchiiiinternationalgangster.jpg",
-	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/dutchiiiinternationalgangster.jpg"	
-}
-}
-
 
 {
 "option": "1",
