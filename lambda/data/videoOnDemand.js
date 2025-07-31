7d7c7583479a8bd00b58e8dad60ed579174c78fd
Tiffany Gray <KarateTD@yahoo.com>
2025-07-30 20:29:54 -0400

    war of the worlds

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "War of the Worlds",
+		"review": "Will Radford logs into his computer at the Department of Homeland Security. He surveils people and their phone calls for potential terrorist threats. One threat is the Disruptor. He is an online vigilante who threatens to expose secrets about a data collection program called Goliath. DHS members reiterate that it doesn't exist. However, they are hunting the Disruptor because it has breached their firewalls and stolen top-secret information. Will uses his tools to keep an eye on his family. His daughter, Faith, and Faith's boyfriend, Mark, are pregnant with their first child. Will spies on Faith at a café and scolds her for not eating enough protein. Then, Will contacts his son, Dave. He urges Dave to use his degree to get a job and move out. However, Dave says his work is video games. Dave believes Will changed after their mom, Erika, died.<br/><br/>Will gets a solid lead on the Disruptor's location. He obtains an arrest warrant and sends it to FBI Agent Jeffries. They don't find the Disruptor during the raid, and Will is furious until he sees meteors fall from the sky. Will receives media notifications that fallen meteors are a worldwide phenomenon. Then, the meteor opens, and a large alien appears. Will calls Faith and finds her hiding in her office. He alerts Mark to Faith's location before an alien strikes her building. Faith survives the blast but has an object embedded in her leg, and the wound is bleeding heavily. Dave tries to contact Will, but Will focuses his concern on Faith. Dave yells that there is more to the aliens than Will knows. Will doesn't want to listen to Dave's conspiracy theories. He must save the world and Faith.<br/><br/>This apocalyptic thriller utilizes powerful screenlife storytelling. It warns about data collection and government surveillance. The introduction gives you a glimpse into Will's life. The film has enough suspense to keep the viewers watching with interest. The script isn't where the picture fell short. The graphics are laughable, and the plot relies on technology that you hear about in spy novels. It removes the reality of the circumstances and makes the dire situations laughable. War of the Worlds found a proper place on Prime Video, but don't start a subscription to watch it.<br/><br/>3 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/waroftheworlds.jpeg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/waroftheworlds.jpeg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Madea's Destination Wedding",
 		"review": "District Attorney Brian waits at a high-end restaurant with his ex-wife, Debrah, and son, BJ. Brian inquires why their daughter, Tiffany, chose this restaurant instead of something more affordable, like Red Lobster. Since Debrah has a wealthy husband, she will never eat at establishments like that again.<br/><br/>Amongst the discord, Tiffany enters with her boyfriend, Zavier. She and Zavier have been dating for over a year, and they have since gotten engaged. Brian believes Zavier should ask for his permission, but Zavier says no. And to add to Brian's stress, Tiffany says she and Zavier want to get married in two weeks. Brian is speechless.<br/><br/>Tiffany calls a family meeting at Madea's home. Madea wonders why Tiffany wants to get married so fast. Debrah, Tiffany, and Zavier enter, and Zavier refuses to introduce himself. The family is appalled until Tiffany makes a declaration. She and Zavier will wed in the Bahamas. The family doesn't believe they can afford it. Debrah says Dennis will cover it. However, she states that the father of the bride should do that. Joe sees Brian about to speak and tells Brian to shut up. Brian states he will pay for the wedding and for everyone to attend. The family celebrates, but Madea can't shake the feeling that something is wrong. And she will get to the bottom of it with a slap, a punch, and a gunshot.<br/><br/>This film is precisely what you would expect from Tyler Perry. It places your favorite characters, Madea, Bam, Mr. Brown, and Joe, in different environments to add laughs and hijinks. They get louder, prouder, and wilder. You will look forward to the family scenes but not the drama. Of course, this movie has a lesson about tolerance, prejudice, acceptance, and true love. It takes up the last 15 minutes but isn't a dominant theme. This film offers a good chuckle and could become a guilty pleasure for some. Otherwise, it's nothing spectacular.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Dora and the Search for Sol Dorado",
 		"review": "Dora and Diego walk through the jungle with their Abuelo. He tells them about the legendary Sol Dorado treasure. It will grant its finder any wish. Abuelo knows Dora and Diego will become great explorers and gives Dora a blank map to guide her way. Later, Dora grew up watching Camila the Crusader. Dora dreams of being like Camila and finding lost treasure.<br/><br/>Now, teens Dora and Diego search the jungle for Sol Dorado. Diego and Dora go to the cave and find a sun tablet encased in the ground. Dora swaps her Camila compass for the tablet and smiles. Then, she sets off a trap. Diego saves Dora, but she loses her map in a fire. She clings to Diego for support. He sighs and pulls out an envelope from his pocket. However, Diego doesn't notice that he also grabbed a flyer for Camila's theme park. Dora sees it and assumes Diego wants to work there. She says yes.<br/><br/>Dora searches for the office and bumps into Camila. Camila praises Dora's adventurous spirit and gives her an ID badge. Dora meets her co-worker, Naiya, and Naiya's brother, Sonny. After a long day at the park, Dora notices she left her ID on the tram. Dora hides in the shadows as she watches Camila and her gang haggle about Sol Dorado. Camila sees her and sends her gang after them. They escape with Naiya and Sonny into the jungle. Now, Dora wants to find Sol Dorado before Camila and her goons exploit it. But can Dora do it without her map?<br/><br/>This new cinematic adaptation of Dora the Explorer combines elements of Spy Kids with Indiana Jones. This version of Dora is different because she is older and missing a key aspect of her character: the map. The storyline is enthralling and packs enough action to keep your kids entertained. This film will attract a slightly older audience without alienating its die-hard fans. This latest Dora adaptation lives up to the Nickelodeon hype and is fun for the entire family.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Heads of State",
 		"review": "Prime Minister Sam lies awake at night because one of his MI-6 officers, Noël, went missing. He gets up early to meet with his team to prepare for his next press conference with U.S. President Will. Sam detests Will because Will is an actor-turned-politician. Will believes Sam doesn't respect him. They attend the press conference, but Will's harsh words about the U.S. involvement against terrorism strike Sam the wrong way. Their handlers devise an idea for them to eat at a pub together while the press takes pictures. It will give Will credibility while making Sam likable. They agree to do it and board Air Force One to discuss strategy.<br/><br/>After boarding Air Force One, Will and Sam enter a private room. Will spots a plane flying too close and walks to his security for answers. The waiter attacks the security, and Will's bodyguard pulls them to safety. He gives Sam and Will a parachute and provides them with the directions to the safe house. They land safely, but Sam warns Will to destroy his phone. The world thinks they are dead, including their enemies. Will and Sam must arrive at the NATO summit before anyone discovers they are alive.<br/><br/>This action-comedy offers nothing new for the genre, actors, or special effects. Even with the star power, it's not enough to pull your attention away from anything else. The movie offers Bullet Train moments when it gives a comical recap, but provides few laughs. The secondary characters are more entertaining and well-written than the leads. It's no wonder the producers put it on Prime.<br/><br/>2.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Deep Cover",
 		"review": "Hugh acts tough to connect with his co-workers, but fails miserably. After the cool guy overlooks him again, Hugh desires to elevate his confidence. He walks and sees a sign for an improv class. It promotes the class as a way to build your voice.<br/><br/>Marlon auditions for a commercial but overacts the part. His agent, Terry, gets Marlon a job flipping signs, dressed as an elf. Marlon takes the part because he needs the money. He joins an improv class to hone his skills.<br/><br/>Acting teacher Kat applauds as two students complete an improv scene. As she describes the 'yes and' technique, Hugh knocks something over. She calls Hugh to the stage and searches the room for a partner. She sees Marlon standing at the bar and asks him to join Hugh on stage.<br/><br/>Two students told Kat that an agent would be in the audience for their show. Kat takes the stage but doesn't impress him. She drowns her sorrows in alcohol when a man, Billings, approaches her about a job. Agent Billings works for law enforcement and wants to hire improv actors to infiltrate a criminal enterprise. He is willing to pay $200 a day if Kat finds two more actors. She hears Hugh drop something and looks over to Marlon. She convinces them to join the team.<br/><br/>Billings needs them to enter a convenience store that sells counterfeit cigarettes. They must buy it while wearing recording devices and bring it to him. Kat and Marlon enter the store while Hugh waits by the door. However, the actors are too good. The store owner assumes they are drug dealers and takes the trio to his boss, Fly. Fly likes the way Kat does business, so he hires them for a bigger job. Kat, Marlon, and Hugh carefully rush out of Fly's lair and return to Billings. He demands that they stay in and get closer to Fly. These three will go to the underbelly of the criminal world, but will they return alive?<br/><br/>This action-comedy has its highs and lows. The action is enjoyable, but nothing new or groundbreaking for the genre. The comedy will make you lightly chuckle rather than laugh out loud. The script did an impressive job with each character's motivation. Viewers can see themselves as one of the protagonists as they fall into this spiral of madness. This picture is similar to Rough Night or Date Night. The setup is outlandish but gripping. While the film is charming, it isn't worth a Prime subscription.<br/><br/>3 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deepcover.jpeg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deepcover.jpeg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Straw",
-		"review": "Janiyah wakes up her daughter, Aria, and takes Aria to the bathtub. Aria had a seizure and hurt herself in the tub. So, Janiyah wipes her back after removing Aria's science fair project from the tub. Aria informs her mother that she has a 40-dollar lunch debt. Janiyah promises to pay it. Janiyah runs to her car with Aria for work while her landlord screams for the rent. Janiya promises that she will get paid today and bring the rent on her break.<br/><br/>Janiyah starts her shift, and an unruly customer throws a bottle at her feet. Her boss, Richard, yells at Janiyah to clean the mess. Janiyah's okay, but the school calls. They reported Aria's bruises to child services. Richard tells Janiyah to be back in 30 minutes, or he will fire her. Janiyah runs to the Benevolent Bank and Trust to retrieve $40, but the bank won't let her withdraw it. She drives to the school, but it's too late. Child Protective Services takes Aria.<br/><br/>Janiyah drives to work but gets stuck in traffic. She tries to move around and almost hits another vehicle. The man, Oliver, chases her and crashes into her car. Janiyah's car stalls, and Officer Sam asks for her license and registration. Janiyah hands Sam her information, but the registration has expired. Sam impounds Janiyah's automobile, and she walks to work in the rain.<br/><br/>Janiyah sees another woman at her register. She took too long, so Richard fired her. Janiyah asks for her check on Richard's desk, but he says no. She returns home and finds her items, including Aria's medication and school project, on the ground in the pouring rain. She gathers the project and medicine and puts them in a clear backpack.<br/><br/>Janiyah enters Richard's office and begs for her check. He refuses as a man enters. The man reads Janiyah's name tag and calls her by her name. He pulls out a gun and demands the money in the safe. The man grabs Janiyah's bookbag, and she fights back. She snatches the gun and kills the robber. Richard remembers the robber said her name, so he calls the police and tells them she set it up. Janiyah rages, shoots Richard, and takes her blood-stained check from his desk, leaving the money behind.<br/><br/>Janiyah walks to the bank and steps up to Tessa's counter. She asks for her ID, but Janiyah doesn't have it. Tessa won't cash the check. Janiyah pulls out the gun and demands they cash it now. Tessa and the other teller, Rayah, assume she's robbing the bank. They give her money while Tessa presses the silent alarm. Bank manager Nicole informs the police that someone is robbing the bank. Then she sees Aria's project blinking and assumes it's a bomb. She warns the police. The security guard locks the doors as police surround the building.<br/><br/>Detective Kay Raymond and Grimes investigate the robbery and shooting at the grocery store. Kay views something different and familiar. She follows Janiyah's path to the bank and makes contact with Janiyah inside. She must convince Janiyah to come out peacefully before the FBI takes her down. However, Kay has one question. What was Janiyah's last straw?<br/><br/>Tyler Perry is back with a thought-provoking, heart-breaking, and breathtaking movie with a stunning twist. You can feel Janiyah's pain, desperation, and frustration resonate off the screen as she runs into a host of unfortunate circumstances. The picture shows different viewpoints on Janiyah's actions. Some call her ungrateful, but others disagree. It's a debate you will have with others as you watch. This film will captivate you from the beginning, and the end will shock you. It is worth a Netflix subscription.<br/><br/>5 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/straw.jpg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/straw.jpg"
 		}
 	}
 ];
@@ -58,7 +58,24 @@ module.exports = videoOnDemand;
 
 
 
+{
+"option": "1",
+"mtitle": "Dutch III: International Gangster",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/dutchiiiinternationalgangster.jpg",
+	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/dutchiiiinternationalgangster.jpg"	
+}
+}
 
 
+{
+"option": "1",
+"mtitle": "My Oxford Year",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/myoxfordyear.jpg",
+	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/myoxfordyear.jpg"	
+}}
 
 */
