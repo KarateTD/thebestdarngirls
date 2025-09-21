217cdc001eba67fca7e21ba16a63448cb80a5de5
Tiffany Gray <KarateTD@yahoo.com>
2025-09-20 20:32:48 -0400

    2 theater

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,24 @@
 var inTheTheaters = [
   {
     "option": "1",
+    "mtitle": "A Big Bold Beautiful Journey",
+    "review": "David talks to his parents on the phone while they encourage him to open his heart to love. He promises to be open-minded and walks to his car to drive to a wedding. Sadly, he finds a boot on his car. He turns to see a flyer for an emergency car rental service. David calls the company.<br/><br/>David arrives for his car and finds a nearly empty warehouse. It has a table and two employees. They offer him a late 90s Saturn SL and a navigation system. David agrees and drives to the wedding.<br/><br/>David sees a beautiful woman named Sarah across the aisle. She asks him to dance, but he doesn't. They converse and have fun. However, they know dating would be a horrible idea.<br/><br/>The next morning, David gets in his vehicle, and the navigation asks if he wants a big, bold, beautiful journey. He agrees, and the navigation tells him to go to Burger King and order a hamburger. David sits at the restaurant and sees Sarah two tables away. She gets up and sits next to him. Although David wants to continue, they decide to go their separate ways. They walk to the parking lot and realize they have the same car from the same rental company. Of course, Sarah got the navigation system.<br/><br/>David starts his car, and the navigation system sends him on a path to continue his journey. To continue, it tells him to pick up Sarah. David hears a honk and rolls down his window. Her car won't start. Sarah gets in his car and they continue their journey together. It will include mistakes, hurt, and love, but what will they uncover about themselves?<br/><br/>This film is an obvious Oscar grab for the actors and the director. While the message is charming, the delivery is bland. The director shot this film magnificently, the writer has an intriguing premise, and the actors are first-class. Nevertheless, they can't save this boring, self-important film. But rest assured, it will win an award.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/abigboldbeautifuljourney.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/abigboldbeautifuljourney.jpeg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "Him",
+    "review": "Cameron Sr. and Yvette sit with their son, Cameron Cade, to watch their favorite team, The Saviors, compete in the championship game. Their favorite player, Isaiah White, scores the winning touchdown during the LXVII championship while breaking his leg. Cameron Sr. tells his son that Isaiah made the ultimate sacrifice for his team.<br/><br/>Fourteen years later, football recruiters have their eyes on Cameron Jr. as the next great quarterback. Isaiah just received his 8th championship, making him the most-winning quarterback in the league. Rumors circulate that Isaiah wants to retire, and fans believe Cameron will be next in line for the position, until someone attacks Cameron and causes him to have a traumatic brain injury. Cameron's agent, Tom, and his family want to know if Cameron is ready to meet the teams.<br/><br/>Cameron parties when he gets the call from the Saviors. They want him to attend a training camp hosted by Isaiah. If they like Cameron, they will sign him. Cameron accepts the offer and agrees to drive out to the desert for this astounding opportunity. Isaiah assures Cameron that they will learn about the nuances of the game, uncover their camaraderie, and have fun. Cameron will spend six days there, covering topics such as fun, poise, leadership, resilience, vision, and sacrifice. Cameron can't wait to start the first day of his career, but will it be the end of his life?<br/><br/>This thriller contains imagery and hidden symbols to warn the viewers about worshipping false idols, success at all costs, the dangers of CTE, or a combination of all three. It's hard to decipher. You look for the hidden messages within the cinematography and forget the story. This movie has a plot that YouTubers will analyze for years to come. However, that's where the story fails. It tries too hard to be intelligent and thought-provoking. It succeeds in being cringy, funny, and honest. See this movie if the trailers pique your interest.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/him.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/.jpeg"
+    }
+  },
+  {
+    "option": "3",
     "mtitle": "The Long Walk",
     "review": "Raymond Garraty, along with 49 other young men, received a letter from The Major. Out of the applicants from his state, the Major chose him to participate in the long walk. The winner will receive a monetary prize and one wish. Garraty's mother, Ginnie, cries as she delivers her son to the starting point. She begs him to quit, but the deadline was yesterday. He grabs his gear and baseball before hugging Ginnie goodbye.<br/><br/>Garraty enters the gates and sits with the other young men. He meets Peter McVries, a man who wishes to do good with the prize. Stebbins is the strongest of the bunch. Arthur Baker's faith inspires him, and Hank Wang focuses on the untold riches. Gary Barkovitch is a loner, and Collie Parker wants to document the experience. Then, the Major walks onto the tank and explains the rules. They must walk 3 miles per hour with no finish line in sight. If they slow down, they get one warning and have 30 seconds to regroup. The third warning means death. They will face instant death if they run away or step off the pavement. It's walk or die. While others want riches, Garraty has another reason to enter this competition: revenge. Who will win?<br/><br/>Based on the novel The Long Walk by Stephen King, this dystopian thriller shares similarities with the original, but it's not a retelling. However, the film retains the book's impact. The director made a bold choice by using natural lighting and empty surroundings to draw the audience's focus to the contestants. Also, the surroundings mirror their despair. The graphics department didn't shy away from the blood and gore. Unlike the Hunger Games, the Major is not a man of reason. He is unflappable, rigid, and single-minded. Also, the juxtaposition of the final stretch breaks your heart. The winner breaks down in gut-wrenching sobs as the crowd celebrates with fireworks and the national anthem. It's a moment that will stick with you after the film. The studio labeled the film a horror, but it's a warning. You should see this film if you can stomach the unflinching violence.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -9,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "2",
+    "option": "4",
     "mtitle": "Caught Stealing",
     "review": "Bartender Hank closes the bar and meets his lady friend, Yvonne. Hank runs into his neighbor, Russ. Russ learned that his father had a stroke, and now he has to fly to London to be at his bedside. He asks Hank to care for his cat, Buddy. Later, Yvonne gathers the litter box and brings it into Hank's bathroom.<br/><br/>After closing the bar, Hank comes home to see two men, Aleksei and Pavel, knocking on Russ's door. They ask Hank for Russ's location, but he refuses to give it to them. They ruptured his kidney, so a doctor had to remove it. Curious, Hank enters Russ's apartment to search it. He escapes when the mobsters return. Hank calls the police.<br/><br/>Detective Elise Roman interviews Hank about the intruders. She informs Hank that Russ is a drug dealer, working for some hardcore criminals. Elise says the Russians are dangerous, but Hasidic gangsters, Lipa and Shmully, are monsters. She gives him a number in case Hank sees the mobster and gangsters again. Hank enters his bathroom and notices something strange in the litter box. One poop is fake and hides a key. Hank runs to his job with the key in hand. His boss, Paul, convinces Hank to drink. Hank gets drunk and returns home. He pukes on the exterior door, takes off his clothes, and enters his apartment.<br/><br/>Inside, Aleksei and Pavel are waiting for him with mobster Colorado. They want the key and torture Hank for it. Hank tells them it's in the litter box, but it's missing. Hank remembers he took it to the bar. They demand that he obtain the key and not call the cops. If Hank doesn't find the key, they vow to kill him and everyone he loves.<br/><br/>Based on Caught Stealing by Charlie Huston, this film is action-packed and surprisingly witty. The storytelling is interconnected and intelligent. Every object, character, and monologue has a deeper meaning and merges into the conclusion. The director didn't choose fancy angles to narrate the story. Instead, they used New York and baseball as the backdrop. The natural light and languages make the tale feel authentic. This film is for you if you enjoy crime comedies with an enigma.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -18,30 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "3",
+    "option": "5",
     "mtitle": "Relay",
     "review": "Ash watches his client, Hoffman, sit nervously at a diner. Hoffman hands documents to the CEO, takes a picture with him, and exits the restaurant. Ash follows Hoffman through the city and watches him board the train. Ash retreats to his lair, stashes a copy of the documents in his safe, and burns envelopes meant for the press. He attends his AA meeting and listens to new messages from potential clients.<br/><br/>Whistleblower Sarah meets with a lawyer. She discovered that her employer, Cybo Sementis, did not disclose the harmful side effects of the new food additive. When they discovered she had a copy of the email and its contents, the company sent goons to harass her. The lawyer alerts Sarah to legal routes she can take under whistleblower protection laws. However, she wants to return the documents to stop the harassment. The lawyer informs her about a service that deals with this sensitive matter. He gives her a number and tells her to leave a message. If they choose her case, they will call back.<br/><br/>Sarah arrives at a safe house, calls the number, and leaves a message. She gets a call from the Tri-State Relay. The calls aren't recorded and are confidential. She agrees to take the call. Ash accepts her case, but she must follow his directions without question. Any failure on her part will void their deal. She will be a lamb to the slaughter. Sarah agrees, and Ash puts his plan into motion. However, Cybo Sementics won't bow down without a fight.<br/><br/>This movie is a battle of wits with low-tech solutions. Ash doesn't use a hacker or a crew to flush out Sarah's harassers. Instead, he uses the relay service, the post office, and cunning to stay one step ahead. During the opening sequence, you never hear Ash speak. Ash feels like a mythical character. This movie waits for the height of the climax to stun you. It's a long-forgotten plot point that knocks you out of your seat. The slick presentation pacifies you into a sense of relaxation, so you don't see what the writer is preparing. This moral dilemma is a highly entertaining and captivating spy thriller.<br/><br/>4 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/relay.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/relay.jpeg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Nobody 2",
-    "review": "Hutch sits in a police station with a dog beside him. He ignites a lighter and uses it to cauterize his wound. The police demand to know who they are. Hutch takes a deep breath and ponders his response. To the outside world, Hutch is married to real estate agent Becca. They raise two children, Sammy and Brady, in a happy and loving environment. However, the underworld knows Hutch as an accountant. At home, Hutch is a man who puts work over family. Becca misses him at dinner time and informs Hutch. She mentions the kids' upcoming summer vacation. Hutch promises to plan everything.<br/><br/>Days later, Hutch shows the family an old video of Plummersville. Hutch has fond memories of that place because it was the only time his father, David, took him and his brother, Harry, on a trip.<br/><br/>Hutch and his family play games at an arcade. Brady gives his extra tickets to a pretty girl and the rest to his sister. Sammy trades her tickets for a badger as Max approaches. He accuses Brady of flirting with his girlfriend, steals Sammy's badger, and rips off its head. As they exit, one of the guards hits Sammy in the back of the head. With a ruse, Hutch reenters the arcade without his family and takes out the guards. The police arrest him and Brady.<br/><br/>The family waits at the police station for Abe. Abe brings Hutch to his office and stands at the corner. Wyatt sits at Abe's desk and states that he runs this town. He demands that Hutch leave and never return. Hutch agrees but stays. Abe's goons spot Hutch and his family at the water park.  Wyatt orders Abe to rid the town of Hutch. He doesn't know who Hutch is, but he understands that Hutch will bring problems to Lendina's business. Lendina is a ruthless bootlegger who will kill anyone who disrupts her cash flow, including Wyatt. Hutch won't budge because he and his family need this time together. No one will ruin this nobody's vacation.<br/><br/>This sequel doesn't follow the action genre pattern. It doesn't put Hutch in a new, exotic location to go on the hunt. Instead, he travels to a small town for family time. This charming and dry comedy features the original cast. It has more explosions, blood, and wall-to-wall action. The fight sequences combine John Wick with Home Alone. The director found exciting and cringy ways to include a funhouse, waterslide, and ball pit. And Lendina is a quirky villain who lives on bloodlust. This film adds to the franchise, not by expanding the world, but by enlightening the audience to Hutch's family. This film is an entertaining, mindless good time.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/nobody2.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/nobody2.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Freakier Friday",
-    "review": "Talent manager Anna bangs on her daughter's door before school. However, Harper won't open the door. Anna's mother, Tess, calls to offer Harper a ride to school. Anna wants to give Harper a ride and tells Tess to relax. Therapist Tess hangs up, finishes her podcast, and kisses her husband, Ryan, goodbye. Finally, Anna opens the door and finds Harper's note. Harper started her morning with surfing.<br/><br/>In the car, Harper groans about her chemistry partner, Lily. Harper thinks Lily is obnoxious. Tess and Anna advise Harper to keep an open mind before dropping Harper off at school. That treaty is non-existent. Lily and Harper argue during chemistry class, causing a foam explosion. The principal calls Anna and Lily's dad, Chef Eric, into school. They run into each other in the halls and fall in love. Six months later, they are engaged.<br/><br/>Lily and Harper disagree during a school bake sale, triggering a school-wide food fight. The tension in the home is rising between Harper and Lily. Anna tells them that they will attend her bachelorette party and get along.<br/><br/>At the bachelorette party, a multi-hyphenate psychic, Madame Jen, offers to give Anna and Tess a reading. Lily and Harper argue in the corner, and Madame Jen interrupts them. Madame Jen tells them, 'Change the hearts you know are wrong, so that you may find where you belong.' Harper thinks she belongs in LA and Lily London. The four feel an earthquake. Tess and Anna try to ignore it.<br/><br/>Harper, Tess, Lily, and Anna return to Tess's home. They make Lily and Harper sleep in the living room to force them to be friendly. They sleep through the night. When Harper wakes up, her face and hands feel dry. Tess hears her daughter's yell and wakes on the couch. She notices her bones feel better. Tess and Lily switched bodies, like Anna and Harper. They ask Harper and Lily if Madame June said anything to break the curse. They lie and say no. Lily and Harper will use the swap to break up their parents' wedding in a few days. Will they succeed and figure out a way to swap back?<br/><br/>Sound the alarm! This sequel respects the original while being a multigenerational, heartfelt, funny film. Like the first movie, the script features a wedding on the line, with a new husband, but changes by adding a relocation and Lily's deceased mother. For the die-hard fans, everyone returns. The Pink Slips, Harry, and Mama P make a satisfying comeback for the fans. The writers hide easter eggs with the soundtrack, decor, jokes, and food. Freakier Friday is a comedy with great timing and physicality. It celebrates complicated family dynamics without muting anyone's perspectives or feelings. You will leave this movie as you did the first, feeling seen and seeing. This film is a sequel that's worth seeing in theaters and is worth the wait.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/freakierfriday.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/freakierfriday.jpeg"
     }
   }
 ];
@@ -58,24 +58,7 @@ module.exports = inTheTheaters;
 /*
 
 
-{
-"option": "1",
-"mtitle": "A Big Bold Beautiful Journey",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/abigboldbeautifuljourney.jpeg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/abigboldbeautifuljourney.jpeg"
-}
-},
-{
-"option": "2",
-"mtitle": "Him",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/him.jpeg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/.jpeg"
-}
-}
+
 
 
 */
