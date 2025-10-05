cbe06569754becd83f2946b6913a7dccbd956d41
Tiffany Gray <KarateTD@yahoo.com>
2025-10-04 23:27:08 -0400

    tv

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Surviving My Father: The Rachel Jeffs Story",
+    "review": "Rachel Jeffs sits with her husband, Richard, for the first time. He's ready to explain how a man and a wife make a baby, but she already knows. This knowledge perplexes Richard because the church bans women from knowing until their wedding day. She knows because she suffered sexual abuse at the hands of her father, Warren Jeffs, for eight years. Richard runs to Warren, and he begs Richard for forgiveness. The two men promise never to speak of this again and shake hands. Rachel believes she has found the lord's grace and can live peacefully. Her devotion to the prophet will sink her deeper into the hands of the devil. Can Rachel escape?<br/><br/>Based on the memoir Breaking Free: How I Escaped Polygamy, the FLDS Cult, and My Father Warren Jeffs by Rachel Jeffs, this film makes you want to throw your remote at the television. The costume department carefully studied the women's clothing. The vanity department gave the actress a signature plain look with big hair. You will wonder why Rachel doesn't walk away. Viewers must remember the control these men enforced on the women. They stole their children, individuality, and faith. The actresses will rip your soul apart as they cry and beg for their kids. However, your blood will boil as they blame their sins for the abuse they suffer. This film will evoke every emotion. This movie is a must-watch, but only with a compassionate heart.<br/><br/>5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/survivingmyfatherracheljeffs.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/survivingmyfatherracheljeffs.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "While They Were Sleeping",
+    "review": "Detectives Rachel and Adam arrive at the home of William and Zoe. Zoe called 911. She woke up and found William dead with a gunshot wound to his head. Rachel interviews Zoe and discovers that Zoe is William's second wife, and they got custody of William's daughter, Jess, from his first marriage to Robin. Rachel and Adam examine the crime scene. They deduce that the culprit was familiar with the layout and entry points. She questions Zoe about William's enemies and competitors, but Zoe says William had none.<br/><br/>Rachel enters Robin's home. Robin tells Rachel she lost her daughter because of her drug addiction after a car accident. On the night of William's murder, Robin was at a bar with her new boyfriend, Dean. They stayed until the last round. Then, Rachel unearths a life insurance policy with Zoe as the beneficiary. Zoe swears she didn't know about the policy, but her signature is on it. Zoe explains that William signed her name to documents. Rachel doesn't believe her. She thinks Zoe killed her husband for the insurance. But can she prove it without solid evidence?<br/><br/>This movie starts with an intriguing mystery that's Dateline-worthy. A woman wakes up next to her dead husband after sleeping through the gunshot. She has no residue on her hands, but the killer knew the home. The writers took a step back and allowed the audience to uncover the culprit with Zoe. While the characters turn on her, the audience knows she's the easy suspect. You will create several suspects and motives. Let's say that yours is better than the conclusion. It's a letdown for a steady climax. Hopefully, this 2025 Shocktober kickoff isn't a sign of the season to come.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/whiletheyweresleeping.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/whiletheyweresleeping.jpg"
+    }
+  },
+  {
+    "option": "3",
+    "mtitle": "Adventures in Love and Birding",
+    "review": "Celeste escapes her nosy co-workers with her best friend Eva. Eva wants Celeste to step out into the world and be more adventurous. Since her divorce, Celeste hasn't thought about love. Eva receives a call that an opportunity has opened up, but she promised her friend, John, that she would help him with his birding competition. Then, she gets a bright idea. Celeste will join him and get out in the world. John attended these competitions with his ex-girlfriend, Breena, and they won for three years. Breena cheated on him with her now partner, Lazlo. As Celeste asks questions, Eva takes another phone call. Celeste inquires if this is a fake girlfriend setup. Eva replies Yes, but to the person on the phone. Celeste agrees.<br/><br/>Celeste arrives at the competition first and meets President Linda. She tells Linda that she's John's partner, and Breena overhears. John greets everyone, and Celeste embraces him with an overly dramatic hug. She tells everyone she's John's new girlfriend and partner. John hides his confusion until they are alone. He explains that Eva was only going to be his partner, not a pretend girlfriend. However, they decide to keep up the lie. John informs Celeste that he must win this competition to beat his ex-girlfriend and start a bird-watching business. Celeste will use her time to be adventurous in birding. But will she gain a passion for love?<br/><br/>This film has what you would expect from a Hallmark film. You have a competition, two people working together, and a unique hobby. The love story is predictable, but the setup isn't. There is a bonus storyline about Celeste and her college-bound daughter, Morgan. This subplot will stun Hallmarkies more than the main one. The actors did their best to present compelling characters, but your interest will wane after the 2nd act.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/adventuresinloveandbirding.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/adventuresinloveandbirding.jpg"
+    }
+  },
+  {
+    "option": "4",
     "mtitle": "I Was a Child Bride: The Courtney Stodden Story",
     "review": "Courtney Stodden cries to her mother, Krista, because she's the victim of bullying. Krista decides to pull Courtney out of traditional school. She can homeschool Courtney and give Courtney the chance to pursue their dream of being famous. Krista seeks a talent manager for guidance. Krista's friend, Tim, recommends Doug Hutchison. Doug has a program called The Art of Stillness. Krista jumps at the opportunity to have Doug coach Courtney. Krista's need for fame and fortune will send Courtney into a tailspin of grooming, abuse, assault, and drugs while being fodder for tabloids and public ridicule.<br/><br/>This film shows that Courtney was a lamb to the slaughter. Krista and Courtney's father, Alex, made it easy for Doug to clean the bones. Her religion lulled her into compliance. Courtney's film explains the abandonment and devastation she faced at home and in public. She was shamed online and abused in her relationship. This movie is a cautionary tale of grooming, vicarious parents, and the dark side of fame. While Courtney forgives her parents, Lifetimers won't. Hopefully, this script will spark a conversation about child brides and their portrayal in the industry.<br/><br/>4 out of 5 stars.",
     "image": {
@@ -9,39 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "5",
     "mtitle": "Deadly Desire",
     "review": "Nurse Brittney prepares to have dinner with her boyfriend, James, James's friend, Ben, and Ben's new friend, photographer Tanya. After dinner, Tanya enters the bathroom with Brittney, making her feel uncomfortable. Brittney doesn't plan on seeing Tanya again, but James invited them for game night.<br/><br/>During game night, Ben is rude to Tanya, and it rubs Brittney the wrong way. Tanya calls Brittney to spend girl time together and release the tension between them. Brittney gives Tanya a second chance and visits Tanya's home. Tanya convinces Brittney to let her take a few photos. Brittney thinks this will be harmless fun between them. However, Tanya will use these images to dismantle Brittney's life, all for revenge. Will Brittney survive with her life intact?<br/><br/>This film is a weird and convoluted waste of time. The plot is all over the place with zero substance. The actors did the best they could with an unbearable script. And sadly, the conclusion hints at a part two. The writers shouldn't waste Lifetimer's time. Skip this film at all costs.<br/><br/>2 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deadlydesire.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deadlydesire.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "No One Believed Me",
-    "review": "Jane's sister, Amy, called Jane upset. Jane's husband, Dan, warns Jane not to fall into Amy's dramatics. Dan believes this call is another cry for attention. He begs Jane to remain vigilant.<br/><br/>Amy arrives at Jane's home with a police officer. Amy told the police that someone had kidnapped her, but she never saw his face or got a license plate. Without evidence, the police don't offer much help. Jane sits with Amy, but Amy can't recall any information. She relays that to Dan, but Dan remains guarded. Amy overhears their conversation and calls a local newspaper to run her story.<br/><br/>Amy meets reporter Tim at her salon. His questions start easy but become increasingly hostile. She runs, and Tim gives chase. Amy bumps into Paul, and Paul scares Tim away. They exchange information and become close. Amy tries to get Tim fired from the paper, but they don't know who Tim is. Amy runs to the police and tells them that Tim is her kidnapper. But will they believe her?<br/><br/>Viewers must watch this film from the start. It shows Amy as the unknown woman running through the woods with her hands bound. So, Lifetimers know Amy's story is real. But we don't know whom she can trust because her kidnapper is out there with no one looking for them. The writers didn't wait until the end to reveal the culprit. However, the motive remains a horrific mystery until the final act. The acting is a bit forced at times, but it doesn't overpower the writer's original intent.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/noonebelievedme.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/noonebelievedme.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "The Groomsmen Last Dance",
-    "review": "Jackson dances with his daughter, Betty, while his friend, Danny, dances with his steady plus-one, Zack, and his other friend, Pete, sneaks away with Chelsea. Jackson looks forward to his time with Betty, but she accidentally mentions that they are moving. Her mother, Tricia, got a job in Italy. Jackson supports the relocation with sadness.<br/><br/>Months later, Pete and Danny marry Chelsea and Zack, respectively. Tricia calls with a request. Her job wants her to travel to Africa for work. Jackson says yes to taking care of Betty before she has a chance to ask. However, she expresses regret for taking Betty out of school. Jackson compromises and flies to Italy.<br/><br/>Jackson talks on the phone with a co-worker and almost hits Gabriella. They blame each other for the near fatality, and she walks to her mother's restaurant with paperwork. She gives her mother the papers to merge the restaurant and her father's vineyard. Her mother is willing to sign it, but she knows her father never would.<br/><br/>Jackson arrives at Tricia's home and gets information about Betty's school and the house. She gives him the landlord's number before exiting for Africa. A few days later, Jackson decides to walk Betty home from school. He sees a restaurant on his way there and enters for a cappuccino. Gabriella rolls her eyes at the rude American.<br/><br/>When Jackson arrives, Betty hides her embarrassment. She doesn't want Jackson to treat her like a child, but doesn't know how to broach the subject. When they arrive at home, Jackson tries to cook but can't get the stove to work. So, he calls the landlord. To his surprise, Gabriella knocks on his door. Betty sees the attraction between them and believes Gabriella is the best distraction for her dad. She will scheme to get them together, but will it become a relationship?<br/><br/>This third installment of The Groomsmen franchise is charming like the others, but not as funny. It has the classic Hallmark meet-cute story as Jackson and Gabriella work together to help save her family's businesses. The runaway bride is still telling the story in her hideout. However, we find out who she is and why she's running. This circular storytelling will put a smile on your face. It's magnificent to see this level of camaraderie between men without violence or degrading women. These three men listen, love, and speak openly and candidly without judgment. With the right script, this story can continue as The Wedding Veil series did.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thegroomsmenlastdance.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thegroomsmenlastdance.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Girl Who Survived: The Alina Thompson Story",
-    "review": "Alina Thompson hides her modeling dreams from her parents, Carl and Nancy. Alina and her friend, Natalie, want a modeling contract and a life in Paris. Alina tells Natalie about a local Fresh Face contest. They can get free headshots from photographers and win $500. Natalie agrees to go.<br/><br/>Alina lies to her parents and rides her bike to the library with Natalie. They catch the bus and attend the contest. A photographer, Bill, takes shots of Natalie and requests that Alina join in. Soon, he only wants pictures of Alina. He asks Alina for her phone number to get more images in a better location, but she refuses.<br/><br/>Later, the contest host calls Nancy and tells her that Alina won. Nancy informs Alina at the dinner table with Carl. She wants to attend the next meetup and obtain her prize money. At first, her parents say no. However, they change their minds, but with a caveat. They must attend with her. Alina huffs and agrees.<br/><br/>Carl, Nancy, and Alina attend the Fresh Faces event. Bill sees Alina again and asks for more photos. Carl keeps close watch on her. Carl wants Alina to succeed, but something about the event is off. He sits his daughter down and tells her not to succumb to peer pressure from men. Nancy believes he will make Alina paranoid. However, Alina will learn that he is giving her the tools to survive a predator with his sights on her.<br/><br/>Thank you Lifetime. We finally have an involved, nurturing, and loving father in a Lifetime film. The script makes Alina appear stubborn, but she's young. Lifetimers must remember this as they watch. The film has pearl-clutching moments as Alina is nearly in Bill's grip, but Dad comes to the rescue. The costume and makeup department didn't do enough research on the era. That doesn't matter because the film's messages are phenomenal: listen to your parents, a little fear is healthy, and no is a complete sentence. This movie is a teaching tool you never knew you needed.<br/><br/>5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thegirlwhosurvivedalinathompson.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thegirlwhosurvivedalinathompson.jpg"
     }
   }
 ];
@@ -58,28 +58,49 @@ module.exports = madeForTV;
 /*
 
 {
-"option":"1",
-"mtitle":"Surviving My Father: The Rachel Jeffs Story",
-"review":"<br/><br/>* out of 5 stars.",
+"option": "1",
+"mtitle": "Monster in the Family: The Stacy Kananen Story",
+"review": "<br/><br/>* out of 5 stars.",
 "image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/survivingmyfatherracheljeffs.jpg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/survivingmyfatherracheljeffs.jpg"
-}},
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/monsterinthefamilystacykananen.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/monsterinthefamilystacykananen.jpg"
+  } 
+},
 {
-"option":"2",
-"mtitle":"While They Were Sleeping",
-"review":"<br/><br/>* out of 5 stars.",
+"option": "2",
+"mtitle": "The Paradise Murders",
+"review": "<br/><br/>* out of 5 stars.",
 "image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/.jpg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/.jpg"
-}},
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/theparadisemurders.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/theparadisemurders.jpg"
+  } 
+},
 {
-"option":"3",
-"mtitle":"Adventures in Love and Birding",
-"review":"<br/><br/>* out of 5 stars.",
+"option": "3",
+"mtitle": "The Jane Mysteries: Too Much to Lose",
+"review": "<br/><br/>* out of 5 stars.",
 "image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/adventuresinloveandbirding.jpg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/adventuresinloveandbirding.jpg"
-}}
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thejanemysteriestoomuchtolose.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thejanemysteriestoomuchtolose.jpg"
+  } 
+},
+{
+"option": "4",
+"mtitle": "The Little Girl Who Lived",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thelittlegirlwholived.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thelittlegirlwholived.jpg"
+  } 
+},
+{
+"option": "5",
+"mtitle": "Heartstrings Attached",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/heartstringsattached.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/heartstringsattached.jpg"
+  } 
+}
 
 */
