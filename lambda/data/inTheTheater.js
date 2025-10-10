79b3aabdc618bd982a568a8b0bb12f72cf59ca79
Tiffany Gray <KarateTD@yahoo.com>
2025-10-09 23:50:07 -0400

    tron

--- lambda/data/inTheTheater.js
+++ lambda/data/inTheTheater.js
@@ -1,6 +1,15 @@
 var inTheTheaters = [
   {
     "option": "1",
+    "mtitlte": "Tron: Ares",
+    "review": "Julian Dillinger took over Dillinger Systems, founded by his grandfather, Ed Dillinger, after his mother, Elisabeth, stepped down. Today, Julian presents a revolutionary program. With lasers, he can shape any hardware the customer needs. If they can create the hardware, why not the human to control it? He presents Ares. A laser-printed, human-like AI program that follows commands. Julian and Elisabeth discuss the program after the potential buyers leave. She fears that Julian is moving too fast because the program can only live in the real world for 29 minutes. Julian is working on the permanence code, which will ensure they live until someone deactivates them. Elisabeth knows Julian isn't near his fix, but his biggest competitor, Eve, may be.<br/><br/>Eve Kim is the surviving CEO of ENCOM. She once headed the company with her sister, Tess, after their founder, Kevin Flynn, disappeared. Eve and her employee, Seth, search the most remote and coldest place in the world for Tess's off-site lab. Tess stored Kevin's old floppy disks there. Eve inserts each disk and examines its contents. She finds the permanence code.<br/><br/>Julian watches the news report about Eve's absence during the launch of ENCOM's latest game. With Eve and Ajay preoccupied, Julian hacks ENCOM's server for the permanence code. He sends Ares, Athena, and more soldiers to the ENCOM headquarters. He watches Eve's videos, reads her text messages, and scans articles about her. He starts to feel something. Ares wants to locate Eva, but only to keep the code for himself. Ares wants to live beyond the grid, but Julian doesn't plan to lose his biggest asset anytime soon.<br/><br/>This iteration of Tron has amazing costumes and stunning backdrops. The plot is intriguing because the writers secretly based it on Frankenstein's monster. The backing track is exciting and fresh, and the film has easter eggs for fans of the video game and the original picture. It is visually dynamic with a compelling script. However, the film drags. The fight scenes will bore you after 2 minutes. The film has one mid-credit scene for die-hard fans. You should see this film on a regular screen at the matinee price if you are a fan of the franchise.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/tronares.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/tronares.jpg"
+    }
+  },
+  {
+    "option": "2",
     "mtitle": "The Smashing Machine",
     "review": "Michael Kerr stands in front of a fighting audience for the first time in Brazil. To everyone's surprise, he wins his first match in a gauntlet-style face-off against a slew of professional fighters. He calmly accepts his prize with humility and grace. He returns home to his girlfriend, Dawn, but can't ignore his nagging pains.<br/><br/>Two years later, Michael battles in the Pride 7 Championship in Japan with his friend, Michael Coleman, and trainer, Bas, in his corner. Michael battles Igor Vovchanchyn, and Igor knees Michael in the head. But the referee doesn't call the foul. Instead, he hands Michael his first loss. Later, the officials change the referee's decision to a no-contest ruling. While this fixes the history books, it breaks Michael. He spirals out of control personally and professionally. He will lose it all to gain himself.<br/><br/>Based on the documentary 'The Smashing Machine,' this film adopts a documentary-style approach, featuring shaky cameras, confessionals, and in-ring grit. That perspective switches when Michael and Dawn are alone. The writers help the audience see this imposing figure as a gentle giant. The choreographers held nothing back with the fight scenes. They will make you flinch and cover your eyes. The lead made a stunning transformation to become Michael Kerr and proved his acting abilities. It's worth seeing in theaters, but you won't want to buy it on DVD.<br/><br/>3.5 out of 5 stars.",
     "image": {
@@ -9,7 +18,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "2",
+    "option": "3",
     "mtitle": "One Battle After Another",
     "review": "Rocketman joins other members of the French 75 outside of an immigration center. Perfidia sees him and asks what's in his wagon. He explains that it's explosive, and she's intrigued. As Rocketman sets the explosives, other members gather the immigrants and detain the soldiers. Perfidia distracts Captain Lockjaw. Her take-charge attitude arouses him as she steals his hat and gun. She, the team, and the immigrants exit the center, using Rocketman's explosions as a distraction.<br/><br/>The French 45 set up political attacks everywhere, but don't know that Lockjaw has fixated on Perfidia. He follows them on their next mission and corners her in a bathroom. He offers to let her go if she meets him at a hotel with his gun and hat. If not, he will expose her crew. She slithers away and completes the mission. Then, she joins Lockjaw in a hotel room and leaves sans hat and gun.<br/><br/>Eight months later, Perfidia is pregnant, and Rocketman worries that she is ignoring it. After their daughter's birth, Perfidia becomes jealous of the time Rocketman spends with their little girl, Charlene. During a grocery run, Lockjaw stands behind Rocketman and asks about the baby. He states it's a girl and biracial.<br/><br/>Perfidia gathers a team to rob a bank to fund the French 75. During the robbery, Perfidia shoots and kills a security officer. The police catch and detain her. Lockjaw gives her an option: give up the crew or die in jail. Perfidia turns state's evidence and enters the witness protection program. The French 75 hears about Perfidia's capture and runs to protect Rocketman and Charlene. They give him a trust device, new identities as Bob and Willa, and a nearby sanctuary city. Lockjaw returns to Perfidia after the roundup, but she's fled to Mexico.<br/><br/>Sixteen years later, a man approaches Colonel Lockjaw with an invitation to an exclusive club, the Christmas Adventurers. They are a group that believes in strong borders, family values, and purity. Then, they inquire about interracial relationships. Lockjaw says he has only slept with women of his race. They inform him that they will conduct a double Yankee background check into his past. Lockjaw leaves with his head full of pride, but he has one problem: Charlene. She could be evidence of his night with Perfidia. He sends the army to locate her and bring her to him.<br/><br/>Willa attends high school and isn't allowed to have a phone. Bob hasn't disclosed much about his past, but tells Willa her mom was a hero who fought for everyone's freedom. Willa attends a school dance and goes to the bathroom. Her trust device starts to make noise, and Deandra gives Willa the passphrase. Under Deandra's instructions, Willa leaves the party with her. Bob receives a phone call that the army captured a former member looking for Willa. Bob, in a stupor, enters a tunnel, grabs his gear, and runs to locate Willa. He must keep her safe from Lockjaw before Lockjaw exposes the truth about him, her mother, and Bob. Can Bob save Willa?<br/><br/>Inspired by Viceland by Thomas Pynchon, this film begins as a revolutionary heist film and changes to a game of chess. The director didn't concentrate on title cards or clothing to give the film's period. It's left up to the viewer to decide. The writers centered on the mission while illuminating issues such as immigration, the undocumented workforce, history, and lineage. The natural lighting and close-up allowed the cinematography to maintain its gritty edge. The script is sharp-witted, charming, and edgy. This film makes you laugh and flinch. However, the writers don't want to change ideals or force policy. They focus on telling a compelling story that captivates from start to finish.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -18,7 +27,7 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "3",
+    "option": "4",
     "mtitle": "A Big Bold Beautiful Journey",
     "review": "David talks to his parents on the phone while they encourage him to open his heart to love. He promises to be open-minded and walks to his car to drive to a wedding. Sadly, he finds a boot on his car. He turns to see a flyer for an emergency car rental service. David calls the company.<br/><br/>David arrives for his car and finds a nearly empty warehouse. It has a table and two employees. They offer him a late 90s Saturn SL and a navigation system. David agrees and drives to the wedding.<br/><br/>David sees a beautiful woman named Sarah across the aisle. She asks him to dance, but he doesn't. They converse and have fun. However, they know dating would be a horrible idea.<br/><br/>The next morning, David gets in his vehicle, and the navigation asks if he wants a big, bold, beautiful journey. He agrees, and the navigation tells him to go to Burger King and order a hamburger. David sits at the restaurant and sees Sarah two tables away. She gets up and sits next to him. Although David wants to continue, they decide to go their separate ways. They walk to the parking lot and realize they have the same car from the same rental company. Of course, Sarah got the navigation system.<br/><br/>David starts his car, and the navigation system sends him on a path to continue his journey. To continue, it tells him to pick up Sarah. David hears a honk and rolls down his window. Her car won't start. Sarah gets in his car and they continue their journey together. It will include mistakes, hurt, and love, but what will they uncover about themselves?<br/><br/>This film is an obvious Oscar grab for the actors and the director. While the message is charming, the delivery is bland. The director shot this film magnificently, the writer has an intriguing premise, and the actors are first-class. Nevertheless, they can't save this boring, self-important film. But rest assured, it will win an award.<br/><br/>3 out of 5 stars.",
     "image": {
@@ -27,21 +36,12 @@ var inTheTheaters = [
     }
   },
   {
-    "option": "4",
+    "option": "5",
     "mtitle": "Him",
     "review": "Cameron Sr. and Yvette sit with their son, Cameron Cade, to watch their favorite team, The Saviors, compete in the championship game. Their favorite player, Isaiah White, scores the winning touchdown during the LXVII championship while breaking his leg. Cameron Sr. tells his son that Isaiah made the ultimate sacrifice for his team.<br/><br/>Fourteen years later, football recruiters have their eyes on Cameron Jr. as the next great quarterback. Isaiah just received his 8th championship, making him the most-winning quarterback in the league. Rumors circulate that Isaiah wants to retire, and fans believe Cameron will be next in line for the position, until someone attacks Cameron and causes him to have a traumatic brain injury. Cameron's agent, Tom, and his family want to know if Cameron is ready to meet the teams.<br/><br/>Cameron parties when he gets the call from the Saviors. They want him to attend a training camp hosted by Isaiah. If they like Cameron, they will sign him. Cameron accepts the offer and agrees to drive out to the desert for this astounding opportunity. Isaiah assures Cameron that they will learn about the nuances of the game, uncover their camaraderie, and have fun. Cameron will spend six days there, covering topics such as fun, poise, leadership, resilience, vision, and sacrifice. Cameron can't wait to start the first day of his career, but will it be the end of his life?<br/><br/>This thriller contains imagery and hidden symbols to warn the viewers about worshipping false idols, success at all costs, the dangers of CTE, or a combination of all three. It's hard to decipher. You look for the hidden messages within the cinematography and forget the story. This movie has a plot that YouTubers will analyze for years to come. However, that's where the story fails. It tries too hard to be intelligent and thought-provoking. It succeeds in being cringy, funny, and honest. See this movie if the trailers pique your interest.<br/><br/>3.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/him.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/him.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Long Walk",
-    "review": "Raymond Garraty, along with 49 other young men, received a letter from The Major. Out of the applicants from his state, the Major chose him to participate in the long walk. The winner will receive a monetary prize and one wish. Garraty's mother, Ginnie, cries as she delivers her son to the starting point. She begs him to quit, but the deadline was yesterday. He grabs his gear and baseball before hugging Ginnie goodbye.<br/><br/>Garraty enters the gates and sits with the other young men. He meets Peter McVries, a man who wishes to do good with the prize. Stebbins is the strongest of the bunch. Arthur Baker's faith inspires him, and Hank Wang focuses on the untold riches. Gary Barkovitch is a loner, and Collie Parker wants to document the experience. Then, the Major walks onto the tank and explains the rules. They must walk 3 miles per hour with no finish line in sight. If they slow down, they get one warning and have 30 seconds to regroup. The third warning means death. They will face instant death if they run away or step off the pavement. It's walk or die. While others want riches, Garraty has another reason to enter this competition: revenge. Who will win?<br/><br/>Based on the novel The Long Walk by Stephen King, this dystopian thriller shares similarities with the original, but it's not a retelling. However, the film retains the book's impact. The director made a bold choice by using natural lighting and empty surroundings to draw the audience's focus to the contestants. Also, the surroundings mirror their despair. The graphics department didn't shy away from the blood and gore. Unlike the Hunger Games, the Major is not a man of reason. He is unflappable, rigid, and single-minded. Also, the juxtaposition of the final stretch breaks your heart. The winner breaks down in gut-wrenching sobs as the crowd celebrates with fireworks and the national anthem. It's a moment that will stick with you after the film. The studio labeled the film a horror, but it's a warning. You should see this film if you can stomach the unflinching violence.<br/><br/>4.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thelongwalk.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thelongwalk.jpg"
     }
   }
 ];
@@ -59,7 +59,23 @@ module.exports = inTheTheaters;
 
 
 
-
-
+{
+"option": "1",
+"mtitle": "Roofman",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/roofman.jpeg",
+"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/roofman.jpeg"
+}
+},
+{
+"option": "2",
+"mtitle": "Kiss of the Spider Woman",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/kissofthespiderwoman.jpg",
+"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/kissofthespiderwoman.jpg"
+}
+}
 */
 
