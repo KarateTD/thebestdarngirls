f80c4c68f42dfd0d9b9cc2df51beac1a89b9f0e1
Tiffany Gray <KarateTD@yahoo.com>
2025-12-28 21:05:52 -0500

    5 tv movies

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -141,7 +141,7 @@ module.exports = mustBuy;
     "mtitle": "The Running Man",
     "review": "Ben asks his old boss, Richard, to remove him from the blacklist. Ben can't find work with the scarlet letter on his record, and his daughter, Cathy, is sick. However, Richard won't change his mind because Ben spoke to union leaders about his working conditions. Ben will remain on the list.<br/><br/>Ben watches game shows with Cathy until his wife, Sheila, returns home. She wants to spend time with Cathy, but changes her mind after feeling Cathy's fever. He hates that she has to flirt with wealthy men for tips, especially after one client killed her co-worker. He gets an idea. He will go to the network building and be a contestant on one of their shows. Sheila rejects the idea because she fears he will try out for The Running Man. Ben assures her he would never try out for that game before leaving.<br/><br/>Ben walks through the gates of the network building. They pull up his work history and tell him to go to elevator 26. Hopeful contestants, Jenni and Tim, greet Ben. The Network showrunners perform physical and psychological tests on the contestants. To Ben's surprise, they picked him to be on the Running Man. Ben tries to force them to change, but they refuse.<br/><br/>Ben meets with the Network owner, producer Dan. Dan shows Ben the lucrative contract. He gets a bonus for every day he remains alive and a $500 signing bonus. Ben signs the contract and assures Dan he will be the first contestant to go the distance.<br/><br/>Dan introduces Ben, Tim, and Jenni to the show's hosts, Bobby T.. Bobby gives a quick hello before running on the stage. Ben rages at the false narrative Bobby spins to make the fans hate him. Then, Bobby gives the rules. The runners will have a 12-hour head start. The further they go, the more money they will make. They have to film themselves every day for 10 minutes and drop it in a mailbox. If they don't, they won't get the money, but the hunters will still come after them. They have to last 30 days. Ultimately, they will receive $1 billion. Will he survive the Running Man and make it home to his family?<br/><br/>Based on The Running Man by Steven King, this film has a spectacular pace, wild stunts, and a fresh twist on a familiar plot. This film isn't just about a man running for his life. It's a searing indictment of media manipulation by corporations. The director uses different camera techniques to capture Ben's runs. It could be a steady drone camera shot, a GoPro-style shot, a handheld camera shot, or standard movie framing. These changes display an enticing view of Ben and his run. The soundtrack stays in step with Ben's speed and gets you excited. The action slows with a purpose. It speaks to corruption, pollution, and screen addiction. The plot delivers these messages without getting too topic-heavy or self-righteous. This film is an exciting and violent ride.<br/><br/>4.5 out of 5 stars.",
     "asin": {
-      "re": ""
+      "re": "B0FZM4QCH2"
     },
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/therunningman.jpeg",
@@ -159,6 +159,45 @@ module.exports = mustBuy;
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/sarahsoil.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/sarahsoil.jpeg"
+    }
+  }
+
+  {
+    "option": "1",
+    "mtitle": "The Housemaid",
+    "review": "Millie arrives at the Winchester Estate, and Nina Winchester greets Millie with a smile and hor d'oeuvres. Nina examines Millie's résumé and qualifications. She reiterates that the position is a live-in maid. Nina confesses that she's expecting but hasn't told her husband, Andrew, or daughter, CeCe. Millie thinks everything is wonderful. Nina expresses that she has more applicants to interview before choosing a housemaid. But she will call Millie back soon.<br/><br/>Millie drives away, removes her glasses, and rolls her eyes. She never expects to hear from Nina again. Millie is on probation for 5 years after serving 10 years in prison. She knows one background check will rule her out. Millie takes showers in gas station bathrooms, applies for any job, and sleeps in her car. When her phone rings, it's Nina offering Millie the job. She wants Millie to start as soon as possible, and Millie is willing to do so this afternoon. Millie thinks her prayer has been answered. She has a steady job, a paycheck, and a place to live. Her probation officer will be pleased. However, Millie has no idea the danger lurking behind the iron gate.<br/><br/>Based on The Housemaid by Freida McFadden, this film is a wild ride that plays with motifs, surroundings, and wardrobe to convey deeper meanings. The house is void of color except for one bathroom with snake wallpaper. Nina always wears a hue of white until the telling ending. She's seen as clean, perfect, and pure. Millie and Andrew stain on the screen as they wear darker wardrobes. The different angles show the power dynamic shift throughout the film. It will leave you dizzy. Also, the split screens and overlays of different people experiencing the same turmoil are jaw-dropping. This film will make you want to read the book and compare it. It's pragmatic, cultural, and surprising.<br/><br/>4.5 out of 5 stars.",
+    "asin": {
+      "re": "B0G3CNZSTC"
+    },
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thehousemaid.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thehousemaid.jpg"
+    }
+  }
+
+  {
+    "option": "2",
+    "mtitle": "Ella McCay",
+    "review": "Lieutenant Governor Ella McCay approaches her office when her secretary, Estelle, hands Ella a letter. A reporter wants exclusive access to her. If not, he will release damaging information he has about her and her husband, Ryan. Ella pushes the matter aside and jumps in her vehicle. Her driver, Trooper Nash, drives Ella to her aunt's bar. Aunt Helen raised Ella since she was 16 and has been the voice of reason for Ella. Helen's advice is to dump the dead weight: Ryan. Ella refuses because he's provided a sense of fun for her.<br/><br/>Ella returns to work, and Governor Bill is waiting in her office. The President chose Bill as the next Secretary of the Interior, and Bill will name Ella the acting governor immediately. While Ella is good at passing legislation, she isn't well-liked amongst constituents. Ella believes she can win them over with her tooth tutor and well baby legislation. Bill shakes his head, stands up, and grabs the doorknob. He lets Ella go first and announces her promotion. Ella shies away from the camera as Ryan smiles for the it, grabs her, and dances with her. She wants to do good for her state, but life will happen.<br/><br/>Ella McCay is a smartly-written film about family, relationships, and politics. The plot teaches micro lessons in civics while entertaining. Ella is highly capable and intelligent. However, she has anchors pulling her down. Sadly, she doesn't see how her two biggest pitfalls mirror each other until it's too late. The cast did a stellar job of acting with little dialogue. They let their facial expressions and hand gestures convey the message. At the conclusion, this picture will encourage you to scream healthily.<br/><br/>4 out of 5 stars.",
+    "asin": {
+      "re": ""
+    },
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/ellamccay.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/ellamccay.jpg"
+    }
+  }
+
+  {
+    "option": "3",
+    "mtitle": "Avatar: Fire and Ash",
+    "review": "After Neteyam's death, Neytiri and Jake debate what to do with Spider. Neytiri, unable to bear Spider's resemblance to those who killed her son, wants him gone, while Jake hesitates. That night, Spider's mask fails, and he begins gasping, prompting a frantic search for his spare mask. They save him for the moment, but Jake realizes the replacement mask will also eventually fail. When the Wind Traders arrive in Metkayina, Jake decides to bring Spider back to the humans for his safety.<br/><br/>Jake, Spider, and the family travel with the Wind Traders toward the RDA ship. En route, the Mangkwan, or Ash People, attack their aircraft. Amidst the chaos, Spider, Kiri, Tuk, and Lo'ak are separated from the group. Spider's mask battery runs low, and his spare batteries are with the wreckage. Spider collapses. Kiri lays him down, links her queue to the ground, and puts a bug in his mouth. Spider miraculously recovers. Colonel Miles witnesses this and becomes determined to capture his son and bring him to RDA for study. Jake knows that if Miles gains access to this miracle, humans will become unstoppable. The coming battle will result in heavy loss, two new unions, and new life for Pandora.<br/><br/>The third installment of the Avatar franchise focuses on the action while building the Pandora lore. The graphics draw you into Na'vi's world with motion and colors that were meant for 3D, and the score teleports the audience into each breathtaking adventure. While nothing will ever top this first, this film towers above the second in every category. This film has no mid-credit or end-credit scenes. If you are a fan of the first two, then you must see this one in theaters and in 3D with the best sound.<br/><br/>5 out of 5 stars.",
+    "asin": {
+      "re": ""
+    },
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/avatarfireandash.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/avatarfireandash.jpeg"
     }
   }
 */
