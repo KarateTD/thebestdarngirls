aec7abef6ac6c1b3e034ed72dc3fb93cc9949989
Tiffany Gray <KarateTD@yahoo.com>
2026-01-12 19:36:00 -0500

    4 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Accused: The Karen Read Story",
+    "review": "Karen Read wakes up in the middle of the night and notices her boyfriend, Officer John, isn't there. Karen wants to look for John, but her dad warns her to stay home. Then, she rings her friends, and they search the snowy roads for John. Karen finds John's body in an embankment covered in snow. She calls 9-1-1 for assistance.<br/><br/>The EMTs put John in an ambulance, but won't let Karen go with him. They escort her into another ambulance. Nurses take Karen's blood at the hospital as she begs for an update on John's status. Her father enters with horrible news. John passed away.<br/><br/>Karen returns to John's home, and his family is in the kitchen. They calmly ask her to pack and leave so they can care for John's children in their home. Later, the police interview Karen. She worries that she is a suspect after they take her car and phone. So, she gets a lawyer. Devastated, Karen doesn't know what's next. That's until her lawyer calls. The police believe she backed into John after a fight and left him to die. Karen hopes the police will understand that she did not end John's life, but she doesn't remember everything that happened because she was intoxicated. They dash her hopes when they change Karen's charge from manslaughter to 2nd degree murder. Will Karen spend her life in jail?<br/><br/>Inspired by a true story, Karen Read's arrest exposed the dark side of the Boston Police Department. This film doesn't take sides. While it's not a linear story, it showed Karen's obsessive nature about their relationship. This movie also highlights the sexist nature of the police department. They didn't investigate the crime and took shortcuts. The trial is over, but the movie lets you decide if Karen was guilty or innocent. That's what makes it compelling.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/accusedthekarenreadstory.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/accusedthekarenreadstory.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "The Pastor Who Preys",
+    "review": "Nicole rents a home next to her cousin Amanda for a few weeks. As Nicole unpacks, a couple approaches her. Pastor Caleb and First Lady Jen introduce themselves and welcome Nicole to the neighborhood. They mention that Amanda is a church member. Nicole makes polite conversation and mentions the letter. Jen believes it sadly belongs to the old owner, Phyllis. She was killed six months ago in a robbery. The police don't have any suspects.<br/><br/>Later, Nicole hears someone rummaging through her home and calls the police. However, no one was there, making Nicole feel silly. Nevertheless, the missing letter solidifies Nicole's fears. Someone was in her home. As she settles in, Amanda introduces Nicole to the church's fundraising committee.<br/><br/>Nicole answers a knock at her door. Amanda stands before her, with a tear-stained face. She admits to having an affair with Pastor Caleb for months. Her husband, Tom, left with no signs of returning. Amanda worries that Tom will tell the church to ruin hers and Caleb's reputation.<br/><br/>Tom frantically knocks on Nicole's door. He returned home, and Amanda wasn't there. Nicole and Tom look for Amanda. Sadly, they find her body at the river. The police declare her death a homicide. While Detective Chandler sets his sights on Tom, Nicole blames Caleb. Who killed Amanda?<br/><br/>This movie is a stellar mystery with a plethora of drama. The film introduces you to a warm and friendly church atmosphere. However, it doesn't take long to see something is lurking under the surface. This pacing captures your attention and keeps you on the edge of your seat. During the interrogations, the viewers see flashbacks. It allows you to see how anyone could be the culprit. This movie is a rousing start to the LMN 2026 season.<br/><br/>4.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thepastorwhopreys.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thepastorwhopreys.jpg"
+    }
+  },
+  {
+    "option": "3",
+    "mtitle": "Mama's Little Murderer",
+    "review": "Constance returns to her quaint cabin in the woods. She calls out for her son, Anthony, and boyfriend, Leo. She spies a note on the dining room table and reads it. Someone kidnapped Anthony. Constance screams for Leo. Later, Mitch calls. He wants Constance to enter her bank and withdraw $100,000, and they will return Anthony safe and sound. Leo wants to inform law enforcement, but Constance says no. She will do anything for her son. Although Anthony hasn't been perfect over the last few months, Constance will fight for him. Can she get her son home safely?<br/><br/>This movie isn't linear. It alternates between today and days ago. This accomplishes two things. It keeps your interest in the film and offers clues about the bigger picture simultaneously. Constance lives in a tug-of-war that will irritate fans while compelling you to watch. As the script reveals the underhanded dealings, you will clutch your pearls and yell at the screen. The film's only downfall is that there are zero murders - absolutely none. The title is misleading. This movie is entertaining, but it raises your blood pressure.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/mamaslittlemurderer.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/mamaslittlemurderer.jpeg"
+    }
+  },
+  {
+    "option": "4",
+    "mtitle": "Lost in Paradise",
+    "review": "Sophia gently rattles a list of demands for her team as she enters the workplace. She is the CEO of Tierra Fashions and a proud dog mom to Gianni. Sophia hears from her assistant, Miranda, that a major stakeholder, Mario, needs to speak with her immediately. Mario wants to sell his shares of the company to a national chain. Sophia wants to meet with investors in Fiji to promote her new spring line to stop him. She booked a private jet and will leave in a few hours.<br/><br/>Max shops for ingredients at a local farmers' market when his friend, pilot Julian, calls him. Julian knows that Max dreams of running a restaurant, but fears of past mistakes hold Max back. Julian met with an investor in Fiji who wants to open a food establishment. Julian informed the investor about Max, a sous chef, and got a meeting. However, Max needs to leave now to catch the flight.<br/><br/>Max arrives at the hangar, and Julian hands him a flight attendant uniform. It was the only way Julian could clear Max for the flight. Sophia hired Julian to fly her to Fiji. As they travel with their co-pilot, Lance, Max will serve her and attend to her needs for free travel. During the flight, Julian warns everyone to take their seat. Hours later, Sophia wakes on a liferaft. The plane crashed, but she, Max, Lance, Julian, and Gianni were marooned on a deserted island. Julian reveals that the crash ruined the transponder, so no one is looking for them. While Julian and Lance brave the elements at sea, Max, Sophia, and Gianni must stay alive on the island. Can they return to civilization to save their careers?<br/><br/>This humorous film is charming and refreshing. Sophia and Max don't turn on each other. Instead, they come together and discover each other's strengths. This twist makes their love story believable. They deal with sunburn, pirates, and fashion faux pas. The setup is expertly done, the comedic timing is unmatched, and the chemistry is off the charts. This film is the perfect start to Hallmark's 2026 season.<br/><br/>4.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/lostinparadise.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/lostinparadise.jpg"
+    }
+  },
+  {
+    "option": "5",
     "mtitle": "Stolen Girl",
     "review": "Maureen cleans herself up after her father, Joe, spots Maureen's ex-husband's car in the driveway. She forgot it was Karim's day to pick up their daughter, Amina. Days later, Maureen purchases Joe's medication from a pharmacy and loses sight of Amina. She searches for the young child but can't find her. She contacts the police. Maureen alerts officers that Karim is a Syrian national. During their divorce, Karim threatened to take Amina to Syria. There is nothing they can do because Karim is a legal parent. Maureen screams and storms out of the station.<br/><br/>Years later, Maureen's community raises funds for her to travel to Washington, D.C. Maureen will ask lawmakers to help her find Amina and change the laws. They ignore her pleas. Robeson approaches her with an offer. He works in international child recovery. He needs a woman on the team for extractions. If she helps him find other missing kids, he will help her locate Amina. After some prodding, Maureen agrees to join his team. Will Maureen find Amina?<br/><br/>Inspired by actual events, this movie combines high-octane action with lulls of dialogue. The actors deliver an amazing performance. They evoke fear and urgency as they save children around the world. The vanity team made them look tired and unkempt as they traveled covertly. While the action is compelling, it's the conclusion that will resonate with parents. This film is best for the small screen and finds the right home on Lifetime.<br/><br/>3 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/stolengirl.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/stolengirl.jpg"
-    }
-  },
-  {
-    "option": "2",
-    "mtitle": "The Past is Never Buried",
-    "review": "Noah interviews Emily for a documentary about her mother's murder. Someone killed Emily's mother, Ginny, but the case remains unsolved. Noah enquires if Emily's father, Alton, would like to participate, although Noah already asked. Emily says no. In the past, Alton was Detective Gates's number one suspect, but he couldn't find enough evidence.<br/><br/>Brad asks his son, Devin, to wake up Devin's sister, Jordyn, for breakfast before school. Devin goes to Jordyn's room, but Jordyn isn't there. Brad calls Jordyn's phone, but she doesn't pick up. Brad and Devin rush to the police station to report her as a missing person. Detective Gates takes the case. He visits Jordyn's workplace and questions the staff and customers. One customer, Alton, catches his eye. Later, they find Jordyn's remains in the woods. Her killer strangled her. Afterward, Detective Gates obtains a search warrant for Alton's home and finds photos of Jordyn inside. He arrests Alton.<br/><br/>Alton calls Emily from jail and begs for her help. He believes Detective Gates is setting him up for murder because he couldn't solve Ginny's case years ago. During Emily's visit to her childhood residence, she uncovers clues that prove Alton is telling the truth. But if he didn't kill Jordyn, then who did? And is it connected to her mother's case? Can she find the truth before Alton is sentenced to life in prison?<br/><br/>This film presents a complex mystery with 20 years between cases. It does offer an unnecessary love story with a play on modern voyeurism. This movie feels like a Dateline episode with overzealous cops, a compelling killer, an enabler, and several points of view. The writers slowly reel the audience in with family conflict over an old murder. Then, they keep your attention with a new crime. Lifetimers, sit back and enjoy this messy show with your tea and popcorn ready.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thepastisneverburied.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thepastisneverburied.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "The Nurse Who Knew Too Much",
-    "review": "Nurse Sarah makes the rounds after celebrating her boss's birthday. Sarah meets with her boss, Charge Nurse Alison, to discuss her patients. Everyone's favorite is Jack. Jack is a baker who had to get an emergency appendectomy. Another is Jack's hospital mate, David. Both men are recovering from their procedure, but are still heavily medicated.<br/><br/>Sarah finishes with David and walks over to Jack. Jack makes a few comments, then admits to killing people. He brags about them begging to live and the life draining from their eyes. Sarah tells Alison about Jack's admission, but Alison says it's the medication.<br/><br/>After some strange occurrences in the hospital, Sarah asks her friend, Sheriff Mark, for assistance. He agrees to look into Jack's past and his confession for more information. Nevertheless, Mark warns Sarah he will stay within the confines of the law. Will Mark prove Sarah right, or is she accusing an innocent man under the influence of medication?<br/><br/>This film starts with a wild premise. Then, Sarah sticks her nose into other people's business and puts her life in danger. This plot point lasts for the majority of the movie. The final 20 minutes provide a strong twist and a harsh narrative. This change will compel the audience to sit up and pay attention. This picture proves the nurse knew something because she was too nosy.<br/><br/>3 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thenursewhoknewtoomuch.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thenursewhoknewtoomuch.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "The Hockey Mom's Revenge",
-    "review": "Rachel arrives late to watch her son, Ethan, play hockey. Michelle updates Rachel while Diane comments on Ethan's tattered pads. Rachel apologizes. Diane tells Rachel not to worry because her son, Alex, loaned Ethan a pair of his old ones. Rachel thinks Diane's gesture is kind, but Michelle sees it as patronizing.<br/><br/>Diane corners Coach Mike about changes to the lineup, practice, and workouts. He listens but is too busy with the Riders to take it into consideration. Diane sees Rachel in the parking lot, struggling with her car, and stops. She reminds Rachel that Ethan is a reflection of the team. Diane will sabotage, blackmail, and injure to get Alex to the top when no one takes her seriously. Will Rachel be able to stop her?<br/><br/>This film has a touch of old-school Lifetime. The writers made Diane an unhinged woman who resorts to self-harm to help her son. The intro gives the viewer a glimpse into Diane's backstory, but you get more information later. The audience will sympathize with the sons. It's the final face-off that alters the film's dynamic. It becomes an impromptu counseling session. One swift move brings this film to reality. This picture is for old and new fans alike.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thehockeysmomrevenge.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thehockeysmomrevenge.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Child They Hid",
-    "review": "Marci surprises her family early with DNA kits for Christmas. She took the test and found a relative in New York who works as a dancer. Marci's mother, Caroline, asks if the woman's name is Helen. Stunned, Marci confirms. They knew about Helen but never spoke of her.<br/><br/>Marci calls Helen. They've been speaking for months, and Helen agreed to meet Marci in person. Helen assures Marci that she will be there. Helen ends the call and answers the door. Her friend, Dominic, left his scarf after her Christmas party. She assumes it's him. However, someone attacks her and leaves Helen for dead.<br/><br/>Marci works in Caroline's store because Caroline battles depression after losing her husband. Marci hasn't heard from Helen. Marci's friend, Tamra, believes Helen ghosted Marci. Tamra observes that Marci is upset, so she takes Marci to the bar to unwind. Nick approaches them with a badge. He explains the attack that left Helen in a coma. He warns Marci that the DNA test triggered the attacks. Nick tells Marci to alert anyone in her family who took one. The attacker has a list, and they are checking it twice. Can Marci uncover the culprit before she's next?<br/><br/>This film starts strong but wanes in the middle as Marci becomes sidelined with mundane personal issues. And the culprit's motive for attacking Marci and her family is a difference of perspective. Also, it's unbelievable that this small town kept this family's secrets from Marci. While the script is tantalizing, it can't overcome the lull in the middle. So keep this film in the background and watch when something good happens.<br/><br/>3 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thechildtheyhid.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thechildtheyhid.jpg"
     }
   }
 ];
@@ -57,41 +57,6 @@ module.exports = madeForTV;
 
 /*
 
-{
-"option": "1",
-"mtitle": "Accused: The Karen Read Story",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/accusedthekarenreadstory.jpg",
-  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/accusedthekarenreadstory.jpg"
-}
-},
-{
-"option": "2",
-"mtitle": "The Pastor Who Preys",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-  "smallImageUrl": "",
-  "largeImageUrl": ""
-}
-},
-{
-"option": "3",
-"mtitle": "Mama's Little Murderer",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-  "smallImageUrl": "",
-  "largeImageUrl": ""
-}
-},
-{
-"option": "4",
-"mtitle": "Lost in Paradise",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/lostinparadise.jpg",
-  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/lostinparadise.jpg"
-}
-}
+
 
 */
