37224fb6c8a5d0927699301e652e35741ae4df21
Tiffany Gray <KarateTD@yahoo.com>
2026-04-07 19:24:23 -0400

    mercy

--- lambda/data/mustBuy.js
+++ lambda/data/mustBuy.js
@@ -1,6 +1,18 @@
 var mustBuy = [
   {
     "option": "1",
+    "mtitle": "Mercy",
+    "review": "Mercy was an Early Screening review for Premium Access members only.  The review and rating have been moved to the Library for U.S., Great Britain, and Canadian users only.  If you live in these locations and want access to the review and the rating say, 'I want to buy Premium Access.' If you have Premium Access, say 'Library.'",
+    "asin": {
+      "re": "B0GFMM6WQB"
+    },
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/mercy.jpeg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/mercy.jpeg"
+    }
+  },
+  {
+    "option": "2",
     "mtitle": "Marty Supreme",
     "review": "Marty Mauser works at his uncle Maury's shoe store. As Marty sneaks in his girlfriend, Rachel, Maury asks to meet with Marty. He hands Marty a business card with Marty's name and the title 'Manager' on it. Marty reminds Maury that he doesn't want to sell shoes. Also, Maury's son, Lloyd, will dislike being passed over. Marty dreams of going to the British Open for Table Tennis.<br/><br/>Later, Marty practices with his friend, Wally, and returns to the store for his check. Lloyd locks the safe, and Marty grabs a gun from Maury's desk. Marty threatens to shoot Lloyd over the money. Then, Marty shows Lloyd the business card. Lloyd opens the safe and gives Marty $700.<br/><br/>Marty arrives in London and hates his accommodations. After making it to the finals, Marty sits down with reporters. They stop talking when actress Kay Stone enters. Marty sets his sights on her and invites her to his room. Before the finals, Marty takes a moment to cozy up to Kay's husband, Milton. However, Marty's plans shatter when he loses to Japanese hopeful Koto Endo. Marty will do anything to prove his purpose: to dominate table tennis. Will he prove the world wrong and become the face of the sport?<br/><br/>It's not easy, but the writers make every character in this film unlikable. It should be about one man's triumph over his doubters. However, Marty is condescending, arrogant, rude, and manipulative. The audience prays for his downfall to humble him. Inspired by Marty Reisman, the technology and style follow the 1950s, but the slang falls short. This picture is Oscar-worthy because the acting is phenomenal. However, the audience won't cheer for anyone.<br/><br/>4 out of 5 stars.",
     "asin": {
@@ -12,7 +24,7 @@ var mustBuy = [
     }
   },
   {
-    "option": "2",
+    "option": "3",
     "mtitle": "The Housemaid",
     "review": "Millie arrives at the Winchester Estate, and Nina Winchester greets Millie with a smile and hor d'oeuvres. Nina examines Millie's résumé and qualifications. She reiterates that the position is a live-in maid. Nina confesses that she's expecting but hasn't told her husband, Andrew, or daughter, CeCe. Millie thinks everything is wonderful. Nina expresses that she has more applicants to interview before choosing a housemaid. But she will call Millie back soon.<br/><br/>Millie drives away, removes her glasses, and rolls her eyes. She never expects to hear from Nina again. Millie is on probation for 5 years after serving 10 years in prison. She knows one background check will rule her out. Millie takes showers in gas station bathrooms, applies for any job, and sleeps in her car. When her phone rings, it's Nina offering Millie the job. She wants Millie to start as soon as possible, and Millie is willing to do so this afternoon. Millie thinks her prayer has been answered. She has a steady job, a paycheck, and a place to live. Her probation officer will be pleased. However, Millie has no idea the danger lurking behind the iron gate.<br/><br/>Based on The Housemaid by Freida McFadden, this film is a wild ride that plays with motifs, surroundings, and wardrobe to convey deeper meanings. The house is void of color except for one bathroom with snake wallpaper. Nina always wears a hue of white until the telling ending. She's seen as clean, perfect, and pure. Millie and Andrew stain on the screen as they wear darker wardrobes. The different angles show the power dynamic shift throughout the film. It will leave you dizzy. Also, the split screens and overlays of different people experiencing the same turmoil are jaw-dropping. This film will make you want to read the book and compare it. It's pragmatic, cultural, and surprising.<br/><br/>4.5 out of 5 stars.",
     "asin": {
@@ -24,7 +36,7 @@ var mustBuy = [
     }
   },
   {
-    "option": "3",
+    "option": "4",
     "mtitle": "Ella McCay",
     "review": "Lieutenant Governor Ella McCay approaches her office when her secretary, Estelle, hands Ella a letter. A reporter wants exclusive access to her. If not, he will release damaging information he has about her and her husband, Ryan. Ella pushes the matter aside and jumps in her vehicle. Her driver, Trooper Nash, drives Ella to her aunt's bar. Aunt Helen raised Ella since she was 16 and has been the voice of reason for Ella. Helen's advice is to dump the dead weight: Ryan. Ella refuses because he's provided a sense of fun for her.<br/><br/>Ella returns to work, and Governor Bill is waiting in her office. The President chose Bill as the next Secretary of the Interior, and Bill will name Ella the acting governor immediately. While Ella is good at passing legislation, she isn't well-liked amongst constituents. Ella believes she can win them over with her tooth tutor and well baby legislation. Bill shakes his head, stands up, and grabs the doorknob. He lets Ella go first and announces her promotion. Ella shies away from the camera as Ryan smiles for the it, grabs her, and dances with her. She wants to do good for her state, but life will happen.<br/><br/>Ella McCay is a smartly-written film about family, relationships, and politics. The plot teaches micro lessons in civics while entertaining. Ella is highly capable and intelligent. However, she has anchors pulling her down. Sadly, she doesn't see how her two biggest pitfalls mirror each other until it's too late. The cast did a stellar job of acting with little dialogue. They let their facial expressions and hand gestures convey the message. At the conclusion, this picture will encourage you to scream healthily.<br/><br/>4 out of 5 stars.",
     "asin": {
@@ -36,7 +48,7 @@ var mustBuy = [
     }
   },
   {
-    "option": "4",
+    "option": "5",
     "mtitle": "The Running Man",
     "review": "Ben asks his old boss, Richard, to remove him from the blacklist. Ben can't find work with the scarlet letter on his record, and his daughter, Cathy, is sick. However, Richard won't change his mind because Ben spoke to union leaders about his working conditions. Ben will remain on the list.<br/><br/>Ben watches game shows with Cathy until his wife, Sheila, returns home. She wants to spend time with Cathy, but changes her mind after feeling Cathy's fever. He hates that she has to flirt with wealthy men for tips, especially after one client killed her co-worker. He gets an idea. He will go to the network building and be a contestant on one of their shows. Sheila rejects the idea because she fears he will try out for The Running Man. Ben assures her he would never try out for that game before leaving.<br/><br/>Ben walks through the gates of the network building. They pull up his work history and tell him to go to elevator 26. Hopeful contestants, Jenni and Tim, greet Ben. The Network showrunners perform physical and psychological tests on the contestants. To Ben's surprise, they picked him to be on the Running Man. Ben tries to force them to change, but they refuse.<br/><br/>Ben meets with the Network owner, producer Dan. Dan shows Ben the lucrative contract. He gets a bonus for every day he remains alive and a $500 signing bonus. Ben signs the contract and assures Dan he will be the first contestant to go the distance.<br/><br/>Dan introduces Ben, Tim, and Jenni to the show's hosts, Bobby T.. Bobby gives a quick hello before running on the stage. Ben rages at the false narrative Bobby spins to make the fans hate him. Then, Bobby gives the rules. The runners will have a 12-hour head start. The further they go, the more money they will make. They have to film themselves every day for 10 minutes and drop it in a mailbox. If they don't, they won't get the money, but the hunters will still come after them. They have to last 30 days. Ultimately, they will receive $1 billion. Will he survive the Running Man and make it home to his family?<br/><br/>Based on The Running Man by Steven King, this film has a spectacular pace, wild stunts, and a fresh twist on a familiar plot. This film isn't just about a man running for his life. It's a searing indictment of media manipulation by corporations. The director uses different camera techniques to capture Ben's runs. It could be a steady drone camera shot, a GoPro-style shot, a handheld camera shot, or standard movie framing. These changes display an enticing view of Ben and his run. The soundtrack stays in step with Ben's speed and gets you excited. The action slows with a purpose. It speaks to corruption, pollution, and screen addiction. The plot delivers these messages without getting too topic-heavy or self-righteous. This film is an exciting and violent ride.<br/><br/>4.5 out of 5 stars.",
     "asin": {
@@ -45,18 +57,6 @@ var mustBuy = [
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/therunningman.jpeg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/therunningman.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Zootopia 2",
-    "review": "Partners Judy and Nick became famous for saving Zootopia from a civil war. However, Chief Bogo thinks it was a fluke because they are different. Bogo presents their latest case to the squad. It's a crooked dock inspector. He gives the team their assignments and puts Nick and Judy on observation. However, they aren't there. Officer Clawhauser informs Bogo that Nick and Judy are on the scene and took the lead.<br/><br/>The bust becomes a TV spectacle, putting Judy and Nick in the hot seat. Judy tries to explain that she found snakeskin in one of the crates, but Bogo won't listen. Besides, reptiles haven't been in Zootopia in 100 years. That's when they created the weather wall. Judy discovers that the culprit will be attending the Zootenial Gala. Zootopia's founder, Ebenezer Lynxley, will be honored at the event. His family will display his journal, which contains the idea and blueprints for Zootopia.<br/><br/>Judy convinces Nick to attend the gala undercover. Judy bumps into Pawbert Lynxley, Ebenezer's grandson, and he's smitten with her. His father, Milton, runs the family business and upholds the legacy with his other two children, Kitty and Cattrick. Nick shines a light on the culprit before they grab the journal and try to run away. Judy and Nick follow them into a private room and remove their cloak. It's Gary De'Snake. He tells Judy they aren't the bad guys and the journal can prove it. Before Gary reads the journal, Milton, Kitty, and Cattrick kick open the doors. Bogo barges into the room, and Gary accidently bites him with one of his fangs. Judy breaks it and holds it in her hand. When the other officers enter, Milton says Judy, Nick, and Gary attempted to kill Bogo. They escape before the police arrest them, but they are enemy number one. Can Judy and Nick clear their name while helping Gary uncover the truth?<br/><br/>This sequel is clever, endearing, and funny. It covers perception, smear campaigns, history washing, and forced migration with ease and compassion. During these hard-hitting topics, the script tackles the partnership between Judy and Nick. The animation rivals the first film. Disney manages to give the animals human-like qualities while displaying their unique animalistic characteristics. This installment expands the animal world while rooted in its core cast. The beginning recaps what happened in the first film, so you don't have to see it to enjoy this. The end-credit scene gives the viewer a hint at what is to come. See Zootopia 2 with the whole family today.<br/><br/>4 out of 5 stars.",
-    "asin": {
-      "re": "B0GKTYTY3J"
-    },
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/zootopia2.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/zootopia2.jpg"
     }
   }
 ];
