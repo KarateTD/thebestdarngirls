b933808bdc8e96afb9085cdeb4de43468b85464e
Tiffany Gray <KarateTD@yahoo.com>
2026-05-05 22:32:11 -0400

    add 3 movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,33 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Kidnapped in My Own Home: The Martha Carelli Story",
+    "review": "Martha Carelli wakes to her radio alarm clock blaring a news report about escaped inmates. Four inmates broke out of prison and went on the run. Since then, authorities caught two, but the others are still on the loose. After an uneventful day, Martha picks up her son, Bennett, from school and drives home. He leaves to do his homework, while she prepares dinner and straightens up the house. Martha opens her closet and sees Michael standing inside. She freezes as he pulls out a gun. She tries to escape, but he beats her with the pistol. When Martha wakes up, she's bound to a chair and gagged. Michael threatens to kill Bennet if she tries to escape again. Also, his partner is in the house. Michael refuses to return to jail and will do anything for his freedom. Can Martha and her family escape a madman?<br/><br/>Inspired by the story 'The Painter's Wife' by Ann Rule, this movie exemplifies character development. Martha is an everyday working woman with a loving family. Then, she is forced into a survivor scenario with her family's life on the line. Martha slowly gains the courage to stand up to Michael and fight back with her emotions and intelligence. While the film follows Martha's heroic story, its conclusion will break your heart. It's a standout performance from the lead actress. Other movies pretend that 'they lived happily ever after,' but this story displays the harsh truth. While it's not the best Lifetime true crime story, it leaves a mark.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/kidnappedinherownhomemarthacarelli.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/kidnappedinherownhomemarthacarelli.jpg"
+}
+},
+{
+    "option": "2",
+    "mtitle": "The Moonlight Murders",
+    "review": "Security guard Zeke greets his neighbor, Poppy, before going to work. He makes the morning rounds at the parking garage and stumbles upon Emily's dead body. He secures the scene and calls the police, but not before noticing the moon tattoo on her arm. Detective Foster and Doye arrive and examine the area. This looks like the work of the Moonlight Murderer. They were dormant for a while, but Doye fears the killer or a copycat has resurfaced. Doye doesn't hide his annoyance when May arrives on the scene. May is a crime reporter with a TV show. Her claim to fame was pointing out the killer's M.O. and analyzing it for her fans. During a B-roll shot, Zeke approaches May. He's a huge fan. Zeke tells her that he found the body, and she jumps at the chance to interview him for an exclusive. She wants to use Zeke's information to find the killer first. However, Zeke's obsession with her grows with a killer on the loose. Will she survive?<br/><br/>This film's unique plot features two evolving mysteries at once. However, you are aware of one mystery while in the dark about the other. Viewers analyze the clues to unmask the culprit, while May unpacks who is stalking her. The relationship between Franco and May is a plot filler and slows down the pace. However, the killer's reveal was well done. It's a testament to good acting, directing, and writing. The motive is worthy of an episode of Criminal Minds, but less entertaining. This movie has spurts of charisma with lulls in between.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/themoonlightmurders.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/themoonlightmurders.jpg"
+}
+},
+{
+    "option": "3",
+    "mtitle": "I'll Be Seeing You",
+    "review": "Mark receives a message from the retirement community manager. Mark has been acting as the interim activities director for months. Now, the boss wants him to take the job permanently. Residents Vivian and Susan tell Mark to take the job. However, Mark has never been a man to settle in one spot. So he planned a trip down the coast to think about it.<br/><br/>Vivian gets a call from her granddaughter, Amy. Amy confirms she is coming to spend quality time with Vivian. When Amy arrives, she quickly settles in and meets her grandmother's friends, including Susan. Amy's boss, Phyllis, calls because a tough client wants to sign the paperwork. But they want to do it in person, and the sooner the better. Phyllis promises to give Amy her coveted promotion if she gets the signature. Vivian overhears the conversation and suggests they turn it into a road trip. She desires to visit her old stomping grounds and visit an old romantic friend. Also, she wants to invite Susan because this will be the first anniversary since she lost her husband. Amy agrees.<br/><br/>Mark helps them pack up the car for their road trip and reminds Vivian to watch Susan. Susan regularly forgets to take her medication. Soon, they hit a pothole, flattening their tire. Vivian calls Mark for help because roadside assistance will take too long. Mark arrives and offers to go with them since they will all be driving down the coast. They load into his VW van and set off on a trip that will change the trajectory of their lives forever.<br/><br/>This film has mystery, love, wisdom, and humor. The plot has several dynamic, well-developed relationships. They feed into a complex storyline and deliver beautiful messages. The age gap plays a pivotal role in this movie. Susan and Vivian offer Mark and Amy a fresh perspective that's full of sage advice. Amy and Mark help Vivian find the one that got away. Susan keeps everyone on course while keeping a positive attitude. Every moment of this trip will live with you and inspire you. There are no wasted scenes or filler. Let's not forget the leads' comedic timing and wonderful musical abilities. This film is an instant Hallmark classic you will keep on your DVR.<br/><br/>5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/illbeseeingyou.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/illbeseeingyou.jpg"
+}
+},
+  {
+    "option": "4",
     "mtitle": "Behind Closed Doors",
     "review": "Morgan jogs with her daughter, Lizzy, before preparing her son, Cody, for his field trip. She calls her husband, Kevin, to coordinate the evening. Morgan drives Cody to his meeting location and notices several police officers, emergency vehicles, and reporters outside the mall parking garage. When they return home, she prepares dinner while listening to the latest news report. An unknown assailant killed someone in the parking garage, and they are on the loose. She ignores the report and focuses on her meal. Morgan tells Lizzy to get Cody. Then, Morgan hears Lizzy scream. She sees a man, Grant, standing in her kitchen. Morgan offers him money, but he's not here for that. He ties up the family and waits for Kevin to come home.<br/><br/>Kevin arrives with a jovial attitude until he sees Grant sitting at the kitchen table with his family. He warns Kevin not to try anything. Grant hid Cody in a secret location and will kill him if Morgan and her family don't comply with his demands. Morgan grasps that Grant is the parking garage murderer. Grant reminds Kevin that he is in charge and kills to prove it. Morgan begs Kevin to follow her lead and try to understand Grant's side. Can Morgan save her family from a madman?<br/><br/>This film has some tense moments throughout the plot. It mentions Lima Syndrome without describing it, but this movie perfectly depicts it. The writer considered today's technology when creating the plot. This story drags because Lifetimers know there is an underlying motive. However, the plot keeps you on the hook until the very end. Some will see it coming while others want more. Either way, you will watch from start to finish. This film is shocking and entertaining, but the pacing is slow.<br/><br/>3.5 out of 5 stars.",
     "image": {
@@ -9,7 +36,7 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "5",
     "mtitle": "A House Built On Lies",
     "review": "Daisy and her best friend, Louise, visit Daisy's new home. She and her husband, Chris, will move in once he's done with the renovations. Daisy finds Chris in the attic, and they kiss. She questions why Chris's best friend, Zack, isn't helping. Chris says they got into an argument, but doesn't go into specifics. Daisy and Louise return to Louise's home.<br/><br/>Daisy and Louise wake to a police officer knocking on the door. The officer expresses his condolences because Chris fell out of the 2nd-story window and died. Daisy collapses to the ground and enters premature labor. Hours later, Daisy wakes with nurse Tyra by her bedside. Tyra explains that they had to perform an emergency C-section to save her baby. Daisy gets better and chooses to move into Zack's home with her newborn baby. After a few days, the police question Daisy. They believe Chris's fall wasn't an accident; someone pushed him. The police discovered that someone had taken $100,000 from his bank account. Daisy admits that she found out about the money recently but hasn't seen it. The officer lets Daisy know she is their prime suspect. Can Daisy clear her name?<br/><br/>This movie takes the long way to the point. It drops breadcrumbs to other suspects, but experienced Lifetime viewers will figure it out after the first commercial break. However, the plot, the murder, and the deception don't grab or hold your attention. The twist isn't surprising, the motive is obvious, the pacing was slow, and the editing was too dark. This movie is better for white noise.<br/><br/>2.5 out of 5 stars.",
     "image": {
@@ -57,6 +84,49 @@ module.exports = madeForTV;
 
 /*
 
-
-
+{
+"option":"1",
+"mtitle":"Love, Again",
+"review":"<br/><br/> out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/loveagain_tv.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/loveagain_tv.jpg"
+}
+},
+{
+"option":"2",
+"mtitle":"The Secrets of the Matriarch",
+"review":"<br/><br/> out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thescretsofthematriarch.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thescretsofthematriarch.jpg"
+}
+},
+{
+"option":"3",
+"mtitle":"The Dating App Nightmare",
+"review":"<br/><br/> out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/thedatingappnightmare.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/thedatingappnightmare.jpg"
+}
+},
+{
+"option":"4",
+"mtitle":"Somebody's Watching Me",
+"review":"<br/><br/> out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/somebodyswatchingme.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/somebodyswatchingme.jpg"
+}
+},
+{
+"option":"5",
+"mtitle":"Kentucky Roses",
+"review":"<br/><br/> out of 5 stars.",
+"image":{
+"smallImageUrl":"https://thebestdarngirls.s3.amazonaws.com/small-image/kentuckyroses.jpg",
+"largeImageUrl":"https://thebestdarngirls.s3.amazonaws.com/large-image/kentuckyroses.jpg"
+}
+}
 */
