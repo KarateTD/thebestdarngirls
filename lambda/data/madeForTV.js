e8d56f2d74247efc9754230056f7785ef3f3422d
Tiffany Gray <KarateTD@yahoo.com>
2026-05-05 22:40:14 -0400

    fixed

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -6,26 +6,26 @@ var madeForTV = [
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/kidnappedinherownhomemarthacarelli.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/kidnappedinherownhomemarthacarelli.jpg"
-}
-},
-{
+    }
+  },
+  {
     "option": "2",
     "mtitle": "The Moonlight Murders",
     "review": "Security guard Zeke greets his neighbor, Poppy, before going to work. He makes the morning rounds at the parking garage and stumbles upon Emily's dead body. He secures the scene and calls the police, but not before noticing the moon tattoo on her arm. Detective Foster and Doye arrive and examine the area. This looks like the work of the Moonlight Murderer. They were dormant for a while, but Doye fears the killer or a copycat has resurfaced. Doye doesn't hide his annoyance when May arrives on the scene. May is a crime reporter with a TV show. Her claim to fame was pointing out the killer's M.O. and analyzing it for her fans. During a B-roll shot, Zeke approaches May. He's a huge fan. Zeke tells her that he found the body, and she jumps at the chance to interview him for an exclusive. She wants to use Zeke's information to find the killer first. However, Zeke's obsession with her grows with a killer on the loose. Will she survive?<br/><br/>This film's unique plot features two evolving mysteries at once. However, you are aware of one mystery while in the dark about the other. Viewers analyze the clues to unmask the culprit, while May unpacks who is stalking her. The relationship between Franco and May is a plot filler and slows down the pace. However, the killer's reveal was well done. It's a testament to good acting, directing, and writing. The motive is worthy of an episode of Criminal Minds, but less entertaining. This movie has spurts of charisma with lulls in between.<br/><br/>3.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/themoonlightmurders.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/themoonlightmurders.jpg"
-}
-},
-{
+    }
+  },
+  {
     "option": "3",
     "mtitle": "I'll Be Seeing You",
     "review": "Mark receives a message from the retirement community manager. Mark has been acting as the interim activities director for months. Now, the boss wants him to take the job permanently. Residents Vivian and Susan tell Mark to take the job. However, Mark has never been a man to settle in one spot. So he planned a trip down the coast to think about it.<br/><br/>Vivian gets a call from her granddaughter, Amy. Amy confirms she is coming to spend quality time with Vivian. When Amy arrives, she quickly settles in and meets her grandmother's friends, including Susan. Amy's boss, Phyllis, calls because a tough client wants to sign the paperwork. But they want to do it in person, and the sooner the better. Phyllis promises to give Amy her coveted promotion if she gets the signature. Vivian overhears the conversation and suggests they turn it into a road trip. She desires to visit her old stomping grounds and visit an old romantic friend. Also, she wants to invite Susan because this will be the first anniversary since she lost her husband. Amy agrees.<br/><br/>Mark helps them pack up the car for their road trip and reminds Vivian to watch Susan. Susan regularly forgets to take her medication. Soon, they hit a pothole, flattening their tire. Vivian calls Mark for help because roadside assistance will take too long. Mark arrives and offers to go with them since they will all be driving down the coast. They load into his VW van and set off on a trip that will change the trajectory of their lives forever.<br/><br/>This film has mystery, love, wisdom, and humor. The plot has several dynamic, well-developed relationships. They feed into a complex storyline and deliver beautiful messages. The age gap plays a pivotal role in this movie. Susan and Vivian offer Mark and Amy a fresh perspective that's full of sage advice. Amy and Mark help Vivian find the one that got away. Susan keeps everyone on course while keeping a positive attitude. Every moment of this trip will live with you and inspire you. There are no wasted scenes or filler. Let's not forget the leads' comedic timing and wonderful musical abilities. This film is an instant Hallmark classic you will keep on your DVR.<br/><br/>5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/illbeseeingyou.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/illbeseeingyou.jpg"
-}
-},
+    }
+  },
   {
     "option": "4",
     "mtitle": "Behind Closed Doors",
@@ -42,33 +42,6 @@ var madeForTV = [
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/ahousebuiltonlies.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/ahousebuiltonlies.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "Murder at the Merriweather",
-    "review": "Sophie and Rena arrive at the Merriweather Hotel to start their workday. They greet the maid, Dasha, and their boss, Alec. Sophie checks the guest list, and they can't believe their former friend, Kelly, will wed Rena's ex-boyfriend, Sam. Sam arrives first with his brother, best man Jasper, and they check into their room. Sam pulls Rena aside and apologizes for the past, while Dr. Joseph checks in. Sam invites them to his bachelor party that evening. Rena convinces Sophie to attend.<br/><br/>Sophie wakes up in the hallway with a horrible hangover. She stumbles to her room when Joseph sees a cut on her arm. He bandages up the wound while questioning her about missing items. She offers to help, but he blames her. Then, they hear a commotion outside. Sophie staggers to the front entrance and sees a crowd gathered around Rena's dead body. Sophie stifles her tears and runs away. She can't remember what happened that night. When she returns to work, the police assessment stuns her. They labeled Rena's death a suicide, but Sophie won't accept it. Sophie returns to work and checks in the bride-to-be, Kelly, and Sam and Jasper's mother, Tamara. Sophie attempts to maintain her professional composure, but Kelly pulls her aside. Sophie confesses that she believes someone killed Rena and the police are covering it up. Kelly offers to help, but will she continue when the evidence points to Sam?<br/><br/>This film has a small cast of characters for a mystery, but the plot uses that to its advantage. The character development is more in-depth and offers a richer plotline. Every character has a motive and poor behavior, which makes the audience suspicious of them. Viewers must watch the first 20 minutes to understand the character build-ups, backstories, and plot points that will return later. The body count is low, but this movie is entertaining.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/murderatthemerriweather.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/murderatthemerriweather.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "To Philly with Love",
-    "review": "Emily returned to her hometown to help her mother clean out her grandmother's house before selling it. She answers a knock at the door, and it's Nancy. Nancy is a member of the Daughters of Liberty and friends with Emily's grandmother, Beverly. Nancy states that the Daughters of Liberty are at risk of losing their non-profit status in two weeks. They need something to prove their worth in society. Nancy hands Emily letters and Beverly's rough draft. Beverly believed her letters were linked to important people in the American Revolutionary War. Emily reluctantly offers to help by finishing her grandmother's research.<br/><br/>Emily arrives at the National Archives at Philadelphia to examine their records. However, she accidentally sets off the alarm. Nate deactivates it and questions her presence in the stacks. She explains why she's there, and he offers to look at her letters. He asks her to leave the letters with him for further examination, but Emily politely declines. Nate gives Emily his business card and offers to help her if she needs it. After Emily leaves, Nate hears his boss, Richard, enter the building. Richard wants an exhibit to wow the guests and donors. Nate hides his panic with a smile and awkwardly accepts. Then, he remembers Emily. He can research her letters and add them to the exhibit. Her new documents can give a new perspective on history. Can he get Emily to work together while falling for her?<br/><br/>This movie brings a dash of American history to its plot. It discusses the making of America, the Declaration of Independence, and Betsy Ross. However, the plot isn't straightforward because the letters use a code and have a poetic flair. The added mystery gives the movie a National Treasure feel. The leads aren't aware of their budding romance, as are those around them. Their friends and family see the sparks and push them together. They fall in love reading the romance between the correspondents. The film is an easy love story with mystique.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/tophillywithlove.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/tophillywithlove.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Murder in the Dark",
-    "review": "Detective Guy takes his friend, Detective Cedric, to a trendy restaurant called the Blind Spot for his birthday. Its employees have low or no vision, and guests eat in the dark. Their waitress, Ash, introduces herself, informs them of their phone policy, and escorts them to their table. After dinner, Guy notices that Cedric is attracted to Ash as they leave the restaurant. A drunken Cedric re-enters the establishment and makes a failed attempt to flirt with Ash. Sadly, he gets sick on her shoes.<br/><br/>After work, Ash walks home and realizes she left her house keys at the restaurant. She returns and feels for them. She trips over something and touches the ground. Soon, she discovers she tripped over Chef André's feet. She hears heavy boots leave the establishment while she calls the police. Guy and Cedric arrive at the scene. Ash gives them all the information she has, thanks to her other senses. As Cedric and Guy investigate, the killer stalks Ash. Soon, the detectives fear she is the intended victim. Can Ash survive a killer she can't see?<br/><br/>This film highlights the capabilities and challenges of people with low vision. The sound editor zoomed in on the sounds Ash wanted to capture. It allows viewers to focus on what they hear rather than what they see. The script carefully weaves in the political issues Ash faces with employment, earnings, and government assistance. It's an eye-opening moment that doesn't take away from the film. Ash isn't a damsel in distress but a capable woman who fights for respect. Even if she must throw coffee at a cop. This film is informative, and the twist is different but expected.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/murderinthedark.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/murderinthedark.jpg"
     }
   }
 ];
