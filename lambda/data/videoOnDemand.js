3abf64049c4373a9360f4642984d865cffc703de
Tiffany Gray <KarateTD@yahoo.com>
2026-05-12 23:36:00 -0400

    add vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Give Me Back My Baby",
+		"review": "During an awards celebration, Olivia felt cramping. She passes out and is rushed to the hospital. Sadly, she suffers a miscarriage. Her husband, Logan, takes her home and helps her into bed.<br/><br/>Later, she and Logan go to a clinic. They scoff at the thought of adoption and say no to surrogacy. However, she gives in to the thought after a bad, drunken night. The agency matches them with potential surrogates, but the interviews leave the couple underwhelmed. The agency gives them one more surrogate. Logan and Olivia set up a meeting at a coffee shop, but the woman never arrives. To Olivia's surprise, an old college friend, Morgan, enters. They exchange numbers before Olivia and Logan exit.<br/><br/>Olivia and Morgan meet for coffee and discuss the past. Morgan listens to Olivia's struggles with infertility and offers to help. Olivia loves the idea because someone she knows will be carrying her child, not a stranger. She tells Logan about her idea and gets pushback. Olivia ignores his reservations and moves forward. She can't wait to become a mother. She has no idea that Morgan wants to keep the baby for herself, and she will kill to do it.<br/><br/>Morgan brings crazy to a new level, but Olivia is relentless. Her pursuit of motherhood clouds her judgment. However, she isn't working with all the information. Her mother, Kristy, and Logan are keeping secrets from her. This movie's insanity level is next to The Hand that Rocks the Cradle. This movie is for Lifetimers who need something macabre with a decent body count.<br/><br/>4 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/givemebackmybaby.jpeg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/givemebackmybaby.jpeg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Swapped",
 		"review": "Ollie is a curious Pooboo. When he ventures off the island again, his father, Caloo, and grandmother tell him about the story of the Pooboos. The creatures of the island lived in constant fear of the Treewolf. One day, their protector, Dzo, entered with a glowing pod. It allowed them to transform their bodies and talk to each other. The Treewolf attacked the Dzo and touched a pod. The pod changed the Treewolf into a Firewolf. It killed the Dzo, creating a dam. The dam separated the creatures. Caloo warns Ollie that he can't trust the other creatures, and he can only be safe on the island with his kind.<br/><br/>Ollie refuses to listen and forages for food. He stumbles across a baby Javan. He teaches the Javan how to open a shell for food. The Javan cries out to others, and they eat all the Pooboos food. Today, the Pooboos try to survive off the scraps the Javans leave behind. Ollie leaves their burrow for scraps, and a Javan attacks him. He discovers they hate light being reflected into their eyes. Ollie runs back with shards of glass, blinding the Javans. One returns and pushes Ollie into a deep hole. Ollie attempts to escape, but the Javan shoves him, forcing Ollie to touch the pod. It turns him into a Javan. When Ollie returns to the Pooboos, they run. Then, Ollie's mom, Callie, hears Ollie's voice from his Javan body. She fears the Javan ate him and attacks. Ollie escapes before falling into a river.<br/><br/>Ollie wakes up with three Javans, Ivy, Lily, and Violet, caring for him. But Ollie won't stay still. He must find another pod to change back. Boogle pops up from the water and tells him about another pod. Ollie runs to it with Ivy chasing after him. They get into a tussle, and Ivy touches it before Ollie can. It turns her into a Pooboo. Boogle tells them about a field of pods in a dangerous location. Ollie and Ivy must work together to find the pods and transform into their old bodies. Easier said than done.<br/><br/>This animated tale is about community and overcoming bias. This film's animation department created several intriguing creatures. The CGI uses incredible lighting techniques to highlight the creatures, environment, and time of day. It brings a new perspective of wonder to this film. And don't be fooled by the promotional images. These two don't swap bodies; they transform. It fails at keeping your attention during Ollie and Ivy's adventure. It's easy for the audience to become preoccupied and look away from the screen. As a result, Netflix is the perfect place for this picture. It lets you enjoy the quality while pausing and rewinding. This movie is a great family film for a lazy afternoon, but don't rearrange plans to watch it.<br/><br/>3 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Balls Up",
 		"review": "Elijah presents his pitch for his new invention to Regal Blue. It's a condom that covers all the male genitalia. His boss, Burgess, loves the product. She sees that Elijah has nervous sweats and stumbles when speaking to a group. So, she asks Brad, head of sales, to pitch the idea to the World Cup Committee. Elijah watches Brad pitch the product to Señor Santos, and he loves it. Brad renames it Balls Up, and it becomes the official condom of the World Cup. Santos takes Elijah and Brad to the club. Soon, Santos loses his sobriety and swings naked above the club. It causes a national incident, and Regal Blue loses the deal. Burgess fires Brad and Elijah.<br/><br/>Three months later, Brad sits at home when a courier brings a package to his door. It's airline tickets to Brazil and an all-access pass to the World Cup. He deduces that the person in charge forgot to cancel their accommodations. So, he takes advantage of the trip. To his surprise, Elijah is sitting next to him on the plane.<br/><br/>During the game, Brad gives Elijah a flask. He drinks his sorrows and thinks the mascot, a linguiça, is mocking him. He attacks the linguiça and accidently makes Brazil lose the World Cup. Elijah wakes up in jail with a hangover. The captain calls them to his office. He's releasing them and gives them instructions on how to walk out. Brad and Elijah realize he's setting them up. The Brazilians hate them. It won't be long before someone kills them. Can they get out of the country alive?<br/><br/>This film is neither funny, entertaining, nor shocking. The twists are predictable, and the crude humor isn't new. It's not worth a free trial on Prime Video. The plot is a huge waste of these actors' talents because fans know what they are capable of doing. The movie's only upside is the pacing. It has enough action to make you look up from the screen once in a while. Otherwise, it's a bore you can skip.<br/><br/>1.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Mike and Nick and Nick and Alice",
 		"review": "Kingpin Sosa throws a lavish party for his newly released son, Jimmy. This won't be the first party, as Sosa has scheduled three after-parties around town. He celebrates with the partiers but warns them that he knows someone ratted out Jimmy. He tells them the rat will die tonight before starting the music again. Loan shark Nick stands in the corner and watches the festivities. His wife, Alice, kisses him before leaving. She questions his affection for her, but he remains cold.<br/><br/>Alice texts gangster Mike after leaving the party. He's waiting for her at the hotel. They have been having an affair since they met at Jackie's wedding. Alice told her friend, inventor Symon, that she wants to leave Nick. However, she fears Nick's violent tendencies. Mike hears banging at the door. Nick asks Mike to go have dinner with him across the street.<br/><br/>At dinner, Mike admits he wants to leave the organization. He has one more job for Mike before they part ways. Nick drives Mike to a house and tells Mike to drug the person inside. Mike walks to the front porch, knocks on the door, and gasps when he sees the person behind it. It's Nick. After a battle, Mike and Nick put Nick in Nick's trunk. Mike demands to know what's going on. Nick confesses that he discovered the affair. To get Mike back, he told Sosa that Mike ratted Jimmy out. Sosa will successfully send an assassin to kill Mike. However, Nick realized he had made a mistake. He uncovered that Symon created a time travel machine. He traveled back in time to stop himself from getting Mike killed. Can Nick keep himself from killing Mike to correct his future?<br/><br/>This film merges several genres into one. It has a buddy-comedy aspect between Mike and future Nick. Then, it offers viewers an action film with an assassin's tale. Finally, the plot adds time travel for sci-fi lovers. It answers an age-old question about time travel with one stab. The fight scenes and graphics aren't revolutionary, but they are entertaining. The dialogue gives respect to Gilmore Girls with a spirited debate. While it won't make you a Hulu subscriber, it's worth a free trial.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Tyler Perry's Joe's College Road Trip",
 		"review": "Brian runs to Aunt Madea's home after hearing his son, BJ, disrespect the importance of HBCUs. He realizes that BJ doesn't understand the issues in the African American community. And he isn't properly prepared to handle how others may perceive him despite his accomplishments. BJ plans to drive from Georgia to Pepperdine University with this friends. Brian asks his father, Joe, and Madea for help. Madea suggests Joe take BJ on his road trip, teaching BJ about his culture along the way. Brian refuses to let Joe be alone with BJ. So, Madea and Joe kick Brian out of the house. Later, Brian changes his mind. This pimp with a 3rd grade education will teach BJ a lesson in African American culture, food, and history. But will BJ survive the trip unscathed?<br/><br/>The film starts with Joe's background and warns the viewers about his language. Like most Tyler Perry comedies, this film is wild and shocking, but ends with a compelling footnote hidden in the images. Tyler made Brian a man between two worlds. Joe was a hard and inappropriate father. Brian swore to be a better father than he was. However, he realizes he overcorrected and hadn't prepared BJ for a world outside his community. This movie could teach viewers a lesson. However, the message will get lost in all the mayhem. This picture is what you expect and doesn't venture off Tyler's comedy formula.<br/><br/>3 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/joescollegeroadtrip.jpg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/joescollegeroadtrip.jpg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Relationship Goals",
-		"review": "Leah's friends, co-anchor Brenda and make-up artist Treese, meet Leah outside their job and squeal. Today, their boss, Dan, will pick the new showrunner. Leah knows the job is hers. Until Dan calls her into the office and says the higher-ups don't believe she is a team player. They want to bring in someone from the outside to see if they are a better fit. Leah is still in the running, but she must prove herself. Leah's competition is Jarrett. She knows the name too well. They dated in the past, and he cheated on her. Since then, she's avoided him.<br/><br/>Jarrett arrives, and everyone, except Leah, greets him warmly. The crew meets with Dan to discuss Valentine's Day programming. Jarrett presents an idea. He's read a book called Relationship Goals. It helped him reimagine what love is, refining his aim. Jarrett dates with intention, throwing away his player card. Secretly, he's still in love with Leah. Dan loves the idea. He wants Jarrett and Leah to work on the feel-good segment together. Jarrett says yes. As Leah starts to reject the job, Dan mentions that she's not being a team player. She grits her teeth and agrees. She will take over the project, produce quality work, and eliminate Jarrett. Will she succeed, or will Jarrett win her over?<br/><br/>This film, inspired by the novel 'Relationship Goals' by Michael Todd, is a Lifetime movie with better wardrobe or a Hallmark movie with sex appeal. Outside of the main storyline, Treese and Brenda also have relationship problems. Viewers know how the film will end, so you look forward to the laughs. The script places them few and far between. Between three storylines, the film drags. The comic relief of Treese and Leah's assistant, Roland, shines in this picture. This film is for the rom-com lover with a slightly spiritual focus.<br/><br/>3.5 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/relationshipgoalsprimevideo.jpeg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/relationshipgoalsprimevideo.jpeg"
 		}
 	}
 ];
@@ -55,6 +55,8 @@ module.exports = videoOnDemand;
 */
 
 /*
+
+
 
 
 */
