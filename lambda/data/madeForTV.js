bf90aad3d7ca6f4aba14c56cd9054276795677f7
Tiffany Gray <KarateTD@yahoo.com>
2026-05-31 20:01:16 -0400

    add 2 tv movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Haunted Harmony Mysteries: Key to the Castle",
+    "review": "Gethsemane sees smoke bellowing from the Dorian Castle. She calls the fire department before entering. Gethsemane enters but can't find the source. The director, Roj, yells cut because Gethsemane ruined their shot. Roj is shooting a paranormal show, Before and Afterlife, hosted by ex-spouses Kent and Ciara. Ciara pulls Gethsemane aside because the local pub owner, Siobhan, told Ciara about Gethsemane solving Eamon's murder with supernatural messages. Gethsemane chuckles and says Siobhan is mistaken. Ciara understands Gethsemane's apprehension. She offers to give Gethsemane a tour of the castle and brunch. Gethsemane agrees.<br/><br/>Gethsemane goes to the local pub and approaches Siobhan. She apologizes for contacting Ciara, but she loves the show and the controversy. Paranormal investigator Ciara worked with medium Ed James. Ed and Ciara were in talks with networks to develop a show together. During a public reading, Ciara's then-husband, Kent, exposed Ed. The exposé ruined Ed's career and marriage. His wife died shortly after. Ciara couldn't look at Kent the same. She divorced him shortly after, but now they still work together to create the show.<br/><br/>Gethsemane arrives at the castle. She cautiously approaches Ciara, having a heated exchange with the cinematographer, Eliza. Ciara tells Gethsemane they will get brunch after they wrap the next scene. Kent introduces himself to Gethsemane. He walks Gethsemane around the set and introduces her to his crew. Hardy is the sound guy; Zak is Roj's assistant; this is his first job, and Eliza is Kent's protege. Soon, they hear a scream. Gethsemane and Kent follow the sound, and they can't believe their eyes. Kent's dead body is on the floor. Eamon appears to help Kent get through the initial shock. When the inspector labels Kent's death a murder, Gethsemane and Eamon are on the case. Can they find Kent's killer before someone else on the set dies?<br/><br/>Inspired by Gethsemane Brown Mysteries by Alexia Gordon, the third installment in this series has Gethsemane hiding her abilities around eagle-eyed guests. The script has Gethsemane and Eamon working together and simultaneously. Eamon also helps Kent cope with being a ghost. The plot adds to Gethsemane's love triangle, but the mystery is more compelling than the scandal. The mystery has several suspects with different motives. And the castle setting adds a layer of intrigue. While this movie builds on the relationship between Gethsemane, Eamon, and the afterlife, the love story falls flat.<br/><br/>4.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/hauntedharmonymsyterieskeytothecastle.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/hauntedharmonymsyterieskeytothecastle.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "When I Say I Do",
+    "review": "K-9 handler Ali listened to Clint and Lisa's radio show while driving down the road. She almost hits a car in the middle of the road while tending to her dog, Grover. Ali pulls alongside the car and asks the driver, Shawn, if he's lost. She can tell by his uniform that he's a first responder like her. Ali tells him to follow her. They arrive at the search-and-rescue headquarters. Two hikers are missing, and police fear they are in danger. Ali and Grover find clues along the way and locate the hikers at the bottom of a cliff. Ali radios her location to the team. Shawn and his friend, rescue specialist Brett, arrive. Brett prepares the ropes for Shawn to rappel to the hikers. Shawn tries to get Ali's name, but she refuses to give it to him.<br/><br/>After a successful rescue, Ali sees Shawn at the bar. He tries to pay for her meal, but she says no. He takes it as a rejection. Unsure of what to do, Shawn calls into the Clint and Lisa Show. He admits that he ended his engagement. It haunts him that he never apologized to his ex-fiancée. They agree with Brett and tell him to ask Ali out, but to ask his ex for forgiveness first. It will help him close the door on his past so he can look to the future.<br/><br/>Shawn asks Ali on a date, and to his surprise, she says yes. During their meal, she admits that she doesn't date co-workers. Her husband, Kent, died on duty, and it broke her heart. However, they decide to throw caution to the wind and begin their relationship. But will their relationship survive their fears?<br/><br/>Inspired by Clint and Lisa's song, When I Say I Do, this movie follows a couple through the ups and downs of a relationship. The writers introduced action and life-altering events to heighten the film's suspense. The leads have relationship issues to overcome, allowing them both to grow as individuals and a couple. This movie isn't the archetype of romance storytelling. It's funny, heartbreaking, and captivating. Lifetimers should save this movie for a night in with heart-pounding romance.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/whenisayido.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/whenisayido.jpg"
+    }
+  },
+  {
+    "option": "3",
     "mtitle": "True Justice: Eye for an Eye",
     "review": "Liam gets a tip from PJ about Sharon's case. PJ believes Sharon was falsely accused of murdering Katherine and dragging her body into the snow. The police uncovered that Sharon and Katherine were part of the Parent School Coalition, or PSC. Sharon became disenfranchised when Katherine's daughter bullied hers, triggering an extreme asthma attack. Sharon decided to join the coalition and dethrone President Katherine. Other members accused Sharon of being enraged and an addict. The team decides to examine PJ's files before taking the case.<br/><br/>Liam, Sarah, Casey, and Eli read through the reports. PSC member Cherelle witnessed Katherine and Sharon fighting a lot, Kimber made scathing remarks about Sharon in the press, and Tarik discussed Sharon's drug abuse. To make matters worse, Katherine's husband, Austin, is best friends with the DA. The one person on Sharon's side is her husband, Dave. After reviewing the case files, the Justice Warriors believe Sharon is innocent, but can they prove it?<br/><br/>The Justice Warriors can do it all. They infiltrate, collect evidence, run diagnostics, and solve the crime. Luckily, they understand the law and how to get around it. The PSC is a Greek tragedy waiting to happen. The only thing missing from this organization was an affair. The crime is easy to follow, and the plot offers a murder board from the beginning. However, showing the after effects of being falsely accused is the film's most honest aspect. This viewpoint will only strengthen the Warriors' resolve. Also, viewers get 3 love stories. This installment is as strong as the first and guarantees more wonderful additions in the future. It is something you want to keep on your DVR to watch again.<br/><br/>4.5 out of 5 stars.",
     "image": {
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "4",
     "mtitle": "Everyone Has Something to Hide",
     "review": "Kathy and her son, Noah, escape her abusive husband, Dan, with the help of her friend, Brooke. Brooke introduces Noah to her son, Ethan. They play together and become fast friends. Six years later, Noah composes music for the band, and Ethan is a high school football star. Noah and Ethan stopped being friends after Ethan's girlfriend, Maddie, broke up with him and started dating Noah. ending.<br/><br/>Kathy works as a realtor but always looks over her shoulder for Dave. One night, the alarm at one of her properties triggers, but she ignores it. She knows Noah is there having a party. She decides to deal with him in the morning. At the party, Maddie asks Noah to come to the back patio. Soon, Ethan enters. At first, the two get along, but their camaraderie ends with Noah hitting Ethan.<br/><br/>Kathy arrives at the property to check the damage in the morning. She finds Ethan's dead body in a chair on the back patio. She can't believe it and calls the police. After they leave, Kathy confronts Noah about his deception. He swears he didn't do it, but rages like his father. Kathy believes him and will fight the town to prove his innocence. Who killed Ethan?<br/><br/>This movie is a strong mystery with several curveballs to keep Lifetimers watching. Soon, everyone will be a suspect to you. However, the conclusion becomes predictable when someone falls ill. Thankfully, Lifetimers will solve the case with Kathy. You will be glued to the TV to prove your suspicions correct. The final conclusion wraps up all the characters, their future, and the consequences. This film is not the best Lifetime mystery, but it deserves your uninterrupted attention.<br/><br/>3.5 out of 5 stars.",
     "image": {
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "3",
+    "option": "5",
     "mtitle": "Friends Until the End",
     "review": "Jules invites her friends Ellie, Taryn, Mya, and Amey to their annual brunch at an off-the-grid estate. Amey calls Taryn because she's concerned about Jules hosting. Last year, Jules didn't show up because her family lost their money, and she suffered emotionally and financially in her divorce. So, Jules missed the last brunch, and they slowly distanced themselves from her. The women arrive in separate cars and walk around the palatial estate. They quip about Jules Pinterest decorations.<br/><br/>Jules appears and invites them to the dining room table. During brunch, Jules takes slight digs at everyone after ignoring her for a year. She leaves the table, and the others roll their eyes at her dramatic nature. After talking amongst themselves, the women look for Jules. They find her dead in her room with red residue in her champagne glass. Then, they try to call for help, but their phones don't have a signal. The staff left, and the concrete fence is too tall to climb. While they argue, Kersten enters. They run out the door to catch her car, but it's too late. They explain the situation to Kersten and take her to Jules's room to show her the body. However, the body is gone. Someone is in the house and playing puppet master. Will they survive?<br/><br/>This film is the perfect display of a victim mentality. The culprit has a slight with every woman in the room, but they had a hand in their demise.  Luckily, the script allows them to push aside petty issues and focus on actual problems. They admit their faults and promise to be better. The conclusion is heartwarming, but the plot mirrors Bodies, Bodies, Bodies, without the blood, gore, and death. Lifetimers should watch this film when they have time off and a high tolerance for nonsense.<br/><br/>3 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/friendsuntiltheend.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/friendsuntiltheend.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Faith and Forgiveness: A Duck Dynasty Love Story",
-    "review": "Lisa dreamed of meeting a prince and living happily ever after like her favorite fairytales. The moment she met Alan, she thought he would be her Prince Charming. Years later, he sees her in school and asks her out on a date. They share ice cream and fall in love. After graduation, Alan gets drunk and throws glass bottles at the scoreboard. The police arrest him and bring him to his parents, Phil and Mrs. Kay. They decide to send Alan away to force him to focus on faith and rebuilding his life. Alan and Lisa promise to remain faithful to each other until he returns.<br/><br/>Alan stops contacting his family and Lisa while away. Mrs. Kay calls Alan's aunt to check on him. Lisa overhears that he's on a date with another girl and decides to break their arrangement. She starts dating other guys and becomes pregnant. Her mother convinces Lisa to end her pregnancy and complete high school. After 18 months, Alan returns with a battered face and body. Alan asks Lisa for a second chance. She tells him about her mistakes, but he already knew. They get married, and Lisa thinks happily ever after starts now. However, she will discover that fairytales aren't real and relationships require faith and forgiveness.<br/><br/>Surprisingly, this film is for everyone, even people who aren't fans of Duck Dynasty. In the script, Lisa doesn't apply the halo effect to her actions. She takes accountability for her misgivings and lets everyone know her mistakes. The clothing, makeup, and hair perfectly fit the era. And the actors' accents were spot-on. This movie is for Lifetimers who need a love-and-faith break from the mayhem.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/faithandforgivenessduckdynasty.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/faithandforgivenessduckdynasty.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "Best Served Cold: A Hannah Swensen Mystery",
-    "review": "District Attorney Chad visits Hannah's shop, The Cookie Jar Bakery, but he can't stay long. His co-worker, Lucas, gave his cases to Chad and disappeared. This occurrence isn't unusual because Lucas often needs space to regroup. However, things got dicey when Lucas's wife, Danielle, called the office looking for him. Hannah understands, but the missing lawyer gnaws at her. So she visits Danielle with blueberry lavender panna cotta. They sit and discuss what happened before Lucas left.  When he leaves, he goes fishing. But he left his equipment and phone behind. Danielle pleads with Hannah to find Lucas. She agrees to help and takes pictures of the home.<br/><br/>Chad handles Lucas's biggest case against forgers. Spouses, Mitzy and Paul Jacobson, sold gold Civil War coins. However, a client hired Hannah's mother, Delores, to investigate their shop for selling fakes. Delores went undercover, caused a distraction, and took pictures while Mitzy was outside. She captured the forging plate and gave the evidence to the police. Now, Delores is Chad's star witness. Mitzy's trial is first, and she maintains her innocence. During Chad's trial, Hannah will uncover that the cases are related. But what will she do when someone threatens her life, someone is lying, and someone turns up dead?<br/><br/>Based on Triple Chocolate Cheesecake Murders by Joanna Fluke, this mystery has two crimes for Hannah to solve. The writer carefully wove a tapestry of clues while continuing the overarching story of Chad and Hannah's romance. The mystery provides different theories and suspects, allowing Hallmarkies to create a murder board in their minds. And the motive will show you how cold a killer can be. So have your lemon whippersnappers, morning glory muffins, and butterscotch marshmallow cookies ready because you don't want to miss this movie.<br/><br/>5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/bestservedcoldhannahswensen.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/bestservedcoldhannahswensen.jpeg"
     }
   }
 ];
@@ -58,21 +58,36 @@ module.exports = madeForTV;
 /*
 
 {
-"option":"1",
-"mtitle":"Haunted Harmony Mysteries: Key to the Castle",
-"review":"<br/><br/>* out of 5 stars.",
+"option": "1",
+"mtitle": "Thou Shalt Not Commit Adultery",
+"review": "<br/><br/>* out of 5 stars.",
 "image": {
-  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/hauntedharmonymsyterieskeytothecastle.jpg",
-  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/hauntedharmonymsyterieskeytothecastle.jpg"
-}
-},
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thoushaltnotcommitadultery.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thoushaltnotcommitadultery.jpg"
+}},
 {
-"option":"2",
-"mtitle":"When I Say I Do",
-"review":"<br/><br/>* out of 5 stars.",
+"option": "2",
+"mtitle": "Her Husband's Double Life",
+"review": "<br/><br/>* out of 5 stars.",
 "image": {
-  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/whenisayido.jpg",
-  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/whenisayido.jpg" 
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/herhusbandsdoublelife.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/herhusbandsdoublelife.jpg"
+}},
+{
+"option": "3",
+"mtitle": "Checkmate",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/checkmate-lifetime.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/checkmate-lifetime.jpg"
+}},
+{
+"option": "4",
+"mtitle": "Where the Heart Lands",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/wheretheheartlands.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/wheretheheartlands.jpg"
 }}
 
 
