a5a099e171559b53ffb3833d5b659700eda4c59c
Tiffany Gray <KarateTD@yahoo.com>
2026-06-06 03:48:57 -0400

    add office romance

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Office Romance",
+		"review": "CEO Jackie Cruz arrives at AirCruz and goes over the latest lawsuit with her right-hand woman, Sydney. Another airline is suing Jackie for poaching their deal at a major airport. Jackie gets her corporate lawyer, Peter, on the phone. He grabs his breakfast burrito and assures her he has a winning strategy. Then, he chokes, and EMTs rush him to the hospital.<br/><br/>Sydney grabs the next lawyer in line, Daniel, and sends him to Jackie's office. They arrive at the negotiating table, and Daniel takes control of the meeting. They return to Jackie's office, and she thanks him for his thorough work. When he shakes her hand, Jackie looks down and sees his erection. She demands he leave, and Daniel makes a hasty exit. Jackie blushes over him in bed, while embarrassment keeps Daniel awake.<br/><br/>Before work, Daniel visits the local prison to visit his sister, Lizzy. Daniel and Lizzy are immigrants. If he loses his job, he could be deported. Daniel won't be able to see Lizzy again. He must steer clear of Jackie.<br/><br/>To everyone's surprise, Peter arrives at work on a scooter. He's ready to take over the lawsuit, but Jackie says no. She wants Daniel to stay on the case. So, Peter quits, triggering a board meeting. The board believes Peter's resignation is a sign of Jackie's unstable leadership. They think she should step down. Her father, Captain Jack Cruz, saunters into the room and defends Jackie. They relent and allow her to remain CEO. Jackie knows the board is looking for an excuse to get rid of her. And an inappropriate relationship with a co-worker is the leverage they need. Jackie swears she will stay away from David. But they can't deny the white-hot heat between them. What will they do when their secret affair becomes corporate fodder?<br/><br/>This edgy comedy feels forced, and the dialogue is mildly entertaining. It's their awkwardness that captivates viewers. Also, the side characters, Lizzy and Sydney, are doing the heavy lifting. Their straightforward comic relief and physical comedy outshine every aspect of this film. This movie found the perfect home on Netflix.<br/><br/>2.5 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/officeromance.jpg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/officeromance.jpg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Miss You, Love You",
 		"review": "Jamie gets piecemeal texts from his boss, Tyler. Jamie waits for the last message to appear before responding. He promises to look after Tyler's mother, Diane, during a difficult time. She abruptly speaks to him while opening the door. She wants to know why Tyler isn't there helping her. Jamie explains that Tyler is out of the country and handling dangerous business. Until then, he will help her plan the funeral for her husband, Henry. Diane immediately interrogates Jamie and tries to push him away. However, he refuses to leave. They will remove the cone of silence and share unbridled honesty. Can they get through Henry's funeral without killing each other?<br/><br/>This drama concludes with peace and acceptance, not flowers and songbirds. The writer didn't force a romance during Diane's moment of loss. She's in different stages of grief at the same time. Diane unloads on Jamie, but not to be mean. The script allows her to see herself in Jamie. And Jamie sees his future in her. The plot doesn't make him an apologetic sympathizer. The dialog pushes him to fight back and match her wit. It's shocking, funny, and heartfelt. This movie has an independent feel with broad appeal. This isn't a glamor movie but a focused snapshot of life. This film found the perfect home on HBO Max.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Tom Clancy's Jack Ryan: Ghost War",
 		"review": "Jack Ryan sits quietly on the bench when he notices someone is following him. He looks up and makes eye contact with James Greer. James wants to hire Jack for a friendly handoff after his meeting in Dubai. Jack reminds James that he's a civilian now. James knows this cover isn't making Jack happy, so he assures Jack that Mike will be with him. Mike greets Jack, and Jack relents. Jack will meet with Nigel Cooke to retrieve information he obtained during a mission and return it to James.<br/><br/>Jack finishes his business in Dubai and meets with Nigel. Nigel is upset because he doesn't trust anyone but James with the information. Instead of giving Jack the intel, Nigel gives Jack an empty pack of cigarettes before someone kills him. Jack, with Mike following close by, runs after the attacker. However, they lose him when MI-6 officer Emma stops them.<br/><br/>Emma informs Jack that the killer was Liam Crown. Liam was a member of Project Starling. James admits to creating the group but swears he disbanded it. However, Liam has resurrected the group to prove it's needed. Liam threatens to kill James if Jack doesn't return what Nigel gave them. Jack agrees to retrieve the information and return it to Liam. However, things go awry, and someone dies. Jack seeks revenge and gets pulled deeper into a life he left behind. Will he be able to pull himself out?<br/><br/>This movie moves like Mission: Impossible but doesn't quite capture your undivided attention. The themes aren't unfamiliar to fans of the genre. However, the acting is spot-on and compelling. Unlike other action films, this film has an unmatched dry humor that expertly breaks apart the action. The stunts are well timed, smooth, and masterfully choreographed. Fans of the series won't be disappointed.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Give Me Back My Baby",
 		"review": "During an awards celebration, Olivia felt cramping. She passes out and is rushed to the hospital. Sadly, she suffers a miscarriage. Her husband, Logan, takes her home and helps her into bed.<br/><br/>Later, she and Logan go to a clinic. They scoff at the thought of adoption and say no to surrogacy. However, she gives in to the thought after a bad, drunken night. The agency matches them with potential surrogates, but the interviews leave the couple underwhelmed. The agency gives them one more surrogate. Logan and Olivia set up a meeting at a coffee shop, but the woman never arrives. To Olivia's surprise, an old college friend, Morgan, enters. They exchange numbers before Olivia and Logan exit.<br/><br/>Olivia and Morgan meet for coffee and discuss the past. Morgan listens to Olivia's struggles with infertility and offers to help. Olivia loves the idea because someone she knows will be carrying her child, not a stranger. She tells Logan about her idea and gets pushback. Olivia ignores his reservations and moves forward. She can't wait to become a mother. She has no idea that Morgan wants to keep the baby for herself, and she will kill to do it.<br/><br/>Morgan brings crazy to a new level, but Olivia is relentless. Her pursuit of motherhood clouds her judgment. However, she isn't working with all the information. Her mother, Kristy, and Logan are keeping secrets from her. This movie's insanity level is next to The Hand that Rocks the Cradle. This movie is for Lifetimers who need something macabre with a decent body count.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Swapped",
 		"review": "Ollie is a curious Pooboo. When he ventures off the island again, his father, Caloo, and grandmother tell him about the story of the Pooboos. The creatures of the island lived in constant fear of the Treewolf. One day, their protector, Dzo, entered with a glowing pod. It allowed them to transform their bodies and talk to each other. The Treewolf attacked the Dzo and touched a pod. The pod changed the Treewolf into a Firewolf. It killed the Dzo, creating a dam. The dam separated the creatures. Caloo warns Ollie that he can't trust the other creatures, and he can only be safe on the island with his kind.<br/><br/>Ollie refuses to listen and forages for food. He stumbles across a baby Javan. He teaches the Javan how to open a shell for food. The Javan cries out to others, and they eat all the Pooboos food. Today, the Pooboos try to survive off the scraps the Javans leave behind. Ollie leaves their burrow for scraps, and a Javan attacks him. He discovers they hate light being reflected into their eyes. Ollie runs back with shards of glass, blinding the Javans. One returns and pushes Ollie into a deep hole. Ollie attempts to escape, but the Javan shoves him, forcing Ollie to touch the pod. It turns him into a Javan. When Ollie returns to the Pooboos, they run. Then, Ollie's mom, Callie, hears Ollie's voice from his Javan body. She fears the Javan ate him and attacks. Ollie escapes before falling into a river.<br/><br/>Ollie wakes up with three Javans, Ivy, Lily, and Violet, caring for him. But Ollie won't stay still. He must find another pod to change back. Boogle pops up from the water and tells him about another pod. Ollie runs to it with Ivy chasing after him. They get into a tussle, and Ivy touches it before Ollie can. It turns her into a Pooboo. Boogle tells them about a field of pods in a dangerous location. Ollie and Ivy must work together to find the pods and transform into their old bodies. Easier said than done.<br/><br/>This animated tale is about community and overcoming bias. This film's animation department created several intriguing creatures. The CGI uses incredible lighting techniques to highlight the creatures, environment, and time of day. It brings a new perspective of wonder to this film. And don't be fooled by the promotional images. These two don't swap bodies; they transform. It fails at keeping your attention during Ollie and Ivy's adventure. It's easy for the audience to become preoccupied and look away from the screen. As a result, Netflix is the perfect place for this picture. It lets you enjoy the quality while pausing and rewinding. This movie is a great family film for a lazy afternoon, but don't rearrange plans to watch it.<br/><br/>3 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/swapped.jpg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/swapped.jpg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Balls Up",
-		"review": "Elijah presents his pitch for his new invention to Regal Blue. It's a condom that covers all the male genitalia. His boss, Burgess, loves the product. She sees that Elijah has nervous sweats and stumbles when speaking to a group. So, she asks Brad, head of sales, to pitch the idea to the World Cup Committee. Elijah watches Brad pitch the product to Señor Santos, and he loves it. Brad renames it Balls Up, and it becomes the official condom of the World Cup. Santos takes Elijah and Brad to the club. Soon, Santos loses his sobriety and swings naked above the club. It causes a national incident, and Regal Blue loses the deal. Burgess fires Brad and Elijah.<br/><br/>Three months later, Brad sits at home when a courier brings a package to his door. It's airline tickets to Brazil and an all-access pass to the World Cup. He deduces that the person in charge forgot to cancel their accommodations. So, he takes advantage of the trip. To his surprise, Elijah is sitting next to him on the plane.<br/><br/>During the game, Brad gives Elijah a flask. He drinks his sorrows and thinks the mascot, a linguiça, is mocking him. He attacks the linguiça and accidently makes Brazil lose the World Cup. Elijah wakes up in jail with a hangover. The captain calls them to his office. He's releasing them and gives them instructions on how to walk out. Brad and Elijah realize he's setting them up. The Brazilians hate them. It won't be long before someone kills them. Can they get out of the country alive?<br/><br/>This film is neither funny, entertaining, nor shocking. The twists are predictable, and the crude humor isn't new. It's not worth a free trial on Prime Video. The plot is a huge waste of these actors' talents because fans know what they are capable of doing. The movie's only upside is the pacing. It has enough action to make you look up from the screen once in a while. Otherwise, it's a bore you can skip.<br/><br/>1.5 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/ballsup.jpg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/ballsup.jpg"
 		}
 	}
 ];
@@ -56,13 +56,6 @@ module.exports = videoOnDemand;
 
 /*
 
-{
-"option":"1",
-"mtitle": "Office Romance",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/officeromance.jpg",
-	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/officeromance.jpg"
-}}
+
 
 */
