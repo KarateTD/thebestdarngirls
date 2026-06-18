4394a47a0fed01ab38042a404cf3c89b31f73ee5
Tiffany Gray <KarateTD@yahoo.com>
2026-06-17 23:39:16 -0400

    never change

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Never Change!",
+		"review": "Teachers and the government praise a new education bill that raises wages, reduces homework, and much more. While it's good for most Americans, it's bad for one batch of students: the North Meadows High 2008 Graduating Class. The new bill mandates that students must attend school for 180 days. A tornado cut their classes short by two weeks. So, the government revoked their diplomas. They have to return to high school to finish their two weeks, take their final exams, and graduate.<br/><br/>Amelia hears the news as she packs her things. Now, she fears returning to high school as an unaccomplished loser. Tedi, a married father, owns a bar. To the world, he looks successful, but he doesn't feel like he has achieved what he desired. Curtis dreams of Amelia from theater class. Sunny was the school's wild child. Today, much hasn't changed, including his burning passion for this ex-girlfriend, Katie. Newscaster Katie reports on the bill and melts down on camera when she realizes it's her graduation class. The class of 2008 is returning, but can they change? Will they learn to change? They have two weeks to find out. And if they don't graduate, they will have to repeat the 12th grade.<br/><br/>This parody comedy has hidden jokes in the background. So let your eyes wander the screen as the main characters speak. It's obvious that the actors ad-libbed some of their dialogue because the jokes are cute or mildly shocking. But nothing will make you laugh out loud or clutch your pearls. If you are expecting the Scary Movie version of The Breakfast Club, then don't watch this film. If you need a mindless, cheap laugh that requires little to no attention, then this is the perfect movie for you.<br/><br/>2.5 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/neverchange.jpg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/neverchange.jpg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Office Romance",
 		"review": "CEO Jackie Cruz arrives at AirCruz and goes over the latest lawsuit with her right-hand woman, Sydney. Another airline is suing Jackie for poaching their deal at a major airport. Jackie gets her corporate lawyer, Peter, on the phone. He grabs his breakfast burrito and assures her he has a winning strategy. Then, he chokes, and EMTs rush him to the hospital.<br/><br/>Sydney grabs the next lawyer in line, Daniel, and sends him to Jackie's office. They arrive at the negotiating table, and Daniel takes control of the meeting. They return to Jackie's office, and she thanks him for his thorough work. When he shakes her hand, Jackie looks down and sees his erection. She demands he leave, and Daniel makes a hasty exit. Jackie blushes over him in bed, while embarrassment keeps Daniel awake.<br/><br/>Before work, Daniel visits the local prison to visit his sister, Lizzy. Daniel and Lizzy are immigrants. If he loses his job, he could be deported. Daniel won't be able to see Lizzy again. He must steer clear of Jackie.<br/><br/>To everyone's surprise, Peter arrives at work on a scooter. He's ready to take over the lawsuit, but Jackie says no. She wants Daniel to stay on the case. So, Peter quits, triggering a board meeting. The board believes Peter's resignation is a sign of Jackie's unstable leadership. They think she should step down. Her father, Captain Jack Cruz, saunters into the room and defends Jackie. They relent and allow her to remain CEO. Jackie knows the board is looking for an excuse to get rid of her. And an inappropriate relationship with a co-worker is the leverage they need. Jackie swears she will stay away from David. But they can't deny the white-hot heat between them. What will they do when their secret affair becomes corporate fodder?<br/><br/>This edgy comedy feels forced, and the dialogue is mildly entertaining. It's their awkwardness that captivates viewers. Also, the side characters, Lizzy and Sydney, are doing the heavy lifting. Their straightforward comic relief and physical comedy outshine every aspect of this film. This movie found the perfect home on Netflix.<br/><br/>2.5 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Miss You, Love You",
 		"review": "Jamie gets piecemeal texts from his boss, Tyler. Jamie waits for the last message to appear before responding. He promises to look after Tyler's mother, Diane, during a difficult time. She abruptly speaks to him while opening the door. She wants to know why Tyler isn't there helping her. Jamie explains that Tyler is out of the country and handling dangerous business. Until then, he will help her plan the funeral for her husband, Henry. Diane immediately interrogates Jamie and tries to push him away. However, he refuses to leave. They will remove the cone of silence and share unbridled honesty. Can they get through Henry's funeral without killing each other?<br/><br/>This drama concludes with peace and acceptance, not flowers and songbirds. The writer didn't force a romance during Diane's moment of loss. She's in different stages of grief at the same time. Diane unloads on Jamie, but not to be mean. The script allows her to see herself in Jamie. And Jamie sees his future in her. The plot doesn't make him an apologetic sympathizer. The dialog pushes him to fight back and match her wit. It's shocking, funny, and heartfelt. This movie has an independent feel with broad appeal. This isn't a glamor movie but a focused snapshot of life. This film found the perfect home on HBO Max.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Tom Clancy's Jack Ryan: Ghost War",
 		"review": "Jack Ryan sits quietly on the bench when he notices someone is following him. He looks up and makes eye contact with James Greer. James wants to hire Jack for a friendly handoff after his meeting in Dubai. Jack reminds James that he's a civilian now. James knows this cover isn't making Jack happy, so he assures Jack that Mike will be with him. Mike greets Jack, and Jack relents. Jack will meet with Nigel Cooke to retrieve information he obtained during a mission and return it to James.<br/><br/>Jack finishes his business in Dubai and meets with Nigel. Nigel is upset because he doesn't trust anyone but James with the information. Instead of giving Jack the intel, Nigel gives Jack an empty pack of cigarettes before someone kills him. Jack, with Mike following close by, runs after the attacker. However, they lose him when MI-6 officer Emma stops them.<br/><br/>Emma informs Jack that the killer was Liam Crown. Liam was a member of Project Starling. James admits to creating the group but swears he disbanded it. However, Liam has resurrected the group to prove it's needed. Liam threatens to kill James if Jack doesn't return what Nigel gave them. Jack agrees to retrieve the information and return it to Liam. However, things go awry, and someone dies. Jack seeks revenge and gets pulled deeper into a life he left behind. Will he be able to pull himself out?<br/><br/>This movie moves like Mission: Impossible but doesn't quite capture your undivided attention. The themes aren't unfamiliar to fans of the genre. However, the acting is spot-on and compelling. Unlike other action films, this film has an unmatched dry humor that expertly breaks apart the action. The stunts are well timed, smooth, and masterfully choreographed. Fans of the series won't be disappointed.<br/><br/>3.5 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Give Me Back My Baby",
 		"review": "During an awards celebration, Olivia felt cramping. She passes out and is rushed to the hospital. Sadly, she suffers a miscarriage. Her husband, Logan, takes her home and helps her into bed.<br/><br/>Later, she and Logan go to a clinic. They scoff at the thought of adoption and say no to surrogacy. However, she gives in to the thought after a bad, drunken night. The agency matches them with potential surrogates, but the interviews leave the couple underwhelmed. The agency gives them one more surrogate. Logan and Olivia set up a meeting at a coffee shop, but the woman never arrives. To Olivia's surprise, an old college friend, Morgan, enters. They exchange numbers before Olivia and Logan exit.<br/><br/>Olivia and Morgan meet for coffee and discuss the past. Morgan listens to Olivia's struggles with infertility and offers to help. Olivia loves the idea because someone she knows will be carrying her child, not a stranger. She tells Logan about her idea and gets pushback. Olivia ignores his reservations and moves forward. She can't wait to become a mother. She has no idea that Morgan wants to keep the baby for herself, and she will kill to do it.<br/><br/>Morgan brings crazy to a new level, but Olivia is relentless. Her pursuit of motherhood clouds her judgment. However, she isn't working with all the information. Her mother, Kristy, and Logan are keeping secrets from her. This movie's insanity level is next to The Hand that Rocks the Cradle. This movie is for Lifetimers who need something macabre with a decent body count.<br/><br/>4 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/givemebackmybaby.jpeg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/givemebackmybaby.jpeg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Swapped",
-		"review": "Ollie is a curious Pooboo. When he ventures off the island again, his father, Caloo, and grandmother tell him about the story of the Pooboos. The creatures of the island lived in constant fear of the Treewolf. One day, their protector, Dzo, entered with a glowing pod. It allowed them to transform their bodies and talk to each other. The Treewolf attacked the Dzo and touched a pod. The pod changed the Treewolf into a Firewolf. It killed the Dzo, creating a dam. The dam separated the creatures. Caloo warns Ollie that he can't trust the other creatures, and he can only be safe on the island with his kind.<br/><br/>Ollie refuses to listen and forages for food. He stumbles across a baby Javan. He teaches the Javan how to open a shell for food. The Javan cries out to others, and they eat all the Pooboos food. Today, the Pooboos try to survive off the scraps the Javans leave behind. Ollie leaves their burrow for scraps, and a Javan attacks him. He discovers they hate light being reflected into their eyes. Ollie runs back with shards of glass, blinding the Javans. One returns and pushes Ollie into a deep hole. Ollie attempts to escape, but the Javan shoves him, forcing Ollie to touch the pod. It turns him into a Javan. When Ollie returns to the Pooboos, they run. Then, Ollie's mom, Callie, hears Ollie's voice from his Javan body. She fears the Javan ate him and attacks. Ollie escapes before falling into a river.<br/><br/>Ollie wakes up with three Javans, Ivy, Lily, and Violet, caring for him. But Ollie won't stay still. He must find another pod to change back. Boogle pops up from the water and tells him about another pod. Ollie runs to it with Ivy chasing after him. They get into a tussle, and Ivy touches it before Ollie can. It turns her into a Pooboo. Boogle tells them about a field of pods in a dangerous location. Ollie and Ivy must work together to find the pods and transform into their old bodies. Easier said than done.<br/><br/>This animated tale is about community and overcoming bias. This film's animation department created several intriguing creatures. The CGI uses incredible lighting techniques to highlight the creatures, environment, and time of day. It brings a new perspective of wonder to this film. And don't be fooled by the promotional images. These two don't swap bodies; they transform. It fails at keeping your attention during Ollie and Ivy's adventure. It's easy for the audience to become preoccupied and look away from the screen. As a result, Netflix is the perfect place for this picture. It lets you enjoy the quality while pausing and rewinding. This movie is a great family film for a lazy afternoon, but don't rearrange plans to watch it.<br/><br/>3 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/swapped.jpg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/swapped.jpg"
 		}
 	}
 ];
@@ -57,5 +57,15 @@ module.exports = videoOnDemand;
 /*
 
 
+
+
+{
+"option": "1",
+"mtitle": "Stepfather",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/stepfather.jpeg",
+	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/stepfather.jpeg"
+}
 
 */
