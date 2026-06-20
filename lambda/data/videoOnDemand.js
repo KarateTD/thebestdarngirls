ef44dea5c0c187cd48e29bd508c20f0d8d350c50
Tiffany Gray <KarateTD@yahoo.com>
2026-06-20 01:30:49 -0400

    new vod

--- lambda/data/videoOnDemand.js
+++ lambda/data/videoOnDemand.js
@@ -1,6 +1,15 @@
 var videoOnDemand = [
 	{
 		"option": "1",
+		"mtitle": "Stepfather",
+		"review": "Asia's husband, Timmy, argues that she does nothing all day. She questions whether there is another woman. Timmy admits that he's seeing someone. Then he asks Asia for a divorce. Their daughter, Sasha, cries as she listens to them argue from her bedroom. Asia has no idea how she will break the news to their other daughter, Melanie, who is attending college.<br/><br/>One year later, Sasha and Asia argue in the grocery store. Sasha's tired of her mother's Mexican-inspired cuisine. A man, Darnell, interjects by telling Asia to add some yellow peppers to her food. They spark a conversation and playfully take verbal jabs at each other. Six months later, Asia and Darnell elope.<br/><br/>Asia and Darnell throw a party to celebrate their marriage. Melanie visits with her boyfriend, Brad, while on break. Melanie even calls him 'dad' because she holds Timmy in disdain for his actions. However, Sasha won't call him dad. Darnell laughs and says it will take time, but he will respect her choice. However, the voices inside his head refuse to let anyone ruin his chance to pick his perfect family. When others tried, he killed them. He's willing to do it again unless the police stop him first.<br/><br/>This thriller goes in-depth into the mind of a killer. It gives Darnell's background history and psychiatric diagnosis. You will understand what caused him to break down. This deep dive makes the film intriguing. The investigation moments give viewers a much-needed comedic break within the insanity. The scene of Darnell arguing with his other personalities displays the director's camera work and the actor's range. It makes the final scene stand out even more. This film ends with a possible part two. Hopefully, it can live up to the first.<br/><br/>3.5 out of 5 stars.",
+		"image": {
+			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/stepfather.jpeg",
+			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/stepfather.jpeg"
+		}
+	},
+	{
+		"option": "2",
 		"mtitle": "Never Change!",
 		"review": "Teachers and the government praise a new education bill that raises wages, reduces homework, and much more. While it's good for most Americans, it's bad for one batch of students: the North Meadows High 2008 Graduating Class. The new bill mandates that students must attend school for 180 days. A tornado cut their classes short by two weeks. So, the government revoked their diplomas. They have to return to high school to finish their two weeks, take their final exams, and graduate.<br/><br/>Amelia hears the news as she packs her things. Now, she fears returning to high school as an unaccomplished loser. Tedi, a married father, owns a bar. To the world, he looks successful, but he doesn't feel like he has achieved what he desired. Curtis dreams of Amelia from theater class. Sunny was the school's wild child. Today, much hasn't changed, including his burning passion for this ex-girlfriend, Katie. Newscaster Katie reports on the bill and melts down on camera when she realizes it's her graduation class. The class of 2008 is returning, but can they change? Will they learn to change? They have two weeks to find out. And if they don't graduate, they will have to repeat the 12th grade.<br/><br/>This parody comedy has hidden jokes in the background. So let your eyes wander the screen as the main characters speak. It's obvious that the actors ad-libbed some of their dialogue because the jokes are cute or mildly shocking. But nothing will make you laugh out loud or clutch your pearls. If you are expecting the Scary Movie version of The Breakfast Club, then don't watch this film. If you need a mindless, cheap laugh that requires little to no attention, then this is the perfect movie for you.<br/><br/>2.5 out of 5 stars.",
 		"image": {
@@ -9,7 +18,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "2",
+		"option": "3",
 		"mtitle": "Office Romance",
 		"review": "CEO Jackie Cruz arrives at AirCruz and goes over the latest lawsuit with her right-hand woman, Sydney. Another airline is suing Jackie for poaching their deal at a major airport. Jackie gets her corporate lawyer, Peter, on the phone. He grabs his breakfast burrito and assures her he has a winning strategy. Then, he chokes, and EMTs rush him to the hospital.<br/><br/>Sydney grabs the next lawyer in line, Daniel, and sends him to Jackie's office. They arrive at the negotiating table, and Daniel takes control of the meeting. They return to Jackie's office, and she thanks him for his thorough work. When he shakes her hand, Jackie looks down and sees his erection. She demands he leave, and Daniel makes a hasty exit. Jackie blushes over him in bed, while embarrassment keeps Daniel awake.<br/><br/>Before work, Daniel visits the local prison to visit his sister, Lizzy. Daniel and Lizzy are immigrants. If he loses his job, he could be deported. Daniel won't be able to see Lizzy again. He must steer clear of Jackie.<br/><br/>To everyone's surprise, Peter arrives at work on a scooter. He's ready to take over the lawsuit, but Jackie says no. She wants Daniel to stay on the case. So, Peter quits, triggering a board meeting. The board believes Peter's resignation is a sign of Jackie's unstable leadership. They think she should step down. Her father, Captain Jack Cruz, saunters into the room and defends Jackie. They relent and allow her to remain CEO. Jackie knows the board is looking for an excuse to get rid of her. And an inappropriate relationship with a co-worker is the leverage they need. Jackie swears she will stay away from David. But they can't deny the white-hot heat between them. What will they do when their secret affair becomes corporate fodder?<br/><br/>This edgy comedy feels forced, and the dialogue is mildly entertaining. It's their awkwardness that captivates viewers. Also, the side characters, Lizzy and Sydney, are doing the heavy lifting. Their straightforward comic relief and physical comedy outshine every aspect of this film. This movie found the perfect home on Netflix.<br/><br/>2.5 out of 5 stars.",
 		"image": {
@@ -18,7 +27,7 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "3",
+		"option": "4",
 		"mtitle": "Miss You, Love You",
 		"review": "Jamie gets piecemeal texts from his boss, Tyler. Jamie waits for the last message to appear before responding. He promises to look after Tyler's mother, Diane, during a difficult time. She abruptly speaks to him while opening the door. She wants to know why Tyler isn't there helping her. Jamie explains that Tyler is out of the country and handling dangerous business. Until then, he will help her plan the funeral for her husband, Henry. Diane immediately interrogates Jamie and tries to push him away. However, he refuses to leave. They will remove the cone of silence and share unbridled honesty. Can they get through Henry's funeral without killing each other?<br/><br/>This drama concludes with peace and acceptance, not flowers and songbirds. The writer didn't force a romance during Diane's moment of loss. She's in different stages of grief at the same time. Diane unloads on Jamie, but not to be mean. The script allows her to see herself in Jamie. And Jamie sees his future in her. The plot doesn't make him an apologetic sympathizer. The dialog pushes him to fight back and match her wit. It's shocking, funny, and heartfelt. This movie has an independent feel with broad appeal. This isn't a glamor movie but a focused snapshot of life. This film found the perfect home on HBO Max.<br/><br/>4 out of 5 stars.",
 		"image": {
@@ -27,21 +36,12 @@ var videoOnDemand = [
 		}
 	},
 	{
-		"option": "4",
+		"option": "5",
 		"mtitle": "Tom Clancy's Jack Ryan: Ghost War",
 		"review": "Jack Ryan sits quietly on the bench when he notices someone is following him. He looks up and makes eye contact with James Greer. James wants to hire Jack for a friendly handoff after his meeting in Dubai. Jack reminds James that he's a civilian now. James knows this cover isn't making Jack happy, so he assures Jack that Mike will be with him. Mike greets Jack, and Jack relents. Jack will meet with Nigel Cooke to retrieve information he obtained during a mission and return it to James.<br/><br/>Jack finishes his business in Dubai and meets with Nigel. Nigel is upset because he doesn't trust anyone but James with the information. Instead of giving Jack the intel, Nigel gives Jack an empty pack of cigarettes before someone kills him. Jack, with Mike following close by, runs after the attacker. However, they lose him when MI-6 officer Emma stops them.<br/><br/>Emma informs Jack that the killer was Liam Crown. Liam was a member of Project Starling. James admits to creating the group but swears he disbanded it. However, Liam has resurrected the group to prove it's needed. Liam threatens to kill James if Jack doesn't return what Nigel gave them. Jack agrees to retrieve the information and return it to Liam. However, things go awry, and someone dies. Jack seeks revenge and gets pulled deeper into a life he left behind. Will he be able to pull himself out?<br/><br/>This movie moves like Mission: Impossible but doesn't quite capture your undivided attention. The themes aren't unfamiliar to fans of the genre. However, the acting is spot-on and compelling. Unlike other action films, this film has an unmatched dry humor that expertly breaks apart the action. The stunts are well timed, smooth, and masterfully choreographed. Fans of the series won't be disappointed.<br/><br/>3.5 out of 5 stars.",
 		"image": {
 			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/jackryanghostwar.jpeg",
 			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/jackryanghostwar.jpeg"
-		}
-	},
-	{
-		"option": "5",
-		"mtitle": "Give Me Back My Baby",
-		"review": "During an awards celebration, Olivia felt cramping. She passes out and is rushed to the hospital. Sadly, she suffers a miscarriage. Her husband, Logan, takes her home and helps her into bed.<br/><br/>Later, she and Logan go to a clinic. They scoff at the thought of adoption and say no to surrogacy. However, she gives in to the thought after a bad, drunken night. The agency matches them with potential surrogates, but the interviews leave the couple underwhelmed. The agency gives them one more surrogate. Logan and Olivia set up a meeting at a coffee shop, but the woman never arrives. To Olivia's surprise, an old college friend, Morgan, enters. They exchange numbers before Olivia and Logan exit.<br/><br/>Olivia and Morgan meet for coffee and discuss the past. Morgan listens to Olivia's struggles with infertility and offers to help. Olivia loves the idea because someone she knows will be carrying her child, not a stranger. She tells Logan about her idea and gets pushback. Olivia ignores his reservations and moves forward. She can't wait to become a mother. She has no idea that Morgan wants to keep the baby for herself, and she will kill to do it.<br/><br/>Morgan brings crazy to a new level, but Olivia is relentless. Her pursuit of motherhood clouds her judgment. However, she isn't working with all the information. Her mother, Kristy, and Logan are keeping secrets from her. This movie's insanity level is next to The Hand that Rocks the Cradle. This movie is for Lifetimers who need something macabre with a decent body count.<br/><br/>4 out of 5 stars.",
-		"image": {
-			"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/givemebackmybaby.jpeg",
-			"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/givemebackmybaby.jpeg"
 		}
 	}
 ];
@@ -59,13 +59,6 @@ module.exports = videoOnDemand;
 
 
 
-{
-"option": "1",
-"mtitle": "Stepfather",
-"review": "<br/><br/>* out of 5 stars.",
-"image": {
-	"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/stepfather.jpeg",
-	"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/stepfather.jpeg"
-}
+
 
 */
