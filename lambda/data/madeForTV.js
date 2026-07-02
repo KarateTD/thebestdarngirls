fea3b624487e29f6ae57098019b7cc6c78887fe7
Tiffany Gray <KarateTD@yahoo.com>
2026-07-02 01:37:14 -0400

    add 4 movie

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,47 +1,47 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "Single Black Tenant",
+    "review": "Esme has fallen on hard times. She works as a mechanic at her best friend's garage, but it doesn't allow her to save enough for a deposit. Today, Lydia will drive Esme around to check out rooms for rent that don't require a deposit. Esme arrives late to the appointment, but the owner, Angie, is ecstatic to see her. However, Angie's husband, Nolan, isn't friendly. Esme reveals that she's handy, and Angie offers Esme a deal. She will lower the rent if Esme fixes up the old unit. Esme agrees, signs the paperwork, and moves in.<br/><br/>Later, Angie tells Esme that she and Nolan don't own the home. The original owner, Barbie, left and never came back. While fixing up the apartment, Esme finds Barbie's ID, bloody rags, and a horrible odor. She believes Nolan must have done something, and Angie is hiding it. When Esme reports her suspicions to the police, they label her a true-crime fanatic. Esme won't give up. She will prove that Nolan is behind Barbie's disappearance and free Angie from his control.<br/><br/>This film isn't about the culprit because Lifetimers can spot them from a mile away. Its focus is the motive, but the plot foreshadows too much and you can guess it. It ruins the suspense of the story. One beacon of hope is the friendship between Lydia and Esme. They see the wedge in their relationship and form a tighter bond. This film could be compelling, but it's too predictable.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/singleblacktenant.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/singleblacktenant.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "The Woman with Three Husbands",
+    "review": "Natalie arrives at her brother's home with solemn news. Their father died, but they feel nothing because their father was emotionally abusive. Natalie's brother, Marcus, invites her into his home to catch up. Moments later, a woman named Larissa enters and introduces herself as Marcus's wife. Then, two men enter the kitchen. Marcus stumbles during their introduction, and Larissa giggles. She tells Marcus to be honest. Jack and Nico are her husbands too. Marcus assures Natalie that he's happy and asks Natalie to stay for the night. Although Jack isn't happy about the arrangements.<br/><br/>The family goes to the pub with Natalie and has a fun night. Natalie walks to the bar and sparks a conversation with Ken. He gossips about every person there. When Natalie points to Marcus, he warns her to stay away from them. He doesn't care about their alternative lifestyle, but he believes one of them is a killer. She begs her brother to leave, but he refuses. He walks away, and Natalie hides as she overhears Nico and Jack arguing. After Nico exits, Jack tells her to leave now, or he will make her.<br/><br/>Marcus notices that Jack missed a critical chore in the morning. So the family and Natalie search the property for him. They find Jack's body in an embankment. Natalie fears her brother is living with a murderer. She won't leave or believe the police's findings. She will stay and help on the farm while trying to find a killer.<br/><br/>This script dips its toe into polyamorous relationships without being judgmental. As each husband talks about meeting Larissa, the viewer will notice a common thread between their story. Now, the killer isn't surprising, but the motive will make your head spin. That final twist saves the film from another Lifetime lost cause. Viewers are left to wonder what the difference is between this family and a cult. This plot is wild, and the pacing is perfect.<br/><br/>3.5 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thewomanwiththreehusbands.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thewomanwiththreehusbands.jpg"
+    }
+  },
+  {
+    "option": "3",
+    "mtitle": "My Husband's Mistress",
+    "review": "Spouses Anna and Brill run a wellness app and desire to go public. Brill is the CTO but doesn't believe Anna respects his decisions. Anna, the CEO, informs Brill that business can't be handled like code. It takes a cool head and patience. Brill runs into Anna's brother, Quan, and loudly wonders when Quan will leave. Anna's best friend calls and offers a girls' yoga night to take Anna's mind off Brill.<br/><br/>Anna arrives at the QHOA studio, but her friend isn't there. As she continues to express regret, Anna sees Brill with the instructor, Ophelia. She sees them kiss while sharing 3 drops of liquid from an unlabeled vial. Ophelia trusts that Brill will mass-produce it to sell in her shop, but he wants to steal it from her while cheating on his wife. They leave for dinner, and Anna breaks in. She destroys Ophelia's studio before stealing the vial. She sits with Quan and comes to a decision. She won't get mad; she will get even. She will take their company, end Brill's affair, and steal his deal with Ophelia. She believes her plan is foolproof, but will it work when Brill starts snooping around?<br/><br/>This film will keep you on the edge of your seat. As Brill and Anna go toe-to-toe to be on top, they will stoop to any low. The plot has many close calls; you will hold your breath. Anna's heart breaks after discovering the affair, but she quickly regroups and plans. The writer created a well-rounded female lead with integrity and street smarts. This sexy drama deserves your attention.<br/><br/>4 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/myhusbandsmistress.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/myhusbandsmistress.jpg"
+    }
+  },
+  {
+    "option": "4",
+    "mtitle": "The Love Heist",
+    "review": "Kayli arrives at the Park Lux hotel and grabs a luggage cart. The head of security, Liam, promptly informs her that she can't take it. Security is on high alert because celebrities are in town for a week-long gala. He escorts Kayli safely to her room, and she unpacks the looks for actress Rini-Jean. Rini-Jean hired Kayli's boss, Celine, to design one-of-a-kind styles for each event. Kayli tells Liam that an important dress will be arriving soon. It's Elizabeth Taylor's 1970s Academy Award dress. The Chicago Fashion Institute loaned the dress to them, and it must be protected at all times. Soon, Celine arrives with exciting news. Her son plans to get married in France in a few days. She doesn't want to miss it. So she will leave Kayli in charge. Kayli is unsure because she once led a design that ruined an actress's career. She assures Kayli that everything will be fine if she sticks to the plan. Celine leaves.<br/><br/>The dress arrives, and Liam brings the wardrobe upstairs. He will return for the wardrobe after Kayli makes changes and lock it away until Rini-Jean wears it. Rini-Jean arrives and tries on the looks, but she hates them all. Kayli leaves the hotel and stops at a consignment shop. She finds everything she needs and creates something new for Rini-Jean. Rini-Jean loves it. When Liam arrives for the dress, Kayli states that she hasn't made the alterations because she had to leave to make Rini-Jean's outfit. Liam's aghast at Kayli's admission because she wasn't supposed to leave the dress. They open the wardrobe to discover that the Elizabeth Taylor dress is gone. Liam must find the culprit. His boss, Errol, informed Liam that he is up for a promotion. Liam will lose it if they can't find the gown. Kayli will lose her job with Celine. Can they save their reputations and find the dress while falling in love with each other?<br/><br/>This mystery is intriguing, but sadly, it takes a back seat to the predictable love story. The enigma is a subplot to bring the leads together. While the chemistry is there, you won't care. Even the culprit's reveal feels like an afterthought. If you take out the mystery, it wouldn't have an effect on the overall storyline. The inspired fashion stands out, and it will catch your eye. It's not enough to make you sit and watch this movie. Viewers should watch this movie if they like a fun rom-com, but not a mystery.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/theloveheist.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/theloveheist.jpg"
+    }
+  },
+  {
+    "option": "5",
     "mtitle": "Don't Trust the Girls Upstairs",
     "review": "Ashley gets a call from police officers. Her sister, Courtney, drowned in her pool after a fall. Courtney leaves behind her daughter Serena. Ashley tells police that she will be there tomorrow to collect Serena. Ashley hangs up the phone, and her husband, Elliot, wonders if that's a good idea. She and Courtney haven't spoken for over a decade because Courtney had a volatile temper. He fears letting a stranger into their house, risking the safety of their children, Luna and Max. Ashley reiterates that family isn't strangers.<br/><br/>Ashley arrives at Courtney's home and finds police tape. While they believe she got drunk, slipped, and fell into the pool, they still want to investigate and do a toxicology report. Ashley goes upstairs to find Serena. Serena keeps her distance and questions where Ashley's been. Ashley explains her complicated past with Courtney and brings Serena home. When strange stuff starts happening, Ashley believes Serena has Courtney's anger issues. But Serena says she's not the cause. As Serena and Luna become friends, Ashley questions if Serena is a bad influence. Or worst, Luna is the ring leader. Who can Ashley trust?<br/><br/>Based on My Sister's Daughter by Liv Constantine, this film is a lesson in misdirection. It leads Lifetimers down the trope rabbit hole. This film's pacing is solid with no wasted scenes. Also, the natural lighting and clean text on the screen make it easy for Lifetimers to see and read information that's important to the plot. While the acting feels flat at times, the well-written story stands out. This film deserves a second viewing.<br/><br/>4 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/donttrustthegirlupstairs.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/donttrustthegirlupstairs.jpg"
-    }
-  },
-  {
-    "option": "2",
-    "mtitle": "Who Killed My Husband's Mistress",
-    "review": "Megan believes she knows her husband's affair partner, Lisa, although her husband, Nathan, denies it. She calls her sister, Molly, while she sits outside Lisa's home. She plans to meet Lisa and have a civil conversation. However, Megan watches someone push Lisa off her balcony. After the police question her, Megan returns home. She finds the strength to confront Nathan. He tries to convince Megan that she's insecure, but Megan doesn't fall for it. Nathan leaves and stays with his stepmother, Gabrielle.<br/><br/>Megan works with a lawyer to draw up a divorce decree. She travels to Gabrielle's home and presents it to Nathan. As Nathan sneers at Megan with contempt, she steps to the side, and a bullet goes through the window and into Nathan's chest. Nathan dies on the scene. Police uncover Lisa and Nathan's affair along with Lisa's pregnancy. The test revealed that Nathan was the father. This gives Megan a motive, plus she was at both crime scenes. The detective can't hold her but asks her not to leave the area.<br/><br/>Megan informs her assistant, Molly, that she needs to take time off from work to handle Nathan's affairs. Nathan's partner, Alberta, will get his half of their company. And Megan has control of Nathan's estate, including Gabrielle's home. As police look into her, she uncovers Nathan's secrets. However, it's not long before the killer targets her. Can Megan find out who killed Nathan and Lisa before someone ends her life?<br/><br/>This film will get you, and all the clues were there. The writer played on Lifetimers expertise to predict the most likely suspect. And you will be wrong. Every character has a secret, suspicious behavior, and a link to Nathan. While the pace is extremely slow, the final reveal makes it all worth it. So give this film a chance and put on your thinking cap.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/whokilledmyhusbandsmistress.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/whokilledmyhusbandsmistress.jpg"
-    }
-  },
-  {
-    "option": "3",
-    "mtitle": "Ex Door Neighbor",
-    "review": "Imani excitedly drags her fiancé, Dean, to their condo appointment. The doorman, Teddy, gives them an abrupt greeting while the property manager, Nicolette, shows them the apartment. Deon wonders why an apartment is suddenly available. Imani pulls him aside and urges him to get over his fears. They sign the contract and purchase the condo.<br/><br/>After they move in, Imani's sister, Jada, visits. Later, Imani and Deon walk to the lobby, and someone calls Deon Woody. Dean turns to see his ex-wife, Tamera. The building owner, Arthur, is her client, and Arthur wanted to settle things after his sister, Madeline, died. Imani warns Deon to keep his distance. However, it won't be easy after Arthur dies and Tamera rent his old unit. This ex-wife wants her husband back and has a plan to destroy Imani. Who will win?<br/><br/>This film has a complex and intriguing storyline. The opening starts with Madeline's murder. So viewers don't know how it ties into the plot until later. The writer left several breadcrumbs for you to piece the story together. However, there's so much scandal that it's easy to ignore. The culprit's monologue carefully weaves this tale together and leaves your jaw on the floor. This compelling thriller is worthy of your time.<br/><br/>4 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/exdoorneighbor.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/exdoorneighbor.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Texas Two-Step",
-    "review": "Olivia pushes her workaholic boss, Meryl, out to the spa so she can start her vacation. As she gets Meryl into her car, Olivia's cousin, Mason, calls. Her aunt Ruby had a dizzy spell and injured herself. Mason worries that running Sweetwaters will exacerbate her condition, but he begs Olivia to visit and stay mum about his call. Olivia tells her friend, June, that she has to cancel their vacation plans. June arrives at Olivia's doorstep with her bags packed. She can't wait to see Olivia's hometown and the bar she talked so much about.<br/><br/>Olivia and June arrive at Sweetwaters and meet the Sugarfoots band. For Olivia, the local tavern looks, smells, and feels the same. Olivia sees that her aunt is in high spirits. She feels the same until she sees a stack of letters on Ruby's dining room table. It's from a wealthy tech tycoon, Bryan, and he wants to buy Sweetwaters. Ruby wouldn't dream of selling the business she and her late husband, Pete, built. However, Sweetwaters is in trouble. Pete missed several tax filings and owes $47,000. If they don't pay, Ruby could lose Sweetwaters forever. Can Olivia rescue it?<br/><br/>Unlike other hallmark movies, this film takes its time revealing the conflict. That's because it focuses on Olivia choosing between Bryan and her high-school love, Luke. That's supposed to be the focal point of the film. However, Hallmarkies know who she will choose. So, the constant back and forth distracts from the film's heart: Ruby and Junebug. Ruby and Junebug play a pivotal role in this film and offer wisdom to everyone. This film's western feel is fun, but viewers will be more interested in the subplots than the love story.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/texastwostep.jpeg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/texastwostep.jpeg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Jealous Bride",
-    "review": "Meg gets texts with directions to a spot. Her boyfriend, Josh, runs up to her and says she missed it. He brushes off the incident and walks Meg over to her proposal. Josh gets on one knee and asks Meg to marry him. She says yes.<br/><br/>Later, they throw a party to celebrate their engagement with their married friends Tracy and Rich. When Tracy sees women flirting with Josh, she runs to Meg's side. Meg stalked her lover's wife in the past and almost lost everything trying to earn his love. Tracy warns Meg that Josh isn't her ex-lover, so she should focus on the future. Meg agrees, walks over to Josh, and pulls him away from the women.<br/><br/>Josh and Rich laugh about marriage at work when their boss, Jerry, approaches Josh. They have a new lawyer at the office, and he wants Josh to mentor her. Josh concedes and meets his mentee, Simone. He flashes back to the past. Simone was the stripper who danced for him at Rich's bachelor party. When Jerry exits, they acknowledge their past but agree never to bring it up in a way that would affect her job or his marriage.<br/><br/>Josh, Meg, Rich, and Tracy go to dinner, and Simone saddles up to their table. She giggles, converses, and leaves. But something rubs Meg the wrong way. Later, Meg helps Josh unpack his things and finds a picture of him with Simone. He admits that he knew her before, but it wasn't a big deal. He cuts up the picture to prove it.<br/><br/>As they sleep, she hears a noise downstairs. Meg walks to the front door and finds it unlocked. She opens the door and spies someone running away. Meg locks the door, finds the picture fragments, and tapes them together. She knows Simone is a threat. Meg will do anything to keep Josh's love. But is she right to fear Simone, or is Meg's jealousy driving her crazy?<br/><br/>You can't miss the film's opening sequence. It introduces Meg's unhinged past and someone's obsession with her. Unlike other Lifetime films, none of the leads are inherently good. The writers made Tracy the voice of reason. She calls Meg out for her bad behavior and tries to rein her in. This movie has a great pace and plenty of crazy to go around. The conclusion leaves the script open for a part two, but it's not necessary.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thejealousbride.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thejealousbride.jpg"
     }
   }
 ];
@@ -59,4 +59,30 @@ module.exports = madeForTV;
 
 
 
+
+
+{
+"option": "1",
+"mtitle": "Christmas Under Construction",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/christmasunderconstruction.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/christmasunderconstruction.jpg"
+}},
+{
+"option": "2",
+"mtitle": "Get Off My Lawn",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/getoffmylawn.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/getoffmylawn.jpg"
+}},
+{
+"option": "3",
+"mtitle": "A Castle of Our Own",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/castleofourown.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/castleofourown.jpg"
+}}
 */
