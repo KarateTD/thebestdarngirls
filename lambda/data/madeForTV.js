8beec6c1bf5f52849e2704d018384c7da6cbe902
Tiffany Gray <KarateTD@yahoo.com>
2026-07-11 23:59:00 -0400

    add 2 movies

--- lambda/data/madeForTV.js
+++ lambda/data/madeForTV.js
@@ -1,6 +1,24 @@
 var madeForTV = [
   {
     "option": "1",
+    "mtitle": "He Couldn't Let Go",
+    "review": "Marianna and Faye enter a conference room to speak to Herbert. He assumes they want to discuss his company's security breach. He assures them that the impact was minimal and that they handled it. However, Marianna confronts Herbert about sexual harassment. When Herbert's cornered, he threatens her. Herbert whispers a threat in her ear before exiting. Marianna will be on high alert until she knows Herbert's mental state.<br/><br/>Marianna relaxes at home until she sees something outside her bedroom window. As she tiptoes over to look, her boyfriend, Nick, appears behind her. She yelps, and he apologizes for scaring her. Later, he gets a message from his friend Dylan. Dylan will be in town for a night and wants to cook for them tomorrow evening. Marianna rolls her eyes and agrees to welcome Dylan.<br/><br/>After work, she gets a text from Nick. He will be late because he's meeting with a client. Marianna arrives home but stops to find her front door ajar. Dylan pops out and comments that Nick told him to use the spare key. Throughout the night, something about Dylan's body language concerns Marianna. Dylan is hiding something. Can Marianna unlock the truth before it's too late?<br/><br/>This calm thriller has a slow pace, leading to a foreseeable twist. The setup gives the viewer plenty of potential threats to choose from. he backing track added some needed thrills to the plot and helped emphasize when you needed to care. This film has a few surprises but lacks inspiration.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/hecouldntletgo.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/hecouldntletgo.jpg"
+    }
+  },
+  {
+    "option": "2",
+    "mtitle": "The Night Everything Changed",
+    "review": "Ellie steals money from her mother, Tina, and stuffs it into her duffel bag. Tina stops Ellie in the kitchen to update her about their upcoming move. Ellie groans because she wants to stay near her boyfriend, Caleb. She brushes it off and leaves to spend the night at her friend's home.<br/><br/>Ellie arrives at Jessica's and greets Jessica's mom, Rachel. Ellie's other friends, Lily and Kendall, are in Jessica's bedroom. They worry that going out tonight is a bad idea. Someone leaked intimate photos of Jessica around the school, but Jessica won't let that deter her. She plans to party with her head held high. But the party is just a ruse. Ellie and Caleb planned to run away together.<br/><br/>The girls enter the party and have fun. Then, Lily pulls Ellie aside and begs her not to leave with Caleb. Lily confesses that Caleb is cheating on Ellie. Ellie darts to find Caleb and confronts him. He follows her outside.<br/><br/>Tina gets word that Ellie missed her morning tennis lesson. Tina arrives at Rachel's house and explains the situation. Rachel informs Tina that the girls went to a party, unbeknownst to Tina. Then, Jessica tells Tina about Ellie's plan to run away. The police uncover party footage of Ellie's argument with Caleb. Now, they fear that Caleb killed Ellie before they could run away. Tina will stop at nothing to find and save Ellie. But will she get there in time?<br/><br/>This suspense thriller spends the first act building the world around Ellie and Tina. Viewers learn Ellie's runaway plot, but you know there is something more when a hooded stranger grabs her from behind. By the third act, the audience knows Ellie's status. So the script changes from 'where is Ellie?' to 'who kidnapped Ellie?' While it's a change in direction, it keeps you on the hook. However, the motive falls flat because of petty rivalries, high school drama, and cattiness. While you don't need to rush home to watch this film, you can use it as a teaching moment for your teens.<br/><br/>3 out of 5 stars.",
+    "image": {
+      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thenighteverythingchanged.jpg",
+      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thenighteverythingchanged.jpg"
+    }
+  },
+  {
+    "option": "3",
     "mtitle": "Christmas Under Construction",
     "review": "Chelsea and Jake, a couple, co-host a home renovation show called Renovation Romance. While they work with the couple to renovate their home, Chelsea, a therapist, guides them through relationship issues. After wrapping their 1st season, network executive Don throws a party to celebrate because their show has the best ratings. Don admits he's unhappy. They follow him to his office. The network lost the bid for the Christmas Parade and will lose advertising without a replacement show. He wants Chelsea and Jake to film a Christmas special in 10 days. The network has a couple in mind because they have a beautiful Christmas cabin. Chelsea agrees.<br/><br/>Chelsea, Jake, and Bethany arrive in New Hampshire. Their driver picks them up from the airport and takes them to the cabin. During the ride, Bethany discusses the couple, Cooper and Janice. Cooper and Janice disagree with the renovation ideas. She wants something more modern, while he wants to keep the family cabin as is. Bethany swoons over the handsome Cooper, while Chelsea maintains her cool. When they arrive at the residence, Jake handles calls outside while Chelsea looks around the home. The driver meets her inside and introduces himself as Cooper. He laughs at Bethany's compliments before they meet with him and Janice.<br/><br/>Chelsea and Jake conduct their first interview with Cooper and Janice for the show. Janice feels uncomfortable when they discuss their personal issues. So she will fly to New York to take care of business while Cooper stays and helps the show with the project. Chelsea and Bethany scramble to find a solution because the show relies on couples. Jake plans to travel to New York for a meeting. He will go with Janice to tape her segments and get design inspirations. Will Cooper and Chelsea rebuild something more than a home this Christmas?<br/><br/>This holiday romance is the first in the Christmas in July slate. The sets have a beautiful holiday feel with lights, snow, and massive Christmas trees. The script has the Hallmark holiday tropes with a meet-cute, snowfall, and Christmas kiss. While the moments feel predictable, the writers made the clichés part of the show. The chemistry between Chelsea and Cooper feels loving, but viewers will feel the disconnect between their partners. This helps build the romance between the leads. While this is a solid film, you will disconnect as the pace slows. However, it's an entertaining start to this season's programming.<br/><br/>3.5 out of 5 stars.",
     "image": {
@@ -9,7 +27,7 @@ var madeForTV = [
     }
   },
   {
-    "option": "2",
+    "option": "4",
     "mtitle": "Get Off My Lawn",
     "review": "Alec walks by his grandfather's old home with his friends, Ethan and Ray. He introduces them to the new couple, Jackie and Jason. They moved into the home today and can't believe their luck. They bid below market value and got the home because they agreed to keep it as is and skipped the inspection. Alec offers to show them the home's secrets, and they let him inside.<br/><br/>Alec shows them his grandfather's rules, their family pictures, and his growth chart. When Jackie mentions taking it down, Alec's rage builds. He thought they would leave the home as is and not destroy his family's memories. He tampers down his anger and asks them to let him mark his height one last time.<br/><br/>After a few more incidents, Alec decides the couple doesn't deserve his grandfather's home. Ray says he can pull pranks on the couple, post them on social media, and make money going viral. Then, he can use the proceeds to buy the home and kick them out. It's all fun and games until the pranks turn violent. Can Jackie and Jason survive this deranged teenager?<br/><br/>This film is listed as a thriller, but it's pure comedy. The 'teens' are too old, the fight scenes are ridiculous, and the plot is outlandish. And the edits are terrible. The film's only redeeming quality is that it's so bad it's funny. You will laugh with each jump scare, threat, and act of violence. Lifetimers should skip this film.<br/><br/>1.5 out of 5 stars.",
     "image": {
@@ -18,30 +36,12 @@ var madeForTV = [
     }
   },
   {
-    "option": "3",
+    "option": "5",
     "mtitle": "A Castle of Our Own",
     "review": "Lily sits in her mother's office waiting for a ride to her father, Dan's, home. Since they separated, Dan and Marley have gotten along for the sake of Lily. Dan and Lily have plans to drive up the coast and stay in Cannon Cove to reconnect. Marley and Lily leave to drive to Dan's home. When Lily goes outside with her grandmother, Dan asks Marley to talk. He's competing for a lucrative contract and needs more time to work on it. He asks Marley to drive Lily up the coast, and he will meet them in Cannon Cove later. Marley agrees. Later, Marley's boss, Joan, gives Marley 12 strip malls to design. Joan hints that Lily will be too wrapped up in her phone to care that Marley is working. Marley promises to get the designs completed during her vacation.<br/><br/>Lily and Marley arrive at the rental home and discover the water isn't working. They call the landlord, who contacts a plumber. Marley steps outside and sees a man, Adam, pulling tools out of his truck. She assumes he's the plumber and walks him inside. He explains that he's the neighbor and a contractor. She apologizes to him before Adam leaves.<br/><br/>Marley and Lily check out the exhibits at the Cannon Cove Sandcastle Festival. The MC tells the crowd that they still have time to sign up for the sandcastle competition, and amateurs are welcome. Marley convinces Lily to join the competition.<br/><br/>Marley watches YouTube videos about building sandcastles, but the tips don't work. Adam, walking the beach, comments that the sand-to-water ratio is off. He built sandcastles in the past and was pretty good at it. During their conversation, Marley's phone buzzes several times, and she answers. She needs this competition to connect to Lily. He offers to join Marley's team if she puts the phone down. She agrees. The two will build a castle and a romance.<br/><br/>This romantic film has great chemistry between the lead characters. The humor between the pair is effortless because it's simple dialogue. However, the conflict isn't really between them. It's between who Marley is and who she wants to become. Adam does have issues, but they're with his past. Viewers know that it will be a non-factor. While the plot is predictable, the story is beautifully written and deserves your attention.<br/><br/>3.5 out of 5 stars.",
     "image": {
       "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/castleofourown.jpg",
       "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/castleofourown.jpg"
-    }
-  },
-  {
-    "option": "4",
-    "mtitle": "Single Black Tenant",
-    "review": "Esme has fallen on hard times. She works as a mechanic at her best friend's garage, but it doesn't allow her to save enough for a deposit. Today, Lydia will drive Esme around to check out rooms for rent that don't require a deposit. Esme arrives late to the appointment, but the owner, Angie, is ecstatic to see her. However, Angie's husband, Nolan, isn't friendly. Esme reveals that she's handy, and Angie offers Esme a deal. She will lower the rent if Esme fixes up the old unit. Esme agrees, signs the paperwork, and moves in.<br/><br/>Later, Angie tells Esme that she and Nolan don't own the home. The original owner, Barbie, left and never came back. While fixing up the apartment, Esme finds Barbie's ID, bloody rags, and a horrible odor. She believes Nolan must have done something, and Angie is hiding it. When Esme reports her suspicions to the police, they label her a true-crime fanatic. Esme won't give up. She will prove that Nolan is behind Barbie's disappearance and free Angie from his control.<br/><br/>This film isn't about the culprit because Lifetimers can spot them from a mile away. Its focus is the motive, but the plot foreshadows too much and you can guess it. It ruins the suspense of the story. One beacon of hope is the friendship between Lydia and Esme. They see the wedge in their relationship and form a tighter bond. This film could be compelling, but it's too predictable.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/singleblacktenant.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/singleblacktenant.jpg"
-    }
-  },
-  {
-    "option": "5",
-    "mtitle": "The Woman with Three Husbands",
-    "review": "Natalie arrives at her brother's home with solemn news. Their father died, but they feel nothing because their father was emotionally abusive. Natalie's brother, Marcus, invites her into his home to catch up. Moments later, a woman named Larissa enters and introduces herself as Marcus's wife. Then, two men enter the kitchen. Marcus stumbles during their introduction, and Larissa giggles. She tells Marcus to be honest. Jack and Nico are her husbands too. Marcus assures Natalie that he's happy and asks Natalie to stay for the night. Although Jack isn't happy about the arrangements.<br/><br/>The family goes to the pub with Natalie and has a fun night. Natalie walks to the bar and sparks a conversation with Ken. He gossips about every person there. When Natalie points to Marcus, he warns her to stay away from them. He doesn't care about their alternative lifestyle, but he believes one of them is a killer. She begs her brother to leave, but he refuses. He walks away, and Natalie hides as she overhears Nico and Jack arguing. After Nico exits, Jack tells her to leave now, or he will make her.<br/><br/>Marcus notices that Jack missed a critical chore in the morning. So the family and Natalie search the property for him. They find Jack's body in an embankment. Natalie fears her brother is living with a murderer. She won't leave or believe the police's findings. She will stay and help on the farm while trying to find a killer.<br/><br/>This script dips its toe into polyamorous relationships without being judgmental. As each husband talks about meeting Larissa, the viewer will notice a common thread between their story. Now, the killer isn't surprising, but the motive will make your head spin. That final twist saves the film from another Lifetime lost cause. Viewers are left to wonder what the difference is between this family and a cult. This plot is wild, and the pacing is perfect.<br/><br/>3.5 out of 5 stars.",
-    "image": {
-      "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thewomanwiththreehusbands.jpg",
-      "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thewomanwiththreehusbands.jpg"
     }
   }
 ];
@@ -60,20 +60,47 @@ module.exports = madeForTV;
 
 {
 "option": "1",
-"mtitle": "He Couldn't Let Go",
+"mtitle": "Crowning at the Prom",
 "review": "<br/><br/>* out of 5 stars.",
 "image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/hecouldntletgo.jpg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/hecouldntletgo.jpg"
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/crowningattheprom.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/crowningattheprom.jpg"
 }
 },
 {
 "option": "2",
-"mtitle": "The Night Everything Changed",
+"mtitle": "My Daughter's Trapped in a Throuple",
 "review": "<br/><br/>* out of 5 stars.",
 "image": {
-"smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/thenighteverythingchanged.jpg",
-"largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/thenighteverythingchanged.jpg"
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/mydaughterstrappedinathrouple.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/mydaughterstrappedinathrouple.jpg"
+}
+},
+{
+"option": "3",
+"mtitle": "Disney's Descendants Wicked Wonderland",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/descedantswickedwonderland.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/descedantswickedwonderland.jpg"
+}
+},
+{
+"option": "4",
+"mtitle": "Deadly Sign-In",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/deadlysignin.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/deadlysignin.jpg"
+}
+},
+{
+"option": "5",
+"mtitle": "O Little Christmas Market",
+"review": "<br/><br/>* out of 5 stars.",
+"image": {
+  "smallImageUrl": "https://thebestdarngirls.s3.amazonaws.com/small-image/olittlechristmasmarket.jpg",
+  "largeImageUrl": "https://thebestdarngirls.s3.amazonaws.com/large-image/olittlechristmasmarket.jpg"
 }
 }
 
